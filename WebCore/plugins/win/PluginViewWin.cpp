6bdc1de674b9691d7e7712642abdbbd3d02fd403
sfalken <sfalken@268f45cc-cd09-0410-ab3c-d52691b4dbfc>
2007-10-23 00:26:23 +0000

    Merged fix from r26898.
    git-svn-id: http://svn.webkit.org/repository/webkit/branches/Safari-3-branch@26900 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebCore/plugins/win/PluginViewWin.cpp
+++ WebCore/plugins/win/PluginViewWin.cpp
@@ -56,6 +56,7 @@
 #include "Settings.h"
 #include <kjs/JSLock.h>
 #include <kjs/value.h>
+#include <WebCore/MIMETypeRegistry.h>
 
 using KJS::ExecState;
 using KJS::Interpreter;
@@ -1357,6 +1358,11 @@ void PluginViewWin::determineQuirks(const String& mimeType)
     // Thus, we don't know that the object should be invalidated when the plug-in instance goes away.
     // See <rdar://problem/5487742>.
     if (mimeType == "application/x-silverlight")
+        m_quirks |= PluginQuirkDontUnloadPlugin;
+
+    // Because a single process cannot create multiple VMs, and we cannot reliably unload a
+    // Java VM, we cannot unload the Java plugin, or we'll lose reference to our only VM
+    if (MIMETypeRegistry::isJavaAppletMIMEType(mimeType))
         m_quirks |= PluginQuirkDontUnloadPlugin;
 
     // Prevent the Real plugin from calling the Window Proc recursively, causing the stack to overflow.
