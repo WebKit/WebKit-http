be7b257ae11cb8a35117c19031565b2e8a1d92cb
mrowe@apple.com <mrowe@apple.com@268f45cc-cd09-0410-ab3c-d52691b4dbfc>
2008-02-18 19:55:21 +0000

    Merge r30370.
    git-svn-id: http://svn.webkit.org/repository/webkit/branches/Safari-3-1-branch@30376 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebCore/plugins/PluginStream.cpp
+++ WebCore/plugins/PluginStream.cpp
@@ -174,7 +174,12 @@ void PluginStream::startStream()
     // Protect the stream if destroystream is called from within the newstream handler
     RefPtr<PluginStream> protect(this);
 
+    // calling into a plug-in could result in re-entrance if the plug-in yields
+    // control to the system (rdar://5744899). prevent this by deferring further
+    // loading while calling into the plug-in.
+    m_loader->setDefersLoading(true);
     NPError npErr = m_pluginFuncs->newstream(m_instance, (NPMIMEType)mimeTypeStr.data(), &m_stream, false, &m_transferMode);
+    m_loader->setDefersLoading(false);
     
     // If the stream was destroyed in the call to newstream we return
     if (m_reason != WebReasonNone)
@@ -238,10 +243,14 @@ void PluginStream::destroyStream()
         if (m_reason == NPRES_DONE && (m_transferMode == NP_ASFILE || m_transferMode == NP_ASFILEONLY)) {
             ASSERT(!m_path.isNull());
 
+            m_loader->setDefersLoading(true);
             m_pluginFuncs->asfile(m_instance, &m_stream, m_path.data());
+            m_loader->setDefersLoading(false);
         }
 
+        m_loader->setDefersLoading(true);
         NPError npErr = m_pluginFuncs->destroystream(m_instance, &m_stream, m_reason);
+        m_loader->setDefersLoading(false);
         LOG_NPERROR(npErr);
 
         m_stream.ndata = 0;
@@ -250,6 +259,7 @@ void PluginStream::destroyStream()
     if (m_sendNotification) {
         // Flash 9 can dereference null if we call NPP_URLNotify without first calling NPP_NewStream
         // for requests made with NPN_PostURLNotify; see <rdar://5588807>
+        m_loader->setDefersLoading(true);
         if (!newStreamCalled && m_quirks.contains(PluginQuirkFlashURLNotifyBug) &&
             equalIgnoringCase(m_resourceRequest.httpMethod(), "POST")) {
             // Protect the stream if NPN_DestroyStream is called from NPP_NewStream
@@ -267,6 +277,7 @@ void PluginStream::destroyStream()
             m_stream.url = 0;
         }
         m_pluginFuncs->urlnotify(m_instance, m_resourceRequest.url().deprecatedString().utf8(), m_reason, m_notifyData);
+        m_loader->setDefersLoading(false);
     }
 
     m_streamState = StreamStopped;
@@ -305,6 +316,7 @@ void PluginStream::deliverData()
     int32 totalBytes = m_deliveryData->size();
     int32 totalBytesDelivered = 0;
 
+    m_loader->setDefersLoading(true);
     while (totalBytesDelivered < totalBytes) {
         int32 deliveryBytes = m_pluginFuncs->writeready(m_instance, &m_stream);
 
@@ -328,6 +340,7 @@ void PluginStream::deliverData()
             totalBytesDelivered += deliveryBytes;
         }
     }
+    m_loader->setDefersLoading(false);
 
     if (totalBytesDelivered > 0) {
         if (totalBytesDelivered < totalBytes) {
@@ -402,10 +415,14 @@ void PluginStream::didFail(NetscapePlugInStreamLoader* loader, const ResourceErr
 {
     ASSERT(loader == m_loader);
 
-    m_loader = 0;
-
     LOG_PLUGIN_NET_ERROR();
+
+    // destroyStream can result in our being deleted
+    RefPtr<PluginStream> protect(this);
+
     destroyStream(NPRES_NETWORK_ERR);
+
+    m_loader = 0;
 }
 
 void PluginStream::didFinishLoading(NetscapePlugInStreamLoader* loader)
@@ -413,9 +430,12 @@ void PluginStream::didFinishLoading(NetscapePlugInStreamLoader* loader)
     ASSERT(loader == m_loader);
     ASSERT(m_streamState == StreamStarted);
 
-    m_loader = 0;
+    // destroyStream can result in our being deleted
+    RefPtr<PluginStream> protect(this);
 
     destroyStream(NPRES_DONE);
+
+    m_loader = 0;
 }
 
 }
