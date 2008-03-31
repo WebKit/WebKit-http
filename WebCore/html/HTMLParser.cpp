9db15001286cd8af6c348590807355918e566c04
mrowe@apple.com <mrowe@apple.com@268f45cc-cd09-0410-ab3c-d52691b4dbfc>
2008-03-31 20:59:35 +0000

    Roll out r31407.
    git-svn-id: http://svn.webkit.org/repository/webkit/branches/Safari-3-1-branch@31471 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebCore/html/HTMLParser.cpp
+++ WebCore/html/HTMLParser.cpp
@@ -318,7 +318,6 @@ bool HTMLParser::insertNode(Node* n, bool flat)
             // optimized version of setCurrent that takes advantage of that fact and also
             // assumes that newNode is neither 0 nor a pointer to the document.
             pushBlock(localName, tagPriority);
-            newNode->beginParsingChildren();
             ASSERT(!didRefCurrent);
             newNode->ref(); 
             current = newNode;
@@ -407,7 +406,6 @@ bool HTMLParser::handleError(Node* n, bool flat, const AtomicString& localName, 
                     reportError(MisplacedHeadContentError, &localName, &current->localName());
                 
                 pushBlock(localName, tagPriority);
-                newNode->beginParsingChildren();
                 setCurrent(newNode);
                 if (!n->attached() && !m_isParsingFragment)
                     n->attach();
@@ -551,7 +549,6 @@ bool HTMLParser::handleError(Node* n, bool flat, const AtomicString& localName, 
                             !flat && static_cast<HTMLElement*>(n)->endTagRequirement() != TagStatusForbidden)
                         {
                             pushBlock(localName, tagPriority);
-                            n->beginParsingChildren();
                             setCurrent(n);
                             inStrayTableContent++;
                             blockStack->strayTableContent = true;
@@ -1203,7 +1200,6 @@ void HTMLParser::reopenResidualStyleTags(HTMLStackElem* elem, Node* malformedTab
 
         // Now push a new stack element for this node we just created.
         pushBlock(elem->tagName, elem->level);
-        newNode->beginParsingChildren();
 
         // Set our strayTableContent boolean if needed, so that the reopened tag also knows
         // that it is inside a malformed table.
@@ -1227,6 +1223,7 @@ void HTMLParser::reopenResidualStyleTags(HTMLStackElem* elem, Node* malformedTab
 
 void HTMLParser::pushBlock(const AtomicString& tagName, int level)
 {
+    current->beginParsingChildren();
     blockStack = new HTMLStackElem(tagName, level, current, didRefCurrent, blockStack);
     didRefCurrent = false;
 }
