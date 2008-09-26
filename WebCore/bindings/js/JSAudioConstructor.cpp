WebKit/WebKit-http

commit 39a180f18766ab5fa074d4075278eb55717677fc

JavaScriptCore:
2008-09-19  Darin Adler  <darin@apple.com>

        Reviewed by Sam Weinig.

        - part 2 of https://bugs.webkit.org/show_bug.cgi?id=20858
          make each distinct C++ class get a distinct JSC::Structure

        * JavaScriptCore.exp: Exported constructEmptyObject for use in WebCore.

        * kjs/JSGlobalObject.h: Changed the protected constructor to take a
        structure instead of a prototype.

        * kjs/JSVariableObject.h: Removed constructor that takes a prototype.

WebCore:

2008-09-19  Darin Adler  <darin@apple.com>

        Reviewed by Sam Weinig.

        - part 2 of https://bugs.webkit.org/show_bug.cgi?id=20858
          make each distinct C++ class get a distinct JSC::Structure

          + Fixed all cases where we were using a shared structure for multiple
            C++ classes in WebCore. This still has to be done in JavaScriptCore.

          + Got rid of cacheGlobalObject.

          + Improved use of PassRefPtr in bindings code.

          + Removed a couple cases where we were potentially allocating prototypes
            inside a JSObject's construction process -- this can lead to trouble if
            we do a garbage collection while an object is only partly constructed.

        * bindings/js/JSAudioConstructor.cpp:
        (WebCore::JSAudioConstructor::JSAudioConstructor): Create a structure explicitly
        so we don't implicitly share the structure with other objects that use the object
        prototype.

        * bindings/js/JSDOMBinding.cpp:
        (WebCore::getCachedDOMConstructor): Added. To be used for constructors so we
        don't need cacheGlobalObject any more.
        (WebCore::cacheDOMConstructor): Ditto.

        * bindings/js/JSDOMBinding.h: Removed DOMObject constructor that takes a prototype.
        Added functions and a function template for getting cached DOM constructors.
        Removed cacheGlobalObject function template.

        * bindings/js/JSDOMWindowBase.cpp:

WebCore/bindings/js/JSAudioConstructor.cpp
@@ -41,7 +41,7 @@ namespace WebCore {
 const ClassInfo JSAudioConstructor::s_info = { "AudioConstructor", 0, 0, 0 };
 
 JSAudioConstructor::JSAudioConstructor(ExecState* exec, Document* document)
-    : DOMObject(exec->lexicalGlobalObject()->objectPrototype())
+    : DOMObject(StructureID::create(exec->lexicalGlobalObject()->objectPrototype()))
     , m_document(document)
 {
     putDirect(exec->propertyNames().length, jsNumber(exec, 1), ReadOnly|DontDelete|DontEnum);

WebCore/bindings/js/JSDOMWindowBase.cpp
@@ -194,17 +194,17 @@ const ClassInfo JSDOMWindowBase::s_info = { "Window", 0, &JSDOMWindowBaseTable, 
 @end
 */
 
-JSDOMWindowBase::JSDOMWindowBaseData::JSDOMWindowBaseData(PassRefPtr<DOMWindow> window_, JSDOMWindowBase* jsWindow_, JSDOMWindowShell* shell_)
-    : JSGlobalObjectData(jsWindow_, shell_)
-    , impl(window_)
+JSDOMWindowBase::JSDOMWindowBaseData::JSDOMWindowBaseData(PassRefPtr<DOMWindow> window, JSDOMWindowBase* jsWindow, JSDOMWindowShell* shell)
+    : JSGlobalObjectData(jsWindow, shell)
+    , impl(window)
     , evt(0)
     , returnValueSlot(0)
-    , shell(shell_)
+    , shell(shell)
 {
 }
 
-JSDOMWindowBase::JSDOMWindowBase(JSObject* prototype, DOMWindow* window, JSDOMWindowShell* shell)
-    : JSGlobalObject(prototype, new JSDOMWindowBaseData(window, this, shell), shell)
+JSDOMWindowBase::JSDOMWindowBase(PassRefPtr<StructureID> structure, PassRefPtr<DOMWindow> window, JSDOMWindowShell* shell)
+    : JSGlobalObject(structure, new JSDOMWindowBaseData(window, this, shell), shell)
 {
     // Time in milliseconds before the script timeout handler kicks in.
     setTimeoutTime(10000);

WebCore/bindings/js/JSDOMWindowBase.h
@@ -45,6 +45,7 @@ namespace WebCore {
     class JSDOMWindowBasePrivate;
 
     typedef HashMap<const JSC::ClassInfo*, RefPtr<JSC::StructureID> > JSDOMStructureMap;
+    typedef HashMap<const JSC::ClassInfo*, JSC::JSObject*> JSDOMConstructorMap;
 
     // This is the only WebCore JS binding which does not inherit from DOMObject
     class JSDOMWindowBase : public JSC::JSGlobalObject {
@@ -52,7 +53,7 @@ namespace WebCore {
 
         friend class ScheduledAction;
     protected:
-        JSDOMWindowBase(JSC::JSObject* prototype, DOMWindow*, JSDOMWindowShell*);
+        JSDOMWindowBase(PassRefPtr<JSC::StructureID>, PassRefPtr<DOMWindow>, JSDOMWindowShell*);
 
     public:
         virtual ~JSDOMWindowBase();
@@ -128,6 +129,7 @@ namespace WebCore {
         void clearAllTimeouts();
 
         JSDOMStructureMap& structures() { return d()->structures; }
+        JSDOMConstructorMap& constructors() { return d()->constructors; }
 
         enum {
             // Attributes
@@ -166,6 +168,7 @@ namespace WebCore {
             TimeoutsMap timeouts;
 
             JSDOMStructureMap structures;
+            JSDOMConstructorMap constructors;
         };
         
         JSC::JSValue* getListener(JSC::ExecState*, const AtomicString& eventType) const;

WebCore/bindings/js/JSHTMLOptionElementConstructor.cpp
@@ -34,7 +34,7 @@ ASSERT_CLASS_FITS_IN_CELL(JSHTMLOptionElementConstructor)
 const ClassInfo JSHTMLOptionElementConstructor::s_info = { "OptionConstructor", 0, 0, 0 };
 
 JSHTMLOptionElementConstructor::JSHTMLOptionElementConstructor(ExecState* exec, Document* document)
-    : DOMObject(exec->lexicalGlobalObject()->objectPrototype())
+    : DOMObject(StructureID::create(exec->lexicalGlobalObject()->objectPrototype()))
     , m_document(document)
 {
     putDirect(exec->propertyNames().length, jsNumber(exec, 4), ReadOnly|DontDelete|DontEnum);

WebCore/bindings/js/JSImageConstructor.cpp
@@ -33,7 +33,7 @@ ASSERT_CLASS_FITS_IN_CELL(JSImageConstructor)
 const ClassInfo JSImageConstructor::s_info = { "ImageConstructor", 0, 0, 0 };
 
 JSImageConstructor::JSImageConstructor(ExecState* exec, Document* document)
-    : DOMObject(exec->lexicalGlobalObject()->objectPrototype())
+    : DOMObject(StructureID::create(exec->lexicalGlobalObject()->objectPrototype()))
     , m_document(document)
 {
 }

WebCore/bindings/js/JSXMLHttpRequestConstructor.cpp
@@ -33,7 +33,7 @@ ASSERT_CLASS_FITS_IN_CELL(JSXMLHttpRequestConstructor)
 const ClassInfo JSXMLHttpRequestConstructor::s_info = { "XMLHttpRequestConstructor", 0, 0, 0 };
 
 JSXMLHttpRequestConstructor::JSXMLHttpRequestConstructor(ExecState* exec, Document* document)
-    : DOMObject(exec->lexicalGlobalObject()->objectPrototype())
+    : DOMObject(StructureID::create(exec->lexicalGlobalObject()->objectPrototype()))
     , m_document(document)
 {
     putDirect(exec->propertyNames().prototype, JSXMLHttpRequestPrototype::self(exec), None);

JavaScriptCore/JavaScriptCore.exp
@@ -157,6 +157,7 @@ __ZN3JSC17PrototypeFunctionC1EPNS_9ExecStateEiRKNS_10IdentifierEPFPNS_7JSValueES
 __ZN3JSC17constructFunctionEPNS_9ExecStateERKNS_7ArgListERKNS_10IdentifierERKNS_7UStringEi
 __ZN3JSC19constructEmptyArrayEPNS_9ExecStateE
 __ZN3JSC19initializeThreadingEv
+__ZN3JSC20constructEmptyObjectEPNS_9ExecStateE
 __ZN3JSC20staticFunctionGetterEPNS_9ExecStateERKNS_10IdentifierERKNS_12PropertySlotE
 __ZN3JSC23objectProtoFuncToStringEPNS_9ExecStateEPNS_8JSObjectEPNS_7JSValueERKNS_7ArgListE
 __ZN3JSC23setUpStaticFunctionSlotEPNS_9ExecStateEPKNS_9HashEntryEPNS_8JSObjectERKNS_10IdentifierERNS_12PropertySlotE

JavaScriptCore/kjs/JSGlobalObject.h
@@ -141,8 +141,8 @@ namespace JSC {
         }
 
     protected:
-        JSGlobalObject(JSObject* prototype, JSGlobalObjectData* d, JSObject* globalThisValue)
-            : JSVariableObject(prototype, d)
+        JSGlobalObject(PassRefPtr<StructureID> structure, JSGlobalObjectData* data, JSObject* globalThisValue)
+            : JSVariableObject(structure, data)
         {
             init(globalThisValue);
         }

commit 38feb8e1c3f1ff151bd6dbd4dd8007a68302c6df

Bug 21054: Construction of certain DOM objects is heavily regressed by r36675 <https://bugs.webkit.org/show_bug.cgi?id=21054>
Reviewed by Maciej Stachowiak

This performance regression is actually just a symptom of a correctness
bug.  The constructor objects for a number of properties that have security
checks on access were returning new objects each time.  The most obvious
symptom of this bug is that window.Image != window.Image, etc.

The solution to this is to make sure we cache these constructors
in the same way as all the other DOM constructors.  To achieve this
without causing any refcount cycles it is necessary to replace the
refcounted document pointer in the Image, MessageChannel, Option,
XMLHttpRequest, and Audio constructor objects with a reference to
the document's JS wrapper.

Tests: fast/dom/constructors-cached-navigate.html
       fast/dom/constructors-cached.html


git-svn-id: http://svn.webkit.org/repository/webkit/trunk@36954 268f45cc-cd09-0410-ab3c-d52691b4dbfc

WebCore/bindings/js/JSAudioConstructor.cpp
@@ -42,7 +42,7 @@ const ClassInfo JSAudioConstructor::s_info = { "AudioConstructor", 0, 0, 0 };
 
 JSAudioConstructor::JSAudioConstructor(ExecState* exec, Document* document)
     : DOMObject(JSAudioConstructor::createStructureID(exec->lexicalGlobalObject()->objectPrototype()))
-    , m_document(document)
+    , m_document(static_cast<JSDocument*>(toJS(exec, document)))
 {
     putDirect(exec->propertyNames().length, jsNumber(exec, 1), ReadOnly|DontDelete|DontEnum);
 }
@@ -63,6 +63,13 @@ ConstructType JSAudioConstructor::getConstructData(ConstructData& constructData)
 {
     constructData.native.function = constructAudio;
     return ConstructTypeHost;
+}
+
+void JSAudioConstructor::mark()
+{
+    DOMObject::mark();
+    if (!m_document->marked())
+        m_document->mark();
 }
 
 } // namespace WebCore

WebCore/bindings/js/JSAudioConstructor.h
@@ -29,6 +29,7 @@
 #if ENABLE(VIDEO)
 
 #include "JSDOMBinding.h"
+#include "JSDocument.h"
 #include <wtf/RefPtr.h>
 
 namespace WebCore {
@@ -37,16 +38,17 @@ namespace WebCore {
     public:
         JSAudioConstructor(JSC::ExecState*, Document*);
 
-        Document* document() const { return m_document.get(); }
+        Document* document() const { return m_document->impl(); }
 
         static const JSC::ClassInfo s_info;
 
+        virtual void mark();
     private:
         virtual JSC::ConstructType getConstructData(JSC::ConstructData&);
 
         virtual const JSC::ClassInfo* classInfo() const { return &s_info; }
 
-        RefPtr<Document> m_document;
+        JSDocument* m_document;
     };
 
 } // namespace WebCore

WebCore/bindings/js/JSDOMWindowBase.cpp
@@ -433,6 +433,16 @@ static JSValue* showModalDialog(ExecState* exec, Frame* frame, const String& url
     return returnValue ? returnValue : jsUndefined();
 }
 
+template<class ConstructorClass> static JSC::JSObject* getDOMConstructor(JSC::ExecState* exec, const JSDOMWindowBase* window)
+{
+    if (JSC::JSObject* constructor = window->constructors().get(&ConstructorClass::s_info))
+        return constructor;
+    JSC::JSObject* constructor = new (exec) ConstructorClass(exec, window->impl()->document());
+    ASSERT(!window->constructors().contains(&ConstructorClass::s_info));
+    window->constructors().set(&ConstructorClass::s_info, constructor);
+    return constructor;
+}
+
 JSValue *JSDOMWindowBase::getValueProperty(ExecState *exec, int token) const
 {
     ASSERT(impl()->frame());
@@ -449,28 +459,26 @@ JSValue *JSDOMWindowBase::getValueProperty(ExecState *exec, int token) const
     case Image:
         if (!allowsAccessFrom(exec))
             return jsUndefined();
-        // FIXME: this property (and the few below) probably shouldn't create a new object every
-        // time
-        return new (exec) JSImageConstructor(exec, impl()->frame()->document());
+        return getDOMConstructor<JSImageConstructor>(exec, this);
     case MessageChannel:
         if (!allowsAccessFrom(exec))
             return jsUndefined();
-        return new (exec) JSMessageChannelConstructor(exec, impl()->frame()->document());
+        return getDOMConstructor<JSMessageChannelConstructor>(exec, this);
     case Option:
         if (!allowsAccessFrom(exec))
             return jsUndefined();
-        return new (exec) JSHTMLOptionElementConstructor(exec, impl()->frame()->document());
+        return getDOMConstructor<JSHTMLOptionElementConstructor>(exec, this);
     case XMLHttpRequest:
         if (!allowsAccessFrom(exec))
             return jsUndefined();
-        return new (exec) JSXMLHttpRequestConstructor(exec, impl()->frame()->document());
+        return getDOMConstructor<JSXMLHttpRequestConstructor>(exec, this);
     case Audio:
 #if ENABLE(VIDEO)
         if (!allowsAccessFrom(exec))
             return jsUndefined();
         if (!MediaPlayer::isAvailable())
             return jsUndefined();
-        return new (exec) JSAudioConstructor(exec, impl()->frame()->document());
+        return getDOMConstructor<JSAudioConstructor>(exec, this);
 #else
         return jsUndefined();
 #endif
@@ -478,7 +486,7 @@ JSValue *JSDOMWindowBase::getValueProperty(ExecState *exec, int token) const
 #if ENABLE(XSLT)
         if (!allowsAccessFrom(exec))
             return jsUndefined();
-        return new (exec) JSXSLTProcessorConstructor(exec);
+        return getDOMConstructor<JSXSLTProcessorConstructor>(exec);
 #else
         return jsUndefined();
 #endif

WebCore/bindings/js/JSDOMWindowBase.h
@@ -129,7 +129,7 @@ namespace WebCore {
         void clearAllTimeouts();
 
         JSDOMStructureMap& structures() { return d()->structures; }
-        JSDOMConstructorMap& constructors() { return d()->constructors; }
+        JSDOMConstructorMap& constructors() const { return d()->constructors; }
 
         enum {
             // Attributes

WebCore/bindings/js/JSHTMLOptionElementConstructor.cpp
@@ -35,7 +35,7 @@ const ClassInfo JSHTMLOptionElementConstructor::s_info = { "OptionConstructor", 
 
 JSHTMLOptionElementConstructor::JSHTMLOptionElementConstructor(ExecState* exec, Document* document)
     : DOMObject(JSHTMLOptionElementConstructor::createStructureID(exec->lexicalGlobalObject()->objectPrototype()))
-    , m_document(document)
+    , m_document(static_cast<JSDocument*>(toJS(exec, document)))
 {
     putDirect(exec->propertyNames().length, jsNumber(exec, 4), ReadOnly|DontDelete|DontEnum);
 }
@@ -73,6 +73,13 @@ ConstructType JSHTMLOptionElementConstructor::getConstructData(ConstructData& co
 {
     constructData.native.function = constructHTMLOptionElement;
     return ConstructTypeHost;
+}
+
+void JSHTMLOptionElementConstructor::mark()
+{
+    DOMObject::mark();
+    if (!m_document->marked())
+        m_document->mark();
 }
 
 } // namespace WebCore

WebCore/bindings/js/JSHTMLOptionElementConstructor.h
@@ -21,6 +21,7 @@
 #define JSHTMLOptionElementConstructor_h
 
 #include "JSDOMBinding.h"
+#include "JSDocument.h"
 #include <wtf/RefPtr.h>
 
 namespace WebCore {
@@ -28,15 +29,16 @@ namespace WebCore {
     class JSHTMLOptionElementConstructor : public DOMObject {
     public:
         JSHTMLOptionElementConstructor(JSC::ExecState*, Document*);
-        Document* document() const { return m_document.get(); }
+        Document* document() const { return m_document->impl(); }
 
         static const JSC::ClassInfo s_info;
-
+        
+        virtual void mark();
     private:
         virtual JSC::ConstructType getConstructData(JSC::ConstructData&);
         virtual const JSC::ClassInfo* classInfo() const { return &s_info; }
 
-        RefPtr<Document> m_document;
+        JSDocument* m_document;
     };
 
 } // namespace WebCore

WebCore/bindings/js/JSImageConstructor.cpp
@@ -34,7 +34,7 @@ const ClassInfo JSImageConstructor::s_info = { "ImageConstructor", 0, 0, 0 };
 
 JSImageConstructor::JSImageConstructor(ExecState* exec, Document* document)
     : DOMObject(JSImageConstructor::createStructureID(exec->lexicalGlobalObject()->objectPrototype()))
-    , m_document(document)
+    , m_document(static_cast<JSDocument*>(toJS(exec, document)))
 {
 }
 
@@ -72,6 +72,13 @@ ConstructType JSImageConstructor::getConstructData(ConstructData& constructData)
 {
     constructData.native.function = constructImage;
     return ConstructTypeHost;
+}
+
+void JSImageConstructor::mark()
+{
+    DOMObject::mark();
+    if (!m_document->marked())
+        m_document->mark();
 }
 
 } // namespace WebCore

WebCore/bindings/js/JSImageConstructor.h
@@ -21,6 +21,7 @@
 #define JSImageConstructor_h
 
 #include "JSDOMBinding.h"
+#include "JSDocument.h"
 
 namespace WebCore {
 
@@ -29,15 +30,16 @@ namespace WebCore {
     class JSImageConstructor : public DOMObject {
     public:
         JSImageConstructor(JSC::ExecState*, Document*);
-        Document* document() const { return m_document.get(); }
+        Document* document() const { return m_document->impl(); }
 
         static const JSC::ClassInfo s_info;
-
+        
+        virtual void mark();
     private:
         virtual JSC::ConstructType getConstructData(JSC::ConstructData&);
         virtual const JSC::ClassInfo* classInfo() const { return &s_info; }
 
-        RefPtr<Document> m_document;
+        JSDocument* m_document;
     };
 
 } // namespace WebCore

WebCore/bindings/js/JSMessageChannelConstructor.cpp
@@ -38,7 +38,7 @@ const ClassInfo JSMessageChannelConstructor::s_info = { "MessageChannelConstruct
 
 JSMessageChannelConstructor::JSMessageChannelConstructor(ExecState* exec, Document* document)
     : DOMObject(JSMessageChannelConstructor::createStructureID(exec->lexicalGlobalObject()->objectPrototype()))
-    , m_document(document)
+    , m_document(static_cast<JSDocument*>(toJS(exec, document)))
 {
     putDirect(exec->propertyNames().prototype, JSMessageChannelPrototype::self(exec), None);
 }
@@ -55,7 +55,14 @@ ConstructType JSMessageChannelConstructor::getConstructData(ConstructData& const
 
 JSObject* JSMessageChannelConstructor::construct(ExecState* exec, JSObject* constructor, const ArgList&)
 {
-    return static_cast<JSObject*>(toJS(exec, MessageChannel::create(static_cast<JSMessageChannelConstructor*>(constructor)->m_document.get())));
+    return static_cast<JSObject*>(toJS(exec, MessageChannel::create(static_cast<JSMessageChannelConstructor*>(constructor)->document())));
+}
+
+void JSMessageChannelConstructor::mark()
+{
+    DOMObject::mark();
+    if (!m_document->marked())
+        m_document->mark();
 }
 
 } // namespace WebCore

WebCore/bindings/js/JSMessageChannelConstructor.h
@@ -27,6 +27,7 @@
 #define JSMessageChannelConstructor_h
 
 #include "JSDOMBinding.h"
+#include "JSDocument.h"
 
 namespace WebCore {
 
@@ -39,12 +40,15 @@ namespace WebCore {
         virtual const JSC::ClassInfo* classInfo() const { return &s_info; }
         static const JSC::ClassInfo s_info;
 
+        Document* document() const { return m_document->impl(); }
+
         virtual bool implementsHasInstance() const { return true; }
         static JSC::JSObject* construct(JSC::ExecState*, JSC::JSObject*, const JSC::ArgList&);
         virtual JSC::ConstructType getConstructData(JSC::ConstructData&);
 
+        virtual void mark();
     private:
-        RefPtr<Document> m_document;
+        JSDocument* m_document;
     };
 
 } // namespace WebCore

WebCore/bindings/js/JSXMLHttpRequestConstructor.cpp
@@ -34,7 +34,7 @@ const ClassInfo JSXMLHttpRequestConstructor::s_info = { "XMLHttpRequestConstruct
 
 JSXMLHttpRequestConstructor::JSXMLHttpRequestConstructor(ExecState* exec, Document* document)
     : DOMObject(JSXMLHttpRequestConstructor::createStructureID(exec->lexicalGlobalObject()->objectPrototype()))
-    , m_document(document)
+    , m_document(static_cast<JSDocument*>(toJS(exec, document)))
 {
     putDirect(exec->propertyNames().prototype, JSXMLHttpRequestPrototype::self(exec), None);
 }
@@ -49,6 +49,13 @@ ConstructType JSXMLHttpRequestConstructor::getConstructData(ConstructData& const
 {
     constructData.native.function = constructXMLHttpRequest;
     return ConstructTypeHost;
+}
+
+void JSXMLHttpRequestConstructor::mark()
+{
+    DOMObject::mark();
+    if (!m_document->marked())
+        m_document->mark();
 }
 
 } // namespace WebCore

WebCore/bindings/js/JSXMLHttpRequestConstructor.h
@@ -21,6 +21,7 @@
 #define JSXMLHttpRequestConstructor_h
 
 #include "JSDOMBinding.h"
+#include "JSDocument.h"
 
 namespace WebCore {
 
@@ -29,14 +30,15 @@ class Document;
 class JSXMLHttpRequestConstructor : public DOMObject {
 public:
     JSXMLHttpRequestConstructor(JSC::ExecState*, Document*);
-    Document* document() const { return m_document.get(); }
+    Document* document() const { return m_document->impl(); }
     static const JSC::ClassInfo s_info;
 
+    virtual void mark();
 private:
     virtual JSC::ConstructType getConstructData(JSC::ConstructData&);
     virtual const JSC::ClassInfo* classInfo() const { return &s_info; }
 
-    RefPtr<Document> m_document;
+    JSDocument* m_document;
 };
 
 } // namespace WebCore

LayoutTests/fast/dom/constructors-cached-expected.txt
@@ -1,0 +1,19 @@
+This test ensures that objects with security restrictions are cached correctly
+
+On success, you will see a series of "PASS" messages, followed by "TEST COMPLETE".
+
+
+PASS Image is Image
+PASS Image.testProperty is "property set successfully"
+PASS MessageChannel is MessageChannel
+PASS MessageChannel.testProperty is "property set successfully"
+PASS Option is Option
+PASS Option.testProperty is "property set successfully"
+PASS XMLHttpRequest is XMLHttpRequest
+PASS XMLHttpRequest.testProperty is "property set successfully"
+PASS Audio is Audio
+PASS Audio.testProperty is "property set successfully"
+PASS successfullyParsed is true
+
+TEST COMPLETE
+

LayoutTests/fast/dom/constructors-cached-navigate-expected.txt
@@ -1,0 +1,50 @@
+
+PASS successfullyParsed is true
+
+TEST COMPLETE
+PASS testFrame.contentWindow.Image is testFrame.contentWindow.Image
+PASS testFrame.contentWindow.Image !== window.Image is true
+PASS testFrame.contentWindow.Image.testProperty is "property set successfully"
+PASS testFrame.contentWindow.MessageChannel is testFrame.contentWindow.MessageChannel
+PASS testFrame.contentWindow.MessageChannel !== window.MessageChannel is true
+PASS testFrame.contentWindow.MessageChannel.testProperty is "property set successfully"
+PASS testFrame.contentWindow.Option is testFrame.contentWindow.Option
+PASS testFrame.contentWindow.Option !== window.Option is true
+PASS testFrame.contentWindow.Option.testProperty is "property set successfully"
+PASS testFrame.contentWindow.XMLHttpRequest is testFrame.contentWindow.XMLHttpRequest
+PASS testFrame.contentWindow.XMLHttpRequest !== window.XMLHttpRequest is true
+PASS testFrame.contentWindow.XMLHttpRequest.testProperty is "property set successfully"
+PASS testFrame.contentWindow.Audio is testFrame.contentWindow.Audio
+PASS testFrame.contentWindow.Audio !== window.Audio is true
+PASS testFrame.contentWindow.Audio.testProperty is "property set successfully"
+PASS testFrame.contentWindow.Image is testFrame.contentWindow.Image
+PASS testFrame.contentWindow.Image !== window.Image is true
+PASS testFrame.contentWindow.Image !== storedConstructors.Image is true
+PASS storedConstructors.Image.testProperty is "property set successfully"
+PASS testFrame.contentWindow.Image.testProperty is undefined.
+PASS testFrame.contentWindow.Image.cachedOnOwnerDocument is true
+PASS testFrame.contentWindow.MessageChannel is testFrame.contentWindow.MessageChannel
+PASS testFrame.contentWindow.MessageChannel !== window.MessageChannel is true
+PASS testFrame.contentWindow.MessageChannel !== storedConstructors.MessageChannel is true
+PASS storedConstructors.MessageChannel.testProperty is "property set successfully"
+PASS testFrame.contentWindow.MessageChannel.testProperty is undefined.
+PASS testFrame.contentWindow.MessageChannel.cachedOnOwnerDocument is true
+PASS testFrame.contentWindow.Option is testFrame.contentWindow.Option
+PASS testFrame.contentWindow.Option !== window.Option is true
+PASS testFrame.contentWindow.Option !== storedConstructors.Option is true
+PASS storedConstructors.Option.testProperty is "property set successfully"
+PASS testFrame.contentWindow.Option.testProperty is undefined.
+PASS testFrame.contentWindow.Option.cachedOnOwnerDocument is true
+PASS testFrame.contentWindow.XMLHttpRequest is testFrame.contentWindow.XMLHttpRequest
+PASS testFrame.contentWindow.XMLHttpRequest !== window.XMLHttpRequest is true
+PASS testFrame.contentWindow.XMLHttpRequest !== storedConstructors.XMLHttpRequest is true
+PASS storedConstructors.XMLHttpRequest.testProperty is "property set successfully"
+PASS testFrame.contentWindow.XMLHttpRequest.testProperty is undefined.
+PASS testFrame.contentWindow.XMLHttpRequest.cachedOnOwnerDocument is true
+PASS testFrame.contentWindow.Audio is testFrame.contentWindow.Audio
+PASS testFrame.contentWindow.Audio !== window.Audio is true
+PASS testFrame.contentWindow.Audio !== storedConstructors.Audio is true
+PASS storedConstructors.Audio.testProperty is "property set successfully"
+PASS testFrame.contentWindow.Audio.testProperty is undefined.
+PASS testFrame.contentWindow.Audio.cachedOnOwnerDocument is true
+

LayoutTests/fast/dom/constructors-cached-navigate.html
@@ -1,0 +1,14 @@
+<!DOCTYPE HTML PUBLIC "-//IETF//DTD HTML//EN">
+<html>
+<head>
+<link rel="stylesheet" href="../js/resources/js-test-style.css">
+<script src="../js/resources/js-test-pre.js"></script>
+</head>
+<body>
+<iframe id="testFrame"></iframe>
+<p id="description"></p>
+<div id="console"></div>
+<script src="resources/constructors-cached-navigate.js"></script>
+<script src="../js/resources/js-test-post.js"></script>
+</body>
+</html>

LayoutTests/fast/dom/constructors-cached.html
@@ -1,0 +1,13 @@
+<!DOCTYPE HTML PUBLIC "-//IETF//DTD HTML//EN">
+<html>
+<head>
+<link rel="stylesheet" href="../js/resources/js-test-style.css">
+<script src="../js/resources/js-test-pre.js"></script>
+</head>
+<body>
+<p id="description"></p>
+<div id="console"></div>
+<script src="resources/constructors-cached.js"></script>
+<script src="../js/resources/js-test-post.js"></script>
+</body>
+</html>

LayoutTests/fast/dom/resources/constructors-cached-navigate.js
@@ -1,0 +1,55 @@
+if (window.layoutTestController)
+    layoutTestController.waitUntilDone();
+
+var constructors = ["Image", "MessageChannel", "Option", "XMLHttpRequest", "Audio"];
+
+window.onload = function () {
+    testFrame = document.getElementById("testFrame");
+    storedConstructors = [];
+    // Identity checks
+    for (var i = 0; i < constructors.length; i++) {
+        var constructor = constructors[i];
+        try {
+            shouldBe("testFrame.contentWindow." + constructor, "testFrame.contentWindow."+constructor);
+            shouldBeTrue("testFrame.contentWindow." + constructor + " !== window." + constructor);
+            testFrame.contentWindow[constructor].testProperty = "property set successfully";
+            shouldBe("testFrame.contentWindow." + constructor + ".testProperty", '"property set successfully"');
+            storedConstructors[constructor] = testFrame.contentWindow[constructor];
+        } catch (e) {
+            testFailed("Testing " + constructor + " threw " + e);
+        }
+    }
+    testFrame.onload = function() {
+        if (window.GCController)
+            GCController.collect();
+
+        // Test properties after load
+        for (var i = 0; i < constructors.length; i++) {
+            var constructor = constructors[i];
+            try {
+                // Repeat the identity checks to be safe
+                shouldBe("testFrame.contentWindow." + constructor, "testFrame.contentWindow."+constructor);
+                shouldBeTrue("testFrame.contentWindow." + constructor + " !== window." + constructor);
+                
+                // Make sure that we haven't reused the constructors from the old document
+                shouldBeTrue("testFrame.contentWindow." + constructor + " !== storedConstructors." + constructor);
+                shouldBe("storedConstructors." + constructor + ".testProperty", '"property set successfully"');
+
+                // Make sure we haven't kept anything over from the original document
+                shouldBeUndefined("testFrame.contentWindow." + constructor + ".testProperty");
+                // Make sure we're getting the same constructor as the frame does internally
+                shouldBeTrue("testFrame.contentWindow." + constructor + ".cachedOnOwnerDocument");
+            } catch (e) {
+                testFailed("Testing " + constructor + " threw " + e);
+            }
+        }
+        
+        if (window.layoutTestController)
+            layoutTestController.notifyDone();
+    };
+    testFrame.src = 'data:text/html,<script>var constructors = ["Image", "MessageChannel", "Option", "XMLHttpRequest", "Audio"];'
+                  + 'for(var i = 0; i < constructors.length; i++) if(window[constructors[i]])'
+                  + 'window[constructors[i]].cachedOnOwnerDocument = true;</script>';
+}
+
+successfullyParsed = true;

LayoutTests/fast/dom/resources/constructors-cached.js
@@ -1,0 +1,20 @@
+description("This test ensures that objects with security restrictions are cached correctly");
+
+var constructors = ["Image", "MessageChannel", "Option", "XMLHttpRequest", "Audio"];
+
+for (var i = 0; i < constructors.length; i++) {
+    var constructor = constructors[i];
+    try {
+        // Test retrieving the object twice results in the same object
+        shouldBe(constructor, constructor);
+
+        // Be paranoid -- make sure that setting a property results in that property
+        // stays
+        this[constructor].testProperty = "property set successfully";
+        shouldBe(constructor + ".testProperty", '"property set successfully"');
+    } catch (e) {
+        testFailed("Testing " + constructor + " threw " + e);
+    }
+}
+
+successfullyParsed = true;