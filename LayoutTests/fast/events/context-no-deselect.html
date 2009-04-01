WebKit/WebKit-http

commit 0c9fb2be68d0e45ca0aee75dab5d707e42b71546

Reviewed by Darin Adler.
        https://bugs.webkit.org/show_bug.cgi?id=19737
        No cursor and paste not enabled right clicking text field/area

        Test: fast/events/right-click-focus.html

        * page/EventHandler.cpp:
        (WebCore::EventHandler::handleMousePressEventSingleClick):
        (WebCore::EventHandler::handleMousePressEvent):
        Take normal code path for right clicks (we were taking it for Ctrl-clicks anyway).



git-svn-id: http://svn.webkit.org/repository/webkit/trunk@41715 268f45cc-cd09-0410-ab3c-d52691b4dbfc

WebCore/page/EventHandler.cpp
@@ -276,9 +276,6 @@ bool EventHandler::handleMousePressEventTripleClick(const MouseEventWithHitTestR
 
 bool EventHandler::handleMousePressEventSingleClick(const MouseEventWithHitTestResults& event)
 {
-    if (event.event().button() != LeftButton)
-        return false;
-    
     Node* innerNode = event.targetNode();
     if (!(innerNode && innerNode->renderer() && m_mouseDownMayStartSelect))
         return false;
@@ -368,18 +365,16 @@ bool EventHandler::handleMousePressEvent(const MouseEventWithHitTestResults& eve
     m_dragStartPos = event.event().pos();
 
     bool swallowEvent = false;
-    if (event.event().button() == LeftButton || event.event().button() == MiddleButton) {
-        m_frame->selection()->setCaretBlinkingSuspended(true);
-        m_mousePressed = true;
-        m_beganSelectingText = false;
+    m_frame->selection()->setCaretBlinkingSuspended(true);
+    m_mousePressed = true;
+    m_beganSelectingText = false;
 
-        if (event.event().clickCount() == 2)
-            swallowEvent = handleMousePressEventDoubleClick(event);
-        else if (event.event().clickCount() >= 3)
-            swallowEvent = handleMousePressEventTripleClick(event);
-        else
-            swallowEvent = handleMousePressEventSingleClick(event);
-    }
+    if (event.event().clickCount() == 2)
+        swallowEvent = handleMousePressEventDoubleClick(event);
+    else if (event.event().clickCount() >= 3)
+        swallowEvent = handleMousePressEventTripleClick(event);
+    else
+        swallowEvent = handleMousePressEventSingleClick(event);
     
     m_mouseDownMayStartAutoscroll = m_mouseDownMayStartSelect || 
         (m_mousePressNode && m_mousePressNode->renderBox() && m_mousePressNode->renderBox()->canBeProgramaticallyScrolled(true));

LayoutTests/fast/events/right-click-focus-expected.txt
@@ -1,0 +1,5 @@
+Test that right-clicking on a text area properly focuses it.
+
+
+PASS
+

LayoutTests/fast/events/right-click-focus.html
@@ -1,0 +1,27 @@
+<head>
+<script>
+function log(t) {
+    document.getElementById('console').innerHTML += t +'<br>';
+}
+
+if (window.layoutTestController)
+    layoutTestController.dumpAsText();
+
+</script>
+<body>
+<p>Test that right-clicking on a text area properly focuses it.</p>
+<textarea></textarea>
+<div id=console></div>
+<script>
+if (window.eventSender) {
+    var elt = document.getElementsByTagName('textarea')[0];
+    eventSender.mouseMoveTo(elt.offsetLeft + 5, elt.offsetTop + 5);
+    eventSender.contextClick();
+    if (!window.getSelection().anchorNode)
+        log("FAIL: No selection after right-clicking");
+    eventSender.keyDown("z");
+    log(document.getElementsByTagName("textarea")[0].value == "z" ? "PASS" : "FAIL: No text in textarea");
+} else {
+    log("Please try right-clicking on the text area below, dismissing the context menu that appears, and typing some text. The caret should be visible, and typing should work.");
+}
+</script>

LayoutTests/platform/mac/editing/selection/5354455-2-expected.checksum
@@ -1,1 +1,1 @@
-1db1b8dbf44496d53cad7c22be04648d
+84cd4927cf64d404e3c9f19d17f3ccad

LayoutTests/platform/mac/editing/selection/5354455-2-expected.txt
@@ -2,26 +2,24 @@ layer at (0,0) size 800x600
   RenderView at (0,0) size 800x600
 layer at (0,0) size 800x600
   RenderBlock {HTML} at (0,0) size 800x600
-layer at (8,8) size 612x160
-  RenderBody {BODY} at (8,8) size 612x160 [border: (1px solid #000000)]
+layer at (8,8) size 612x142
+  RenderBody {BODY} at (8,8) size 612x142 [border: (1px solid #000000)]
     RenderBlock {H1} at (6,6) size 600x37
       RenderText {#text} at (0,0) size 451x37
         text run at (0,0) width 451: "Word selection on right click test"
-    RenderBlock (anonymous) at (6,64) size 600x90
+    RenderBlock (anonymous) at (6,64) size 600x72
       RenderText {#text} at (0,0) size 597x36
         text run at (0,0) width 597: "This tests to make sure that right clicking non-editable content only creates a word selection for"
-        text run at (0,18) width 132: "clicks over text. The "
-      RenderInline {SPAN} at (0,0) size 44x18
-        RenderText {#text} at (132,18) size 44x18
-          text run at (132,18) width 44: "second"
-      RenderText {#text} at (176,18) size 587x72
-        text run at (176,18) width 373: " word in this sentence should be selected when run through"
-        text run at (0,36) width 552: "DumpRenderTree. To run manually, right click on text, and a word should be selected. "
-        text run at (552,36) width 35: "Right"
-        text run at (0,54) width 574: "click on the empty space outside and to the right of the black border, no selection should be"
-        text run at (0,72) width 49: "created."
+        text run at (0,18) width 103: "clicks over text. "
+      RenderInline {SPAN} at (0,0) size 18x18
+        RenderText {#text} at (103,18) size 18x18
+          text run at (103,18) width 18: "To"
+      RenderText {#text} at (121,18) size 577x54
+        text run at (121,18) width 411: " run manually, right click on text, and a word should be selected. "
+        text run at (532,18) width 35: "Right"
+        text run at (0,36) width 577: "click on the empty space outside and to the right of the black border, the selection should be"
+        text run at (0,54) width 59: "removed."
       RenderText {#text} at (0,0) size 0x0
       RenderText {#text} at (0,0) size 0x0
       RenderText {#text} at (0,0) size 0x0
-selection start: position 0 of child 0 {#text} of child 3 {SPAN} of child 1 {BODY} of child 0 {HTML} of document
-selection end:   position 6 of child 0 {#text} of child 3 {SPAN} of child 1 {BODY} of child 0 {HTML} of document
+caret: position 0 of child 0 {#text} of child 1 {H1} of child 1 {BODY} of child 0 {HTML} of document

commit 08c7ecf468e27fb29c8eca47063992373b680400

2009-04-01  Tony Chang  <tony@chromium.org>
        Reviewed by Darin Fisher.

        Ensure the right click doesn't deselect text under it.  This was
        happening in the Chromium and GTK+ ports because they don't select
        the text under the cursor on right click.
        
        This was regressed in r41715, https://bugs.webkit.org/show_bug.cgi?id=19737
        
        https://bugs.webkit.org/show_bug.cgi?id=24946

        Test: fast/events/context-no-deselect.html

        * page/EventHandler.cpp:
        (WebCore::EventHandler::handleMouseReleaseEvent):



git-svn-id: http://svn.webkit.org/repository/webkit/trunk@42154 268f45cc-cd09-0410-ab3c-d52691b4dbfc

WebCore/page/EventHandler.cpp
@@ -549,12 +549,14 @@ bool EventHandler::handleMouseReleaseEvent(const MouseEventWithHitTestResults& e
   
     bool handled = false;
 
-    // Clear the selection if the mouse didn't move after the last mouse press.
-    // We do this so when clicking on the selection, the selection goes away.
-    // However, if we are editing, place the caret.
+    // Clear the selection if the mouse didn't move after the last mouse
+    // press and it's not a context menu click.  We do this so when clicking
+    // on the selection, the selection goes away.  However, if we are
+    // editing, place the caret.
     if (m_mouseDownWasSingleClickInSelection && !m_beganSelectingText
             && m_dragStartPos == event.event().pos()
-            && m_frame->selection()->isRange()) {
+            && m_frame->selection()->isRange()
+            && event.event().button() != RightButton) {
         VisibleSelection newSelection;
         Node *node = event.targetNode();
         if (node && node->isContentEditable() && node->renderer()) {

LayoutTests/fast/events/context-no-deselect.html
@@ -1,0 +1,22 @@
+<html>
+<body>
+<input type="text" id="text" value="some sample text">
+</body>
+</html>
+
+<script>
+// This test checks that if the user right clicks on selected text,
+// the selected text doesn't change or get deselected.
+var input = document.getElementById("text");
+input.selectionStart = 5;
+input.selectionEnd = 15;
+
+if (window.layoutTestController) {
+    var x, y;
+    x = input.offsetParent.offsetLeft + input.offsetLeft + input.offsetWidth / 2;
+    y = input.offsetParent.offsetTop + input.offsetTop + input.offsetHeight / 2;
+    eventSender.mouseMoveTo(x, y);
+    eventSender.contextClick();
+}
+
+</script>

LayoutTests/platform/mac/fast/events/context-no-deselect-expected.checksum
@@ -1,0 +1,1 @@
+d59884cef781bc592520f85696ac72fd

LayoutTests/platform/mac/fast/events/context-no-deselect-expected.txt
@@ -1,0 +1,16 @@
+layer at (0,0) size 800x600
+  RenderView at (0,0) size 800x600
+layer at (0,0) size 800x600
+  RenderBlock {HTML} at (0,0) size 800x600
+    RenderBody {BODY} at (8,8) size 784x584
+      RenderTextControl {INPUT} at (2,2) size 148x19 [bgcolor=#FFFFFF] [border: (2px inset #000000)]
+      RenderText {#text} at (0,0) size 0x0
+      RenderText {#text} at (0,0) size 0x0
+      RenderText {#text} at (0,0) size 0x0
+      RenderText {#text} at (0,0) size 0x0
+layer at (13,13) size 142x13
+  RenderBlock {DIV} at (3,3) size 142x13
+    RenderText {#text} at (1,0) size 96x13
+      text run at (1,0) width 96: "some sample text"
+selection start: position 5 of child 0 {#text} of child 0 {DIV} of child 1 {INPUT} of child 0 {BODY} of child 0 {HTML} of document
+selection end:   position 15 of child 0 {#text} of child 0 {DIV} of child 1 {INPUT} of child 0 {BODY} of child 0 {HTML} of document