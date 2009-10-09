24b2d3663b54e1201b45c42974331a8a745289a8
mrowe@apple.com <mrowe@apple.com@268f45cc-cd09-0410-ab3c-d52691b4dbfc>
2009-10-09 02:54:00 +0000

    Merge r46598.
    git-svn-id: http://svn.webkit.org/repository/webkit/branches/safari-4-branch@49349 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- JavaScriptCore/assembler/AbstractMacroAssembler.h
+++ JavaScriptCore/assembler/AbstractMacroAssembler.h
@@ -369,6 +369,8 @@ public:
         friend class LinkBuffer;
 
     public:
+        typedef Vector<Jump, 16> JumpVector;
+
         void link(AbstractMacroAssembler<AssemblerType>* masm)
         {
             size_t size = m_jumps.size();
@@ -399,9 +401,11 @@ public:
         {
             return !m_jumps.size();
         }
+        
+        const JumpVector& jumps() { return m_jumps; }
 
     private:
-        Vector<Jump, 16> m_jumps;
+        JumpVector m_jumps;
     };
 
 
--- JavaScriptCore/runtime/Collector.cpp
+++ JavaScriptCore/runtime/Collector.cpp
@@ -1148,8 +1148,10 @@ static const char* typeName(JSCell* cell)
 {
     if (cell->isString())
         return "string";
+#if USE(JSVALUE32)
     if (cell->isNumber())
         return "number";
+#endif
     if (cell->isGetterSetter())
         return "gettersetter";
     ASSERT(cell->isObject());
