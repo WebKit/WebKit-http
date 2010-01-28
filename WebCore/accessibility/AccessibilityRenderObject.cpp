2a9a888b702125f235dd301f17bfd59c655078b6
mrowe@apple.com <mrowe@apple.com@268f45cc-cd09-0410-ab3c-d52691b4dbfc>
2010-01-28 08:13:14 +0000

    Merge r52352.
    git-svn-id: http://svn.webkit.org/repository/webkit/branches/safari-4-branch@53979 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebCore/accessibility/AccessibilityRenderObject.cpp
+++ WebCore/accessibility/AccessibilityRenderObject.cpp
@@ -2676,14 +2676,25 @@ String AccessibilityRenderObject::descriptionForMSAA() const
     return String();
 }
 
+static AccessibilityRole msaaRoleForRenderer(const RenderObject* renderer)
+{
+    if (!renderer)
+        return UnknownRole;
+
+    if (renderer->isText())
+        return EditableTextRole;
+
+    return UnknownRole;
+}
+
 AccessibilityRole AccessibilityRenderObject::roleValueForMSAA() const
 {
     if (m_roleForMSAA != UnknownRole)
         return m_roleForMSAA;
 
-    if (m_renderer && m_renderer->isText())
-        m_roleForMSAA = EditableTextRole;
-    else
+    m_roleForMSAA = msaaRoleForRenderer(m_renderer);
+
+    if (m_roleForMSAA == UnknownRole)
         m_roleForMSAA = roleValue();
 
     return m_roleForMSAA;
