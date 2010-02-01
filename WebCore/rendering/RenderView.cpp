b7f6c991c588de797a9bdb89f899bdf1152e20c4
mrowe@apple.com <mrowe@apple.com@268f45cc-cd09-0410-ab3c-d52691b4dbfc>
2010-02-01 23:43:32 +0000

    Merge r47726.
    git-svn-id: http://svn.webkit.org/repository/webkit/branches/safari-4-branch@54164 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebCore/rendering/RenderView.cpp
+++ WebCore/rendering/RenderView.cpp
@@ -460,6 +460,8 @@ void RenderView::setSelection(RenderObject* start, int startPos, RenderObject* e
         return;
     }
 
+    m_frameView->beginDeferredRepaints();
+
     // Have any of the old selected objects changed compared to the new selection?
     for (SelectedObjectMap::iterator i = oldSelectedObjects.begin(); i != oldObjectsEnd; ++i) {
         RenderObject* obj = i->first;
@@ -511,6 +513,8 @@ void RenderView::setSelection(RenderObject* start, int startPos, RenderObject* e
         newInfo->repaint();
         delete newInfo;
     }
+
+    m_frameView->endDeferredRepaints();
 }
 
 void RenderView::clearSelection()
