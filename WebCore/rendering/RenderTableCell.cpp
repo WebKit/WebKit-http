c369bf8ff307b44633027ba841fc08e54246cff3
mrowe@apple.com <mrowe@apple.com@268f45cc-cd09-0410-ab3c-d52691b4dbfc>
2010-02-04 01:09:58 +0000

    Merge r54311.
    git-svn-id: http://svn.webkit.org/repository/webkit/branches/safari-4-branch@54315 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- WebCore/rendering/RenderTableCell.cpp
+++ WebCore/rendering/RenderTableCell.cpp
@@ -800,6 +800,9 @@ void RenderTableCell::paintCollapsedBorder(GraphicsContext* graphicsContext, int
 
 void RenderTableCell::paintBackgroundsBehindCell(PaintInfo& paintInfo, int tx, int ty, RenderObject* backgroundObject)
 {
+    if (!shouldPaintWithinRoot(paintInfo))
+        return;
+
     if (!backgroundObject)
         return;
 
@@ -839,6 +842,9 @@ void RenderTableCell::paintBackgroundsBehindCell(PaintInfo& paintInfo, int tx, i
 
 void RenderTableCell::paintBoxDecorations(PaintInfo& paintInfo, int tx, int ty)
 {
+    if (!shouldPaintWithinRoot(paintInfo))
+        return;
+
     RenderTable* tableElt = table();
     if (!tableElt->collapseBorders() && style()->emptyCells() == HIDE && !firstChild())
         return;
