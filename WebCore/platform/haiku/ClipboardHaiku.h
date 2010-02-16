1ad023d4014c59564294bebee514dffb3aa1d6d8
stippi <stippi@94f232f2-1747-11df-bad5-a5bfde151594>
2010-02-16 22:03:20 +0000

    Style cleanup.
    git-svn-id: http://svn.haiku-os.org/webpositive/webkit/trunk@108 94f232f2-1747-11df-bad5-a5bfde151594

--- WebCore/platform/haiku/ClipboardHaiku.h
+++ WebCore/platform/haiku/ClipboardHaiku.h
@@ -31,43 +31,44 @@
 #include "ClipboardAccessPolicy.h"
 
 namespace WebCore {
-    class CachedImage;
+class CachedImage;
 
-    // State available during IE's events for drag and drop and copy/paste.
-    class ClipboardHaiku : public Clipboard {
-    public:
-        static PassRefPtr<ClipboardHaiku> create(ClipboardAccessPolicy policy, bool forDragging)
-        {
-            return adoptRef(new ClipboardHaiku(policy, forDragging));
-        }
-        ~ClipboardHaiku() { }
+// State available during IE's events for drag and drop and copy/paste.
+class ClipboardHaiku : public Clipboard {
+public:
+    static PassRefPtr<ClipboardHaiku> create(ClipboardAccessPolicy policy, bool forDragging)
+    {
+        return adoptRef(new ClipboardHaiku(policy, forDragging));
+    }
+    ~ClipboardHaiku() { }
 
-        void clearData(const String& type);
-        void clearAllData();
-        String getData(const String& type, bool& success) const;
-        bool setData(const String& type, const String& data);
+    virtual void clearData(const String& type);
+    virtual void clearAllData();
+    virtual String getData(const String& type, bool& success) const;
+    virtual bool setData(const String& type, const String& data);
 
-        // Extensions beyond IE's API.
-        HashSet<String> types() const;
-        virtual PassRefPtr<FileList> files() const;
+    // Extensions beyond IE's API.
+    virtual HashSet<String> types() const;
+    virtual PassRefPtr<FileList> files() const;
 
-        IntPoint dragLocation() const;
-        CachedImage* dragImage() const;
-        void setDragImage(CachedImage*, const IntPoint&);
-        Node* dragImageElement();
-        void setDragImageElement(Node*, const IntPoint&);
+    IntPoint dragLocation() const;
+    CachedImage* dragImage() const;
+    virtual void setDragImage(CachedImage*, const IntPoint&);
+    Node* dragImageElement();
+    virtual void setDragImageElement(Node*, const IntPoint&);
 
-        virtual DragImageRef createDragImage(IntPoint& dragLoc) const;
-        virtual void declareAndWriteDragImage(Element*, const KURL&, const String& title, Frame*);
-        virtual void writeURL(const KURL&, const String&, Frame*);
-        virtual void writeRange(Range*, Frame*);
-        virtual void writePlainText(const String&);
+    virtual DragImageRef createDragImage(IntPoint& dragLoc) const;
+    virtual void declareAndWriteDragImage(Element*, const KURL&, const String& title, Frame*);
+    virtual void writeURL(const KURL&, const String&, Frame*);
+    virtual void writeRange(Range*, Frame*);
+    virtual void writePlainText(const String&);
 
-        virtual bool hasData();
+    virtual bool hasData();
 
-    private:
-        ClipboardHaiku(ClipboardAccessPolicy, bool forDragging);
-    };
+private:
+    ClipboardHaiku(ClipboardAccessPolicy, bool forDragging);
+};
+
 } // namespace WebCore
 
 #endif // ClipboardHaiku_h
