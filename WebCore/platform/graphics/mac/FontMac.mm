WebKit/WebKit-http

commit 713cc9e3309f8435014e96787423dd90b282da23

<rdar://problem/7728903> Support color bitmap fonts
Reviewed by Darin Adler.

WebCore: 

Test: platform/mac/fonts/color-bitmap.html

* platform/graphics/mac/FontMac.mm:
(WebCore::showGlyphsWithAdvances): Added. Calls CGContextShowGlyphsWithAdvances()
or CTFontShowGlyphs() depending on whether the font is a color bitmap font.
(WebCore::Font::drawGlyphs): Use showGlyphsWithAdvances(). Don’t include the font’s matrix in
the context’s text matrix when drawing with a color bitmap font, because CTFontShowGlyphs()
factors it in.
* platform/graphics/mac/FontPlatformData.h:
(WebCore::FontPlatformData::FontPlatformData): Added m_isColorBitmapFont member
and initialized it in constructors.
(WebCore::FontPlatformData::isColorBitmapFont): Added this getter.
* platform/graphics/mac/FontPlatformDataMac.mm:
(WebCore::FontPlatformData::FontPlatformData): Initialize m_isColorBitmapFont.
(WebCore::FontPlatformData::operator=): Copy m_isColorBitmapFont.
(WebCore::FontPlatformData::setFont): Set m_isColorBitmapFont.

WebKitTools: 

* DumpRenderTree/DumpRenderTree.xcodeproj/project.pbxproj: Added ColorBits.ttf
and ColorBits-A.png.
* DumpRenderTree/fonts/ColorBits-A.png: Copied from WebCore/inspector/front-end/Images/successGreenDot.png.
* DumpRenderTree/fonts/ColorBits.ttf: Added.
* DumpRenderTree/mac/DumpRenderTree.mm:
(activateFonts): Activate ColorBits.ttf.

LayoutTests: 

* platform/mac-leopard/Skipped:
* platform/mac-snowleopard/Skipped:
* platform/mac-tiger/Skipped:
* platform/mac/fonts/color-bitmap-expected.checksum: Added.
* platform/mac/fonts/color-bitmap-expected.png: Added.
* platform/mac/fonts/color-bitmap-expected.txt: Added.
* platform/mac/fonts/color-bitmap.html: Added.



git-svn-id: http://svn.webkit.org/repository/webkit/trunk@56597 268f45cc-cd09-0410-ab3c-d52691b4dbfc

WebCore/platform/graphics/mac/FontMac.mm
@@ -2,7 +2,7 @@
  * Copyright (C) 1999 Lars Knoll (knoll@kde.org)
  *           (C) 1999 Antti Koivisto (koivisto@kde.org)
  *           (C) 2000 Dirk Mueller (mueller@kde.org)
- * Copyright (C) 2003, 2006 Apple Computer, Inc.
+ * Copyright (C) 2003, 2006, 2007, 2008, 2009, 2010 Apple Inc.
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Library General Public
@@ -45,6 +45,28 @@ namespace WebCore {
 bool Font::canReturnFallbackFontsForComplexText()
 {
     return true;
+}
+
+static void showGlyphsWithAdvances(const FontPlatformData& font, CGContextRef context, const CGGlyph* glyphs, const CGSize* advances, size_t count)
+{
+    if (!font.isColorBitmapFont())
+        CGContextShowGlyphsWithAdvances(context, glyphs, advances, count);
+#if !defined(BUILDING_ON_TIGER) && !defined(BUILDING_ON_LEOPARD) && !defined(BUILDING_ON_SNOW_LEOPARD)
+    else {
+        if (!count)
+            return;
+
+        Vector<CGPoint, 256> positions(count);
+        CGAffineTransform matrix = CGAffineTransformInvert(CGContextGetTextMatrix(context));
+        positions[0] = CGPointZero;
+        for (size_t i = 1; i < count; ++i) {
+            CGSize advance = CGSizeApplyAffineTransform(advances[i - 1], matrix);
+            positions[i].x = positions[i - 1].x + advance.width;
+            positions[i].y = positions[i - 1].y + advance.height;
+        }
+        CTFontDrawGlyphs(toCTFontRef(font.font()), glyphs, positions.data(), count, context);
+    }
+#endif
 }
 
 void Font::drawGlyphs(GraphicsContext* context, const SimpleFontData* font, const GlyphBuffer& glyphBuffer, int from, int numGlyphs, const FloatPoint& point) const
@@ -98,7 +120,7 @@ void Font::drawGlyphs(GraphicsContext* context, const SimpleFontData* font, cons
     CGContextSetFont(cgContext, platformData.cgFont());
 
     CGAffineTransform matrix = CGAffineTransformIdentity;
-    if (drawFont)
+    if (drawFont && !platformData.isColorBitmapFont())
         memcpy(&matrix, [drawFont matrix], sizeof(matrix));
     matrix.b = -matrix.b;
     matrix.d = -matrix.d;
@@ -112,6 +134,8 @@ void Font::drawGlyphs(GraphicsContext* context, const SimpleFontData* font, cons
     } else
         CGContextSetFontSize(cgContext, platformData.m_size);
 
+
+#if defined(BUILDING_ON_TIGER) || defined(BUILDING_ON_LEOPARD)
     IntSize shadowSize;
     int shadowBlur;
     Color shadowColor;
@@ -126,23 +150,26 @@ void Font::drawGlyphs(GraphicsContext* context, const SimpleFontData* font, cons
         Color shadowFillColor(shadowColor.red(), shadowColor.green(), shadowColor.blue(), shadowColor.alpha() * fillColor.alpha() / 255);
         context->setFillColor(shadowFillColor, fillColorSpace);
         CGContextSetTextPosition(cgContext, point.x() + shadowSize.width(), point.y() + shadowSize.height());
-        CGContextShowGlyphsWithAdvances(cgContext, glyphBuffer.glyphs(from), glyphBuffer.advances(from), numGlyphs);
+        showGlyphsWithAdvances(platformData, cgContext, glyphBuffer.glyphs(from), glyphBuffer.advances(from), numGlyphs);
         if (font->syntheticBoldOffset()) {
             CGContextSetTextPosition(cgContext, point.x() + shadowSize.width() + font->syntheticBoldOffset(), point.y() + shadowSize.height());
-            CGContextShowGlyphsWithAdvances(cgContext, glyphBuffer.glyphs(from), glyphBuffer.advances(from), numGlyphs);
+            showGlyphsWithAdvances(platformData, cgContext, glyphBuffer.glyphs(from), glyphBuffer.advances(from), numGlyphs);
         }
         context->setFillColor(fillColor, fillColorSpace);
     }
+#endif
 
     CGContextSetTextPosition(cgContext, point.x(), point.y());
-    CGContextShowGlyphsWithAdvances(cgContext, glyphBuffer.glyphs(from), glyphBuffer.advances(from), numGlyphs);
+    showGlyphsWithAdvances(platformData, cgContext, glyphBuffer.glyphs(from), glyphBuffer.advances(from), numGlyphs);
     if (font->syntheticBoldOffset()) {
         CGContextSetTextPosition(cgContext, point.x() + font->syntheticBoldOffset(), point.y());
-        CGContextShowGlyphsWithAdvances(cgContext, glyphBuffer.glyphs(from), glyphBuffer.advances(from), numGlyphs);
+        showGlyphsWithAdvances(platformData, cgContext, glyphBuffer.glyphs(from), glyphBuffer.advances(from), numGlyphs);
     }
 
+#if defined(BUILDING_ON_TIGER) || defined(BUILDING_ON_LEOPARD)
     if (hasSimpleShadow)
         context->setShadow(shadowSize, shadowBlur, shadowColor, fillColorSpace);
+#endif
 
     if (originalShouldUseFontSmoothing != newShouldUseFontSmoothing)
         CGContextSetShouldSmoothFonts(cgContext, originalShouldUseFontSmoothing);

LayoutTests/platform/mac-leopard/Skipped
@@ -53,3 +53,6 @@ http/tests/media/video-referer.html
 # Disable tests until we can turn on HW comp as described in https://bugs.webkit.org/show_bug.cgi?id=35897
 fast/media/mq-transform-02.html
 fast/media/mq-transform-03.html
+
+# Color bitmap fonts not supported on Leopard
+platform/mac/fonts/color-bitmap.html

LayoutTests/platform/mac-snowleopard/Skipped
@@ -105,3 +105,6 @@ media/video-display-aspect-ratio.html
 # Flakey on Snow Leopard Debug builds, skip it until it can be made non-flakey:
 # https://bugs.webkit.org/show_bug.cgi?id=33543
 fast/forms/search-event-delay.html
+
+# Color bitmap fonts not supported on Snow Leopard
+platform/mac/fonts/color-bitmap.html

LayoutTests/platform/mac-tiger/Skipped
@@ -132,3 +132,6 @@ http/tests/local/send-sliced-dragged-file.html
 # Times out on Tiger bot.
 # https://bugs.webkit.org/show_bug.cgi?id=36382
 fast/loader/frame-src-change-added-to-history.html
+
+# Color bitmap fonts not supported on Tiger
+platform/mac/fonts/color-bitmap.html

LayoutTests/platform/mac/fonts/color-bitmap-expected.checksum
@@ -1,0 +1,1 @@
+2b1ddff7844b8f9cfd85980092c817da

LayoutTests/platform/mac/fonts/color-bitmap-expected.txt
@@ -1,0 +1,17 @@
+layer at (0,0) size 800x600
+  RenderView at (0,0) size 800x600
+layer at (0,0) size 800x600
+  RenderBlock {HTML} at (0,0) size 800x600
+    RenderBody {BODY} at (8,8) size 784x552
+      RenderBlock {P} at (0,0) size 784x18
+        RenderText {#text} at (0,0) size 601x18
+          text run at (0,0) width 601: "This tests color bitmap font rendering. There should be a green dot below, enclosed in brackets."
+      RenderBlock {P} at (0,38) size 784x20
+        RenderText {#text} at (0,0) size 48x20
+          text run at (0,0) width 48: "[A]"
+      RenderBlock {P} at (0,78) size 784x18
+        RenderText {#text} at (0,0) size 466x18
+          text run at (0,0) width 466: "This should be a bigger, pixelated version of the dot, enclosed in brackets:"
+      RenderBlock {P} at (0,136) size 784x40
+        RenderText {#text} at (0,0) size 96x40
+          text run at (0,0) width 96: "[A]"

LayoutTests/platform/mac/fonts/color-bitmap.html
@@ -1,0 +1,12 @@
+<p>
+    This tests color bitmap font rendering. There should be a green dot below, enclosed in brackets.
+</p>
+<p style="font-size: 20px; font-family: colorbits">
+    [A]
+</p>
+<p>
+    This should be a bigger, pixelated version of the dot, enclosed in brackets:
+</p>
+<p style="font-size: 40px; font-family: colorbits">
+    [A]
+</p>

commit 507667ee0080eee9f487776638fc2ef005ec01dd

Fix Acid3 text-shadow rendering regression introduced in r56597.
Reviewed by Mark Rowe.

* platform/graphics/mac/FontMac.mm:
(WebCore::Font::drawGlyphs): Revert to using the simple shadow logic for zero-blur shadows,
except for color bitmap fonts.



git-svn-id: http://svn.webkit.org/repository/webkit/trunk@56659 268f45cc-cd09-0410-ab3c-d52691b4dbfc

WebCore/platform/graphics/mac/FontMac.mm
@@ -135,14 +135,13 @@ void Font::drawGlyphs(GraphicsContext* context, const SimpleFontData* font, cons
         CGContextSetFontSize(cgContext, platformData.m_size);
 
 
-#if defined(BUILDING_ON_TIGER) || defined(BUILDING_ON_LEOPARD)
     IntSize shadowSize;
     int shadowBlur;
     Color shadowColor;
     ColorSpace fillColorSpace = context->fillColorSpace();
     context->getShadow(shadowSize, shadowBlur, shadowColor);
 
-    bool hasSimpleShadow = context->textDrawingMode() == cTextFill && shadowColor.isValid() && !shadowBlur;
+    bool hasSimpleShadow = context->textDrawingMode() == cTextFill && shadowColor.isValid() && !shadowBlur && !platformData.isColorBitmapFont();
     if (hasSimpleShadow) {
         // Paint simple shadows ourselves instead of relying on CG shadows, to avoid losing subpixel antialiasing.
         context->clearShadow();
@@ -157,7 +156,6 @@ void Font::drawGlyphs(GraphicsContext* context, const SimpleFontData* font, cons
         }
         context->setFillColor(fillColor, fillColorSpace);
     }
-#endif
 
     CGContextSetTextPosition(cgContext, point.x(), point.y());
     showGlyphsWithAdvances(platformData, cgContext, glyphBuffer.glyphs(from), glyphBuffer.advances(from), numGlyphs);
@@ -166,10 +164,8 @@ void Font::drawGlyphs(GraphicsContext* context, const SimpleFontData* font, cons
         showGlyphsWithAdvances(platformData, cgContext, glyphBuffer.glyphs(from), glyphBuffer.advances(from), numGlyphs);
     }
 
-#if defined(BUILDING_ON_TIGER) || defined(BUILDING_ON_LEOPARD)
     if (hasSimpleShadow)
         context->setShadow(shadowSize, shadowBlur, shadowColor, fillColorSpace);
-#endif
 
     if (originalShouldUseFontSmoothing != newShouldUseFontSmoothing)
         CGContextSetShouldSmoothFonts(cgContext, originalShouldUseFontSmoothing);