WebKit/WebKit-http

commit 5e6361d5c4ff45c2fa9f38cf70acc8b2250cae38

2010-04-11  Robert Hogan  <robert@webkit.org>
        Reviewed by Simon Hausmann.

        [Qt] Update layoutTestController.DumpResourceLoadCallbacks to match other ports.

        Unskip  http/tests/xmlhttprequest/abort-should-cancel-load.html
                http/tests/misc/will-send-request-returns-null-on-redirect.html
                fast/loader/user-style-sheet-resource-load-callbacks.html
                http/tests/misc/window-dot-stop.html
                http/tests/security/XFrameOptions/x-frame-options-deny-meta-tag-parent-same-origin-allow.html
                http/tests/security/XFrameOptions/x-frame-options-deny.html
                http/tests/security/XFrameOptions/x-frame-options-parent-same-origin-allow.html
                http/tests/security/XFrameOptions/x-frame-options-parent-same-origin-deny.html
                http/tests/xmlhttprequest/abort-should-cancel-load.html

        QNetworkReply::OperationCanceledError has a value of 5 rather than -999, so add platform-specific results accordingly.

        Add platform-specific results for the the following, though leave them skipped for now. They've started failing again
        since review was granted (see https://bugs.webkit.org/show_bug.cgi?id=37382):
                http/tests/security/XFrameOptions/x-frame-options-deny-meta-tag-in-body.html
                http/tests/security/XFrameOptions/x-frame-options-deny-meta-tag-parent-same-origin-deny.html
                http/tests/security/XFrameOptions/x-frame-options-deny-meta-tag.html

        https://bugs.webkit.org/show_bug.cgi?id=37237

        * platform/qt/Skipped:
        * platform/qt/http/tests/misc/window-dot-stop-expected.txt: Added.
        * platform/qt/http/tests/security/XFrameOptions/x-frame-options-deny-meta-tag-expected.txt: Added.
        * platform/qt/http/tests/security/XFrameOptions/x-frame-options-deny-meta-tag-in-body-expected.txt: Added.

WebKitTools/DumpRenderTree/qt/LayoutTestControllerQt.cpp
@@ -40,6 +40,7 @@ extern void qt_wrt_setViewMode(QWebPage*, const QString& mode);
 extern void qt_dump_editing_callbacks(bool b);
 extern void qt_dump_frame_loader(bool b);
 extern void qt_dump_resource_load_callbacks(bool b);
+extern void qt_set_will_send_request_returns_null_on_redirect(bool b);
 extern void qt_drt_setMediaType(QWebFrame* qframe, const QString& type);
 extern void qt_drt_setFrameFlatteningEnabled(QWebPage*, bool);
 extern void qt_drt_setJavaScriptProfilingEnabled(QWebFrame*, bool enabled);
@@ -239,6 +240,11 @@ void LayoutTestController::dumpFrameLoadCallbacks()
 void LayoutTestController::dumpResourceLoadCallbacks()
 {
     qt_dump_resource_load_callbacks(true);
+}
+
+void LayoutTestController::setWillSendRequestReturnsNullOnRedirect(bool enabled)
+{
+    qt_set_will_send_request_returns_null_on_redirect(enabled);
 }
 
 void LayoutTestController::queueBackNavigation(int howFarBackward)

LayoutTests/platform/qt/Skipped
@@ -309,9 +309,6 @@ http/tests/security/no-referrer.html
 fast/loader/onload-willSendRequest-null-for-frame.html
 fast/loader/onload-willSendRequest-null-for-script.html
 
-# Missing layoutTestController.setWillSendRequestReturnsNullOnRedirect()
-http/tests/misc/will-send-request-returns-null-on-redirect.html
-
 # Missing layoutTestController.evaluateScriptInIsolatedWorld()
 http/tests/security/isolatedWorld
 
@@ -515,7 +512,6 @@ fast/xsl/xslt_unicode.xml
 # ------- failures for tests with results
 fast/forms/drag-out-of-textarea.html
 fast/frames/frame-src-attribute.html
-fast/loader/user-style-sheet-resource-load-callbacks.html
 fast/overflow/003.xml
 fast/text/international/bidi-menulist.html
 fast/text/international/wrap-CJK-001.html
@@ -4960,7 +4956,13 @@ http/tests/misc/isindex-with-no-form-base-href.html
 http/tests/misc/isindex-with-no-form.html
 http/tests/misc/refresh-headers.php
 http/tests/misc/timer-vs-loading.html
-http/tests/misc/window-dot-stop.html
+
+
+# Have an additional line in results for GET of favicon.ico. Probably innocuous.
+# https://bugs.webkit.org/show_bug.cgi?id=37382
+http/tests/security/XFrameOptions/x-frame-options-deny-meta-tag-in-body.html
+http/tests/security/XFrameOptions/x-frame-options-deny-meta-tag-parent-same-origin-deny.html
+http/tests/security/XFrameOptions/x-frame-options-deny-meta-tag.html
 
 # Skip failing http/tests/security tests
 http/tests/security/cross-frame-access-callback-explicit-domain-ALLOW.html
@@ -4970,13 +4972,6 @@ http/tests/security/drag-over-remote-content-iframe.html
 http/tests/security/host-compare-case-insensitive.html
 http/tests/security/xss-DENIED-xsl-document-redirect.xml
 http/tests/security/xss-DENIED-xsl-document.xml
-http/tests/security/XFrameOptions/x-frame-options-deny-meta-tag-in-body.html
-http/tests/security/XFrameOptions/x-frame-options-deny-meta-tag-parent-same-origin-allow.html
-http/tests/security/XFrameOptions/x-frame-options-deny-meta-tag-parent-same-origin-deny.html
-http/tests/security/XFrameOptions/x-frame-options-deny-meta-tag.html
-http/tests/security/XFrameOptions/x-frame-options-deny.html
-http/tests/security/XFrameOptions/x-frame-options-parent-same-origin-allow.html
-http/tests/security/XFrameOptions/x-frame-options-parent-same-origin-deny.html
 http/tests/security/aboutBlank/security-context-alias.html
 http/tests/security/aboutBlank/security-context-window-open.html
 http/tests/security/aboutBlank/xss-DENIED-navigate-opener-javascript-url.html
@@ -4997,7 +4992,6 @@ http/tests/xmlhttprequest/re-login.html
 # Skip xmlhttprequest tests
 # Many of these tests exercise preflighted requests, which are not supported with Qt older than 4.7.
 http/tests/xmlhttprequest/XMLHttpRequestException.html
-http/tests/xmlhttprequest/abort-should-cancel-load.html
 http/tests/xmlhttprequest/access-control-and-redirects.html
 http/tests/xmlhttprequest/access-control-basic-allow-preflight-cache-invalidation-by-header.html
 http/tests/xmlhttprequest/access-control-basic-allow-preflight-cache-invalidation-by-method.html

WebKit/qt/WebCoreSupport/FrameLoaderClientQt.cpp
@@ -79,6 +79,7 @@
 
 static bool dumpFrameLoaderCallbacks = false;
 static bool dumpResourceLoadCallbacks = false;
+static bool sendRequestReturnsNullOnRedirect = false;
 
 static QMap<unsigned long, QString> dumpAssignedUrls;
 
@@ -90,6 +91,11 @@ void QWEBKIT_EXPORT qt_dump_frame_loader(bool b)
 void QWEBKIT_EXPORT qt_dump_resource_load_callbacks(bool b)
 {
     dumpResourceLoadCallbacks = b;
+}
+
+void QWEBKIT_EXPORT qt_set_will_send_request_returns_null_on_redirect(bool b)
+{
+    sendRequestReturnsNullOnRedirect = b;
 }
 
 // Compare with WebKitTools/DumpRenderTree/mac/FrameLoadDelegate.mm
@@ -125,16 +131,16 @@ static QString drtDescriptionSuitableForTestResult(const WebCore::ResourceError&
 static QString drtDescriptionSuitableForTestResult(const WebCore::ResourceRequest& request)
 {
     QString url = request.url().string();
-    return QString::fromLatin1("<NSURLRequest %1>").arg(url);
+    QString httpMethod = request.httpMethod();
+    QString mainDocumentUrl = request.firstPartyForCookies().string();
+    return QString::fromLatin1("<NSURLRequest URL %1, main document URL %2, http method %3>").arg(url).arg(mainDocumentUrl).arg(httpMethod);
 }
 
 static QString drtDescriptionSuitableForTestResult(const WebCore::ResourceResponse& response)
 {
-    QString text = response.httpStatusText();
-    if (text.isEmpty())
-        return QLatin1String("(null)");
-
-    return text;
+    QString url = response.url().string();
+    int httpStatusCode = response.httpStatusCode();
+    return QString::fromLatin1("<NSURLResponse %1, http status code %2>").arg(url).arg(httpStatusCode);
 }
 
 
@@ -841,11 +847,17 @@ void FrameLoaderClientQt::assignIdentifierToInitialRequest(unsigned long identif
 
 void FrameLoaderClientQt::dispatchWillSendRequest(WebCore::DocumentLoader*, unsigned long identifier, WebCore::ResourceRequest& newRequest, const WebCore::ResourceResponse& redirectResponse)
 {
+
     if (dumpResourceLoadCallbacks)
         printf("%s - willSendRequest %s redirectResponse %s\n",
                qPrintable(dumpAssignedUrls[identifier]),
                qPrintable(drtDescriptionSuitableForTestResult(newRequest)),
-               qPrintable(drtDescriptionSuitableForTestResult(redirectResponse)));
+               (redirectResponse.isNull()) ? "(null)" : qPrintable(drtDescriptionSuitableForTestResult(redirectResponse)));
+
+    if (sendRequestReturnsNullOnRedirect && !redirectResponse.isNull()) {
+        printf("Returning null for this redirect\n");
+        newRequest.setURL(QUrl());
+    }
 
     // seems like the Mac code doesn't do anything here by default neither
     //qDebug() << "FrameLoaderClientQt::dispatchWillSendRequest" << request.isNull() << request.url().string`();
@@ -868,11 +880,15 @@ void FrameLoaderClientQt::dispatchDidCancelAuthenticationChallenge(DocumentLoade
     notImplemented();
 }
 
-void FrameLoaderClientQt::dispatchDidReceiveResponse(WebCore::DocumentLoader*, unsigned long, const WebCore::ResourceResponse& response)
+void FrameLoaderClientQt::dispatchDidReceiveResponse(WebCore::DocumentLoader*, unsigned long identifier, const WebCore::ResourceResponse& response)
 {
 
     m_response = response;
     m_firstData = true;
+    if (dumpResourceLoadCallbacks)
+        printf("%s - didReceiveResponse %s\n",
+               qPrintable(dumpAssignedUrls[identifier]),
+               qPrintable(drtDescriptionSuitableForTestResult(response)));
     //qDebug() << "    got response from" << response.url().string();
 }
 
@@ -880,14 +896,19 @@ void FrameLoaderClientQt::dispatchDidReceiveContentLength(WebCore::DocumentLoade
 {
 }
 
-void FrameLoaderClientQt::dispatchDidFinishLoading(WebCore::DocumentLoader*, unsigned long)
+void FrameLoaderClientQt::dispatchDidFinishLoading(WebCore::DocumentLoader*, unsigned long identifier)
 {
+    if (dumpResourceLoadCallbacks)
+        printf("%s - didFinishLoading\n",
+               (dumpAssignedUrls.contains(identifier) ? qPrintable(dumpAssignedUrls[identifier]) : "<unknown>"));
 }
 
 void FrameLoaderClientQt::dispatchDidFailLoading(WebCore::DocumentLoader* loader, unsigned long identifier, const WebCore::ResourceError& error)
 {
     if (dumpResourceLoadCallbacks)
-        printf("%s - didFailLoadingWithError: %s\n", qPrintable(dumpAssignedUrls[identifier]), qPrintable(drtDescriptionSuitableForTestResult(error)));
+        printf("%s - didFailLoadingWithError: %s\n",
+               (dumpAssignedUrls.contains(identifier) ? qPrintable(dumpAssignedUrls[identifier]) : "<unknown>"),
+               qPrintable(drtDescriptionSuitableForTestResult(error)));
 
     if (m_firstData) {
         FrameLoader *fl = loader->frameLoader();

WebKitTools/DumpRenderTree/qt/DumpRenderTreeQt.cpp
@@ -86,6 +86,7 @@ extern void qt_drt_clearFrameName(QWebFrame* qFrame);
 extern void qt_drt_overwritePluginDirectories();
 extern void qt_drt_resetOriginAccessWhiteLists();
 extern bool qt_drt_hasDocumentElement(QWebFrame* qFrame);
+extern void qt_dump_resource_load_callbacks(bool b);
 
 namespace WebCore {
 
@@ -728,8 +729,9 @@ static const char *methodNameStringForFailedTest(LayoutTestController *controlle
 
 void DumpRenderTree::dump()
 {
-    // Prevent any further frame load callbacks from appearing after we dump the result.
+    // Prevent any further frame load or resource load callbacks from appearing after we dump the result.
     qt_dump_frame_loader(false);
+    qt_dump_resource_load_callbacks(false);
 
     QWebFrame *mainFrame = m_page->mainFrame();
 

WebKitTools/DumpRenderTree/qt/LayoutTestControllerQt.h
@@ -101,6 +101,7 @@ public slots:
     void dumpEditingCallbacks();
     void dumpFrameLoadCallbacks();
     void dumpResourceLoadCallbacks();
+    void setWillSendRequestReturnsNullOnRedirect(bool enabled);
     void queueBackNavigation(int howFarBackward);
     void queueForwardNavigation(int howFarForward);
     void queueLoad(const QString& url, const QString& target = QString());

commit 06385d059301a9f9bffef2b0f9e86401b282069b

[Qt] Fix regressions in http/tests/navigation from r57416
2010-04-10  Robert Hogan  <robert@webkit.org>

        Unreviewed fix to regressions in r57416.

        [Qt] Fix regressions in http/tests/navigation from r57416

        Unskip tests skipped after regression in r57416.

        https://bugs.webkit.org/show_bug.cgi?id=37237

        * platform/qt/Skipped:

2010-04-10  Robert Hogan  <robert@webkit.org>

        Unreviewed fix to regressions in r57416.

        [Qt] Fix regressions in http/tests/navigation from r57416

        Reset willSendRequestReturnsNullOnRedirect after each test to
        prevent it leaking to subsequent tests.

        Error pointed out by Jakub Wieczorek.

        https://bugs.webkit.org/show_bug.cgi?id=37237

        * DumpRenderTree/qt/LayoutTestControllerQt.cpp:
        (LayoutTestController::reset):

git-svn-id: http://svn.webkit.org/repository/webkit/trunk@57418 268f45cc-cd09-0410-ab3c-d52691b4dbfc

WebKitTools/DumpRenderTree/qt/LayoutTestControllerQt.cpp
@@ -91,6 +91,7 @@ void LayoutTestController::reset()
     qt_dump_editing_callbacks(false);
     qt_dump_frame_loader(false);
     qt_dump_resource_load_callbacks(false);
+    qt_set_will_send_request_returns_null_on_redirect(false);
     emit hidePage();
 }
 

LayoutTests/platform/qt/Skipped
@@ -5235,8 +5235,3 @@ websocket/tests/long-invalid-header.html
 # [Qt] REGRESSION: editing/selection/shrink-selection-after-shift-pagedown.html fails after 56567
 # https://bugs.webkit.org/show_bug.cgi?id=36628
 editing/selection/shrink-selection-after-shift-pagedown.html
-
-# REGRESSION by r57416.(https://bugs.webkit.org/show_bug.cgi?id=37237)
-# Assigned to robert@webkit.org
-http/tests/navigation/postredirect-reload.html
-http/tests/navigation/redirect302-basic.html