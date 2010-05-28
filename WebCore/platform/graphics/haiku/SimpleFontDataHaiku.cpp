0fb3d3495f5a36b7946a82d749978ab4ac22b1a3
stippi <stippi@94f232f2-1747-11df-bad5-a5bfde151594>
2010-05-28 23:59:03 +0000

    Use correct rounding for determining the ascent/descent of a type face. Fixes the sometimes missing underscores or other drawing glitches with the bottom- most line of pixels of a text line.
    git-svn-id: http://svn.haiku-os.org/webpositive/webkit/trunk@520 94f232f2-1747-11df-bad5-a5bfde151594

--- WebCore/platform/graphics/haiku/SimpleFontDataHaiku.cpp
+++ WebCore/platform/graphics/haiku/SimpleFontDataHaiku.cpp
@@ -52,8 +52,8 @@ void SimpleFontData::platformInit()
 
     font_height height;
     font->GetHeight(&height);
-    m_ascent = static_cast<int>(height.ascent);
-    m_descent = static_cast<int>(height.descent);
+    m_ascent = static_cast<int>(ceilf(height.ascent));
+    m_descent = static_cast<int>(ceilf(height.descent));
     m_lineSpacing = m_ascent + m_descent;
     m_xHeight = height.ascent * 0.56f; // Hack taken from the win port.
     m_lineGap = height.leading;
