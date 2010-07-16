WebKit/WebKit-http

commit 4ea43b5be74d4283755836557eaab811dab244fd

2010-07-12  Nate Chapin  <japhet@chromium.org>
        Reviewed by Darin Fisher.

        Ensure that a cache policy that forces validation is cleared once
        the load event is fired, rather than only doing so at the next
        navigation. This leads to a lot of unnecessary load on AJAX-y
        websites.

        https://bugs.webkit.org/show_bug.cgi?id=41813

        Test: http/tests/xmlhttprequest/cache-headers-after-reload.html

        * loader/FrameLoader.cpp:
        (WebCore::FrameLoader::handledOnloadEvents): Reset m_loadType to FrameLoadTypeStandard.
        (WebCore::FrameLoader::addExtraFieldsToRequest): Only respect the original request's cache policy if the
            DocumentLoader is still loading, and handle the other cache policy settings that were scattered around the loader.
        (WebCore::FrameLoader::loadResourceSynchronously): Merge cachePolicy setting into FrameLoader::addExtraFieldsToRequest.
        * loader/SubresourceLoader.cpp:
        (WebCore::SubresourceLoader::create): Merge cachePolicy setting into FrameLoader::addExtraFieldsToRequest.



git-svn-id: http://svn.webkit.org/repository/webkit/trunk@63100 268f45cc-cd09-0410-ab3c-d52691b4dbfc

WebCore/loader/FrameLoader.cpp
@@ -2605,6 +2605,9 @@ String FrameLoader::userAgent(const KURL& url) const
 void FrameLoader::handledOnloadEvents()
 {
     m_client->dispatchDidHandleOnloadEvents();
+
+    m_loadType = FrameLoadTypeStandard;
+
 #if ENABLE(OFFLINE_WEB_APPLICATIONS)
     if (documentLoader())
         documentLoader()->applicationCacheHost()->stopDeferringEvents();
@@ -2677,6 +2680,13 @@ void FrameLoader::addExtraFieldsToRequest(ResourceRequest& request, FrameLoadTyp
 
     applyUserAgent(request);
     
+    // If we inherit cache policy from a main resource, we use the DocumentLoader's 
+    // original request cache policy for two reasons:
+    // 1. For POST requests, we mutate the cache policy for the main resource,
+    //    but we do not want this to apply to subresources
+    // 2. Delegates that modify the cache policy using willSendRequest: should
+    //    not affect any other resources. Such changes need to be done
+    //    per request.
     if (loadType == FrameLoadTypeReload) {
         request.setCachePolicy(ReloadIgnoringCacheData);
         request.setHTTPHeaderField("Cache-Control", "max-age=0");
@@ -2684,8 +2694,12 @@ void FrameLoader::addExtraFieldsToRequest(ResourceRequest& request, FrameLoadTyp
         request.setCachePolicy(ReloadIgnoringCacheData);
         request.setHTTPHeaderField("Cache-Control", "no-cache");
         request.setHTTPHeaderField("Pragma", "no-cache");
-    } else if (isBackForwardLoadType(loadType) && !request.url().protocolIs("https"))
+    } else if (request.isConditional())
+        request.setCachePolicy(ReloadIgnoringCacheData);
+    else if (isBackForwardLoadType(loadType) && !request.url().protocolIs("https"))
         request.setCachePolicy(ReturnCacheDataElseLoad);
+    else if (!mainResource && documentLoader()->isLoadingInAPISense())
+        request.setCachePolicy(documentLoader()->originalRequest().cachePolicy());
     
     if (mainResource)
         request.setHTTPAccept(defaultAcceptHeader);
@@ -2782,17 +2796,6 @@ unsigned long FrameLoader::loadResourceSynchronously(const ResourceRequest& requ
     ResourceRequest initialRequest = request;
     initialRequest.setTimeoutInterval(10);
     
-    // Use the original request's cache policy for two reasons:
-    // 1. For POST requests, we mutate the cache policy for the main resource,
-    //    but we do not want this to apply to subresources
-    // 2. Delegates that modify the cache policy using willSendRequest: should
-    //    not affect any other resources. Such changes need to be done
-    //    per request.
-    if (initialRequest.isConditional())
-        initialRequest.setCachePolicy(ReloadIgnoringCacheData);
-    else
-        initialRequest.setCachePolicy(originalRequest().cachePolicy());
-    
     if (!referrer.isEmpty())
         initialRequest.setHTTPReferrer(referrer);
     addHTTPOriginIfNeeded(initialRequest, outgoingOrigin());
@@ -2800,6 +2803,8 @@ unsigned long FrameLoader::loadResourceSynchronously(const ResourceRequest& requ
     if (Page* page = m_frame->page())
         initialRequest.setFirstPartyForCookies(page->mainFrame()->loader()->documentLoader()->request().url());
     initialRequest.setHTTPUserAgent(client()->userAgent(request.url()));
+    
+    addExtraFieldsToSubresourceRequest(initialRequest);
 
     unsigned long identifier = 0;    
     ResourceRequest newRequest(initialRequest);

LayoutTests/http/tests/xmlhttprequest/cache-headers-after-reload-expected.txt
@@ -1,0 +1,8 @@
+This test does the following:
+1. Load a page, trigger an XHR.
+2. Reload the page.
+3. Dump the http readers for the reload of the XHR.
+
+The XHR is trigger after onload has fired. There should not be an HTTP_CACHE_CONTROL header forcing reload, so an http cache could load the resource without contacting the server.
+The test passes if there is no HTTP_CACHE_CONTROL header displayed below.
+

LayoutTests/http/tests/xmlhttprequest/cache-headers-after-reload.html
@@ -1,0 +1,44 @@
+<html>
+<body onload="setTimeout(test, 100);">
+This test does the following:<br>
+1. Load a page, trigger an XHR.<br>
+2. Reload the page.<br>
+3. Dump the http readers for the reload of the XHR.<br><br>
+The XHR is trigger after onload has fired. There should not be an HTTP_CACHE_CONTROL header forcing reload, so an http cache
+could load the resource without contacting the server.<br>
+The test passes if there is no HTTP_CACHE_CONTROL header displayed below.<br>
+<div id="console"></div>
+<script>
+    if (window.layoutTestController) {
+        layoutTestController.dumpAsText();
+        layoutTestController.waitUntilDone();
+    }
+
+    function log(message) {
+        document.getElementById("console").appendChild(document.createTextNode(message));
+    }
+
+    var xhr;    
+    function test() {
+        xhr = new XMLHttpRequest();
+        if (localStorage.reloaded)
+            xhr.onload = finish;
+        else         
+            xhr.onload = reload;
+        xhr.open("GET", "resources/print-cache-control-header.cgi", true);
+        xhr.send(null);
+    }
+    
+    function reload() {
+        localStorage.reloaded = true;
+        location.reload(true);
+    }
+    
+    function finish() {
+        log(xhr.responseText);
+        if (window.layoutTestController)
+            layoutTestController.notifyDone();
+    }
+</script>
+</body>
+</html>

LayoutTests/http/tests/xmlhttprequest/resources/print-cache-control-header.cgi
@@ -1,0 +1,10 @@
+#!/usr/bin/perl -wT
+use strict;
+
+print "Content-Type: text/plain\n\n";
+
+foreach (keys %ENV) {
+    if ($_ =~ "HTTP_CACHE_CONTROL") {
+        print $_ . ": " . $ENV{$_} . "\n";
+    }
+}

WebCore/loader/SubresourceLoader.cpp
@@ -84,17 +84,6 @@ PassRefPtr<SubresourceLoader> SubresourceLoader::create(Frame* frame, Subresourc
         newRequest.setHTTPReferrer(fl->outgoingReferrer());
     FrameLoader::addHTTPOriginIfNeeded(newRequest, fl->outgoingOrigin());
 
-    // Use the original request's cache policy for two reasons:
-    // 1. For POST requests, we mutate the cache policy for the main resource,
-    //    but we do not want this to apply to subresources
-    // 2. Delegates that modify the cache policy using willSendRequest: should
-    //    not affect any other resources. Such changes need to be done
-    //    per request.
-    if (newRequest.isConditional())
-        newRequest.setCachePolicy(ReloadIgnoringCacheData);
-    else
-        newRequest.setCachePolicy(fl->originalRequest().cachePolicy());
-
     fl->addExtraFieldsToSubresourceRequest(newRequest);
 
     RefPtr<SubresourceLoader> subloader(adoptRef(new SubresourceLoader(frame, client, sendResourceLoadCallbacks, shouldContentSniff)));

commit 1fcd0ce13d906c9a3596114c14ad210e74e88483

2010-07-16  Nate Chapin  <japhet@chromium.org>
        Reviewed by Darin Fisher.

        Regression in r63100: Don't clear m_loadType in
        FrameLoader::handledOnloadEvents, as it is used
        to make some decisions after the load has actually
        completed.

        Tickling this bug requires manipulation
        via the API (layout tests don't appear to traverse
        the correct codepath), so no layout test.

        https://bugs.webkit.org/show_bug.cgi?id=42298

        * loader/FrameLoader.cpp:
        (WebCore::FrameLoader::handledOnloadEvents): Don't reset m_loadType.
        (WebCore::FrameLoader::addExtraFieldsToRequest): Set subresource cache
           policy in one place and don't depend on m_loadType.
2010-07-16  Nate Chapin  <japhet@chromium.org>

        Reviewed by Darin Fisher.

        Uncomment an assert that broke due to r63100.

        https://bugs.webkit.org/show_bug.cgi?id=42298

        * tests/testwebview.c:

git-svn-id: http://svn.webkit.org/repository/webkit/trunk@63528 268f45cc-cd09-0410-ab3c-d52691b4dbfc

WebCore/loader/FrameLoader.cpp
@@ -2609,8 +2609,6 @@ void FrameLoader::handledOnloadEvents()
 {
     m_client->dispatchDidHandleOnloadEvents();
 
-    m_loadType = FrameLoadTypeStandard;
-
 #if ENABLE(OFFLINE_WEB_APPLICATIONS)
     if (documentLoader())
         documentLoader()->applicationCacheHost()->stopDeferringEvents();
@@ -2690,7 +2688,14 @@ void FrameLoader::addExtraFieldsToRequest(ResourceRequest& request, FrameLoadTyp
     // 2. Delegates that modify the cache policy using willSendRequest: should
     //    not affect any other resources. Such changes need to be done
     //    per request.
-    if (loadType == FrameLoadTypeReload) {
+    if (!mainResource) {
+        if (request.isConditional())
+            request.setCachePolicy(ReloadIgnoringCacheData);
+        else if (documentLoader()->isLoadingInAPISense())
+            request.setCachePolicy(documentLoader()->originalRequest().cachePolicy());
+        else
+            request.setCachePolicy(UseProtocolCachePolicy);
+    } else if (loadType == FrameLoadTypeReload) {
         request.setCachePolicy(ReloadIgnoringCacheData);
         request.setHTTPHeaderField("Cache-Control", "max-age=0");
     } else if (loadType == FrameLoadTypeReloadFromOrigin) {
@@ -2701,8 +2706,6 @@ void FrameLoader::addExtraFieldsToRequest(ResourceRequest& request, FrameLoadTyp
         request.setCachePolicy(ReloadIgnoringCacheData);
     else if (isBackForwardLoadType(loadType) && m_stateMachine.committedFirstRealDocumentLoad() && !request.url().protocolIs("https"))
         request.setCachePolicy(ReturnCacheDataElseLoad);
-    else if (!mainResource && documentLoader()->isLoadingInAPISense())
-        request.setCachePolicy(documentLoader()->originalRequest().cachePolicy());
     
     if (mainResource)
         request.setHTTPAccept(defaultAcceptHeader);

WebKit/gtk/tests/testwebview.c
@@ -266,10 +266,7 @@ static void do_test_webkit_web_view_adjustments(gboolean with_page_cache)
     g_assert_cmpfloat(lower, ==, gtk_adjustment_get_lower(adjustment));
     g_assert_cmpfloat(upper, ==, gtk_adjustment_get_upper(adjustment));
 
-    // This assert is temporarily disabled until we fix the following bug:
-    // https://bugs.webkit.org/show_bug.cgi?id=42114
-    // It should be re-enabled ASAP.
-    // g_assert_cmpfloat(gtk_adjustment_get_value(adjustment), ==, 100.0);
+    g_assert_cmpfloat(gtk_adjustment_get_value(adjustment), ==, 100.0);
 
     g_free(effective_uri);
     g_free(second_uri);