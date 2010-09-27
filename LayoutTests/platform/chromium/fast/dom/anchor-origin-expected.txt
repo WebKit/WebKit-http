WebKit/WebKit-http

commit 4087d793b9d88f3c64556a9766aa0a398f59e9bb

2010-09-26  Adam Barth  <abarth@webkit.org>
        Reviewed by Dimitri Glazkov.

        Implement location.origin and HTMLAnchorElement.origin
        https://bugs.webkit.org/show_bug.cgi?id=46558

        Test the origin attribute of Location and HTMLAnchorElement.

        * fast/dom/anchor-origin-expected.txt: Added.
        * fast/dom/anchor-origin.html: Added.
        * http/tests/misc/location-origin-expected.txt: Added.
        * http/tests/misc/location-origin.html: Added.
2010-09-26  Adam Barth  <abarth@webkit.org>

        Reviewed by Dimitri Glazkov.

        Implement location.origin and HTMLAnchorElement.origin
        https://bugs.webkit.org/show_bug.cgi?id=46558

        These attributes are part of my proposed URL API:
        https://docs.google.com/document/edit?id=1r_VTFKApVOaNIkocrg0z-t7lZgzisTuGTXkdzAk4gLU&hl=en

        Hixie thought we should implement as much of this API as makes sense on
        Location and HTMLAnchorElement as well.  I'm starting with the origin
        attribute, because that seems like the biggest bang for the buck.

        Tests: fast/dom/anchor-origin.html
               http/tests/misc/location-origin.html

        * WebCore.xcodeproj/project.pbxproj:
        * html/HTMLAnchorElement.cpp:
        (WebCore::HTMLAnchorElement::origin):
        * html/HTMLAnchorElement.h:
        * html/HTMLAnchorElement.idl:
        * page/Location.cpp:
        (WebCore::Location::origin):
        * page/Location.h:
        * page/Location.idl:

git-svn-id: http://svn.webkit.org/repository/webkit/trunk@68354 268f45cc-cd09-0410-ab3c-d52691b4dbfc

LayoutTests/fast/dom/anchor-origin-expected.txt
@@ -1,0 +1,13 @@
+Link
+Link
+Link
+Link
+Link
+Link
+http://example.com/foo/bar => http://example.com
+HTTP://example.com/foo/bar => http://example.com
+https://example.com/ttt?ggg => https://example.com
+ftp://example.com/ttt?ggg => ftp://example.com
+file:///home/abarth => file://
+data:text/html,%3Cb%3Efoo%3C/b%3E => null
+

LayoutTests/fast/dom/Window/window-appendages-cleared-expected.txt
@@ -9,6 +9,7 @@ PASS location.hash == "LEFTOVER" is false
 PASS location.host == "LEFTOVER" is false
 PASS location.hostname == "LEFTOVER" is false
 PASS location.href == "LEFTOVER" is false
+PASS location.origin == "LEFTOVER" is false
 PASS location.pathname == "LEFTOVER" is false
 PASS location.port == "LEFTOVER" is false
 PASS location.protocol == "LEFTOVER" is false

LayoutTests/fast/dom/Window/window-properties-expected.txt
@@ -2224,6 +2224,7 @@ window.location.hash [string]
 window.location.host [string]
 window.location.hostname [string]
 window.location.href [string]
+window.location.origin [string]
 window.location.pathname [string]
 window.location.port [string]
 window.location.protocol [string]

LayoutTests/fast/dom/anchor-origin.html
@@ -1,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<body>
+<script>
+if (window.layoutTestController)
+    layoutTestController.dumpAsText();
+
+var cases = [
+    "http://example.com/foo/bar",
+    "HTTP://example.com/foo/bar",
+    "https://example.com/ttt?ggg",
+    "ftp://example.com/ttt?ggg",
+    "file:///home/abarth",
+    "data:text/html,<b>foo</b>"
+];
+
+for (var i = 0; i < cases.length; ++i)
+    document.write('<a href="' + cases[i] + '">Link</a><br>');
+
+var elmts = document.getElementsByTagName('a');
+for (var i = 0; i < elmts.length; ++i)
+    document.write(elmts[i].href + ' => ' + elmts[i].origin + '<br>');
+</script>
+</body>
+</html>

LayoutTests/fast/dom/prototype-inheritance-2-expected.txt
@@ -81,7 +81,7 @@ PASS DOMWindow from inner
 PASS DOMWindowPrototype from inner.document.forms.testForm.0.ownerDocument.defaultView.__proto__
 PASS DocumentPrototype from inner.document.forms.testForm.0.ownerDocument.__proto__.__proto__
 PASS ElementPrototype from inner.document.forms.testForm.0.__proto__.__proto__.__proto__
-FAIL Function from inner.document.location.pathname.constructor
+FAIL Function from inner.document.location.protocol.constructor
 PASS HTMLAnchorElement from inner.document.forms.testForm.0.parentNode.firstElementChild
 PASS HTMLAnchorElementConstructor from inner.document.forms.testForm.0.parentNode.firstElementChild.constructor
 PASS HTMLAnchorElementPrototype from inner.document.forms.testForm.0.parentNode.firstElementChild.__proto__
@@ -189,7 +189,7 @@ PASS ScreenPrototype from inner.document.forms.testForm.0.ownerDocument.defaultV
 PASS Storage from inner.document.forms.testForm.0.ownerDocument.defaultView.localStorage
 PASS StorageConstructor from inner.document.forms.testForm.0.ownerDocument.defaultView.localStorage.constructor
 PASS StoragePrototype from inner.document.forms.testForm.0.ownerDocument.defaultView.localStorage.__proto__
-FAIL String from inner.document.location.pathname
+FAIL String from inner.document.location.protocol
 PASS StyleMedia from inner.document.forms.testForm.0.ownerDocument.defaultView.styleMedia
 PASS StyleMediaConstructor from inner.document.forms.testForm.0.ownerDocument.defaultView.styleMedia.constructor
 PASS StyleMediaPrototype from inner.document.forms.testForm.0.ownerDocument.defaultView.styleMedia.__proto__

LayoutTests/http/tests/misc/location-origin-expected.txt
@@ -1,0 +1,3 @@
+http://127.0.0.1:8000
+http://127.0.0.1:8000
+

LayoutTests/http/tests/misc/location-origin.html
@@ -1,0 +1,7 @@
+<script>
+if (window.layoutTestController)
+    layoutTestController.dumpAsText();
+
+document.write(window.location.origin + '<br>');
+document.write(document.location.origin + '<br>');
+</script>

WebCore/html/HTMLAnchorElement.cpp
@@ -442,6 +442,12 @@ String HTMLAnchorElement::search() const
     return query.isEmpty() ? "" : "?" + query;
 }
 
+String HTMLAnchorElement::origin() const
+{
+    RefPtr<SecurityOrigin> origin = SecurityOrigin::create(href());
+    return origin->toString();
+}
+
 void HTMLAnchorElement::setSearch(const String& value)
 {
     KURL url = href();

commit 0aa3fbc007adc143b4d682328b14e40ed4c77a84

2010-09-27  James Kozianski  <koz@chromium.org>
        Reviewed by Adam Barth.

        [Chromium] fast/dom/anchor-origin.html failing since r68354
        https://bugs.webkit.org/show_bug.cgi?id=46597

        Adding expectations needed for Chrome due to URL parsing differences
        between KURL and GURL. Once a spec for URL parsing is defined we can
        update these expectations to conform to it.

        * platform/chromium/fast/dom/anchor-origin-expected.txt: Added.

git-svn-id: http://svn.webkit.org/repository/webkit/trunk@68376 268f45cc-cd09-0410-ab3c-d52691b4dbfc

LayoutTests/platform/chromium/fast/dom/anchor-origin-expected.txt
@@ -1,0 +1,12 @@
+Link
+Link
+Link
+Link
+Link
+Link
+http://example.com/foo/bar => http://example.com
+http://example.com/foo/bar => http://example.com
+https://example.com/ttt?ggg => https://example.com
+ftp://example.com/ttt?ggg => ftp://example.com
+file:///home/abarth => file://
+data:text/html,foo => null