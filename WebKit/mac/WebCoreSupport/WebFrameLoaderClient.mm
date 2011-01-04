WebKit/WebKit-http

commit 0c260bc104e17bf673dd26a9a502bc9d9a2eb67c

2010-06-21  Anders Carlsson  <andersca@apple.com>
        Reviewed by Sam Weinig.

        More WebBasePluginPackage cleanup
        https://bugs.webkit.org/show_bug.cgi?id=40944

        * Plugins/Hosted/HostedNetscapePluginStream.mm:
        (WebKit::HostedNetscapePluginStream::pluginCancelledConnectionError):
        * Plugins/Hosted/NetscapePluginHostManager.mm:
        (WebKit::NetscapePluginHostManager::spawnPluginHost):
        * Plugins/WebBaseNetscapePluginStream.mm:
        (WebNetscapePluginStream::pluginCancelledConnectionError):
        * Plugins/WebBaseNetscapePluginView.mm:
        (WebHaltablePlugin::pluginName):
        * Plugins/WebBasePluginPackage.h:
        * Plugins/WebBasePluginPackage.mm:
        (-[WebBasePluginPackage initWithPath:]):
        (-[WebBasePluginPackage _objectForInfoDictionaryKey:]):
        (-[WebBasePluginPackage getPluginInfoFromPLists]):
        (-[WebBasePluginPackage load]):
        (-[WebBasePluginPackage dealloc]):
        (-[WebBasePluginPackage finalize]):
        (-[WebBasePluginPackage pluginInfo]):
        (-[WebBasePluginPackage supportsExtension:]):
        (-[WebBasePluginPackage supportsMIMEType:WebCore::]):
        (-[WebBasePluginPackage MIMETypeForExtension:]):
        (-[WebBasePluginPackage isJavaPlugIn]):
        (-[WebBasePluginPackage versionNumber]):
        (-[WebBasePluginPackage WebCore::]):
        * Plugins/WebNetscapePluginPackage.mm:
        (-[WebNetscapePluginPackage openResourceFile]):
        (-[WebNetscapePluginPackage closeResourceFile:]):
        (-[WebNetscapePluginPackage getPluginInfoFromResources]):
        (-[WebNetscapePluginPackage _initWithPath:]):
        (-[WebNetscapePluginPackage _applyDjVuWorkaround]):
        (-[WebNetscapePluginPackage _tryLoad]):
        (-[WebNetscapePluginPackage supportsSnapshotting]):
        (-[WebNetscapePluginPackage _unloadWithShutdown:]):
        * Plugins/WebPluginDatabase.mm:
        (-[WebPluginDatabase refresh]):

WebKit/mac/WebCoreSupport/WebFrameLoaderClient.mm
@@ -1632,7 +1632,7 @@ PassRefPtr<Widget> WebFrameLoaderClient::createPlugin(const IntSize& size, HTMLP
             KURL pluginPageURL = document->completeURL(deprecatedParseURL(parameterValue(paramNames, paramValues, "pluginspage")));
             if (!pluginPageURL.protocolInHTTPFamily())
                 pluginPageURL = KURL();
-            NSString *pluginName = pluginPackage ? (NSString *)[pluginPackage name] : nil;
+            NSString *pluginName = pluginPackage ? (NSString *)[pluginPackage pluginInfo].name : nil;
 
             NSError *error = [[NSError alloc] _initWithPluginErrorCode:errorCode
                                                             contentURL:pluginURL pluginPageURL:pluginPageURL pluginName:pluginName MIMEType:MIMEType];
@@ -1724,7 +1724,7 @@ PassRefPtr<Widget> WebFrameLoaderClient::createJavaAppletWidget(const IntSize& s
     if (!view) {
         WebResourceDelegateImplementationCache* implementations = WebViewGetResourceLoadDelegateImplementations(getWebView(m_webFrame.get()));
         if (implementations->plugInFailedWithErrorFunc) {
-            NSString *pluginName = pluginPackage ? (NSString *)[pluginPackage name] : nil;
+            NSString *pluginName = pluginPackage ? (NSString *)[pluginPackage pluginInfo].name : nil;
             NSError *error = [[NSError alloc] _initWithPluginErrorCode:WebKitErrorJavaUnavailable contentURL:nil pluginPageURL:nil pluginName:pluginName MIMEType:MIMEType];
             CallResourceLoadDelegate(implementations->plugInFailedWithErrorFunc, [m_webFrame.get() webView],
                                      @selector(webView:plugInFailedWithError:dataSource:), error, [m_webFrame.get() _dataSource]);

WebKit/mac/Plugins/Hosted/HostedNetscapePluginStream.mm
@@ -256,7 +256,7 @@ NSError *HostedNetscapePluginStream::pluginCancelledConnectionError() const
     return [[[NSError alloc] _initWithPluginErrorCode:WebKitErrorPlugInCancelledConnection
                                            contentURL:m_responseURL ? m_responseURL.get() : m_requestURL.get()
                                         pluginPageURL:nil
-                                           pluginName:[[m_instance->pluginView() pluginPackage] name]
+                                           pluginName:[[m_instance->pluginView() pluginPackage] pluginInfo].name
                                              MIMEType:m_mimeType.get()] autorelease];
 }
 

WebKit/mac/Plugins/Hosted/NetscapePluginHostManager.mm
@@ -152,7 +152,7 @@ bool NetscapePluginHostManager::spawnPluginHost(WebNetscapePluginPackage *packag
     NSString *visibleName = [NSString stringWithFormat:UI_STRING("%@ (%@ Internet plug-in)",
                                                                  "visible name of the plug-in host process. The first argument is the plug-in name "
                                                                  "and the second argument is the application name."),
-                             [[package filename] stringByDeletingPathExtension], [[NSProcessInfo processInfo] processName]];
+                             [(NSString *)[package pluginInfo].file stringByDeletingPathExtension], [[NSProcessInfo processInfo] processName]];
     
     NSDictionary *hostProperties = [[NSDictionary alloc] initWithObjectsAndKeys:
                                     visibleName, @"visibleName",

WebKit/mac/Plugins/WebBaseNetscapePluginStream.mm
@@ -104,7 +104,7 @@ NSError *WebNetscapePluginStream::pluginCancelledConnectionError() const
     return [[[NSError alloc] _initWithPluginErrorCode:WebKitErrorPlugInCancelledConnection
                                            contentURL:m_responseURL ? m_responseURL.get() : m_requestURL.get()
                                         pluginPageURL:nil
-                                           pluginName:[[m_pluginView.get() pluginPackage] name]
+                                           pluginName:[[m_pluginView.get() pluginPackage] pluginInfo].name
                                              MIMEType:m_mimeType.get()] autorelease];
 }
 

WebKit/mac/Plugins/WebBaseNetscapePluginView.mm
@@ -108,7 +108,7 @@ bool WebHaltablePlugin::isWindowed() const
 
 String WebHaltablePlugin::pluginName() const
 {
-    return [[m_view pluginPackage] name];
+    return [[m_view pluginPackage] pluginInfo].name;
 }
 
 @implementation WebBaseNetscapePluginView

WebKit/mac/Plugins/WebBasePluginPackage.h
@@ -27,6 +27,7 @@
  */
 
 #import <WebCore/PluginData.h>
+#import <wtf/RetainPtr.h>
 
 #if ENABLE(NETSCAPE_PLUGIN_API)
 #import <WebKit/npfunctions.h>
@@ -56,13 +57,10 @@ typedef void (*BP_CreatePluginMIMETypesPreferencesFuncPtr)(void);
 {
     NSMutableSet *pluginDatabases;
     
-    WebCore::String name;
     WebCore::String path;
-    WebCore::String pluginDescription;
+    WebCore::PluginInfo pluginInfo;
 
-    Vector<WebCore::MimeClassInfo> mimeTypes;
-
-    CFBundleRef cfBundle;
+    RetainPtr<CFBundleRef> cfBundle;
 
     BP_CreatePluginMIMETypesPreferencesFuncPtr BP_CreatePluginMIMETypesPreferences;
 }
@@ -75,12 +73,9 @@ typedef void (*BP_CreatePluginMIMETypesPreferencesFuncPtr)(void);
 - (BOOL)load;
 - (void)unload;
 
-- (WebCore::String)filename;
-- (const WebCore::String&)name;
 - (const WebCore::String&)path;
-- (const WebCore::String&)pluginDescription;
 
-- (const Vector<WebCore::MimeClassInfo>&)mimeTypes;
+- (const WebCore::PluginInfo&)pluginInfo;
 
 - (WebCore::String)bundleIdentifier;
 

WebKit/mac/Plugins/WebBasePluginPackage.mm
@@ -50,7 +50,7 @@
 
 #define JavaCocoaPluginIdentifier   "com.apple.JavaPluginCocoa"
 #define JavaCarbonPluginIdentifier  "com.apple.JavaAppletPlugin"
-#define JavaCFMPluginFilename       @"Java Applet Plugin Enabler"
+#define JavaCFMPluginFilename       "Java Applet Plugin Enabler"
 
 #define QuickTimeCarbonPluginIdentifier       "com.apple.QuickTime Plugin.plugin"
 #define QuickTimeCocoaPluginIdentifier        "com.apple.quicktime.webplugin"
@@ -126,7 +126,7 @@ static NSString *pathByResolvingSymlinksAndAliases(NSString *thePath)
         return nil;
         
     path = pathByResolvingSymlinksAndAliases(pluginPath);
-    cfBundle = CFBundleCreate(kCFAllocatorDefault, (CFURLRef)[NSURL fileURLWithPath:path]);
+    cfBundle.adoptCF(CFBundleCreate(kCFAllocatorDefault, (CFURLRef)[NSURL fileURLWithPath:path]));
 
 #ifndef __ppc__
     // 32-bit PowerPC is the only platform where non-bundled CFM plugins are supported
@@ -170,7 +170,7 @@ static NSString *pathByResolvingSymlinksAndAliases(NSString *thePath)
 
 - (id)_objectForInfoDictionaryKey:(NSString *)key
 {
-    CFDictionaryRef bundleInfoDictionary = CFBundleGetInfoDictionary(cfBundle);
+    CFDictionaryRef bundleInfoDictionary = CFBundleGetInfoDictionary(cfBundle.get());
     if (!bundleInfoDictionary)
         return nil;
 
@@ -233,22 +233,23 @@ static NSString *pathByResolvingSymlinksAndAliases(NSString *thePath)
         description = [MIMEDictionary objectForKey:WebPluginTypeDescriptionKey];
         mimeClassInfo.desc = description;
 
-        mimeTypes.append(mimeClassInfo);
+        pluginInfo.mimes.append(mimeClassInfo);
         if (!description)
             description = @"";
     }
 
-    NSString *filename = [self filename];
+    NSString *filename = [(NSString *)path lastPathComponent];
+    pluginInfo.file = filename;
 
     NSString *theName = [self _objectForInfoDictionaryKey:WebPluginNameKey];
     if (!theName)
         theName = filename;
-    name = theName;
+    pluginInfo.name = theName;
 
     description = [self _objectForInfoDictionaryKey:WebPluginDescriptionKey];
     if (!description)
         description = filename;
-    pluginDescription = description;
+    pluginInfo.desc = description;
 
     return YES;
 }
@@ -256,7 +257,7 @@ static NSString *pathByResolvingSymlinksAndAliases(NSString *thePath)
 - (BOOL)load
 {
     if (cfBundle && !BP_CreatePluginMIMETypesPreferences)
-        BP_CreatePluginMIMETypesPreferences = (BP_CreatePluginMIMETypesPreferencesFuncPtr)CFBundleGetFunctionPointerForName(cfBundle, CFSTR("BP_CreatePluginMIMETypesPreferences"));
+        BP_CreatePluginMIMETypesPreferences = (BP_CreatePluginMIMETypesPreferencesFuncPtr)CFBundleGetFunctionPointerForName(cfBundle.get(), CFSTR("BP_CreatePluginMIMETypesPreferences"));
     
     return YES;
 }
@@ -265,9 +266,6 @@ static NSString *pathByResolvingSymlinksAndAliases(NSString *thePath)
 {
     ASSERT(!pluginDatabases || [pluginDatabases count] == 0);
     [pluginDatabases release];
-    
-    if (cfBundle)
-        CFRelease(cfBundle);
     
     [super dealloc];
 }
@@ -278,15 +276,7 @@ static NSString *pathByResolvingSymlinksAndAliases(NSString *thePath)
     ASSERT(!pluginDatabases || [pluginDatabases count] == 0);
     [pluginDatabases release];
 
-    if (cfBundle)
-        CFRelease(cfBundle);
-
     [super finalize];
-}
-
-- (const String&)name
-{
-    return name;
 }
 
 - (const String&)path
@@ -294,27 +284,17 @@ static NSString *pathByResolvingSymlinksAndAliases(NSString *thePath)
     return path;
 }
 
-- (String)filename
+- (const PluginInfo&)pluginInfo
 {
-    return [(NSString *)path lastPathComponent];
-}
-
-- (const String&)pluginDescription
-{
-    return pluginDescription;
-}
-
-- (const Vector<WebCore::MimeClassInfo>&)mimeTypes
-{
-    return mimeTypes;
+    return pluginInfo;
 }
 
 - (BOOL)supportsExtension:(const String&)extension
 {
     ASSERT(extension.lower() == extension);
     
-    for (size_t i = 0; i < mimeTypes.size(); ++i) {
-        const Vector<String>& extensions = mimeTypes[i].extensions;
+    for (size_t i = 0; i < pluginInfo.mimes.size(); ++i) {
+        const Vector<String>& extensions = pluginInfo.mimes[i].extensions;
 
         if (find(extensions.begin(), extensions.end(), extension) != extensions.end())
             return YES;
@@ -327,8 +307,8 @@ static NSString *pathByResolvingSymlinksAndAliases(NSString *thePath)
 {
     ASSERT(mimeType.lower() == mimeType);
     
-    for (size_t i = 0; i < mimeTypes.size(); ++i) {
-        if (mimeTypes[i].type == mimeType)
+    for (size_t i = 0; i < pluginInfo.mimes.size(); ++i) {
+        if (pluginInfo.mimes[i].type == mimeType)
             return YES;
     }
     
@@ -339,8 +319,8 @@ static NSString *pathByResolvingSymlinksAndAliases(NSString *thePath)
 {
     ASSERT(extension.lower() == extension);
     
-    for (size_t i = 0; i < mimeTypes.size(); ++i) {
-        const MimeClassInfo& mimeClassInfo = mimeTypes[i];
+    for (size_t i = 0; i < pluginInfo.mimes.size(); ++i) {
+        const MimeClassInfo& mimeClassInfo = pluginInfo.mimes[i];
         const Vector<String>& extensions = mimeClassInfo.extensions;
 
         if (find(extensions.begin(), extensions.end(), extension) != extensions.end())
@@ -360,7 +340,7 @@ static NSString *pathByResolvingSymlinksAndAliases(NSString *thePath)
 {
     const String& bundleIdentifier = [self bundleIdentifier];
     return bundleIdentifier == JavaCocoaPluginIdentifier || bundleIdentifier == JavaCarbonPluginIdentifier ||
-        [(NSString *)[self filename] _webkit_isCaseInsensitiveEqualToString:JavaCFMPluginFilename];
+        equalIgnoringCase(pluginInfo.file, JavaCFMPluginFilename);
 }
 
 static inline void swapIntsInHeader(uint8_t* bytes, unsigned length)
@@ -445,7 +425,7 @@ static inline void swapIntsInHeader(uint8_t* bytes, unsigned length)
 - (UInt32)versionNumber
 {
     // CFBundleGetVersionNumber doesn't work with all possible versioning schemes, but we think for now it's good enough for us.
-    return CFBundleGetVersionNumber(cfBundle);
+    return CFBundleGetVersionNumber(cfBundle.get());
 }
 
 - (void)wasAddedToPluginDatabase:(WebPluginDatabase *)database
@@ -467,7 +447,7 @@ static inline void swapIntsInHeader(uint8_t* bytes, unsigned length)
 
 - (WebCore::String)bundleIdentifier
 {
-    return CFBundleGetIdentifier(cfBundle);
+    return CFBundleGetIdentifier(cfBundle.get());
 }
 
 @end

WebKit/mac/Plugins/WebNetscapePluginPackage.mm
@@ -107,7 +107,7 @@ static TransitionVector tVectorForFunctionPointer(FunctionPointer);
     }
 #endif
 
-    return CFBundleOpenBundleResourceMap(cfBundle);
+    return CFBundleOpenBundleResourceMap(cfBundle.get());
 }
 
 - (void)closeResourceFile:(ResFileRefNum)resRef
@@ -119,7 +119,7 @@ static TransitionVector tVectorForFunctionPointer(FunctionPointer);
     }
 #endif
 
-    CFBundleCloseBundleResourceMap(cfBundle, resRef);
+    CFBundleCloseBundleResourceMap(cfBundle.get(), resRef);
 }
 
 - (NSString *)stringForStringListID:(SInt16)stringListID andIndex:(SInt16)index
@@ -177,24 +177,25 @@ static TransitionVector tVectorForFunctionPointer(FunctionPointer);
         }
         
         description = [self stringForStringListID:MIMEDescriptionStringNumber
-                                         andIndex:mimeTypes.size()];
+                                         andIndex:pluginInfo.mimes.size()];
         mimeClassInfo.desc = description;
 
-        mimeTypes.append(mimeClassInfo);
+        pluginInfo.mimes.append(mimeClassInfo);
     }
 
-    NSString *filename = [self filename];
+    NSString *filename = [(NSString *)path lastPathComponent];
+    pluginInfo.file = filename;
     
     description = [self stringForStringListID:PluginNameOrDescriptionStringNumber andIndex:1];
     if (!description)
         description = filename;
-    pluginDescription = description;
+    pluginInfo.desc = description;
     
     
     NSString *theName = [self stringForStringListID:PluginNameOrDescriptionStringNumber andIndex:2];
     if (!theName)
         theName = filename;
-    name = theName;
+    pluginInfo.name = theName;
     
     [self closeResourceFile:resRef];
     
@@ -209,7 +210,7 @@ static TransitionVector tVectorForFunctionPointer(FunctionPointer);
 
     if (cfBundle) {
         // Bundle
-        CFBundleGetPackageInfo(cfBundle, &type, NULL);
+        CFBundleGetPackageInfo(cfBundle.get(), &type, NULL);
 #ifdef SUPPORT_CFM
         isBundle = YES;
 #endif
@@ -230,7 +231,7 @@ static TransitionVector tVectorForFunctionPointer(FunctionPointer);
 
     // Check if the executable is Mach-O or CFM.
     if (cfBundle) {
-        RetainPtr<CFURLRef> executableURL(AdoptCF, CFBundleCopyExecutableURL(cfBundle));
+        RetainPtr<CFURLRef> executableURL(AdoptCF, CFBundleCopyExecutableURL(cfBundle.get()));
         if (!executableURL)
             return NO;
         NSFileHandle *executableFile = [NSFileHandle fileHandleForReadingAtPath:[(NSURL *)executableURL.get() path]];
@@ -248,7 +249,7 @@ static TransitionVector tVectorForFunctionPointer(FunctionPointer);
 #endif
 
 #if USE(PLUGIN_HOST_PROCESS)
-        RetainPtr<CFArrayRef> archs(AdoptCF, CFBundleCopyExecutableArchitectures(cfBundle));
+        RetainPtr<CFArrayRef> archs(AdoptCF, CFBundleCopyExecutableArchitectures(cfBundle.get()));
         
         if ([(NSArray *)archs.get() containsObject:[NSNumber numberWithInteger:NSBundleExecutableArchitectureX86_64]])
             pluginHostArchitecture = CPU_TYPE_X86_64;
@@ -325,7 +326,7 @@ static TransitionVector tVectorForFunctionPointer(FunctionPointer);
     if (!cfBundle)
         return;
     
-    if ([(NSString *)CFBundleGetIdentifier(cfBundle) isEqualToString:@"com.lizardtech.NPDjVu"]) {
+    if ([self bundleIdentifier] == "com.lizardtech.NPDjVu") {
         // The DjVu plug-in will crash copying the vtable if it's too big so we cap it to 
         // what the plug-in expects here. 
         // size + version + 40 function pointers.
@@ -354,7 +355,7 @@ static TransitionVector tVectorForFunctionPointer(FunctionPointer);
     CFAbsoluteTime currentTime;
     CFAbsoluteTime duration;
 #endif
-    LOG(Plugins, "%f Load timing started for: %@", start, (NSString *)[self name]);
+    LOG(Plugins, "%f Load timing started for: %@", start, (NSString *)[self pluginInfo].name);
 
     if (isLoaded)
         return YES;
@@ -362,7 +363,7 @@ static TransitionVector tVectorForFunctionPointer(FunctionPointer);
 #ifdef SUPPORT_CFM
     if (isBundle) {
 #endif
-        if (!CFBundleLoadExecutable(cfBundle))
+        if (!CFBundleLoadExecutable(cfBundle.get()))
             return NO;
 #if !LOG_DISABLED
         currentTime = CFAbsoluteTimeGetCurrent();
@@ -378,9 +379,9 @@ static TransitionVector tVectorForFunctionPointer(FunctionPointer);
                 return NO;
         } else {
 #endif
-            NP_Initialize = (NP_InitializeFuncPtr)CFBundleGetFunctionPointerForName(cfBundle, CFSTR("NP_Initialize"));
-            NP_GetEntryPoints = (NP_GetEntryPointsFuncPtr)CFBundleGetFunctionPointerForName(cfBundle, CFSTR("NP_GetEntryPoints"));
-            NP_Shutdown = (NPP_ShutdownProcPtr)CFBundleGetFunctionPointerForName(cfBundle, CFSTR("NP_Shutdown"));
+            NP_Initialize = (NP_InitializeFuncPtr)CFBundleGetFunctionPointerForName(cfBundle.get(), CFSTR("NP_Initialize"));
+            NP_GetEntryPoints = (NP_GetEntryPointsFuncPtr)CFBundleGetFunctionPointerForName(cfBundle.get(), CFSTR("NP_GetEntryPoints"));
+            NP_Shutdown = (NPP_ShutdownProcPtr)CFBundleGetFunctionPointerForName(cfBundle.get(), CFSTR("NP_Shutdown"));
             if (!NP_Initialize || !NP_GetEntryPoints || !NP_Shutdown)
                 return NO;
 #ifdef SUPPORT_CFM
@@ -627,9 +628,9 @@ static TransitionVector tVectorForFunctionPointer(FunctionPointer);
         pluginVersion = pluginFuncs.version;
         
         if (pluginFuncs.javaClass)
-            LOG(LiveConnect, "%@:  mach-o entry point for NPP_GetJavaClass = %p", (NSString *)[self name], pluginFuncs.javaClass);
+            LOG(LiveConnect, "%@:  mach-o entry point for NPP_GetJavaClass = %p", (NSString *)[self pluginInfo].name, pluginFuncs.javaClass);
         else
-            LOG(LiveConnect, "%@:  no entry point for NPP_GetJavaClass", (NSString *)[self name]);
+            LOG(LiveConnect, "%@:  no entry point for NPP_GetJavaClass", (NSString *)[self pluginInfo].name);
 
 #ifdef SUPPORT_CFM
     }
@@ -698,7 +699,7 @@ static TransitionVector tVectorForFunctionPointer(FunctionPointer);
         return YES;
     
     // Flash has a bogus Info.plist entry for CFBundleVersionString, so use CFBundleShortVersionString.
-    NSString *versionString = (NSString *)CFDictionaryGetValue(CFBundleGetInfoDictionary(cfBundle), CFSTR("CFBundleShortVersionString"));
+    NSString *versionString = (NSString *)CFDictionaryGetValue(CFBundleGetInfoDictionary(cfBundle.get()), CFSTR("CFBundleShortVersionString"));
     
     if (![versionString hasPrefix:@"10.1"])
         return YES;
@@ -754,7 +755,7 @@ TransitionVector tVectorForFunctionPointer(FunctionPointer fp)
     if (!isLoaded)
         return;
     
-    LOG(Plugins, "Unloading %@...", (NSString *)name);
+    LOG(Plugins, "Unloading %@...", (NSString *)pluginInfo.name);
 
     // Cannot unload a plug-in package while an instance is still using it
     if (instanceCount > 0) {

commit 1230fa5971b913c3e5919b7055335450746b228e

<http://webkit.org/b/51889> WebFrameLoaderClient::createMediaPlayerProxyPlugin() should use WebCore::PluginInfo.name
Reviewed by Eric Carlson.

This originally broke in r61581.  It is inside an
ENABLE(PLUGIN_PROXY_FOR_VIDEO) macro, so it didn't break the Mac
build then.

* WebCoreSupport/WebFrameLoaderClient.mm: Call
(WebFrameLoaderClient::createMediaPlayerProxyPlugin): Updated to
use WebCore::PluginInfo.name instead of
-[WebBasePluginPackage name].

git-svn-id: http://svn.webkit.org/repository/webkit/trunk@74996 268f45cc-cd09-0410-ab3c-d52691b4dbfc

WebKit/mac/WebCoreSupport/WebFrameLoaderClient.mm
@@ -1866,7 +1866,7 @@ PassRefPtr<Widget> WebFrameLoaderClient::createMediaPlayerProxyPlugin(const IntS
 
     if (errorCode) {
         NSError *error = [[NSError alloc] _initWithPluginErrorCode:errorCode
-            contentURL:URL pluginPageURL:nil pluginName:[pluginPackage name] MIMEType:mimeType];
+            contentURL:URL pluginPageURL:nil pluginName:[pluginPackage pluginInfo].name MIMEType:mimeType];
         WebNullPluginView *nullView = [[[WebNullPluginView alloc] initWithFrame:NSMakeRect(0, 0, size.width(), size.height())
             error:error DOMElement:kit(element)] autorelease];
         view = nullView;