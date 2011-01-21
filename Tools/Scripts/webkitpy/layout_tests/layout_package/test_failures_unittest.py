WebKit/WebKit-http

commit 4ae944bbb9287d5b9e152b4dcb108ed516174339

2010-11-24  Hayato Ito  <hayato@chromium.org>
        Reviewed by David Levin.

        Add files which Chromium port generates to .gitignore.

        https://bugs.webkit.org/show_bug.cgi?id=49943

        * .gitignore:

git-svn-id: http://svn.webkit.org/repository/webkit/trunk@72688 268f45cc-cd09-0410-ab3c-d52691b4dbfc

.gitignore
@@ -60,6 +60,10 @@ WebKit/chromium/webkit/
 WebKit/chromium/printing/
 WebKit/chromium/depot_tools/
 
+# Ignore files that Chromium port generates
+/Makefile.chromium
+/out
+
 # Ignore compiled java tests:
 LayoutTests/java/*.class
 

commit 0a64fc05bf06498716cf882be94ceba7bc480963

2011-01-20  Dirk Pranke  <dpranke@chromium.org>
        Reviewed by Tony Chang.

        Fix bug introduced in r72688 / bug 52768 that broke the
        generation of the output filename links in the results.html
        output. Apparently none of the unit tests actually checked to
        make sure the generated links were correct :(

        https://bugs.webkit.org/show_bug.cgi?id=52854

        * Scripts/webkitpy/layout_tests/layout_package/test_failures.py:
        * Scripts/webkitpy/layout_tests/layout_package/test_failures_unittest.py:

git-svn-id: http://svn.webkit.org/repository/webkit/trunk@76311 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Tools/Scripts/webkitpy/layout_tests/layout_package/test_failures.py
@@ -123,7 +123,7 @@ class TestFailure(object):
         # FIXME: technically this breaks if files don't use ".ext" to indicate
         # the extension, but passing in a Filesystem object here is a huge
         # hassle.
-        return filename[filename.rfind('.')] + modifier
+        return filename[:filename.rfind('.')] + modifier
 
 
 class FailureWithType(TestFailure):

Tools/Scripts/webkitpy/layout_tests/layout_package/test_failures_unittest.py
@@ -89,6 +89,12 @@ class Test(unittest.TestCase):
         crash_set = set([FailureCrash(), "FailureCrash"])
         self.assertEqual(len(crash_set), 2)
 
+    def test_relative_output_filename(self):
+        # This could be any Failure* object, since we're testing a method
+        # on the base class.
+        failure_obj = FailureTextMismatch()
+        actual_filename = failure_obj.relative_output_filename("fast/html/article-element.html", "-actual.txt")
+        self.assertEquals(actual_filename, "fast/html/article-element-actual.txt")
 
 if __name__ == '__main__':
     unittest.main()