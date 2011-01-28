WebKit/WebKit-http

commit 580769f8e9a0359127a50562931a7e5523573a04

2011-01-27  Rob Buis  <rwlbuis@gmail.com>
        Reviewed by Kent Tamura.

        Color changes to option elements in a select multiple aren't drawn immediately
        https://bugs.webkit.org/show_bug.cgi?id=49790

        Redirect style changes on <option> element to the owner <select> element.

        Test: fast/repaint/select-option-background-color.html

        * html/HTMLOptionElement.cpp:
        (WebCore::HTMLOptionElement::setRenderStyle):

git-svn-id: http://svn.webkit.org/repository/webkit/trunk@76826 268f45cc-cd09-0410-ab3c-d52691b4dbfc

LayoutTests/platform/mac/fast/repaint/select-option-background-color-expected.txt
@@ -1,0 +1,14 @@
+layer at (0,0) size 800x600
+  RenderView at (0,0) size 800x600
+layer at (0,0) size 800x600
+  RenderBlock {HTML} at (0,0) size 800x600
+    RenderBody {BODY} at (0,0) size 800x600
+      RenderBlock {P} at (0,0) size 800x18
+        RenderText {#text} at (0,0) size 220x18
+          text run at (0,0) width 220: "This is a test for regression against "
+        RenderInline {A} at (0,0) size 305x18 [color=#0000EE]
+          RenderText {#text} at (220,0) size 305x18
+            text run at (220,0) width 305: "https://bugs.webkit.org/show_bug.cgi?id=49790"
+      RenderBlock (anonymous) at (0,34) size 800x61
+        RenderListBox {SELECT} at (2,2) size 25x57 [bgcolor=#FFFFFF] [border: (1px inset #808080)]
+        RenderText {#text} at (0,0) size 0x0

LayoutTests/fast/repaint/select-option-background-color.html
@@ -1,0 +1,21 @@
+<head>
+<title>This is a test for regression against https://bugs.webkit.org/show_bug.cgi?id=49790</title>
+</head>
+
+<body style="margin: 0;" onload="runRepaintTest()">
+    <script>
+        function repaintTest()
+        {
+            document.getElementById('option').style.backgroundColor = 'green';
+        }
+    </script>
+
+    <script src="resources/repaint.js"></script>
+
+    <p>This is a test for regression against <a href="https://bugs.webkit.org/show_bug.cgi?id=49790">https://bugs.webkit.org/show_bug.cgi?id=49790</a></p>
+
+    <select multiple="multiple">
+        <option id="option" value="A">A</option>
+        <option id="option" value="B">B</option>
+    </select>
+</body>

LayoutTests/platform/mac/fast/repaint/select-option-background-color-expected.checksum
@@ -1,0 +1,1 @@
+29bae32726dbba8b202adb59b33e7947

Source/WebCore/html/HTMLOptionElement.cpp
@@ -221,6 +221,9 @@ String HTMLOptionElement::label() const
 void HTMLOptionElement::setRenderStyle(PassRefPtr<RenderStyle> newStyle)
 {
     m_style = newStyle;
+    if (HTMLSelectElement* select = ownerSelectElement())
+        if (RenderObject* renderer = select->renderer())
+            renderer->repaint();
 }
 
 RenderStyle* HTMLOptionElement::nonRendererRenderStyle() const

commit 0aedc31335d566c4c989ee0625de3cbcdaa6e461

[Windows 7 Release Tests] fast/repaint/select-option-background-color.html failing since introduction in r76826 https://bugs.webkit.org/show_bug.cgi?id=53327
Unreviewed, adding Windows-specific results to get the Windows 7 Release Test bot green.

* platform/win/fast/repaint/select-option-background-color-expected.txt: Added.



git-svn-id: http://svn.webkit.org/repository/webkit/trunk@76976 268f45cc-cd09-0410-ab3c-d52691b4dbfc

LayoutTests/platform/win/fast/repaint/select-option-background-color-expected.txt
@@ -1,0 +1,14 @@
+layer at (0,0) size 800x600
+  RenderView at (0,0) size 800x600
+layer at (0,0) size 800x600
+  RenderBlock {HTML} at (0,0) size 800x600
+    RenderBody {BODY} at (0,0) size 800x600
+      RenderBlock {P} at (0,0) size 800x18
+        RenderText {#text} at (0,0) size 220x18
+          text run at (0,0) width 220: "This is a test for regression against "
+        RenderInline {A} at (0,0) size 305x18 [color=#0000EE]
+          RenderText {#text} at (220,0) size 305x18
+            text run at (220,0) width 305: "https://bugs.webkit.org/show_bug.cgi?id=49790"
+      RenderBlock (anonymous) at (0,34) size 800x61
+        RenderListBox {SELECT} at (2,2) size 29x57 [bgcolor=#FFFFFF] [border: (1px inset #808080)]
+        RenderText {#text} at (0,0) size 0x0