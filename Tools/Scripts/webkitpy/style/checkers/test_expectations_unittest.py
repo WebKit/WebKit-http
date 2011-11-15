WebKit/WebKit-http

commit c403b676eca4b1252ebdb1ce20802dceff8e76ce

Make the Port object have a Host https://bugs.webkit.org/show_bug.cgi?id=71961
Reviewed by Adam Barth.

This has been a long time coming.
Now that Port has a Host object, it's very easy
to write unittests which use a properly mocked Port.
Soon we will remove the executive, filesystem and user
members of Port and get everything through the Host.

Note how in many callsites we're actually removing
explicit mocks, since we're now always going to get the
correct Executive/User/FileSystem from the
Host and callsites don't need to be careful to pass each one.

This is prep-work for adding a new Environment member to
Host and needing to be able to access the Environment
member from Port functions w/o needing to pass an Environment
object (or appropriate Mock) every place we create a Port object.
Now that we have a single Host object to reach out through, its
simple to add additional functionality (like Environment) while
keeping our unittests properly mocked.

* Scripts/webkitpy/common/host.py:
* Scripts/webkitpy/common/net/credentials_unittest.py:
* Scripts/webkitpy/layout_tests/controllers/manager_unittest.py:
* Scripts/webkitpy/layout_tests/controllers/manager_worker_broker.py:
* Scripts/webkitpy/layout_tests/port/apple.py:
* Scripts/webkitpy/layout_tests/port/base.py:
* Scripts/webkitpy/layout_tests/port/base_unittest.py:
* Scripts/webkitpy/layout_tests/port/chromium.py:
* Scripts/webkitpy/layout_tests/port/chromium_gpu.py:
* Scripts/webkitpy/layout_tests/port/chromium_gpu_unittest.py:
* Scripts/webkitpy/layout_tests/port/chromium_linux.py:
* Scripts/webkitpy/layout_tests/port/chromium_linux_unittest.py:
* Scripts/webkitpy/layout_tests/port/chromium_mac.py:
* Scripts/webkitpy/layout_tests/port/chromium_mac_unittest.py:
* Scripts/webkitpy/layout_tests/port/chromium_unittest.py:
* Scripts/webkitpy/layout_tests/port/chromium_win.py:
* Scripts/webkitpy/layout_tests/port/chromium_win_unittest.py:
* Scripts/webkitpy/layout_tests/port/dryrun.py:

Tools/Scripts/webkitpy/common/host.py
@@ -60,6 +60,7 @@ class Host(object):
         # FIXME: Unfortunately Port objects are currently the central-dispatch objects of the NRWT world.
         # In order to instantiate a port correctly, we have to pass it at least an executive, user, scm, and filesystem
         # so for now we just pass along the whole Host object.
+        # FIXME: PortFactory doesn't belong on this Host object if Port is going to have a Host (circular dependency).
         self.port_factory = PortFactory(self)
 
     def _initialize_scm(self, patch_directories=None):

Tools/Scripts/webkitpy/common/net/credentials_unittest.py
@@ -37,7 +37,6 @@ from webkitpy.common.system.outputcapture import OutputCapture
 from webkitpy.thirdparty.mock import Mock
 from webkitpy.tool.mocktool import MockOptions
 from webkitpy.common.system.executive_mock import MockExecutive
-from webkitpy.common.system.user_mock import MockUser
 
 
 # FIXME: Other unit tests probably want this class.

Tools/Scripts/webkitpy/layout_tests/controllers/manager_unittest.py
@@ -47,7 +47,6 @@ from webkitpy.layout_tests.models.result_summary import ResultSummary
 from webkitpy.layout_tests.views import printing
 from webkitpy.tool.mocktool import MockOptions
 from webkitpy.common.system.executive_mock import MockExecutive
-from webkitpy.common.system.user_mock import MockUser
 from webkitpy.common.host_mock import MockHost
 
 
@@ -188,7 +187,7 @@ class ManagerTest(unittest.TestCase):
 
         options, args = run_webkit_tests.parse_args(['--platform=test', '--print=nothing', 'http/tests/passes', 'passes'])
         host = MockHost()
-        port = host.port_factory.get(port_name=options.platform, options=options, user=MockUser(), executive=MockExecutive())
+        port = host.port_factory.get(port_name=options.platform, options=options)
         run_webkit_tests._set_up_derived_options(port, options)
         printer = printing.Printer(port, options, StringIO.StringIO(), StringIO.StringIO(), configure_logging=True)
         manager = LockCheckingManager(port, options, printer)

Tools/Scripts/webkitpy/layout_tests/controllers/manager_worker_broker.py
@@ -52,7 +52,7 @@ try:
 except ImportError:
     multiprocessing = None
 
-from webkitpy.layout_tests.port.factory import PortFactory
+from webkitpy.common.host import Host  # FIXME: This should not be needed!
 from webkitpy.layout_tests.controllers import message_broker
 from webkitpy.layout_tests.views import printing
 
@@ -252,8 +252,10 @@ if multiprocessing:
 
         def run(self):
             options = self._options
-            # FIXME: This should get the PortFactory from the Host.
-            port_obj = PortFactory().get(self._platform_name, options)
+            # FIXME: This should get the Host from the owner of this object
+            # so this function can be properly mocked!
+            host = Host()
+            port_obj = host.port_factory.get(self._platform_name, options)
 
             # The unix multiprocessing implementation clones the
             # log handler configuration into the child processes,

Tools/Scripts/webkitpy/layout_tests/port/apple.py
@@ -56,9 +56,9 @@ class ApplePort(WebKitPort):
         # instead of the individual port constructors.
         return port_name[len(self.port_name + '-'):]
 
-    def __init__(self, port_name=None, os_version_string=None, **kwargs):
+    def __init__(self, host, port_name=None, os_version_string=None, **kwargs):
         port_name = port_name or self.port_name
-        WebKitPort.__init__(self, port_name=port_name, **kwargs)
+        WebKitPort.__init__(self, host, port_name=port_name, **kwargs)
 
         # FIXME: This sort of parsing belongs in factory.py!
         if port_name == '%s-wk2' % self.port_name:

Tools/Scripts/webkitpy/layout_tests/port/base.py
@@ -47,11 +47,9 @@ try:
 except ImportError:
     multiprocessing = None
 
-from webkitpy.common import system
 from webkitpy.common.system import logutils
 from webkitpy.common.system import path
-from webkitpy.common.system.executive import Executive, ScriptError
-from webkitpy.common.system.user import User
+from webkitpy.common.system.executive import ScriptError
 from webkitpy.layout_tests import read_checksum_from_png
 from webkitpy.layout_tests.models.test_configuration import TestConfiguration
 from webkitpy.layout_tests.port import config as port_config
@@ -67,7 +65,7 @@ _log = logutils.get_logger(__file__)
 class DummyOptions(object):
     """Fake implementation of optparse.Values. Cloned from webkitpy.tool.mocktool.MockOptions."""
 
-    def __init__(self, **kwargs):
+    def __init__(self, *args, **kwargs):
         # The caller can set option values using keyword arguments. We don't
         # set any values by default because we don't know how this
         # object will be used. Generally speaking unit tests should
@@ -88,7 +86,8 @@ class Port(object):
 
     ALL_BUILD_TYPES = ('debug', 'release')
 
-    def __init__(self, port_name=None, options=None,
+    def __init__(self, host,
+                 port_name=None, options=None,
                  executive=None,
                  user=None,
                  filesystem=None,
@@ -107,9 +106,12 @@ class Port(object):
         # options defined on it.
         self.options = options or DummyOptions()
 
-        self.executive = executive or Executive()
-        self.user = user or User()
-        self.filesystem = filesystem or system.filesystem.FileSystem()
+        self.host = host
+
+        # FIXME: Remove thes accessors once all callers have moved to using self.host.
+        self.executive = executive or self.host.executive
+        self.user = user or self.host.user
+        self.filesystem = filesystem or self.host.filesystem
         self.config = config or port_config.Config(self.executive, self.filesystem)
 
         # FIXME: Remove all of the old "protected" versions when we can.

Tools/Scripts/webkitpy/layout_tests/port/base_unittest.py
@@ -39,8 +39,7 @@ from webkitpy.common.system.path import abspath_to_uri
 from webkitpy.thirdparty.mock import Mock
 from webkitpy.tool.mocktool import MockOptions
 from webkitpy.common.system.executive_mock import MockExecutive
-from webkitpy.common.system.user_mock import MockUser
-
+from webkitpy.common.host_mock import MockHost
 
 from webkitpy.layout_tests.port import Port, Driver, DriverOutput
 
@@ -50,9 +49,7 @@ import config_mock
 
 class PortTest(unittest.TestCase):
     def make_port(self, *args, **kwargs):
-        kwargs.setdefault('filesystem', MockFileSystem())
-        kwargs.setdefault('user', MockUser())
-        kwargs.setdefault('executive', MockExecutive())
+        kwargs.setdefault('host', MockHost())
         return Port(*args, **kwargs)
 
     def test_format_wdiff_output_as_html(self):
@@ -284,7 +281,7 @@ class VirtualTest(unittest.TestCase):
         self.assertRaises(NotImplementedError, method, *args, **kwargs)
 
     def test_virtual_methods(self):
-        port = Port()
+        port = Port(MockHost())
         self.assertVirtual(port.baseline_path)
         self.assertVirtual(port.baseline_search_path)
         self.assertVirtual(port.check_build, None)

Tools/Scripts/webkitpy/layout_tests/port/chromium.py
@@ -88,8 +88,8 @@ class ChromiumPort(Port):
         'linux': ['lucid'],
     }
 
-    def __init__(self, **kwargs):
-        Port.__init__(self, **kwargs)
+    def __init__(self, host, **kwargs):
+        Port.__init__(self, host, **kwargs)
         # All sub-classes override this, but we need an initial value for testing.
         self._version = 'xp'
         self._chromium_base_dir = None

commit 0b6aabbbf6a8b98bca0ac6232aa396b8b421c0ec

check-webkit-style broken by r99773: "Could not determine the port" https://bugs.webkit.org/show_bug.cgi?id=72275
Reviewed by Adam Barth.

The TestExpectationsChecker was using a generic try/except block
which caught all exceptions, so we didn't notice that failing
to pass a Host to PortFactory was causing an exception in port instantiation.
I've factored out the "lookup the port" logic into a separate function
which I've now unittested.  This should fix the bug and prevent
others like it from occuring the the future.

* Scripts/webkitpy/style/checkers/test_expectations.py:
* Scripts/webkitpy/style/checkers/test_expectations_unittest.py:

git-svn-id: http://svn.webkit.org/repository/webkit/trunk@100231 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Tools/Scripts/webkitpy/style/checkers/test_expectations.py
@@ -34,7 +34,7 @@ import re
 import sys
 
 from common import TabChecker
-from webkitpy.layout_tests.port.factory import PortFactory
+from webkitpy.common.host import Host
 from webkitpy.layout_tests.models import test_expectations
 
 
@@ -52,6 +52,21 @@ class TestExpectationsChecker(object):
 
     categories = set(['test/expectations'])
 
+    def _determine_port_from_exepectations_path(self, host, expectations_path):
+        try:
+            # I believe what this is trying to do is "when the port name is chromium,
+            # get the chromium-port for this platform".  Unclear why that's needed??
+            port_name = expectations_path.split(host.filesystem.sep)[-2]
+            if port_name == "chromium":
+                return host.port_factory.get(options=ChromiumOptions())
+            # Passing port_name=None to the factory would just return the current port, which isn't what we want, I don't think.
+            if not port_name:
+                return None
+            return host.port_factory.get(port_name)
+        except Exception, e:
+            _log.warn("Exception while getting port for path %s" % expectations_path)
+            return None
+
     def __init__(self, file_path, handle_style_error):
         self._file_path = file_path
         self._handle_style_error = handle_style_error
@@ -59,25 +74,20 @@ class TestExpectationsChecker(object):
         self._tab_checker = TabChecker(file_path, handle_style_error)
         self._output_regex = re.compile('Line:(?P<line>\d+)\s*(?P<message>.+)')
 
-        # FIXME: This should get the PortFactory from a Host object!
-        port_factory = PortFactory()
+        # FIXME: A host should be passed to the constructor instead!
+        host = Host()
+        host._initialize_scm()
 
         # Determining the port of this expectations.
-        try:
-            port_name = self._file_path.split(os.sep)[-2]
-            if port_name == "chromium":
-                self._port_obj = port_factory.get(options=ChromiumOptions())
-            else:
-                self._port_obj = port_factory.get(port_name)
-        except:
-            # Using 'test' port when we couldn't determine the port for this
-            # expectations.
+        self._port_obj = self._determine_port_from_exepectations_path(host, file_path)
+        # Using 'test' port when we couldn't determine the port for this
+        # expectations.
+        if not self._port_obj:
             _log.warn("Could not determine the port for %s. "
                       "Using 'test' port, but platform-specific expectations "
                       "will fail the check." % self._file_path)
-            self._port_obj = port_factory.get('test')
-        # Suppress error messages of test_expectations module since they will be
-        # reported later.
+            self._port_obj = host.port_factory.get('test')
+        # Suppress error messages of test_expectations module since they will be reported later.
         log = logging.getLogger("webkitpy.layout_tests.layout_package.test_expectations")
         log.setLevel(logging.CRITICAL)
 

Tools/Scripts/webkitpy/style/checkers/test_expectations_unittest.py
@@ -32,7 +32,7 @@ import sys
 import unittest
 
 from test_expectations import TestExpectationsChecker
-from webkitpy.layout_tests import port
+from webkitpy.common.host_mock import MockHost
 
 
 class ErrorCollector(object):
@@ -63,8 +63,18 @@ class TestExpectationsTestCase(unittest.TestCase):
         self._error_collector = ErrorCollector()
         self._test_file = 'passes/text.html'
 
-    def process_expectations(self, expectations, overrides=None):
-        self._checker = TestExpectationsChecker()
+    def _expect_port_for_expectations_path(self, expected_port_or_port_class, expectations_path):
+        host = MockHost()
+        checker = TestExpectationsChecker(expectations_path, ErrorCollector())
+        port = checker._determine_port_from_exepectations_path(host, expectations_path)
+        if port:
+            self.assertEquals(port.__class__.__name__, expected_port_or_port_class)
+        else:
+            self.assertEquals(port, expected_port_or_port_class)
+
+    def test_determine_port_from_exepectations_path(self):
+        self._expect_port_for_expectations_path(None, "/")
+        self._expect_port_for_expectations_path("ChromiumMacPort", "/mock-checkout/LayoutTests/chromium-mac/test_expectations.txt")
 
     def assert_lines_lint(self, lines, expected):
         self._error_collector.reset_errors()