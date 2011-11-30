WebKit/WebKit-http

commit 0ee2cf0b08f2690700d6289c71f85046b6238aa2

Web Inspector: get rid of deferred backend->front-end commands processing. https://bugs.webkit.org/show_bug.cgi?id=73439
Source/WebCore:

We should issue tests upon InspectorAgent::enable and should not do any deferred command processing.

Reviewed by Yury Semikhatsky.

* inspector/InspectorAgent.cpp:
(WebCore::InspectorAgent::InspectorAgent):
(WebCore::InspectorAgent::setFrontend):
(WebCore::InspectorAgent::clearFrontend):
(WebCore::InspectorAgent::enable):
(WebCore::InspectorAgent::evaluateForTestInFrontend):
* inspector/InspectorAgent.h:
* inspector/front-end/inspector.js:

Source/WebKit/chromium:

Reviewed by Yury Semikhatsky.

* src/WebDevToolsFrontendImpl.cpp:
(WebKit::WebDevToolsFrontendImpl::WebDevToolsFrontendImpl):
(WebKit::WebDevToolsFrontendImpl::frontendLoaded):
(WebKit::WebDevToolsFrontendImpl::dispatchOnInspectorFrontend):
* src/WebDevToolsFrontendImpl.h:

git-svn-id: http://svn.webkit.org/repository/webkit/trunk@101503 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Source/WebCore/inspector/InspectorAgent.cpp
@@ -67,7 +67,6 @@ InspectorAgent::InspectorAgent(Page* page, InjectedScriptManager* injectedScript
     , m_inspectedPage(page)
     , m_frontend(0)
     , m_injectedScriptManager(injectedScriptManager)
-    , m_canIssueEvaluateForTestInFrontend(false)
     , m_didCommitLoadFired(false)
 {
     ASSERT_ARG(page, page);
@@ -97,14 +96,10 @@ void InspectorAgent::didClearWindowObjectInWorld(Frame* frame, DOMWrapperWorld* 
 void InspectorAgent::setFrontend(InspectorFrontend* inspectorFrontend)
 {
     m_frontend = inspectorFrontend;
-
-    // Dispatch pending frontend commands
-    issueEvaluateForTestCommands();
 }
 
 void InspectorAgent::clearFrontend()
 {
-    m_canIssueEvaluateForTestInFrontend = false;
     m_pendingEvaluateTestCommands.clear();
     m_frontend = 0;
     m_didCommitLoadFired = false;
@@ -133,6 +128,10 @@ void InspectorAgent::enable(ErrorString*)
         m_frontend->inspector()->didCreateWorker(worker->id(), worker->url(), worker->isSharedWorker());
     }
 #endif
+
+    for (Vector<pair<long, String> >::iterator it = m_pendingEvaluateTestCommands.begin(); m_frontend && it != m_pendingEvaluateTestCommands.end(); ++it)
+        m_frontend->inspector()->evaluateForTestInFrontend((*it).first, (*it).second);
+    m_pendingEvaluateTestCommands.clear();
 }
 
 void InspectorAgent::disable(ErrorString*)
@@ -222,9 +221,10 @@ void InspectorAgent::didDestroyWorker(intptr_t id)
 
 void InspectorAgent::evaluateForTestInFrontend(long callId, const String& script)
 {
-    m_pendingEvaluateTestCommands.append(pair<long, String>(callId, script));
-    if (m_canIssueEvaluateForTestInFrontend)
-        issueEvaluateForTestCommands();
+    if (m_state->getBoolean(InspectorAgentState::inspectorAgentEnabled))
+        m_frontend->inspector()->evaluateForTestInFrontend(callId, script);
+    else
+        m_pendingEvaluateTestCommands.append(pair<long, String>(callId, script));
 }
 
 void InspectorAgent::setInspectorExtensionAPI(const String& source)
@@ -249,17 +249,6 @@ bool InspectorAgent::developerExtrasEnabled() const
     if (!m_inspectedPage)
         return false;
     return m_inspectedPage->settings()->developerExtrasEnabled();
-}
-
-void InspectorAgent::issueEvaluateForTestCommands()
-{
-    if (m_frontend) {
-        Vector<pair<long, String> > copy = m_pendingEvaluateTestCommands;
-        m_pendingEvaluateTestCommands.clear();
-        for (Vector<pair<long, String> >::iterator it = copy.begin(); m_frontend && it != copy.end(); ++it)
-            m_frontend->inspector()->evaluateForTestInFrontend((*it).first, (*it).second);
-        m_canIssueEvaluateForTestInFrontend = true;
-    }
 }
 
 } // namespace WebCore

Source/WebCore/inspector/InspectorAgent.h
@@ -107,7 +107,6 @@ private:
 #endif
 
     bool isMainResourceLoader(DocumentLoader*, const KURL& requestUrl);
-    void issueEvaluateForTestCommands();
 
     Page* m_inspectedPage;
     InspectorFrontend* m_frontend;
@@ -119,7 +118,6 @@ private:
     typedef HashMap<intptr_t, RefPtr<InspectorWorkerResource> > WorkersMap;
     WorkersMap m_workers;
 #endif
-    bool m_canIssueEvaluateForTestInFrontend;
     bool m_didCommitLoadFired;
 };
 

Source/WebCore/inspector/front-end/inspector.js
@@ -317,6 +317,8 @@ WebInspector.loaded = function()
 
 WebInspector.doLoadedDone = function()
 {
+    InspectorFrontendHost.loaded();
+
     WebInspector.WorkerManager.loaded();
 
     WebInspector.installPortStyles();
@@ -395,7 +397,6 @@ WebInspector.doLoadedDone = function()
 
     WebInspector.CSSCompletions.requestCSSNameCompletions();
     WebInspector.WorkerManager.loadCompleted();
-    InspectorFrontendHost.loaded();
 }
 
 WebInspector.addPanel = function(panel)

Source/WebKit/chromium/src/InspectorFrontendClientImpl.cpp
@@ -77,7 +77,6 @@ void InspectorFrontendClientImpl::windowObjectCleared()
 
 void InspectorFrontendClientImpl::frontendLoaded()
 {
-    m_frontend->frontendLoaded();
 }
 
 void InspectorFrontendClientImpl::moveWindowBy(float x, float y)

Source/WebKit/chromium/src/WebDevToolsFrontendImpl.cpp
@@ -91,7 +91,6 @@ WebDevToolsFrontendImpl::WebDevToolsFrontendImpl(
     : m_webViewImpl(webViewImpl)
     , m_client(client)
     , m_applicationLocale(applicationLocale)
-    , m_loaded(false)
 {
     InspectorController* ic = m_webViewImpl->page()->inspectorController();
     ic->setInspectorFrontendClient(adoptPtr(new InspectorFrontendClientImpl(m_webViewImpl->page(), m_client, this)));
@@ -106,22 +105,6 @@ WebDevToolsFrontendImpl::~WebDevToolsFrontendImpl()
 }
 
 void WebDevToolsFrontendImpl::dispatchOnInspectorFrontend(const WebString& message)
-{
-    if (m_loaded)
-        doDispatchOnInspectorFrontend(message);
-    else
-        m_pendingMessages.append(message);
-}
-
-void WebDevToolsFrontendImpl::frontendLoaded()
-{
-    m_loaded = true;
-    for (Vector<String>::iterator it = m_pendingMessages.begin(); it != m_pendingMessages.end(); ++it)
-        doDispatchOnInspectorFrontend(*it);
-    m_pendingMessages.clear();
-}
-
-void WebDevToolsFrontendImpl::doDispatchOnInspectorFrontend(const String& message)
 {
     WebFrameImpl* frame = m_webViewImpl->mainFrameImpl();
     v8::HandleScope scope;

Source/WebKit/chromium/src/WebDevToolsFrontendImpl.h
@@ -57,16 +57,12 @@ public:
     // WebDevToolsFrontend implementation.
     virtual void dispatchOnInspectorFrontend(const WebString& message);
 
-    void frontendLoaded();
-
 private:
     void doDispatchOnInspectorFrontend(const String& message);
 
     WebKit::WebViewImpl* m_webViewImpl;
     WebKit::WebDevToolsFrontendClient* m_client;
     String m_applicationLocale;
-    bool m_loaded;
-    Vector<String> m_pendingMessages;
 };
 
 } // namespace WebKit

commit 2ba3d55e28465926a599a2de0d5e2d7c88457f4c

Web Inspector: Inspect element does not highlight element in elements panel when inspector is opened. https://bugs.webkit.org/show_bug.cgi?id=73459
Reviewed by Pavel Feldman.

This regressed in r101503.

* inspector/InjectedScriptHost.cpp:
(WebCore::InjectedScriptHost::InjectedScriptHost):
(WebCore::InjectedScriptHost::disconnect):
(WebCore::InjectedScriptHost::inspectImpl):
* inspector/InjectedScriptHost.h:
(WebCore::InjectedScriptHost::init):
* inspector/InspectorAgent.cpp:
(WebCore::InspectorAgent::enable):
(WebCore::InspectorAgent::inspect):
* inspector/InspectorAgent.h:
* inspector/InspectorController.cpp:
(WebCore::InspectorController::connectFrontend):
(WebCore::InspectorController::disconnectFrontend):
* inspector/WorkerInspectorController.cpp:
(WebCore::WorkerInspectorController::connectFrontend):
(WebCore::WorkerInspectorController::disconnectFrontend):

git-svn-id: http://svn.webkit.org/repository/webkit/trunk@101531 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Source/WebCore/inspector/InjectedScriptHost.cpp
@@ -73,7 +73,6 @@ InjectedScriptHost::InjectedScriptHost()
     , m_databaseAgent(0)
 #endif
     , m_domStorageAgent(0)
-    , m_frontend(0)
     , m_lastWorkerId(1 << 31) // Distinguish ids of fake workers from real ones, to minimize the chances they overlap.
 {
 }
@@ -90,7 +89,6 @@ void InjectedScriptHost::disconnect()
     m_databaseAgent = 0;
 #endif
     m_domStorageAgent = 0;
-    m_frontend = 0;
 }
 
 void InjectedScriptHost::addInspectedNode(Node* node)
@@ -107,8 +105,8 @@ void InjectedScriptHost::clearInspectedNodes()
 
 void InjectedScriptHost::inspectImpl(PassRefPtr<InspectorValue> object, PassRefPtr<InspectorValue> hints)
 {
-    if (m_frontend)
-        m_frontend->inspector()->inspect(object->asObject(), hints->asObject());
+    if (m_inspectorAgent)
+        m_inspectorAgent->inspect(object->asObject(), hints->asObject());
 }
 
 void InjectedScriptHost::clearConsoleMessages()

Source/WebCore/inspector/InjectedScriptHost.h
@@ -72,8 +72,6 @@ public:
 #endif
         m_domStorageAgent = domStorageAgent;
     }
-    void setFrontend(InspectorFrontend* frontend) { m_frontend = frontend; }
-    void clearFrontend() { m_frontend = 0; }
 
     static Node* scriptValueAsNode(ScriptValue);
     static ScriptValue nodeAsScriptValue(ScriptState*, Node*);
@@ -106,7 +104,6 @@ private:
     InspectorDatabaseAgent* m_databaseAgent;
 #endif
     InspectorDOMStorageAgent* m_domStorageAgent;
-    InspectorFrontend* m_frontend;
     long m_lastWorkerId;
     Vector<RefPtr<Node> > m_inspectedNodes;
 };

Source/WebCore/inspector/InspectorAgent.cpp
@@ -129,6 +129,9 @@ void InspectorAgent::enable(ErrorString*)
     }
 #endif
 
+    if (m_pendingInspectData.first)
+        inspect(m_pendingInspectData.first, m_pendingInspectData.second);
+
     for (Vector<pair<long, String> >::iterator it = m_pendingEvaluateTestCommands.begin(); m_frontend && it != m_pendingEvaluateTestCommands.end(); ++it)
         m_frontend->inspector()->evaluateForTestInFrontend((*it).first, (*it).second);
     m_pendingEvaluateTestCommands.clear();
@@ -230,6 +233,18 @@ void InspectorAgent::evaluateForTestInFrontend(long callId, const String& script
 void InspectorAgent::setInspectorExtensionAPI(const String& source)
 {
     m_inspectorExtensionAPI = source;
+}
+
+void InspectorAgent::inspect(PassRefPtr<InspectorObject> objectToInspect, PassRefPtr<InspectorObject> hints)
+{
+    if (m_state->getBoolean(InspectorAgentState::inspectorAgentEnabled) && m_frontend) {
+        m_frontend->inspector()->inspect(objectToInspect, hints);
+        m_pendingInspectData.first = 0;
+        m_pendingInspectData.second = 0;
+        return;
+    }
+    m_pendingInspectData.first = objectToInspect;
+    m_pendingInspectData.second = hints;
 }
 
 KURL InspectorAgent::inspectedURL() const

Source/WebCore/inspector/InspectorAgent.h
@@ -97,6 +97,8 @@ public:
 
     void setInspectorExtensionAPI(const String& source);
 
+    void inspect(PassRefPtr<InspectorObject> objectToInspect, PassRefPtr<InspectorObject> hints);
+
 private:
     InspectorAgent(Page*, InjectedScriptManager*, InstrumentingAgents*, InspectorState*);
 
@@ -113,6 +115,7 @@ private:
     InjectedScriptManager* m_injectedScriptManager;
 
     Vector<pair<long, String> > m_pendingEvaluateTestCommands;
+    pair<RefPtr<InspectorObject>, RefPtr<InspectorObject> > m_pendingInspectData;
     String m_inspectorExtensionAPI;
 #if ENABLE(WORKERS)
     typedef HashMap<intptr_t, RefPtr<InspectorWorkerResource> > WorkersMap;

Source/WebCore/inspector/InspectorController.cpp
@@ -192,7 +192,6 @@ void InspectorController::connectFrontend()
 {
     m_openingFrontend = false;
     m_inspectorFrontend = adoptPtr(new InspectorFrontend(m_inspectorClient));
-    m_injectedScriptManager->injectedScriptHost()->setFrontend(m_inspectorFrontend.get());
     // We can reconnect to existing front-end -> unmute state.
     m_state->unmute();
 
@@ -225,8 +224,6 @@ void InspectorController::disconnectFrontend()
 
     for (Agents::iterator it = m_agents.begin(); it != m_agents.end(); ++it)
         (*it)->clearFrontend();
-
-    m_injectedScriptManager->injectedScriptHost()->clearFrontend();
 
     m_inspectorFrontend.clear();
 

Source/WebCore/inspector/WorkerInspectorController.cpp
@@ -130,7 +130,6 @@ void WorkerInspectorController::connectFrontend()
 #endif
     m_runtimeAgent->registerInDispatcher(m_backendDispatcher.get());
 
-    m_injectedScriptManager->injectedScriptHost()->setFrontend(m_frontend.get());
 #if ENABLE(JAVASCRIPT_DEBUGGER)
     m_debuggerAgent->setFrontend(m_frontend.get());
 #endif
@@ -150,7 +149,6 @@ void WorkerInspectorController::disconnectFrontend()
     m_debuggerAgent->clearFrontend();
 #endif
     m_consoleAgent->clearFrontend();
-    m_injectedScriptManager->injectedScriptHost()->clearFrontend();
 
     m_frontend.clear();
     m_frontendChannel.clear();