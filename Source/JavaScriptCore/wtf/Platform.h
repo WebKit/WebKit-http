WebKit/WebKit-http

commit 8d7a5b111ec848a0bb49bfffa139e9990fc25e0d

[Qt][WK2][Mac] Conflict of MacTypes.h defining a Fixed type after r104560. https://bugs.webkit.org/show_bug.cgi?id=76175
Defining ENABLE_CSS_FILTERS leads to ambiguous references
due to MacTypes.h being included.
Defining CF_OPEN_SOURCE works around this problem.

Reviewed by Simon Hausmann.

* wtf/Platform.h:

git-svn-id: http://svn.webkit.org/repository/webkit/trunk@104826 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Source/JavaScriptCore/wtf/Platform.h
@@ -561,6 +561,8 @@
 #if PLATFORM(QT) && OS(DARWIN)
 #define WTF_USE_CF 1
 #define HAVE_DISPATCH_H 1
+/* FIXME: This is a hack to work around a conflict of MacTypes.h defining a Fixed type. */
+#define CF_OPEN_SOURCE 1
 #endif
 
 #if OS(DARWIN) && !PLATFORM(GTK) && !PLATFORM(QT)

commit 0f0605ef7acb22b2a04a9aba178256792a635e92

[Qt] Fix QT_VERSION related warnings when building on Mac OS X https://bugs.webkit.org/show_bug.cgi?id=76340
Source/JavaScriptCore:

This bug was caused by r104826.
As already mentioned for https://bugs.webkit.org/show_bug.cgi?id=57239
we should not use "using namespace WebCore" in header files,
because it might cause ambiguous references.
This patch reverts the changes from r104826 and r104981
and removes the "using namespace WebCore" statement from
two header files.

Reviewed by Tor Arne Vestbø.

* wtf/Platform.h:

Source/WebKit2:

This bug was caused by r104826.
As was already mentioned for https://bugs.webkit.org/show_bug.cgi?id=57239
we should not use "using namespace WebCore" in header files,
because it might cause ambiguous references.
This patch reverts the changes from r104826 and r104981
and removes the "using namespace WebCore" statement from
two header files.

Reviewed by Tor Arne Vestbø.

* Shared/WebLayerTreeInfo.h:
(WebKit::WebLayerUpdateInfo::WebLayerUpdateInfo):
(WebKit::WebLayerAnimation::WebLayerAnimation):
* UIProcess/LayerTreeHostProxy.h:
(WebKit::LayerTreeHostProxy::layerByID):
(WebKit::LayerTreeHostProxy::rootLayer):
(WebKit::LayerTreeHostProxy::notifyAnimationStarted):
(WebKit::LayerTreeHostProxy::notifySyncRequired):
* UIProcess/qt/LayerTreeHostProxyQt.cpp:
* WebProcess/WebCoreSupport/WebGraphicsLayer.h:
* WebProcess/WebPage/qt/LayerTreeHostQt.h:

git-svn-id: http://svn.webkit.org/repository/webkit/trunk@105074 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Source/JavaScriptCore/wtf/Platform.h
@@ -563,10 +563,6 @@
 #if PLATFORM(QT) && OS(DARWIN)
 #define WTF_USE_CF 1
 #define HAVE_DISPATCH_H 1
-#if QT_VERSION >= 0x050000
-/* FIXME: This is a hack to work around a conflict of MacTypes.h defining a Fixed type. */
-#define CF_OPEN_SOURCE 1
-#endif
 #endif
 
 #if OS(DARWIN) && !PLATFORM(GTK) && !PLATFORM(QT)

Source/WebKit2/Shared/WebLayerTreeInfo.h
@@ -29,8 +29,6 @@
 #include "GraphicsLayer.h"
 #include "ShareableBitmap.h"
 
-using namespace WebCore;
-
 namespace WebKit {
 
 typedef uint32_t WebLayerID;
@@ -38,12 +36,12 @@ enum { InvalidWebLayerID = 0 };
 
 struct WebLayerUpdateInfo {
     WebLayerUpdateInfo() { }
-    WebLayerUpdateInfo(const IntRect& r)
+    WebLayerUpdateInfo(const WebCore::IntRect& r)
         : layerID(InvalidWebLayerID)
         , rect(r) { }
 
     WebLayerID layerID;
-    IntRect rect;
+    WebCore::IntRect rect;
     ShareableBitmap::Handle bitmapHandle;
 
     void encode(CoreIPC::ArgumentEncoder*) const;
@@ -53,9 +51,9 @@ struct WebLayerUpdateInfo {
 struct WebLayerAnimation {
     WebLayerAnimation()
         : operation(InvalidAnimation)
-        , keyframeList(AnimatedPropertyInvalid)
+        , keyframeList(WebCore::AnimatedPropertyInvalid)
         , startTime(0) { }
-    WebLayerAnimation(const KeyframeValueList& valueList) 
+    WebLayerAnimation(const WebCore::KeyframeValueList& valueList)
         : operation(InvalidAnimation)
         , keyframeList(valueList)
         , startTime(0) { }
@@ -67,9 +65,9 @@ struct WebLayerAnimation {
         PauseAnimation,
         InvalidAnimation
     } operation;
-    IntSize boxSize;
-    RefPtr<Animation> animation;
-    KeyframeValueList keyframeList;
+    WebCore::IntSize boxSize;
+    RefPtr<WebCore::Animation> animation;
+    WebCore::KeyframeValueList keyframeList;
     double startTime;
 
     void encode(CoreIPC::ArgumentEncoder*) const;
@@ -93,12 +91,12 @@ struct WebLayerInfo {
     WebLayerID mask;
     int64_t imageBackingStoreID;
 
-    FloatPoint pos;
-    FloatPoint3D anchorPoint;
-    FloatSize size;
-    TransformationMatrix transform;
-    TransformationMatrix childrenTransform;
-    IntRect contentsRect;
+    WebCore::FloatPoint pos;
+    WebCore::FloatPoint3D anchorPoint;
+    WebCore::FloatSize size;
+    WebCore::TransformationMatrix transform;
+    WebCore::TransformationMatrix childrenTransform;
+    WebCore::IntRect contentsRect;
     float opacity;
 
     union {

Source/WebKit2/UIProcess/LayerTreeHostProxy.h
@@ -42,11 +42,9 @@ namespace WebKit {
 class WebLayerInfo;
 class WebLayerUpdateInfo;
 
-using namespace WebCore;
-
 class LayerTreeMessageToRenderer;
 
-class LayerTreeHostProxy : public GraphicsLayerClient {
+class LayerTreeHostProxy : public WebCore::GraphicsLayerClient {
 public:
     LayerTreeHostProxy(DrawingAreaProxy*);
     virtual ~LayerTreeHostProxy();
@@ -54,10 +52,10 @@ public:
     void deleteCompositingLayer(WebLayerID);
     void setRootCompositingLayer(WebLayerID);
     void didReceiveMessage(CoreIPC::Connection*, CoreIPC::MessageID, CoreIPC::ArgumentDecoder*);
-    void paintToCurrentGLContext(const TransformationMatrix&, float);
+    void paintToCurrentGLContext(const WebCore::TransformationMatrix&, float);
     void purgeGLResources();
-    void setVisibleContentsRectAndScale(const IntRect&, float);
-    void setVisibleContentRectTrajectoryVector(const FloatPoint&);
+    void setVisibleContentsRectAndScale(const WebCore::IntRect&, float);
+    void setVisibleContentRectTrajectoryVector(const WebCore::FloatPoint&);
 #if USE(TILED_BACKING_STORE)
     void syncRemoteContent();
     void swapContentBuffers();
@@ -68,19 +66,19 @@ public:
     void createDirectlyCompositedImage(int64_t, const WebKit::ShareableBitmap::Handle&);
     void destroyDirectlyCompositedImage(int64_t);
     void didReceiveLayerTreeHostProxyMessage(CoreIPC::Connection*, CoreIPC::MessageID, CoreIPC::ArgumentDecoder*);
-    void setVisibleContentsRectForLayer(WebLayerID, const IntRect&);
+    void setVisibleContentsRectForLayer(WebLayerID, const WebCore::IntRect&);
     void updateViewport();
 #endif
 
 protected:
-    PassOwnPtr<GraphicsLayer> createLayer(WebLayerID);
+    PassOwnPtr<WebCore::GraphicsLayer> createLayer(WebLayerID);
 
-    GraphicsLayer* layerByID(WebLayerID id) { return (id == InvalidWebLayerID) ? 0 : m_layers.get(id); }
-    GraphicsLayer* rootLayer() { return m_rootLayer.get(); }
+    WebCore::GraphicsLayer* layerByID(WebLayerID id) { return (id == InvalidWebLayerID) ? 0 : m_layers.get(id); }
+    WebCore::GraphicsLayer* rootLayer() { return m_rootLayer.get(); }
 
     // Reimplementations from WebCore::GraphicsLayerClient.
-    virtual void notifyAnimationStarted(const GraphicsLayer*, double) { }
-    virtual void notifySyncRequired(const GraphicsLayer*) { }
+    virtual void notifyAnimationStarted(const WebCore::GraphicsLayer*, double) { }
+    virtual void notifySyncRequired(const WebCore::GraphicsLayer*) { }
     virtual bool showDebugBorders() const { return false; }
     virtual bool showRepaintCounter() const { return false; }
     void paintContents(const WebCore::GraphicsLayer*, WebCore::GraphicsContext&, WebCore::GraphicsLayerPaintingPhase, const WebCore::IntRect&) { }
@@ -89,19 +87,19 @@ protected:
     DrawingAreaProxy* m_drawingAreaProxy;
 
     typedef HashMap<WebLayerID, WebCore::GraphicsLayer*> LayerMap;
-    IntRect m_visibleContentsRect;
+    WebCore::IntRect m_visibleContentsRect;
     float m_contentsScale;
 
     MessageQueue<LayerTreeMessageToRenderer> m_messagesToRenderer;
     void pushUpdateToQueue(PassOwnPtr<LayerTreeMessageToRenderer>);
 
 #if USE(TEXTURE_MAPPER)
-    OwnPtr<TextureMapper> m_textureMapper;
+    OwnPtr<WebCore::TextureMapper> m_textureMapper;
 #endif
 
 #if PLATFORM(QT)
-    typedef HashMap<IntPoint, RefPtr<BitmapTexture> > TiledImage;
-    TextureMapperNode::NodeRectMap m_nodeVisualContentsRectMap;
+    typedef HashMap<WebCore::IntPoint, RefPtr<WebCore::BitmapTexture> > TiledImage;
+    WebCore::TextureMapperNode::NodeRectMap m_nodeVisualContentsRectMap;
     HashMap<int, int> m_tileToNodeTile;
     int remoteTileIDToNodeTileID(int tileID) const;
     HashMap<int64_t, TiledImage> m_directlyCompositedImages;
@@ -113,7 +111,7 @@ protected:
     void syncLayerParameters(const WebLayerInfo&);
     void createTile(WebLayerID, int, float scale);
     void removeTile(WebLayerID, int);
-    void updateTile(WebLayerID, int, const IntRect&, const IntRect&, const QImage&);
+    void updateTile(WebLayerID, int, const WebCore::IntRect&, const WebCore::IntRect&, const QImage&);
     void createImage(int64_t, const QImage&);
     void destroyImage(int64_t);
     void assignImageToLayer(WebCore::GraphicsLayer*, int64_t imageID);
@@ -123,12 +121,12 @@ protected:
 
 #endif
 
-    OwnPtr<GraphicsLayer> m_rootLayer;
+    OwnPtr<WebCore::GraphicsLayer> m_rootLayer;
     Vector<WebLayerID> m_layersToDelete;
 
 #if PLATFORM(QT)
-    void didFireViewportUpdateTimer(Timer<LayerTreeHostProxy>*);
-    Timer<LayerTreeHostProxy> m_viewportUpdateTimer;
+    void didFireViewportUpdateTimer(WebCore::Timer<LayerTreeHostProxy>*);
+    WebCore::Timer<LayerTreeHostProxy> m_viewportUpdateTimer;
 #endif
 
     LayerMap m_layers;

Source/WebKit2/UIProcess/qt/LayerTreeHostProxyQt.cpp
@@ -57,6 +57,8 @@ public:
     virtual Type type() const = 0;
 };
 
+using namespace WebCore;
+
 template<class MessageData, LayerTreeMessageToRenderer::Type messageType>
 class LayerTreeMessageToRendererWithData : public LayerTreeMessageToRenderer {
 public:

Source/WebKit2/WebProcess/WebCoreSupport/WebGraphicsLayer.h
@@ -46,7 +46,7 @@ public:
     virtual void updateTile(WebLayerID, int tileID, const UpdateInfo&) = 0;
     virtual void removeTile(WebLayerID, int tileID) = 0;
     virtual bool layerTreeTileUpdatesAllowed() const = 0;
-    virtual int64_t adoptImageBackingStore(Image*) = 0;
+    virtual int64_t adoptImageBackingStore(WebCore::Image*) = 0;
     virtual void releaseImageBackingStore(int64_t) = 0;
     virtual void didSyncCompositingStateForLayer(const WebLayerInfo&) = 0;
     virtual void didDeleteLayer(WebLayerID) = 0;

Source/WebKit2/WebProcess/WebPage/qt/LayerTreeHostQt.h
@@ -62,7 +62,7 @@ public:
     virtual void pauseRendering() { m_isSuspended = true; }
     virtual void resumeRendering() { m_isSuspended = false; scheduleLayerFlush(); }
     virtual void deviceScaleFactorDidChange() { }
-    virtual int64_t adoptImageBackingStore(Image*);
+    virtual int64_t adoptImageBackingStore(WebCore::Image*);
     virtual void releaseImageBackingStore(int64_t);
 
 #if USE(TILED_BACKING_STORE)
@@ -73,8 +73,8 @@ public:
     virtual void renderNextFrame();
     virtual void purgeBackingStores();
     virtual bool layerTreeTileUpdatesAllowed() const;
-    virtual void setVisibleContentRectAndScale(const IntRect&, float scale);
-    virtual void setVisibleContentRectTrajectoryVector(const FloatPoint&);
+    virtual void setVisibleContentRectAndScale(const WebCore::IntRect&, float scale);
+    virtual void setVisibleContentRectTrajectoryVector(const WebCore::FloatPoint&);
     virtual void didSyncCompositingStateForLayer(const WebLayerInfo&);
     virtual void didDeleteLayer(WebLayerID);
 #endif