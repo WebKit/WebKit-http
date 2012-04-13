WebKit/WebKit-http

commit 358be96d6c8d61e07dfd1c07a7397acd4cea89ec

Source/WebCore: The main code structure for placing future text track cue rendering code and main outer rendering steps. https://bugs.webkit.org/show_bug.cgi?id=79746
Patch by Victor Carbune <vcarbune@adobe.com> on 2012-03-11
Reviewed by Eric Carlson.

No new tests. Only refactoring, but some chromium tests require rebaselining
and have been marked accordingly.

* css/mediaControls.css:
(::-webkit-media-controls): Changed the default display to -webkit-box, as
captions need to always be rendered on top of the controls, if they are visible.
(audio::-webkit-media-controls-panel, video::-webkit-media-controls-panel):
Default position attribute is now relative, handled by the parent -webkit-box
(video::-webkit-media-text-track-container): The position of the container is
now relative, handled by -webkit-box.
(video::-webkit-media-text-track-display): Adjusted text color to match
the color required in the WebVTT spec (section 3.5.1 'color' property)
* css/mediaControlsChromium.css:
(audio::-webkit-media-controls-panel, video::-webkit-media-controls-panel):
Default position attribute is now relative, handled by the parent -webkit-box

* html/shadow/MediaControlElements.cpp:
(WebCore::MediaControlPanelElement::MediaControlPanelElement): Added a timer
for the webkit fade out transition event. This timer is required for setting
the display property to 'none', when the fade out transition is over. Otherwise,
captions would not be displayed at the bottom of the video.
(WebCore::MediaControlPanelElement::startTimer): Added.
(WebCore):
(WebCore::MediaControlPanelElement::stopTimer): Added.
(WebCore::MediaControlPanelElement::transitionTimerFired): Added. If the current
state of the controls is transparent, the display property is set to 'none'.
(WebCore::MediaControlPanelElement::makeOpaque): The inline display:'none' property
is removed before the fade in transition.
(WebCore::MediaControlPanelElement::makeTransparent): Added the timer start.

Source/WebCore/html/shadow/MediaControlElements.cpp
@@ -40,6 +40,7 @@
 #include "Frame.h"
 #include "HTMLMediaElement.h"
 #include "HTMLNames.h"
+#include "HTMLVideoElement.h"
 #include "LocalizedStrings.h"
 #include "MediaControls.h"
 #include "MouseEvent.h"
@@ -52,6 +53,7 @@
 #include "RenderView.h"
 #include "ScriptController.h"
 #include "Settings.h"
+#include "Text.h"
 
 namespace WebCore {
 
@@ -107,6 +109,7 @@ inline MediaControlPanelElement::MediaControlPanelElement(Document* document)
     , m_canBeDragged(false)
     , m_isBeingDragged(false)
     , m_opaque(true)
+    , m_transitionTimer(this, &MediaControlPanelElement::transitionTimerFired)
 {
 }
 
@@ -173,6 +176,32 @@ void MediaControlPanelElement::endDrag()
     frame->eventHandler()->setCapturingMouseEventsNode(0);
 }
 
+void MediaControlPanelElement::startTimer()
+{
+    stopTimer();
+
+    // The timer is required to set the property display:'none' on the panel,
+    // such that captions are correctly displayed at the bottom of the video
+    // at the end of the fadeout transition.
+    double duration = document()->page() ? document()->page()->theme()->mediaControlsFadeOutDuration() : 0;
+    m_transitionTimer.startOneShot(duration);
+}
+
+void MediaControlPanelElement::stopTimer()
+{
+    if (m_transitionTimer.isActive())
+        m_transitionTimer.stop();
+}
+
+
+void MediaControlPanelElement::transitionTimerFired(Timer<MediaControlPanelElement>*)
+{
+    if (!m_opaque)
+        hide();
+
+    stopTimer();
+}
+
 void MediaControlPanelElement::setPosition(const LayoutPoint& position)
 {
     double left = position.x();
@@ -212,6 +241,8 @@ void MediaControlPanelElement::makeOpaque()
     setInlineStyleProperty(CSSPropertyOpacity, 1.0, CSSPrimitiveValue::CSS_NUMBER);
 
     m_opaque = true;
+
+    show();
 }
 
 void MediaControlPanelElement::makeTransparent()
@@ -224,6 +255,8 @@ void MediaControlPanelElement::makeTransparent()
     setInlineStyleProperty(CSSPropertyOpacity, 0.0, CSSPrimitiveValue::CSS_NUMBER);
 
     m_opaque = false;
+
+    startTimer();
 }
 
 void MediaControlPanelElement::defaultEventHandler(Event* event)
@@ -1165,7 +1198,6 @@ void RenderTextTrackContainerElement::layout()
 inline MediaControlTextTrackContainerElement::MediaControlTextTrackContainerElement(Document* document)
     : MediaControlElement(document)
     , m_fontSize(0)
-    , m_bottom(0)
 {
 }
 
@@ -1187,16 +1219,89 @@ const AtomicString& MediaControlTextTrackContainerElement::shadowPseudoId() cons
     return id;
 }
 
+void MediaControlTextTrackContainerElement::updateDisplay()
+{
+    HTMLMediaElement* mediaElement = toParentMediaElement(this);
+
+    // 1. If the media element is an audio element, or is another playback
+    // mechanism with no rendering area, abort these steps. There is nothing to
+    // render.
+    if (!mediaElement->isVideo())
+        return;
+
+    // 2. Let video be the media element or other playback mechanism.
+    HTMLVideoElement* video = static_cast<HTMLVideoElement*>(mediaElement);
+
+    // 3. Let output be an empty list of absolutely positioned CSS block boxes.
+    Vector<RefPtr<HTMLDivElement> > output;
+
+    // 4. If the user agent is exposing a user interface for video, add to
+    // output one or more completely transparent positioned CSS block boxes that
+    // cover the same region as the user interface.
+
+    // 5. If the last time these rules were run, the user agent was not exposing
+    // a user interface for video, but now it is, let reset be true. Otherwise,
+    // let reset be false.
+
+    // There is nothing to be done explicitly for 4th and 5th steps, as
+    // everything is handled through CSS. The caption box is on top of the
+    // controls box, in a container set with the -webkit-box display property.
+
+    // 6. Let tracks be the subset of video's list of text tracks that have as
+    // their rules for updating the text track rendering these rules for
+    // updating the display of WebVTT text tracks, and whose text track mode is
+    // showing or showing by default.
+    // 7. Let cues be an empty list of text track cues.
+    // 8. For each track track in tracks, append to cues all the cues from
+    // track's list of cues that have their text track cue active flag set.
+    CueList activeCues = video->currentlyActiveCues();
+
+    // 9. If reset is false, then, for each text track cue cue in cues: if cue's
+    // text track cue display state has a set of CSS boxes, then add those boxes
+    // to output, and remove cue from cues.
+
+    // There is nothing explicitly to be done here, as all the caching occurs
+    // within the TextTrackCue instance itself. If parameters of the cue change,
+    // the display tree is cleared.
+
+    // 10. For each text track cue cue in cues that has not yet had
+    // corresponding CSS boxes added to output, in text track cue order, run the
+    // following substeps:
+
+    // Simple renderer for now.
+    for (size_t i = 0; i < activeCues.size(); ++i) {
+        TextTrackCue* cue = activeCues[i].data();
+
+        ASSERT(cue->isActive());
+        if (!cue->track() || cue->track()->mode() != TextTrack::SHOWING)
+            continue;
+
+        RefPtr<HTMLDivElement> displayTree = cue->getDisplayTree();
+
+        // Append only new display trees.
+        if (displayTree->hasChildNodes() && !contains(displayTree.get()))
+            appendChild(displayTree, ASSERT_NO_EXCEPTION, true);
+
+        // The display tree of a cue is removed when the active flag of the cue is unset.
+
+        // FIXME(BUG 79750): Render the TextTrackCue when snap-to-lines is set.
+        // FIXME(BUG 79751): Render the TextTrackCue when snap-to-lines is not set.
+    }
+
+    // 11. Return output.
+    hasChildNodes() ? show() : hide();
+}
+
 static const float mimimumFontSize = 16;
 static const float videoHeightFontSizePercentage = .05;
 static const float trackBottomMultiplier = .9;
-    
+
 void MediaControlTextTrackContainerElement::updateSizes()
 {
     HTMLMediaElement* mediaElement = toParentMediaElement(this);
     if (!mediaElement || !mediaElement->renderer() || !mediaElement->renderer()->isVideo())
         return;
-    
+
     IntRect videoBox = toRenderVideo(mediaElement->renderer())->videoBox();
     if (m_videoDisplaySize == videoBox)
         return;
@@ -1207,33 +1312,9 @@ void MediaControlTextTrackContainerElement::updateSizes()
         m_fontSize = fontSize;
         setInlineStyleProperty(CSSPropertyFontSize, String::number(fontSize) + "px");
     }
-
-    LayoutUnit bottom = static_cast<LayoutUnit>(m_videoDisplaySize.y() + m_videoDisplaySize.height() - (m_videoDisplaySize.height() * trackBottomMultiplier));
-    if (bottom != m_bottom) {
-        m_bottom = bottom;
-        setInlineStyleProperty(CSSPropertyBottom, String::number(roundToInt(bottom)) + "px");
-    }
 }
 
-// ----------------------------
-
-MediaControlTextTrackDisplayElement::MediaControlTextTrackDisplayElement(Document* document)
-    : MediaControlElement(document)
-{
-}
-
-PassRefPtr<MediaControlTextTrackDisplayElement> MediaControlTextTrackDisplayElement::create(Document* document)
-{
-    return adoptRef(new MediaControlTextTrackDisplayElement(document));
-}
-
-const AtomicString& MediaControlTextTrackDisplayElement::shadowPseudoId() const
-{
-    DEFINE_STATIC_LOCAL(AtomicString, id, ("-webkit-media-text-track-display"));
-    return id;
-}
-
-#endif
+#endif // ENABLE(VIDEO_TRACK)
 
 // ----------------------------
 

Source/WebCore/html/shadow/MediaControlElements.h
@@ -122,6 +122,10 @@ private:
     void continueDrag(const LayoutPoint& eventLocation);
     void endDrag();
 
+    void startTimer();
+    void stopTimer();
+    void transitionTimerFired(Timer<MediaControlPanelElement>*);
+
     void setPosition(const LayoutPoint&);
 
     bool m_canBeDragged;
@@ -129,6 +133,8 @@ private:
     bool m_opaque;
     LayoutPoint m_dragStartPosition;
     LayoutPoint m_dragStartEventLocation;
+
+    Timer<MediaControlPanelElement> m_transitionTimer;
 };
 
 // ----------------------------
@@ -491,9 +497,9 @@ private:
 #if ENABLE(VIDEO_TRACK)
 class MediaControlTextTrackContainerElement : public MediaControlElement {
 public:
-    
     static PassRefPtr<MediaControlTextTrackContainerElement> create(Document*);
-    
+
+    void updateDisplay();
     void updateSizes();
 
 private:
@@ -505,23 +511,9 @@ private:
 
     IntRect m_videoDisplaySize;
     float m_fontSize;
-    LayoutUnit m_bottom;
 };
 
-// ----------------------------
-
-class MediaControlTextTrackDisplayElement : public MediaControlElement {
-public:
-    static PassRefPtr<MediaControlTextTrackDisplayElement> create(Document*);
-
-private:
-    MediaControlTextTrackDisplayElement(Document*);
-
-    virtual MediaControlElementType displayType() const { return MediaTextTrackDisplay; }
-    virtual const AtomicString& shadowPseudoId() const;
-};
 #endif
-
 // ----------------------------
 
 } // namespace WebCore

LayoutTests/media/media-controls.js
@@ -2,7 +2,6 @@
 function mediaControlsElement(first, id)
 {
     for (var element = first; element; element = element.nextSibling) {
-
         // Not every element in the media controls has a shadow pseudo ID, eg. the
         // text nodes for the time values, so guard against exceptions.
         try {
@@ -35,9 +34,20 @@ function mediaControlsButtonCoordinates(element, id)
 
 function textTrackDisplayElement(parentElement, id)
 {
-    var controlID = "-webkit-media-text-track-" + id;
-    var displayElement = mediaControlsElement(internals.shadowRoot(parentElement).firstChild, controlID);
+    var textTrackContainerID = "-webkit-media-text-track-container";
+    var containerElement = mediaControlsElement(internals.shadowRoot(parentElement).firstChild, "-webkit-media-text-track-container");
+
+    if (!containerElement)
+        throw "Failed to find text track container element";
+
+    if (!id)
+        return containerElement;
+
+    var controlID = "-webkit-media-text-track-" + arguments[1];
+
+    var displayElement = mediaControlsElement(containerElement.firstChild, controlID);
     if (!displayElement)
-        throw "Failed to find media control element ID '" + controlID + "'";
+        throw "No text track cue with display id '" + controlID + "' is currently visible";
+
     return displayElement;
 }

LayoutTests/media/track/track-cue-mutable-text-expected.txt
@@ -3,7 +3,7 @@ Test that cue text is mutable.
 
 ** Test initial cue info
 EXPECTED (testTrack.track.activeCues.length == '0') OK
-EXPECTED (textTrackDisplayElement(video, 'display').innerText == '') OK
+No text track cue with display id '-webkit-media-text-track-display' is currently visible
 EXPECTED (testTrack.track.cues[0].startTime == '1') OK
 EXPECTED (testTrack.track.cues[1].startTime == '3') OK
 EXPECTED (testTrack.track.cues[0].text == 'Lorem ipsum dolor sit amet,') OK

LayoutTests/media/track/track-cue-nothing-to-render-expected.txt
@@ -3,7 +3,7 @@ Test that cues are rendered and removed.
 EVENT(canplaythrough)
 EVENT(seeked)
 EXPECTED (video.currentTime.toFixed(1) == '0.5') OK
-EXPECTED (textTrackDisplayElement(video, 'display').innerText == '') OK
+No text track cue with display id '-webkit-media-text-track-display' is currently visible
 
 RUN(video.currentTime = 1.5)
 EVENT(seeked)
@@ -14,7 +14,7 @@ EXPECTED (textTrackDisplayElement(video, 'display').innerText == 'Lorem ipsum do
 RUN(video.currentTime = 2.5)
 EVENT(seeked)
 EXPECTED (video.currentTime.toFixed(1) == '2.5') OK
-EXPECTED (textTrackDisplayElement(video, 'display').innerText == '') OK
+No text track cue with display id '-webkit-media-text-track-display' is currently visible
 
 RUN(video.currentTime = 3.3)
 EVENT(seeked)
@@ -25,7 +25,7 @@ EXPECTED (textTrackDisplayElement(video, 'display').innerText == 'consectetuer a
 RUN(video.currentTime = 0.6)
 EVENT(seeked)
 EXPECTED (video.currentTime.toFixed(1) == '0.6') OK
-EXPECTED (textTrackDisplayElement(video, 'display').innerText == '') OK
+No text track cue with display id '-webkit-media-text-track-display' is currently visible
 
 RUN(video.currentTime = 5.9)
 EVENT(seeked)
@@ -36,7 +36,7 @@ EXPECTED (textTrackDisplayElement(video, 'display').innerText == 'sed diam nonum
 RUN(video.currentTime = 4.4)
 EVENT(seeked)
 EXPECTED (video.currentTime.toFixed(1) == '4.4') OK
-EXPECTED (textTrackDisplayElement(video, 'display').innerText == '') OK
+No text track cue with display id '-webkit-media-text-track-display' is currently visible
 
 RUN(video.currentTime = 7.9)
 EVENT(seeked)

LayoutTests/media/track/track-cue-rendering-expected.txt
@@ -30,22 +30,22 @@ EXPECTED (textTrackDisplayElement(video, 'display').innerText == 'sit') OK
 Test the cue display colors and font.
 RUN(video.width = 320)
 RUN(video.height = 240)
-EXPECTED (getComputedStyle(textTrackDisplayElement(video, 'container')).fontSize == '12px') OK
+EXPECTED (getComputedStyle(textTrackDisplayElement(video)).fontSize == '12px') OK
 
 RUN(video.width = 640)
 RUN(video.height = 480)
-EXPECTED (getComputedStyle(textTrackDisplayElement(video, 'container')).fontSize == '24px') OK
+EXPECTED (getComputedStyle(textTrackDisplayElement(video)).fontSize == '24px') OK
 
 RUN(video.width = 1280)
 RUN(video.height = 960)
-EXPECTED (getComputedStyle(textTrackDisplayElement(video, 'container')).fontSize == '48px') OK
+EXPECTED (getComputedStyle(textTrackDisplayElement(video)).fontSize == '48px') OK
 
 RUN(video.width = 2560)
 RUN(video.height = 1440)
-EXPECTED (getComputedStyle(textTrackDisplayElement(video, 'container')).fontSize == '72px') OK
+EXPECTED (getComputedStyle(textTrackDisplayElement(video)).fontSize == '72px') OK
 
-EXPECTED (getComputedStyle(textTrackDisplayElement(video, 'container')).fontFamily == 'sans-serif') OK
-EXPECTED (getComputedStyle(textTrackDisplayElement(video, 'container')).color == 'rgba(255, 255, 255, 0)') OK
+EXPECTED (getComputedStyle(textTrackDisplayElement(video)).fontFamily == 'sans-serif') OK
+EXPECTED (getComputedStyle(textTrackDisplayElement(video)).color == 'rgba(255, 255, 255, 0)') OK
 EXPECTED (getComputedStyle(textTrackDisplayElement(video, 'display')).backgroundColor == 'rgba(0, 0, 0, 0.796875)') OK
 END OF TEST
 

LayoutTests/media/track/track-cue-rendering.html
@@ -18,7 +18,7 @@
             run("video.width = " + width);
             run("video.height = " + height);
             document.body.offsetTop;
-            testExpected("getComputedStyle(textTrackDisplayElement(video, 'container')).fontSize", parseInt(height * 0.05) + "px");
+            testExpected("getComputedStyle(textTrackDisplayElement(video)).fontSize", parseInt(height * 0.05) + "px");
             consoleWrite("");
         }
 
@@ -30,8 +30,8 @@
             testFontSize(1280, 960);
             testFontSize(2560, 1440);
 
-            testExpected("getComputedStyle(textTrackDisplayElement(video, 'container')).fontFamily", "sans-serif");
-            testExpected("getComputedStyle(textTrackDisplayElement(video, 'container')).color", "rgba(255, 255, 255, 0)");
+            testExpected("getComputedStyle(textTrackDisplayElement(video)).fontFamily", "sans-serif");
+            testExpected("getComputedStyle(textTrackDisplayElement(video)).color", "rgba(255, 255, 255, 0)");
             testExpected("getComputedStyle(textTrackDisplayElement(video, 'display')).backgroundColor", "rgba(0, 0, 0, 0.796875)");
 
             endTest();

Source/WebCore/css/mediaControls.css
@@ -37,8 +37,11 @@ audio {
     width: inherit;
     height: inherit;
     position: relative;
-    display: block;
+    display: -webkit-box;
     direction: ltr;
+    -webkit-box-align: start;
+    -webkit-box-pack: end;
+    -webkit-box-orient: vertical;
 }
 
 audio::-webkit-media-controls-panel, video::-webkit-media-controls-panel {
@@ -46,7 +49,7 @@ audio::-webkit-media-controls-panel, video::-webkit-media-controls-panel {
     -webkit-box-orient: horizontal;
     -webkit-box-align: center;
     -webkit-user-select: none;
-    position: absolute;
+    position: relative;
     bottom: 0;
     width: 100%;
     z-index: 0;
@@ -201,15 +204,16 @@ audio::-webkit-media-controls-fullscreen-volume-max-button, video::-webkit-media
 }
 
 video::-webkit-media-text-track-container {
-    position: absolute;
+    position: relative;
     width: 100%;
     overflow: hidden;
+    padding-bottom: 5px;
 
     font-size: 22px;
     font-family: sans-serif;
     text-align: center;
     color: rgba(255, 255, 255, 0);
-    
+
     letter-spacing: normal;
     word-spacing: normal;
     text-transform: none;
@@ -222,6 +226,6 @@ video::-webkit-media-text-track-container {
 video::-webkit-media-text-track-display {
     display: inline;
     background-color: rgba(0, 0, 0, 0.8);
-    color: yellow;
+    color: rgba(255, 255, 255, 1);
     padding: 0px 2px;
 }

commit 15bf286222b95d733c717f93f4bc0bada94e784d

fullscreen/video-controls-drag.html failing on Mac https://bugs.webkit.org/show_bug.cgi?id=81176
Reviewed by Eric Carlson.

Source/WebCore:

No new tests.  Fixes fullscreen/video-controls-drag.html.

Regressed in r110409.  When the -webkit-media-controls-panel were changed
from position:absolute to position:relative, this broke dragging of the
controls in fullscreen, which relies on the panel being absolutely positioned
to do its drag positioning. Change the way drag positioning is calculated to
accommodate position:relative.

* css/fullscreenQuickTime.css:
(video:-webkit-full-screen::-webkit-media-controls-panel): Instead of bottom:50px,
    use margin-bottom:50px to push panel up from the bottom of the screen.
* html/shadow/MediaControlElements.cpp:
(WebCore::MediaControlPanelElement::startDrag): Do not ask for the current renderer location.
(WebCore::MediaControlPanelElement::continueDrag): Use the delta between the event locations
    only to calculate the new panel position.
* html/shadow/MediaControlElements.h:

LayoutTests:

Removed expected failing results from platform/mac.

* platform/mac/fullscreen/video-controls-drag-expected.txt: Removed.

git-svn-id: http://svn.webkit.org/repository/webkit/trunk@114179 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Source/WebCore/css/fullscreenQuickTime.css
@@ -32,6 +32,7 @@ video:-webkit-full-screen::-webkit-media-controls-panel {
     padding: 12px 0 0 10px !important;
     width: 430px !important;
     height: 48px !important;
+    margin: 0 auto 50px auto !important;
     
     background-image: -webkit-gradient(
         linear,
@@ -53,12 +54,6 @@ video:-webkit-full-screen::-webkit-media-controls-panel {
     -webkit-border-radius: 8px !important;
 
     -webkit-transition: opacity 0.3s linear !important;
-}
-
-video:-webkit-full-screen::-webkit-media-controls-panel:not(.dragged) {
-    bottom: 50px !important;
-    left: 50% !important;
-    margin-left: -220px !important;
 }
 
 video:-webkit-animating-full-screen-transition::-webkit-media-controls-panel {

Source/WebCore/html/shadow/MediaControlElements.cpp
@@ -145,7 +145,6 @@ void MediaControlPanelElement::startDrag(const LayoutPoint& eventLocation)
     if (!frame)
         return;
 
-    m_dragStartPosition = toRenderBox(renderer)->location();
     m_dragStartEventLocation = eventLocation;
 
     frame->eventHandler()->setCapturingMouseEventsNode(this);
@@ -159,7 +158,7 @@ void MediaControlPanelElement::continueDrag(const LayoutPoint& eventLocation)
         return;
 
     LayoutSize distanceDragged = eventLocation - m_dragStartEventLocation;
-    setPosition(m_dragStartPosition + distanceDragged);
+    setPosition(LayoutPoint(distanceDragged.width(), distanceDragged.height()));
 }
 
 void MediaControlPanelElement::endDrag()

Source/WebCore/html/shadow/MediaControlElements.h
@@ -132,7 +132,6 @@ private:
     bool m_canBeDragged;
     bool m_isBeingDragged;
     bool m_opaque;
-    LayoutPoint m_dragStartPosition;
     LayoutPoint m_dragStartEventLocation;
 
     Timer<MediaControlPanelElement> m_transitionTimer;

LayoutTests/platform/mac/fullscreen/video-controls-drag-expected.txt
@@ -1,7 +1,0 @@
-This tests that the video element's controls are draggable in full screen. Press any key to continue.
-
-EVENT(webkitfullscreenchange)
-EXPECTED (endBox.left - startBox.left == '15'), OBSERVED '-385' FAIL
-EXPECTED (endBox.top - startBox.top == '15'), OBSERVED '605' FAIL
-END OF TEST
-