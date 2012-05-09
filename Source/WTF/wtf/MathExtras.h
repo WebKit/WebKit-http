e6b81309723577a34599e9e9e20a42580be4d615
Alexandre Deckner <alexandre.deckner@uzzl.com>
2012-05-09 13:44:59 +0200

    Port our changes to MathExtras.h from JavaScriptCore/wtf/MathExtras.h

--- Source/WTF/wtf/MathExtras.h
+++ Source/WTF/wtf/MathExtras.h
@@ -277,7 +277,7 @@ inline bool isWithinIntRange(float x)
     return x > static_cast<float>(std::numeric_limits<int>::min()) && x < static_cast<float>(std::numeric_limits<int>::max());
 }
 
-#if !COMPILER(MSVC) && !COMPILER(RVCT) && !OS(SOLARIS)
+#if !COMPILER(MSVC) && !COMPILER(RVCT) && !OS(SOLARIS) && !OS(HAIKU)
 using std::isfinite;
 using std::isinf;
 using std::isnan;
