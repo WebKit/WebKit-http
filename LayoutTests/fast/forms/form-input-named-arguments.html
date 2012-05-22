WebKit/WebKit-http

commit 4f0ef143fa92c617e9932a47da70207767343be3

[V8] Fix object scope for inline event attribute handlers https://bugs.webkit.org/show_bug.cgi?id=80329
Reviewed by Ojan Vafai.

Source/WebCore:

We now create the funciton inside the with-statements with the current scope objects.
This is important for a few reasons:

- We need to use the real objects and not just lookup the JS properties because these might have been overridden.
- We need to use the node, form and document at the time of the preparation and not at the time of calling.
- We need to ensure that event/evt is bound closer than a property with the same name in the object environment
  created by the with-statements.

Tests: fast/dom/inline-event-attributes-lookup-removed-form.html
       fast/dom/inline-event-attributes-lookup-removed.html
       fast/dom/inline-event-attributes-lookup.html

* bindings/v8/ScriptEventListener.cpp:
(WebCore::eventParameterName):
(WebCore):
(WebCore::createAttributeEventListener):
* bindings/v8/V8LazyEventListener.cpp:
(WebCore::V8LazyEventListener::V8LazyEventListener):
(WebCore):
(WebCore::toObjectWrapper):
(WebCore::V8LazyEventListener::callListenerFunction):
(WebCore::V8LazyEventListener::prepareListenerObject):
* bindings/v8/V8LazyEventListener.h:
(WebCore):
(WebCore::V8LazyEventListener::create):
(V8LazyEventListener):

LayoutTests:

* fast/dom/inline-event-attributes-lookup-expected.txt: Added.
* fast/dom/inline-event-attributes-lookup-removed-expected.txt: Added.
* fast/dom/inline-event-attributes-lookup-removed-form-expected.txt: Added.
* fast/dom/inline-event-attributes-lookup-removed-form.html: Added.
* fast/dom/inline-event-attributes-lookup-removed.html: Added.
* fast/dom/inline-event-attributes-lookup.html: Added.
* fast/forms/lazy-event-listener-scope-chain-expected.txt:
* fast/forms/lazy-event-listener-scope-chain.html:

git-svn-id: http://svn.webkit.org/repository/webkit/trunk@110315 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Source/WebCore/bindings/v8/V8LazyEventListener.cpp
@@ -32,9 +32,17 @@
 #include "V8LazyEventListener.h"
 
 #include "ContentSecurityPolicy.h"
+#include "Document.h"
 #include "Frame.h"
+#include "HTMLElement.h"
+#include "HTMLFormElement.h"
+#include "Node.h"
 #include "V8Binding.h"
+#include "V8DOMWrapper.h"
+#include "V8Document.h"
+#include "V8HTMLFormElement.h"
 #include "V8HiddenPropertyName.h"
+#include "V8Node.h"
 #include "V8Proxy.h"
 #include "WorldContextHandle.h"
 
@@ -42,14 +50,29 @@
 
 namespace WebCore {
 
-V8LazyEventListener::V8LazyEventListener(const String& functionName, bool isSVGEvent, const String& code, const String sourceURL, const TextPosition& position, const WorldContextHandle& worldContext)
+V8LazyEventListener::V8LazyEventListener(const AtomicString& functionName, const AtomicString& eventParameterName, const String& code, const String sourceURL, const TextPosition& position, PassRefPtr<Node> node, const WorldContextHandle& worldContext)
     : V8AbstractEventListener(true, worldContext)
     , m_functionName(functionName)
-    , m_isSVGEvent(isSVGEvent)
+    , m_eventParameterName(eventParameterName)
     , m_code(code)
     , m_sourceURL(sourceURL)
+    , m_node(node)
+    , m_formElement(0)
     , m_position(position)
 {
+    if (m_node && m_node->isHTMLElement())
+        m_formElement = static_cast<HTMLElement*>(m_node.get())->form();
+}
+
+template<typename T>
+v8::Handle<v8::Object> toObjectWrapper(T* domObject)
+{
+    if (!domObject)
+        return v8::Object::New();
+    v8::Handle<v8::Value> value = toV8(domObject);
+    if (value.IsEmpty())
+        return v8::Object::New();
+    return value.As<v8::Object>();
 }
 
 v8::Local<v8::Value> V8LazyEventListener::callListenerFunction(ScriptExecutionContext* context, v8::Handle<v8::Value> jsEvent, Event* event)
@@ -58,7 +81,7 @@ v8::Local<v8::Value> V8LazyEventListener::callListenerFunction(ScriptExecutionCo
     if (listenerObject.IsEmpty())
         return v8::Local<v8::Value>();
 
-    v8::Local<v8::Function> handlerFunction = v8::Local<v8::Function>::Cast(listenerObject);
+    v8::Local<v8::Function> handlerFunction = listenerObject.As<v8::Function>();
     v8::Local<v8::Object> receiver = getReceiverObject(event);
     if (handlerFunction.IsEmpty() || receiver.IsEmpty())
         return v8::Local<v8::Value>();
@@ -104,8 +127,6 @@ void V8LazyEventListener::prepareListenerObject(ScriptExecutionContext* context)
 
     v8::Context::Scope scope(v8Context);
 
-    // FIXME: cache the wrapper function.
-
     // Nodes other than the document object, when executing inline event
     // handlers push document, form, and the target node on the scope chain.
     // We do this by using 'with' statement.
@@ -115,66 +136,86 @@ void V8LazyEventListener::prepareListenerObject(ScriptExecutionContext* context)
     //
     // Don't use new lines so that lines in the modified handler
     // have the same numbers as in the original code.
-    // FIXME: This approach is a giant hack! What if m_code escapes to run
-    //        arbitrary script?
-    String eventParameterName = m_isSVGEvent ? "evt" : "event";
-    String code = "(function(";
-    code.append(eventParameterName);
-    code.append(") {" \
-            "with (this.ownerDocument ? this.ownerDocument : {}) {" \
-            "with (this.form ? this.form : {}) {" \
-            "with (this) {" \
-            "return (function(");
-    code.append(eventParameterName);
-    code.append("){");
+    // FIXME: V8 does not allow us to programmatically create object environments so
+    //        we have to do this hack! What if m_code escapes to run arbitrary script?
+    //
+    String code = "(function() {" \
+        "with (arguments[2]) {" \
+        "with (arguments[1]) {" \
+        "with (arguments[0]) {";
+    code.append("return function(");
+    code.append(m_eventParameterName);
+    code.append(") {");
+
+    int codePrexixLength = code.length();
+
     code.append(m_code);
     // Insert '\n' otherwise //-style comments could break the handler.
-    code.append("\n}).call(this, ");
-    code.append(eventParameterName);
-    code.append(");}}}})");
+    code.append("\n};}}}})");
     v8::Handle<v8::String> codeExternalString = v8ExternalString(code);
-    v8::Handle<v8::Script> script = V8Proxy::compileScript(codeExternalString, m_sourceURL, m_position);
-    if (!script.IsEmpty()) {
-        // Call v8::Script::Run() directly to avoid an erroneous call to V8RecursionScope::didLeaveScriptContext().
-        // FIXME: Remove this code when we stop doing the 'with' hack above.
-        v8::Local<v8::Value> value = script->Run();
-        if (!value.IsEmpty()) {
-            ASSERT(value->IsFunction());
 
-            v8::Local<v8::Function> wrappedFunction = v8::Local<v8::Function>::Cast(value);
+    TextPosition adjustedPosition(m_position.m_line, OrdinalNumber::fromZeroBasedInt(m_position.m_column.zeroBasedInt() - codePrexixLength));
 
-            // Change the toString function on the wrapper function to avoid it
-            // returning the source for the actual wrapper function. Instead it
-            // returns source for a clean wrapper function with the event
-            // argument wrapping the event source code. The reason for this is
-            // that some web sites use toString on event functions and eval the
-            // source returned (sometimes a RegExp is applied as well) for some
-            // other use. That fails miserably if the actual wrapper source is
-            // returned.
-            v8::Persistent<v8::FunctionTemplate>& toStringTemplate =
-                V8BindingPerIsolateData::current()->lazyEventListenerToStringTemplate();
-            if (toStringTemplate.IsEmpty())
-                toStringTemplate = v8::Persistent<v8::FunctionTemplate>::New(v8::FunctionTemplate::New(V8LazyEventListenerToString));
-            v8::Local<v8::Function> toStringFunction;
-            if (!toStringTemplate.IsEmpty())
-                toStringFunction = toStringTemplate->GetFunction();
-            if (!toStringFunction.IsEmpty()) {
-                String toStringResult = "function ";
-                toStringResult.append(m_functionName);
-                toStringResult.append("(");
-                toStringResult.append(eventParameterName);
-                toStringResult.append(") {\n  ");
-                toStringResult.append(m_code);
-                toStringResult.append("\n}");
-                wrappedFunction->SetHiddenValue(V8HiddenPropertyName::toStringString(), v8ExternalString(toStringResult));
-                wrappedFunction->Set(v8::String::New("toString"), toStringFunction);
-            }
+    v8::Handle<v8::Script> script = V8Proxy::compileScript(codeExternalString, m_sourceURL, adjustedPosition);
+    if (script.IsEmpty())
+        return;
 
-            wrappedFunction->SetName(v8::String::New(fromWebCoreString(m_functionName), m_functionName.length()));
+    // Call v8::Script::Run() directly to avoid an erroneous call to V8RecursionScope::didLeaveScriptContext().
+    // FIXME: Remove this code when we stop doing the 'with' hack above.
+    v8::Local<v8::Value> value = script->Run();
+    if (value.IsEmpty())
+        return;
 
-            setListenerObject(wrappedFunction);
-        }
+    // Call the outer function to get the inner function.
+    ASSERT(value->IsFunction());
+    v8::Local<v8::Function> intermediateFunction = value.As<v8::Function>();
+
+    v8::Handle<v8::Object> nodeWrapper = toObjectWrapper<Node>(m_node.get());
+    v8::Handle<v8::Object> formWrapper = toObjectWrapper<HTMLFormElement>(m_formElement.get());
+    v8::Handle<v8::Object> documentWrapper = toObjectWrapper<Document>(m_node ? m_node->ownerDocument() : 0);
+
+    m_node.clear();
+    m_formElement.clear();
+
+    v8::Handle<v8::Value> parameters[3] = { nodeWrapper, formWrapper, documentWrapper };
+
+    // Use Call directly to avoid an erroneous call to V8RecursionScope::didLeaveScriptContext().
+    // FIXME: Remove this code when we stop doing the 'with' hack above.
+    v8::Local<v8::Value> innerValue = intermediateFunction->Call(v8Context->Global(), 3, parameters);
+
+    ASSERT(innerValue->IsFunction());
+    v8::Local<v8::Function> wrappedFunction = innerValue.As<v8::Function>();
+
+    // Change the toString function on the wrapper function to avoid it
+    // returning the source for the actual wrapper function. Instead it
+    // returns source for a clean wrapper function with the event
+    // argument wrapping the event source code. The reason for this is
+    // that some web sites use toString on event functions and eval the
+    // source returned (sometimes a RegExp is applied as well) for some
+    // other use. That fails miserably if the actual wrapper source is
+    // returned.
+    v8::Persistent<v8::FunctionTemplate>& toStringTemplate =
+        V8BindingPerIsolateData::current()->lazyEventListenerToStringTemplate();
+    if (toStringTemplate.IsEmpty())
+        toStringTemplate = v8::Persistent<v8::FunctionTemplate>::New(v8::FunctionTemplate::New(V8LazyEventListenerToString));
+    v8::Local<v8::Function> toStringFunction;
+    if (!toStringTemplate.IsEmpty())
+        toStringFunction = toStringTemplate->GetFunction();
+    if (!toStringFunction.IsEmpty()) {
+        String toStringResult = "function ";
+        toStringResult.append(m_functionName);
+        toStringResult.append("(");
+        toStringResult.append(m_eventParameterName);
+        toStringResult.append(") {\n  ");
+        toStringResult.append(m_code);
+        toStringResult.append("\n}");
+        wrappedFunction->SetHiddenValue(V8HiddenPropertyName::toStringString(), v8ExternalString(toStringResult));
+        wrappedFunction->Set(v8::String::New("toString"), toStringFunction);
     }
+
+    wrappedFunction->SetName(v8::String::New(fromWebCoreString(m_functionName), m_functionName.length()));
+
+    setListenerObject(wrappedFunction);
 }
 
 } // namespace WebCore

LayoutTests/fast/dom/inline-event-attributes-lookup-expected.txt
@@ -1,0 +1,17 @@
+Tests the lookup in inline event handlers
+
+On success, you will see a series of "PASS" messages, followed by "TEST COMPLETE".
+
+
+PASS result is "test-input-a"
+PASS result is "test-form-b"
+PASS result is "document-c"
+PASS result is "div-a"
+PASS result is "document-b"
+PASS result is "document-c"
+PASS result is div
+PASS result is not "FAIL"
+PASS successfullyParsed is true
+
+TEST COMPLETE
+

LayoutTests/fast/dom/inline-event-attributes-lookup-removed-expected.txt
@@ -1,0 +1,10 @@
+Tests that lookup in inline event handlers keeps references alive
+
+On success, you will see a series of "PASS" messages, followed by "TEST COMPLETE".
+
+
+PASS result is "PASS"
+PASS successfullyParsed is true
+
+TEST COMPLETE
+

LayoutTests/fast/dom/inline-event-attributes-lookup-removed-form-expected.txt
@@ -1,0 +1,10 @@
+Tests that lookup in inline event handlers keeps references alive
+
+On success, you will see a series of "PASS" messages, followed by "TEST COMPLETE".
+
+
+PASS result is "PASS"
+PASS successfullyParsed is true
+
+TEST COMPLETE
+

LayoutTests/fast/dom/inline-event-attributes-lookup-removed-form.html
@@ -1,0 +1,39 @@
+<!DOCTYPE html>
+<script src="../js/resources/js-test-pre.js"></script>
+<form id="test-form">
+    <input id="test-input">
+</form>
+<script>
+
+description('Tests that lookup in inline event handlers keeps references alive');
+
+function forceGC()
+{
+    if (window.GCController)
+        return GCController.collect();
+
+    // Force garbage collection
+    for (var ndx = 0; ndx < 99000; ndx++)
+        var str = new String("1234");
+}
+
+var result;
+var pa = '(FAIL input)'
+var ss = '(FAIL form)'
+
+var input = document.getElementById('test-input');
+input.pa = 'PA';
+input.form.ss = 'SS';
+input.setAttribute('onclick', 'result = pa + ss');
+
+var f = input.onclick;
+
+input.parentNode.removeChild(input);
+input = null;
+forceGC();
+
+f();
+shouldBeEqualToString('result', 'PASS');
+
+</script>
+<script src="../js/resources/js-test-post.js"></script>

LayoutTests/fast/dom/inline-event-attributes-lookup-removed.html
@@ -1,0 +1,41 @@
+<!DOCTYPE html>
+<script src="../js/resources/js-test-pre.js"></script>
+<script>
+
+description('Tests that lookup in inline event handlers keeps references alive');
+
+function forceGC()
+{
+    if (window.GCController)
+        return GCController.collect();
+
+    // Force garbage collection
+    for (var ndx = 0; ndx < 99000; ndx++)
+        var str = new String("1234");
+}
+
+function dispatchClick(element)
+{
+    var clickEvent = document.createEvent('MouseEvent');
+    clickEvent.initMouseEvent('click', true, false, window, 0, 0, 0, 0, 0,false, false, false, false, 0, null);
+    element.dispatchEvent(clickEvent);
+}
+
+var result;
+var custom = 'FAIL';
+
+var div1 = document.createElement('div');
+div1.custom = 'PASS';
+div1.setAttribute('onclick', 'result = custom');
+
+var div2 = document.createElement('div');
+div2.onclick = div1.onclick;
+
+div1 = null;
+forceGC();
+
+dispatchClick(div2);
+shouldBeEqualToString('result', 'PASS');
+
+</script>
+<script src="../js/resources/js-test-post.js"></script>

LayoutTests/fast/dom/inline-event-attributes-lookup.html
@@ -1,0 +1,70 @@
+<!DOCTYPE html>
+<script src="../js/resources/js-test-pre.js"></script>
+<form id="test-form">
+    <input id="test-input">
+</form>
+<script>
+
+description('Tests the lookup in inline event handlers');
+
+function dispatchClick(element)
+{
+    var clickEvent = document.createEvent('MouseEvent');
+    clickEvent.initMouseEvent('click', true, false, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null);
+    element.dispatchEvent(clickEvent);
+}
+
+var result;
+var testForm = document.getElementById('test-form');
+var testInput = document.getElementById('test-input');
+
+document.a = 'document-a';
+testForm.a = 'test-form-a';
+testInput.a = 'test-input-a';
+
+document.b = 'document-b';
+testForm.b = 'test-form-b';
+
+document.c = 'document-c';
+
+testInput.setAttribute('onclick', 'result = a');
+dispatchClick(testInput);
+shouldBeEqualToString('result', 'test-input-a');
+
+testInput.setAttribute('onclick', 'result = b');
+dispatchClick(testInput);
+shouldBeEqualToString('result', 'test-form-b');
+
+testInput.setAttribute('onclick', 'result = c');
+dispatchClick(testInput);
+shouldBeEqualToString('result', 'document-c');
+
+
+var div = document.createElement('div');
+
+div.a = 'div-a';
+div.setAttribute('onclick', 'result = a');
+dispatchClick(div);
+shouldBeEqualToString('result', 'div-a');
+
+div.form = {b: 'fake-b'};
+div.setAttribute('onclick', 'result = b');
+dispatchClick(div);
+shouldBeEqualToString('result', 'document-b');
+
+div.ownerDocument = {c: 'fake-c'};
+div.setAttribute('onclick', 'result = c');
+dispatchClick(div);
+shouldBeEqualToString('result', 'document-c');
+
+div.setAttribute('onclick', 'result = this');
+dispatchClick(div);
+shouldBe('result', 'div');
+
+div.event = 'FAIL';
+div.setAttribute('onclick', 'result = event');
+dispatchClick(div);
+shouldNotBe('result', '"FAIL"');
+
+</script>
+<script src="../js/resources/js-test-post.js"></script>

LayoutTests/fast/forms/lazy-event-listener-scope-chain-expected.txt
@@ -1,4 +1,9 @@
-This test tests that a lazy event listener attached to a form element keeps its form in the scope chain when the listener is called by javascript. 
- 
-abc
+This test tests that a lazy event listener attached to a form element keeps its form in the scope chain when the listener is called by JavaScript.
 
+On success, you will see a series of "PASS" messages, followed by "TEST COMPLETE".
+
+
+PASS result is "abc"
+PASS result is "abc"
+PASS result is "abc"
+ 

commit 188f91ee17bca0fc7908d83e3438f2f409fc8f55

REGRESSION r110315: Event handler throws TypeError for an input element with name="arguments" https://bugs.webkit.org/show_bug.cgi?id=86991
Reviewed by Ojan Vafai.

Source/WebCore:

Original Chromium bug: http://code.google.com/p/chromium/issues/detail?id=128723

Consider the following html:

<html><body><form>
<input type="hidden" name="arguments"></input>
<div onclick="onclicked()" id="divInsideForm">Click here</div>
</form></body>
<script>
function onclicked() {
  alert("onclicked");
}
</script>
</html>

If we click "Click here", JavaScript throws "Uncaught TypeError: undefined has no properties".

This is a regression caused by r110315. V8LazyEventListener should not use
'arguments' to retrieve the execution contexts, since 'arguments' can be
shadowed by JavaScript.

This patch changes V8LazyEventListener so that it retrieves contexts
by this[2], this[1] and this[0].

Test: fast/forms/form-input-named-arguments.html

* bindings/v8/V8LazyEventListener.cpp:
(WebCore::V8LazyEventListener::prepareListenerObject):

LayoutTests:

The added test checks whether an event handler is successfully invoked
for an input element with name="arguments".

* fast/forms/form-input-named-arguments-expected.txt: Added.
* fast/forms/form-input-named-arguments.html: Added.


git-svn-id: http://svn.webkit.org/repository/webkit/trunk@117928 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Source/WebCore/bindings/v8/V8LazyEventListener.cpp
@@ -137,16 +137,21 @@ void V8LazyEventListener::prepareListenerObject(ScriptExecutionContext* context)
     // FIXME: V8 does not allow us to programmatically create object environments so
     //        we have to do this hack! What if m_code escapes to run arbitrary script?
     //
+    // Call with 4 arguments instead of 3, pass additional null as the last parameter.
+    // By calling the function with 4 arguments, we create a setter on arguments object
+    // which would shadow property "3" on the prototype.
     String code = "(function() {" \
-        "with (arguments[2]) {" \
-        "with (arguments[1]) {" \
-        "with (arguments[0]) {";
+        "arguments[3] = function() {" \
+        "with (this[2]) {" \
+        "with (this[1]) {" \
+        "with (this[0]) {";
     code.append("return function(");
     code.append(m_eventParameterName);
     code.append(") {");
     code.append(m_code);
     // Insert '\n' otherwise //-style comments could break the handler.
-    code.append("\n};}}}})");
+    code.append("\n};}}}};");
+    code.append("return arguments[3]();})");
     v8::Handle<v8::String> codeExternalString = v8ExternalString(code);
 
     v8::Handle<v8::Script> script = V8Proxy::compileScript(codeExternalString, m_sourceURL, m_position);
@@ -174,7 +179,7 @@ void V8LazyEventListener::prepareListenerObject(ScriptExecutionContext* context)
     v8::Handle<v8::Object> formWrapper = toObjectWrapper<HTMLFormElement>(formElement);
     v8::Handle<v8::Object> documentWrapper = toObjectWrapper<Document>(m_node ? m_node->ownerDocument() : 0);
 
-    v8::Handle<v8::Value> parameters[3] = { nodeWrapper, formWrapper, documentWrapper };
+    v8::Handle<v8::Value> parameters[4] = { nodeWrapper, formWrapper, documentWrapper, v8::Handle<v8::Value>(v8::Null()) };
 
     // FIXME: Remove this code when we stop doing the 'with' hack above.
     v8::Local<v8::Value> innerValue;

LayoutTests/fast/forms/form-input-named-arguments-expected.txt
@@ -1,0 +1,5 @@
+PASS clicked is true
+PASS successfullyParsed is true
+
+TEST COMPLETE
+

LayoutTests/fast/forms/form-input-named-arguments.html
@@ -1,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head></head>
+<body>
+<script src="../js/resources/js-test-pre.js"></script>
+<form>
+<input type="hidden" name="arguments"></input>
+<div onclick="onclicked()" id="divInsideForm"></div>
+</form>
+</body>
+<script>
+var clicked = false;
+
+function onclicked() {
+   clicked = true;
+}
+
+var event = document.createEvent("MouseEvents");
+event.initMouseEvent("click", true, true, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null);
+var div = document.getElementById("divInsideForm");
+div.dispatchEvent(event);
+
+shouldBeTrue('clicked');
+
+var successfullyParsed = true;
+</script>
+<script src="../js/resources/js-test-post.js"></script>
+</html>

LayoutTests/platform/chromium-win/inspector/debugger/debugger-scripts-expected.txt
@@ -18,6 +18,6 @@ script 5:
     end: 48:0
 script 6:
     start: 51:56
-    end: 52:7
+    end: 52:31
 Debugger was disabled.
 