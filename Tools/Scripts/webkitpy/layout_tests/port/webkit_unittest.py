WebKit/WebKit-http

commit 4326928306969fa59bd8c38c8ddbf91109ad58d9

nrwt: seems to leak temp dirs https://bugs.webkit.org/show_bug.cgi?id=87795
Reviewed by Ojan Vafai.

There appears to be a bug where the chromium bots are creating
temporary directories and not cleaning them up that started
after the switch to WebKitDriver. It's possible that __del__
wasn't getting called in a timely manner (or at all), and it's
generally bad style to rely on __del__ being called, so this
code changes things so that we create a temp dir in
driver.start() and remove it in driver.stop(). We could be
paranoid and leave the __del__ code in, but there doesn't seem
to be much advantage to it. If there are bugs that result in
drivers being started but not stopped, we have other problems.

* Scripts/webkitpy/common/system/filesystem_mock.py:
(MockFileSystem.__init__):
(MockFileSystem._mktemp):
(MockFileSystem.mkdtemp):
* Scripts/webkitpy/layout_tests/port/webkit.py:
(WebKitDriver.__init__):
(WebKitDriver._start):
(WebKitDriver.stop):
* Scripts/webkitpy/layout_tests/port/webkit_unittest.py:
(WebKitDriverTest.test_check_for_driver_crash):
(WebKitDriverTest):
(WebKitDriverTest.test_creating_a_port_does_not_write_to_the_filesystem):
(WebKitDriverTest.test_stop_cleans_up_properly):

git-svn-id: http://svn.webkit.org/repository/webkit/trunk@118979 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Tools/Scripts/webkitpy/layout_tests/port/webkit.py
@@ -441,7 +441,7 @@ class WebKitDriver(Driver):
 
     def __init__(self, port, worker_number, pixel_tests, no_timeout=False):
         Driver.__init__(self, port, worker_number, pixel_tests, no_timeout)
-        self._driver_tempdir = port._filesystem.mkdtemp(prefix='%s-' % self._port.driver_name())
+        self._driver_tempdir = None
         # WebKitTestRunner can report back subprocess crashes by printing
         # "#CRASHED - PROCESSNAME".  Since those can happen at any time
         # and ServerProcess won't be aware of them (since the actual tool
@@ -457,10 +457,9 @@ class WebKitDriver(Driver):
         self.err_seen_eof = False
         self._server_process = None
 
-    # FIXME: This may be unsafe, as python does not guarentee any ordering of __del__ calls
-    # I believe it's possible that self._port or self._port._filesystem may already be destroyed.
     def __del__(self):
-        self._port._filesystem.rmtree(str(self._driver_tempdir))
+        assert(self._server_process is None)
+        assert(self._driver_tempdir is None)
 
     def cmd_line(self, pixel_tests, per_test_args):
         cmd = self._command_wrapper(self._port.get_option('wrapper'))
@@ -485,6 +484,7 @@ class WebKitDriver(Driver):
         return cmd
 
     def _start(self, pixel_tests, per_test_args):
+        self._driver_tempdir = self._port._filesystem.mkdtemp(prefix='%s-' % self._port.driver_name())
         server_name = self._port.driver_name()
         environment = self._port.setup_environ_for_server(server_name)
         environment['DYLD_LIBRARY_PATH'] = self._port._build_path()
@@ -667,6 +667,10 @@ class WebKitDriver(Driver):
         if self._server_process:
             self._server_process.stop()
             self._server_process = None
+
+        if self._driver_tempdir:
+            self._port._filesystem.rmtree(str(self._driver_tempdir))
+            self._driver_tempdir = None
 
 
 class ContentBlock(object):

Tools/Scripts/webkitpy/layout_tests/port/webkit_unittest.py
@@ -277,6 +277,7 @@ class WebKitDriverTest(unittest.TestCase):
         self.assertEquals(content_block.content_type, 'my_type')
         self.assertEquals(content_block.encoding, 'none')
         self.assertEquals(content_block.content_hash, 'foobar')
+        driver._server_process = None
 
     def test_read_binary_block(self):
         port = TestWebKitPort()
@@ -294,6 +295,7 @@ class WebKitDriverTest(unittest.TestCase):
         self.assertEquals(content_block.content_hash, 'actual')
         self.assertEquals(content_block.content, '12345678')
         self.assertEquals(content_block.decoded_content, '12345678')
+        driver._server_process = None
 
     def test_no_timeout(self):
         port = TestWebKitPort()
@@ -317,10 +319,14 @@ class WebKitDriverTest(unittest.TestCase):
             def has_crashed(self):
                 return self.crashed
 
+            def stop(self):
+                pass
+
         def assert_crash(driver, error_line, crashed, name, pid):
             self.assertEquals(driver._check_for_driver_crash(error_line), crashed)
             self.assertEquals(driver._crashed_process_name, name)
             self.assertEquals(driver._crashed_pid, pid)
+            driver.stop()
 
         driver._server_process = FakeServerProcess(False)
         assert_crash(driver, '', False, None, None)
@@ -344,3 +350,18 @@ class WebKitDriverTest(unittest.TestCase):
         driver._crashed_pid = None
         driver._server_process = FakeServerProcess(True)
         assert_crash(driver, '', True, 'FakeServerProcess', 1234)
+
+    def test_creating_a_port_does_not_write_to_the_filesystem(self):
+        port = TestWebKitPort()
+        driver = WebKitDriver(port, 0, pixel_tests=True)
+        self.assertEquals(port._filesystem.written_files, {})
+        self.assertEquals(port._filesystem.last_tmpdir, None)
+
+    def test_stop_cleans_up_properly(self):
+        port = TestWebKitPort()
+        driver = WebKitDriver(port, 0, pixel_tests=True)
+        driver.start(True, [])
+        last_tmpdir = port._filesystem.last_tmpdir
+        self.assertNotEquals(last_tmpdir, None)
+        driver.stop()
+        self.assertFalse(port._filesystem.isdir(last_tmpdir))

Tools/Scripts/webkitpy/common/system/filesystem_mock.py
@@ -47,6 +47,7 @@ class MockFileSystem(object):
         """
         self.files = files or {}
         self.written_files = {}
+        self.last_tmpdir = None
         self._sep = '/'
         self.current_tmpno = 0
         self.cwd = cwd
@@ -233,7 +234,8 @@ class MockFileSystem(object):
             dir = self.sep + '__im_tmp'
         curno = self.current_tmpno
         self.current_tmpno += 1
-        return self.join(dir, "%s_%u_%s" % (prefix, curno, suffix))
+        self.last_tmpdir = self.join(dir, '%s_%u_%s' % (prefix, curno, suffix))
+        return self.last_tmpdir
 
     def mkdtemp(self, **kwargs):
         class TemporaryDirectory(object):

commit 115a489fff805c6e8649c2f378f63ef1e8b90749

nrwt seems to leak tmpdirs https://bugs.webkit.org/show_bug.cgi?id=88126
Reviewed by Ojan Vafai.

Fix leak possibly introduced in r118979 - we now will
make sure we clean up any stale temp directories during the
start routine and attempt to clean up again in __del__. It
turns out that asserting in __del__ logs a message but is
caught by the runtime, so it's mostly useless.

* Scripts/webkitpy/layout_tests/port/webkit.py:
(WebKitDriver.__del__):
(WebKitDriver._start):
(WebKitDriver.run_test):
* Scripts/webkitpy/layout_tests/port/webkit_unittest.py:
(WebKitDriverTest.test_check_for_driver_crash):
(WebKitDriverTest.test_stop_cleans_up_properly):
(WebKitDriverTest):
(WebKitDriverTest.test_two_starts_cleans_up_properly):

git-svn-id: http://svn.webkit.org/repository/webkit/trunk@119297 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Tools/Scripts/webkitpy/layout_tests/port/webkit.py
@@ -462,8 +462,7 @@ class WebKitDriver(Driver):
         self._server_process = None
 
     def __del__(self):
-        assert(self._server_process is None)
-        assert(self._driver_tempdir is None)
+        self.stop()
 
     def cmd_line(self, pixel_tests, per_test_args):
         cmd = self._command_wrapper(self._port.get_option('wrapper'))
@@ -488,6 +487,7 @@ class WebKitDriver(Driver):
         return cmd
 
     def _start(self, pixel_tests, per_test_args):
+        self.stop()
         self._driver_tempdir = self._port._filesystem.mkdtemp(prefix='%s-' % self._port.driver_name())
         server_name = self._port.driver_name()
         environment = self._port.setup_environ_for_server(server_name)
@@ -565,8 +565,7 @@ class WebKitDriver(Driver):
 
     def run_test(self, driver_input):
         start_time = time.time()
-        if not self._server_process:
-            self._start(driver_input.should_run_pixel_test, driver_input.args)
+        self.start(driver_input.should_run_pixel_test, driver_input.args)
         self.error_from_test = str()
         self.err_seen_eof = False
 

Tools/Scripts/webkitpy/layout_tests/port/webkit_unittest.py
@@ -349,7 +349,7 @@ class WebKitDriverTest(unittest.TestCase):
         driver._server_process = FakeServerProcess(False)
         driver._subprocess_was_unresponsive = False
         assert_crash(driver, '#CRASHED - WebProcess (pid 8675)\n', True, 'WebProcess', 8675)
-        
+
         driver._crashed_process_name = None
         driver._crashed_pid = None
         driver._server_process = FakeServerProcess(False)
@@ -375,4 +375,12 @@ class WebKitDriverTest(unittest.TestCase):
         last_tmpdir = port._filesystem.last_tmpdir
         self.assertNotEquals(last_tmpdir, None)
         driver.stop()
+        self.assertFalse(port._filesystem.isdir(last_tmpdir))
+
+    def test_two_starts_cleans_up_properly(self):
+        port = TestWebKitPort()
+        driver = WebKitDriver(port, 0, pixel_tests=True)
+        driver.start(True, [])
+        last_tmpdir = port._filesystem.last_tmpdir
+        driver._start(True, [])
         self.assertFalse(port._filesystem.isdir(last_tmpdir))