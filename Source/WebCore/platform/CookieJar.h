46a7d43f6b4edba0daafb852f4108ecf39e6ebdf
Alexandre Deckner <alexandre.deckner@uzzl.com>
2012-06-05 18:23:37 +0200

    Import platform/haiku/CookieJarHaiku.cpp and our changes to platform/CookieJar.h

--- Source/WebCore/platform/CookieJar.h
+++ Source/WebCore/platform/CookieJar.h
@@ -50,6 +50,21 @@ namespace WebCore {
     void deleteCookiesForHostname(const String& hostname);
     void deleteAllCookies();
 
+#if PLATFORM(HAIKU)
+class CookieJarClient {
+public:
+    virtual ~CookieJarClient() {}
+
+    virtual String cookies(const Document*, const KURL&) = 0;
+    virtual String cookieRequestHeaderFieldValue(const Document*, const KURL&) = 0;
+    virtual void setCookies(Document*, const KURL&, const String&) = 0;
+    virtual bool cookiesEnabled(const Document*) = 0;
+    virtual bool getRawCookies(const Document*, const KURL&, Vector<Cookie>&) = 0;
+    virtual void deleteCookie(const Document*, const KURL&, const String&) = 0;
+};
+
+void setCookieJarClient(CookieJarClient*);
+#endif // PLATFORM(HAIKU)
 }
 
 #endif
