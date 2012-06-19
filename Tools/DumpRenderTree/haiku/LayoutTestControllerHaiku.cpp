3dfa7518bf12ebb31e6b4f590bd2fc6c06fabb68
Alexandre Deckner <alexandre.deckner@uzzl.com>
2012-06-19 23:29:00 +0200

    Import DumpRenderTree/haiku

---
+++ Tools/DumpRenderTree/haiku/LayoutTestControllerHaiku.cpp
@@ -0,0 +1,355 @@
+/*
+ * Copyright (C) 2008 Kevin Ollivier <kevino@theolliviers.com>
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1.  Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ * 2.  Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimer in the
+ *     documentation and/or other materials provided with the distribution.
+ * 3.  Neither the name of Apple Computer, Inc. ("Apple") nor the names of
+ *     its contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY APPLE AND ITS CONTRIBUTORS "AS IS" AND ANY
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL APPLE OR ITS CONTRIBUTORS BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+ * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include "config.h"
+#include "LayoutTestController.h"
+
+#include "DumpRenderTree.h"
+#include "WorkQueue.h"
+#include "WorkQueueItem.h"
+#include <JavaScriptCore/JSRetainPtr.h>
+#include <JavaScriptCore/JSStringRef.h>
+
+
+
+LayoutTestController::~LayoutTestController()
+{
+    // FIXME: implement
+}
+
+void LayoutTestController::addDisallowedURL(JSStringRef url)
+{
+    // FIXME: implement
+}
+
+void LayoutTestController::clearBackForwardList()
+{
+}
+
+JSStringRef LayoutTestController::copyDecodedHostName(JSStringRef name)
+{
+    // FIXME: implement
+    return 0;
+}
+
+JSStringRef LayoutTestController::copyEncodedHostName(JSStringRef name)
+{
+    // FIXME: implement
+    return 0;
+}
+
+void LayoutTestController::dispatchPendingLoadRequests()
+{
+    // FIXME: Implement
+}
+
+void LayoutTestController::display()
+{
+}
+
+void LayoutTestController::keepWebHistory()
+{
+    // FIXME: implement
+}
+
+void LayoutTestController::notifyDone()
+{
+    if (m_waitToDump && !WorkQueue::shared()->count())
+        notifyDoneFired();
+    m_waitToDump = false;
+}
+
+JSStringRef LayoutTestController::pathToLocalResource(JSContextRef context, JSStringRef url)
+{
+    return JSStringRetain(url); // Do nothing on Unix.
+}
+
+void LayoutTestController::queueLoad(JSStringRef url, JSStringRef target)
+{
+    // FIXME: We need to resolve relative URLs here
+    WorkQueue::shared()->queue(new LoadItem(url, target));
+}
+
+void LayoutTestController::setAcceptsEditing(bool acceptsEditing)
+{
+}
+
+void LayoutTestController::setAlwaysAcceptCookies(bool)
+{
+    // FIXME: implement
+}
+
+void LayoutTestController::setCustomPolicyDelegate(bool, bool)
+{
+    // FIXME: implement
+}
+
+void LayoutTestController::setMainFrameIsFirstResponder(bool flag)
+{
+    // FIXME: implement
+}
+
+void LayoutTestController::setTabKeyCyclesThroughElements(bool cycles)
+{
+    // FIXME: implement
+}
+
+void LayoutTestController::setTimelineProfilingEnabled(bool flag)
+{
+    // FIXME: implement
+}
+
+void LayoutTestController::setUseDashboardCompatibilityMode(bool flag)
+{
+    // FIXME: implement
+}
+
+void LayoutTestController::setUserStyleSheetEnabled(bool flag)
+{
+    // FIXME: implement
+}
+
+void LayoutTestController::setUserStyleSheetLocation(JSStringRef path)
+{
+    // FIXME: implement
+}
+
+void LayoutTestController::setWindowIsKey(bool windowIsKey)
+{
+    // FIXME: implement
+}
+
+void LayoutTestController::setSmartInsertDeleteEnabled(bool flag)
+{
+    // FIXME: implement
+}
+
+void LayoutTestController::setJavaScriptProfilingEnabled(bool flag)
+{
+    // FIXME: implement
+}
+
+void LayoutTestController::setWaitToDump(bool waitUntilDone)
+{
+    static const int timeoutSeconds = 10;
+    m_waitToDump = waitUntilDone;
+}
+
+int LayoutTestController::windowCount()
+{
+    // FIXME: implement
+    return 1;
+}
+
+void LayoutTestController::setPrivateBrowsingEnabled(bool privateBrowsingEnabled)
+{
+    // FIXME: implement
+}
+
+void LayoutTestController::setXSSAuditorEnabled(bool enabled)
+{
+    // FIXME: implement
+}
+
+void LayoutTestController::setAuthorAndUserStylesEnabled(bool flag)
+{
+    // FIXME: implement
+}
+
+void LayoutTestController::setPopupBlockingEnabled(bool popupBlockingEnabled)
+{
+    // FIXME: implement
+}
+
+bool LayoutTestController::elementDoesAutoCompleteForElementWithId(JSStringRef id)
+{
+    // FIXME: implement
+    return false;
+}
+
+void LayoutTestController::execCommand(JSStringRef name, JSStringRef value)
+{
+    // FIXME: implement
+}
+
+void LayoutTestController::setPersistentUserStyleSheetLocation(JSStringRef jsURL)
+{
+    // FIXME: implement
+}
+
+void LayoutTestController::clearPersistentUserStyleSheet()
+{
+    // FIXME: implement
+}
+
+void LayoutTestController::clearAllDatabases()
+{
+    // FIXME: implement
+}
+
+void LayoutTestController::setDatabaseQuota(unsigned long long quota)
+{
+    // FIXME: implement
+}
+
+void LayoutTestController::setAppCacheMaximumSize(unsigned long long size)
+{
+    // FIXME: implement
+}
+
+bool LayoutTestController::sampleSVGAnimationForElementAtTime(JSStringRef animationId, double time, JSStringRef elementId)
+{
+    // FIXME: implement
+}
+
+unsigned LayoutTestController::numberOfActiveAnimations() const
+{
+    // FIXME: implement
+    return 0;
+}
+
+unsigned LayoutTestController::workerThreadCount() const
+{
+    // FIXME: implement
+    return 0;
+}
+
+void LayoutTestController::setSelectTrailingWhitespaceEnabled(bool flag)
+{
+    // FIXME: implement
+}
+
+bool LayoutTestController::pauseTransitionAtTimeOnElementWithId(JSStringRef propertyName, double time, JSStringRef elementId)
+{
+    // FIXME: implement
+    return false;
+}
+
+void LayoutTestController::setAllowUniversalAccessFromFileURLs(bool flag)
+{
+    // FIXME: implement
+}
+
+void LayoutTestController::disableImageLoading()
+{
+    // FIXME: implement
+}
+
+void LayoutTestController::setIconDatabaseEnabled(bool iconDatabaseEnabled)
+{
+    // FIXME: implement
+}
+
+bool LayoutTestController::pauseAnimationAtTimeOnElementWithId(JSStringRef animationName, double time, JSStringRef elementId)
+{
+    // FIXME: implement
+    return false;
+}
+
+void LayoutTestController::setCacheModel(int)
+{
+    // FIXME: implement
+}
+
+bool LayoutTestController::isCommandEnabled(JSStringRef /*name*/)
+{
+    // FIXME: implement
+    return false;
+}
+
+size_t LayoutTestController::webHistoryItemCount()
+{
+    // FIXME: implement
+    return 0;
+}
+
+void LayoutTestController::waitForPolicyDelegate()
+{
+    // FIXME: Implement
+}
+
+void LayoutTestController::overridePreference(JSStringRef /*key*/, JSStringRef /*value*/)
+{
+    // FIXME: Implement
+}
+
+void LayoutTestController::whiteListAccessFromOrigin(JSStringRef sourceOrigin, JSStringRef destinationProtocol, JSStringRef destinationHost, bool allowDestinationSubdomains)
+{
+    // FIXME: Implement
+}
+
+void LayoutTestController::addUserScript(JSStringRef source, bool runAtStart)
+{
+    // FIXME: Implement
+}
+
+void LayoutTestController::addUserStyleSheet(JSStringRef source)
+{
+    // FIXME: Implement
+}
+
+void LayoutTestController::showWebInspector()
+{
+    // FIXME: Implement
+}
+
+void LayoutTestController::closeWebInspector()
+{
+    // FIXME: Implement
+}
+
+void LayoutTestController::evaluateInWebInspector(long callId, JSStringRef script)
+{
+    // FIXME: Implement
+}
+
+void LayoutTestController::evaluateScriptInIsolatedWorld(unsigned worldID, JSObjectRef globalObject, JSStringRef script)
+{
+    // FIXME: Implement
+}
+
+void LayoutTestController::removeAllVisitedLinks()
+{
+    // FIXME: Implement
+}
+
+void LayoutTestController::setMockGeolocationError(int code, JSStringRef message)
+{
+    // FIXME: Implement
+}
+
+void LayoutTestController::setMockGeolocationPosition(double latitude, double longitude, double accuracy)
+{
+    // FIXME: Implement
+}
+
+JSRetainPtr<JSStringRef> LayoutTestController::counterValueForElementById(JSStringRef id)
+{
+    // FIXME: Implement
+    return 0;
+}
