f3b528565ef579357f0bbfd4bd38733d581b924a
Alexandre Deckner <alexandre.deckner@uzzl.com>
2012-06-23 02:08:09 +0200

    Apply our cURL change r73 / f8c95c6c
    Adding handling for HTTP auth to the cURL backend. It is done by creating and
    forwarding an AuthenticationChallenge to the upper layers that then add
    credentials and then retrying the request with these supplied credentials.
    
    mmlr Sun Feb 14 23:17:52 2010 +0000

--- Source/WebCore/platform/network/curl/ResourceHandleCurl.cpp
+++ Source/WebCore/platform/network/curl/ResourceHandleCurl.cpp
@@ -192,14 +192,17 @@ void ResourceHandle::loadResourceSynchronously(NetworkingContext*, const Resourc
 }
 
 //stubs needed for windows version
-void ResourceHandle::didReceiveAuthenticationChallenge(const AuthenticationChallenge&) 
+void ResourceHandle::didReceiveAuthenticationChallenge(const AuthenticationChallenge& challenge)
 {
-    notImplemented();
+    if (client())
+        client()->didReceiveAuthenticationChallenge(this, challenge);
 }
 
-void ResourceHandle::receivedCredential(const AuthenticationChallenge&, const Credential&) 
+void ResourceHandle::receivedCredential(const AuthenticationChallenge&, const Credential& credential)
 {
-    notImplemented();
+    ResourceHandleInternal* internal = getInternal();
+    internal->m_user = credential.user();
+    internal->m_pass = credential.password();
 }
 
 void ResourceHandle::receivedRequestToContinueWithoutCredential(const AuthenticationChallenge&) 
