a1172059657353955a801b2f4fbeb5b86602e11c
Alexandre Deckner <alexandre.deckner@uzzl.com>
2012-07-10 23:16:16 +0200

    Retrieve WebKit version via the script generated WebKitVersion.h
    Inspired by the other platforms
    
    Fix typo in generate-webkitversion.pl

---
+++ Source/WebKit/haiku/API/Version.h
@@ -0,0 +1,17 @@
+/*
+ * Copyright 2012, Alexandre Deckner, alexandre.deckner@uzzl.com.
+ * Distributed under the terms of the MIT License.
+ */
+
+#ifndef _VERSION_H_
+#define _VERSION_H_
+
+#include <String.h>
+
+BString webKitVersion();
+
+int webKitMajorVersion();
+
+int webKitMinorVersion();
+
+#endif
