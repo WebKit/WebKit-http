096ac89432dea0627f4509a23e1b758a87ea0231
Rene Gollent <anevilyak@gmail.com>
2012-08-24 19:14:27 -0400

    Fix 64-bit JavascriptCore build.
    - Patch by Alex Smith.

--- Source/WTF/wtf/InlineASM.h
+++ Source/WTF/wtf/InlineASM.h
@@ -42,7 +42,7 @@
 #define THUMB_FUNC_PARAM(name)
 #endif
 
-#if (OS(LINUX) || OS(FREEBSD)) && CPU(X86_64)
+#if (OS(LINUX) || OS(FREEBSD) || OS(HAIKU)) && CPU(X86_64)
 #define SYMBOL_STRING_RELOCATION(name) #name "@plt"
 #elif OS(DARWIN) || (CPU(X86_64) && COMPILER(MINGW) && !GCC_VERSION_AT_LEAST(4, 5, 0))
 #define SYMBOL_STRING_RELOCATION(name) "_" #name
