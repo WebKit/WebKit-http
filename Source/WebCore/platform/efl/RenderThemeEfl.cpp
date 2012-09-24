WebKit/WebKit-http

commit 03bd47ca30145b9d63c61eb0dd1d1b647234b73c

[EFL] Duplicated RenderThemeEfl creation for same page https://bugs.webkit.org/show_bug.cgi?id=88391
Reviewed by Gyuyoung Kim.

Other ports return one or two RenderTheme regardless of page.
But, WebKit/Efl can create different theme for each page and
themeForPage creates new RenderTheme whenever it is called with page.
So more than one RenderTheme can be created for a page.

This patches checks whether page already has RenderTheme not to create new
RenderTheme for same page.

* platform/efl/RenderThemeEfl.cpp:
(WebCore::RenderTheme::themeForPage):


git-svn-id: http://svn.webkit.org/repository/webkit/trunk@129328 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Source/WebCore/platform/efl/RenderThemeEfl.cpp
@@ -421,7 +421,7 @@ PassRefPtr<RenderTheme> RenderThemeEfl::create(Page* page)
 PassRefPtr<RenderTheme> RenderTheme::themeForPage(Page* page)
 {
     if (page)
-        return RenderThemeEfl::create(page);
+        return page->theme() ? page->theme() : RenderThemeEfl::create(page);
 
     static RenderTheme* fallback = RenderThemeEfl::create(0).leakRef();
     return fallback;

commit 0778dbe8f02cb81cc8507b56d11c5dff4c03a0f2

[EFL] Regression after r129328 https://bugs.webkit.org/show_bug.cgi?id=97429
Unreviewed, rolling out r129328.

* platform/efl/RenderThemeEfl.cpp:
(WebCore::RenderTheme::themeForPage):

git-svn-id: http://svn.webkit.org/repository/webkit/trunk@129329 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Source/WebCore/platform/efl/RenderThemeEfl.cpp
@@ -421,7 +421,7 @@ PassRefPtr<RenderTheme> RenderThemeEfl::create(Page* page)
 PassRefPtr<RenderTheme> RenderTheme::themeForPage(Page* page)
 {
     if (page)
-        return page->theme() ? page->theme() : RenderThemeEfl::create(page);
+        return RenderThemeEfl::create(page);
 
     static RenderTheme* fallback = RenderThemeEfl::create(0).leakRef();
     return fallback;