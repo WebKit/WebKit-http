WebKit/WebKit-http

commit 9d183950a7da0cf13b13f8bda4c8a68599195c64

[CSSRegions]Add support for auto-height regions (without region-breaks) https://bugs.webkit.org/show_bug.cgi?id=97533
Reviewed by Julien Chaffraix.

Source/WebCore:

Implement the 2pass layout algorithm for computing the height of regions with auto logical height, without taking region breaks into account.
The algorithm is described in the CSSRegions spec: http://dev.w3.org/csswg/css3-regions/#regions-visual-formatting-steps

When the flow thread contains regions with auto logical height, a 2pass layout algorithm is needed to determine the height of auto logical height regions.
In each step of the layout, the regions are laid out before the flow thread is laid out.

In the first pass of layout (normal layout phase), the content of the flow thread is laid out unconstrained in the regions. The region marked as having auto logical height
displays all the corresponding content from the flow thread (taking min-height and max-height into account), and the height of the content displayed
determines the overridden logical content height. Thus, after the first pass of layout, each auto logical height region will store the logical height
value for the second pass in its overridden logical content height property.

In the second pass of layout (constrained flow threads layout phase), when the content of the flow thread is laid out constrained in the regions, the regions are laid out again and they are using
the overridden logical content height from the first pass to update their logical height. Then, the flow thread content is laid out in the associated region chain,
for which the auto logical height regions have proper logical height.

A future patch will properly take region breaks into account for the first pass of layout.

Tests: fast/regions/autoheight-abspos-region.html
       fast/regions/autoheight-floatright-region.html
       fast/regions/autoheight-horizontal-bt.html
       fast/regions/autoheight-inlineblock-normalflow-region.html

Source/WebCore/rendering/RenderView.cpp
@@ -65,6 +65,7 @@ RenderView::RenderView(Node* node, FrameView* view)
     , m_layoutStateDisableCount(0)
     , m_renderQuoteHead(0)
     , m_renderCounterCount(0)
+    , m_layoutPhase(RenderViewNormalLayout)
 {
     // Clear our anonymous bit, set because RenderObject assumes
     // any renderer with document as the node is anonymous.
@@ -132,6 +133,27 @@ bool RenderView::isChildAllowed(RenderObject* child, RenderStyle*) const
     return child->isBox();
 }
 
+void RenderView::layoutContent(const LayoutState& state)
+{
+    ASSERT(needsLayout());
+
+    RenderBlock::layout();
+    if (hasRenderNamedFlowThreads())
+        flowThreadController()->layoutRenderNamedFlowThreads();
+#ifndef NDEBUG
+    checkLayoutState(state);
+#endif
+}
+
+#ifndef NDEBUG
+void RenderView::checkLayoutState(const LayoutState& state)
+{
+    ASSERT(layoutDelta() == LayoutSize());
+    ASSERT(!m_layoutStateDisableCount);
+    ASSERT(m_layoutState == &state);
+}
+#endif
+
 void RenderView::layout()
 {
     if (!document()->paginated())
@@ -154,6 +176,9 @@ void RenderView::layout()
     }
 
     ASSERT(!m_layoutState);
+    if (!needsLayout())
+        return;
+
     LayoutState state;
     // FIXME: May be better to push a clip and avoid issuing offscreen repaints.
     state.m_clipped = false;
@@ -163,15 +188,23 @@ void RenderView::layout()
     m_pageLogicalHeightChanged = false;
     m_layoutState = &state;
 
-    if (needsLayout()) {
-        RenderBlock::layout();
-        if (hasRenderNamedFlowThreads())
-            flowThreadController()->layoutRenderNamedFlowThreads();
+    m_layoutPhase = RenderViewNormalLayout;
+    bool needsTwoPassLayoutForAutoLogicalHeightRegions = hasRenderNamedFlowThreads() && flowThreadController()->hasAutoLogicalHeightRegions();
+
+    if (needsTwoPassLayoutForAutoLogicalHeightRegions)
+        flowThreadController()->resetRegionsOverrideLogicalContentHeight();
+
+    layoutContent(state);
+
+    if (needsTwoPassLayoutForAutoLogicalHeightRegions) {
+        m_layoutPhase = ConstrainedFlowThreadsLayoutInAutoLogicalHeightRegions;
+        flowThreadController()->markAutoLogicalHeightRegionsForLayout();
+        layoutContent(state);
     }
 
-    ASSERT(layoutDelta() == LayoutSize());
-    ASSERT(m_layoutStateDisableCount == 0);
-    ASSERT(m_layoutState == &state);
+#ifndef NDEBUG
+    checkLayoutState(state);
+#endif
     m_layoutState = 0;
     setNeedsLayout(false);
 }

LayoutTests/fast/regions/autoheight-abspos-region-expected.html
@@ -1,0 +1,15 @@
+<!doctype html>
+<html>
+    <head>
+        <style>
+            .expectedRegionSize  { width: 50px; height: 50px; }
+            #green { background-color: green; position: absolute; top: 100px; }
+        </style>
+    </head>
+    <body>
+        <p>Test for <a href="https://bugs.webkit.org/show_bug.cgi?id=91097"> Auto height is not working for regions</a>.</p>
+        <p>The region is a block element, absolutely positioned.</p>
+        <p>On success, you should not see a red rectangle below.</p>
+        <div id="green" class="expectedRegionSize"></div>
+    <body>
+</html>

LayoutTests/fast/regions/autoheight-abspos-region.html
@@ -1,0 +1,23 @@
+<!doctype html>
+<html>
+    <head>
+        <style>
+            .flowContentSize  { width: 50px; height: 50px; }
+            #article { -webkit-flow-into: flow; background-color: green; }
+
+            .expectedRegionSize  { width: 50px; height: 50px; }
+            #red { background-color: red; position: absolute; top: 100px; }
+
+            .regionSize { width: 50px; }
+            #region { -webkit-flow-from: flow; position: absolute; top: 100px; }
+        </style>
+    </head>
+    <body>
+        <p>Test for <a href="https://bugs.webkit.org/show_bug.cgi?id=91097"> Auto height is not working for regions</a>.</p>
+        <p>The region is a block element, absolutely positioned.</p>
+        <p>On success, you should not see a red rectangle below.</p>
+        <div id="red" class="expectedRegionSize"></div>
+        <div id="article" class="flowContentSize"></div>
+        <div id="region" class="regionSize"></div>
+    </body>
+</html>

LayoutTests/fast/regions/autoheight-floatright-region-expected.html
@@ -1,0 +1,16 @@
+<!doctype html>
+<html>
+    <head>
+        <style>
+            body { margin: 10px; }
+            .expectedRegionSize { width: 50px; height: 50px; }
+            #green { background-color: green; position: absolute; top: 150px; left: 60px; }
+        </style>
+    </head>
+    <body>
+        <p>Test for <a href="https://bugs.webkit.org/show_bug.cgi?id=91097"> Auto height is not working for regions</a>.</p>
+        <p>The region is a block element, right floating.</p>
+        <p>On success, you should not see a red rectangle below.</p>
+        <div id="green" class="expectedRegionSize"></div>
+    </body>
+</html>

LayoutTests/fast/regions/autoheight-floatright-region.html
@@ -1,0 +1,28 @@
+<!doctype html>
+<html>
+    <head>
+        <style>
+            body { margin: 10px; }
+
+            .flowContentSize { width: 50px; height: 50px; }
+            #article { -webkit-flow-into: flow;  background-color: green; }
+
+            .expectedRegionSize { width: 50px; height: 50px; }
+            #red { background-color: red; position: absolute; top: 150px; left: 60px; }
+
+            .regionContainerSize { width: 100px; height: 100px; }
+            #container { position: absolute;  top: 150px; }
+            #region { -webkit-flow-from: flow; width: 50px; float: right; }
+        </style>
+    </head>
+    <body>
+        <p>Test for <a href="https://bugs.webkit.org/show_bug.cgi?id=91097"> Auto height is not working for regions</a>.</p>
+        <p>The region is a block element, right floating.</p>
+        <p>On success, you should not see a red rectangle below.</p>
+        <div id="red" class="expectedRegionSize"></div>
+        <div id="article" class="flowContentSize"></div>
+        <div id="container" class="regionContainerSize">
+            <div id="region"></div>
+        </div>
+    </body>
+</html>

LayoutTests/fast/regions/autoheight-horizontal-bt-expected.html
@@ -1,0 +1,19 @@
+<!doctype html>
+<html style="-webkit-writing-mode: horizontal-bt;">
+    <head>
+        <style>
+            .greenBox { position: absolute; width: 250px; height: 50px; background-color: green; }
+            #green { bottom: 125px; }
+            #green1 { bottom: 225px; }
+            #green2 { bottom: 325px; }
+        </style>
+    </head>
+    <body>
+        <p>Test for <a href="https://bugs.webkit.org/show_bug.cgi?id=91097"> Auto height is not working for regions</a>.</p>
+        <p>The region is a block element, writing mode horizontal-bt.</p>
+        <p>On success, you should see 3 green rectangles.</p>
+        <div id="green" class="greenBox"></div>
+        <div id="green1" class="greenBox"></div>
+        <div id="green2" class="greenBox"></div>
+    </body>
+</html>

LayoutTests/fast/regions/autoheight-horizontal-bt.html
@@ -1,0 +1,31 @@
+<!doctype html>
+<html style="-webkit-writing-mode: horizontal-bt;">
+    <head>
+        <style>
+            .flowContent { font-size: 50px; font-family: Ahem; line-height: 2; color: green; }
+            #article { -webkit-flow-into: flow; }
+
+            .regionSize { width: 250px; }
+            #region { -webkit-flow-from: flow; position: absolute; bottom: 100px; }
+
+            .redBox { position: absolute; width: 250px; height: 50px; background-color: red; }
+            #red { bottom: 125px; }
+            #red1 { bottom: 225px; }
+            #red2 { bottom: 325px; }
+        </style>
+    </head>
+    <body>
+        <p>Test for <a href="https://bugs.webkit.org/show_bug.cgi?id=91097"> Auto height is not working for regions</a>.</p>
+        <p>The region is a block element, writing mode horizontal-bt.</p>
+        <p>On success, you should see 3 green rectangles.</p>
+        <div id="red" class="redBox"></div>
+        <div id="red1" class="redBox"></div>
+        <div id="red2" class="redBox"></div>
+        <div id="article" class="flowContent">
+            <span style="background-color: green">
+                a a a a a a a a a
+            </span>
+        </div>
+        <div id="region" class="regionSize"></div>
+    </body>
+</html>

LayoutTests/fast/regions/autoheight-inlineblock-normalflow-region-expected.html
@@ -1,0 +1,15 @@
+<!doctype html>
+<html>
+    <head>
+        <style>
+            .expectedRegionSize { width: 50px; height: 50px; }
+            #green { background-color: green; }
+        </style>
+    </head>
+    <body>
+        <p>Test for <a href="https://bugs.webkit.org/show_bug.cgi?id=91097"> Auto height is not working for regions</a>.</p>
+        <p>The region is an inline-block element in normal flow</p>
+        <p>On success, you should not see a red rectangle below.</p>
+        <div id="green" class="expectedRegionSize"></div>
+    <body>
+</html>

commit 02b9853edd32ea9947d1b4e2f03d433ba5d1a0e2

Fix build broken by r131348. https://bugs.webkit.org/show_bug.cgi?id=97533.
Not reviewed.

* rendering/RenderView.cpp:
(WebCore::RenderView::layoutContent):



git-svn-id: http://svn.webkit.org/repository/webkit/trunk@131363 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Source/WebCore/rendering/RenderView.cpp
@@ -134,6 +134,7 @@ bool RenderView::isChildAllowed(RenderObject* child, RenderStyle*) const
 
 void RenderView::layoutContent(const LayoutState& state)
 {
+    UNUSED_PARAM(state);
     ASSERT(needsLayout());
 
     RenderBlock::layout();