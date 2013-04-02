WebKit/WebKit-http

commit 2b307a73ffcd5ec909dd0914226cff4a9365471d

Inline Containing Only Collapsed Whitespace Not Getting a Linebox https://bugs.webkit.org/show_bug.cgi?id=31397
Reviewed by David Hyatt.

Source/WebCore:

Treat inlines containing only collapsed whitespace as empty. This allows them
to get a linebox.

Test: fast/inline/inline-containing-collapsed-whitespace-treated-as-empty.html

* dom/Position.cpp:
(WebCore::boundingBoxLogicalHeight):
(WebCore):
(WebCore::Position::hasRenderedNonAnonymousDescendantsWithHeight):
* rendering/InlineIterator.h:
(WebCore::isEmptyInline):
(WebCore):
(WebCore::bidiNextShared):
(WebCore::bidiFirstSkippingEmptyInlines):
* rendering/RenderBlockLineLayout.cpp:
(WebCore::alwaysRequiresLineBox):
(WebCore::requiresLineBox):
(WebCore::RenderBlock::LineBreaker::nextSegmentBreak):
* rendering/RenderText.cpp:
* rendering/RenderText.h:
(RenderText):

LayoutTests:

* fast/css/first-letter-capitalized-edit-select-crash-expected.txt:
* fast/inline/inline-containing-collapsed-whitespace-treated-as-empty-expected.html: Added.
* fast/inline/inline-containing-collapsed-whitespace-treated-as-empty-vertical-rl-expected.html: Added.
* fast/inline/inline-containing-collapsed-whitespace-treated-as-empty-vertical-rl.html: Added.
* fast/inline/inline-containing-collapsed-whitespace-treated-as-empty.html: Added.
* platform/chromium/TestExpectations:
* platform/efl/TestExpectations:
* platform/gtk/TestExpectations:
* platform/mac/TestExpectations:
* platform/qt/TestExpectations:

git-svn-id: http://svn.webkit.org/repository/webkit/trunk@140693 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Source/WebCore/rendering/RenderBlockLineLayout.cpp
@@ -2212,12 +2212,12 @@ static bool requiresLineBoxForContent(RenderInline* flow, const LineInfo& lineIn
     return false;
 }
 
-static bool alwaysRequiresLineBox(RenderInline* flow)
+static bool alwaysRequiresLineBox(RenderObject* flow)
 {
     // FIXME: Right now, we only allow line boxes for inlines that are truly empty.
     // We need to fix this, though, because at the very least, inlines containing only
     // ignorable whitespace should should also have line boxes.
-    return !flow->firstChild() && flow->hasInlineDirectionBordersPaddingOrMargin();
+    return isEmptyInline(flow) && toRenderInline(flow)->hasInlineDirectionBordersPaddingOrMargin();
 }
 
 static bool requiresLineBox(const InlineIterator& it, const LineInfo& lineInfo = LineInfo(), WhitespacePosition whitespacePosition = LeadingWhitespace)
@@ -2225,15 +2225,15 @@ static bool requiresLineBox(const InlineIterator& it, const LineInfo& lineInfo =
     if (it.m_obj->isFloatingOrOutOfFlowPositioned())
         return false;
 
-    if (it.m_obj->isRenderInline() && !alwaysRequiresLineBox(toRenderInline(it.m_obj)) && !requiresLineBoxForContent(toRenderInline(it.m_obj), lineInfo))
+    if (it.m_obj->isRenderInline() && !alwaysRequiresLineBox(it.m_obj) && !requiresLineBoxForContent(toRenderInline(it.m_obj), lineInfo))
         return false;
 
     if (!shouldCollapseWhiteSpace(it.m_obj->style(), lineInfo, whitespacePosition) || it.m_obj->isBR())
         return true;
 
     UChar current = it.current();
-    return current != ' ' && current != '\t' && current != softHyphen && (current != '\n' || it.m_obj->preservesNewline())
-    && !skipNonBreakingSpace(it, lineInfo);
+    bool notJustWhitespace = current != ' ' && current != '\t' && current != softHyphen && (current != '\n' || it.m_obj->preservesNewline()) && !skipNonBreakingSpace(it, lineInfo);
+    return notJustWhitespace || isEmptyInline(it.m_obj);
 }
 
 bool RenderBlock::generatesLineBoxesForInlineChild(RenderObject* inlineObj)
@@ -2672,7 +2672,7 @@ InlineIterator RenderBlock::LineBreaker::nextSegmentBreak(InlineBidiResolver& re
                 floatsFitOnLine = false;
         } else if (current.m_obj->isRenderInline()) {
             // Right now, we should only encounter empty inlines here.
-            ASSERT(!current.m_obj->firstChild());
+            ASSERT(isEmptyInline(current.m_obj));
 
             RenderInline* flowBox = toRenderInline(current.m_obj);
 
@@ -2680,7 +2680,7 @@ InlineIterator RenderBlock::LineBreaker::nextSegmentBreak(InlineBidiResolver& re
             // to make sure that we stop to include this object and then start ignoring spaces again.
             // If this object is at the start of the line, we need to behave like list markers and
             // start ignoring spaces.
-            bool requiresLineBox = alwaysRequiresLineBox(flowBox);
+            bool requiresLineBox = alwaysRequiresLineBox(current.m_obj);
             if (requiresLineBox || requiresLineBoxForContent(flowBox, lineInfo)) {
                 // An empty inline that only has line-height, vertical-align or font-metrics will only get a
                 // line box to affect the height of the line if the rest of the line is not empty.

LayoutTests/fast/css/first-letter-capitalized-edit-select-crash-expected.txt
@@ -1,3 +1,3 @@
 Pass if no crash or assert in debug
-
+ 
 

LayoutTests/fast/inline/inline-containing-collapsed-whitespace-treated-as-empty-expected.html
@@ -1,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<body>
+<p> https://bugs.webkit.org/show_bug.cgi?id=31397: You should see two orange-filled boxes below. </p>
+<a style="padding:5px; border:1px solid silver; background:gold;" href="#"/></a>
+<a style="padding:5px; border:1px solid silver; background:gold;" href="#"/></a>
+</body>
+</html>
+

LayoutTests/fast/inline/inline-containing-collapsed-whitespace-treated-as-empty-vertical-rl-expected.html
@@ -1,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<body style="-webkit-writing-mode: vertical-rl;">
+<p> https://bugs.webkit.org/show_bug.cgi?id=31397: You should see two orange-filled boxes below. </p>
+<a style="padding:5px; border:1px solid silver; background:gold; " href="#"/></a>
+<a style="padding:5px; border:1px solid silver; background:gold;" href="#"/></a>
+</body>
+</html>
+

LayoutTests/fast/inline/inline-containing-collapsed-whitespace-treated-as-empty-vertical-rl.html
@@ -1,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<body style="-webkit-writing-mode: vertical-rl;">
+<p> https://bugs.webkit.org/show_bug.cgi?id=31397: You should see two orange-filled boxes below. </p>
+<a style="padding:5px; border:1px solid silver; background:gold; " href="#"/> </a>
+<a style="padding:5px; border:1px solid silver; background:gold;" href="#"/> </a>
+</body>
+</html>
+

LayoutTests/fast/inline/inline-containing-collapsed-whitespace-treated-as-empty.html
@@ -1,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<body>
+<p> https://bugs.webkit.org/show_bug.cgi?id=31397: You should see two orange-filled boxes below. </p>
+<a style="padding:5px; border:1px solid silver; background:gold;" href="#"/> </a>
+<a style="padding:5px; border:1px solid silver; background:gold;" href="#"/> </a>
+</body>
+</html>
+

LayoutTests/platform/chromium/TestExpectations
@@ -4333,3 +4333,16 @@ webkit.org/b/107341 [ Debug ] inspector/profiler/memory-instrumentation-cached-i
 webkit.org/b/107579 [ Debug ] media/track/track-css-matching.html [ Crash ]
 webkit.org/b/107579 [ Debug ] media/track/track-css-property-whitelist.html [ Crash ]
 
+# Rebaseline required after https://webkit.org/b/31397
+webkit.org/b/107567 svg/batik/text/xmlSpace.svg [ Failure ] 
+webkit.org/b/107567 fast/text/capitalize-empty-generated-string.html [ Failure ] 
+webkit.org/b/107567 fast/text/whitespace/007.html [ Failure ] 
+webkit.org/b/107567 fast/text/whitespace/006.html [ Failure ] 
+webkit.org/b/107567 fast/inline/drawStyledEmptyInlinesWithWS.html [ Failure ] 
+webkit.org/b/107567 tables/mozilla/bugs/bug1318.html [ Failure ] 
+webkit.org/b/107567 fast/inline/drawStyledEmptyInlines.html [ Failure ] 
+webkit.org/b/107567 tables/mozilla/bugs/bug113235-3.html [ Failure ] 
+webkit.org/b/107567 css2.1/t0505-c16-descendant-01-e.html [ Failure ] 
+webkit.org/b/107567 tables/mozilla/bugs/bug1188.html [ Failure ] 
+webkit.org/b/107567 editing/selection/extend-by-sentence-001.html [ Failure ] 
+webkit.org/b/107567 svg/carto.net/combobox.svg [ Failure ] 

LayoutTests/platform/efl/TestExpectations
@@ -1815,3 +1815,17 @@ webkit.org/b/107375 fast/css/sticky/sticky-top-zoomed.html [ ImageOnlyFailure ]
 # Needs rebaseline after https://bugs.webkit.org/show_bug.cgi?id=14664
 webkit.org/b/107476 fast/block/float/024.html [ Failure ]
 webkit.org/b/107476 fast/block/margin-collapse/empty-clear-blocks.html [ Failure ]
+
+# Rebaseline required after https://webkit.org/b/31397
+webkit.org/b/107567 svg/batik/text/xmlSpace.svg [ Failure ] 
+webkit.org/b/107567 fast/text/capitalize-empty-generated-string.html [ Failure ] 
+webkit.org/b/107567 fast/text/whitespace/007.html [ Failure ] 
+webkit.org/b/107567 fast/text/whitespace/006.html [ Failure ] 
+webkit.org/b/107567 fast/inline/drawStyledEmptyInlinesWithWS.html [ Failure ] 
+webkit.org/b/107567 tables/mozilla/bugs/bug1318.html [ Failure ] 
+webkit.org/b/107567 fast/inline/drawStyledEmptyInlines.html [ Failure ] 
+webkit.org/b/107567 tables/mozilla/bugs/bug113235-3.html [ Failure ] 
+webkit.org/b/107567 css2.1/t0505-c16-descendant-01-e.html [ Failure ] 
+webkit.org/b/107567 tables/mozilla/bugs/bug1188.html [ Failure ] 
+webkit.org/b/107567 editing/selection/extend-by-sentence-001.html [ Failure ] 
+webkit.org/b/107567 svg/carto.net/combobox.svg [ Failure ] 

commit 039c76add5fc5c23fd3b4005d698b848c6d2b7b2

Padding applied twice for empty generated RenderInlines https://bugs.webkit.org/show_bug.cgi?id=113800
Source/WebCore:

Reviewed by Levi Weintraub.

The padding for empty generated content render objects is
incorrectly applied twice. Regression from r140693.

Test: fast/css-generated-content/margin-empty-content.html

* rendering/RenderBlockLineLayout.cpp:
(WebCore::inlineLogicalWidth):

LayoutTests:

Reviewed by Levi Weintraub.

Add test for empty generated content with padding.

* fast/css-generated-content/margin-empty-content-expected.html: Added.
* fast/css-generated-content/margin-empty-content.html: Added.


git-svn-id: http://svn.webkit.org/repository/webkit/trunk@147505 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Source/WebCore/rendering/RenderBlockLineLayout.cpp
@@ -322,12 +322,14 @@ static LayoutUnit inlineLogicalWidth(RenderObject* child, bool start = true, boo
     RenderObject* parent = child->parent();
     while (parent->isRenderInline() && lineDepth++ < cMaxLineDepth) {
         RenderInline* parentAsRenderInline = toRenderInline(parent);
-        if (start && shouldAddBorderPaddingMargin(child->previousSibling(), start))
-            extraWidth += borderPaddingMarginStart(parentAsRenderInline);
-        if (end && shouldAddBorderPaddingMargin(child->nextSibling(), end))
-            extraWidth += borderPaddingMarginEnd(parentAsRenderInline);
-        if (!start && !end)
-            return extraWidth;
+        if (!isEmptyInline(parentAsRenderInline)) {
+            if (start && shouldAddBorderPaddingMargin(child->previousSibling(), start))
+                extraWidth += borderPaddingMarginStart(parentAsRenderInline);
+            if (end && shouldAddBorderPaddingMargin(child->nextSibling(), end))
+                extraWidth += borderPaddingMarginEnd(parentAsRenderInline);
+            if (!start && !end)
+                return extraWidth;
+        }
         child = parent;
         parent = child->parent();
     }

LayoutTests/fast/css-generated-content/margin-empty-content-expected.html
@@ -1,0 +1,20 @@
+<html>
+    <head>
+        <style>
+            ul {
+                list-style-type: none;
+            }
+            li {
+                display: inline-block;
+            }
+            li:after {
+                margin: 0 0 0 1px;
+            }
+        </style>
+    </head>
+    <body>
+        <ul>
+            <li>Should not wrap</li>
+        </ul>
+    </body>
+</html>

LayoutTests/fast/css-generated-content/margin-empty-content.html
@@ -1,0 +1,21 @@
+<html>
+    <head>
+        <style>
+            ul {
+                list-style-type: none;
+            }
+            li {
+                display: inline-block;
+            }
+            li:after {
+                margin: 0 0 0 1px;
+                content: "";
+            }
+        </style>
+    </head>
+    <body>
+        <ul>
+            <li>Should not wrap</li>
+        </ul>
+    </body>
+</html>