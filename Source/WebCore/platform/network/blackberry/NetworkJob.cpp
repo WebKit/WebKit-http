WebKit/WebKit-http

commit 216f5d7c406d4ef32eb02b80afb63cf5ccd2dc05

[BlackBerry] Infinite redirect loop is not displayed to user https://bugs.webkit.org/show_bug.cgi?id=100420
Reviewed by George Staikos.

CNN.com is redirecting us infinitely with 302 http code and response body. We catch
it my setting our extended error code as we defined. But we were not trying to notify
error because we have received body. This was wrong. For extended http status code
which is less than 0 we should display our own error page regardless there was data
received from the server or not.

Tested by manually loading CNN.com.

* platform/network/blackberry/NetworkJob.cpp:
(WebCore::NetworkJob::shouldNotifyClientFailed):


git-svn-id: http://svn.webkit.org/repository/webkit/trunk@132556 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Source/WebCore/platform/network/blackberry/NetworkJob.cpp
@@ -538,9 +538,7 @@ bool NetworkJob::shouldReleaseClientResource()
 
 bool NetworkJob::shouldNotifyClientFailed() const
 {
-    if (m_handle->firstRequest().targetType() == ResourceRequest::TargetIsXHR)
-        return m_extendedStatusCode < 0;
-    return isError(m_extendedStatusCode) && !m_dataReceived;
+    return m_extendedStatusCode < 0 || (isError(m_extendedStatusCode) && !m_dataReceived);
 }
 
 bool NetworkJob::retryAsFTPDirectory()

commit 2969f8bd3758c5bc1ae4c10397dcc46110338d1f

[BlackBerry] Reveal HTTP error code for XHR even if there is no response body https://bugs.webkit.org/show_bug.cgi?id=116543
Reviewed by Rob Buis.
Reviewed internally by Joe Mason.

It's a regression introduced by r132556.
BlackBerry is doing friendly HTTP error handling: if there is no response body
for an HTTP error it will display an error message made by the port. But for a
XHR request, which usually does not result in displaying error messages to the
end user, HTTP error code should be revealed to let the JavaScript know what
the actual server error code is.

Covered by existing tests:
ManualTests/blackberry/head-xhr-nonexistant-file.html
LayoutTests/http/tests/xmlhttprequest/xmlhttprequest-check-head-readystate-for-404.html
LayoutTests/http/tests/xmlhttprequest/xmlhttprequest-check-get-readystate-for-404-without-body.html

* platform/network/blackberry/NetworkJob.cpp:
(WebCore::NetworkJob::shouldNotifyClientFailed):


git-svn-id: http://svn.webkit.org/repository/webkit/trunk@150759 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Source/WebCore/platform/network/blackberry/NetworkJob.cpp
@@ -601,7 +601,7 @@ bool NetworkJob::shouldReleaseClientResource()
 
 bool NetworkJob::shouldNotifyClientFailed() const
 {
-    return m_extendedStatusCode < 0 || (isError(m_extendedStatusCode) && !m_dataReceived && !m_isHeadMethod);
+    return m_extendedStatusCode < 0 || (isError(m_extendedStatusCode) && !m_dataReceived && !m_isHeadMethod && m_handle->firstRequest().targetType() != ResourceRequest::TargetIsXHR);
 }
 
 bool NetworkJob::retryAsFTPDirectory()