WebKit/WebKit-http

commit 9bcbd6ece50563a6716175e570bddcfd34cddc65

[GTK] Make DRT obbey testRunner's addURLToRedirect https://bugs.webkit.org/show_bug.cgi?id=118239
Patch by Simon Pena <simon.pena@samsung.com> on 2013-07-17
Reviewed by Gustavo Noronha Silva.

Tools:

Support testRunner's addURLToRedirect in GTK's DRT, so it redirects
to the proper URI when indicated.

* DumpRenderTree/gtk/DumpRenderTree.cpp:
(willSendRequestCallback):

LayoutTests:

Unskip http/tests/loading/cross-origin-XHR-willLoadRequest.html now that DRT obbeys
testRunner's addURLToRedirect.

* platform/gtk/TestExpectations:

git-svn-id: http://svn.webkit.org/repository/webkit/trunk@152782 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Tools/DumpRenderTree/gtk/DumpRenderTree.cpp
@@ -1288,17 +1288,22 @@ static void willSendRequestCallback(WebKitWebView* webView, WebKitWebFrame* webF
 
     SoupMessage* soupMessage = webkit_network_request_get_message(request);
     SoupURI* uri = soup_uri_new(webkit_network_request_get_uri(request));
+    GOwnPtr<char> uriString(soup_uri_to_string(uri, FALSE));
 
     if (SOUP_URI_VALID_FOR_HTTP(uri) && g_strcmp0(uri->host, "127.0.0.1")
         && g_strcmp0(uri->host, "255.255.255.255")
         && g_ascii_strncasecmp(uri->host, "localhost", 9)) {
-        printf("Blocked access to external URL %s\n", soup_uri_to_string(uri, FALSE));
+        printf("Blocked access to external URL %s\n", uriString.get());
         // Cancel load of blocked resource to avoid potential
         // network-related timeouts in tests.
         webkit_network_request_set_uri(request, "about:blank");
         soup_uri_free(uri);
         return;
     }
+
+    const string& destination = gTestRunner->redirectionDestinationForURL(uriString.get());
+    if (!destination.empty())
+        webkit_network_request_set_uri(request, destination.c_str());
 
     if (uri)
         soup_uri_free(uri);

LayoutTests/platform/gtk/TestExpectations
@@ -1063,9 +1063,6 @@ Bug(GTK) fast/frames/meta-refresh-user-gesture.html [ Failure Timeout ]
 # Rounding color problems inside pixman
 webkit.org/b/49964 fast/canvas/canvas-fillPath-shadow.html [ Failure ]
 
-# DRT does not obey testRunner.addURLToRedirect()
-Bug(GTK) http/tests/loading/cross-origin-XHR-willLoadRequest.html [ Failure ]
-
 # See also https://bugs.webkit.org/show_bug.cgi?id=54926
 webkit.org/b/53707 http/tests/security/xss-DENIED-xsl-document-redirect.xml [ Failure ]
 webkit.org/b/53707 http/tests/security/xss-DENIED-xsl-external-entity-redirect.xml [ Failure ]

commit 38a481590748baef62a96d7a9a265ca83272d2db

[GTK] Fix regression in DumpRenderTree introduced in r152782 by checking if uri is valid https://bugs.webkit.org/show_bug.cgi?id=118895
Patch by Simon Pena <simon.pena@samsung.com> on 2013-07-19
Reviewed by Philippe Normand.

In r152782 an uri is used to create an string representation without checking first if it
is valid. That fails later when the string is used to check for existing redirections. This
patch ensures that the uri is valid before using it.

* DumpRenderTree/gtk/DumpRenderTree.cpp:
(willSendRequestCallback): Ensure an uri is valid before using it.

git-svn-id: http://svn.webkit.org/repository/webkit/trunk@152897 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Tools/DumpRenderTree/gtk/DumpRenderTree.cpp
@@ -1288,22 +1288,25 @@ static void willSendRequestCallback(WebKitWebView* webView, WebKitWebFrame* webF
 
     SoupMessage* soupMessage = webkit_network_request_get_message(request);
     SoupURI* uri = soup_uri_new(webkit_network_request_get_uri(request));
-    GOwnPtr<char> uriString(soup_uri_to_string(uri, FALSE));
 
-    if (SOUP_URI_VALID_FOR_HTTP(uri) && g_strcmp0(uri->host, "127.0.0.1")
-        && g_strcmp0(uri->host, "255.255.255.255")
-        && g_ascii_strncasecmp(uri->host, "localhost", 9)) {
-        printf("Blocked access to external URL %s\n", uriString.get());
-        // Cancel load of blocked resource to avoid potential
-        // network-related timeouts in tests.
-        webkit_network_request_set_uri(request, "about:blank");
-        soup_uri_free(uri);
-        return;
+    if (SOUP_URI_IS_VALID(uri)) {
+        GOwnPtr<char> uriString(soup_uri_to_string(uri, FALSE));
+
+        if (SOUP_URI_VALID_FOR_HTTP(uri) && g_strcmp0(uri->host, "127.0.0.1")
+            && g_strcmp0(uri->host, "255.255.255.255")
+            && g_ascii_strncasecmp(uri->host, "localhost", 9)) {
+            printf("Blocked access to external URL %s\n", uriString.get());
+            // Cancel load of blocked resource to avoid potential
+            // network-related timeouts in tests.
+            webkit_network_request_set_uri(request, "about:blank");
+            soup_uri_free(uri);
+            return;
+        }
+
+        const string& destination = gTestRunner->redirectionDestinationForURL(uriString.get());
+        if (!destination.empty())
+            webkit_network_request_set_uri(request, destination.c_str());
     }
-
-    const string& destination = gTestRunner->redirectionDestinationForURL(uriString.get());
-    if (!destination.empty())
-        webkit_network_request_set_uri(request, destination.c_str());
 
     if (uri)
         soup_uri_free(uri);