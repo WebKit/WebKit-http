ba89bc21198b6eddb99410e8115a68b363482464
Ingo Weinhold <ingo_weinhold@gmx.de>
2013-08-10 07:04:09 +0200

    generate-build-info.sh: make optionally work outside git repo
    If the build variables WEBKIT_REVISION and HAIKU_WEBKIT_REVISION are
    set, the script will use the value of those instead of trying to pull
    the info from the git repository.

--- Tools/haiku/generate-build-info.sh
+++ Tools/haiku/generate-build-info.sh
@@ -5,15 +5,24 @@
 # Distributed under the terms of the MIT License.
 #
 
-CURENT_HASH=`git log --pretty=format:'%h' -n 1`
+# Usage: generate-build-info.sh <source top dir>
+#			[ <webkit revision> <haiku webkit revision> ]
 
-MERGE_HASH=`git log --pretty=format:'%H' --date-order --merges -n 1`
-REV_1=`git log $MERGE_HASH^1 -n 1 | perl -nle '/webkit\.org.+trunk@(\d+)/ and print $1'`
-REV_2=`git log $MERGE_HASH^2 -n 1 | perl -nle '/webkit\.org.+trunk@(\d+)/ and print $1'`
+
+if [ $# -gt 2 ]; then
+	WEBKIT_REVISION=$2
+	HAIKU_WEBKIT_REVISION=$3
+else
+	MERGE_HASH=`git log --pretty=format:'%H' --date-order --merges -n 1`
+	REV_1=`git log $MERGE_HASH^1 -n 1 | perl -nle '/webkit\.org.+trunk@(\d+)/ and print $1'`
+	REV_2=`git log $MERGE_HASH^2 -n 1 | perl -nle '/webkit\.org.+trunk@(\d+)/ and print $1'`
+	WEBKIT_REVISION=r$REV_1$REV_2
+	HAIKU_WEBKIT_REVISION=`git log --pretty=format:'%h' -n 1`
+fi
 
 HAIKU_WEBKIT_VERSION=1.1
 
-TOP=`git rev-parse --show-toplevel`
+TOP="$1"
 WEBKIT_MAJOR=`cat $TOP/Source/WebKit/mac/Configurations/Version.xcconfig | perl -nle '/MAJOR_VERSION = (\d+);/ and print $1'`
 WEBKIT_MINOR=`cat $TOP/Source/WebKit/mac/Configurations/Version.xcconfig | perl -nle '/MINOR_VERSION = (\d+);/ and print $1'`
 
@@ -23,10 +32,10 @@ echo '/*
 #ifndef BuildInfo_h
 #define BuildInfo_h
 
-#define HAIKU_WEBKIT_REVISION "'$CURENT_HASH'"
+#define HAIKU_WEBKIT_REVISION "'$HAIKU_WEBKIT_REVISION'"
 #define HAIKU_WEBKIT_VERSION "'$HAIKU_WEBKIT_VERSION'"
 
-#define WEBKIT_REVISION "r'$REV_1$REV_2'"
+#define WEBKIT_REVISION "'$WEBKIT_REVISION'"
 #define WEBKIT_MAJOR_VERSION '$WEBKIT_MAJOR'
 #define WEBKIT_MINOR_VERSION '$WEBKIT_MINOR'
 
