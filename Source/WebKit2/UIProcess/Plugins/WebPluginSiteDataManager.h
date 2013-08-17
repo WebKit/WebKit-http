WebKit/WebKit-http

commit e564e9672b9eca92cf1a0207f05bf82451024393

2011-02-15  Anders Carlsson  <andersca@apple.com>
        Reviewed by Jon Honeycutt.

        Implement WKPluginSiteDataManagerGetSitesWithData
        https://bugs.webkit.org/show_bug.cgi?id=54510
        <rdar://problem/8689312>

        * UIProcess/API/C/WKContext.cpp:
        (WKContextGetPluginSiteDataManager):
        Call WebContext::pluginSiteDataManager.
        
        * UIProcess/API/C/WKPluginSiteDataManager.cpp:
        (WKPluginSiteDataManagerGetSitesWithData):
        Call WebPluginSiteDataManager::getSitesWithData.

        * UIProcess/Plugins/PluginInfoStore.cpp:
        (WebKit::PluginInfoStore::getPluginPaths):
        * UIProcess/Plugins/PluginInfoStore.h:
        Populate the paths argument with the paths of all plug-ins.
        
        * UIProcess/Plugins/WebPluginSiteDataManager.cpp: Added.
        (WebKit::WebPluginSiteDataManager::invalidate):
        Invalidate the callbacks map.

        (WebKit::WebPluginSiteDataManager::getSitesWithData):
        Ask the plug-in info store for all plug-in paths and send a
        GetSitesWithPluginData message to the web process.

        (WebKit::WebPluginSiteDataManager::didGetSitesWithPluginData):
        Call the callback.

        * UIProcess/Plugins/WebPluginSiteDataManager.h: Added.
        (WebKit::WebPluginSiteDataManager::clearContext):
        Clear the context.

        * UIProcess/WebContext.cpp:
        (WebKit::WebContext::WebContext):
        Initialize m_pluginSiteDataManager.

        (WebKit::WebContext::~WebContext):
        Invalidate the plug-in site data manager and clear its context.

        (WebKit::WebContext::processDidClose):
        Invalidate the plug-in site data manager.
        
        (WebKit::WebContext::didGetSitesWithPluginData):
        Call through to the plug-in site data manager.

        * UIProcess/WebContext.h:
        * UIProcess/WebContext.messages.in:
        Add GetSitesWithPluginData message.

        * WebKit2.pro:

Source/WebKit2/UIProcess/Plugins/WebPluginSiteDataManager.h
@@ -23,32 +23,49 @@
  * THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-#include "config.h"
-#include "WKPluginSiteDataManager.h"
+#ifndef WebPluginSiteDataManagerh
+#define WebPluginSiteDataManager_h
 
-#include "WKAPICast.h"
-#include "WebPluginSiteDataManager.h"
-#include <WebCore/npapi.h>
+#include "APIObject.h"
+#include "Arguments.h"
+#include "GenericCallback.h"
+#include <wtf/HashMap.h>
+#include <wtf/PassRefPtr.h>
 
-using namespace WebKit;
-
-WKTypeID WKPluginSiteDataManagerGetTypeID()
-{
-    // FIXME: Implement.
-    return 0;
+namespace CoreIPC {
+class ArgumentDecoder;
+class Connection;
+class MessageID;
 }
 
-void WKPluginSiteDataManagerGetSitesWithData(WKPluginSiteDataManagerRef managerRef, void* context, WKPluginSiteDataManagerGetSitesWithDataFunction callback)
-{
-    toImpl(managerRef)->getSitesWithData(ArrayCallback::create(context, callback));
-}
+namespace WebKit {
 
-void WKPluginSiteDataManagerClearSiteData(WKPluginSiteDataManagerRef manager, WKArrayRef sites, WKClearSiteDataFlags flags, uint64_t maxAgeInSeconds)
-{
-    // FIXME: Implement.
-}
+class WebContext;
 
-void WKPluginSiteDataManagerClearAllSiteData(WKPluginSiteDataManagerRef manager)
-{
-    // FIXME: Implement.
-}
+typedef GenericCallback<WKArrayRef> ArrayCallback;
+
+class WebPluginSiteDataManager : public APIObject {
+public:
+    static const Type APIType = TypePluginSiteDataManager;
+
+    static PassRefPtr<WebPluginSiteDataManager> create(WebContext*);
+    virtual ~WebPluginSiteDataManager();
+
+    void invalidate();
+    void clearContext() { m_webContext = 0; }
+
+    void getSitesWithData(PassRefPtr<ArrayCallback>);
+    void didGetSitesWithPluginData(const Vector<String>& sites, uint64_t callbackID);
+
+private:
+    explicit WebPluginSiteDataManager(WebContext*);
+
+    virtual Type type() const { return APIType; }
+
+    WebContext* m_webContext;
+    HashMap<uint64_t, RefPtr<ArrayCallback> > m_arrayCallbacks;
+};
+
+} // namespace WebKit
+
+#endif // WebPluginSiteDataManager_h

Source/WebKit2/UIProcess/API/C/WKContext.cpp
@@ -170,8 +170,7 @@ WKGeolocationManagerRef WKContextGetGeolocationManager(WKContextRef contextRef)
 
 WKPluginSiteDataManagerRef WKContextGetPluginSiteDataManager(WKContextRef contextRef)
 {
-    // FIXME: Implement.
-    return 0;
+    return toAPI(toImpl(contextRef)->pluginSiteDataManager());
 }
 
 void WKContextStartMemorySampler(WKContextRef contextRef, WKDoubleRef interval)

Source/WebKit2/UIProcess/Plugins/PluginInfoStore.cpp
@@ -115,6 +115,14 @@ void PluginInfoStore::getPlugins(Vector<PluginInfo>& plugins)
         plugins.append(m_plugins[i].info);
 }
 
+void PluginInfoStore::getPluginPaths(Vector<String>& pluginPaths)
+{
+    loadPluginsIfNecessary();
+    
+    for (size_t i = 0; i < m_plugins.size(); ++i)
+        pluginPaths.append(m_plugins[i].path);
+}
+
 PluginInfoStore::Plugin PluginInfoStore::findPluginForMIMEType(const String& mimeType)
 {
     ASSERT(!mimeType.isNull());

Source/WebKit2/UIProcess/Plugins/PluginInfoStore.h
@@ -41,8 +41,9 @@ public:
     void setAdditionalPluginsDirectories(const Vector<String>&);
 
     void refresh();
-    void getPlugins(Vector<WebCore::PluginInfo>& plugins);
-    
+    void getPlugins(Vector<WebCore::PluginInfo>&);
+    void getPluginPaths(Vector<String>&);
+
     // Represents a single plug-in.
     struct Plugin {
         String path;

Source/WebKit2/UIProcess/Plugins/WebPluginSiteDataManager.cpp
@@ -1,0 +1,100 @@
+/*
+ * Copyright (C) 2011 Apple Inc. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY APPLE INC. AND ITS CONTRIBUTORS ``AS IS''
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL APPLE INC. OR ITS CONTRIBUTORS
+ * BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
+ * THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include "config.h"
+#include "WebPluginSiteDataManager.h"
+
+#include "ImmutableArray.h"
+#include "WebContext.h"
+#include "WebProcessMessages.h"
+
+using namespace WebCore;
+
+namespace WebKit {
+
+PassRefPtr<WebPluginSiteDataManager> WebPluginSiteDataManager::create(WebContext* webContext)
+{
+    return adoptRef(new WebPluginSiteDataManager(webContext));
+}
+
+WebPluginSiteDataManager::WebPluginSiteDataManager(WebContext* webContext)
+    : m_webContext(webContext)
+{
+}
+
+WebPluginSiteDataManager::~WebPluginSiteDataManager()
+{
+}
+
+void WebPluginSiteDataManager::invalidate()
+{
+    invalidateCallbackMap(m_arrayCallbacks);
+}
+
+void WebPluginSiteDataManager::getSitesWithData(PassRefPtr<ArrayCallback> prpCallback)
+{
+    RefPtr<ArrayCallback> callback = prpCallback;
+
+    if (!m_webContext) {
+        callback->invalidate();
+        return;
+    }
+        
+#if ENABLE(PLUGIN_PROCESS)
+    // FIXME: Implement.
+    callback->invalidate();
+#else
+    if (!m_webContext->hasValidProcess()) {
+        callback->invalidate();
+        return;
+    }
+    uint64_t callbackID = callback->callbackID();
+    m_arrayCallbacks.set(callbackID, callback.release());
+
+    Vector<String> pluginPaths;
+    m_webContext->pluginInfoStore()->getPluginPaths(pluginPaths);
+    m_webContext->process()->send(Messages::WebProcess::GetSitesWithPluginData(pluginPaths, callbackID), 0);
+#endif
+}
+
+void WebPluginSiteDataManager::didGetSitesWithPluginData(const Vector<String>& sites, uint64_t callbackID)
+{
+    RefPtr<ArrayCallback> callback = m_arrayCallbacks.take(callbackID);
+    if (!callback) {
+        // FIXME: Log error or assert.
+        return;
+    }
+
+    Vector<RefPtr<APIObject> > sitesWK(sites.size());
+
+    for (size_t i = 0; i < sites.size(); ++i)
+        sitesWK[i] = WebString::create(sites[i]);
+
+    RefPtr<ImmutableArray> resultArray = ImmutableArray::adopt(sitesWK);
+    callback->performCallbackWithReturnValue(resultArray.get());
+}
+
+} // namespace WebKit
+

Source/WebKit2/UIProcess/WebContext.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2010 Apple Inc. All rights reserved.
+ * Copyright (C) 2010, 2011 Apple Inc. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -38,6 +38,7 @@
 #include "WebCoreArgumentCoders.h"
 #include "WebDatabaseManagerProxy.h"
 #include "WebGeolocationManagerProxy.h"
+#include "WebPluginSiteDataManager.h"
 #include "WebPageGroup.h"
 #include "WebMemorySampler.h"
 #include "WebProcessCreationParameters.h"
@@ -97,6 +98,7 @@ WebContext::WebContext(ProcessModel processModel, const String& injectedBundlePa
     , m_memorySamplerInterval(1400.0)
     , m_databaseManagerProxy(WebDatabaseManagerProxy::create(this))
     , m_geolocationManagerProxy(WebGeolocationManagerProxy::create(this))
+    , m_pluginSiteDataManager(WebPluginSiteDataManager::create(this))
 #if PLATFORM(WIN)
     , m_shouldPaintNativeControls(true)
 #endif
@@ -119,6 +121,9 @@ WebContext::~WebContext()
 
     m_databaseManagerProxy->invalidate();
     m_databaseManagerProxy->clearContext();
+
+    m_pluginSiteDataManager->invalidate();
+    m_pluginSiteDataManager->clearContext();
 
 #ifndef NDEBUG
     webContextCounter.decrement();
@@ -245,6 +250,7 @@ void WebContext::processDidClose(WebProcessProxy* process)
 
     m_databaseManagerProxy->invalidate();
     m_geolocationManagerProxy->invalidate();
+    m_pluginSiteDataManager->invalidate();
 
     m_process = 0;
 }
@@ -438,6 +444,13 @@ void WebContext::getPluginPath(const String& mimeType, const String& urlString, 
 
     pluginPath = plugin.path;
 }
+
+#if !ENABLE(PLUGIN_PROCESS)
+void WebContext::didGetSitesWithPluginData(const Vector<String>& sites, uint64_t callbackID)
+{
+    m_pluginSiteDataManager->didGetSitesWithPluginData(sites, callbackID);
+}
+#endif
 
 uint64_t WebContext::createDownloadProxy()
 {

Source/WebKit2/UIProcess/WebContext.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2010 Apple Inc. All rights reserved.
+ * Copyright (C) 2010, 2011 Apple Inc. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -129,6 +129,7 @@ public:
 
     WebDatabaseManagerProxy* databaseManagerProxy() const { return m_databaseManagerProxy.get(); }
     WebGeolocationManagerProxy* geolocationManagerProxy() const { return m_geolocationManagerProxy.get(); }
+    WebPluginSiteDataManager* pluginSiteDataManager() const { return m_pluginSiteDataManager.get(); }
 
     struct Statistics {
         unsigned wkViewCount;
@@ -156,7 +157,10 @@ private:
     // Plugins
     void getPlugins(bool refresh, Vector<WebCore::PluginInfo>& plugins);
     void getPluginPath(const String& mimeType, const String& urlString, String& pluginPath);
-
+#if !ENABLE(PLUGIN_PROCESS)
+    void didGetSitesWithPluginData(const Vector<String>& sites, uint64_t callbackID);
+#endif
+        
     // Implemented in generated WebContextMessageReceiver.cpp
     void didReceiveWebContextMessage(CoreIPC::Connection*, CoreIPC::MessageID, CoreIPC::ArgumentDecoder*);
     CoreIPC::SyncReplyMode didReceiveSyncWebContextMessage(CoreIPC::Connection*, CoreIPC::MessageID, CoreIPC::ArgumentDecoder*, CoreIPC::ArgumentEncoder*);
@@ -204,6 +208,7 @@ private:
 
     RefPtr<WebDatabaseManagerProxy> m_databaseManagerProxy;
     RefPtr<WebGeolocationManagerProxy> m_geolocationManagerProxy;
+    RefPtr<WebPluginSiteDataManager> m_pluginSiteDataManager;
 
 #if PLATFORM(WIN)
     bool m_shouldPaintNativeControls;

Source/WebKit2/UIProcess/WebContext.messages.in
@@ -35,4 +35,8 @@ messages -> WebContext {
     GetPlugins(bool refresh) -> (Vector<WebCore::PluginInfo> plugins)
     GetPluginPath(WTF::String mimeType, WTF::String urlString) -> (WTF::String pluginPath)
 
+#if !ENABLE(PLUGIN_PROCESS)
+    void DidGetSitesWithPluginData(Vector<WTF::String> sites, uint64_t callbackID)
+#endif
+
 }

commit 5b42b1f5a6fbf667d4e465e139dd07f6d3713a45

WebKit2 fails to build with trunk clang: error: 'WebPluginSiteDataManagerh' is used as a header guard here, followed by #define of a different macro [-Werror,-Wheader-guard] <http://webkit.org/b/119935> <rdar://problem/14764131>
Reviewed by Darin Adler.

* UIProcess/Plugins/WebPluginSiteDataManager.h: Fix typo
introduced in r78647.


git-svn-id: http://svn.webkit.org/repository/webkit/trunk@154222 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Source/WebKit2/UIProcess/Plugins/WebPluginSiteDataManager.h
@@ -23,7 +23,7 @@
  * THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-#ifndef WebPluginSiteDataManagerh
+#ifndef WebPluginSiteDataManager_h
 #define WebPluginSiteDataManager_h
 
 #if ENABLE(NETSCAPE_PLUGIN_API)