WebKit/WebKit-http

commit b70e41bc4bcb268a83daab18d497d9aac64c694c

fourthTier: Change JSStack to grow from high to low addresses https://bugs.webkit.org/show_bug.cgi?id=118758
Reviewed by Oliver Hunt.

Changed the JSC stack to grow down.  Effectively the JSC stack frame is flipped from
what it was.  See JSStack.h for the new offsets.  Changed JSStack begin() and end()
to be getBaseOfStack() and getLimitOfStack().  Most of the changes are address or offset
calculation changes.  Decoupled a local register ordinal (loop variable or array index)
from the offset into the callFrame using localToOperand() and the inverse operandToLocal().

* assembler/MacroAssembler.h:
(JSC::MacroAssembler::trustedImm32ForShift):
(JSC::MacroAssembler::lshiftPtr): Added to create scaled addresses with a negative index
* assembler/MacroAssemblerX86_64.h:
(JSC::MacroAssemblerX86_64::lshift64): Added to create scaled addresses with a negative index
* assembler/X86Assembler.h:
(JSC::X86Assembler::shlq_i8r): Added to create scaled addresses with a negative index
* bytecode/CodeBlock.cpp:
(JSC::CodeBlock::dumpBytecode):
* bytecode/CodeBlock.h:
(JSC::unmodifiedArgumentsRegister):
(JSC::CodeBlock::isCaptured):
* bytecode/CodeOrigin.h:
(JSC::CodeOrigin::stackOffset):
* bytecode/Operands.h:
(JSC::localToOperand):
(JSC::operandIsLocal):
(JSC::operandToLocal):
(JSC::operandIsArgument):
(JSC::operandToArgument):
(JSC::argumentToOperand):
* bytecode/VirtualRegister.h: Made InvalidVirtualRegister a positive value that fits in
31 bits since it can be placed into the 31 bit field "stackOffset" in struct InlineCallFrame.
* bytecompiler/BytecodeGenerator.cpp:
(JSC::BytecodeGenerator::addVar):
(JSC::BytecodeGenerator::BytecodeGenerator):
(JSC::BytecodeGenerator::createLazyRegisterIfNecessary):
(JSC::BytecodeGenerator::newRegister):
(JSC::BytecodeGenerator::emitNewArray):
* bytecompiler/BytecodeGenerator.h:
(JSC::CallArguments::registerOffset):
* bytecompiler/NodesCodegen.cpp:
(JSC::CallArguments::CallArguments):
* dfg/DFGByteCodeParser.cpp:

Source/JavaScriptCore/ftl/FTLLink.cpp
@@ -75,10 +75,10 @@ void link(State& state)
         // Plant a check that sufficient space is available in the JSStack.
         // FIXME: https://bugs.webkit.org/show_bug.cgi?id=56291
         jit.addPtr(
-            CCallHelpers::TrustedImm32(codeBlock->m_numCalleeRegisters * sizeof(Register)),
+            CCallHelpers::TrustedImm32(-codeBlock->m_numCalleeRegisters * sizeof(Register)),
             GPRInfo::callFrameRegister, GPRInfo::regT1);
         CCallHelpers::Jump stackCheck = jit.branchPtr(
-            CCallHelpers::Below,
+            CCallHelpers::Above,
             CCallHelpers::AbsoluteAddress(state.graph.m_vm.interpreter->stack().addressOfEnd()),
             GPRInfo::regT1);
         CCallHelpers::Label fromStackCheck = jit.label();

Source/JavaScriptCore/assembler/MacroAssembler.h
@@ -208,6 +208,13 @@ public:
     }
 #endif
 
+    // Immediate shifts only have 5 controllable bits
+    // so we'll consider them safe for now.
+    TrustedImm32 trustedImm32ForShift(Imm32 imm)
+    {
+        return TrustedImm32(imm.asTrustedImm32().m_value & 31);
+    }
+
     // Backwards banches, these are currently all implemented using existing forwards branch mechanisms.
     void branchPtr(RelationalCondition cond, RegisterID op1, TrustedImmPtr imm, Label target)
     {
@@ -629,6 +636,11 @@ public:
         and64(imm, srcDest);
     }
     
+    void lshiftPtr(Imm32 imm, RegisterID srcDest)
+    {
+        lshift64(trustedImm32ForShift(imm), srcDest);
+    }
+
     void negPtr(RegisterID dest)
     {
         neg64(dest);
@@ -1411,13 +1423,6 @@ public:
         return branchSub32(cond, src, imm.asTrustedImm32(), dest);            
     }
     
-    // Immediate shifts only have 5 controllable bits
-    // so we'll consider them safe for now.
-    TrustedImm32 trustedImm32ForShift(Imm32 imm)
-    {
-        return TrustedImm32(imm.asTrustedImm32().m_value & 31);
-    }
-
     void lshift32(Imm32 imm, RegisterID dest)
     {
         lshift32(trustedImm32ForShift(imm), dest);

Source/JavaScriptCore/assembler/MacroAssemblerX86_64.h
@@ -225,6 +225,11 @@ public:
         m_assembler.andq_ir(imm.m_value, srcDest);
     }
     
+    void lshift64(TrustedImm32 imm, RegisterID dest)
+    {
+        m_assembler.shlq_i8r(imm.m_value, dest);
+    }
+    
     void neg64(RegisterID dest)
     {
         m_assembler.negq_r(dest);

Source/JavaScriptCore/assembler/X86Assembler.h
@@ -819,6 +819,16 @@ public:
             m_formatter.immediate8(imm);
         }
     }
+
+    void shlq_i8r(int imm, RegisterID dst)
+    {
+        if (imm == 1)
+            m_formatter.oneByteOp64(OP_GROUP2_Ev1, GROUP2_OP_SHL, dst);
+        else {
+            m_formatter.oneByteOp64(OP_GROUP2_EvIb, GROUP2_OP_SHL, dst);
+            m_formatter.immediate8(imm);
+        }
+    }
 #endif
 
     void imull_rr(RegisterID src, RegisterID dst)

Source/JavaScriptCore/bytecode/CodeBlock.cpp
@@ -507,7 +507,7 @@ void CodeBlock::dumpBytecode(PrintStream& out)
     if (symbolTable() && symbolTable()->captureCount()) {
         out.printf(
             "; %d captured var(s) (from r%d to r%d, inclusive)",
-            symbolTable()->captureCount(), symbolTable()->captureStart(), symbolTable()->captureEnd() - 1);
+            symbolTable()->captureCount(), symbolTable()->captureStart(), symbolTable()->captureEnd() + 1);
     }
     if (usesArguments()) {
         out.printf(

Source/JavaScriptCore/bytecode/CodeBlock.h
@@ -86,7 +86,7 @@ class ExecState;
 class LLIntOffsetsExtractor;
 class RepatchBuffer;
 
-inline int unmodifiedArgumentsRegister(int argumentsRegister) { return argumentsRegister - 1; }
+inline int unmodifiedArgumentsRegister(int argumentsRegister) { return argumentsRegister + 1; }
 
 static ALWAYS_INLINE int missingThisObjectMarker() { return std::numeric_limits<int>::max(); }
 
@@ -386,8 +386,8 @@ public:
         if (!symbolTable())
             return false;
 
-        return operand >= symbolTable()->captureStart()
-        && operand < symbolTable()->captureEnd();
+        return operand <= symbolTable()->captureStart()
+            && operand > symbolTable()->captureEnd();
     }
 
     CodeType codeType() const { return m_unlinkedCode->codeType(); }

Source/JavaScriptCore/bytecode/CodeOrigin.h
@@ -76,7 +76,7 @@ struct CodeOrigin {
     // would have owned the code if it had not been inlined. Otherwise returns 0.
     ScriptExecutable* codeOriginOwner() const;
     
-    unsigned stackOffset() const;
+    int stackOffset() const;
     
     static unsigned inlineDepthForCallFrame(InlineCallFrame*);
     
@@ -97,7 +97,7 @@ struct InlineCallFrame {
     WriteBarrier<JSFunction> callee; // This may be null, indicating that this is a closure call and that the JSFunction and JSScope are already on the stack.
     CodeOrigin caller;
     BitVector capturedVars; // Indexed by the machine call frame's variable numbering.
-    signed int stackOffset : 31;
+    signed stackOffset : 31;
     bool isCall : 1;
     
     CodeSpecializationKind specializationKind() const { return specializationFromIsCall(isCall); }
@@ -119,7 +119,7 @@ struct InlineCallFrame {
     MAKE_PRINT_METHOD(InlineCallFrame, dumpBriefFunctionInformation, briefFunctionInformation);
 };
 
-inline unsigned CodeOrigin::stackOffset() const
+inline int CodeOrigin::stackOffset() const
 {
     if (!inlineCallFrame)
         return 0;

Source/JavaScriptCore/bytecode/Operands.h
@@ -28,20 +28,19 @@
 
 #include "CallFrame.h"
 #include "JSObject.h"
-#include "VirtualRegister.h"
 #include <wtf/PrintStream.h>
 #include <wtf/Vector.h>
 
 namespace JSC {
 
-inline VirtualRegister localToOperand(int local) { return (VirtualRegister)local; }
-inline bool operandIsLocal(int operand) { return operand >= 0; }
-inline int operandToLocal(int operand) { return operand; }
+inline int localToOperand(int local) { return -local; }
+inline bool operandIsLocal(int operand) { return operand <= 0; }
+inline int operandToLocal(int operand) { return -operand; }
 
 // argument 0 is 'this'.
-inline bool operandIsArgument(int operand) { return operand < 0; }
-inline int operandToArgument(int operand) { return -operand + CallFrame::thisArgumentOffset(); }
-inline int argumentToOperand(int argument) { return -argument + CallFrame::thisArgumentOffset(); }
+inline bool operandIsArgument(int operand) { return operand > 0; }
+inline int operandToArgument(int operand) { return operand - CallFrame::thisArgumentOffset(); }
+inline int argumentToOperand(int argument) { return argument + CallFrame::thisArgumentOffset(); }
 
 template<typename T> struct OperandValueTraits;
 

commit 3c5a96ebf259b6620d672a73864a207f563f7f6f

FTL JIT broke after r155711 https://bugs.webkit.org/show_bug.cgi?id=121332
Reviewed by Geoffrey Garen.

Fixed OSR entry to use the local variable's index instead of its VirtualRegister.
Initialized ExitArgumentForOperand::m_operand to InvalidVirtualRegister instead of -1.
Fixed compileCallOrConstruct() to update locals on callframe going down.
Fixed prepareOSREntry() to grow stack down if needed.

* ftl/FTLExitArgumentForOperand.h:
(JSC::FTL::ExitArgumentForOperand::ExitArgumentForOperand):
* ftl/FTLLink.cpp:
(JSC::FTL::link):
* ftl/FTLLowerDFGToLLVM.cpp:
(JSC::FTL::LowerDFGToLLVM::compileExtractOSREntryLocal):
(JSC::FTL::LowerDFGToLLVM::compileCallOrConstruct):
* ftl/FTLOSREntry.cpp:
(JSC::FTL::prepareOSREntry):


git-svn-id: http://svn.webkit.org/repository/webkit/trunk@155735 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Source/JavaScriptCore/ftl/FTLExitArgumentForOperand.h
@@ -31,13 +31,14 @@
 #if ENABLE(FTL_JIT)
 
 #include "FTLExitArgument.h"
+#include "VirtualRegister.h"
 
 namespace JSC { namespace FTL {
 
 class ExitArgumentForOperand {
 public:
     ExitArgumentForOperand()
-        : m_operand(-1)
+        : m_operand(InvalidVirtualRegister)
     {
     }
     

Source/JavaScriptCore/ftl/FTLLink.cpp
@@ -75,7 +75,7 @@ void link(State& state)
         // Plant a check that sufficient space is available in the JSStack.
         // FIXME: https://bugs.webkit.org/show_bug.cgi?id=56291
         jit.addPtr(
-            CCallHelpers::TrustedImm32(-codeBlock->m_numCalleeRegisters * sizeof(Register)),
+            CCallHelpers::TrustedImm32(localToOperand(codeBlock->m_numCalleeRegisters) * sizeof(Register)),
             GPRInfo::callFrameRegister, GPRInfo::regT1);
         CCallHelpers::Jump stackCheck = jit.branchPtr(
             CCallHelpers::Above,

Source/JavaScriptCore/ftl/FTLLowerDFGToLLVM.cpp
@@ -546,7 +546,7 @@ private:
     {
         EncodedJSValue* buffer = static_cast<EncodedJSValue*>(
             m_ftlState.jitCode->ftlForOSREntry()->entryBuffer()->dataBuffer());
-        setJSValue(m_out.load64(m_out.absolute(buffer + m_node->unlinkedLocal())));
+        setJSValue(m_out.load64(m_out.absolute(buffer + operandToLocal(m_node->unlinkedLocal()))));
     }
     
     void compileGetLocal()
@@ -1863,7 +1863,7 @@ private:
         
         LValue calleeFrame = m_out.add(
             m_callFrame,
-            m_out.constIntPtr(sizeof(Register) * codeBlock()->m_numCalleeRegisters));
+            m_out.constIntPtr(sizeof(Register) * localToOperand(codeBlock()->m_numCalleeRegisters)));
         
         m_out.store32(
             m_out.constInt32(numPassedArgs + dummyThisArgument),

Source/JavaScriptCore/ftl/FTLOSREntry.cpp
@@ -77,8 +77,8 @@ void* prepareOSREntry(
     for (int local = values.numberOfLocals(); local--;)
         scratch[local] = JSValue::encode(values.local(local));
     
-    unsigned stackFrameSize = entryCodeBlock->m_numCalleeRegisters;
-    if (!vm.interpreter->stack().grow(&exec->registers()[stackFrameSize])) {
+    int stackFrameSize = entryCodeBlock->m_numCalleeRegisters;
+    if (!vm.interpreter->stack().grow(&exec->registers()[localToOperand(stackFrameSize)])) {
         if (Options::verboseOSR())
             dataLog("    OSR failed bcause stack growth failed.\n");
         return 0;