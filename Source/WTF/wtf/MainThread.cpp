WebKit/WebKit-http

commit c253c44671c09b9c20cdf823ff3b9a6dfe2e67e8

Change callOnMainThread to take an std::function https://bugs.webkit.org/show_bug.cgi?id=122698
Reviewed by Darin Adler.

This will let us pass anything that can be converted to an std::function (including lambdas and
WTF::Function objects) to callOnMainThread.

* wtf/MainThread.cpp:
(WTF::callOnMainThread):
* wtf/MainThread.h:

git-svn-id: http://svn.webkit.org/repository/webkit/trunk@157374 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Source/WTF/wtf/MainThread.cpp
@@ -232,14 +232,13 @@ void cancelCallOnMainThread(MainThreadFunction* function, void* context)
 
 static void callFunctionObject(void* context)
 {
-    Function<void ()>* function = static_cast<Function<void ()>*>(context);
+    auto function = std::unique_ptr<std::function<void ()>>(static_cast<std::function<void ()>*>(context));
     (*function)();
-    delete function;
 }
 
-void callOnMainThread(const Function<void ()>& function)
+void callOnMainThread(std::function<void ()> function)
 {
-    callOnMainThread(callFunctionObject, new Function<void ()>(function));
+    callOnMainThread(callFunctionObject, std::make_unique<std::function<void ()>>(std::move(function)).release());
 }
 
 void setMainThreadCallbacksPaused(bool paused)

Source/WTF/wtf/MainThread.h
@@ -30,6 +30,7 @@
 #ifndef MainThread_h
 #define MainThread_h
 
+#include <functional>
 #include <stdint.h>
 
 namespace WTF {
@@ -44,9 +45,8 @@ WTF_EXPORT_PRIVATE void callOnMainThread(MainThreadFunction*, void* context);
 WTF_EXPORT_PRIVATE void callOnMainThreadAndWait(MainThreadFunction*, void* context);
 WTF_EXPORT_PRIVATE void cancelCallOnMainThread(MainThreadFunction*, void* context);
 
-template<typename> class Function;
-WTF_EXPORT_PRIVATE void callOnMainThread(const Function<void ()>&);
-    
+WTF_EXPORT_PRIVATE void callOnMainThread(std::function<void ()>);
+
 WTF_EXPORT_PRIVATE void setMainThreadCallbacksPaused(bool paused);
 
 WTF_EXPORT_PRIVATE bool isMainThread();

commit 000b4a19f85d7b5450aac5bd11f058fe19fc0e9f

WebKit Nightlies broken by r157374 https://bugs.webkit.org/show_bug.cgi?id=122736
Reviewed by Andreas Kling.

Add back a callOnMainThread overload that Safari is using.

* wtf/MainThread.cpp:
(WTF::callOnMainThread):
* wtf/MainThread.h:

git-svn-id: http://svn.webkit.org/repository/webkit/trunk@157396 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Source/WTF/wtf/MainThread.cpp
@@ -241,6 +241,11 @@ void callOnMainThread(std::function<void ()> function)
     callOnMainThread(callFunctionObject, std::make_unique<std::function<void ()>>(std::move(function)).release());
 }
 
+void callOnMainThread(const Function<void ()>& function)
+{
+    callOnMainThread(std::function<void ()>(function));
+}
+
 void setMainThreadCallbacksPaused(bool paused)
 {
     ASSERT(isMainThread());

Source/WTF/wtf/MainThread.h
@@ -47,6 +47,10 @@ WTF_EXPORT_PRIVATE void cancelCallOnMainThread(MainThreadFunction*, void* contex
 
 WTF_EXPORT_PRIVATE void callOnMainThread(std::function<void ()>);
 
+// FIXME: This symbol is used by Safari and should be removed once Safari is no longer using it.
+template<typename> class Function;
+WTF_EXPORT_PRIVATE void callOnMainThread(const Function<void ()>&);
+
 WTF_EXPORT_PRIVATE void setMainThreadCallbacksPaused(bool paused);
 
 WTF_EXPORT_PRIVATE bool isMainThread();