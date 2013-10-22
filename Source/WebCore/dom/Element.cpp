WebKit/WebKit-http

commit be79cf1e39f31462e31c2d7eb743256189336ad3

[CSS Regions] Anonymous nested regions https://bugs.webkit.org/show_bug.cgi?id=119135
Reviewed by David Hyatt.

Source/WebCore:

Tests: fast/regions/table-caption-as-region.html
       fast/regions/table-cell-as-region.html

This patch allows any non-replaced block to behave like a region. When an element is styled with the
-webkit-flow-from property, instead of making the renderer a RenderRegion, we let the original
renderer be created the same way and we add a region as an anonymous child for the renderer.
The anonymous block child, modeled by the new RenderNamedFlowFragment class, will be responsible
for the fragmentation of the named flow thread content.

A RenderBlockFlow object will keep a reference to a RenderNamedFlowFragment(RenderRegion) inside its
rare data structures.

Contains code contributed by Catalin Badea.

* CMakeLists.txt:
* GNUmakefile.list.am:
* WebCore.vcxproj/WebCore.vcxproj:
* WebCore.xcodeproj/project.pbxproj:
* dom/Element.cpp: Changed to take the anonymous region into account.
(WebCore::Element::renderRegion):
(WebCore::Element::webkitGetRegionFlowRanges):
* dom/WebKitNamedFlow.cpp: Ditto.
(WebCore::WebKitNamedFlow::firstEmptyRegionIndex):
(WebCore::WebKitNamedFlow::getRegionsByContent):
(WebCore::WebKitNamedFlow::getRegions):
* inspector/InspectorOverlay.cpp: Take into account the new model for regions, with an anonymous region inside a block.
(WebCore::buildObjectForRegionHighlight):
(WebCore::buildObjectForElementInfo):
* rendering/RenderBlock.cpp:
(WebCore::RenderBlock::computeShapeSize): For a render named flow fragment, there is no need to recompute the shape inside
we can take it from the parent.
(WebCore::RenderBlock::renderName): Make the block that contains a render named flow fragment (region) report RenderRegion.
A future patch that will change this will need to rebase a lot of tests.
* rendering/RenderBlockFlow.cpp:

Source/WebCore/dom/Element.cpp
@@ -74,6 +74,7 @@
 #include "PlatformWheelEvent.h"
 #include "PointerLockController.h"
 #include "PseudoElement.h"
+#include "RenderNamedFlowFragment.h"
 #include "RenderRegion.h"
 #include "RenderTheme.h"
 #include "RenderView.h"
@@ -2705,8 +2706,8 @@ bool Element::isSpellCheckingEnabled() const
 
 RenderRegion* Element::renderRegion() const
 {
-    if (renderer() && renderer()->isRenderRegion())
-        return toRenderRegion(renderer());
+    if (renderer() && renderer()->isRenderNamedFlowFragmentContainer())
+        return toRenderBlockFlow(renderer())->renderNamedFlowFragment();
 
     return 0;
 }
@@ -2763,8 +2764,8 @@ Vector<RefPtr<Range> > Element::webkitGetRegionFlowRanges() const
     document().updateLayoutIgnorePendingStylesheets();
 
     Vector<RefPtr<Range> > rangeObjects;
-    if (document().cssRegionsEnabled() && renderer() && renderer()->isRenderRegion()) {
-        RenderRegion* region = toRenderRegion(renderer());
+    if (document().cssRegionsEnabled() && renderer() && renderer()->isRenderNamedFlowFragmentContainer()) {
+        RenderNamedFlowFragment* region = toRenderBlockFlow(renderer())->renderNamedFlowFragment();
         if (region->isValid())
             region->getRanges(rangeObjects);
     }

Source/WebCore/inspector/InspectorOverlay.cpp
@@ -47,6 +47,7 @@
 #include "RenderElement.h"
 #include "RenderFlowThread.h"
 #include "RenderInline.h"
+#include "RenderNamedFlowFragment.h"
 #include "RenderNamedFlowThread.h"
 #include "RenderRegion.h"
 #include "RenderView.h"
@@ -390,9 +391,10 @@ static PassRefPtr<InspectorObject> buildObjectForRegionHighlight(FrameView* main
     if (!containingView)
         return nullptr;
 
-    LayoutRect borderBox = region->borderBoxRect();
-    borderBox.setWidth(borderBox.width() + region->verticalScrollbarWidth());
-    borderBox.setHeight(borderBox.height() + region->horizontalScrollbarHeight());
+    RenderBlockFlow* regionContainer = toRenderBlockFlow(region->parent());
+    LayoutRect borderBox = regionContainer->borderBoxRect();
+    borderBox.setWidth(borderBox.width() + regionContainer->verticalScrollbarWidth());
+    borderBox.setHeight(borderBox.height() + regionContainer->horizontalScrollbarHeight());
 
     // Create incoming and outgoing boxes that we use to chain the regions toghether.
     const LayoutSize linkBoxSize(10, 10);
@@ -407,9 +409,9 @@ static PassRefPtr<InspectorObject> buildObjectForRegionHighlight(FrameView* main
     incomingRectBox.move(0, linkBoxVerticalOffset);
     outgoingRectBox.move(0, -linkBoxVerticalOffset);
 
-    FloatQuad borderRectQuad = region->localToAbsoluteQuad(FloatRect(borderBox));
-    FloatQuad incomingRectQuad = region->localToAbsoluteQuad(FloatRect(incomingRectBox));
-    FloatQuad outgoingRectQuad = region->localToAbsoluteQuad(FloatRect(outgoingRectBox));
+    FloatQuad borderRectQuad = regionContainer->localToAbsoluteQuad(FloatRect(borderBox));
+    FloatQuad incomingRectQuad = regionContainer->localToAbsoluteQuad(FloatRect(incomingRectBox));
+    FloatQuad outgoingRectQuad = regionContainer->localToAbsoluteQuad(FloatRect(outgoingRectBox));
 
     contentsQuadToPage(mainView, containingView, borderRectQuad);
     contentsQuadToPage(mainView, containingView, incomingRectQuad);
@@ -560,8 +562,8 @@ static PassRefPtr<InspectorObject> buildObjectForElementInfo(Node* node)
     elementInfo->setString("nodeWidth", String::number(modelObject ? adjustForAbsoluteZoom(modelObject->pixelSnappedOffsetWidth(), modelObject) : boundingBox.width()));
     elementInfo->setString("nodeHeight", String::number(modelObject ? adjustForAbsoluteZoom(modelObject->pixelSnappedOffsetHeight(), modelObject) : boundingBox.height()));
     
-    if (renderer->isRenderRegion()) {
-        RenderRegion* region = toRenderRegion(renderer);
+    if (renderer->isRenderNamedFlowFragmentContainer()) {
+        RenderNamedFlowFragment* region = toRenderBlockFlow(renderer)->renderNamedFlowFragment();
         RenderFlowThread* flowThread = region->flowThread();
         if (flowThread && flowThread->isRenderNamedFlowThread()) {
             RefPtr<InspectorObject> regionFlowInfo = InspectorObject::create();

Source/WebCore/rendering/RenderBlock.cpp
@@ -1474,7 +1474,14 @@ bool RenderBlock::updateShapesBeforeBlockLayout()
 void RenderBlock::computeShapeSize()
 {
     ShapeInsideInfo* shapeInsideInfo = this->shapeInsideInfo();
-    if (shapeInsideInfo) {
+    if (!shapeInsideInfo)
+        return;
+
+    if (isRenderNamedFlowFragment()) {
+        ShapeInsideInfo* parentShapeInsideInfo = toRenderBlock(parent())->shapeInsideInfo();
+        ASSERT(parentShapeInsideInfo);
+        shapeInsideInfo->setShapeSize(parentShapeInsideInfo->shapeSize().width(), parentShapeInsideInfo->shapeSize().height());
+    } else {
         bool percentageLogicalHeightResolvable = percentageLogicalHeightIsResolvableFromBlock(this, false);
         shapeInsideInfo->setShapeSize(logicalWidth(), percentageLogicalHeightResolvable ? logicalHeight() : LayoutUnit());
     }
@@ -5867,7 +5874,9 @@ const char* RenderBlock::renderName() const
 {
     if (isBody())
         return "RenderBody"; // FIXME: Temporary hack until we know that the regression tests pass.
-    
+
+    if (isRenderNamedFlowFragmentContainer())
+        return "RenderRegion";
     if (isFloating())
         return "RenderBlock (floating)";
     if (isOutOfFlowPositioned())

commit 0e81bc7840b4daf7f4616051074dca36541b789d

[CSS Regions] Possible performance regression after r157567 https://bugs.webkit.org/show_bug.cgi?id=123016
Reviewed by Andreas Kling.

The revision 157567 http://trac.webkit.org/changeset/157567 may have regressed
Parser/html5-full-render by ~1.1% and Parser/html-parser by ~2%. These changes
try to optimize the initial patch, based on Andreas Kling's review.

The patch also adds a couple of refactorings that should make the code easier to read:
- the CSS Shapes functions are now wrapped in a single #if clause
- the CSS Shapes and CSS Regions pre-layout preparations are wrapped in a helper function

The RenderFlowThread::logicalWidthChangedInRegionsForBlock function is optimized by passing
it information about the state of the relayout children flag. If the flag is true already,
some of the steps are skipped.

Tests: no new tests.

* dom/Element.cpp:
(WebCore::Element::webkitGetRegionFlowRanges):
* inspector/InspectorOverlay.cpp:
(WebCore::buildObjectForElementInfo):
* rendering/RenderBlock.cpp:
(WebCore::shapeInfoRequiresRelayout):
(WebCore::RenderBlock::updateShapesBeforeBlockLayout):
(WebCore::RenderBlock::computeShapeSize):
(WebCore::RenderBlock::prepareShapesAndPaginationBeforeBlockLayout):
* rendering/RenderBlock.h:
* rendering/RenderBlockFlow.cpp:
(WebCore::RenderBlockFlow::layoutBlock):
(WebCore::RenderBlockFlow::createRenderNamedFlowFragmentIfNeeded):
(WebCore::RenderBlockFlow::setRenderNamedFlowFragment):
(WebCore::RenderBlockFlow::ensureRareData):
* rendering/RenderBlockFlow.h:
(WebCore::RenderBlockFlow::RenderBlockFlowRareData::RenderBlockFlowRareData):
(WebCore::RenderElement::isRenderNamedFlowFragmentContainer):
* rendering/RenderDeprecatedFlexibleBox.cpp:
(WebCore::RenderDeprecatedFlexibleBox::layoutBlock):
* rendering/RenderElement.h:
(WebCore::RenderElement::generatingElement):
* rendering/RenderFlexibleBox.cpp:
(WebCore::RenderFlexibleBox::layoutBlock):
* rendering/RenderFlowThread.cpp:

Source/WebCore/dom/Element.cpp
@@ -2761,10 +2761,12 @@ const AtomicString& Element::webkitRegionOverset() const
 
 Vector<RefPtr<Range>> Element::webkitGetRegionFlowRanges() const
 {
-    document().updateLayoutIgnorePendingStylesheets();
-
     Vector<RefPtr<Range>> rangeObjects;
-    if (document().cssRegionsEnabled() && renderer() && renderer()->isRenderNamedFlowFragmentContainer()) {
+    if (!document().cssRegionsEnabled())
+        return rangeObjects;
+
+    document().updateLayoutIgnorePendingStylesheets();
+    if (renderer() && renderer()->isRenderNamedFlowFragmentContainer()) {
         RenderNamedFlowFragment* region = toRenderBlockFlow(renderer())->renderNamedFlowFragment();
         if (region->isValid())
             region->getRanges(rangeObjects);

Source/WebCore/inspector/InspectorOverlay.cpp
@@ -554,7 +554,7 @@ static PassRefPtr<InspectorObject> buildObjectForElementInfo(Node* node)
         elementInfo->setString("className", classNames.toString());
     }
 
-    RenderObject* renderer = node->renderer();
+    RenderElement* renderer = element->renderer();
     Frame* containingFrame = node->document().frame();
     FrameView* containingView = containingFrame->view();
     IntRect boundingBox = pixelSnappedIntRect(containingView->contentsToRootView(renderer->absoluteBoundingBoxRect()));

Source/WebCore/rendering/RenderBlock.cpp
@@ -1405,6 +1405,22 @@ static inline bool shapeInfoRequiresRelayout(const RenderBlock* block)
         info = block->layoutShapeInsideInfo();
     return info && info->needsLayout();
 }
+
+void RenderBlock::computeShapeSize()
+{
+    ShapeInsideInfo* shapeInsideInfo = this->shapeInsideInfo();
+    if (!shapeInsideInfo)
+        return;
+
+    if (isRenderNamedFlowFragment()) {
+        ShapeInsideInfo* parentShapeInsideInfo = toRenderBlock(parent())->shapeInsideInfo();
+        ASSERT(parentShapeInsideInfo);
+        shapeInsideInfo->setShapeSize(parentShapeInsideInfo->shapeSize().width(), parentShapeInsideInfo->shapeSize().height());
+    } else {
+        bool percentageLogicalHeightResolvable = percentageLogicalHeightIsResolvableFromBlock(this, false);
+        shapeInsideInfo->setShapeSize(logicalWidth(), percentageLogicalHeightResolvable ? logicalHeight() : LayoutUnit());
+    }
+}
 #endif
 
 bool RenderBlock::updateShapesBeforeBlockLayout()
@@ -1432,24 +1448,6 @@ bool RenderBlock::updateShapesBeforeBlockLayout()
 #endif
 }
 
-#if ENABLE(CSS_SHAPES)
-void RenderBlock::computeShapeSize()
-{
-    ShapeInsideInfo* shapeInsideInfo = this->shapeInsideInfo();
-    if (!shapeInsideInfo)
-        return;
-
-    if (isRenderNamedFlowFragment()) {
-        ShapeInsideInfo* parentShapeInsideInfo = toRenderBlock(parent())->shapeInsideInfo();
-        ASSERT(parentShapeInsideInfo);
-        shapeInsideInfo->setShapeSize(parentShapeInsideInfo->shapeSize().width(), parentShapeInsideInfo->shapeSize().height());
-    } else {
-        bool percentageLogicalHeightResolvable = percentageLogicalHeightIsResolvableFromBlock(this, false);
-        shapeInsideInfo->setShapeSize(logicalWidth(), percentageLogicalHeightResolvable ? logicalHeight() : LayoutUnit());
-    }
-}
-#endif
-
 void RenderBlock::updateShapesAfterBlockLayout(bool heightChanged)
 {
 #if ENABLE(CSS_SHAPES)
@@ -1460,6 +1458,16 @@ void RenderBlock::updateShapesAfterBlockLayout(bool heightChanged)
 #else
     UNUSED_PARAM(heightChanged);
 #endif
+}
+
+void RenderBlock::prepareShapesAndPaginationBeforeBlockLayout(bool& relayoutChildren)
+{
+    // Regions changing widths can force us to relayout our children.
+    RenderFlowThread* flowThread = flowThreadContainingBlock();
+    if (updateShapesBeforeBlockLayout())
+        relayoutChildren = true;
+    if (flowThread)
+        flowThread->logicalWidthChangedInRegionsForBlock(this, relayoutChildren);
 }
 
 bool RenderBlock::updateLogicalWidthAndColumnWidth()
@@ -5273,14 +5281,6 @@ void RenderBlock::setStaticInlinePositionForChild(RenderBox* child, LayoutUnit b
         inlinePosition += startOffsetForContent() - startOffsetForContent(blockOffset);
     }
     child->layer()->setStaticInlinePosition(inlinePosition);
-}
-
-bool RenderBlock::logicalWidthChangedInRegions(RenderFlowThread* flowThread) const
-{
-    if (!flowThread || !flowThread->hasValidRegionInfo())
-        return false;
-    
-    return flowThread->logicalWidthChangedInRegionsForBlock(this);
 }
 
 void RenderBlock::computeRegionRangeForBoxChild(const RenderBox* box) const

Source/WebCore/rendering/RenderBlock.h
@@ -506,6 +506,7 @@ protected:
     void updateBlockChildDirtyBitsBeforeLayout(bool relayoutChildren, RenderBox*);
 
     virtual void checkForPaginationLogicalHeightChange(LayoutUnit& pageLogicalHeight, bool& pageLogicalHeightChanged, bool& hasSpecifiedPageLogicalHeight);
+    void prepareShapesAndPaginationBeforeBlockLayout(bool&);
 
 private:
     // FIXME-BLOCKFLOW: Remove virtualizaion when all callers have moved to RenderBlockFlow
@@ -714,8 +715,6 @@ protected:
     // region/page/column that has a different available line width than the old one. Used to know when you have to dirty a
     // line, i.e., that it can't be re-used.
     bool lineWidthForPaginatedLineChanged(RootInlineBox*, LayoutUnit lineDelta, RenderFlowThread*) const;
-
-    bool logicalWidthChangedInRegions(RenderFlowThread*) const;
 
     virtual bool requiresColumns(int desiredColumnCount) const;
 

Source/WebCore/rendering/RenderBlockFlow.cpp
@@ -326,14 +326,9 @@ void RenderBlockFlow::layoutBlock(bool relayoutChildren, LayoutUnit pageLogicalH
     RenderStyle* styleToUse = style();
     LayoutStateMaintainer statePusher(&view(), this, locationOffset(), hasColumns() || hasTransform() || hasReflection() || styleToUse->isFlippedBlocksWritingMode(), pageLogicalHeight, pageLogicalHeightChanged, columnInfo());
 
-    // Regions changing widths can force us to relayout our children.
-    RenderFlowThread* flowThread = flowThreadContainingBlock();
-    if (logicalWidthChangedInRegions(flowThread))
-        relayoutChildren = true;
-    if (updateShapesBeforeBlockLayout())
-        relayoutChildren = true;
-    if (namedFlowFragmentNeedsUpdate())
-        relayoutChildren = true;
+    prepareShapesAndPaginationBeforeBlockLayout(relayoutChildren);
+    if (!relayoutChildren)
+        relayoutChildren = namedFlowFragmentNeedsUpdate();
 
     // We use four values, maxTopPos, maxTopNeg, maxBottomPos, and maxBottomNeg, to track
     // our current maximal positive and negative margins. These values are used when we
@@ -2586,10 +2581,10 @@ GapRects RenderBlockFlow::inlineSelectionGaps(RenderBlock* rootBlock, const Layo
 
 void RenderBlockFlow::createRenderNamedFlowFragmentIfNeeded()
 {
-    if (renderNamedFlowFragment() || isRenderNamedFlowFragment())
+    if (!document().cssRegionsEnabled() || renderNamedFlowFragment() || isRenderNamedFlowFragment())
         return;
 
-    if (document().cssRegionsEnabled() && style()->isDisplayRegionType() && style()->hasFlowFrom()) {
+    if (style()->isDisplayRegionType() && style()->hasFlowFrom()) {
         RenderNamedFlowFragment* flowFragment = new RenderNamedFlowFragment(document());
         flowFragment->setStyleForNamedFlowFragment(style());
         setRenderNamedFlowFragment(flowFragment);
@@ -2625,11 +2620,20 @@ void RenderBlockFlow::updateLogicalHeight()
 
 void RenderBlockFlow::setRenderNamedFlowFragment(RenderNamedFlowFragment* flowFragment)
 {
-    if (!m_rareData)
-        m_rareData = adoptPtr(new RenderBlockFlowRareData(this));
-    if (m_rareData->m_renderNamedFlowFragment)
-        m_rareData->m_renderNamedFlowFragment->destroy();
-    m_rareData->m_renderNamedFlowFragment = flowFragment;
+    RenderBlockFlowRareData& rareData = ensureRareData();
+    if (rareData.m_renderNamedFlowFragment)
+        rareData.m_renderNamedFlowFragment->destroy();
+    rareData.m_renderNamedFlowFragment = flowFragment;
+}
+
+// FIXME: Use this function in more places.
+RenderBlockFlow::RenderBlockFlowRareData& RenderBlockFlow::ensureRareData()
+{
+    if (m_rareData)
+        return *m_rareData;
+
+    m_rareData = adoptPtr(new RenderBlockFlowRareData(this));
+    return *m_rareData;
 }
 
 static bool shouldCheckLines(RenderObject& obj)

Source/WebCore/rendering/RenderBlockFlow.h
@@ -96,8 +96,8 @@ public:
         RenderBlockFlowRareData(const RenderBlockFlow* block)
             : m_margins(positiveMarginBeforeDefault(block), negativeMarginBeforeDefault(block), positiveMarginAfterDefault(block), negativeMarginAfterDefault(block))
             , m_lineBreakToAvoidWidow(-1)
-            , m_lineGridBox(0)
-            , m_renderNamedFlowFragment(0)
+            , m_lineGridBox(nullptr)
+            , m_renderNamedFlowFragment(nullptr)
             , m_discardMarginBefore(false)
             , m_discardMarginAfter(false)
             , m_didBreakAtLineToAvoidWidow(false)
@@ -413,7 +413,7 @@ private:
     
     Position positionForBox(InlineBox*, bool start = true) const;
     virtual VisiblePosition positionForPointWithInlineChildren(const LayoutPoint& pointInLogicalContents) OVERRIDE;
-
+    RenderBlockFlowRareData& ensureRareData();
     virtual void addFocusRingRectsForInlineChildren(Vector<IntRect>& rects, const LayoutPoint& additionalOffset, const RenderLayerModelObject*) OVERRIDE;
 
 // FIXME-BLOCKFLOW: These methods have implementations in
@@ -504,6 +504,11 @@ inline const RenderBlockFlow* toRenderBlockFlow(const RenderObject* object)
 { 
     ASSERT_WITH_SECURITY_IMPLICATION(!object || object->isRenderBlockFlow());
     return static_cast<const RenderBlockFlow*>(object);
+}
+
+inline bool RenderElement::isRenderNamedFlowFragmentContainer() const
+{
+    return isRenderBlockFlow() && toRenderBlockFlow(this)->renderNamedFlowFragment();
 }
 
 // This will catch anyone doing an unnecessary cast.

Source/WebCore/rendering/RenderDeprecatedFlexibleBox.cpp
@@ -287,12 +287,7 @@ void RenderDeprecatedFlexibleBox::layoutBlock(bool relayoutChildren, LayoutUnit)
     LayoutRepainter repainter(*this, checkForRepaintDuringLayout());
     LayoutStateMaintainer statePusher(&view(), this, locationOffset(), hasTransform() || hasReflection() || style()->isFlippedBlocksWritingMode());
 
-    // Regions changing widths can force us to relayout our children.
-    RenderFlowThread* flowThread = flowThreadContainingBlock();
-    if (logicalWidthChangedInRegions(flowThread))
-        relayoutChildren = true;
-    if (updateShapesBeforeBlockLayout())
-        relayoutChildren = true;
+    prepareShapesAndPaginationBeforeBlockLayout(relayoutChildren);
 
     LayoutSize previousSize = size();
 

Source/WebCore/rendering/RenderElement.h
@@ -55,6 +55,7 @@ public:
     bool isRenderBlockFlow() const;
     bool isRenderReplaced() const;
     bool isRenderInline() const;
+    bool isRenderNamedFlowFragmentContainer() const;
 
     virtual bool isChildAllowed(const RenderObject&, const RenderStyle&) const { return true; }
     virtual void addChild(RenderObject* newChild, RenderObject* beforeChild = 0);
@@ -260,7 +261,7 @@ inline const RenderElement* toRenderElement(const RenderObject* object)
 
 inline Element* RenderElement::generatingElement() const
 {
-    if (isRenderNamedFlowFragment() && parent())
+    if (parent() && isRenderNamedFlowFragment())
         return toRenderElement(parent())->generatingElement();
     return toElement(RenderObject::generatingNode());
 }

Source/WebCore/rendering/RenderFlexibleBox.cpp
@@ -339,12 +339,7 @@ void RenderFlexibleBox::layoutBlock(bool relayoutChildren, LayoutUnit)
 
     LayoutStateMaintainer statePusher(&view(), this, locationOffset(), hasTransform() || hasReflection() || style()->isFlippedBlocksWritingMode());
 
-    // Regions changing widths can force us to relayout our children.
-    RenderFlowThread* flowThread = flowThreadContainingBlock();
-    if (logicalWidthChangedInRegions(flowThread))
-        relayoutChildren = true;
-    if (updateShapesBeforeBlockLayout())
-        relayoutChildren = true;
+    prepareShapesAndPaginationBeforeBlockLayout(relayoutChildren);
 
     m_numberOfInFlowChildrenOnFirstLine = -1;
 

Source/WebCore/rendering/RenderFlowThread.cpp
@@ -727,18 +727,23 @@ void RenderFlowThread::removeRenderBoxRegionInfo(RenderBox* box)
     m_regionRangeMap.remove(box);
 }
 
-bool RenderFlowThread::logicalWidthChangedInRegionsForBlock(const RenderBlock* block)
+void RenderFlowThread::logicalWidthChangedInRegionsForBlock(const RenderBlock* block, bool& relayoutChildren)
 {
-    if (!hasRegions())
-        return false;
+    if (!hasValidRegionInfo())
+        return;
 
     RenderRegionRangeMap::iterator it = m_regionRangeMap.find(block);
     if (it == m_regionRangeMap.end())
-        return false;
+        return;
 
     RenderRegionRange& range = it->value;
     bool rangeInvalidated = range.rangeInvalidated();
     range.clearRangeInvalidated();
+
+    // If there will be a relayout anyway skip the next steps because they only verify
+    // the state of the ranges.
+    if (relayoutChildren)
+        return;
 
     RenderRegion* startRegion;
     RenderRegion* endRegion;
@@ -746,8 +751,10 @@ bool RenderFlowThread::logicalWidthChangedInRegionsForBlock(const RenderBlock* b
 
     // Not necessary for the flow thread, since we already computed the correct info for it.
     // If the regions have changed invalidate the children.
-    if (block == this)
-        return m_pageLogicalSizeChanged;
+    if (block == this) {
+        relayoutChildren = m_pageLogicalSizeChanged;
+        return;
+    }
 
     for (RenderRegionList::iterator iter = m_regionList.find(startRegion); iter != m_regionList.end(); ++iter) {
         RenderRegion* region = *iter;
@@ -755,19 +762,21 @@ bool RenderFlowThread::logicalWidthChangedInRegionsForBlock(const RenderBlock* b
 
         // We have no information computed for this region so we need to do it.
         OwnPtr<RenderBoxRegionInfo> oldInfo = region->takeRenderBoxRegionInfo(block);
-        if (!oldInfo)
-            return rangeInvalidated;
+        if (!oldInfo) {
+            relayoutChildren = rangeInvalidated;
+            return;
+        }
 
         LayoutUnit oldLogicalWidth = oldInfo->logicalWidth();
         RenderBoxRegionInfo* newInfo = block->renderBoxRegionInfo(region);
-        if (!newInfo || newInfo->logicalWidth() != oldLogicalWidth)
-            return true;
+        if (!newInfo || newInfo->logicalWidth() != oldLogicalWidth) {
+            relayoutChildren = true;
+            return;
+        }
 
         if (region == endRegion)
             break;
     }
-
-    return false;
 }
 
 LayoutUnit RenderFlowThread::contentLogicalWidthOfFirstRegion() const

Source/WebCore/rendering/RenderFlowThread.h
@@ -126,7 +126,7 @@ public:
     RenderRegion* mapFromFlowToRegion(TransformState&) const;
 
     void removeRenderBoxRegionInfo(RenderBox*);
-    bool logicalWidthChangedInRegionsForBlock(const RenderBlock*);
+    void logicalWidthChangedInRegionsForBlock(const RenderBlock*, bool&);
 
     LayoutUnit contentLogicalWidthOfFirstRegion() const;
     LayoutUnit contentLogicalHeightOfFirstRegion() const;

Source/WebCore/rendering/RenderGrid.cpp
@@ -164,12 +164,7 @@ void RenderGrid::layoutBlock(bool relayoutChildren, LayoutUnit)
     LayoutRepainter repainter(*this, checkForRepaintDuringLayout());
     LayoutStateMaintainer statePusher(&view(), this, locationOffset(), hasTransform() || hasReflection() || style()->isFlippedBlocksWritingMode());
 
-    // Regions changing widths can force us to relayout our children.
-    RenderFlowThread* flowThread = flowThreadContainingBlock();
-    if (logicalWidthChangedInRegions(flowThread))
-        relayoutChildren = true;
-    if (updateShapesBeforeBlockLayout())
-        relayoutChildren = true;
+    prepareShapesAndPaginationBeforeBlockLayout(relayoutChildren);
 
     LayoutSize previousSize = size();
 

Source/WebCore/rendering/RenderNamedFlowFragment.h
@@ -37,7 +37,7 @@ namespace WebCore {
 class Element;
 class RenderStyle;
 
-class RenderNamedFlowFragment : public RenderRegion {
+class RenderNamedFlowFragment FINAL : public RenderRegion {
 public:
     explicit RenderNamedFlowFragment(Document&);
     virtual ~RenderNamedFlowFragment();
@@ -45,7 +45,7 @@ public:
     void setStyleForNamedFlowFragment(const RenderStyle*);
 
     virtual bool isRenderNamedFlowFragment() const OVERRIDE FINAL { return true; }
-    virtual void styleDidChange(StyleDifference, const RenderStyle* oldStyle);
+    virtual void styleDidChange(StyleDifference, const RenderStyle* oldStyle) OVERRIDE;
 
     virtual LayoutUnit maxPageLogicalHeight() const;
 
@@ -58,11 +58,9 @@ public:
     RenderLayerModelObject* layerOwner() const { return parent() && parent()->isRenderLayerModelObject() ?
         toRenderLayerModelObject(parent()) : nullptr; }
 
-protected:
-    virtual bool shouldHaveAutoLogicalHeight() const;
-
 private:
-    virtual const char* renderName() const { return "RenderNamedFlowFragment"; }
+    virtual bool shouldHaveAutoLogicalHeight() const OVERRIDE;
+    virtual const char* renderName() const OVERRIDE { return "RenderNamedFlowFragment"; }
 };
 
 inline RenderNamedFlowFragment* toRenderNamedFlowFragment(RenderObject* object)

Source/WebCore/rendering/RenderObject.cpp
@@ -2558,14 +2558,6 @@ bool RenderObject::canBeReplacedWithInlineRunIn() const
     return true;
 }
 
-bool RenderObject::isRenderNamedFlowFragmentContainer() const
-{
-    if (!isRenderBlockFlow())
-        return false;
-
-    return toRenderBlockFlow(this)->renderNamedFlowFragment();
-}
-
 #if ENABLE(SVG)
 
 RenderSVGResourceContainer* RenderObject::toRenderSVGResourceContainer()

Source/WebCore/rendering/RenderObject.h
@@ -363,7 +363,6 @@ public:
     virtual bool isRenderNamedFlowThread() const { return false; }
     bool isInFlowRenderFlowThread() const { return isRenderFlowThread() && !isOutOfFlowPositioned(); }
     bool isOutOfFlowRenderFlowThread() const { return isRenderFlowThread() && isOutOfFlowPositioned(); }
-    bool isRenderNamedFlowFragmentContainer() const;
 
     virtual bool isRenderMultiColumnBlock() const { return false; }
     virtual bool isRenderMultiColumnSet() const { return false; }

Source/WebCore/rendering/RenderTreeAsText.cpp
@@ -575,7 +575,7 @@ void write(TextStream& ts, const RenderObject& o, int indent, RenderAsTextBehavi
             writeTextRun(ts, text, *box);
         }
     } else {
-        if (!o.isRenderNamedFlowFragmentContainer()) {
+        if (!toRenderElement(o).isRenderNamedFlowFragmentContainer()) {
             for (RenderObject* child = toRenderElement(o).firstChild(); child; child = child->nextSibling()) {
                 if (child->hasLayer())
                     continue;

Source/WebCore/style/StyleResolveTree.cpp
@@ -164,7 +164,8 @@ static bool shouldCreateRenderer(const Element& element, const ContainerNode* re
 static bool elementInsideRegionNeedsRenderer(Element& element, const ContainerNode* renderingParentNode, RefPtr<RenderStyle>& style)
 {
 #if ENABLE(CSS_REGIONS)
-    const RenderObject* parentRenderer = renderingParentNode ? renderingParentNode->renderer() : 0;
+    // The parent of a region should always be an element.
+    const RenderElement* parentRenderer = renderingParentNode ? renderingParentNode->renderer() : 0;
 
     bool parentIsRegion = parentRenderer && !parentRenderer->canHaveChildren() && parentRenderer->isRenderNamedFlowFragmentContainer();
     bool parentIsNonRenderedInsideRegion = !parentRenderer && element.parentElement() && element.parentElement()->isInsideRegion();