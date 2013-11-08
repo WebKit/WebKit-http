WebKit/WebKit-http

commit adc50523c9b068107d4ee1d2cdf341d3b6d07f6f

Change ctiTrampoline into a thunk https://bugs.webkit.org/show_bug.cgi?id=123844
Reviewed by Filip Pizlo.

Converted ctiTrampoline and ctiOpThrowNotCaught into thunks named callToJavaScript
and returnFromJavaScript.  Cleaned up and in some cases removed JITStubsXXX.h files
after removing ctiTrampoline and ctiOpThrowNotCaught.  Added callJavaScriptJITFunction
to VM that is a function pointer to the callToJavaScript thunk.

* GNUmakefile.list.am:
* JavaScriptCore.xcodeproj/project.pbxproj:
* dfg/DFGDriver.cpp:
(JSC::DFG::compileImpl):
* jit/JITCode.cpp:
(JSC::JITCode::execute):
* jit/JITExceptions.cpp:
(JSC::genericUnwind):
* jit/JITStubs.cpp:
* jit/JITStubs.h:
* jit/JITStubsARM.h:
* jit/JITStubsARM64.h: Removed.
* jit/JITStubsARMv7.h:
* jit/JITStubsMIPS.h: Removed.
* jit/JITStubsMSVC64.asm:
* jit/JITStubsSH4.h: Removed.
* jit/JITStubsX86.h:
* jit/JITStubsX86_64.h:
* jit/JSInterfaceJIT.h:
* jit/ThunkGenerators.cpp:
(JSC::returnFromJavaScript):
(JSC::callToJavaScript):
* jit/ThunkGenerators.h:
* runtime/VM.cpp:
(JSC::VM::VM):
* runtime/VM.h:


git-svn-id: http://svn.webkit.org/repository/webkit/trunk@158751 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Source/JavaScriptCore/jit/ThunkGenerators.cpp
@@ -56,6 +56,274 @@ inline void emitPointerValidation(CCallHelpers& jit, GPRReg pointerGPR)
 #endif
 }
 
+static void returnFromJavaScript(CCallHelpers& jit)
+{
+#if CPU(X86_64)
+#if OS(WINDOWS)
+    // JIT operations can use up to 6 args (4 in registers and 2 on the stack).
+    // In addition, X86_64 ABI specifies that the worse case stack alignment
+    // requirement is 32 bytes. Based on these factors, we need to pad the stack
+    // and additional 40 bytes.
+#   define EXTRA_STACK_SIZE              40
+    jit.addPtr(CCallHelpers::TrustedImm32(8), X86Registers::esp);
+#else
+    // We don't need extra stack space for out-going args, but we need to align the
+    // stack to 32 byte alignment, therefore add 8 bytes to the stack.
+#   define EXTRA_STACK_SIZE              8
+    jit.addPtr(CCallHelpers::TrustedImm32(EXTRA_STACK_SIZE), X86Registers::esp);
+#endif
+
+    jit.pop(X86Registers::ebx);
+    jit.pop(X86Registers::r15);
+    jit.pop(X86Registers::r14);
+    jit.pop(X86Registers::r13);
+    jit.pop(X86Registers::r12);
+    jit.pop(X86Registers::ebp);
+#elif CPU(X86)
+    // JIT Operation can use up to 6 arguments right now. So, we need to
+    // reserve space in this stack frame for the out-going args. To ensure that
+    // the stack remains aligned on an 16 byte boundary, we round the padding up
+    // by 28 bytes.
+#   define EXTRA_STACK_SIZE 28
+
+    jit.addPtr(CCallHelpers::TrustedImm32(EXTRA_STACK_SIZE), X86Registers::esp);
+
+    jit.pop(X86Registers::ebx);
+    jit.pop(X86Registers::edi);
+    jit.pop(X86Registers::esi);
+    jit.pop(X86Registers::ebp);
+#elif CPU(ARM_TRADITIONAL)
+    // JIT Operation can use up to 6 arguments right now. So, we need to
+    // reserve space in this stack frame for the out-going args. We need to
+    // add space for 16 more bytes.
+#   define EXTRA_STACK_SIZE              16
+
+    jit.addPtr(CCallHelpers::TrustedImm32(EXTRA_STACK_SIZE), ARMRegisters::sp);
+
+    jit.pop(ARMRegisters::lr);
+    jit.pop(ARMRegisters::r11);
+    jit.pop(ARMRegisters::r10);
+    jit.pop(ARMRegisters::r9);
+    jit.pop(ARMRegisters::r8);
+    jit.pop(ARMRegisters::r6);
+    jit.pop(ARMRegisters::r5);
+    jit.pop(ARMRegisters::r4);
+#elif CPU(ARM_THUMB2)
+    // JIT Operation can use up to 6 arguments right now. So, we need to
+    // reserve space in this stack frame for the out-going args. To ensure that
+    // the stack remains aligned on an 16 byte boundary, we round the padding up
+    // by 28 bytes.
+#   define EXTRA_STACK_SIZE              28
+
+    jit.addPtr(CCallHelpers::TrustedImm32(EXTRA_STACK_SIZE), ARMRegisters::sp);
+    jit.push(ARMRegisters::r11);
+    jit.push(ARMRegisters::r10);
+    jit.push(ARMRegisters::r9);
+    jit.push(ARMRegisters::r8);
+    jit.push(ARMRegisters::r7);
+    jit.push(ARMRegisters::r6);
+    jit.push(ARMRegisters::r5);
+    jit.push(ARMRegisters::r4);
+    jit.push(ARMRegisters::lr);
+#elif CPU(ARM64)
+    jit.pop(ARM64Registers::x28);
+    jit.pop(ARM64Registers::x27);
+    jit.pop(ARM64Registers::x26);
+    jit.pop(ARM64Registers::x25);
+    jit.pop(ARM64Registers::x24);
+    jit.pop(ARM64Registers::x23);
+    jit.pop(ARM64Registers::x22);
+    jit.pop(ARM64Registers::x21);
+    jit.pop(ARM64Registers::x20);
+    jit.pop(ARM64Registers::x19);
+    jit.pop(ARM64Registers::lr);
+#elif CPU(MIPS)
+#   define PRESERVED_GP_OFFSET         60
+#   define PRESERVED_S0_OFFSET         64
+#   define PRESERVED_S1_OFFSET         68
+#   define PRESERVED_S2_OFFSET         72
+#   define PRESERVED_S3_OFFSET         76
+#   define PRESERVED_S4_OFFSET         80
+#   define PRESERVED_RETURN_ADDRESS_OFFSET 84
+#   define STACK_LENGTH               112
+
+    jit.loadPtr(CCallHelpers::Address(MIPSRegisters::sp, PRESERVED_S0_OFFSET), MIPSRegisters::s0);
+    jit.loadPtr(CCallHelpers::Address(MIPSRegisters::sp, PRESERVED_S1_OFFSET), MIPSRegisters::s1);
+    jit.loadPtr(CCallHelpers::Address(MIPSRegisters::sp, PRESERVED_S2_OFFSET), MIPSRegisters::s2);
+    jit.loadPtr(CCallHelpers::Address(MIPSRegisters::sp, PRESERVED_S3_OFFSET), MIPSRegisters::s3);
+    jit.loadPtr(CCallHelpers::Address(MIPSRegisters::sp, PRESERVED_S4_OFFSET), MIPSRegisters::s4);
+    jit.loadPtr(CCallHelpers::Address(MIPSRegisters::sp, PRESERVED_RETURN_ADDRESS_OFFSET), MIPSRegisters::ra);
+    jit.addPtr(CCallHelpers::TrustedImm32(STACK_LENGTH), MIPSRegisters::sp);
+#elif CPU(SH4)
+#   define EXTRA_STACK_SIZE 20
+
+    jit.addPtr(CCallHelpers::TrustedImm32(EXTRA_STACK_SIZE), SH4Registers::sp);
+    jit.pop(SH4Registers::r8);
+    jit.pop(SH4Registers::r9);
+    jit.pop(SH4Registers::r10);
+    jit.pop(SH4Registers::r11);
+    jit.pop(SH4Registers::r13);
+    jit.pop(SH4Registers::pr);
+    jit.pop(SH4Registers::fp);
+#endif
+
+    jit.ret();
+}
+
+MacroAssemblerCodeRef callToJavaScript(VM* vm)
+{
+    // The signature of the code generated is EncodedJSValue callToJavaScript(void* code, CallFrame*)
+
+    CCallHelpers jit(vm);
+
+#if CPU(X86_64)
+#if OS(WINDOWS)
+    // Dump register parameters to their home address
+    jit.storePtr(X86Registers::r9, CCallHelpers::Address(X86Registers::esp, 0x20));
+    jit.storePtr(X86Registers::r8, CCallHelpers::Address(X86Registers::esp, 0x18));
+    jit.storePtr(X86Registers::edx, CCallHelpers::Address(X86Registers::esp, 0x10));
+    jit.storePtr(X86Registers::ecx, CCallHelpers::Address(X86Registers::esp, 0x8));
+#endif // OS(WINDOWS)
+    jit.push(X86Registers::ebp);
+    jit.move(X86Registers::ebp, GPRInfo::nonArgGPR0);
+    jit.move(X86Registers::esp, X86Registers::ebp);
+    jit.push(X86Registers::r12);
+    jit.push(X86Registers::r13);
+    jit.push(X86Registers::r14);
+    jit.push(X86Registers::r15);
+    jit.push(X86Registers::ebx);
+
+    jit.subPtr(CCallHelpers::TrustedImm32(EXTRA_STACK_SIZE), X86Registers::esp);
+
+#   define CALLFRAME_SRC_REG GPRInfo::argumentGPR1
+#   define PREVIOUS_CALLFRAME_REG GPRInfo::nonArgGPR0
+#elif CPU(X86)
+    jit.push(X86Registers::ebp);
+    jit.move(X86Registers::ebp, GPRInfo::nonArgGPR0);
+    jit.move(X86Registers::esp, X86Registers::ebp);
+    jit.push(X86Registers::esi);
+    jit.push(X86Registers::edi);
+    jit.push(X86Registers::ebx);
+
+    jit.subPtr(CCallHelpers::TrustedImm32(EXTRA_STACK_SIZE), X86Registers::esp);
+#if COMPILER(MSVC)
+    jit.move(X86Registers::esp, X86Registers::ecx);
+#endif
+
+    jit.load32(CCallHelpers::Address(X86Registers::esp, EXTRA_STACK_SIZE + 24), GPRInfo::callFrameRegister);
+
+#   define CALLFRAME_SRC_REG GPRInfo::callFrameRegister
+#   define CALL_CODE jit.call(CCallHelpers::Address(X86Registers::esp, EXTRA_STACK_SIZE + 20))
+#   define PREVIOUS_CALLFRAME_REG GPRInfo::nonArgGPR0
+#elif CPU(ARM_TRADITIONAL)
+    jit.push(ARMRegisters::r4);
+    jit.push(ARMRegisters::r5);
+    jit.push(ARMRegisters::r6);
+    jit.push(ARMRegisters::r8);
+    jit.push(ARMRegisters::r9);
+    jit.push(ARMRegisters::r10);
+    jit.push(ARMRegisters::r11);
+    jit.push(ARMRegisters::lr);
+
+    jit.subPtr(CCallHelpers::TrustedImm32(EXTRA_STACK_SIZE), ARMRegisters::sp);
+
+#   define CALLFRAME_SRC_REG GPRInfo::argumentGPR1
+#   define PREVIOUS_CALLFRAME_REG ARMRegisters::r11
+#elif CPU(ARM_THUMB2)
+    jit.push(ARMRegisters::lr);
+    jit.push(ARMRegisters::r4);
+    jit.push(ARMRegisters::r5);
+    jit.push(ARMRegisters::r6);
+    jit.push(ARMRegisters::r7);
+    jit.push(ARMRegisters::r8);
+    jit.push(ARMRegisters::r9);
+    jit.push(ARMRegisters::r10);
+    jit.push(ARMRegisters::r11);
+    jit.subPtr(CCallHelpers::TrustedImm32(EXTRA_STACK_SIZE), ARMRegisters::sp);
+
+#   define CALLFRAME_SRC_REG GPRInfo::argumentGPR1
+#   define PREVIOUS_CALLFRAME_REG ARMRegisters::r7
+#elif CPU(ARM64)
+    jit.push(ARM64Registers::lr);
+    jit.push(ARM64Registers::x19);
+    jit.push(ARM64Registers::x20);
+    jit.push(ARM64Registers::x21);
+    jit.push(ARM64Registers::x22);
+    jit.push(ARM64Registers::x23);
+    jit.push(ARM64Registers::x24);
+    jit.push(ARM64Registers::x25);
+    jit.push(ARM64Registers::x26);
+    jit.push(ARM64Registers::x27);
+    jit.push(ARM64Registers::x28);
+    jit.push(ARM64Registers::x29);
+
+#   define CALLFRAME_SRC_REG GPRInfo::argumentGPR1
+#   define PREVIOUS_CALLFRAME_REG ARM64Registers::x29
+#elif CPU(MIPS)
+    jit.subPtr(CCallHelpers::TrustedImm32(STACK_LENGTH), MIPSRegisters::sp);
+    jit.storePtr(MIPSRegisters::ra, CCallHelpers::Address(MIPSRegisters::sp, PRESERVED_RETURN_ADDRESS_OFFSET));
+    jit.storePtr(MIPSRegisters::s4, CCallHelpers::Address(MIPSRegisters::sp, PRESERVED_S4_OFFSET));
+    jit.storePtr(MIPSRegisters::s3, CCallHelpers::Address(MIPSRegisters::sp, PRESERVED_S3_OFFSET));
+    jit.storePtr(MIPSRegisters::s2, CCallHelpers::Address(MIPSRegisters::sp, PRESERVED_S2_OFFSET));
+    jit.storePtr(MIPSRegisters::s1, CCallHelpers::Address(MIPSRegisters::sp, PRESERVED_S1_OFFSET));
+    jit.storePtr(MIPSRegisters::s0, CCallHelpers::Address(MIPSRegisters::sp, PRESERVED_S0_OFFSET));
+#if WTF_MIPS_PIC
+    jit.storePtr(MIPSRegisters::gp), CCallHelpers::Address(MIPSRegisters::sp, PRESERVED_GP_OFFSET));
+#endif
+
+#   define CALLFRAME_SRC_REG GPRInfo::argumentGPR1
+#   define PREVIOUS_CALLFRAME_REG MIPSRegisters::fp
+#elif CPU(SH4)
+    jit.push(SH4Registers::fp);
+    jit.push(SH4Registers::pr);
+    jit.push(SH4Registers::r13);
+    jit.push(SH4Registers::r11);
+    jit.push(SH4Registers::r10);
+    jit.push(SH4Registers::r9);
+    jit.push(SH4Registers::r8);
+    jit.subPtr(CCallHelpers::TrustedImm32(EXTRA_STACK_SIZE), SH4Registers::sp);
+    jit.move(SH4Registers::fp, GPRInfo::nonArgGPR0);
+
+#   define CALLFRAME_SRC_REG GPRInfo::argumentGPR1
+#   define PREVIOUS_CALLFRAME_REG GPRInfo::nonArgGPR0
+#endif
+
+    if (CALLFRAME_SRC_REG != GPRInfo::callFrameRegister)
+        jit.move(CALLFRAME_SRC_REG, GPRInfo::callFrameRegister);
+    jit.loadPtr(CCallHelpers::Address(GPRInfo::callFrameRegister), GPRInfo::nonArgGPR1);
+    jit.storePtr(PREVIOUS_CALLFRAME_REG, CCallHelpers::Address(GPRInfo::nonArgGPR1));
+
+#if USE(JSVALUE64)
+    jit.move(CCallHelpers::TrustedImm64(0xffff000000000000L), GPRInfo::tagTypeNumberRegister);
+    jit.move(CCallHelpers::TrustedImm64(0xffff000000000002L), GPRInfo::tagMaskRegister);
+#endif
+
+#if defined(CALL_CODE)
+    CALL_CODE;
+#else
+    jit.call(GPRInfo::argumentGPR0);
+#endif
+
+#undef CALLFRAME_SRC_REG
+#undef CALL_CODE
+#undef PREVIOUS_CALLFRAME_REG
+
+    returnFromJavaScript(jit);
+
+    LinkBuffer patchBuffer(*vm, &jit, GLOBAL_THUNK_ID);
+    return FINALIZE_CODE(patchBuffer, ("Call to JavaScript thunk"));
+}
+
+MacroAssemblerCodeRef throwNotCaught(VM* vm)
+{
+    CCallHelpers jit(vm);
+
+    returnFromJavaScript(jit);
+
+    LinkBuffer patchBuffer(*vm, &jit, GLOBAL_THUNK_ID);
+    return FINALIZE_CODE(patchBuffer, ("Uncaught throw thunk"));
+}
+
 // We will jump here if the JIT code tries to make a call, but the
 // linking helper (C++ code) decides to throw an exception instead.
 MacroAssemblerCodeRef throwExceptionFromCallSlowPathGenerator(VM* vm)

Source/JavaScriptCore/GNUmakefile.list.am
@@ -662,10 +662,7 @@ javascriptcore_sources += \
 	Source/JavaScriptCore/jit/JITStubs.cpp \
 	Source/JavaScriptCore/jit/JITStubs.h \
 	Source/JavaScriptCore/jit/JITStubsARM.h \
-	Source/JavaScriptCore/jit/JITStubsARM64.h \
 	Source/JavaScriptCore/jit/JITStubsARMv7.h \
-	Source/JavaScriptCore/jit/JITStubsMIPS.h \
-	Source/JavaScriptCore/jit/JITStubsSH4.h \
 	Source/JavaScriptCore/jit/JITStubsX86.h \
 	Source/JavaScriptCore/jit/JITStubsX86_64.h \
 	Source/JavaScriptCore/jit/JITStubsX86Common.h \

Source/JavaScriptCore/JavaScriptCore.xcodeproj/project.pbxproj
@@ -714,7 +714,6 @@
 		371D842D17C98B6E00ECF994 /* libz.dylib in Frameworks */ = {isa = PBXBuildFile; fileRef = 371D842C17C98B6E00ECF994 /* libz.dylib */; };
 		41359CF30FDD89AD00206180 /* DateConversion.h in Headers */ = {isa = PBXBuildFile; fileRef = D21202290AD4310C00ED79B6 /* DateConversion.h */; };
 		4443AE3316E188D90076F110 /* Foundation.framework in Frameworks */ = {isa = PBXBuildFile; fileRef = 51F0EB6105C86C6B00E6DF1B /* Foundation.framework */; };
-		44DF93E617A1D9200097B97B /* JITStubsARM64.h in Headers */ = {isa = PBXBuildFile; fileRef = 44DF93E517A1D9200097B97B /* JITStubsARM64.h */; };
 		451539B912DC994500EF7AC4 /* Yarr.h in Headers */ = {isa = PBXBuildFile; fileRef = 451539B812DC994500EF7AC4 /* Yarr.h */; settings = {ATTRIBUTES = (Private, ); }; };
 		5D53726F0E1C54880021E549 /* Tracing.h in Headers */ = {isa = PBXBuildFile; fileRef = 5D53726E0E1C54880021E549 /* Tracing.h */; };
 		5D5D8AD10E0D0EBE00F9C692 /* libedit.dylib in Frameworks */ = {isa = PBXBuildFile; fileRef = 5D5D8AD00E0D0EBE00F9C692 /* libedit.dylib */; };
@@ -969,8 +968,6 @@
 		A78A9781179738D5009DF744 /* FTLJITFinalizer.h in Headers */ = {isa = PBXBuildFile; fileRef = A78A977E179738D5009DF744 /* FTLJITFinalizer.h */; };
 		A7986D5717A0BB1E00A95DD0 /* DFGEdgeUsesStructure.h in Headers */ = {isa = PBXBuildFile; fileRef = A7986D5617A0BB1E00A95DD0 /* DFGEdgeUsesStructure.h */; settings = {ATTRIBUTES = (Private, ); }; };
 		A7A4AE0817973B26005612B1 /* MacroAssemblerX86Common.cpp in Sources */ = {isa = PBXBuildFile; fileRef = A7A4AE0717973B26005612B1 /* MacroAssemblerX86Common.cpp */; };
-		A7A4AE0D17973B4D005612B1 /* JITStubsMIPS.h in Headers */ = {isa = PBXBuildFile; fileRef = A7A4AE0917973B4D005612B1 /* JITStubsMIPS.h */; };
-		A7A4AE0F17973B4D005612B1 /* JITStubsSH4.h in Headers */ = {isa = PBXBuildFile; fileRef = A7A4AE0B17973B4D005612B1 /* JITStubsSH4.h */; };
 		A7A4AE1017973B4D005612B1 /* JITStubsX86Common.h in Headers */ = {isa = PBXBuildFile; fileRef = A7A4AE0C17973B4D005612B1 /* JITStubsX86Common.h */; };
 		A7A8AF3417ADB5F3005AB174 /* ArrayBuffer.cpp in Sources */ = {isa = PBXBuildFile; fileRef = A7A8AF2517ADB5F2005AB174 /* ArrayBuffer.cpp */; };
 		A7A8AF3517ADB5F3005AB174 /* ArrayBuffer.h in Headers */ = {isa = PBXBuildFile; fileRef = A7A8AF2617ADB5F3005AB174 /* ArrayBuffer.h */; settings = {ATTRIBUTES = (Private, ); }; };
@@ -1943,7 +1940,6 @@
 		2AD8932917E3868F00668276 /* HeapIterationScope.h */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.c.h; path = HeapIterationScope.h; sourceTree = "<group>"; };
 		371D842C17C98B6E00ECF994 /* libz.dylib */ = {isa = PBXFileReference; lastKnownFileType = "compiled.mach-o.dylib"; name = libz.dylib; path = usr/lib/libz.dylib; sourceTree = SDKROOT; };
 		449097EE0F8F81B50076A327 /* FeatureDefines.xcconfig */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = text.xcconfig; path = FeatureDefines.xcconfig; sourceTree = "<group>"; };
-		44DF93E517A1D9200097B97B /* JITStubsARM64.h */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.c.h; path = JITStubsARM64.h; sourceTree = "<group>"; };
 		451539B812DC994500EF7AC4 /* Yarr.h */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.c.h; name = Yarr.h; path = yarr/Yarr.h; sourceTree = "<group>"; };
 		45E12D8806A49B0F00E9DF84 /* jsc.cpp */ = {isa = PBXFileReference; fileEncoding = 30; indentWidth = 4; lastKnownFileType = sourcecode.cpp.cpp; path = jsc.cpp; sourceTree = "<group>"; tabWidth = 4; };
 		51F0EB6105C86C6B00E6DF1B /* Foundation.framework */ = {isa = PBXFileReference; lastKnownFileType = wrapper.framework; name = Foundation.framework; path = /System/Library/Frameworks/Foundation.framework; sourceTree = "<absolute>"; };
@@ -2253,8 +2249,6 @@
 		A79E781F15EECBA80047C855 /* UnlinkedCodeBlock.h */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.c.h; path = UnlinkedCodeBlock.h; sourceTree = "<group>"; };
 		A79EDB0811531CD60019E912 /* JSObjectRefPrivate.h */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.c.h; path = JSObjectRefPrivate.h; sourceTree = "<group>"; };
 		A7A4AE0717973B26005612B1 /* MacroAssemblerX86Common.cpp */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.cpp.cpp; path = MacroAssemblerX86Common.cpp; sourceTree = "<group>"; };
-		A7A4AE0917973B4D005612B1 /* JITStubsMIPS.h */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.c.h; path = JITStubsMIPS.h; sourceTree = "<group>"; };
-		A7A4AE0B17973B4D005612B1 /* JITStubsSH4.h */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.c.h; path = JITStubsSH4.h; sourceTree = "<group>"; };
 		A7A4AE0C17973B4D005612B1 /* JITStubsX86Common.h */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.c.h; path = JITStubsX86Common.h; sourceTree = "<group>"; };
 		A7A7EE7411B98B8D0065A14F /* ASTBuilder.h */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.c.h; path = ASTBuilder.h; sourceTree = "<group>"; };
 		A7A7EE7711B98B8D0065A14F /* SyntaxChecker.h */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.c.h; path = SyntaxChecker.h; sourceTree = "<group>"; };
@@ -2935,10 +2929,7 @@
 				14A23D6C0F4E19CE0023CDAD /* JITStubs.cpp */,
 				14A6581A0F4E36F4000150FD /* JITStubs.h */,
 				FEF6835A174343CC00A32E25 /* JITStubsARM.h */,
-				44DF93E517A1D9200097B97B /* JITStubsARM64.h */,
 				FEF6835B174343CC00A32E25 /* JITStubsARMv7.h */,
-				A7A4AE0917973B4D005612B1 /* JITStubsMIPS.h */,
-				A7A4AE0B17973B4D005612B1 /* JITStubsSH4.h */,
 				FEF6835D174343CC00A32E25 /* JITStubsX86.h */,
 				FEF6835C174343CC00A32E25 /* JITStubsX86_64.h */,
 				A7A4AE0C17973B4D005612B1 /* JITStubsX86Common.h */,
@@ -4410,10 +4401,7 @@
 				0F766D2C15A8CC3A008F363E /* JITStubRoutineSet.h in Headers */,
 				14C5242B0F5355E900BA3D04 /* JITStubs.h in Headers */,
 				FEF6835E174343CC00A32E25 /* JITStubsARM.h in Headers */,
-				44DF93E617A1D9200097B97B /* JITStubsARM64.h in Headers */,
 				FEF6835F174343CC00A32E25 /* JITStubsARMv7.h in Headers */,
-				A7A4AE0D17973B4D005612B1 /* JITStubsMIPS.h in Headers */,
-				A7A4AE0F17973B4D005612B1 /* JITStubsSH4.h in Headers */,
 				FEF68361174343CC00A32E25 /* JITStubsX86.h in Headers */,
 				FEF68360174343CC00A32E25 /* JITStubsX86_64.h in Headers */,
 				A7A4AE1017973B4D005612B1 /* JITStubsX86Common.h in Headers */,

Source/JavaScriptCore/dfg/DFGDriver.cpp
@@ -78,6 +78,8 @@ static CompilationResult compileImpl(
     
     // Make sure that any stubs that the DFG is going to use are initialized. We want to
     // make sure that all JIT code generation does finalization on the main thread.
+    vm.getCTIStub(callToJavaScript);
+    vm.getCTIStub(throwNotCaught);
     vm.getCTIStub(osrExitGenerationThunkGenerator);
     vm.getCTIStub(throwExceptionFromCallSlowPathGenerator);
     vm.getCTIStub(linkCallThunkGenerator);

Source/JavaScriptCore/jit/JITCode.cpp
@@ -27,6 +27,7 @@
 #include "JITCode.h"
 
 #include "Operations.h"
+#include "ThunkGenerators.h"
 #include <wtf/PrintStream.h>
 
 namespace JSC {
@@ -43,7 +44,9 @@ JITCode::~JITCode()
 #if ENABLE(JIT)
 JSValue JITCode::execute(JSStack* stack, CallFrame* callFrame, VM* vm)
 {
-    JSValue result = JSValue::decode(ctiTrampoline(executableAddress(), stack, callFrame, 0, 0, vm));
+    UNUSED_PARAM(stack);
+
+    JSValue result = JSValue::decode(vm->callJavaScriptJITFunction(executableAddress(), callFrame));
     return vm->exception() ? jsNull() : result;
 }
 #endif

Source/JavaScriptCore/jit/JITExceptions.cpp
@@ -49,7 +49,7 @@ void genericUnwind(VM* vm, ExecState* callFrame, JSValue exceptionValue)
         catchPCForInterpreter = &callFrame->codeBlock()->instructions()[handler->target];
         catchRoutine = ExecutableBase::catchRoutineFor(handler, catchPCForInterpreter);
     } else
-        catchRoutine = FunctionPtr(LLInt::getCodePtr(ctiOpThrowNotCaught)).value();
+        catchRoutine = vm->getCTIStub(throwNotCaught).code().executableAddress();
     
     vm->callFrameForThrow = callFrame;
     vm->targetMachinePCForThrow = catchRoutine;

Source/JavaScriptCore/jit/JITStubs.cpp
@@ -33,82 +33,22 @@
 #if ENABLE(JIT)
 #include "JITStubs.h"
 
-#include "Arguments.h"
-#include "ArrayConstructor.h"
-#include "CallFrame.h"
-#include "CallFrameInlines.h"
-#include "CodeBlock.h"
-#include "CodeProfiling.h"
-#include "CommonSlowPaths.h"
-#include "DFGCompilationMode.h"
-#include "DFGDriver.h"
-#include "DFGOSREntry.h"
-#include "DFGWorklist.h"
-#include "Debugger.h"
-#include "DeferGC.h"
-#include "ErrorInstance.h"
-#include "ExceptionHelpers.h"
-#include "GetterSetter.h"
-#include "Heap.h"
-#include <wtf/InlineASM.h>
-#include "JIT.h"
-#include "JITExceptions.h"
-#include "JITToDFGDeferredCompilationCallback.h"
-#include "JSActivation.h"
-#include "JSArray.h"
-#include "JSFunction.h"
-#include "JSGlobalObjectFunctions.h"
-#include "JSNameScope.h"
-#include "JSNotAnObject.h"
-#include "JSPropertyNameIterator.h"
-#include "JSString.h"
-#include "JSWithScope.h"
-#include "LegacyProfiler.h"
-#include "NameInstance.h"
-#include "ObjectConstructor.h"
-#include "ObjectPrototype.h"
-#include "Operations.h"
-#include "Parser.h"
-#include "RegExpObject.h"
-#include "RegExpPrototype.h"
-#include "Register.h"
-#include "RepatchBuffer.h"
-#include "SamplingTool.h"
-#include "SlowPathCall.h"
-#include "Strong.h"
-#include "StructureRareDataInlines.h"
-#include <wtf/StdLibExtras.h>
-#include <stdarg.h>
-#include <stdio.h>
-
-using namespace std;
-
 #if CPU(ARM_TRADITIONAL)
 #include "JITStubsARM.h"
 #elif CPU(ARM_THUMB2)
 #include "JITStubsARMv7.h"
-#elif CPU(MIPS)
-#include "JITStubsMIPS.h"
-#elif CPU(SH4)
-#include "JITStubsSH4.h"
 #elif CPU(X86)
 #include "JITStubsX86.h"
 #elif CPU(X86_64)
 #include "JITStubsX86_64.h"
 #elif CPU(ARM64)
-#include "JITStubsARM64.h"
+// There isn't an ARM64 specific .h file
+#elif CPU(MIPS)
+// There isn't a MIPS specific .h file
+#elif CPU(SH4)
+// There isn't an SH4 specific .h file
 #else
 #error "JIT not supported on this platform."
 #endif
-
-namespace JSC {
-
-#if ENABLE(OPCODE_SAMPLING)
-    #define CTI_SAMPLER stackFrame.vm->interpreter->sampler()
-#else
-    #define CTI_SAMPLER 0
-#endif
-
-} // namespace JSC
 
 #endif // ENABLE(JIT)

Source/JavaScriptCore/jit/JITStubs.h
@@ -30,57 +30,9 @@
 #ifndef JITStubs_h
 #define JITStubs_h
 
-#include "CallData.h"
-#include "Intrinsic.h"
-#include "JITExceptions.h"
-#include "LowLevelInterpreter.h"
-#include "MacroAssemblerCodeRef.h"
-#include "Register.h"
-
 namespace JSC {
 
 #if ENABLE(JIT)
-
-struct StructureStubInfo;
-
-class ArrayAllocationProfile;
-class CodeBlock;
-class ExecutablePool;
-class FunctionExecutable;
-class Identifier;
-class VM;
-class JSGlobalObject;
-class JSObject;
-class JSPropertyNameIterator;
-class JSStack;
-class JSValue;
-class JSValueEncodedAsPointer;
-class LegacyProfiler;
-class NativeExecutable;
-class PropertySlot;
-class PutPropertySlot;
-class RegExp;
-class Structure;
-
-#define STUB_ARGS_DECLARATION void** args
-#define STUB_ARGS (args)
-
-#if CPU(X86)
-#if COMPILER(MSVC)
-#define JIT_STUB __fastcall
-#elif COMPILER(GCC)
-#define JIT_STUB  __attribute__ ((fastcall))
-#elif COMPILER(SUNCC)
-#define JIT_STUB
-#else
-#error "JIT_STUB function calls require fastcall conventions on x86, add appropriate directive/attribute here for your compiler!"
-#endif
-#else
-#define JIT_STUB
-#endif
-
-extern "C" void ctiOpThrowNotCaught();
-extern "C" EncodedJSValue ctiTrampoline(void* code, JSStack*, CallFrame*, void* /*unused1*/, void* /*unused2*/, VM*);
 
 #if USE(MASM_PROBE)
 extern "C" void ctiMasmProbeTrampoline();

commit 4da0da40d8ee9f183a763f6df671e47d921cc14e

[mips] Fix typo (introduced in r158751). https://bugs.webkit.org/show_bug.cgi?id=124033.
Patch by Julien Brianceau <jbriance@cisco.com> on 2013-11-08
Reviewed by Csaba Osztrogonác.

* jit/ThunkGenerators.cpp:
(JSC::callToJavaScript):

git-svn-id: http://svn.webkit.org/repository/webkit/trunk@158916 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Source/JavaScriptCore/jit/ThunkGenerators.cpp
@@ -267,7 +267,7 @@ MacroAssemblerCodeRef callToJavaScript(VM* vm)
     jit.storePtr(MIPSRegisters::s1, CCallHelpers::Address(MIPSRegisters::sp, PRESERVED_S1_OFFSET));
     jit.storePtr(MIPSRegisters::s0, CCallHelpers::Address(MIPSRegisters::sp, PRESERVED_S0_OFFSET));
 #if WTF_MIPS_PIC
-    jit.storePtr(MIPSRegisters::gp), CCallHelpers::Address(MIPSRegisters::sp, PRESERVED_GP_OFFSET));
+    jit.storePtr(MIPSRegisters::gp, CCallHelpers::Address(MIPSRegisters::sp, PRESERVED_GP_OFFSET));
 #endif
     jit.move(MIPSRegisters::fp, GPRInfo::nonArgGPR0);
 