WebKit/WebKit-http

commit 4655f7909a1edd2f879d1cf12593a5974e74a273

Change callToJavaScript thunk into an offline assembled stub https://bugs.webkit.org/show_bug.cgi?id=124251
Reviewed by Geoffrey Garen.

Changed callToJavaScript and throwNotCaught into stubs generated by the offline assembler.
Added popCalleeSaves and pushCalleeSaves pseudo ops to the offline assembler to handle
the saving and restoring of callee save registers.  Fixed callFrameRegister differences
between arm traditional (r11) and arm Thumb2 (r7) in GPRInfo.h.  Also fixed implementation
of pop & push in arm.rb.

Since the offline assembler and therefore the LLInt don't work on Windows, the Windows stubs
are handled as inline assembly in JITStubsX86.h and JITStubsMSVC64.asm.

* dfg/DFGDriver.cpp:
(JSC::DFG::compileImpl):
* jit/GPRInfo.h:
(JSC::GPRInfo::toIndex):
(JSC::GPRInfo::debugName):
* jit/JITCode.cpp:
(JSC::JITCode::execute):
* jit/JITExceptions.cpp:
(JSC::genericUnwind):
* jit/JITStubs.h:
* jit/JITStubsMSVC64.asm:
* jit/JITStubsX86.h:
* jit/ThunkGenerators.cpp:
* jit/ThunkGenerators.h:
* llint/LLIntThunks.h:
* llint/LowLevelInterpreter.asm:
* llint/LowLevelInterpreter32_64.asm:
* llint/LowLevelInterpreter64.asm:
* offlineasm/arm.rb:
* offlineasm/arm64.rb:
* offlineasm/instructions.rb:
* offlineasm/mips.rb:
* offlineasm/registers.rb:
* offlineasm/sh4.rb:
* offlineasm/x86.rb:
* runtime/VM.cpp:
(JSC::VM::VM):
* runtime/VM.h:


git-svn-id: http://svn.webkit.org/repository/webkit/trunk@159276 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Source/JavaScriptCore/offlineasm/sh4.rb
@@ -61,6 +61,10 @@ class RegisterID
             "r4"
         when "a1"
             "r5"
+        when "a2"
+            "r6"
+        when "a3"
+            "r7"
         when "t0"
             "r0"
         when "t1"
@@ -70,7 +74,9 @@ class RegisterID
         when "t3"
             "r10"
         when "t4"
-            "r6"
+            "r4"
+        when "t5"
+            "r5"
         when "cfr"
             "r14"
         when "sp"
@@ -905,6 +911,22 @@ class Instruction
             $asm.puts "lds #{sh4Operands(operands)}, pr"
         when "stspr"
             $asm.puts "sts pr, #{sh4Operands(operands)}"
+        when "popCalleeSaves"
+            $asm.puts "mov.l @r15+, r8"
+            $asm.puts "mov.l @r15+, r9"
+            $asm.puts "mov.l @r15+, r10"
+            $asm.puts "mov.l @r15+, r11"
+            $asm.puts "mov.l @r15+, r13"
+            $asm.puts "lds.l @r15+,pr"
+            $asm.puts "mov.l @r15+, fp"
+        when "pushCalleeSaves"
+            $asm.puts "mov.l fp, @-r15"
+            $asm.puts "sts.l pr, @-r15"
+            $asm.puts "mov.l r13, @-r15"
+            $asm.puts "mov.l r11, @-r15"
+            $asm.puts "mov.l r10, @-r15"
+            $asm.puts "mov.l r9, @-r15"
+            $asm.puts "mov.l r8, @-r15"
         when "break"
             # This special opcode always generates an illegal instruction exception.
             $asm.puts ".word 0xfffd"

Source/JavaScriptCore/dfg/DFGDriver.cpp
@@ -78,8 +78,6 @@ static CompilationResult compileImpl(
     
     // Make sure that any stubs that the DFG is going to use are initialized. We want to
     // make sure that all JIT code generation does finalization on the main thread.
-    vm.getCTIStub(callToJavaScript);
-    vm.getCTIStub(throwNotCaught);
     vm.getCTIStub(osrExitGenerationThunkGenerator);
     vm.getCTIStub(throwExceptionFromCallSlowPathGenerator);
     vm.getCTIStub(linkCallThunkGenerator);

Source/JavaScriptCore/jit/GPRInfo.h
@@ -460,7 +460,11 @@ public:
     static const GPRReg regT4 = ARMRegisters::r8;
     static const GPRReg regT5 = ARMRegisters::r9;
     static const GPRReg regT6 = ARMRegisters::r10;
-    static const GPRReg regT7 = ARMRegisters::r5;
+#if CPU(ARM_THUMB2)
+    static const GPRReg regT7 = ARMRegisters::r11;
+#else 
+    static const GPRReg regT7 = ARMRegisters::r7;
+#endif
     static const GPRReg regT8 = ARMRegisters::r3;
     // These registers match the baseline JIT.
     static const GPRReg cachedResultRegister = regT0;
@@ -488,8 +492,13 @@ public:
     static unsigned toIndex(GPRReg reg)
     {
         ASSERT(reg != InvalidGPRReg);
-        ASSERT(reg < 16);
-        static const unsigned indexForRegister[16] = { 0, 1, 2, 8, 3, 7, InvalidIndex, InvalidIndex, 4, 5, 6, InvalidIndex, InvalidIndex, InvalidIndex, InvalidIndex, InvalidIndex };
+        ASSERT(static_cast<int>(reg) < 16);
+        static const unsigned indexForRegister[16] =
+#if CPU(ARM_THUMB2)
+            { 0, 1, 2, 8, 3, 9, InvalidIndex, InvalidIndex, 4, 5, 6, 7, InvalidIndex, InvalidIndex, InvalidIndex, InvalidIndex };
+#else
+            { 0, 1, 2, 8, 3, 9, InvalidIndex, 7, 4, 5, 6, InvalidIndex, InvalidIndex, InvalidIndex, InvalidIndex, InvalidIndex };
+#endif
         unsigned result = indexForRegister[reg];
         ASSERT(result != InvalidIndex);
         return result;
@@ -498,7 +507,7 @@ public:
     static const char* debugName(GPRReg reg)
     {
         ASSERT(reg != InvalidGPRReg);
-        ASSERT(reg < 16);
+        ASSERT(static_cast<int>(reg) < 16);
         static const char* nameForRegister[16] = {
             "r0", "r1", "r2", "r3",
             "r4", "r5", "r6", "r7",

Source/JavaScriptCore/jit/JITCode.cpp
@@ -26,8 +26,8 @@
 #include "config.h"
 #include "JITCode.h"
 
+#include "LLIntThunks.h"
 #include "Operations.h"
-#include "ThunkGenerators.h"
 #include <wtf/PrintStream.h>
 
 namespace JSC {
@@ -46,7 +46,7 @@ JSValue JITCode::execute(JSStack* stack, CallFrame* callFrame, VM* vm)
 {
     UNUSED_PARAM(stack);
 
-    JSValue result = JSValue::decode(vm->callJavaScriptJITFunction(executableAddress(), callFrame));
+    JSValue result = JSValue::decode(callToJavaScript(executableAddress(), callFrame));
     return vm->exception() ? jsNull() : result;
 }
 #endif

Source/JavaScriptCore/jit/JITExceptions.cpp
@@ -30,7 +30,9 @@
 #include "CallFrameInlines.h"
 #include "CodeBlock.h"
 #include "Interpreter.h"
+#include "JITStubs.h"
 #include "JSCJSValue.h"
+#include "LLIntThunks.h"
 #include "VM.h"
 #include "Operations.h"
 
@@ -49,7 +51,7 @@ void genericUnwind(VM* vm, ExecState* callFrame, JSValue exceptionValue)
         catchPCForInterpreter = &callFrame->codeBlock()->instructions()[handler->target];
         catchRoutine = ExecutableBase::catchRoutineFor(handler, catchPCForInterpreter);
     } else
-        catchRoutine = vm->getCTIStub(throwNotCaught).code().executableAddress();
+        catchRoutine = FunctionPtr(LLInt::getCodePtr(returnFromJavaScript)).value();
     
     vm->callFrameForThrow = callFrame;
     vm->targetMachinePCForThrow = catchRoutine;

Source/JavaScriptCore/jit/JITStubs.h
@@ -30,9 +30,20 @@
 #ifndef JITStubs_h
 #define JITStubs_h
 
+#include "JSCJSValue.h"
+
 namespace JSC {
 
 #if ENABLE(JIT)
+
+#if OS(WINDOWS)
+class ExecState;
+
+extern "C" {
+    EncodedJSValue callToJavaScript(void*, ExecState*);
+    void returnFromJavaScript();
+}
+#endif
 
 #if USE(MASM_PROBE)
 extern "C" void ctiMasmProbeTrampoline();

Source/JavaScriptCore/jit/JITStubsMSVC64.asm
@@ -25,10 +25,54 @@
 
 EXTERN getHostCallReturnValueWithExecState : near
 
+PUBLIC callToJavaScript
+PUBLIC returnFromJavaScript
 PUBLIC getHostCallReturnValue
 
 _TEXT   SEGMENT
 
+callToJavaScript PROC
+    push rbp
+    mov rbp, rax ; Save previous frame pointer
+    mov rbp, rsp
+    push r12
+    push r13
+    push r14
+    push r15
+    push rbx
+
+    ; JIT operations can use up to 6 args (4 in registers and 2 on the stack).
+    ; In addition, X86_64 ABI specifies that the worse case stack alignment
+    ; requirement is 32 bytes. Based on these factors, we need to pad the stack
+    ; an additional 28h bytes.
+    sub rsp, 28h
+    mov rbp, rdx
+    mov r11, qword ptr[rbp] ; Put the previous frame pointer in the sentinel call frame above us
+    mov qword ptr[r11], rax
+    mov r14, 0FFFF000000000000h
+    mov r15, 0FFFF000000000002h
+    call rcx
+    add rsp, 28h
+    pop rbx
+    pop r15
+    pop r14
+    pop r13
+    pop r12
+    pop rbp
+    ret
+callToJavaScript ENDP
+
+returnFromJavaScript PROC
+    add rsp, 28h
+    pop rbx
+    pop r15
+    pop r14
+    pop r13
+    pop r12
+    pop rbp
+    ret
+returnFromJavaScript ENDP
+	
 getHostCallReturnValue PROC
     sub r13, 40
     mov r13, rdi

Source/JavaScriptCore/jit/JITStubsX86.h
@@ -200,6 +200,50 @@ SYMBOL_STRING(ctiMasmProbeTrampolineEnd) ":" "\n"
 
 #endif // COMPILER(GCC)
 
+#if COMPILER(MSVC)
+
+extern "C" {
+
+    // FIXME: Since Windows doesn't use the LLInt, we have inline stubs here.
+    // Until the LLInt is changed to support Windows, these stub needs to be updated.
+    __declspec(naked) EncodedJSValue callToJavaScript(void* code, ExecState*)
+    {
+        __asm {
+            push ebp;
+            mov eax, ebp;
+            mov ebp, esp;
+            push esi;
+            push edi;
+            push ebx;
+            sub esp, 0x1c;
+            mov ebp, [esp + 0x34];
+            mov ebx, [ebp];
+            mov [ebx], eax;
+            call [esp + 0x30];
+            add esp, 0x1c;
+            pop ebx;
+            pop edi;
+            pop esi;
+            pop ebp;
+            ret;
+        }
+    }
+
+    __declspec(naked) void returnFromJavaScript()
+    {
+        __asm {
+            add esp, 0x1c;
+            pop ebx;
+            pop edi;
+            pop esi;
+            pop ebp;
+            ret;
+        }
+    }
+}
+
+#endif // COMPILER(MSVC)
+
 } // namespace JSC
 
 #endif // JITStubsX86_h

commit 1886381f819c8136191ceeedb4d88c17f9450f3b

Fix build for sh4 architectures (broken since r159276). https://bugs.webkit.org/show_bug.cgi?id=124344
Patch by Julien Brianceau <jbriance@cisco.com> on 2013-11-14
Reviewed by Csaba Osztrogonác.

* offlineasm/sh4.rb: There is no fp alias for r14 register for sh4.

git-svn-id: http://svn.webkit.org/repository/webkit/trunk@159283 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Source/JavaScriptCore/offlineasm/sh4.rb
@@ -917,10 +917,10 @@ class Instruction
             $asm.puts "mov.l @r15+, r10"
             $asm.puts "mov.l @r15+, r11"
             $asm.puts "mov.l @r15+, r13"
-            $asm.puts "lds.l @r15+,pr"
-            $asm.puts "mov.l @r15+, fp"
+            $asm.puts "lds.l @r15+, pr"
+            $asm.puts "mov.l @r15+, r14"
         when "pushCalleeSaves"
-            $asm.puts "mov.l fp, @-r15"
+            $asm.puts "mov.l r14, @-r15"
             $asm.puts "sts.l pr, @-r15"
             $asm.puts "mov.l r13, @-r15"
             $asm.puts "mov.l r11, @-r15"