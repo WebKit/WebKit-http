e5329c8199b47751ac795d912716ff1deb54736b
Michael Brüning <michael.bruning@digia.com>
2013-11-21 17:22:21 +0100

    Detach image data when returning an ImageFrame as a native image.
    With error analysis and fixing guidance by Allan Sandfeld Jensen.
    
    This caused crashes when a gif was being loaded and the data in the
    backing store for image frame was prematurely freed.
    
    Task-number: QTBUG-31936
    Change-Id: I7f4454db00cbbcf53f18060aa986a843c7522d57
    Reviewed-by: Jocelyn Turcotte <jocelyn.turcotte@digia.com>

--- Source/WebCore/platform/graphics/qt/ImageDecoderQt.cpp
+++ Source/WebCore/platform/graphics/qt/ImageDecoderQt.cpp
@@ -274,7 +274,7 @@ PassNativeImagePtr ImageFrame::asNewNativeImage() const
 
     QImage img(reinterpret_cast<uchar*>(m_bytes), m_size.width(), m_size.height(), sizeof(PixelData) * m_size.width(), format);
 
-    return new QPixmap(QPixmap::fromImage(img));
+    return new QPixmap(QPixmap::fromImage(img).copy());
 }
 
 }
