23abf7d181c9eef40ce738b1a82248bbfb486f4b
Adrien Destugues <pulkomandy@pulkomandy.tk>
2013-12-02 18:53:58 +0100

    Force SHARED_CORE for Haiku.
    * DumpRenderTree can't work without it, and we would rather use what we
    test.

--- Source/PlatformHaiku.cmake
+++ Source/PlatformHaiku.cmake
@@ -16,3 +16,8 @@ if (ENABLE_INSPECTOR)
     #                   PATTERN "*.gif"
     #                   PATTERN "*.png")
 endif ()
+
+# DRT needs to access WebCore and JSC internals, this can't work in non-shared
+# mode because it ends up linking them twice (in the DRT executable and the 
+# merged libWebKit library).
+set(SHARED_CORE 1)
