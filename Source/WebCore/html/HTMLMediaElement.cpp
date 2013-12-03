WebKit/WebKit-http

commit bff8689affdf2a62802d268da3544860e915d528

[PluginProxy] Add a setting to disable video plugin proxy support in HTMLMediaElement. https://bugs.webkit.org/show_bug.cgi?id=123621
Reviewed by Eric Carlson.

Source/WebCore:

Add a new Setting which will disable the video plugin proxy. Enable support for AVFoundation
in iOS (which requries fixing a few compile errors resulting from classes and methods which
are not available on iOS.

* WebCore.exp.in: Export wkAVAssetResolvedURL.
* css/StyleResolver.cpp:
(WebCore::StyleResolver::canShareStyleWithElement): Make conditional upon
    new isVideoPluginProxyEnabled() setting.
* html/HTMLMediaElement.cpp:
(WebCore::HTMLMediaElement::parseAttribute): Ditto.
* html/HTMLMediaElement.h:
* html/HTMLVideoElement.cpp:
(WebCore::HTMLVideoElement::createRenderer): Ditto.
(WebCore::HTMLVideoElement::attach): Ditto.
(WebCore::HTMLVideoElement::parseAttribute): Ditto.
(HTMLVideoElement::setDisplayMode): Ditto.
* html/HTMLVideoElement.h:
* page/Settings.cpp:
(WebCore::Settings::setVideoPluginProxyEnabled): Simple setter.
* page/Settings.h:
(WebCore::Settings::isVideoPluginProxyEnabled): Simple getter.
* platform/graphics/MediaPlayer.cpp:
(WebCore::installedMediaEngines): Conditionally add MediaPlayerPrivateIOS and always add
    MediaPlayerPriateAVFoundationObjC.
* platform/graphics/MediaPlayerPrivate.h:
(WebCore::MediaPlayerPrivateInterface::deliverNotification): Add default implementation.
(WebCore::MediaPlayerPrivateInterface::setMediaPlayerProxy): Ditto.
* platform/graphics/avfoundation/objc/MediaPlayerPrivateAVFoundationObjC.h:
* platform/graphics/avfoundation/objc/MediaPlayerPrivateAVFoundationObjC.mm:
(WebCore::MediaPlayerPrivateAVFoundationObjC::MediaPlayerPrivateAVFoundationObjC): Use new
    HAS_ and USE_ macros instead of __MAC_OS_MIN_VERNSION_REQUIRED.
(WebCore::MediaPlayerPrivateAVFoundationObjC::~MediaPlayerPrivateAVFoundationObjC): Ditto.
(WebCore::MediaPlayerPrivateAVFoundationObjC::hasContextRenderer): Ditto.

Source/WebCore/html/HTMLMediaElement.cpp
@@ -547,8 +547,10 @@ void HTMLMediaElement::finishParsingChildren()
     m_parsingInProgress = false;
 
 #if ENABLE(PLUGIN_PROXY_FOR_VIDEO)
-    document().updateStyleIfNeeded();
-    createMediaPlayerProxy();
+    if (shouldUseVideoPluginProxy()) {
+        document().updateStyleIfNeeded();
+        createMediaPlayerProxy();
+    }
 #endif
     
 #if ENABLE(VIDEO_TRACK)
@@ -563,28 +565,28 @@ void HTMLMediaElement::finishParsingChildren()
 bool HTMLMediaElement::rendererIsNeeded(const RenderStyle& style)
 {
 #if ENABLE(PLUGIN_PROXY_FOR_VIDEO)
-    UNUSED_PARAM(style);
-    return true;
-#else
-    return controls() && HTMLElement::rendererIsNeeded(style);
+    if (shouldUseVideoPluginProxy())
+        return true;
 #endif
+    return controls() && HTMLElement::rendererIsNeeded(style);
 }
 
 RenderElement* HTMLMediaElement::createRenderer(PassRef<RenderStyle> style)
 {
 #if ENABLE(PLUGIN_PROXY_FOR_VIDEO)
-    // Setup the renderer if we already have a proxy widget.
-    RenderEmbeddedObject* mediaRenderer = new RenderEmbeddedObject(*this, std::move(style));
-    if (m_proxyWidget) {
-        mediaRenderer->setWidget(m_proxyWidget);
+    if (shouldUseVideoPluginProxy()) {
+        // Setup the renderer if we already have a proxy widget.
+        RenderEmbeddedObject* mediaRenderer = new RenderEmbeddedObject(*this, std::move(style));
+        if (m_proxyWidget) {
+            mediaRenderer->setWidget(m_proxyWidget);
 
-        if (Frame* frame = document().frame())
-            frame->loader().client().showMediaPlayerProxyPlugin(m_proxyWidget.get());
+            if (Frame* frame = document().frame())
+                frame->loader().client().showMediaPlayerProxyPlugin(m_proxyWidget.get());
+        }
+        return mediaRenderer;
     }
-    return mediaRenderer;
-#else
-    return new RenderMedia(*this, std::move(style));
 #endif
+    return new RenderMedia(*this, std::move(style));
 }
 
 bool HTMLMediaElement::childShouldCreateRenderer(const Node* child) const
@@ -653,7 +655,8 @@ void HTMLMediaElement::willAttachRenderers()
     ASSERT(!attached());
 
 #if ENABLE(PLUGIN_PROXY_FOR_VIDEO)
-    m_needWidgetUpdate = true;
+    if (shouldUseVideoPluginProxy())
+        m_needWidgetUpdate = true;
 #endif
 }
 
@@ -681,9 +684,10 @@ void HTMLMediaElement::scheduleDelayedAction(DelayedActionType actionType)
 
     if ((actionType & LoadMediaResource) && !(m_pendingActionFlags & LoadMediaResource)) {
 #if ENABLE(PLUGIN_PROXY_FOR_VIDEO)
-        createMediaPlayerProxy();
+        if (shouldUseVideoPluginProxy())
+            createMediaPlayerProxy();
 #endif
-        
+
         prepareForLoad();
         setFlags(m_pendingActionFlags, LoadMediaResource);
     }
@@ -842,14 +846,15 @@ void HTMLMediaElement::prepareForLoad()
     closeMediaSource();
 #endif
 
-#if !ENABLE(PLUGIN_PROXY_FOR_VIDEO)
-    createMediaPlayer();
-#else
-    if (m_player)
-        m_player->cancelLoad();
-    else
-        createMediaPlayerProxy();
+#if ENABLE(PLUGIN_PROXY_FOR_VIDEO)
+    if (shouldUseVideoPluginProxy())
+        if (m_player)
+            m_player->cancelLoad();
+        else
+            createMediaPlayerProxy();
+    } else
 #endif
+    createMediaPlayer();
 
     // 4 - If the media element's networkState is not set to NETWORK_EMPTY, then run these substeps
     if (m_networkState != NETWORK_EMPTY) {
@@ -1020,10 +1025,11 @@ void HTMLMediaElement::loadNextSourceChild()
         return;
     }
 
-#if !ENABLE(PLUGIN_PROXY_FOR_VIDEO)
-    // Recreate the media player for the new url
-    createMediaPlayer();
+#if ENABLE(PLUGIN_PROXY_FOR_VIDEO)
+    if (!shouldUseVideoPluginProxy())
 #endif
+        // Recreate the media player for the new url
+        createMediaPlayer();
 
     m_loadState = LoadingFromSourceElement;
     loadResource(mediaURL, contentType, keySystem);
@@ -4163,14 +4169,11 @@ void HTMLMediaElement::clearMediaPlayer(int flags)
     removeAllInbandTracks();
 #endif
 
-#if !ENABLE(PLUGIN_PROXY_FOR_VIDEO)
-
 #if ENABLE(MEDIA_SOURCE)
     closeMediaSource();
 #endif
 
     m_player.clear();
-#endif
     stopPeriodicTimers();
     m_loadTimer.stop();
 
@@ -4287,24 +4290,26 @@ void HTMLMediaElement::visibilityStateChanged()
 void HTMLMediaElement::defaultEventHandler(Event* event)
 {
 #if ENABLE(PLUGIN_PROXY_FOR_VIDEO)
-    auto renderer = this->renderer();
-    if (!renderer || !renderer->isWidget())
-        return;
+    if (shouldUseVideoPluginProxy()) {
+        auto renderer = this->renderer();
+        if (!renderer || !renderer->isWidget())
+            return;
 
-    if (Widget* widget = toRenderWidget(renderer)->widget())
-        widget->handleEvent(event);
-#else
-    HTMLElement::defaultEventHandler(event);
+        if (Widget* widget = toRenderWidget(renderer)->widget())
+            widget->handleEvent(event);
+        return;
+    }
 #endif
+    HTMLElement::defaultEventHandler(event);
 }
 
 bool HTMLMediaElement::willRespondToMouseClickEvents()
 {
 #if ENABLE(PLUGIN_PROXY_FOR_VIDEO)
-    return true;
-#else
-    return HTMLElement::willRespondToMouseClickEvents();
+    if (shouldUseVideoPluginProxy())
+        return true;
 #endif
+    return HTMLElement::willRespondToMouseClickEvents();
 }
 
 #if ENABLE(VIDEO_TRACK)
@@ -4724,13 +4729,25 @@ bool HTMLMediaElement::createMediaControls()
 
 void HTMLMediaElement::configureMediaControls()
 {
+#if ENABLE(PLUGIN_PROXY_FOR_VIDEO)
+    if (shouldUseVideoPluginProxy()) {
+        if (m_player)
+            m_player->setControls(controls());
+
+        if (!hasMediaControls() && inDocument())
+            createMediaControls();
+        return;
+    }
+#endif
+
 #if ENABLE(MEDIA_CONTROLS_SCRIPT)
     if (!controls() || !inDocument())
         return;
 
     ensureUserAgentShadowRoot();
     return;
-#elif !ENABLE(PLUGIN_PROXY_FOR_VIDEO)
+#endif
+
     if (!controls() || !inDocument()) {
         if (hasMediaControls())
             mediaControls()->hide();
@@ -4741,13 +4758,6 @@ void HTMLMediaElement::configureMediaControls()
         return;
 
     mediaControls()->show();
-#else 
-    if (m_player)
-        m_player->setControls(controls());
-
-    if (!hasMediaControls() && inDocument())
-        createMediaControls();
-#endif
 }
 
 #if ENABLE(VIDEO_TRACK)
@@ -5184,6 +5194,13 @@ void HTMLMediaElement::removeBehaviorsRestrictionsAfterFirstUserGesture()
 {
     m_restrictions = NoRestrictions;
 }
+
+#if ENABLE(PLUGIN_PROXY_FOR_VIDEO)
+bool HTMLMediaElement::shouldUseVideoPluginProxy() const
+{
+    return document()->settings() && document()->settings()->isVideoPluginProxyEnabled();
+}
+#endif
 
 #if ENABLE(MEDIA_CONTROLS_SCRIPT)
 DOMWrapperWorld& HTMLMediaElement::ensureIsolatedWorld()

Source/WTF/wtf/Platform.h
@@ -965,24 +965,30 @@
 #define ENABLE_BINDING_INTEGRITY 1
 #endif
 
-#if PLATFORM(MAC) && !PLATFORM(IOS)
+#if PLATFORM(IOS) || PLATFORM(MAC)
 #define WTF_USE_AVFOUNDATION 1
 #endif
 
-#if (PLATFORM(IOS) && __IPHONE_OS_VERSION_MIN_REQUIRED >= 60000) || (PLATFORM(MAC) && __MAC_OS_X_VERSION_MIN_REQUIRED >= 1080)
+#if (PLATFORM(IOS) && __IPHONE_OS_VERSION_MIN_REQUIRED >= 60000) || ((PLATFORM(MAC) && !PLATFORM(IOS)) && __MAC_OS_X_VERSION_MIN_REQUIRED >= 1080)
 #define WTF_USE_COREMEDIA 1
+#define HAVE_AVFOUNDATION_VIDEO_OUTPUT 1
 #endif
 
-#if (PLATFORM(MAC) || (OS(WINDOWS) && USE(CG))) && !PLATFORM(IOS) && __MAC_OS_X_VERSION_MIN_REQUIRED >= 1080
+#if (PLATFORM(IOS) && __IPHONE_OS_VERSION_MIN_REQUIRED >= 50000) || ((PLATFORM(MAC) && !PLATFORM(IOS)) || (OS(WINDOWS) && USE(CG)) && __MAC_OS_X_VERSION_MIN_REQUIRED >= 1080)
 #define HAVE_AVFOUNDATION_MEDIA_SELECTION_GROUP 1
 #endif
 
-#if (PLATFORM(MAC) || (OS(WINDOWS) && USE(CG))) && !PLATFORM(IOS) && __MAC_OS_X_VERSION_MIN_REQUIRED >= 1090
+#if (PLATFORM(IOS) && __IPHONE_OS_VERSION_MIN_REQUIRED >= 70000) || (((PLATFORM(MAC) && !PLATFORM(IOS)) || (OS(WINDOWS) && USE(CG))) && __MAC_OS_X_VERSION_MIN_REQUIRED >= 1090)
 #define HAVE_AVFOUNDATION_LEGIBLE_OUTPUT_SUPPORT 1
+#define HAVE_MEDIA_ACCESSIBILITY_FRAMEWORK 1
 #endif
 
-#if (PLATFORM(IOS) && __IPHONE_OS_VERSION_MIN_REQUIRED >= 70000) || ((PLATFORM(MAC) || (OS(WINDOWS) && USE(CG))) && !PLATFORM(IOS) && __MAC_OS_X_VERSION_MIN_REQUIRED >= 1090)
-#define HAVE_MEDIA_ACCESSIBILITY_FRAMEWORK 1
+#if (PLATFORM(IOS) && __IPHONE_OS_VERSION_MIN_REQUIRED >= 60000) || ((PLATFORM(MAC) && !PLATFORM(IOS)) && __MAC_OS_X_VERSION_MIN_REQUIRED >= 1090)
+#define HAVE_AVFOUNDATION_LOADER_DELEGATE 1
+#endif
+
+#if (PLATFORM(MAC) && !PLATFORM(IOS)) && __MAC_OS_X_VERSION_MIN_REQUIRED >= 1080
+#define WTF_USE_VIDEOTOOLBOX 1
 #endif
 
 #if PLATFORM(MAC) || PLATFORM(GTK) || (PLATFORM(WIN) && !USE(WINGDI) && !PLATFORM(WIN_CAIRO))

Source/WebCore/WebCore.exp.in
@@ -2790,6 +2790,8 @@ __ZN7WebCore16ScriptController16createRootObjectEPv
 #if ENABLE(PLUGIN_PROXY_FOR_VIDEO)
 __ZN7WebCore16HTMLMediaElement19deliverNotificationE32MediaPlayerProxyNotificationType
 __ZN7WebCore16HTMLMediaElement19setMediaPlayerProxyEP19WebMediaPlayerProxy
+__ZN7WebCore8Settings24gVideoPluginProxyEnabledE
+__ZN7WebCore8Settings26setVideoPluginProxyEnabledEb
 __ZN7WebCore9HTMLNames8audioTagE
 #endif
 

Source/WebCore/css/StyleResolver.cpp
@@ -187,6 +187,10 @@
 #include "WebVTTElement.h"
 #endif
 
+#if ENABLE(PLUGIN_PROXY_FOR_VIDEO)
+#include "HTMLMediaElement.h"
+#endif
+
 namespace WebCore {
 
 using namespace HTMLNames;
@@ -671,13 +675,15 @@ bool StyleResolver::canShareStyleWithElement(StyledElement* element) const
 #if USE(ACCELERATED_COMPOSITING)
     // Turn off style sharing for elements that can gain layers for reasons outside of the style system.
     // See comments in RenderObject::setStyle().
-    if (element->hasTagName(iframeTag) || element->hasTagName(frameTag) || element->hasTagName(embedTag) || element->hasTagName(objectTag) || element->hasTagName(appletTag) || element->hasTagName(canvasTag)
-#if ENABLE(PLUGIN_PROXY_FOR_VIDEO)
-        // With proxying, the media elements are backed by a RenderEmbeddedObject.
-        || element->hasTagName(videoTag) || isHTMLAudioElement(element)
-#endif
-        )
+    if (element->hasTagName(iframeTag) || element->hasTagName(frameTag) || element->hasTagName(embedTag) || element->hasTagName(objectTag) || element->hasTagName(appletTag) || element->hasTagName(canvasTag))
         return false;
+
+#if ENABLE(PLUGIN_PROXY_FOR_VIDEO)
+    // With proxying, the media elements are backed by a RenderEmbeddedObject.
+    if ((element->hasTagName(videoTag) || element->hasTagName(audioTag)) && toMediaElement(element)->shouldUseVideoPluginProxy())
+        return false;
+#endif
+
 #endif
 
     if (elementHasDirectionAuto(element))

Source/WebCore/html/HTMLMediaElement.h
@@ -383,6 +383,10 @@ public:
 
     void enteredOrExitedFullscreen() { configureMediaControls(); }
 
+#if ENABLE(PLUGIN_PROXY_FOR_VIDEO)
+    bool shouldUseVideoPluginProxy() const;
+#endif
+
 protected:
     HTMLMediaElement(const QualifiedName&, Document&, bool);
     virtual ~HTMLMediaElement();
@@ -427,6 +431,8 @@ protected:
     void endIgnoringTrackDisplayUpdateRequests();
 #endif
 
+    virtual RenderElement* createRenderer(PassRef<RenderStyle>) OVERRIDE;
+
 private:
     void createMediaPlayer();
 
@@ -437,7 +443,6 @@ private:
     virtual bool supportsFocus() const OVERRIDE;
     virtual bool isMouseFocusable() const OVERRIDE;
     virtual bool rendererIsNeeded(const RenderStyle&) OVERRIDE;
-    virtual RenderElement* createRenderer(PassRef<RenderStyle>) OVERRIDE;
     virtual bool childShouldCreateRenderer(const Node*) const OVERRIDE;
     virtual InsertionNotificationRequest insertedInto(ContainerNode&) OVERRIDE;
     virtual void removedFrom(ContainerNode&) OVERRIDE;

Source/WebCore/html/HTMLVideoElement.cpp
@@ -68,25 +68,31 @@ bool HTMLVideoElement::rendererIsNeeded(const RenderStyle& style)
     return HTMLElement::rendererIsNeeded(style); 
 }
 
-#if !ENABLE(PLUGIN_PROXY_FOR_VIDEO)
 RenderElement* HTMLVideoElement::createRenderer(PassRef<RenderStyle> style)
 {
+#if ENABLE(PLUGIN_PROXY_FOR_VIDEO)
+    if (shouldUseVideoPluginProxy())
+        return HTMLMediaElement::createRenderer(arena, style);
+#endif
     return new RenderVideo(*this, std::move(style));
 }
-#endif
 
 void HTMLVideoElement::didAttachRenderers()
 {
     HTMLMediaElement::didAttachRenderers();
 
-#if !ENABLE(PLUGIN_PROXY_FOR_VIDEO)
-    updateDisplayState();
-    if (shouldDisplayPosterImage()) {
-        if (!m_imageLoader)
-            m_imageLoader = adoptPtr(new HTMLImageLoader(this));
-        m_imageLoader->updateFromElement();
-        if (renderer())
-            toRenderImage(renderer())->imageResource()->setCachedImage(m_imageLoader->image()); 
+#if ENABLE(PLUGIN_PROXY_FOR_VIDEO)
+    if (!shouldUseVideoPluginProxy()) {
+#endif
+        updateDisplayState();
+        if (shouldDisplayPosterImage()) {
+            if (!m_imageLoader)
+                m_imageLoader = adoptPtr(new HTMLImageLoader(this));
+            m_imageLoader->updateFromElement();
+            if (renderer())
+                toRenderImage(renderer())->imageResource()->setCachedImage(m_imageLoader->image()); 
+        }
+#if ENABLE(PLUGIN_PROXY_FOR_VIDEO)
     }
 #endif
 }
@@ -114,7 +120,10 @@ void HTMLVideoElement::parseAttribute(const QualifiedName& name, const AtomicStr
         // Force a poster recalc by setting m_displayMode to Unknown directly before calling updateDisplayState.
         HTMLMediaElement::setDisplayMode(Unknown);
         updateDisplayState();
-#if !ENABLE(PLUGIN_PROXY_FOR_VIDEO)
+#if ENABLE(PLUGIN_PROXY_FOR_VIDEO)
+        if (shouldUseVideoPluginProxy())
+            return;
+#endif
         if (shouldDisplayPosterImage()) {
             if (!m_imageLoader)
                 m_imageLoader = adoptPtr(new HTMLImageLoader(this));
@@ -123,7 +132,6 @@ void HTMLVideoElement::parseAttribute(const QualifiedName& name, const AtomicStr
             if (renderer())
                 toRenderImage(renderer())->imageResource()->setCachedImage(0); 
         }
-#endif
     } else
         HTMLMediaElement::parseAttribute(name, value);
 }
@@ -220,10 +228,12 @@ void HTMLVideoElement::setDisplayMode(DisplayMode mode)
             player()->setPoster(poster);
     }
 
-#if !ENABLE(PLUGIN_PROXY_FOR_VIDEO)
+#if ENABLE(PLUGIN_PROXY_FOR_VIDEO)
+    if (shouldUseVideoPluginProxy())
+        return;
+#endif
     if (renderer() && displayMode() != oldMode)
         renderer()->updateFromElement();
-#endif
 }
 
 void HTMLVideoElement::updateDisplayState()

Source/WebCore/html/HTMLVideoElement.h
@@ -75,9 +75,7 @@ private:
     HTMLVideoElement(const QualifiedName&, Document&, bool);
 
     virtual bool rendererIsNeeded(const RenderStyle&) OVERRIDE;
-#if !ENABLE(PLUGIN_PROXY_FOR_VIDEO)
     virtual RenderElement* createRenderer(PassRef<RenderStyle>) OVERRIDE;
-#endif
     virtual void didAttachRenderers() OVERRIDE;
     virtual void parseAttribute(const QualifiedName&, const AtomicString&) OVERRIDE;
     virtual bool isPresentationAttribute(const QualifiedName&) const OVERRIDE;

Source/WebCore/page/Settings.cpp
@@ -81,6 +81,10 @@ bool Settings::gAVFoundationEnabled = false;
 bool Settings::gQTKitEnabled = true;
 #endif
 
+#if ENABLE(PLUGIN_PROXY_FOR_VIDEO)
+bool Settings::gVideoPluginProxyEnabled = true;
+#endif
+
 bool Settings::gMockScrollbarsEnabled = false;
 bool Settings::gUsesOverlayScrollbars = false;
 
@@ -575,6 +579,17 @@ void Settings::setQTKitEnabled(bool enabled)
         return;
 
     gQTKitEnabled = enabled;
+    HTMLMediaElement::resetMediaEngines();
+}
+#endif
+
+#if ENABLE(PLUGIN_PROXY_FOR_VIDEO)
+void Settings::setVideoPluginProxyEnabled(bool enabled)
+{
+    if (gVideoPluginProxyEnabled == enabled)
+        return;
+
+    gVideoPluginProxyEnabled = enabled;
     HTMLMediaElement::resetMediaEngines();
 }
 #endif

commit 16e99a3f34bab47cbdddb787b56d1afa003f092d

Fix regression caused by r158599 https://bugs.webkit.org/show_bug.cgi?id=125188
Reviewed by Jer Noble.

* html/HTMLMediaElement.cpp:
(HTMLMediaElement::clearMediaPlayer): Do not clear m_player when PLUGIN_PROXY_FOR_VIDEO
    is enabled.


git-svn-id: http://svn.webkit.org/repository/webkit/trunk@160046 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Source/WebCore/html/HTMLMediaElement.cpp
@@ -4212,7 +4212,11 @@ void HTMLMediaElement::clearMediaPlayer(int flags)
     closeMediaSource();
 #endif
 
+#if ENABLE(PLUGIN_PROXY_FOR_VIDEO)
+    if (!shouldUseVideoPluginProxy())
+#endif
     m_player.clear();
+
     stopPeriodicTimers();
     m_loadTimer.stop();
 