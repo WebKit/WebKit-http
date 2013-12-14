WebKit/WebKit-http

commit 9f6bcaa3090a2d76ee6caeea55b6bcab24d1e70f

Simplify SVG animated type handling in the JSC bindings generator https://bugs.webkit.org/show_bug.cgi?id=118845
Reviewed by Kentaro Hara.

Simplify IsSVGAnimatedType subroutine in the bindings generator so that
we no longer need to hardcode SVG animated types. Also remove the
CanUseFastAttribute subroutine as it is equivalent to IsSVGAnimatedType.
This allows us to simplify the GetterExpression subroutine so that
we can use fastHasAttribute() unconditionally for boolean type.

No new tests, no behavior change.

* bindings/scripts/CodeGenerator.pm:
(IsSVGAnimatedType):
(GetterExpression):


git-svn-id: http://svn.webkit.org/repository/webkit/trunk@152845 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Source/WebCore/bindings/scripts/CodeGenerator.pm
@@ -66,14 +66,6 @@ my %enumTypeHash = ();
 
 my %nonPointerTypeHash = ("DOMTimeStamp" => 1, "CompareHow" => 1);
 
-my %svgAnimatedTypeHash = ("SVGAnimatedAngle" => 1, "SVGAnimatedBoolean" => 1,
-                           "SVGAnimatedEnumeration" => 1, "SVGAnimatedInteger" => 1,
-                           "SVGAnimatedLength" => 1, "SVGAnimatedLengthList" => 1,
-                           "SVGAnimatedNumber" => 1, "SVGAnimatedNumberList" => 1,
-                           "SVGAnimatedPreserveAspectRatio" => 1,
-                           "SVGAnimatedRect" => 1, "SVGAnimatedString" => 1,
-                           "SVGAnimatedTransformList" => 1);
-
 my %svgAttributesInHTMLHash = ("class" => 1, "id" => 1, "onabort" => 1, "onclick" => 1,
                                "onerror" => 1, "onload" => 1, "onmousedown" => 1,
                                "onmouseenter" => 1, "onmouseleave" => 1,
@@ -430,8 +422,7 @@ sub IsSVGAnimatedType
     my $object = shift;
     my $type = shift;
 
-    return 1 if $svgAnimatedTypeHash{$type};
-    return 0;
+    return $type =~ /^SVGAnimated/;
 }
 
 sub GetSequenceType
@@ -548,16 +539,6 @@ sub ContentAttributeName
     return "WebCore::${namespace}::${contentAttributeName}Attr";
 }
 
-sub CanUseFastAttribute
-{
-    my ($generator, $attribute) = @_;
-    my $attributeType = $attribute->signature->type;
-    # HTMLNames::styleAttr cannot be used with fast{Get,Has}Attribute but we do not [Reflect] the
-    # style attribute.
-
-    return !$generator->IsSVGAnimatedType($attributeType);
-}
-
 sub GetterExpression
 {
     my ($generator, $implIncludes, $interfaceName, $attribute) = @_;
@@ -573,11 +554,7 @@ sub GetterExpression
         $functionName = "getURLAttribute";
     } elsif ($attribute->signature->type eq "boolean") {
         my $namespace = $generator->NamespaceForAttributeName($interfaceName, $contentAttributeName);
-        if ($generator->CanUseFastAttribute($attribute)) {
-            $functionName = "fastHasAttribute";
-        } else {
-            $functionName = "hasAttribute";
-        }
+        $functionName = "hasAttribute";
     } elsif ($attribute->signature->type eq "long") {
         $functionName = "getIntegralAttribute";
     } elsif ($attribute->signature->type eq "unsigned long") {
@@ -589,10 +566,10 @@ sub GetterExpression
         } elsif ($contentAttributeName eq "WebCore::HTMLNames::nameAttr") {
             $functionName = "getNameAttribute";
             $contentAttributeName = "";
-        } elsif ($generator->CanUseFastAttribute($attribute)) {
-            $functionName = "fastGetAttribute";
-        } else {
+        } elsif ($generator->IsSVGAnimatedType($attribute)) {
             $functionName = "getAttribute";
+        } else {
+            $functionName = "fastGetAttribute";
         }
     }
 

commit 062d9af2f3db67323f4574bf954616e052ff584e

SVG bindings are improperly being generated with "fastGetAttribute" https://bugs.webkit.org/show_bug.cgi?id=125670
Reviewed by Darin Adler.

A bug was introduced in r152845 that improperly called the
IsSVGAnimatedType using the $attribute hash, rather than the
expected $attribute->signature->type.

* bindings/scripts/CodeGenerator.pm:
(GetterExpression): Clean up attribute type confusion.


git-svn-id: http://svn.webkit.org/repository/webkit/trunk@160578 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Source/WebCore/bindings/scripts/CodeGenerator.pm
@@ -555,14 +555,16 @@ sub GetterExpression
         return ($generator->WK_lcfirst($generator->AttributeNameForGetterAndSetter($attribute)));
     }
 
+    my $attributeType = $attribute->signature->type;
+
     my $functionName;
     if ($attribute->signature->extendedAttributes->{"URL"}) {
         $functionName = "getURLAttribute";
-    } elsif ($attribute->signature->type eq "boolean") {
+    } elsif ($attributeType eq "boolean") {
         $functionName = "fastHasAttribute";
-    } elsif ($attribute->signature->type eq "long") {
+    } elsif ($attributeType eq "long") {
         $functionName = "getIntegralAttribute";
-    } elsif ($attribute->signature->type eq "unsigned long") {
+    } elsif ($attributeType eq "unsigned long") {
         $functionName = "getUnsignedIntegralAttribute";
     } else {
         if ($contentAttributeName eq "WebCore::HTMLNames::idAttr") {
@@ -571,7 +573,7 @@ sub GetterExpression
         } elsif ($contentAttributeName eq "WebCore::HTMLNames::nameAttr") {
             $functionName = "getNameAttribute";
             $contentAttributeName = "";
-        } elsif ($generator->IsSVGAnimatedType($attribute)) {
+        } elsif ($generator->IsSVGAnimatedType($attributeType)) {
             $functionName = "getAttribute";
         } else {
             $functionName = "fastGetAttribute";