WebKit/WebKit-http

commit d86e50524420a051ad296246039e0ee7e53cd475

WTF fails to compile with gcc 4.8.2 and -Werror=array-bounds https://bugs.webkit.org/show_bug.cgi?id=125936
Reviewed by Anders Carlsson.

* wtf/dtoa/double-conversion.cc: Use a unsigned instead of int on buffer_pos type.


git-svn-id: http://svn.webkit.org/repository/webkit/trunk@160834 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Source/WTF/wtf/dtoa/double-conversion.cc
@@ -446,7 +446,7 @@ namespace double_conversion {
         // The longest form of simplified number is: "-<significant digits>.1eXXX\0".
         const int kBufferSize = kMaxSignificantDigits + 10;
         char buffer[kBufferSize];  // NOLINT: size is known at compile time.
-        int buffer_pos = 0;
+        unsigned buffer_pos = 0;
         
         // Exponent will be adjusted if insignificant digits of the integer part
         // or insignificant leading zeros of the fractional part are dropped.

commit 0ef165bf39c04758ea0b113ee9be63e0b3a8ab44

Build fails in debug mode after r160834. https://bugs.webkit.org/show_bug.cgi?id=126059
Patch by Tamas Gergely <tgergely.u-szeged@partner.samsung.com> on 2013-12-20
Reviewed by Csaba Osztrogonác.

Debug mode build failed due to comparing signed and unsigned in
ASSERT(). Now both values are unsigned.

* wtf/dtoa/double-conversion.cc:

git-svn-id: http://svn.webkit.org/repository/webkit/trunk@160899 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Source/WTF/wtf/dtoa/double-conversion.cc
@@ -444,7 +444,7 @@ namespace double_conversion {
         if (current == end) return 0.0;
         
         // The longest form of simplified number is: "-<significant digits>.1eXXX\0".
-        const int kBufferSize = kMaxSignificantDigits + 10;
+        const unsigned kBufferSize = kMaxSignificantDigits + 10;
         char buffer[kBufferSize];  // NOLINT: size is known at compile time.
         unsigned buffer_pos = 0;
         