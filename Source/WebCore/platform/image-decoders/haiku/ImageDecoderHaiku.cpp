2c50f830f4568d1be67ecf07bfe755cc80cd763e
Adrien Destugues <pulkomandy@pulkomandy.tk>
2014-01-10 18:30:58 +0100

    Image Decoder: fix another crash cause.
    Sometimes we're asked to draw an image that has no pixel data.
    Do nothing in that case. Probably the decoder has loaded enough data to
    know the image size, but not started decoding anything yet. When the
    image loading progresses, WebKit will refresh the image and that time we
    will get a buffer.

--- Source/WebCore/platform/image-decoders/haiku/ImageDecoderHaiku.cpp
+++ Source/WebCore/platform/image-decoders/haiku/ImageDecoderHaiku.cpp
@@ -35,7 +35,7 @@ PassNativeImagePtr ImageFrame::asNewNativeImage() const
 {
     int bytesPerRow = width() * sizeof(PixelData);
     BBitmap* bitmap = new BBitmap(BRect(0, 0, width() - 1, height() - 1), 0, B_RGBA32, bytesPerRow);
-    if (!bitmap || !bitmap->IsValid()) {
+    if (!bitmap || !bitmap->IsValid() || !m_bytes) {
         delete bitmap;
         return NULL;
      }
