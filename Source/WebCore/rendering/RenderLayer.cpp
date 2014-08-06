WebKit/WebKit-http

commit 4bab80cf644e4ff58a57b7e6a5fd7be772bce437

REGRESSION (r159609): Images are corrupted when hovering over buttons @ github.com https://bugs.webkit.org/show_bug.cgi?id=127729
Reviewed by Antti Koivisto.

Source/WebCore:

When clipping a rect, the RenderLayer would not properly save the context when
the clipping rect is the same as the paint rect and the clipping rect
has radius.

Test: fast/regions/repaint/hover-border-radius.html

* rendering/RenderLayer.cpp:
(WebCore::RenderLayer::clipToRect):
(WebCore::RenderLayer::restoreClip):

LayoutTests:

Added test for the correct repainting of buttons with border radius when hovering.

* fast/regions/repaint/hover-border-radius-expected.txt: Added.
* fast/regions/repaint/hover-border-radius.html: Added.


git-svn-id: http://svn.webkit.org/repository/webkit/trunk@163382 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Source/WebCore/rendering/RenderLayer.cpp
@@ -3563,7 +3563,7 @@ static bool inContainingBlockChain(RenderLayer* startLayer, RenderLayer* endLaye
 void RenderLayer::clipToRect(RenderLayer* rootLayer, GraphicsContext* context, const LayoutRect& paintDirtyRect, const ClipRect& clipRect,
                              BorderRadiusClippingRule rule)
 {
-    if (clipRect.rect() != paintDirtyRect) {
+    if (clipRect.rect() != paintDirtyRect || clipRect.hasRadius()) {
         context->save();
         context->clip(pixelSnappedIntRect(clipRect.rect()));
     }
@@ -3588,7 +3588,7 @@ void RenderLayer::clipToRect(RenderLayer* rootLayer, GraphicsContext* context, c
 
 void RenderLayer::restoreClip(GraphicsContext* context, const LayoutRect& paintDirtyRect, const ClipRect& clipRect)
 {
-    if (clipRect.rect() == paintDirtyRect)
+    if (clipRect.rect() == paintDirtyRect && !clipRect.hasRadius())
         return;
     context->restore();
 }

LayoutTests/fast/regions/repaint/hover-border-radius-expected.txt
@@ -1,0 +1,17 @@
+This test passes if moving the cursor from one of the blue items to the other produces no repaint errors (such as cropping of one of the blue items).
+
+(repaint rects
+  (rect 49 84 350 215)
+  (rect 49 84 350 215)
+  (rect 49 84 350 215)
+  (rect 49 84 350 18)
+  (rect 49 84 350 215)
+  (rect 49 84 350 215)
+  (rect 49 84 350 215)
+  (rect 400 84 350 215)
+  (rect 400 84 350 215)
+  (rect 400 84 350 215)
+  (rect 49 84 350 18)
+  (rect 400 84 350 18)
+)
+

LayoutTests/fast/regions/repaint/hover-border-radius.html
@@ -1,0 +1,64 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <script src="../../repaint/resources/text-based-repaint.js"></script>
+
+    <style>
+      .page_body
+      {
+        border-bottom-left-radius: 5px;
+        overflow-x: hidden;
+        overflow-y: hidden;
+      }
+
+      li
+      {
+        background-color: lightblue;
+        margin-left: 1px;
+        border-top-left-radius: 5px;
+        float: left;
+        height: 215px;
+        overflow-x: hidden;
+        overflow-y: hidden;
+        position: relative;
+        width: 350px;
+      }
+
+      li:hover
+      {
+        background-color: red;
+      }
+    </style>
+
+    <script type="text/javascript">
+      function repaintTest() {
+        if(!window.testRunner)
+          return;
+
+        var first = document.getElementById("first");
+        var second = document.getElementById("second");
+
+        // hover the 1st item
+        var left = first.offsetLeft;
+        var top = first.offsetTop;
+        eventSender.mouseMoveTo(left + 20, top + 20);
+
+        // now hover the 2nd item
+        left = second.offsetLeft;
+        top = second.offsetTop;
+        eventSender.mouseMoveTo(left + 20, top + 20);
+      }
+    </script>
+
+  </head>
+
+  <body onload="runRepaintTest();">
+    <p>This test passes if moving the cursor from one of the blue items to the other produces no repaint errors (such as cropping of one of the blue items).
+    <div class="page_body">
+      <ul>
+        <li id="first"></li>
+        <li id="second"></li>
+      </ul>
+    </div>
+  </body>
+</html>

commit 04cddeaf35324bd0718073098ef9cad4a964429b

REGRESSION (r163382): Overflow hidden for inner elements breaks blurring https://bugs.webkit.org/show_bug.cgi?id=135318
Reviewed by Zalan Bujtas.

Source/WebCore:

For elements with border radius, clipping must be applied using clipRoundedRect.
This regressed in r163382, when normal clipping started being applied also
for elements having border radius.

Test: fast/filter-image/clipped-filter.html

* rendering/RenderLayer.cpp:
(WebCore::RenderLayer::clipToRect):
(WebCore::RenderLayer::restoreClip):

LayoutTests:

Added test for filter applied on an element overflowing its parent, which has overflow:hidden.

* fast/filter-image/clipped-filter-expected.html: Added.
* fast/filter-image/clipped-filter.html: Added.


git-svn-id: http://svn.webkit.org/repository/webkit/trunk@172146 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Source/WebCore/rendering/RenderLayer.cpp
@@ -3586,36 +3586,34 @@ static bool inContainingBlockChain(RenderLayer* startLayer, RenderLayer* endLaye
 void RenderLayer::clipToRect(const LayerPaintingInfo& paintingInfo, GraphicsContext* context, const ClipRect& clipRect, BorderRadiusClippingRule rule)
 {
     float deviceScaleFactor = renderer().document().deviceScaleFactor();
-    if (clipRect.rect() != paintingInfo.paintDirtyRect || clipRect.hasRadius()) {
+    if (clipRect.hasRadius()) {
+        context->save();
+        
+        // If the clip rect has been tainted by a border radius, then we have to walk up our layer chain applying the clips from
+        // any layers with overflow. The condition for being able to apply these clips is that the overflow object be in our
+        // containing block chain so we check that also.
+        for (RenderLayer* layer = rule == IncludeSelfForBorderRadius ? this : parent(); layer; layer = layer->parent()) {
+            if (layer->renderer().hasOverflowClip() && layer->renderer().style().hasBorderRadius() && inContainingBlockChain(this, layer)) {
+                LayoutRect adjustedClipRect = LayoutRect(toLayoutPoint(layer->offsetFromAncestor(paintingInfo.rootLayer)), layer->size());
+                adjustedClipRect.move(paintingInfo.subpixelAccumulation);
+                context->clipRoundedRect(layer->renderer().style().getRoundedInnerBorderFor(adjustedClipRect).pixelSnappedRoundedRectForPainting(deviceScaleFactor));
+            }
+            
+            if (layer == paintingInfo.rootLayer)
+                break;
+        }
+    } else if (clipRect.rect() != paintingInfo.paintDirtyRect) {
         context->save();
         LayoutRect adjustedClipRect = clipRect.rect();
         adjustedClipRect.move(paintingInfo.subpixelAccumulation);
         context->clip(pixelSnappedForPainting(adjustedClipRect, deviceScaleFactor));
     }
-
-    if (!clipRect.hasRadius())
-        return;
-
-    // If the clip rect has been tainted by a border radius, then we have to walk up our layer chain applying the clips from
-    // any layers with overflow. The condition for being able to apply these clips is that the overflow object be in our
-    // containing block chain so we check that also.
-    for (RenderLayer* layer = rule == IncludeSelfForBorderRadius ? this : parent(); layer; layer = layer->parent()) {
-        if (layer->renderer().hasOverflowClip() && layer->renderer().style().hasBorderRadius() && inContainingBlockChain(this, layer)) {
-            LayoutRect adjustedClipRect = LayoutRect(toLayoutPoint(layer->offsetFromAncestor(paintingInfo.rootLayer)), layer->size());
-            adjustedClipRect.move(paintingInfo.subpixelAccumulation);
-            context->clipRoundedRect(layer->renderer().style().getRoundedInnerBorderFor(adjustedClipRect).pixelSnappedRoundedRectForPainting(deviceScaleFactor));
-        }
-
-        if (layer == paintingInfo.rootLayer)
-            break;
-    }
 }
 
 void RenderLayer::restoreClip(GraphicsContext* context, const LayoutRect& paintDirtyRect, const ClipRect& clipRect)
 {
-    if (clipRect.rect() == paintDirtyRect && !clipRect.hasRadius())
-        return;
-    context->restore();
+    if (clipRect.rect() != paintDirtyRect || clipRect.hasRadius())
+        context->restore();
 }
 
 static void performOverlapTests(OverlapTestRequestMap& overlapTestRequests, const RenderLayer* rootLayer, const RenderLayer* layer)

LayoutTests/fast/filter-image/clipped-filter-expected.html
@@ -1,0 +1,51 @@
+<!DOCTYPE html>
+
+<html>
+<head>
+    <style>
+        .circle {
+            width: 300px;
+            height: 300px;
+            margin-left: 100px;
+            margin-top: 60px;
+            background-color: #f5ad61;
+            border-radius: 50% 50%;
+        }
+        .inner {
+            -webkit-filter: drop-shadow(16px 16px 0px black);
+            background-color: red;
+            height: 60px;
+            width: 200px;
+        }
+
+        #clip {
+            position: absolute;
+            top: 0px;
+            left: 0px;
+            width: 500px;
+            height: 500px;
+            background-color: white;
+            -webkit-clip-path: url(#clipPath);
+        }
+    </style>
+</head>
+
+<body>
+    <a href="https://bugs.webkit.org/show_bug.cgi?id=135318">Bug 135318 - This test passes if the red element has a black shadow and is clipped by the orange circle</a>
+
+    <div class="circle">
+        <div class="inner"></div>
+    </div>
+
+    <div id="clip"></div>
+</body>
+
+<svg>
+    <defs>
+        <clipPath id="clipPath">
+            <path d="M18,38 v200 h90 a150,150 0 0,1 300,0 h50 v-200 z"
+        </clipPath>
+    </defs>
+</svg>
+
+</html>

LayoutTests/fast/filter-image/clipped-filter.html
@@ -1,0 +1,53 @@
+<!DOCTYPE html>
+
+<html>
+<head>
+    <style>
+        .circle {
+            width: 300px;
+            height: 300px;
+            margin-left: 100px;
+            margin-top: 60px;
+            background-color: #f5ad61;
+            overflow: hidden;
+            border-radius: 50% 50%;
+        }
+
+        .inner {
+            -webkit-filter: drop-shadow(16px 16px 0px black);
+            background-color: red;
+            height: 60px;
+            width: 200px;
+        }
+
+        #clip {
+            position: absolute;
+            top: 0px;
+            left: 0px;
+            width: 500px;
+            height: 500px;
+            background-color: white;
+            -webkit-clip-path: url(#clipPath);
+        }
+    </style>
+</head>
+
+<body>
+    <a href="https://bugs.webkit.org/show_bug.cgi?id=135318">Bug 135318 - This test passes if the red element has a black shadow and is clipped by the orange circle</a>
+
+    <div class="circle">
+        <div class="inner"></div>
+    </div>
+
+    <div id="clip"></div>
+</body>
+
+<svg>
+    <defs>
+        <clipPath id="clipPath">
+            <path d="M18,38 v200 h90 a150,150 0 0,1 300,0 h50 v-200 z"
+        </clipPath>
+    </defs>
+</svg>
+
+</html>