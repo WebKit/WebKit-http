4c5b66361bb9dcb1b136ec94a2c6fb2846ed1a9c
Zan Dobersek <zdobersek@igalia.com>
2014-11-25 09:06:53 -0800

    Comply with OpenGL ES 2.0 and use GL_CLAMP_TO_EDGE as argument for glTextParameterf().

--- Source/WebCore/platform/graphics/cairo/ImageBufferCairo.cpp
+++ Source/WebCore/platform/graphics/cairo/ImageBufferCairo.cpp
@@ -80,8 +80,8 @@ PassRefPtr<cairo_surface_t> createCairoGLSurface(const FloatSize& size, uint32_t
     // from a pre-existing surface.
     glGenTextures(1, &texture);
     glBindTexture(GL_TEXTURE_2D, texture);
-    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP);
-    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP);
+    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 
