WebKit/WebKit-http

commit 4de78831c65f48acc16ee3bbe78ae82ce711fbc2

WK1: Need new SPI for Webkit clients to override default immediate action https://bugs.webkit.org/show_bug.cgi?id=139568 -and corresponding- rdar://problem/19225448
Reviewed by Dan Bernstein.

Call SPI giving clients a chance to override the default behavior.
* WebView/WebImmediateActionController.mm:
(-[WebImmediateActionController _updateImmediateActionItem]):

New SPI.
* WebView/WebUIDelegatePrivate.h:



git-svn-id: http://svn.webkit.org/repository/webkit/trunk@177199 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Source/WebKit/mac/WebView/WebImmediateActionController.mm
@@ -27,6 +27,7 @@
 
 #if PLATFORM(MAC) && __MAC_OS_X_VERSION_MIN_REQUIRED >= 101000
 
+#import "WebElementDictionary.h"
 #import "WebFrameInternal.h"
 #import "WebHTMLView.h"
 #import "WebHTMLViewInternal.h"
@@ -126,6 +127,7 @@ using namespace WebCore;
 {
     _type = WebImmediateActionNone;
     _immediateActionRecognizer.animationController = nil;
+    id <NSImmediateActionAnimationController> defaultAnimationController = nil;
 
     NSURL *url = _hitTestResult.absoluteLinkURL();
     NSString *absoluteURLString = [url absoluteString];
@@ -135,9 +137,22 @@ using namespace WebCore;
         RetainPtr<QLPreviewMenuItem> qlPreviewLinkItem = [NSMenuItem standardQuickLookMenuItem];
         [qlPreviewLinkItem setPreviewStyle:QLPreviewStylePopover];
         [qlPreviewLinkItem setDelegate:self];
-        _immediateActionRecognizer.animationController = (id<NSImmediateActionAnimationController>)qlPreviewLinkItem.get();
-        return;
+        defaultAnimationController = (id <NSImmediateActionAnimationController>)qlPreviewLinkItem.get();
     }
+
+    // Allow clients the opportunity to override the default immediate action.
+    id customClientAnimationController = nil;
+    if ([[_webView UIDelegate] respondsToSelector:@selector(_webView:immediateActionAnimationControllerForHitTestResult:withType:)]) {
+        RetainPtr<WebElementDictionary> webHitTestResult = [[WebElementDictionary alloc] initWithHitTestResult:_hitTestResult];
+        customClientAnimationController = [[_webView UIDelegate] _webView:_webView immediateActionAnimationControllerForHitTestResult:webHitTestResult.get() withType:_type];
+    }
+
+    if (customClientAnimationController == [NSNull null])
+        return;
+    if (customClientAnimationController && [customClientAnimationController conformsToProtocol:@protocol(NSImmediateActionAnimationController)])
+        _immediateActionRecognizer.animationController = (id <NSImmediateActionAnimationController>)customClientAnimationController;
+    else
+        _immediateActionRecognizer.animationController = defaultAnimationController;
 }
 
 #pragma mark QLPreviewMenuItemDelegate implementation

Source/WebKit/mac/WebView/WebUIDelegatePrivate.h
@@ -234,6 +234,10 @@ extern NSString *WebConsoleMessageErrorMessageLevel;
 - (void)webView:(WebView *)sender saveFrameView:(WebFrameView *)frameView showingPanel:(BOOL)showingPanel;
 - (NSArray *)_webView:(WebView *)sender actionMenuItemsForHitTestResult:(NSDictionary *)hitTestResult withType:(WebActionMenuType)type defaultActionMenuItems:(NSArray *)defaultMenuItems;
 - (DDActionContext *)_webView:(WebView *)sender actionContextForHitTestResult:(NSDictionary *)hitTestResult range:(DOMRange **)range;
+
+// Clients that want to maintain default behavior can return nil. To disable the immediate action entirely, return NSNull. And to
+// do something custom, return an object that conforms to the NSImmediateActionAnimationController protocol.
+- (id)_webView:(WebView *)sender immediateActionAnimationControllerForHitTestResult:(NSDictionary *)hitTestResult withType:(WebImmediateActionType)type;
 #endif
 - (BOOL)webView:(WebView *)sender didPressMissingPluginButton:(DOMElement *)element;
 /*!

commit 2c0de0383070c9ea8ff6d023267ec9c88d3adb8e

Fix a leak introduced in r177199.
Reviewed by Anders Carlsson.

* WebView/WebImmediateActionController.mm:
(-[WebImmediateActionController _updateImmediateActionItem]):



git-svn-id: http://svn.webkit.org/repository/webkit/trunk@177201 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Source/WebKit/mac/WebView/WebImmediateActionController.mm
@@ -143,7 +143,7 @@ using namespace WebCore;
     // Allow clients the opportunity to override the default immediate action.
     id customClientAnimationController = nil;
     if ([[_webView UIDelegate] respondsToSelector:@selector(_webView:immediateActionAnimationControllerForHitTestResult:withType:)]) {
-        RetainPtr<WebElementDictionary> webHitTestResult = [[WebElementDictionary alloc] initWithHitTestResult:_hitTestResult];
+        RetainPtr<WebElementDictionary> webHitTestResult = adoptNS([[WebElementDictionary alloc] initWithHitTestResult:_hitTestResult]);
         customClientAnimationController = [[_webView UIDelegate] _webView:_webView immediateActionAnimationControllerForHitTestResult:webHitTestResult.get() withType:_type];
     }
 