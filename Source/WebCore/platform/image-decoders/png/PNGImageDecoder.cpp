ae8b16a3178fe473024facb67901ebd2b04bcbf3
Ashish Kulkarni <kulkarni.ashish@gmail.com>
2014-12-18 20:58:23 +0530

    fix build on MSVC with --system-libpng or --system-libjpeg
    This was introduced in WebKit r108792, as in the related bug
    https://bugs.webkit.org/show_bug.cgi?id=32410#c26
    
    it was confirmed as broken for Windows MSVC (for Chromium). The final
    merge made it conditional for PLATFORM(QT) but did not fix the
    underlying issue, which was the wrong placement of NO_RETURN as MSVC
    does not allow __declspec(noreturn) to be put after the declaration
    but GCC allows __attribute((__noreturn__)) at either location. This
    is similar with the usage of NO_RETURN in other places.
    
    Change-Id: I95ae3727553093b84b3645eb6dad63779f08e09d
    Reviewed-by: Kai Koehne <kai.koehne@theqtcompany.com>
    Reviewed-by: Allan Sandfeld Jensen <allan.jensen@theqtcompany.com>

--- Source/WebCore/platform/image-decoders/png/PNGImageDecoder.cpp
+++ Source/WebCore/platform/image-decoders/png/PNGImageDecoder.cpp
@@ -66,9 +66,7 @@ const double cInverseGamma = 0.45455;
 const unsigned long cMaxPNGSize = 1000000UL;
 
 // Called if the decoding of the image fails.
-#if PLATFORM(QT)
-static void PNGAPI decodingFailed(png_structp, png_const_charp) NO_RETURN;
-#endif
+static NO_RETURN void PNGAPI decodingFailed(png_structp, png_const_charp);
 static void PNGAPI decodingFailed(png_structp png, png_const_charp)
 {
     longjmp(JMPBUF(png), 1);
