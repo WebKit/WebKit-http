WebKit/WebKit-http

commit 38d97bbe049acbc010ae2df275919144c4487b40

Fix Border-radius clipping issue on a composited descendants https://bugs.webkit.org/show_bug.cgi?id=138551
Patch by Byungseon Shin <sun.shin@lge.com> on 2015-01-07
Reviewed by Simon Fraser.

Fix clipping compositing descendants of an accelerated layer having
border radius and clip overflow issue by using layer corner-radius
or a CAShapeLayer mask on Mac, and setting up a separate mask layer
on the childContainmentLayer on other platforms.

This patch is extracted from following Blink revisions:
<http://src.chromium.org/viewvc/blink?view=revision&revision=162463>
<http://src.chromium.org/viewvc/blink?view=revision&revision=158258>
by <rosca@adobe.com>

Clip accelerated descendants of an accelerated layer having border radius and clip overflow.
Large canvas does not honor containing div's border radius.

and <http://src.chromium.org/viewvc/blink?view=revision&revision=160578>
by <junov@chromium.org>

Fix for CSS clip-path with accelerated 2D canvas.

* WebCore.exp.in:
* platform/graphics/GraphicsLayer.cpp:
(WebCore::GraphicsLayer::dumpProperties):
* platform/graphics/GraphicsLayer.h:
(WebCore::GraphicsLayer::applyClippingBorder):
(WebCore::GraphicsLayer::clearClippingBorder):
(WebCore::GraphicsLayer::needsClippingMaskLayer):
* platform/graphics/GraphicsLayerClient.h:
* platform/graphics/ca/GraphicsLayerCA.cpp:
(WebCore::GraphicsLayerCA::applyClippingBorder):
(WebCore::GraphicsLayerCA::clearClippingBorder):
* platform/graphics/ca/GraphicsLayerCA.h:
* rendering/PaintPhase.h:
* rendering/RenderBlock.cpp:
(WebCore::RenderBlock::paintObject):
* rendering/RenderBox.cpp:
(WebCore::RenderBox::paintClippingMask):
* rendering/RenderBox.h:
* rendering/RenderLayer.cpp:
(WebCore::RenderLayer::paintLayerContents):
(WebCore::RenderLayer::paintChildClippingMaskForFragments):
* rendering/RenderLayer.h:
* rendering/RenderLayerBacking.cpp:
(WebCore::RenderLayerBacking::~RenderLayerBacking):
(WebCore::RenderLayerBacking::destroyGraphicsLayers):

Source/WebCore/rendering/RenderLayerBacking.cpp
@@ -147,6 +147,7 @@ RenderLayerBacking::~RenderLayerBacking()
     updateForegroundLayer(false);
     updateBackgroundLayer(false);
     updateMaskLayer(false);
+    updateChildClippingStrategy(false);
     updateScrollingLayers(false);
     detachFromScrollingCoordinator();
     destroyGraphicsLayers();
@@ -348,6 +349,7 @@ void RenderLayerBacking::destroyGraphicsLayers()
     m_backgroundLayer = nullptr;
     m_childContainmentLayer = nullptr;
     m_maskLayer = nullptr;
+    m_childClippingMaskLayer = nullptr;
 
     m_scrollingLayer = nullptr;
     m_scrollingContentsLayer = nullptr;
@@ -568,6 +570,8 @@ bool RenderLayerBacking::updateConfiguration()
     }
 
     updateMaskLayer(renderer().hasMask());
+
+    updateChildClippingStrategy(needsDescendentsClippingLayer);
 
     if (m_owningLayer.hasReflection()) {
         if (m_owningLayer.reflectionLayer()->backing()) {
@@ -814,6 +818,12 @@ void RenderLayerBacking::updateGeometry()
         clipLayer->setPosition(FloatPoint(clippingBox.location() - localCompositingBounds.location()));
         clipLayer->setSize(clippingBox.size());
         clipLayer->setOffsetFromRenderer(toFloatSize(clippingBox.location()));
+
+        if (m_childClippingMaskLayer && !m_scrollingLayer) {
+            m_childClippingMaskLayer->setSize(clipLayer->size());
+            m_childClippingMaskLayer->setPosition(FloatPoint());
+            m_childClippingMaskLayer->setOffsetFromRenderer(clipLayer->offsetFromRenderer());
+        }
     }
     
     if (m_maskLayer) {
@@ -941,6 +951,12 @@ void RenderLayerBacking::updateGeometry()
 
         FloatSize oldScrollingLayerOffset = m_scrollingLayer->offsetFromRenderer();
         m_scrollingLayer->setOffsetFromRenderer(-toFloatSize(paddingBox.location()));
+
+        if (m_childClippingMaskLayer) {
+            m_childClippingMaskLayer->setPosition(m_scrollingLayer->position());
+            m_childClippingMaskLayer->setSize(m_scrollingLayer->size());
+            m_childClippingMaskLayer->setOffsetFromRenderer(toFloatSize(paddingBox.location()));
+        }
 
         bool paddingBoxOffsetChanged = oldScrollingLayerOffset != m_scrollingLayer->offsetFromRenderer();
 
@@ -1412,6 +1428,37 @@ void RenderLayerBacking::updateMaskLayer(bool needsMaskLayer)
 
     if (layerChanged)
         m_graphicsLayer->setPaintingPhase(paintingPhaseForPrimaryLayer());
+}
+
+void RenderLayerBacking::updateChildClippingStrategy(bool needsDescendentsClippingLayer)
+{
+    if (hasClippingLayer() && needsDescendentsClippingLayer) {
+        if (is<RenderBox>(renderer())) {
+            LayoutRect boxRect(LayoutPoint(), downcast<RenderBox>(renderer()).size());
+            FloatRoundedRect contentsClippingRect = renderer().style().getRoundedInnerBorderFor(boxRect).pixelSnappedRoundedRectForPainting(deviceScaleFactor());
+            contentsClippingRect.move(contentOffsetInCompostingLayer());
+            if (clippingLayer()->applyClippingBorder(contentsClippingRect)) {
+                if (m_childClippingMaskLayer) 
+                    m_childClippingMaskLayer = nullptr;
+                return;
+            }
+        }
+
+        if (!m_childClippingMaskLayer) {
+            m_childClippingMaskLayer = createGraphicsLayer("Child Clipping Mask Layer");
+            m_childClippingMaskLayer->setDrawsContent(true);
+            m_childClippingMaskLayer->setPaintingPhase(GraphicsLayerPaintChildClippingMask);
+            clippingLayer()->setMaskLayer(m_childClippingMaskLayer.get());
+        }
+    } else {
+        if (m_childClippingMaskLayer) {
+            m_childClippingMaskLayer = nullptr;
+            if (hasClippingLayer())
+                clippingLayer()->setMaskLayer(nullptr);
+        } else 
+            if (hasClippingLayer())
+                clippingLayer()->clearClippingBorder();
+    }
 }
 
 bool RenderLayerBacking::updateScrollingLayers(bool needsScrollingLayers)
@@ -2065,6 +2112,9 @@ void RenderLayerBacking::setContentsNeedDisplay(GraphicsLayer::ShouldClipToLayer
     if (m_maskLayer && m_maskLayer->drawsContent())
         m_maskLayer->setNeedsDisplay();
 
+    if (m_childClippingMaskLayer && m_childClippingMaskLayer->drawsContent())
+        m_childClippingMaskLayer->setNeedsDisplay();
+
     if (m_scrollingContentsLayer && m_scrollingContentsLayer->drawsContent())
         m_scrollingContentsLayer->setNeedsDisplay();
 }
@@ -2104,6 +2154,12 @@ void RenderLayerBacking::setContentsNeedDisplayInRect(const LayoutRect& r, Graph
         m_maskLayer->setNeedsDisplayInRect(layerDirtyRect, shouldClip);
     }
 
+    if (m_childClippingMaskLayer && m_childClippingMaskLayer->drawsContent()) {
+        FloatRect layerDirtyRect = r;
+        layerDirtyRect.move(-m_childClippingMaskLayer->offsetFromRenderer());
+        m_childClippingMaskLayer->setNeedsDisplayInRect(layerDirtyRect);
+    }
+
     if (m_scrollingContentsLayer && m_scrollingContentsLayer->drawsContent()) {
         FloatRect layerDirtyRect = pixelSnappedRectForPainting;
         layerDirtyRect.move(-m_scrollingContentsLayer->offsetFromRenderer() + m_devicePixelFractionFromRenderer);
@@ -2138,6 +2194,8 @@ void RenderLayerBacking::paintIntoLayer(const GraphicsLayer* graphicsLayer, Grap
         paintFlags |= RenderLayer::PaintLayerPaintingCompositingForegroundPhase;
     if (paintingPhase & GraphicsLayerPaintMask)
         paintFlags |= RenderLayer::PaintLayerPaintingCompositingMaskPhase;
+    if (paintingPhase & GraphicsLayerPaintChildClippingMask)
+        paintFlags |= RenderLayer::PaintLayerPaintingChildClippingMaskPhase;
     if (paintingPhase & GraphicsLayerPaintOverflowContents)
         paintFlags |= RenderLayer::PaintLayerPaintingOverflowContents;
     if (paintingPhase & GraphicsLayerPaintCompositedScroll)
@@ -2202,6 +2260,7 @@ void RenderLayerBacking::paintContents(const GraphicsLayer* graphicsLayer, Graph
         || graphicsLayer == m_foregroundLayer.get()
         || graphicsLayer == m_backgroundLayer.get()
         || graphicsLayer == m_maskLayer.get()
+        || graphicsLayer == m_childClippingMaskLayer.get()
         || graphicsLayer == m_scrollingContentsLayer.get()) {
         InspectorInstrumentation::willPaint(&renderer());
 
@@ -2578,6 +2637,8 @@ double RenderLayerBacking::backingStoreMemoryEstimate() const
         backingMemory += m_backgroundLayer->backingStoreMemoryEstimate();
     if (m_maskLayer)
         backingMemory += m_maskLayer->backingStoreMemoryEstimate();
+    if (m_childClippingMaskLayer)
+        backingMemory += m_childClippingMaskLayer->backingStoreMemoryEstimate();
 
     if (m_scrollingContentsLayer)
         backingMemory += m_scrollingContentsLayer->backingStoreMemoryEstimate();

Source/WebCore/WebCore.exp.in
@@ -589,6 +589,8 @@ __ZN7WebCore15GraphicsLayerCA18setContentsVisibleEb
 __ZN7WebCore15GraphicsLayerCA18setOpacityInternalEf
 __ZN7WebCore15GraphicsLayerCA18setReplicatedLayerEPNS_13GraphicsLayerE
 __ZN7WebCore15GraphicsLayerCA18setShowDebugBorderEb
+__ZN7WebCore15GraphicsLayerCA19applyClippingBorderERKNS_16FloatRoundedRectE
+__ZN7WebCore15GraphicsLayerCA19clearClippingBorderEv
 __ZN7WebCore15GraphicsLayerCA19setCustomAppearanceENS_13GraphicsLayer16CustomAppearanceE
 __ZN7WebCore15GraphicsLayerCA20setChildrenTransformERKNS_20TransformationMatrixE
 __ZN7WebCore15GraphicsLayerCA20setReplicatedByLayerEPNS_13GraphicsLayerE

Source/WebCore/platform/graphics/GraphicsLayer.cpp
@@ -761,6 +761,10 @@ void GraphicsLayer::dumpProperties(TextStream& ts, int indent, LayerTreeAsTextBe
             writeIndent(ts, indent + 2);
             ts << "GraphicsLayerPaintMask\n";
         }
+        if (paintingPhase() & GraphicsLayerPaintChildClippingMask) {
+            writeIndent(ts, indent + 2);
+            ts << "GraphicsLayerPaintChildClippingMask\n";
+        }
         if (paintingPhase() & GraphicsLayerPaintOverflowContents) {
             writeIndent(ts, indent + 2);
             ts << "GraphicsLayerPaintOverflowContents\n";

Source/WebCore/platform/graphics/GraphicsLayer.h
@@ -368,6 +368,9 @@ public:
     FloatRoundedRect contentsClippingRect() const { return m_contentsClippingRect; }
     virtual void setContentsClippingRect(const FloatRoundedRect& roundedRect) { m_contentsClippingRect = roundedRect; }
 
+    virtual bool applyClippingBorder(const FloatRoundedRect&) { return false; }
+    virtual void clearClippingBorder() { return; }
+
     // Transitions are identified by a special animation name that cannot clash with a keyframe identifier.
     static String animationNameForTransition(AnimatedPropertyID);
     
@@ -497,6 +500,8 @@ public:
     virtual bool isGraphicsLayerCA() const { return false; }
     virtual bool isGraphicsLayerCARemote() const { return false; }
     virtual bool isGraphicsLayerTextureMapper() const { return false; }
+
+    virtual bool needsClippingMaskLayer() { return true; };
 
 protected:
     // Should be called from derived class destructors. Should call willBeDestroyed() on super.

Source/WebCore/platform/graphics/GraphicsLayerClient.h
@@ -44,6 +44,7 @@ enum GraphicsLayerPaintingPhaseFlags {
     GraphicsLayerPaintMask = (1 << 2),
     GraphicsLayerPaintOverflowContents = (1 << 3),
     GraphicsLayerPaintCompositedScroll = (1 << 4),
+    GraphicsLayerPaintChildClippingMask = (1 << 5),
     GraphicsLayerPaintAllWithOverflowClip = (GraphicsLayerPaintBackground | GraphicsLayerPaintForeground | GraphicsLayerPaintMask)
 };
 typedef unsigned GraphicsLayerPaintingPhase;

Source/WebCore/platform/graphics/ca/GraphicsLayerCA.cpp
@@ -2027,6 +2027,38 @@ void GraphicsLayerCA::updateContentsColorLayer()
     }
 }
 
+bool GraphicsLayerCA::applyClippingBorder(const FloatRoundedRect& roundedRect)
+{
+    if (roundedRect.radii().isUniformCornerRadius()) {
+        m_layer->setMask(nullptr);
+        m_layer->setMasksToBounds(true);
+        m_layer->setCornerRadius(roundedRect.radii().topLeft().width());
+    } else {
+        if (!m_shapeMaskLayer) {
+            m_shapeMaskLayer = createPlatformCALayer(PlatformCALayer::LayerTypeShapeLayer, this);
+            m_shapeMaskLayer->setAnchorPoint(FloatPoint3D());
+        }
+
+        m_shapeMaskLayer->setPosition(FloatPoint());
+        m_shapeMaskLayer->setBounds(m_layer->bounds());
+    
+        m_layer->setCornerRadius(0);
+        m_layer->setMask(m_shapeMaskLayer.get());
+
+        FloatRoundedRect offsetClippingRoundedRect(m_layer->bounds(), roundedRect.radii());
+        m_shapeMaskLayer->setShapeRoundedRect(offsetClippingRoundedRect);
+    }
+
+    return true;
+}
+
+void GraphicsLayerCA::clearClippingBorder()
+{
+    m_layer->setCornerRadius(0);
+    m_layer->setMasksToBounds(false);
+    m_layer->setMask(nullptr);
+}
+
 // The clipping strategy depends on whether the rounded rect has equal corner radii.
 void GraphicsLayerCA::updateClippingStrategy(PlatformCALayer& clippingLayer, RefPtr<PlatformCALayer>& shapeMaskLayer, const FloatRoundedRect& roundedRect)
 {

Source/WebCore/platform/graphics/ca/GraphicsLayerCA.h
@@ -173,6 +173,9 @@ protected:
 private:
     virtual bool isGraphicsLayerCA() const override { return true; }
 
+    virtual bool applyClippingBorder(const FloatRoundedRect&) override;
+    virtual void clearClippingBorder() override;
+
     WEBCORE_EXPORT virtual void willBeDestroyed() override;
 
     // PlatformCALayerClient overrides

Source/WebCore/rendering/PaintPhase.h
@@ -49,7 +49,8 @@ enum PaintPhase {
     PaintPhaseSelection,
     PaintPhaseCollapsedTableBorders,
     PaintPhaseTextClip,
-    PaintPhaseMask
+    PaintPhaseMask,
+    PaintPhaseClippingMask
 };
 
 enum PaintBehaviorFlags {

commit 1face709f9bb67cab56498899d10017ddffcd408

[EFL] Fix crash introduced in r178029 https://bugs.webkit.org/show_bug.cgi?id=140289
Patch by Byungseon Shin <sun.shin@lge.com> on 2015-01-09
Reviewed by Martin Robinson.

Clearing childClippingMaskLayer of CoordinatedGraphics should be called
before clearing childClippingLayer.

No new tests, covered by existing tests.

* rendering/RenderLayerBacking.cpp:
(WebCore::RenderLayerBacking::~RenderLayerBacking):

git-svn-id: http://svn.webkit.org/repository/webkit/trunk@178176 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Source/WebCore/rendering/RenderLayerBacking.cpp
@@ -142,12 +142,12 @@ RenderLayerBacking::RenderLayerBacking(RenderLayer& layer)
 RenderLayerBacking::~RenderLayerBacking()
 {
     updateAncestorClippingLayer(false);
+    updateChildClippingStrategy(false);
     updateDescendantClippingLayer(false);
     updateOverflowControlsLayers(false, false, false);
     updateForegroundLayer(false);
     updateBackgroundLayer(false);
     updateMaskLayer(false);
-    updateChildClippingStrategy(false);
     updateScrollingLayers(false);
     detachFromScrollingCoordinator();
     destroyGraphicsLayers();