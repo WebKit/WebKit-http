WebKit/WebKit-http

commit 16b580d3ad4cdbcfbd2f904484f39cd0cf0f2707

2011-04-29  Jon Lee  <jonlee@apple.com>
        Reviewed by mitzpettel.

        REGRESSION (WebKit2): (Mac) Selection is gone after switching tabs (59721)
        <rdar://problem/9327332>
        https://bugs.webkit.org/show_bug.cgi?id=59721

        Removing shouldClearSelectionWhenLosingWebPageFocus().  Reverting changelist 83814.

        * WebCore.exp.in:
        * editing/EditingBehavior.h:
2011-04-29  Jon Lee  <jonlee@apple.com>

        Reviewed by mitzpettel.

        REGRESSION (WebKit2): (Mac) Selection is gone after switching tabs (59721)
        <rdar://problem/9327332>
        https://bugs.webkit.org/show_bug.cgi?id=59721

        When resigning first responder status, check to see if we need to maintain an inactive
        selection in the web view. If we don't, we clear the selection.

        This also reverts the change made in 83814, since that was a different attempt to solve this same issue.

        * UIProcess/API/C/WKPage.cpp:
        (WKPageSetMaintainsInactiveSelection): Export setMaintainsInactiveSelection()
        * UIProcess/API/C/WKPage.h:
        * UIProcess/API/mac/WKView.mm:
        (-[WKView resignFirstResponder]): Check and see if we need to maintain the selection in the view prior to resigning.
        If not, we clear the selection
        * UIProcess/WebPageProxy.cpp:
        (WebKit::WebPageProxy::clearSelection): Send message across process boundary
        (WebKit::WebPageProxy::setMaintainsInactiveSelection): Sets variable
        * UIProcess/WebPageProxy.h: New member variable to keep track of whether we need to maintain the current selection
        (WebKit::WebPageProxy::maintainsInactiveSelection): Returns variable
        * WebProcess/WebPage/WebPage.cpp:
        (WebKit::WebPage::setFocused): Reverting code to just set the focused bit instead of also clearing the selection
        (WebKit::WebPage::clearSelection):
        * WebProcess/WebPage/WebPage.h:

Source/WebKit2/UIProcess/API/mac/WKView.mm
@@ -314,6 +314,9 @@ struct WKViewInterpretKeyEventsParameters {
         _data->_page->confirmCompositionWithoutDisturbingSelection();
     [self _resetTextInputState];
     
+    if (!_data->_page->maintainsInactiveSelection())
+        _data->_page->clearSelection();
+    
     _data->_page->viewStateDidChange(WebPageProxy::ViewIsFocused);
 
     _data->_inResignFirstResponder = false;

Source/WebKit2/UIProcess/WebPageProxy.cpp
@@ -642,6 +642,13 @@ void WebPageProxy::setWindowResizerSize(const IntSize& windowResizerSize)
         return;
     process()->send(Messages::WebPage::SetWindowResizerSize(windowResizerSize), m_pageID);
 }
+    
+void WebPageProxy::clearSelection()
+{
+    if (!isValid())
+        return;
+    process()->send(Messages::WebPage::ClearSelection(), m_pageID);
+}
 
 void WebPageProxy::validateCommand(const String& commandName, PassRefPtr<ValidateCommandCallback> callback)
 {
@@ -653,6 +660,11 @@ void WebPageProxy::validateCommand(const String& commandName, PassRefPtr<Validat
     uint64_t callbackID = callback->callbackID();
     m_validateCommandCallbacks.set(callbackID, callback.get());
     process()->send(Messages::WebPage::ValidateCommand(commandName, callbackID), m_pageID);
+}
+
+void WebPageProxy::setMaintainsInactiveSelection(bool newValue)
+{
+    m_maintainsInactiveSelection = newValue;
 }
     
 void WebPageProxy::executeEditCommand(const String& commandName)

Source/WebKit2/UIProcess/WebPageProxy.h
@@ -234,6 +234,8 @@ public:
 
     void setInitialFocus(bool);
     void setWindowResizerSize(const WebCore::IntSize&);
+    
+    void clearSelection();
 
     void setViewNeedsDisplay(const WebCore::IntRect&);
     void displayView();
@@ -259,6 +261,9 @@ public:
     bool canDelete() const { return hasSelectedRange() && isContentEditable(); }
     bool hasSelectedRange() const { return m_editorState.selectionIsRange; }
     bool isContentEditable() const { return m_editorState.isContentEditable; }
+    
+    bool maintainsInactiveSelection() const { return m_maintainsInactiveSelection; }
+    void setMaintainsInactiveSelection(bool);
 
 #if PLATFORM(MAC)
     void updateWindowIsVisible(bool windowIsVisible);
@@ -784,6 +789,8 @@ private:
     bool m_canGoBack;
     bool m_canGoForward;
     RefPtr<WebBackForwardList> m_backForwardList;
+    
+    bool m_maintainsInactiveSelection;
 
     String m_toolTip;
 

Source/WebKit2/WebProcess/WebPage/WebPage.cpp
@@ -78,7 +78,6 @@
 #include <WebCore/DocumentMarkerController.h>
 #include <WebCore/DragController.h>
 #include <WebCore/DragData.h>
-#include <WebCore/EditingBehavior.h>
 #include <WebCore/EventHandler.h>
 #include <WebCore/FocusController.h>
 #include <WebCore/FormState.h>
@@ -1206,9 +1205,6 @@ void WebPage::viewWillEndLiveResize()
 
 void WebPage::setFocused(bool isFocused)
 {
-    if (!isFocused && m_page->focusController()->focusedOrMainFrame()->editor()->behavior().shouldClearSelectionWhenLosingWebPageFocus())
-        m_page->focusController()->focusedOrMainFrame()->selection()->clear();
-
     m_page->focusController()->setFocused(isFocused);
 }
 
@@ -1805,6 +1801,11 @@ void WebPage::replaceSelectionWithText(Frame* frame, const String& text)
     RefPtr<DocumentFragment> textFragment = createFragmentFromText(frame->selection()->toNormalizedRange().get(), text);
     applyCommand(ReplaceSelectionCommand::create(frame->document(), textFragment.release(), ReplaceSelectionCommand::SelectReplacement | ReplaceSelectionCommand::MatchStyle | ReplaceSelectionCommand::PreventNesting));
     frame->selection()->revealSelection(ScrollAlignment::alignToEdgeIfNeeded);
+}
+
+void WebPage::clearSelection()
+{
+    m_page->focusController()->focusedOrMainFrame()->selection()->clear();
 }
 
 bool WebPage::mainFrameHasCustomRepresentation() const

Source/WebKit2/WebProcess/WebPage/WebPage.h
@@ -364,6 +364,7 @@ public:
 #endif
 
     void replaceSelectionWithText(WebCore::Frame*, const String&);
+    void clearSelection();
 #if PLATFORM(WIN)
     void performDragControllerAction(uint64_t action, WebCore::IntPoint clientPosition, WebCore::IntPoint globalPosition, uint64_t draggingSourceOperationMask, const WebCore::DragDataMap&, uint32_t flags);
 #else

Source/WebKit2/WebProcess/WebPage/WebPage.messages.in
@@ -67,6 +67,8 @@ messages -> WebPage {
 
     DidReceivePolicyDecision(uint64_t frameID, uint64_t listenerID, uint32_t policyAction, uint64_t downloadID)
 
+    ClearSelection()
+
     # Callbacks.
     GetContentsAsString(uint64_t callbackID)
     GetMainResourceDataOfFrame(uint64_t frameID, uint64_t callbackID)

Source/WebCore/WebCore.exp.in
@@ -1248,7 +1248,6 @@ __ZNK7WebCore6Editor7Command7executeERKN3WTF6StringEPNS_5EventE
 __ZNK7WebCore6Editor7Command9isEnabledEPNS_5EventE
 __ZNK7WebCore6Editor7canCopyEv
 __ZNK7WebCore6Editor7canEditEv
-__ZNK7WebCore6Editor8behaviorEv
 __ZNK7WebCore6Editor8canPasteEv
 __ZNK7WebCore6Editor9canDeleteEv
 __ZNK7WebCore6Widget14platformWidgetEv

Source/WebCore/editing/EditingBehavior.h
@@ -59,10 +59,6 @@ public:
 
     // On Mac, when processing a contextual click, the object being clicked upon should be selected.
     bool shouldSelectOnContextualMenuClick() const { return m_type == EditingMacBehavior; }
-    
-    // On Mac, when the web view loses focus, any active selection clears. On Windows, the selection
-    // should remain highlighted, just in an inactive state.
-    bool shouldClearSelectionWhenLosingWebPageFocus() const { return m_type == EditingMacBehavior; }
 
 private:
     EditingBehaviorType m_type;

commit 685abaf541a866b974d701492ccd71e5e8eda38c

Web Inspector: Switching tabs to window with inspector open prohibits typing into console https://bugs.webkit.org/show_bug.cgi?id=126800
Reviewed by Anders Carlsson.

This is a regression from r85356 and r83814. These two patches made WKWebView clear its selection
when WKView resigns the first responder without ever restoring it even if WKView later becomes
the first responder again. This is problematic when a text field or a editing host element had been
focused and selected prior to the resignation since the editing code uses the selection to determine
the editability of the element.

Fixed the bug by restoring selection in [WKView becomeFirstResponder] if the selection is empty.

* UIProcess/API/mac/WKView.mm:
(-[WKView becomeFirstResponder]):
* UIProcess/WebPageProxy.cpp:
(WebKit::WebPageProxy::restoreSelectionInFocusedEditableElement):
* UIProcess/WebPageProxy.h:
* WebProcess/WebPage/WebPage.cpp:
(WebKit::WebPage::restoreSelectionInFocusedEditableElement):
* WebProcess/WebPage/WebPage.h:
* WebProcess/WebPage/WebPage.messages.in:


git-svn-id: http://svn.webkit.org/repository/webkit/trunk@181972 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Source/WebKit2/UIProcess/API/mac/WKView.mm
@@ -447,6 +447,8 @@ struct WKViewInterpretKeyEventsParameters {
     
     [self _updateSecureInputState];
     _data->_page->viewStateDidChange(ViewState::IsFocused);
+    // Restore the selection in the editable region if resigning first responder cleared selection.
+    _data->_page->restoreSelectionInFocusedEditableElement();
 
     _data->_inBecomeFirstResponder = false;
     

Source/WebKit2/UIProcess/WebPageProxy.cpp
@@ -1444,6 +1444,13 @@ void WebPageProxy::clearSelection()
     m_process->send(Messages::WebPage::ClearSelection(), m_pageID);
 }
 
+void WebPageProxy::restoreSelectionInFocusedEditableElement()
+{
+    if (!isValid())
+        return;
+    m_process->send(Messages::WebPage::RestoreSelectionInFocusedEditableElement(), m_pageID);
+}
+
 void WebPageProxy::validateCommand(const String& commandName, std::function<void (const String&, bool, int32_t, CallbackBase::Error)> callbackFunction)
 {
     if (!isValid()) {

Source/WebKit2/UIProcess/WebPageProxy.h
@@ -393,6 +393,7 @@ public:
     void setWindowResizerSize(const WebCore::IntSize&);
     
     void clearSelection();
+    void restoreSelectionInFocusedEditableElement();
 
     void setViewNeedsDisplay(const WebCore::IntRect&);
     void displayView();

Source/WebKit2/WebProcess/WebPage/WebPage.cpp
@@ -3366,6 +3366,18 @@ void WebPage::clearSelection()
 }
 #endif
 
+void WebPage::restoreSelectionInFocusedEditableElement()
+{
+    Frame& frame = m_page->focusController().focusedOrMainFrame();
+    if (!frame.selection().isNone())
+        return;
+
+    if (auto document = frame.document()) {
+        if (auto element = document->focusedElement())
+            element->updateFocusAppearance(true /* restoreSelection */);
+    }
+}
+
 bool WebPage::mainFrameHasCustomContentProvider() const
 {
     if (Frame* frame = mainFrame()) {

Source/WebKit2/WebProcess/WebPage/WebPage.h
@@ -683,6 +683,7 @@ public:
 
     void replaceSelectionWithText(WebCore::Frame*, const String&);
     void clearSelection();
+    void restoreSelectionInFocusedEditableElement();
 
 #if ENABLE(DRAG_SUPPORT)
 #if PLATFORM(GTK)

Source/WebKit2/WebProcess/WebPage/WebPage.messages.in
@@ -143,6 +143,7 @@ messages -> WebPage LegacyReceiver {
     DidReceivePolicyDecision(uint64_t frameID, uint64_t listenerID, uint32_t policyAction, uint64_t navigationID, uint64_t downloadID)
 
     ClearSelection()
+    RestoreSelectionInFocusedEditableElement()
 
     # Callbacks.
     GetContentsAsString(uint64_t callbackID)