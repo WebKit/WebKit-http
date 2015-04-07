d55edb6bdec40bd43b7b5d0953270c32f06fcfe9
Zan Dobersek <zdobersek@igalia.com>
2015-04-07 03:06:09 -0700

    [WIP][TEMP] Use GSourceWrap::Static for the layer flush timer.

--- Source/WebKit2/WebProcess/WebPage/CoordinatedGraphics/ThreadedCoordinatedLayerTreeHost.h
+++ Source/WebKit2/WebProcess/WebPage/CoordinatedGraphics/ThreadedCoordinatedLayerTreeHost.h
@@ -42,8 +42,8 @@
 #include <WebCore/PageOverlay.h>
 #include <WebCore/Timer.h>
 #include <wtf/OwnPtr.h>
-#include <wtf/RunLoop.h>
 #include <wtf/Threading.h>
+#include <wtf/gobject/GSourceWrap.h>
 
 namespace WebCore {
 class CoordinatedSurface;
@@ -146,7 +146,7 @@ private:
     float m_lastScaleFactor;
     WebCore::IntPoint m_lastScrollPosition;
 
-    RunLoop::Timer<ThreadedCoordinatedLayerTreeHost> m_layerFlushTimer;
+    GSourceWrap::Static m_layerFlushTimer;
     bool m_layerFlushSchedulingEnabled;
 };
 
