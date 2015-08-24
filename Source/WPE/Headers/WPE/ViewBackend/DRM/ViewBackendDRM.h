d1939f8cfa960b3f720518bbb9c6ff977706846b
Zan Dobersek <zdobersek@igalia.com>
2015-08-24 04:20:46 -0700

    Add ViewBackendDRM implementation.

---
+++ Source/WPE/Headers/WPE/ViewBackend/DRM/ViewBackendDRM.h
@@ -0,0 +1,78 @@
+/*
+ * Copyright (C) 2015 Igalia S.L.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY APPLE INC. AND ITS CONTRIBUTORS ``AS IS''
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL APPLE INC. OR ITS CONTRIBUTORS
+ * BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
+ * THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#ifndef WPE_ViewBackend_ViewBackendDRM_h
+#define WPE_ViewBackend_ViewBackendDRM_h
+
+#include <unordered_map>
+#include <utility>
+
+typedef struct _drmModeModeInfo drmModeModeInfo;
+struct gbm_bo;
+struct gbm_device;
+
+namespace WPE {
+
+namespace ViewBackend {
+
+class Client;
+
+class ViewBackendDRM {
+public:
+    ViewBackendDRM();
+    ~ViewBackendDRM();
+    void setClient(Client*);
+
+    void commitPrimeFD(int fd, uint32_t handle, uint32_t width, uint32_t height, uint32_t stride, uint32_t format);
+
+    struct PageFlipHandlerData {
+        Client* client;
+        bool bufferLocked { false };
+        uint32_t lockedBufferHandle { 0 };
+    };
+
+private:
+    struct {
+        int fd;
+        struct gbm_device* device;
+    } m_gbm;
+
+    struct {
+        int fd;
+        drmModeModeInfo* mode;
+        uint32_t crtcId;
+        uint32_t connectorId;
+    } m_drm;
+
+    PageFlipHandlerData m_pageFlipData;
+
+    std::unordered_map<uint32_t, std::pair<struct gbm_bo*, uint32_t>> m_fbMap;
+};
+
+} // namespace ViewBackend
+
+} // namespace WPE
+
+#endif // WPE_ViewBackend_ViewBackendDRM_h
