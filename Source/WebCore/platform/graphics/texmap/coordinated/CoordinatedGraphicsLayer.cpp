05db745f692b62b8f67cb7edd892dc29c458524c
Zan Dobersek <zdobersek@igalia.com>
2015-09-22 23:31:17 -0700

    Decouple CSS animation non-updates from layer flushes.

--- Source/WebCore/platform/graphics/texmap/coordinated/CoordinatedGraphicsLayer.cpp
+++ Source/WebCore/platform/graphics/texmap/coordinated/CoordinatedGraphicsLayer.cpp
@@ -781,9 +781,6 @@ void CoordinatedGraphicsLayer::flushCompositingStateForThisLayerOnly(bool)
 {
     ASSERT(m_coordinator->isFlushingLayerChanges());
 
-    if (m_animations.hasRunningAnimations())
-        client().notifyFlushBeforeDisplayRefresh(this);
-
     // When we have a transform animation, we need to update visible rect every frame to adjust the visible rect of a backing store.
     bool hasActiveTransformAnimation = selfOrAncestorHasActiveTransformAnimation();
     if (hasActiveTransformAnimation)
