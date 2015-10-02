a3cf708ed3a0d6af43ce066b466534e4d2ab8f19
Zan Dobersek <zdobersek@igalia.com>
2015-10-02 02:52:23 -0700

    Remove the DIAL source files. The DIAL server will be reimplemented as necessary, probably not inside WebKit.

--- Source/WebKit2/UIProcess/wpe/AtholShell/DIAL/quick_ssdp.h
+++
@@ -1,9 +0,0 @@
-#ifndef quick_ssdp_h
-#define quick_ssdp_h
-
-#ifdef __cplusplus
-extern "C"
-#endif
-void run_ssdp(int port, const char *pFriendlyName, const char * pModelName, const char *pUuid);
-
-#endif // quick_ssdp
