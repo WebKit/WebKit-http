129325fe5b3f32bc79c481f66232c886911d85fb
Claudio Saavedra <csaavedra@igalia.com>
2015-10-21 19:20:22 +0300

    WebEditorClientWPE: Support most keybindings and special keys
    This handles most common keybindings, including Shift selection,
    navigation, etc.
    
    Do not handle yet clipboard commands as there is no clipboard support
    yet.

--- Source/WebKit2/WebProcess/WebCoreSupport/wpe/WebEditorClientWPE.cpp
+++ Source/WebKit2/WebProcess/WebCoreSupport/wpe/WebEditorClientWPE.cpp
@@ -38,33 +38,126 @@ using namespace WebCore;
 
 namespace WebKit {
 
+// The idea for the array/map below comes from Blink's EditingBehavior.cpp.
+
+static const unsigned CtrlKey  = 1 << 0;
+static const unsigned AltKey   = 1 << 1;
+static const unsigned ShiftKey = 1 << 2;
+static const unsigned MetaKey  = 1 << 3;
+
+// Keys with special meaning. These will be delegated to the editor using
+// the execCommand() method
+struct KeyDownEntry {
+    unsigned virtualKey;
+    unsigned modifiers;
+    const char* name;
+};
+
+static const KeyDownEntry keyDownEntries[] = {
+    { VK_LEFT,   0,                  "MoveLeft"                                },
+    { VK_LEFT,   ShiftKey,           "MoveLeftAndModifySelection"              },
+    { VK_LEFT,   CtrlKey,            "MoveWordLeft"                            },
+    { VK_LEFT,   CtrlKey | ShiftKey,
+        "MoveWordLeftAndModifySelection"                                       },
+    { VK_RIGHT,  0,                  "MoveRight"                               },
+    { VK_RIGHT,  ShiftKey,           "MoveRightAndModifySelection"             },
+    { VK_RIGHT,  CtrlKey,            "MoveWordRight"                           },
+    { VK_RIGHT,  CtrlKey | ShiftKey, "MoveWordRightAndModifySelection"         },
+    { VK_UP,     0,                  "MoveUp"                                  },
+    { VK_UP,     ShiftKey,           "MoveUpAndModifySelection"                },
+    { VK_PRIOR,  ShiftKey,           "MovePageUpAndModifySelection"            },
+    { VK_DOWN,   0,                  "MoveDown"                                },
+    { VK_DOWN,   ShiftKey,           "MoveDownAndModifySelection"              },
+    { VK_NEXT,   ShiftKey,           "MovePageDownAndModifySelection"          },
+    { VK_UP,     CtrlKey | ShiftKey, "MoveParagraphBackwardAndModifySelection" },
+    { VK_DOWN,   CtrlKey | ShiftKey, "MoveParagraphForwardAndModifySelection"  },
+    { VK_PRIOR,  0,                  "MovePageUp"                              },
+    { VK_NEXT,   0,                  "MovePageDown"                            },
+    { VK_HOME,   0,                  "MoveToBeginningOfLine"                   },
+    { VK_HOME,   ShiftKey,
+        "MoveToBeginningOfLineAndModifySelection"                              },
+    { VK_HOME,   CtrlKey,            "MoveToBeginningOfDocument"               },
+    { VK_HOME,   CtrlKey | ShiftKey,
+        "MoveToBeginningOfDocumentAndModifySelection"                          },
+    { VK_END,    0,                  "MoveToEndOfLine"                         },
+    { VK_END,    ShiftKey,           "MoveToEndOfLineAndModifySelection"       },
+    { VK_END,    CtrlKey,            "MoveToEndOfDocument"                     },
+    { VK_END,    CtrlKey | ShiftKey,
+        "MoveToEndOfDocumentAndModifySelection"                                },
+    { VK_BACK,   0,                  "DeleteBackward"                          },
+    { VK_BACK,   ShiftKey,           "DeleteBackward"                          },
+    { VK_DELETE, 0,                  "DeleteForward"                           },
+    { VK_BACK,   CtrlKey,            "DeleteWordBackward"                      },
+    { VK_DELETE, CtrlKey,            "DeleteWordForward"                       },
+    { 'B',         CtrlKey,          "ToggleBold"                              },
+    { 'I',         CtrlKey,          "ToggleItalic"                            },
+    { 'U',         CtrlKey,          "ToggleUnderline"                         },
+    { VK_ESCAPE, 0,                  "Cancel"                                  },
+    { VK_OEM_PERIOD, CtrlKey,        "Cancel"                                  },
+    { VK_TAB,    0,                  "InsertTab"                               },
+    { VK_TAB,    ShiftKey,           "InsertBacktab"                           },
+    { VK_RETURN, 0,                  "InsertNewline"                           },
+    { VK_RETURN, CtrlKey,            "InsertNewline"                           },
+    { VK_RETURN, AltKey,             "InsertNewline"                           },
+    { VK_RETURN, AltKey | ShiftKey,  "InsertNewline"                           },
+    { VK_RETURN, ShiftKey,           "InsertLineBreak"                         },
+    // These probably need handling somewhere else so do not execute them. The
+    // 'Cut' command is removing text so let's avoid losing the user losing data
+    // until we implement clipboard support wherever it should be.
+    // { VK_INSERT, CtrlKey,            "Copy"                                    },
+    // { VK_INSERT, ShiftKey,           "Paste"                                   },
+    // { VK_DELETE, ShiftKey,           "Cut"                                     },
+    // { 'C',         CtrlKey,          "Copy"                                    },
+    // { 'V',         CtrlKey,          "Paste"                                   },
+    // { 'V',         CtrlKey | ShiftKey, "PasteAndMatchStyle"                    },
+    // { 'X',         CtrlKey,          "Cut"                                     },
+    { 'A',         CtrlKey,          "SelectAll"                               },
+    { 'Z',         CtrlKey,          "Undo"                                    },
+    { 'Z',         CtrlKey | ShiftKey, "Redo"                                  },
+    { 'Y',         CtrlKey,          "Redo"                                    },
+    { VK_INSERT, 0,                  "OverWrite"                               },
+};
+
+static const char* interpretKeyEvent(const KeyboardEvent& event)
+{
+    static HashMap<int, const char*>* keyDownCommandsMap = nullptr;
+
+    if (!keyDownCommandsMap) {
+        keyDownCommandsMap = new HashMap<int, const char*>;
+        unsigned arraysize = sizeof(keyDownEntries)/sizeof(keyDownEntries[0]);
+        for (unsigned i = 0; i < arraysize; i++)
+            keyDownCommandsMap->set(keyDownEntries[i].modifiers << 16 | keyDownEntries[i].virtualKey, keyDownEntries[i].name);
+    }
+
+    unsigned modifiers = 0;
+    if (event.shiftKey())
+        modifiers |= ShiftKey;
+    if (event.altKey())
+        modifiers |= AltKey;
+    if (event.ctrlKey())
+        modifiers |= CtrlKey;
+    if (event.metaKey())
+        modifiers |= MetaKey;
+
+    int mapKey = modifiers << 16 | event.keyCode();
+    return mapKey ? keyDownCommandsMap->get(mapKey) : 0;
+}
+
 static void handleKeyPress(Frame& frame, KeyboardEvent& event, const PlatformKeyboardEvent& platformEvent)
 {
-    {
-        bool handled = true;
-        int keyCode = event.keyCode();
+    String commandName = interpretKeyEvent(event);
 
-        switch (keyCode) {
-        case VK_BACK:
-            frame.editor().command("DeleteBackward").execute();
-            break;
-        case VK_DELETE:
-            frame.editor().command("DeleteForward").execute();
-            break;
-        default:
-            handled = false;
-            break;
-        }
-
-        if (handled)
-            return;
+    if (!commandName.isEmpty()) {
+        frame.editor().command(commandName).execute();
+        event.setDefaultHandled();
+        return;
     }
 
     // Don't insert null or control characters as they can result in unexpected behaviour
     if (event.charCode() < ' ')
         return;
 
-    // Don't insert anything if a modifier is pressed
+    // Don't insert anything if a modifier is pressed and it has not been handled yet
     if (platformEvent.ctrlKey() || platformEvent.altKey())
         return;
 
@@ -74,26 +167,17 @@ static void handleKeyPress(Frame& frame, KeyboardEvent& event, const PlatformKey
 
 static void handleKeyDown(Frame& frame, KeyboardEvent& event, const PlatformKeyboardEvent&)
 {
-    bool handled = true;
-    int keyCode = event.keyCode();
+    String commandName = interpretKeyEvent(event);
+    Editor::Command command = frame.editor().command(commandName);
 
-    switch (keyCode) {
-    case VK_LEFT:
-        frame.editor().command("MoveLeft").execute();
-        break;
-    case VK_RIGHT:
-        frame.editor().command("MoveRight").execute();
-        break;
-    case VK_DELETE:
-        frame.editor().command("DeleteForward").execute();
-        break;
-    default:
-        handled = false;
-        break;
-    }
+    // We shouldn't insert text through the editor. Let WebCore decide
+    // how to handle that (say, Tab, which could also be used to
+    // change focus).
+    if (command.isTextInsertion() || commandName.isEmpty())
+        return;
 
-    if (handled)
-        event.setDefaultHandled();
+    command.execute();
+    event.setDefaultHandled();
 }
 
 void WebEditorClient::handleKeyboardEvent(WebCore::KeyboardEvent* event)
