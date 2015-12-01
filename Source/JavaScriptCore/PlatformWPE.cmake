a672e3af02bbbd3e0fedb389bdec0689779f7629
Zan Dobersek <zdobersek@igalia.com>
2015-12-01 03:04:22 -0800

    WPE: make JSC headers installable.

--- Source/JavaScriptCore/PlatformWPE.cmake
+++ Source/JavaScriptCore/PlatformWPE.cmake
@@ -5,3 +5,18 @@ list(APPEND JavaScriptCore_INCLUDE_DIRECTORIES
     ${GLIB_INCLUDE_DIRS}
     ${WTF_DIR}
 )
+
+set(WPE_INSTALLED_JAVASCRIPTCORE_HEADERS
+    API/JavaScript.h
+    API/JSBase.h
+    API/JSContextRef.h
+    API/JSObjectRef.h
+    API/JSStringRef.h
+    API/JSValueRef.h
+    API/WebKitAvailability.h
+)
+
+install(FILES ${WPE_INSTALLED_JAVASCRIPTCORE_HEADERS}
+    DESTINATION "${CMAKE_INSTALL_INCLUDEDIR}/wpe-${WPE_API_VERSION}/WPE/JavaScriptCore"
+    COMPONENT "Development"
+)
