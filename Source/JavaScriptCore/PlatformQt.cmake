82e80070e551441774e52be1bb41ce5c0dc79926
Vitaly Slobodin <vitaliy.slobodin@gmail.com>
2016-01-26 21:13:09 +0300

    [Qt, Windows] Copy forwarding headers before building a project

--- Source/JavaScriptCore/PlatformQt.cmake
+++ Source/JavaScriptCore/PlatformQt.cmake
@@ -20,13 +20,15 @@ if (WIN32)
 
     file(MAKE_DIRECTORY ${DERIVED_SOURCES_DIR}/ForwardingHeaders/JavaScriptCore)
 
+    set(JavaScriptCore_PRE_BUILD_COMMAND "${CMAKE_BINARY_DIR}/DerivedSources/JavaScriptCore/preBuild.cmd")
+    file(REMOVE "${JavaScriptCore_PRE_BUILD_COMMAND}")
+    foreach (_directory ${JavaScriptCore_FORWARDING_HEADERS_DIRECTORIES})
+        file(APPEND "${JavaScriptCore_PRE_BUILD_COMMAND}" "@xcopy /y /d /f \"${JAVASCRIPTCORE_DIR}/${_directory}/*.h\" \"${DERIVED_SOURCES_DIR}/ForwardingHeaders/JavaScriptCore\" >nul 2>nul\n")
+    endforeach ()
+
     set(JavaScriptCore_POST_BUILD_COMMAND "${CMAKE_BINARY_DIR}/DerivedSources/JavaScriptCore/postBuild.cmd")
     file(WRITE "${JavaScriptCore_POST_BUILD_COMMAND}" "@xcopy /y /d /f \"${DERIVED_SOURCES_DIR}/JavaScriptCore/*.h\" \"${DERIVED_SOURCES_DIR}/ForwardingHeaders/JavaScriptCore\" >nul 2>nul\n")
     file(APPEND "${JavaScriptCore_POST_BUILD_COMMAND}" "@xcopy /y /d /f \"${DERIVED_SOURCES_DIR}/JavaScriptCore/inspector/*.h\" \"${DERIVED_SOURCES_DIR}/ForwardingHeaders/JavaScriptCore\" >nul 2>nul\n")
-
-    foreach (_directory ${JavaScriptCore_FORWARDING_HEADERS_DIRECTORIES})
-        file(APPEND "${JavaScriptCore_POST_BUILD_COMMAND}" "@xcopy /y /d /f \"${JAVASCRIPTCORE_DIR}/${_directory}/*.h\" \"${DERIVED_SOURCES_DIR}/ForwardingHeaders/JavaScriptCore\" >nul 2>nul\n")
-    endforeach ()
 
     set(JavaScriptCore_LIBRARY_TYPE STATIC)
 endif ()
