fb0f23d32b6e71c16a6df6c4c7a55b354531856e
Zan Dobersek <zdobersek@igalia.com>
2016-02-09 03:26:42 -0800

    WebKitWebSourceGStreamer: revert 364cdd95, assert we're on the main thread.

--- Source/WebCore/platform/graphics/gstreamer/WebKitWebSourceGStreamer.cpp
+++ Source/WebCore/platform/graphics/gstreamer/WebKitWebSourceGStreamer.cpp
@@ -420,24 +420,15 @@ static void webKitWebSrcStop(WebKitWebSrc* src)
 
     priv->notifier.cancelPendingNotifications(MainThreadSourceNotification::NeedData | MainThreadSourceNotification::EnoughData | MainThreadSourceNotification::Seek);
 
-    {
-        ResourceHandleStreamingClient* client = nullptr;
-        if (priv->client) {
-            client = priv->client;
-            priv->client = nullptr;
-        }
+    ASSERT(isMainThread());
 
-        PlatformMediaResourceLoader* loader = nullptr;
-        if (!priv->keepAlive && priv->loader)
-            loader = priv->loader.leakRef();
-
-        if (client || loader)
-            callOnMainThread([client, loader] {
-                delete client;
-
-                RefPtr<PlatformMediaResourceLoader> loaderRef = adoptRef(loader);
-            });
+    if (priv->client) {
+        delete priv->client;
+        priv->client = nullptr;
     }
+
+    if (!priv->keepAlive)
+        priv->loader = nullptr;
 
     if (priv->buffer) {
         unmapGstBuffer(priv->buffer.get());
