b4dd806d7bc7778136deb232d94bf8cf97f95847
Zan Dobersek <zdobersek@igalia.com>
2016-02-10 01:52:27 -0800

    BufferFactoryBCMNexus: implement by leveraging the wl_nsc interface.

--- Source/WPE/Source/Graphics/Wayland/BufferFactoryBCMNexus.cpp
+++ Source/WPE/Source/Graphics/Wayland/BufferFactoryBCMNexus.cpp
@@ -5,23 +5,88 @@
 
 #include "BufferDataBCMNexus.h"
 #include "WaylandDisplay.h"
+#include "nsc-client-protocol.h"
+#include <string.h>
 
-#define ALIGN_UP(x, y)  ((x + (y)-1) & ~((y)-1))
+#include <refsw/nexus_config.h>
+#include <refsw/nexus_platform.h>
+#include <refsw/nexus_display.h>
+#include <refsw/nexus_core_utils.h>
+#include <refsw/default_nexus.h>
+#include <refsw/nxclient.h>
 
 namespace WPE {
 
 namespace Graphics {
 
+static const struct wl_nsc_listener g_nscListener = {
+    // handle_standby_status
+    [](void*, struct wl_nsc*, struct wl_array*) { },
+    // connectID
+    [](void*, struct wl_nsc*, uint32_t) { },
+    // composition
+    [](void*, struct wl_nsc*, struct wl_array*) { },
+    // authenticated
+    [](void* data, struct wl_nsc*, const char* certData, uint32_t certLength)
+    {
+        auto& nscData = *static_cast<BufferFactoryBCMNexus::NSCData*>(data);
+        nscData.authenticationData = { certData, certLength };
+    },
+    // clientID_created
+    [](void* data, struct wl_nsc*, struct wl_array* clientIDArray)
+    {
+        auto& nscData = *static_cast<BufferFactoryBCMNexus::NSCData*>(data);
+        nscData.clientID = static_cast<uint32_t*>(clientIDArray->data)[0];
+    },
+    // display_geometry
+    [](void* data, struct wl_nsc*, uint32_t width, uint32_t height) { },
+    // audiosettings
+    [](void*, struct wl_nsc*, struct wl_array*) { },
+    // displaystatus
+    [](void*, struct wl_nsc*, struct wl_array*) { },
+    // picturequalitysettings
+    [](void*, struct wl_nsc*, struct wl_array*) { },
+    // displaysettings
+    [](void*, struct wl_nsc*, struct wl_array*) { },
+};
+
+BufferFactoryBCMNexus::BufferFactoryBCMNexus()
+    : m_display(ViewBackend::WaylandDisplay::singleton())
+{
+    wl_nsc_add_listener(m_display.interfaces().nsc, &g_nscListener, &m_nscData);
+}
+
 BufferFactoryBCMNexus::~BufferFactoryBCMNexus() = default;
 
-uint32_t BufferFactoryBCMNexus::constructRenderingTarget(uint32_t, uint32_t)
+std::pair<const uint8_t*, size_t> BufferFactoryBCMNexus::authenticate()
 {
-    return 0;
+    wl_nsc_authenticate(m_display.interfaces().nsc);
+    wl_display_roundtrip(m_display.display());
+
+    return { reinterpret_cast<const uint8_t*>(m_nscData.authenticationData.data()), m_nscData.authenticationData.size() };
+}
+
+uint32_t BufferFactoryBCMNexus::constructRenderingTarget(uint32_t width, uint32_t height)
+{
+    wl_nsc_request_clientID(m_display.interfaces().nsc, WL_NSC_CLIENT_SURFACE);
+    wl_display_roundtrip(m_display.display());
+
+    wl_nsc_create_window(m_display.interfaces().nsc, m_nscData.clientID, 0, width, height);
+    wl_display_roundtrip(m_display.display());
+
+    return m_nscData.clientID;
 }
 
 std::pair<bool, std::pair<uint32_t, struct wl_buffer*>> BufferFactoryBCMNexus::createBuffer(int, const uint8_t*, size_t)
 {
-    return { false, { 0, nullptr } };
+    if (!m_nscData.buffer) {
+        m_nscData.buffer = wl_nsc_create_buffer(m_display.interfaces().nsc, m_nscData.clientID, 800, 600);
+        wl_display_roundtrip(m_display.display());
+
+        return { true, { 0, m_nscData.buffer } };
+    }
+
+    return { true, { 0, nullptr } };
 }
 
 void BufferFactoryBCMNexus::destroyBuffer(uint32_t, struct wl_buffer*)
--- Source/WPE/Source/Graphics/Wayland/BufferFactoryBCMNexus.h
+++ Source/WPE/Source/Graphics/Wayland/BufferFactoryBCMNexus.h
@@ -5,19 +5,37 @@
 
 #include "BufferFactory.h"
 
+#include <string>
 #include <unordered_map>
 
 namespace WPE {
+
+namespace ViewBackend {
+class WaylandDisplay;
+}
 
 namespace Graphics {
 
 class BufferFactoryBCMNexus : public BufferFactory {
 public:
+    BufferFactoryBCMNexus();
     virtual ~BufferFactoryBCMNexus();
 
+    std::pair<const uint8_t*, size_t> authenticate() override;
     uint32_t constructRenderingTarget(uint32_t, uint32_t) override;
     std::pair<bool, std::pair<uint32_t, struct wl_buffer*>> createBuffer(int, const uint8_t*, size_t) override;
     void destroyBuffer(uint32_t, struct wl_buffer*) override;
+
+    struct NSCData {
+        uint32_t clientID;
+        struct wl_buffer* buffer;
+        std::string authenticationData;
+    };
+
+private:
+    ViewBackend::WaylandDisplay& m_display;
+
+    NSCData m_nscData { 0, nullptr, std::string{ } };
 };
 
 } // namespace Graphics
