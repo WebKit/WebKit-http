WebKit/WebKit-http

commit 0b912557c049efbf3806dd65e0e07221aa7210d5

Font antialiasing (smoothing) changes when elements are rendered into compositing layers https://bugs.webkit.org/show_bug.cgi?id=23364
Reviewed by Tim Horton.

Source/WebCore:

Improve the appearance of subpixel-antialiased ("smoothed") text in non-opaque layers
by opting in to a new CALayer backing store format.

GraphicsLayer now has setSupportsSmoothedFonts(), which is called by RenderLayerBacking
when the platform has support for the new feature. Ideally this would only be set when
we know a layer has smoothed text drawn into it, but, for now, enable this for all
layers. The right thing happens with opaque layers under the hood.

setSupportsSmoothedFonts() is turned into a PlatformCALayer contentsFormat flag, which
is ultimately passed to setBackingStoreFormat().

We also need to propagate this flag to TileController tiles.

* platform/graphics/GraphicsLayer.cpp:
(WebCore::GraphicsLayer::supportsSmoothedLayerText):
(WebCore::GraphicsLayer::setSmoothedLayerTextEnabled):
(WebCore::GraphicsLayer::smoothedLayerTextEnabled):
(WebCore::GraphicsLayer::GraphicsLayer):
* platform/graphics/GraphicsLayer.h:
(WebCore::GraphicsLayer::supportsSmoothedFonts):
(WebCore::GraphicsLayer::setSupportsSmoothedFonts):
* platform/graphics/TiledBacking.h:
* platform/graphics/ca/GraphicsLayerCA.cpp:
(WebCore::GraphicsLayer::supportsSmoothedLayerText):
(WebCore::GraphicsLayer::setSmoothedLayerTextEnabled):
(WebCore::GraphicsLayer::smoothedLayerTextEnabled):
(WebCore::GraphicsLayerCA::setSupportsSmoothedFonts):
(WebCore::GraphicsLayerCA::commitLayerChangesBeforeSublayers):
(WebCore::GraphicsLayerCA::updateContentsFormat):
* platform/graphics/ca/GraphicsLayerCA.h:
* platform/graphics/ca/PlatformCALayer.cpp:
(WebCore::PlatformCALayer::drawRepaintIndicator): Give the number a "shadow" when
the contents format says we support smoothed fonts.
* platform/graphics/ca/PlatformCALayer.h:
* platform/graphics/ca/TileController.cpp:
(WebCore::TileController::setTileContentsFormatFlags):

Source/WebCore/platform/graphics/ca/GraphicsLayerCA.cpp
@@ -309,6 +309,26 @@ bool GraphicsLayer::supportsBackgroundColorContent()
     return true;
 }
 
+static bool isSmoothedLayerTextEnabled = true;
+
+bool GraphicsLayer::supportsSmoothedLayerText()
+{
+#if PLATFORM(MAC) && __MAC_OS_X_VERSION_MIN_REQUIRED >= 101200
+    return isSmoothedLayerTextEnabled;
+#endif
+    return false;
+}
+
+void GraphicsLayer::setSmoothedLayerTextEnabled(bool flag)
+{
+    isSmoothedLayerTextEnabled = flag;
+}
+
+bool GraphicsLayer::smoothedLayerTextEnabled()
+{
+    return isSmoothedLayerTextEnabled;
+}
+
 std::unique_ptr<GraphicsLayer> GraphicsLayer::create(GraphicsLayerFactory* factory, GraphicsLayerClient& client, Type layerType)
 {
     std::unique_ptr<GraphicsLayer> graphicsLayer;
@@ -707,6 +727,15 @@ void GraphicsLayerCA::setContentsOpaque(bool opaque)
 
     GraphicsLayer::setContentsOpaque(opaque);
     noteLayerPropertyChanged(ContentsOpaqueChanged);
+}
+
+void GraphicsLayerCA::setSupportsSmoothedFonts(bool supportsSmoothedFonts)
+{
+    if (m_supportsSmoothedFonts == supportsSmoothedFonts)
+        return;
+
+    GraphicsLayer::setSupportsSmoothedFonts(supportsSmoothedFonts);
+    noteLayerPropertyChanged(ContentsFormatChanged);
 }
 
 void GraphicsLayerCA::setBackfaceVisibility(bool visible)
@@ -1580,6 +1609,9 @@ void GraphicsLayerCA::commitLayerChangesBeforeSublayers(CommitState& commitState
     if (m_uncommittedChanges & ContentsOpaqueChanged)
         updateContentsOpaque(pageScaleFactor);
 
+    if (m_uncommittedChanges & ContentsFormatChanged)
+        updateContentsFormat();
+
     if (m_uncommittedChanges & BackfaceVisibilityChanged)
         updateBackfaceVisibility();
 
@@ -1887,6 +1919,20 @@ void GraphicsLayerCA::updateContentsOpaque(float pageScaleFactor)
     if (LayerMap* layerCloneMap = m_layerClones.get()) {
         for (auto& layer : layerCloneMap->values())
             layer->setOpaque(contentsOpaque);
+    }
+}
+
+void GraphicsLayerCA::updateContentsFormat()
+{
+    PlatformCALayer::ContentsFormatFlags formatFlags = 0;
+    if (supportsSmoothedFonts())
+        formatFlags |= PlatformCALayer::SmoothedFonts;
+
+    m_layer->setContentsFormat(formatFlags);
+
+    if (LayerMap* layerCloneMap = m_layerClones.get()) {
+        for (auto& layer : layerCloneMap->values())
+            layer->setContentsFormat(formatFlags);
     }
 }
 

Source/WebCore/platform/graphics/GraphicsLayer.cpp
@@ -105,6 +105,22 @@ bool GraphicsLayer::supportsContentsTiling()
 }
 #endif
 
+#if !USE(CA)
+bool GraphicsLayer::supportsSmoothedLayerText()
+{
+    return false;
+}
+
+void GraphicsLayer::setSmoothedLayerTextEnabled(bool)
+{
+}
+
+bool GraphicsLayer::smoothedLayerTextEnabled()
+{
+    return false;
+}
+#endif
+
 GraphicsLayer::GraphicsLayer(Type type, GraphicsLayerClient& client)
     : m_client(client)
     , m_anchorPoint(0.5f, 0.5f, 0)
@@ -115,6 +131,7 @@ GraphicsLayer::GraphicsLayer(Type type, GraphicsLayerClient& client)
 #endif
     , m_type(type)
     , m_contentsOpaque(false)
+    , m_supportsSmoothedFonts(false)
     , m_preserves3D(false)
     , m_backfaceVisibility(true)
     , m_usingTiledBacking(false)

Source/WebCore/platform/graphics/GraphicsLayer.h
@@ -364,6 +364,9 @@ public:
     bool contentsOpaque() const { return m_contentsOpaque; }
     virtual void setContentsOpaque(bool b) { m_contentsOpaque = b; }
 
+    bool supportsSmoothedFonts() const { return m_supportsSmoothedFonts; }
+    virtual void setSupportsSmoothedFonts(bool b) { m_supportsSmoothedFonts = b; }
+
     bool backfaceVisibility() const { return m_backfaceVisibility; }
     virtual void setBackfaceVisibility(bool b) { m_backfaceVisibility = b; }
 
@@ -540,6 +543,10 @@ public:
     static bool supportsBackgroundColorContent();
     static bool supportsLayerType(Type);
     static bool supportsContentsTiling();
+    static bool supportsSmoothedLayerText();
+
+    WEBCORE_EXPORT static void setSmoothedLayerTextEnabled(bool);
+    WEBCORE_EXPORT static bool smoothedLayerTextEnabled();
 
     void updateDebugIndicators();
 
@@ -613,6 +620,7 @@ protected:
     const Type m_type;
 
     bool m_contentsOpaque : 1;
+    bool m_supportsSmoothedFonts : 1;
     bool m_preserves3D: 1;
     bool m_backfaceVisibility : 1;
     bool m_usingTiledBacking : 1;

Source/WebCore/platform/graphics/TiledBacking.h
@@ -79,6 +79,8 @@ public:
     virtual void setTiledScrollingIndicatorPosition(const FloatPoint&) = 0;
     virtual void setTopContentInset(float) = 0;
 
+    virtual void setTileContentsFormatFlags(unsigned) = 0;
+
     virtual void setVelocity(const VelocityData&) = 0;
     
     enum {

Source/WebCore/platform/graphics/ca/GraphicsLayerCA.h
@@ -95,6 +95,8 @@ public:
     WEBCORE_EXPORT void setBackgroundColor(const Color&) override;
 
     WEBCORE_EXPORT void setContentsOpaque(bool) override;
+    WEBCORE_EXPORT void setSupportsSmoothedFonts(bool) override;
+
     WEBCORE_EXPORT void setBackfaceVisibility(bool) override;
 
     // return true if we started an animation
@@ -391,6 +393,7 @@ private:
     void updateMasksToBounds();
     void updateContentsVisibility();
     void updateContentsOpaque(float pageScaleFactor);
+    void updateContentsFormat();
     void updateBackfaceVisibility();
     void updateStructuralLayer();
     void updateDrawsContent();
@@ -455,43 +458,44 @@ private:
     bool appendToUncommittedAnimations(const KeyframeValueList&, const FilterOperation*, const Animation*, const String& animationName, int animationIndex, double timeOffset);
 
     enum LayerChange : uint64_t {
-        NoChange =                      0,
-        NameChanged =                   1LLU << 1,
-        ChildrenChanged =               1LLU << 2, // also used for content layer, and preserves-3d, and size if tiling changes?
-        GeometryChanged =               1LLU << 3,
-        TransformChanged =              1LLU << 4,
-        ChildrenTransformChanged =      1LLU << 5,
-        Preserves3DChanged =            1LLU << 6,
-        MasksToBoundsChanged =          1LLU << 7,
-        DrawsContentChanged =           1LLU << 8,
-        BackgroundColorChanged =        1LLU << 9,
-        ContentsOpaqueChanged =         1LLU << 10,
-        BackfaceVisibilityChanged =     1LLU << 11,
-        OpacityChanged =                1LLU << 12,
-        AnimationChanged =              1LLU << 13,
-        DirtyRectsChanged =             1LLU << 14,
-        ContentsImageChanged =          1LLU << 15,
-        ContentsPlatformLayerChanged =  1LLU << 16,
-        ContentsColorLayerChanged =     1LLU << 17,
-        ContentsRectsChanged =          1LLU << 18,
-        MasksToBoundsRectChanged =      1LLU << 19,
-        MaskLayerChanged =              1LLU << 20,
-        ReplicatedLayerChanged =        1LLU << 21,
-        ContentsNeedsDisplay =          1LLU << 22,
-        AcceleratesDrawingChanged =     1LLU << 23,
-        ContentsScaleChanged =          1LLU << 24,
-        ContentsVisibilityChanged =     1LLU << 25,
-        CoverageRectChanged =           1LLU << 26,
-        FiltersChanged =                1LLU << 27,
-        BackdropFiltersChanged =        1LLU << 28,
-        BackdropFiltersRectChanged =    1LLU << 29,
-        TilingAreaChanged =             1LLU << 30,
-        TilesAdded =                    1LLU << 31,
-        DebugIndicatorsChanged =        1LLU << 32,
-        CustomAppearanceChanged =       1LLU << 33,
-        BlendModeChanged =              1LLU << 34,
-        ShapeChanged =                  1LLU << 35,
-        WindRuleChanged =               1LLU << 36,
+        NoChange                        = 0,
+        NameChanged                     = 1LLU << 1,
+        ChildrenChanged                 = 1LLU << 2, // also used for content layer, and preserves-3d, and size if tiling changes?
+        GeometryChanged                 = 1LLU << 3,
+        TransformChanged                = 1LLU << 4,
+        ChildrenTransformChanged        = 1LLU << 5,
+        Preserves3DChanged              = 1LLU << 6,
+        MasksToBoundsChanged            = 1LLU << 7,
+        DrawsContentChanged             = 1LLU << 8,
+        BackgroundColorChanged          = 1LLU << 9,
+        ContentsOpaqueChanged           = 1LLU << 10,
+        ContentsFormatChanged           = 1LLU << 11,
+        BackfaceVisibilityChanged       = 1LLU << 12,
+        OpacityChanged                  = 1LLU << 13,
+        AnimationChanged                = 1LLU << 14,
+        DirtyRectsChanged               = 1LLU << 15,
+        ContentsImageChanged            = 1LLU << 16,
+        ContentsPlatformLayerChanged    = 1LLU << 17,
+        ContentsColorLayerChanged       = 1LLU << 18,
+        ContentsRectsChanged            = 1LLU << 19,
+        MasksToBoundsRectChanged        = 1LLU << 20,
+        MaskLayerChanged                = 1LLU << 21,
+        ReplicatedLayerChanged          = 1LLU << 22,
+        ContentsNeedsDisplay            = 1LLU << 23,
+        AcceleratesDrawingChanged       = 1LLU << 24,
+        ContentsScaleChanged            = 1LLU << 25,
+        ContentsVisibilityChanged       = 1LLU << 26,
+        CoverageRectChanged             = 1LLU << 27,
+        FiltersChanged                  = 1LLU << 28,
+        BackdropFiltersChanged          = 1LLU << 29,
+        BackdropFiltersRectChanged      = 1LLU << 30,
+        TilingAreaChanged               = 1LLU << 31,
+        TilesAdded                      = 1LLU << 32,
+        DebugIndicatorsChanged          = 1LLU << 33,
+        CustomAppearanceChanged         = 1LLU << 34,
+        BlendModeChanged                = 1LLU << 35,
+        ShapeChanged                    = 1LLU << 36,
+        WindRuleChanged                 = 1LLU << 37,
     };
     typedef uint64_t LayerChangeFlags;
     enum ScheduleFlushOrNot { ScheduleFlush, DontScheduleFlush };

Source/WebCore/platform/graphics/ca/PlatformCALayer.cpp
@@ -80,17 +80,22 @@ void PlatformCALayer::drawRepaintIndicator(CGContextRef context, PlatformCALayer
         CGContextSetRGBFillColor(context, 0, 0.5f, 0.25f, 1);
     
     CGContextFillRect(context, indicatorBox);
-    
-    if (platformCALayer->acceleratesDrawing())
-        CGContextSetRGBFillColor(context, 1, 0, 0, 1);
-    else
-        CGContextSetRGBFillColor(context, 1, 1, 1, 1);
 
     if (platformCALayer->owner()->isUsingDisplayListDrawing(platformCALayer)) {
         CGContextSetRGBStrokeColor(context, 0, 0, 0, 0.65);
         CGContextSetLineWidth(context, 2);
         CGContextStrokeRect(context, indicatorBox);
     }
+    
+    if (!platformCALayer->isOpaque() && (platformCALayer->contentsFormat() & SmoothedFonts)) {
+        CGContextSetRGBFillColor(context, 1, 1, 1, 0.4);
+        platformCALayer->drawTextAtPoint(context, indicatorBox.origin.x + 7, indicatorBox.origin.y + 24, CGSizeMake(1, -1), 22, text, strlen(text));
+    }
+
+    if (platformCALayer->acceleratesDrawing())
+        CGContextSetRGBFillColor(context, 1, 0, 0, 1);
+    else
+        CGContextSetRGBFillColor(context, 1, 1, 1, 1);
     
     platformCALayer->drawTextAtPoint(context, indicatorBox.origin.x + 5, indicatorBox.origin.y + 22, CGSizeMake(1, -1), 22, text, strlen(text));
     

Source/WebCore/platform/graphics/ca/PlatformCALayer.h
@@ -139,6 +139,14 @@ public:
     virtual bool isOpaque() const = 0;
     virtual void setOpaque(bool) = 0;
 
+    enum ContentsFormatFlag {
+        DeepColor       = 1 << 0,
+        SmoothedFonts   = 1 << 1,
+    };
+    typedef unsigned ContentsFormatFlags;
+    virtual void setContentsFormat(ContentsFormatFlags) = 0;
+    virtual ContentsFormatFlags contentsFormat() const = 0;
+
     virtual FloatRect bounds() const = 0;
     virtual void setBounds(const FloatRect&) = 0;
 

Source/WebCore/platform/graphics/ca/TileController.cpp
@@ -175,6 +175,15 @@ void TileController::setTilesOpaque(bool opaque)
     tileGrid().updateTileLayerProperties();
 }
 
+void TileController::setTileContentsFormatFlags(PlatformCALayer::ContentsFormatFlags flags)
+{
+    if (flags == m_contentsFormatFlags)
+        return;
+
+    m_contentsFormatFlags = flags;
+    tileGrid().updateTileLayerProperties();
+}
+
 void TileController::setVisibleRect(const FloatRect& rect)
 {
     if (rect == m_visibleRect)
@@ -675,6 +684,7 @@ RefPtr<PlatformCALayer> TileController::createTileLayer(const IntRect& tileRect,
     layer->setBorderWidth(m_tileDebugBorderWidth);
     layer->setEdgeAntialiasingMask(0);
     layer->setOpaque(m_tilesAreOpaque);
+    layer->setContentsFormat(m_contentsFormatFlags);
 #ifndef NDEBUG
     layer->setName("Tile");
 #endif

commit 421146556981acf3ed883bb42499c72928bf87d1

Regression(r197981): Huge regression on Mac PLT https://bugs.webkit.org/show_bug.cgi?id=155443 <rdar://problem/25113391>
Reviewed by Gavin Barraclough.

We have experience a huge regression on Mac PLT after r197981, so
disable the feature until the performance issue is resolved.

* platform/graphics/ca/GraphicsLayerCA.cpp:
(WebCore::GraphicsLayer::supportsSmoothedLayerText):

git-svn-id: http://svn.webkit.org/repository/webkit/trunk@198145 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Source/WebCore/platform/graphics/ca/GraphicsLayerCA.cpp
@@ -314,7 +314,9 @@ static bool isSmoothedLayerTextEnabled = true;
 bool GraphicsLayer::supportsSmoothedLayerText()
 {
 #if PLATFORM(MAC) && __MAC_OS_X_VERSION_MIN_REQUIRED >= 101200
-    return isSmoothedLayerTextEnabled;
+    // FIXME: This is currently a major regression on PLT (rdar://problem/25113391).
+    // return isSmoothedLayerTextEnabled;
+    return false;
 #endif
     return false;
 }