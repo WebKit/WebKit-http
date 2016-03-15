ad926db0b430103c6200c9bcceae9c0ed61ee433
Zan Dobersek <zdobersek@igalia.com>
2016-03-15 03:52:23 -0700

    WPE-mesa: _wpe_loader_interface should handle the _wpe_pasteboard_interface object.

--- Source/ThirdParty/WPE-mesa/Source/wpe/mesa.cpp
+++ Source/ThirdParty/WPE-mesa/Source/wpe/mesa.cpp
@@ -1,5 +1,6 @@
 #include <wpe/loader.h>
 
+#include "pasteboard-wayland.h"
 #include "renderer-gbm.h"
 #include "view-backend-drm.h"
 #include "view-backend-wayland.h"
@@ -9,10 +10,12 @@
 
 extern "C" {
 
+static bool under_wayland = !!std::getenv("WAYLAND_DISPLAY");
+
 struct wpe_loader_interface _wpe_loader_interface = {
     [](const char* object_name) -> void* {
         if (!std::strcmp(object_name, "_wpe_view_backend_interface")) {
-            if (std::getenv("WAYLAND_DISPLAY"))
+            if (under_wayland)
                 return reinterpret_cast<void*>(&wayland_view_backend_interface);
             return reinterpret_cast<void*>(&drm_view_backend_interface);
         }
@@ -23,6 +26,9 @@ struct wpe_loader_interface _wpe_loader_interface = {
             return &gbm_renderer_backend_egl_target_interface;
         if (!std::strcmp(object_name, "_wpe_renderer_backend_egl_offscreen_target_interface"))
             return &gbm_renderer_backend_egl_offscreen_target_interface;
+
+        if (!std::strcmp(object_name, "_wpe_pasteboard_interface") && under_wayland)
+            return &wayland_pasteboard_interface;
 
         return nullptr;
     },
