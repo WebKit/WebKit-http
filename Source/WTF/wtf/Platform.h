WebKit/WebKit-http

commit c13f255ede7787893b1dcf2089596608a6bfad22

Use NSURLSession for loading in WebKit2 https://bugs.webkit.org/show_bug.cgi?id=154993
Reviewed by Sam Weinig.

Source/WebKit2:

* config.h:

Source/WTF:

* wtf/Platform.h:



git-svn-id: http://svn.webkit.org/repository/webkit/trunk@197572 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Source/WTF/wtf/Platform.h
@@ -553,9 +553,6 @@
 
 #define HAVE_NETWORK_EXTENSION 1
 #define HAVE_READLINE 1
-#if USE(APPLE_INTERNAL_SDK)
-#define USE_CFNETWORK 1
-#endif
 #define USE_UIKIT_EDITING 1
 #define USE_WEB_THREAD 1
 

Source/WebKit2/config.h
@@ -73,8 +73,11 @@
 #ifndef HAVE_WINDOW_SERVER_OCCLUSION_NOTIFICATIONS
 #define HAVE_WINDOW_SERVER_OCCLUSION_NOTIFICATIONS 1
 #endif
+#endif
+
+#if (PLATFORM(MAC) && __MAC_OS_X_VERSION_MIN_REQUIRED >= 101200) || (PLATFORM(IOS) && __IPHONE_OS_VERSION_MIN_REQUIRED >= 100000)
 #ifndef USE_NETWORK_SESSION
-#define USE_NETWORK_SESSION 0
+#define USE_NETWORK_SESSION 1
 #endif
 
 // FIXME: We should work towards not using CredentialStorage in WebKit2 to not have problems with digest authentication.

commit 08af29d7e7564c3409300a3f5347aa5a6112249c

Fix iOS9 performance regression after r197572 https://bugs.webkit.org/show_bug.cgi?id=155845 <rdar://problem/25144924>
Patch by Alex Christensen <achristensen@webkit.org> on 2016-03-24
Reviewed by Chris Dumez.

* wtf/Platform.h:
Use CFURLConnection instead of NSURLConnection on internal iOS9 builds.

git-svn-id: http://svn.webkit.org/repository/webkit/trunk@198641 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Source/WTF/wtf/Platform.h
@@ -551,6 +551,10 @@
 
 #if PLATFORM(IOS)
 
+#if USE(APPLE_INTERNAL_SDK) && __IPHONE_OS_VERSION_MIN_REQUIRED < 100000
+#define USE_CFNETWORK 1
+#endif
+
 #define HAVE_NETWORK_EXTENSION 1
 #define HAVE_READLINE 1
 #define USE_UIKIT_EDITING 1