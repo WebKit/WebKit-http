66a80589f17d3571c3fd198ad23d81531ac3fb7f
Bram Oosterhuis <bram.oosterhuis.nl@gmail.com>
2016-04-06 12:36:56 +0200

    Simplify virtualkeyboard module

--- Source/cmake/FindLibVirtualKeyboard.cmake
+++ Source/cmake/FindLibVirtualKeyboard.cmake
@@ -1,50 +1,19 @@
 # - Try to find libvirtualkeyboard
 # Once done this will define
-#  LIBVIRTUAL_KEYBOARD_FOUND - System has LibGeneric
-#  LIBVIRTUAL_KEYBOARD_INCLUDE_DIRS - The LibGeneric include directories
-#  LIBVIRTUAL_KEYBOARD_LIBRARIES - The libraries needed to use LibGeneric
+#  LIBVIRTUAL_KEYBOARD_FOUND - System has libvirtualkeyboard
+#  LIBVIRTUAL_KEYBOARD_INCLUDE_DIRS - The libvirtualkeyboard include directories
+#  LIBVIRTUAL_KEYBOARD_LIBRARIES - The libraries needed to use libvirtualkeyboard
 #
-# Copyright (C) 2015 Metrological.
+# Copyright (C) 2016 Metrological.
 #
-# Redistribution and use in source and binary forms, with or without
-# modification, are permitted provided that the following conditions
-# are met:
-# 1.  Redistributions of source code must retain the above copyright
-#     notice, this list of conditions and the following disclaimer.
-# 2.  Redistributions in binary form must reproduce the above copyright
-#     notice, this list of conditions and the following disclaimer in the
-#     documentation and/or other materials provided with the distribution.
-#
-# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDER AND ITS CONTRIBUTORS ``AS
-# IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
-# THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
-# PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR ITS
-# CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
-# EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
-# PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
-# OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
-# WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
-# OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
-# ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-
 find_package(PkgConfig)
-pkg_check_modules(PC_VIRTUAL_KEYBOARD QUIET virtualkeyboard)
-
-find_path(LIBVIRTUAL_KEYBOARD_INCLUDE_DIR gluelogic/virtualkeyboard/VirtualKeyboard.h
-          HINTS ${PC_VIRTUAL_KEYBOARD_INCLUDEDIR} ${PC_VIRTUAL_KEYBOARD_INCLUDE_DIRS}
-          PATH_SUFFIXES gluelogic)
-
-find_library(LIBVIRTUAL_KEYBOARD_LIBRARY NAMES virtualkeyboard 
-             HINTS ${PC_VIRTUAL_KEYBOARD_LIBDIR} ${PC_VIRTUAL_KEYBOARD_LIBRARY_DIRS})
-
-set(LIBVIRTUAL_KEYBOARD_LIBRARIES ${LIBVIRTUAL_KEYBOARD_LIBRARY})
-set(LIBVIRTUAL_KEYBOARD_INCLUDE_DIRS ${LIBVIRTUAL_KEYBOARD_INCLUDE_DIR})
-set(LIBVIRTUAL_KEYBOARD_DEFINES ${PC_VIRTUAL_KEYBOARD_CFLAGS})
-
-add_definitions ( ${LIBVIRTUAL_KEYBOARD_DEFINES} )
+pkg_check_modules(VIRTUALKEYBOARD REQUIRED virtualkeyboard)
 
 include(FindPackageHandleStandardArgs)
+FIND_PACKAGE_HANDLE_STANDARD_ARGS(VIRTUALKEYBOARD DEFAULT_MSG
+        VIRTUALKEYBOARD_FOUND
+        VIRTUALKEYBOARD_INCLUDE_DIRS
+        VIRTUALKEYBOARD_LIBRARIES
+        )
 
-find_package_handle_standard_args(LIBVIRTUAL_KEYBOARD DEFAULT_MSG LIBVIRTUAL_KEYBOARD_LIBRARIES LIBVIRTUAL_KEYBOARD_INCLUDE_DIRS)
-
-mark_as_advanced(LIBVIRTUAL_KEYBOARD_LIBRARIES LIBVIRTUAL_KEYBOARD_INCLUDE_DIRS )
+mark_as_advanced(VIRTUALKEYBOARD_INCLUDE_DIRS VIRTUALKEYBOARD_LIBRARIES)
