89fc73edb1a31464a5c96dcc0c3bfe821d284ab6
Zan Dobersek <zdobersek@igalia.com>
2016-05-03 00:23:54 -0700

    WebProcessMainWPE: use WTF::sleep().

--- Source/WebKit2/WebProcess/wpe/WebProcessMainWPE.cpp
+++ Source/WebKit2/WebProcess/wpe/WebProcessMainWPE.cpp
@@ -46,7 +46,7 @@ public:
         // TODO: Wrap with #ifndef NDEBUG
         if (g_getenv("WEBKIT2_PAUSE_WEB_PROCESS_ON_LAUNCH")) {
             g_printerr("WebProcess PID: %d\n", getpid());
-            sleep(30u);
+            WTF::sleep(30);
         }
 
         // Despite using system CAs to validate certificates we're
