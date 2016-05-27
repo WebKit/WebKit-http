233f29f7926e10bfc4d60364508110bf2c0f8f56
Zan Dobersek <zdobersek@igalia.com>
2016-05-27 02:16:13 -0700

    WPE-mesa: enhance view-backend-exportable.

--- Source/ThirdParty/WPE-mesa/include/wpe-mesa/view-backend-exportable.h
+++ Source/ThirdParty/WPE-mesa/include/wpe-mesa/view-backend-exportable.h
@@ -7,8 +7,26 @@ extern "C" {
 
 #include <wpe/view-backend.h>
 
+struct wpe_mesa_view_backend_exportable;
+
+struct wpe_mesa_view_backend_exportable_client {
+    void (*export_dma_buf)(void*, int32_t, uint32_t, uint32_t, uint32_t, uint32_t, uint32_t);
+};
+
+struct wpe_mesa_view_backend_exportable*
+wpe_mesa_view_backend_exportable_create(struct wpe_mesa_view_backend_exportable_client*, void*);
+
+void
+wpe_mesa_view_backend_exportable_destroy(struct wpe_mesa_view_backend_exportable*);
+
 struct wpe_view_backend*
-wpe_mesa_view_backend_exportable_create();
+wpe_mesa_view_backend_exportable_get_view_backend(struct wpe_mesa_view_backend_exportable*);
+
+void
+wpe_mesa_view_backend_exportable_dispatch_frame_complete(struct wpe_mesa_view_backend_exportable*);
+
+void
+wpe_mesa_view_backend_exportable_dispatch_release_buffer(struct wpe_mesa_view_backend_exportable*, uint32_t);
 
 #ifdef __cplusplus
 }
--- Source/ThirdParty/WPE-mesa/src/exportable/view-backend-exportable.cpp
+++ Source/ThirdParty/WPE-mesa/src/exportable/view-backend-exportable.cpp
@@ -5,9 +5,18 @@
 
 namespace Exportable {
 
+class ViewBackend;
+
+class ClientBundle {
+public:
+    struct wpe_mesa_view_backend_exportable_client* client;
+    void* data;
+    ViewBackend* viewBackend;
+};
+
 class ViewBackend : public GBM::Host::Client {
 public:
-    ViewBackend(struct wpe_view_backend* backend);
+    ViewBackend(ClientBundle*, struct wpe_view_backend* backend);
     virtual ~ViewBackend();
 
     void initialize();
@@ -19,6 +28,7 @@ private:
     void importBufferFd(int) override;
     void commitBuffer(struct buffer_commit*) override;
 
+    ClientBundle* m_clientBundle;
     struct wpe_view_backend* m_backend;
 
     struct {
@@ -27,9 +37,11 @@ private:
     } m_renderer;
 };
 
-ViewBackend::ViewBackend(struct wpe_view_backend* backend)
-    : m_backend(backend)
+ViewBackend::ViewBackend(ClientBundle* clientBundle, struct wpe_view_backend* backend)
+    : m_clientBundle(clientBundle)
+    , m_backend(backend)
 {
+    m_clientBundle->viewBackend = this;
     m_renderer.gbmHost.initialize(*this);
 }
 
@@ -46,6 +58,7 @@ ViewBackend::~ViewBackend()
 
 void ViewBackend::initialize()
 {
+    wpe_view_backend_dispatch_set_size(m_backend, 800, 600);
 }
 
 void ViewBackend::importBufferFd(int fd)
@@ -55,53 +68,93 @@ void ViewBackend::importBufferFd(int fd)
     m_renderer.pendingBufferFd = fd;
 }
 
-void ViewBackend::commitBuffer(struct buffer_commit*)
+void ViewBackend::commitBuffer(struct buffer_commit* commit)
 {
+    m_clientBundle->client->export_dma_buf(m_clientBundle->data, m_renderer.pendingBufferFd,
+        commit->handle, commit->width, commit->height, commit->stride, commit->format);
+
+    m_renderer.pendingBufferFd = -1;
 }
 
 } // namespace Exportable
 
 extern "C" {
 
+struct wpe_mesa_view_backend_exportable {
+    Exportable::ClientBundle* clientBundle;
+    struct wpe_view_backend* backend;
+};
+
 struct wpe_view_backend_interface exportable_view_backend_interface = {
     // create
-    [](void*, struct wpe_view_backend* backend) -> void*
+    [](void* data, struct wpe_view_backend* backend) -> void*
     {
-        fprintf(stderr, "exportable_view_backend_interface::create()\n");
-
-        return new Exportable::ViewBackend(backend);
+        auto* clientBundle = static_cast<Exportable::ClientBundle*>(data);
+        return new Exportable::ViewBackend(clientBundle, backend);
     },
     // destroy
     [](void* data)
     {
-        fprintf(stderr, "exportable_view_backend_interface::destroy()\n");
-
         auto* backend = static_cast<Exportable::ViewBackend*>(data);
         delete backend;
     },
     // initialize
     [](void* data)
     {
-        fprintf(stderr, "exportable_view_backend_interface::initialize()\n");
-
         auto& backend = *static_cast<Exportable::ViewBackend*>(data);
         backend.initialize();
     },
     // get_renderer_host_fd
     [](void* data) -> int
     {
-        fprintf(stderr, "exportable_view_backend_interface::get_renderer_host_fd()\n");
-
         auto& backend = *static_cast<Exportable::ViewBackend*>(data);
         return backend.gbmHost().releaseClientFD();
     },
 };
 
 __attribute__((visibility("default")))
-struct wpe_view_backend*
-wpe_mesa_view_backend_exportable_create()
+struct wpe_mesa_view_backend_exportable*
+wpe_mesa_view_backend_exportable_create(struct wpe_mesa_view_backend_exportable_client* client, void* client_data)
 {
-    return wpe_view_backend_create_with_backend_interface(&exportable_view_backend_interface, 0);
+    auto* clientBundle = new Exportable::ClientBundle{ client, client_data, nullptr };
+    struct wpe_view_backend* backend = wpe_view_backend_create_with_backend_interface(&exportable_view_backend_interface, clientBundle);
+
+    auto* exportable = new struct wpe_mesa_view_backend_exportable;
+    exportable->clientBundle = clientBundle;
+    exportable->backend = backend;
+
+    return exportable;
 }
+
+__attribute__((visibility("default")))
+void
+wpe_mesa_view_backend_exportable_destroy(struct wpe_mesa_view_backend_exportable* exportable)
+{
+    wpe_view_backend_destroy(exportable->backend);
+    delete exportable->clientBundle;
+    delete exportable;
+}
+
+__attribute__((visibility("default")))
+struct wpe_view_backend*
+wpe_mesa_view_backend_exportable_get_view_backend(struct wpe_mesa_view_backend_exportable* exportable)
+{
+    return exportable->backend;
+}
+
+__attribute__((visibility("default")))
+void
+wpe_mesa_view_backend_exportable_dispatch_frame_complete(struct wpe_mesa_view_backend_exportable* exportable)
+{
+    exportable->clientBundle->viewBackend->gbmHost().frameComplete();
+}
+
+__attribute__((visibility("default")))
+void
+wpe_mesa_view_backend_exportable_dispatch_release_buffer(struct wpe_mesa_view_backend_exportable* exportable, uint32_t handle)
+{
+    exportable->clientBundle->viewBackend->gbmHost().releaseBuffer(handle);
+}
+
 
 }
