WebKit/WebKit-http

commit 2e5139b48b59b428afd03cbaf6b143c001057d05

Refactor RenderMathMLOperator and RenderMathMLToken to avoid using anonymous renderers. https://bugs.webkit.org/show_bug.cgi?id=155018
Patch by Frederic Wang <fwang@igalia.com> on 2016-06-24
Reviewed by Martin Robinson.

Source/WebCore:

No new tests, already covered by existing tests.

We use MathOperator for RenderMathMLOperator to avoid creating anonymous text nodes again
and again. We reimplement implicit mathvariant="italic" on single-char mi in a way that does
not rely on creating anonymous text nodes. Finally, we improve the determination/update of
when mathvariant is italic to avoid breaking foreign-mi-dynamic test.
The change in the render tree structure breaks mfenced accessibility support but that will
be fixed in follow-up patches. The simplifications made here will also allow to simplify the
accessibility code.

* css/mathml.css:
(mo): Deleted. This flexbox rule is no longer needed.
* rendering/mathml/RenderMathMLBlock.cpp:
(WebCore::RenderMathMLBlock::createAnonymousMathMLBlock): Deleted. We no longer need to
create anonymous renderer with this function.
* rendering/mathml/RenderMathMLBlock.h: Delete createAnonymousMathMLBlock.
* rendering/mathml/RenderMathMLOperator.cpp: Implement layout functions without relying on
flexbox or anonymous.
(WebCore::RenderMathMLOperator::computePreferredLogicalWidths): Handle the case of !useMathOperator()
for which we need to add extra operator spacing after the RenderMathMLToken layout.
(WebCore::RenderMathMLOperator::layoutBlock): Ditto.
(WebCore::RenderMathMLOperator::isChildAllowed): Deleted. We allow the non-anonymous text.
(WebCore::RenderMathMLOperator::rebuildTokenContent): No longer destroy and rebuild
anonymous wrapper. Remove updateStyle call.
(WebCore::RenderMathMLOperator::updateStyle): Deleted. We no longer need anonymous style for the spacing.
* rendering/mathml/RenderMathMLOperator.h: Remove updateStyle() and isChildAllowed().

Source/WebCore/rendering/mathml/RenderMathMLOperator.h
@@ -55,7 +55,6 @@ public:
     LayoutUnit italicCorrection() const { return m_mathOperator.italicCorrection(); }
 
     void styleDidChange(StyleDifference, const RenderStyle* oldStyle) final;
-    void updateStyle() final;
 
     void paint(PaintInfo&, const LayoutPoint&) final;
 
@@ -78,7 +77,6 @@ private:
     bool isRenderMathMLOperator() const final { return true; }
     // The following operators are invisible: U+2061 FUNCTION APPLICATION, U+2062 INVISIBLE TIMES, U+2063 INVISIBLE SEPARATOR, U+2064 INVISIBLE PLUS.
     bool isInvisibleOperator() const { return 0x2061 <= m_textContent && m_textContent <= 0x2064; }
-    bool isChildAllowed(const RenderObject&, const RenderStyle&) const final;
 
     Optional<int> firstLineBaseline() const final;
     RenderMathMLOperator* unembellishedOperator() final { return this; }

LayoutTests/accessibility/mac/mathml-elements-expected.txt
@@ -67,13 +67,6 @@ PASS fenced.role is 'AXRole: AXGroup'
 PASS fenced.subrole is 'AXSubrole: AXMathFenced'
 PASS fenced.stringAttributeValue('AXMathFencedOpen') is '{'
 PASS fenced.stringAttributeValue('AXMathFencedClose') is '}'
-PASS child.stringValue is 'AXValue: {'
-PASS child.childAtIndex(0).stringValue is 'AXValue: 2'
-PASS child.stringValue is 'AXValue: ,'
-PASS child.childAtIndex(0).stringValue is 'AXValue: a'
-PASS child.stringValue is 'AXValue: ,'
-PASS child.childAtIndex(0).stringValue is 'AXValue: e'
-PASS child.stringValue is 'AXValue: }'
 PASS sub.role is 'AXRole: AXGroup'
 PASS sub.subrole is 'AXSubrole: AXMathSubscriptSuperscript'
 PASS subBase.subrole is 'AXSubrole: AXMathIdentifier'

LayoutTests/accessibility/mac/mathml-elements.html
@@ -108,6 +108,9 @@ if (window.testRunner && window.accessibilityController) {
    shouldBe("fenced.subrole", "'AXSubrole: AXMathFenced'");
    shouldBe("fenced.stringAttributeValue('AXMathFencedOpen')", "'{'");
    shouldBe("fenced.stringAttributeValue('AXMathFencedClose')", "'}'");
+   /*
+   FIXME: AccessibilityRenderObject should be updated to properly expose the text of mfenced operators.
+   See https://bugs.webkit.org/show_bug.cgi?id=139582.
    var fenceValues = new Array("{", "2", ",", "a", ",", "e", "}");
    for (var k = 0; k < fenceValues.length; k++) {
       var child = fenced.childAtIndex(k);
@@ -116,6 +119,7 @@ if (window.testRunner && window.accessibilityController) {
       else
           shouldBe("child.childAtIndex(0).stringValue", "'AXValue: " + fenceValues[k] + "'");
    }
+   */
 
    // Subscript
    var sub = accessibilityController.accessibleElementById("sub").childAtIndex(0);

LayoutTests/accessibility/ios-simulator/math-expected.txt
@@ -1,15 +1,20 @@
 Raman scattering study of the 
 Fe
 1
+−
 x
 Co
 x
 Sb
 2
  and 
+(
 0
+≤
 x
+≤
 1
+)
  single crystals.
 This test the math role type will be an accessibility element.
 

LayoutTests/accessibility/mac/math-alttext-expected.txt
@@ -1,5 +1,6 @@
 Fe
 1
+-
 x
 Co
 x

LayoutTests/accessibility/math-multiscript-attributes-expected.txt
@@ -14,67 +14,7 @@ AXRoleDescription: group
 AXChildren: 
 AXHelp: 
 AXParent: 
-AXSize: NSSize: {12, 25}
-AXTitle: 
-AXDescription: 
-AXValue: 
-AXFocused: 0
-AXEnabled: 1
-AXWindow: 
-AXSelectedTextMarkerRange: (null)
-AXStartTextMarker: 
-AXEndTextMarker: 
-AXVisited: 0
-AXLinkedUIElements: 
-AXSelected: 0
-AXBlockQuoteLevel: 0
-AXTopLevelUIElement: 
-AXLanguage: 
-AXDOMIdentifier: 
-AXDOMClassList: 
-AXFocusableAncestor: 
-AXEditableAncestor: (null)
-AXHighestEditableAncestor: (null)
-AXElementBusy: 0
-
-------------
-AXRole: AXGroup
-AXSubrole: AXMathIdentifier
-AXRoleDescription: group
-AXChildren: 
-AXHelp: 
-AXParent: 
-AXSize: NSSize: {11, 19}
-AXTitle: 
-AXDescription: 
-AXValue: 
-AXFocused: 0
-AXEnabled: 1
-AXWindow: 
-AXSelectedTextMarkerRange: (null)
-AXStartTextMarker: 
-AXEndTextMarker: 
-AXVisited: 0
-AXLinkedUIElements: 
-AXSelected: 0
-AXBlockQuoteLevel: 0
-AXTopLevelUIElement: 
-AXLanguage: 
-AXDOMIdentifier: 
-AXDOMClassList: 
-AXFocusableAncestor: 
-AXEditableAncestor: (null)
-AXHighestEditableAncestor: (null)
-AXElementBusy: 0
-
-------------
-AXRole: AXGroup
-AXSubrole: AXMathIdentifier
-AXRoleDescription: group
-AXChildren: 
-AXHelp: 
-AXParent: 
-AXSize: NSSize: {10, 18}
+AXSize: NSSize: {14, 25}
 AXTitle: 
 AXDescription: 
 AXValue: 
@@ -105,6 +45,66 @@ AXChildren:
 AXHelp: 
 AXParent: 
 AXSize: NSSize: {10, 19}
+AXTitle: 
+AXDescription: 
+AXValue: 
+AXFocused: 0
+AXEnabled: 1
+AXWindow: 
+AXSelectedTextMarkerRange: (null)
+AXStartTextMarker: 
+AXEndTextMarker: 
+AXVisited: 0
+AXLinkedUIElements: 
+AXSelected: 0
+AXBlockQuoteLevel: 0
+AXTopLevelUIElement: 
+AXLanguage: 
+AXDOMIdentifier: 
+AXDOMClassList: 
+AXFocusableAncestor: 
+AXEditableAncestor: (null)
+AXHighestEditableAncestor: (null)
+AXElementBusy: 0
+
+------------
+AXRole: AXGroup
+AXSubrole: AXMathIdentifier
+AXRoleDescription: group
+AXChildren: 
+AXHelp: 
+AXParent: 
+AXSize: NSSize: {9, 18}
+AXTitle: 
+AXDescription: 
+AXValue: 
+AXFocused: 0
+AXEnabled: 1
+AXWindow: 
+AXSelectedTextMarkerRange: (null)
+AXStartTextMarker: 
+AXEndTextMarker: 
+AXVisited: 0
+AXLinkedUIElements: 
+AXSelected: 0
+AXBlockQuoteLevel: 0
+AXTopLevelUIElement: 
+AXLanguage: 
+AXDOMIdentifier: 
+AXDOMClassList: 
+AXFocusableAncestor: 
+AXEditableAncestor: (null)
+AXHighestEditableAncestor: (null)
+AXElementBusy: 0
+
+------------
+AXRole: AXGroup
+AXSubrole: AXMathIdentifier
+AXRoleDescription: group
+AXChildren: 
+AXHelp: 
+AXParent: 
+AXSize: NSSize: {9, 19}
 AXTitle: 
 AXDescription: 
 AXValue: 

LayoutTests/fast/css/readonly-pseudoclass-common-element-expected.txt
@@ -24,5 +24,6 @@ TEST COMPLETE
        
 x
 2
+=
 0
 

LayoutTests/fast/css/readwrite-pseudoclass-editable-expected.txt
@@ -23,5 +23,6 @@ TEST COMPLETE
   
 x
 2
+=
 0
 

commit 01df32f4dc6745e2605e6009cb2d4b9f1400be8a

AX: Anonymous RenderMathMLOperators are not exposed to the accessibility tree https://bugs.webkit.org/show_bug.cgi?id=139582 <rdar://problem/26938849>
Reviewed by Chris Fleizach.

This is based on a patch by Frederic Wang <fwang@igalia.com>.

Source/WebCore:

WebCore assigns the generic MathElementRole AccessibilityRole to elements
which are expected to be included in the accessibility tree. This assignment
is based on the AccessibilityRenderObject's node being a MathMLElement. The
anonymous RenderMathMLOperators fail that test.

From the perspective of accessibility support, these operators function
like MathMLElements. Furthermore, both WebCore and the platforms rely
upon MathElementRole to identify accessible MathML objects. The simplest
fix is to have AccessibilityRenderObject::isMathElement() treat anonymous
MathML operators as if they were MathMLElements.

Now that these operators are being exposed, we need to handle them in
AccessibilityRenderObject::textUnderElement() which assumes that anonymous
objects either have nodes or have children with nodes. And crashes when
that fails to be the case. Making RenderMathMLOperator::textContent()
public and then using it to get the text under anonymous operators solves
this problem. We also assign StaticTextRole to these operators on the Mac
because the default platform mapping of MathElementRole is GroupRole, which
made sense when we had a child RenderText object holding the operator.

Lastly, AccessibilityRenderObject::isIgnoredElementWithinMathTree() no
longer needs to special-case anonymous operators because they now have
MathElementRole.

Tests: accessibility/math-fenced.html
       accessibility/math-foreign-content.html

* accessibility/AccessibilityObject.h:
(WebCore::AccessibilityObject::isAnonymousMathOperator):
* accessibility/AccessibilityRenderObject.cpp:
(WebCore::AccessibilityRenderObject::textUnderElement):
(WebCore::AccessibilityRenderObject::stringValue):
(WebCore::AccessibilityRenderObject::isMathElement):
(WebCore::AccessibilityRenderObject::isAnonymousMathOperator):
(WebCore::AccessibilityRenderObject::isIgnoredElementWithinMathTree):
* accessibility/AccessibilityRenderObject.h:
* accessibility/mac/WebAccessibilityObjectWrapperMac.mm:
(-[WebAccessibilityObjectWrapper role]):
* rendering/mathml/RenderMathMLMath.h:
* rendering/mathml/RenderMathMLOperator.h:
(WebCore::RenderMathMLOperator::textContent):

LayoutTests:

One of the things making it difficult to tell that the operators were not
exposed on the Mac is the fact that the operator text is present. But the
operator text is simply a StaticTextRole object corresponding to the
RenderText descendant of the operator. Furthermore, on the Mac, accessible
math operators have subroles which are missing from the StaticTextRole
object which is exposed. In order to make issues like this more obvious,
add an option to include the subrole to dumpAccessibilityTree() and use it
in the new math-fenced.html.

This change also fixes the mfenced in mac/mathml-elements.html that was
broken after r202420. We enable it again and update the expectations of

Source/WebCore/accessibility/AccessibilityObject.h
@@ -995,6 +995,7 @@ public:
     virtual String mathFencedOpenString() const { return String(); }
     virtual String mathFencedCloseString() const { return String(); }
     virtual int mathLineThickness() const { return 0; }
+    virtual bool isAnonymousMathOperator() const { return false; }
     
     // Multiscripts components.
     typedef Vector<std::pair<AccessibilityObject*, AccessibilityObject*>> AccessibilityMathMultiscriptPairs;

Source/WebCore/accessibility/AccessibilityRenderObject.cpp
@@ -84,6 +84,7 @@
 #include "RenderListMarker.h"
 #include "RenderMathMLBlock.h"
 #include "RenderMathMLFraction.h"
+#include "RenderMathMLMath.h"
 #include "RenderMathMLOperator.h"
 #include "RenderMathMLRoot.h"
 #include "RenderMenuList.h"
@@ -635,6 +636,10 @@ String AccessibilityRenderObject::textUnderElement(AccessibilityTextUnderElement
     // so rangeOfContents does not work for them (nor does regular text selection).
     if (isRenderText && m_renderer->isAnonymous() && ancestorsOfType<RenderMathMLOperator>(*m_renderer).first())
         return downcast<RenderText>(*m_renderer).text();
+    if (isAnonymousMathOperator()) {
+        UChar operatorChar = downcast<RenderMathMLOperator>(*m_renderer).textContent();
+        return operatorChar ? String(&operatorChar, 1) : String();
+    }
     if (is<RenderMathMLOperator>(*m_renderer) && !m_renderer->isAnonymous())
         return downcast<RenderMathMLOperator>(*m_renderer).element().textContent();
 #endif
@@ -760,6 +765,9 @@ String AccessibilityRenderObject::stringValue() const
     }
         
     if (is<RenderText>(*m_renderer))
+        return textUnderElement();
+
+    if (isAnonymousMathOperator())
         return textUnderElement();
     
     if (is<RenderMenuList>(cssBox)) {
@@ -3609,6 +3617,12 @@ bool AccessibilityRenderObject::isMathElement() const
     if (!m_renderer)
         return false;
     
+    // The mfenced element creates anonymous RenderMathMLOperators which should be treated
+    // as MathML elements and assigned the MathElementRole so that platform logic regarding
+    // inclusion and role mapping is not bypassed.
+    if (isAnonymousMathOperator())
+        return true;
+
     return is<MathMLElement>(node());
 }
 
@@ -3658,6 +3672,11 @@ bool AccessibilityRenderObject::isMathOperator() const
         return false;
 
     return true;
+}
+
+bool AccessibilityRenderObject::isAnonymousMathOperator() const
+{
+    return is<RenderMathMLOperator>(m_renderer) && m_renderer->isAnonymous();
 }
 
 bool AccessibilityRenderObject::isMathFenceOperator() const
@@ -3751,17 +3770,9 @@ bool AccessibilityRenderObject::isIgnoredElementWithinMathTree() const
 {
     if (!m_renderer)
         return true;
-    
-    // We ignore anonymous renderers inside math blocks.
-    // However, we do not exclude anonymous RenderMathMLOperator nodes created by the mfenced element nor RenderText nodes created by math operators so that the text can be exposed by AccessibilityRenderObject::textUnderElement.
-    if (m_renderer->isAnonymous()) {
-        if (m_renderer->isRenderMathMLOperator())
-            return false;
-        for (AccessibilityObject* parent = parentObject(); parent; parent = parent->parentObject()) {
-            if (parent->isMathElement())
-                return !(m_renderer->isText() && ancestorsOfType<RenderMathMLOperator>(*m_renderer).first());
-        }
-    }
+
+    if (is<RenderText>(*m_renderer))
+        return false;
 
     // Only math elements that we explicitly recognize should be included
     // We don't want things like <mstyle> to appear in the tree.
@@ -3774,7 +3785,7 @@ bool AccessibilityRenderObject::isIgnoredElementWithinMathTree() const
         return true;
     }
 
-    return false;
+    return m_renderer->isAnonymous() && m_renderer->parent() && is<RenderMathMLBlock>(m_renderer->parent());
 }
 
 AccessibilityObject* AccessibilityRenderObject::mathRadicandObject()

Source/WebCore/accessibility/AccessibilityRenderObject.h
@@ -327,6 +327,7 @@ private:
     String mathFencedOpenString() const override;
     String mathFencedCloseString() const override;
     int mathLineThickness() const override;
+    bool isAnonymousMathOperator() const override;
 
     // Multiscripts components.
     void mathPrescripts(AccessibilityMathMultiscriptPairs&) override;

Source/WebCore/accessibility/mac/WebAccessibilityObjectWrapperMac.mm
@@ -2237,6 +2237,12 @@ static NSString* roleValueToNSString(AccessibilityRole value)
     if (role == LabelRole && is<AccessibilityLabel>(*m_object) && downcast<AccessibilityLabel>(*m_object).containsOnlyStaticText())
         role = StaticTextRole;
 
+    // The mfenced element creates anonymous RenderMathMLOperators with no RenderText
+    // descendants. Because these anonymous renderers are the only accessible objects
+    // containing the operator, assign StaticTextRole.
+    if (m_object->isAnonymousMathOperator())
+        role = StaticTextRole;
+
     if (role == CanvasRole && m_object->canvasHasFallbackContent())
         role = GroupRole;
     NSString* string = roleValueToNSString(role);

Source/WebCore/rendering/mathml/RenderMathMLMath.h
@@ -43,5 +43,7 @@ private:
     
 }
 
+SPECIALIZE_TYPE_TRAITS_RENDER_OBJECT(RenderMathMLMath, isRenderMathMLMath())
+
 #endif // ENABLE(MATHML)
 #endif // RenderMathMLMath_h

Source/WebCore/rendering/mathml/RenderMathMLOperator.h
@@ -62,6 +62,7 @@ public:
     void updateTokenContent() final;
     void updateOperatorProperties();
     void updateFromElement() final;
+    UChar textContent() const { return m_textContent; }
 
 protected:
     virtual void setOperatorProperties();
@@ -69,7 +70,6 @@ protected:
     void layoutBlock(bool relayoutChildren, LayoutUnit pageLogicalHeight = 0) final;
     void setLeadingSpace(LayoutUnit leadingSpace) { m_leadingSpace = leadingSpace; }
     void setTrailingSpace(LayoutUnit trailingSpace) { m_trailingSpace = trailingSpace; }
-    UChar textContent() const { return m_textContent; }
 
 private:
     const char* renderName() const override { return isAnonymous() ? "RenderMathMLOperator (anonymous)" : "RenderMathMLOperator"; }

LayoutTests/accessibility/mac/mathml-elements-expected.txt
@@ -67,6 +67,13 @@ PASS fenced.role is 'AXRole: AXGroup'
 PASS fenced.subrole is 'AXSubrole: AXMathFenced'
 PASS fenced.stringAttributeValue('AXMathFencedOpen') is '{'
 PASS fenced.stringAttributeValue('AXMathFencedClose') is '}'
+PASS child.stringValue is 'AXValue: {'
+PASS child.childAtIndex(0).stringValue is 'AXValue: 2'
+PASS child.stringValue is 'AXValue: ,'
+PASS child.childAtIndex(0).stringValue is 'AXValue: a'
+PASS child.stringValue is 'AXValue: ,'
+PASS child.childAtIndex(0).stringValue is 'AXValue: e'
+PASS child.stringValue is 'AXValue: }'
 PASS sub.role is 'AXRole: AXGroup'
 PASS sub.subrole is 'AXSubrole: AXMathSubscriptSuperscript'
 PASS subBase.subrole is 'AXSubrole: AXMathIdentifier'

LayoutTests/accessibility/mac/mathml-elements.html
@@ -108,9 +108,6 @@ if (window.testRunner && window.accessibilityController) {
    shouldBe("fenced.subrole", "'AXSubrole: AXMathFenced'");
    shouldBe("fenced.stringAttributeValue('AXMathFencedOpen')", "'{'");
    shouldBe("fenced.stringAttributeValue('AXMathFencedClose')", "'}'");
-   /*
-   FIXME: AccessibilityRenderObject should be updated to properly expose the text of mfenced operators.
-   See https://bugs.webkit.org/show_bug.cgi?id=139582.
    var fenceValues = new Array("{", "2", ",", "a", ",", "e", "}");
    for (var k = 0; k < fenceValues.length; k++) {
       var child = fenced.childAtIndex(k);
@@ -119,7 +116,6 @@ if (window.testRunner && window.accessibilityController) {
       else
           shouldBe("child.childAtIndex(0).stringValue", "'AXValue: " + fenceValues[k] + "'");
    }
-   */
 
    // Subscript
    var sub = accessibilityController.accessibleElementById("sub").childAtIndex(0);

LayoutTests/accessibility/math-fenced.html
@@ -1,0 +1,30 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<script src="../resources/js-test-pre.js"></script>
+<script src="../resources/accessibility-helper.js"></script>
+</head>
+<body>
+<div id="content">
+  <math id="math">
+    <mfenced open="{" close="}" separators=",;">
+      <mi>2</mi>
+      <mi>a</mi>
+      <mi>e</mi>
+    </mfenced>
+  </math>
+</div>
+<p id="description"></p>
+<pre id="tree"></pre>
+<div id="console"></div>
+<script>
+    description("This verifies the expected roles and values are exposed for mfenced.");
+
+    if (window.accessibilityController) {
+        dumpAccessibilityTree(accessibilityController.accessibleElementById("math"), null, 0, false, false, true);
+        document.getElementById("content").style.visibility = "hidden";
+    }
+</script>
+<script src="../resources/js-test-post.js"></script>
+</body>
+</html>

LayoutTests/accessibility/math-foreign-content.html
@@ -1,0 +1,34 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<script src="../resources/js-test-pre.js"></script>
+<script src="../resources/accessibility-helper.js"></script>
+</head>
+<body>
+<div id="content">
+  <math id="math">
+    <semantics>
+      <csymbol>Content MathML</csymbol>
+      <annotation-xml encoding="image/svg+xml">
+        <svg height="30" width="75">
+          <text x="0" y="15" transform="rotate(30 20,40)">SVG</text>
+        </svg> 
+      </annotation-xml>
+    </semantics>
+    <mtext><span>HTML</span></mtext>
+  </math>
+</div>
+<p id="description"></p>
+<pre id="tree"></pre>
+<div id="console"></div>
+<script>
+    description("This verifies the expected roles and values are exposed for foreign content in MathML.");
+
+    if (window.accessibilityController) {
+        dumpAccessibilityTree(accessibilityController.accessibleElementById("math"), null, 0, false, false, true);
+        document.getElementById("content").style.visibility = "hidden";
+    }
+</script>
+<script src="../resources/js-test-post.js"></script>
+</body>
+</html>

LayoutTests/platform/gtk/accessibility/math-fenced-expected.txt
@@ -1,0 +1,18 @@
+This verifies the expected roles and values are exposed for mfenced.
+
+On success, you will see a series of "PASS" messages, followed by "TEST COMPLETE".
+
+
+AXRole: AXMath 
+    AXRole: AXGroup 
+        AXRole: AXStatic AXValue: {
+        AXRole: AXStatic AXValue: 2
+        AXRole: AXStatic AXValue: ,
+        AXRole: AXStatic AXValue: a
+        AXRole: AXStatic AXValue: ;
+        AXRole: AXStatic AXValue: e
+        AXRole: AXStatic AXValue: }
+PASS successfullyParsed is true
+
+TEST COMPLETE
+

LayoutTests/platform/gtk/accessibility/math-foreign-content-expected.txt
@@ -1,0 +1,14 @@
+This verifies the expected roles and values are exposed for foreign content in MathML.
+
+On success, you will see a series of "PASS" messages, followed by "TEST COMPLETE".
+
+
+AXRole: AXMath 
+    AXRole: AXGroup 
+        AXRole: AXGroup 
+            AXRole: AXSection AXValue: SVG
+    AXRole: AXStatic AXValue: HTML
+PASS successfullyParsed is true
+
+TEST COMPLETE
+

LayoutTests/platform/mac/accessibility/math-fenced-expected.txt
@@ -1,0 +1,21 @@
+This verifies the expected roles and values are exposed for mfenced.
+
+On success, you will see a series of "PASS" messages, followed by "TEST COMPLETE".
+
+
+AXRole: AXGroup AXSubrole: AXDocumentMath AXValue: 
+    AXRole: AXGroup AXSubrole: AXMathFenced AXValue: 
+        AXRole: AXStaticText AXSubrole: AXMathFenceOperator AXValue: {
+        AXRole: AXGroup AXSubrole: AXMathIdentifier AXValue: 
+            AXRole: AXStaticText AXSubrole:  AXValue: 2
+        AXRole: AXStaticText AXSubrole: AXMathSeparatorOperator AXValue: ,
+        AXRole: AXGroup AXSubrole: AXMathIdentifier AXValue: 
+            AXRole: AXStaticText AXSubrole:  AXValue: a
+        AXRole: AXStaticText AXSubrole: AXMathSeparatorOperator AXValue: ;
+        AXRole: AXGroup AXSubrole: AXMathIdentifier AXValue: 
+            AXRole: AXStaticText AXSubrole:  AXValue: e
+        AXRole: AXStaticText AXSubrole: AXMathFenceOperator AXValue: }
+PASS successfullyParsed is true
+
+TEST COMPLETE
+

LayoutTests/platform/mac/accessibility/math-foreign-content-expected.txt
@@ -1,0 +1,16 @@
+This verifies the expected roles and values are exposed for foreign content in MathML.
+
+On success, you will see a series of "PASS" messages, followed by "TEST COMPLETE".
+
+
+AXRole: AXGroup AXSubrole: AXDocumentMath AXValue: 
+    AXRole: AXGroup AXSubrole: AXMathRow AXValue: 
+        AXRole: AXGroup AXSubrole: AXMathRow AXValue: 
+            AXRole: AXGroup AXSubrole:  AXValue: 
+                AXRole: AXStaticText AXSubrole:  AXValue: SVG
+    AXRole: AXGroup AXSubrole: AXMathText AXValue: 
+        AXRole: AXStaticText AXSubrole:  AXValue: HTML
+PASS successfullyParsed is true
+
+TEST COMPLETE
+

LayoutTests/resources/accessibility-helper.js
@@ -1,10 +1,12 @@
-function dumpAccessibilityTree(accessibilityObject, stopElement, indent, allAttributesIfNeeded, getValueFromTitle) {
+function dumpAccessibilityTree(accessibilityObject, stopElement, indent, allAttributesIfNeeded, getValueFromTitle, includeSubrole) {
     var str = "";
     var i = 0;
 
     for (i = 0; i < indent; i++)
         str += "    ";
     str += accessibilityObject.role;
+    if (includeSubrole === true && accessibilityObject.subrole)
+        str += " " + accessibilityObject.subrole;
     str += " " + (getValueFromTitle === true ? accessibilityObject.title : accessibilityObject.stringValue);
     str += allAttributesIfNeeded && accessibilityObject.role == '' ? accessibilityObject.allAttributes() : '';
     str += "\n";
@@ -16,7 +18,7 @@ function dumpAccessibilityTree(accessibilityObject, stopElement, indent, allAttr
 
     var count = accessibilityObject.childrenCount;
     for (i = 0; i < count; ++i) {
-        if (!dumpAccessibilityTree(accessibilityObject.childAtIndex(i), stopElement, indent + 1, allAttributesIfNeeded, getValueFromTitle))
+        if (!dumpAccessibilityTree(accessibilityObject.childAtIndex(i), stopElement, indent + 1, allAttributesIfNeeded, getValueFromTitle, includeSubrole))
             return false;
     }
 