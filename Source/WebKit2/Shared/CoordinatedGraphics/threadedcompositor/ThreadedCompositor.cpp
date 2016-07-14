WebKit/WebKit-http

commit 7a8cb961d4edf5ab7d0a766c158d9d8d92a1bee1

[ThreadedCompositor] Opening the inspector in a window causes a crash. https://bugs.webkit.org/show_bug.cgi?id=154444
Reviewed by Žan Doberšek.

The threaded compositor doesn't handle the case of changing or removing the native surface handle. When the web
view is reparented, the current native surface handle is destroyed when the view is removed from the parent, and
a new one is created when added to the new parent. We need to handle this case in the threaded compositor.

* Shared/CoordinatedGraphics/threadedcompositor/CompositingRunLoop.cpp:
(WebKit::CompositingRunLoop::stopUpdateTimer): Allow users to stop the update timer.
* Shared/CoordinatedGraphics/threadedcompositor/CompositingRunLoop.h:
* Shared/CoordinatedGraphics/threadedcompositor/ThreadedCompositor.cpp:
(WebKit::ThreadedCompositor::setNativeSurfaceHandleForCompositing): Use performTaskSync because this is called
from a synchronous IPC message and right after it returns, the current native surface is destroyed by the UI
process. So we need to ensure we finish all pending operations for the current native surface in the compositing
thread before it's destroyed. Then we enable or disable the scene depending on whether the native surface has
been created or destroyed and destroy the current context in case the new handle is 0.
(WebKit::ThreadedCompositor::tryEnsureGLContext): Just renamed to make it clear that it can fail.
(WebKit::ThreadedCompositor::glContext):
(WebKit::ThreadedCompositor::renderLayerTree): Return early if scene is not active.
* WebProcess/WebPage/CoordinatedGraphics/ThreadedCoordinatedLayerTreeHost.cpp:
(WebKit::ThreadedCoordinatedLayerTreeHost::setNativeSurfaceHandleForCompositing): Schedule a new layer flush
after the native surface handle changed.

git-svn-id: http://svn.webkit.org/repository/webkit/trunk@202040 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Source/WebKit2/Shared/CoordinatedGraphics/threadedcompositor/ThreadedCompositor.cpp
@@ -61,9 +61,15 @@ ThreadedCompositor::~ThreadedCompositor()
 
 void ThreadedCompositor::setNativeSurfaceHandleForCompositing(uint64_t handle)
 {
-    m_compositingRunLoop->performTask([this, protectedThis = Ref<ThreadedCompositor>(*this), handle] {
+    m_compositingRunLoop->stopUpdateTimer();
+    m_compositingRunLoop->performTaskSync([this, protectedThis = Ref<ThreadedCompositor>(*this), handle] {
+        m_scene->setActive(!!handle);
+
+        // A new native handle can't be set without destroying the previous one first if any.
+        ASSERT(!!handle ^ !!m_nativeSurfaceHandle);
         m_nativeSurfaceHandle = handle;
-        m_scene->setActive(true);
+        if (!m_nativeSurfaceHandle)
+            m_context = nullptr;
     });
 }
 
@@ -130,7 +136,7 @@ void ThreadedCompositor::commitScrollOffset(uint32_t layerID, const IntSize& off
     m_client->commitScrollOffset(layerID, offset);
 }
 
-bool ThreadedCompositor::ensureGLContext()
+bool ThreadedCompositor::tryEnsureGLContext()
 {
     if (!glContext())
         return false;
@@ -156,7 +162,7 @@ GLContext* ThreadedCompositor::glContext()
         return m_context.get();
 
     if (!m_nativeSurfaceHandle)
-        return 0;
+        return nullptr;
 
     m_context = GLContext::createContextForWindow(reinterpret_cast<GLNativeWindowType>(m_nativeSurfaceHandle), GLContext::sharingContext());
     return m_context.get();
@@ -181,10 +187,10 @@ void ThreadedCompositor::didChangeVisibleRect()
 void ThreadedCompositor::renderLayerTree()
 {
     ASSERT(&RunLoop::current() == &m_compositingRunLoop->runLoop());
-    if (!m_scene)
+    if (!m_scene || !m_scene->isActive())
         return;
 
-    if (!ensureGLContext())
+    if (!tryEnsureGLContext())
         return;
 
     FloatRect clipRect(0, 0, m_viewportSize.width(), m_viewportSize.height());

Source/WebKit2/Shared/CoordinatedGraphics/threadedcompositor/CompositingRunLoop.cpp
@@ -71,6 +71,11 @@ void CompositingRunLoop::startUpdateTimer(UpdateTiming timing)
     m_updateTimer.startOneShot(nextUpdateTime);
 }
 
+void CompositingRunLoop::stopUpdateTimer()
+{
+    m_updateTimer.stop();
+}
+
 void CompositingRunLoop::updateTimerFired()
 {
     m_updateFunction();

Source/WebKit2/Shared/CoordinatedGraphics/threadedcompositor/CompositingRunLoop.h
@@ -50,6 +50,7 @@ public:
     void performTaskSync(NoncopyableFunction<void ()>&&);
 
     void startUpdateTimer(UpdateTiming = Immediate);
+    void stopUpdateTimer();
 
     void run();
     void stop();

Source/WebKit2/Shared/CoordinatedGraphics/threadedcompositor/ThreadedCompositor.h
@@ -88,7 +88,7 @@ private:
     void scheduleDisplayImmediately();
     void didChangeVisibleRect() override;
 
-    bool ensureGLContext();
+    bool tryEnsureGLContext();
     WebCore::GLContext* glContext();
 
     void createCompositingThread();

Source/WebKit2/WebProcess/WebPage/CoordinatedGraphics/ThreadedCoordinatedLayerTreeHost.cpp
@@ -198,6 +198,7 @@ void ThreadedCoordinatedLayerTreeHost::setNativeSurfaceHandleForCompositing(uint
 {
     m_layerTreeContext.contextID = handle;
     m_compositor->setNativeSurfaceHandleForCompositing(handle);
+    scheduleLayerFlush();
 }
 #endif
 

commit 47d1dbcc544fbb58d4ccd9cc268e780dd78d4891

[ThreadedCompositor] Build fails in debug mode after r202040 https://bugs.webkit.org/show_bug.cgi?id=159757
Patch by Hyunjun Ko <zzoon@igalia.com> on 2016-07-14
Reviewed by Michael Catanzaro.

Debug mode build failed due to call getter method, which is
removed, in ASSERT().

* Shared/CoordinatedGraphics/threadedcompositor/ThreadedCompositor.cpp:
(WebKit::ThreadedCompositor::glContext): Deleted.
(WebKit::ThreadedCompositor::didChangeVisibleRect): Deleted.
(WebKit::ThreadedCompositor::renderLayerTree): Deleted.

git-svn-id: http://svn.webkit.org/repository/webkit/trunk@203223 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Source/WebKit2/Shared/CoordinatedGraphics/threadedcompositor/ThreadedCompositor.cpp
@@ -156,8 +156,6 @@ bool ThreadedCompositor::tryEnsureGLContext()
 
 GLContext* ThreadedCompositor::glContext()
 {
-    ASSERT(&RunLoop::current() == &m_compositingRunLoop->runLoop());
-
     if (m_context)
         return m_context.get();
 
@@ -175,8 +173,6 @@ void ThreadedCompositor::scheduleDisplayImmediately()
 
 void ThreadedCompositor::didChangeVisibleRect()
 {
-    ASSERT(&RunLoop::current() == &m_compositingRunLoop->runLoop());
-
     RunLoop::main().dispatch([this, protectedThis = Ref<ThreadedCompositor>(*this), visibleRect = m_viewportController->visibleContentsRect(), scale = m_viewportController->pageScaleFactor()] {
         m_client->setVisibleContentsRect(visibleRect, FloatPoint::zero(), scale);
     });
@@ -186,7 +182,6 @@ void ThreadedCompositor::didChangeVisibleRect()
 
 void ThreadedCompositor::renderLayerTree()
 {
-    ASSERT(&RunLoop::current() == &m_compositingRunLoop->runLoop());
     if (!m_scene || !m_scene->isActive())
         return;
 