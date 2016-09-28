c7c3dc26b1acddd32879a15b61d91e5ef649545d
Konstantin Tokarev <annulen@yandex.ru>
2016-09-28 17:06:40 +0300

    make-snapshot.pl show create commit automatically
    Extra files in qt.io repo still require manual handling.
    
    Change-Id: I907d96e3569bbe24138c6dbf2d7ec62903e746b2

--- Tools/qt/make-snapshot.pl
+++ Tools/qt/make-snapshot.pl
@@ -14,10 +14,27 @@ scalar @ARGV == 0 || usage();
 -f "ChangeLog" && die "This script must be run in snapshots repository";
 -d ".git" || usage();
 
+`git status` =~ "nothing to commit, working tree clean" or die "Target working tree is dirty";
+
 my $src_repo = dirname(dirname(dirname(__FILE__)));
 my $target_repo = getcwd();
 
 chdir $src_repo;
 (-f "ChangeLog" && -x "Tools/gtk/make-dist.py") or usage("Target repository path is invalid!\n");
+my $commit = `git rev-parse HEAD` or usage("Cannot get HEAD revision in target repo!\n");
+chomp $commit;
 
-exec("Tools/gtk/make-dist.py -t snapshot Tools/qt/manifest.txt")
+my @commands = (
+    "Tools/gtk/make-dist.py -t snapshot Tools/qt/manifest.txt",
+    "cd $target_repo",
+    "git rm -rf *",
+    "tar -xf $src_repo/snapshot.tar --strip-components=1",
+    "git add -A",
+    "rm $src_repo/snapshot.tar",
+    "git commit -m 'Imported WebKit commit $commit'"
+);
+
+my $cmd = join " && ", @commands;
+print "Executing $cmd\n";
+
+exec $cmd;
