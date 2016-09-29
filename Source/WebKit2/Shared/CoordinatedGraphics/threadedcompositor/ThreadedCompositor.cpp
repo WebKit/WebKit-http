37332141a52c8746cf750dff2934bac8269ea218
Joshua Watt <JPEWhacker@gmail.com>
2016-09-29 13:45:23 -0500

    Remove broken assert
    Removes an assert that no longer compiles. It appears this was broken in
    2cb195ae3d5b62e2848b8dbeb542b44c78989df9

--- Source/WebKit2/Shared/CoordinatedGraphics/threadedcompositor/ThreadedCompositor.cpp
+++ Source/WebKit2/Shared/CoordinatedGraphics/threadedcompositor/ThreadedCompositor.cpp
@@ -320,7 +320,6 @@ static void debugThreadedCompositorFPS()
 
 void ThreadedCompositor::frameComplete()
 {
-    ASSERT(&RunLoop::current() == &m_compositingRunLoop->runLoop());
     static bool reportFPS = !!std::getenv("WPE_THREADED_COMPOSITOR_FPS");
     if (reportFPS)
         debugThreadedCompositorFPS();
