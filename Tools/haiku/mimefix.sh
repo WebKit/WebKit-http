ce9d3f2028b2724b9181525014226643e1f6fc1c
Adrien Destugues <pulkomandy@pulkomandy.tk>
2016-10-30 21:25:40 +0100

    Fix MIME type for some more tests.

--- Tools/haiku/mimefix.sh
+++ Tools/haiku/mimefix.sh
@@ -3,5 +3,7 @@
 # always make the best guess on the type of files, and the test suite relies on the types being
 # correct. So, let's fix them!
 
-rmattr BEOS:TYPE /Dev/Haiku/webkit/LayoutTests/webgl/1.0.3/resources/webgl_test_files/conformance/uniforms/*.html
-addattr BEOS:TYPE text/html /Dev/Haiku/webkit/LayoutTests/webgl/1.0.3/resources/webgl_test_files/conformance/uniforms/*.html
+set TESTS=BEOS:TYPE LayoutTests/webgl/1.0.3/resources/webgl_test_files/conformance/*/*.html LayoutTests/css3/filters/regions-expanding.html LayoutTests/editing/caret/*.html
+
+rmattr -f BEOS:TYPE $TESTS
+addattr BEOS:TYPE $TESTS
