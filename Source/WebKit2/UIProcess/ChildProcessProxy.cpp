24869b06b12203df1afb65d9e2484bed8278605d
Zan Dobersek <zdobersek@igalia.com>
2016-11-27 23:18:22 -0800

    ChildProcessProxy: WPE should support cmd prefixing.

--- Source/WebKit2/UIProcess/ChildProcessProxy.cpp
+++ Source/WebKit2/UIProcess/ChildProcessProxy.cpp
@@ -51,7 +51,7 @@ void ChildProcessProxy::getLaunchOptions(ProcessLauncher::LaunchOptions& launchO
     if (const char* userDirectorySuffix = getenv("DIRHELPER_USER_DIR_SUFFIX"))
         launchOptions.extraInitializationData.add(ASCIILiteral("user-directory-suffix"), userDirectorySuffix);
 
-#if ENABLE(DEVELOPER_MODE) && (PLATFORM(GTK) || PLATFORM(EFL))
+#if ENABLE(DEVELOPER_MODE) && (PLATFORM(GTK) || PLATFORM(WPE) || PLATFORM(EFL))
     const char* varname;
     switch (launchOptions.processType) {
     case ProcessLauncher::ProcessType::Web:
@@ -75,7 +75,7 @@ void ChildProcessProxy::getLaunchOptions(ProcessLauncher::LaunchOptions& launchO
     const char* processCmdPrefix = getenv(varname);
     if (processCmdPrefix && *processCmdPrefix)
         launchOptions.processCmdPrefix = String::fromUTF8(processCmdPrefix);
-#endif // !defined(NDEBUG) && (PLATFORM(GTK) || PLATFORM(EFL)
+#endif // !defined(NDEBUG) && (PLATFORM(GTK) || PLATFORM(WPE) || PLATFORM(EFL)
 }
 
 void ChildProcessProxy::connect()
