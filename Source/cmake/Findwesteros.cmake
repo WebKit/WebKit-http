7b531ba40a79b9ab5182034e88ac886371f06022
Zan Dobersek <zdobersek@igalia.com>
2017-01-10 02:50:52 -0800

    Findwesteros: allow searching for Westeros that's installed out of the usual paths.

--- Source/cmake/Findwesteros.cmake
+++ Source/cmake/Findwesteros.cmake
@@ -29,7 +29,19 @@
 # ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 find_package(PkgConfig)
-pkg_check_modules(WESTEROS westeros-compositor)
+pkg_check_modules(PC_WESTEROS westeros-compositor)
+
+find_path(WESTEROS_INCLUDE_DIRS
+    NAMES westeros-compositor.h
+    HINTS ${PC_WESTEROS_INCLUDE_DIRS} ${PC_WESTEROS_INCUDEDIR}
+)
+
+find_library(WESTEROS_EGL_LIBRARIES
+    NAMES ${PC_WESTEROS_LIBRARIES}
+    HINTS ${PC_WESTEROS_LIBRARY_DIRS} ${PC_WESTEROS_LIBDIR}
+)
 
 include(FindPackageHandleStandardArgs)
 FIND_PACKAGE_HANDLE_STANDARD_ARGS(WESTEROS DEFAULT_MSG WESTEROS_FOUND)
+
+mark_as_advanced(WESTEROS_INCLUDE_DIRS WESTEROS_LIBRARIES)
