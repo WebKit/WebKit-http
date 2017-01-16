7c5fd873887e6d0b1d002120e8c43dc8d6f444f0
Konstantin Tokarev <annulen@yandex.ru>
2017-01-16 19:39:53 +0300

    [jhbuild] Build Mesa with llvmpipe for headless running of GL tests
    Change-Id: I9c8e2117dc3695e0fde1b418917cbbec17accc48

--- Tools/Scripts/webkitpy/port/qt.py
+++ Tools/Scripts/webkitpy/port/qt.py
@@ -177,6 +177,16 @@ class QtPort(Port):
         self._copy_value_from_environ_if_set(clean_env, 'QT_WEBKIT_PAUSE_UI_PROCESS')
         self._copy_value_from_environ_if_set(clean_env, 'QT_QPA_PLATFORM_PLUGIN_PATH')
         self._copy_value_from_environ_if_set(clean_env, 'QT_WEBKIT_DISABLE_UIPROCESS_DUMPPIXELS')
+        if self._driver_class() == XvfbDriver and self._should_use_jhbuild():
+            llvmpipe_libgl_path = self.host.executive.run_command(self._jhbuild_wrapper + ['printenv', 'LLVMPIPE_LIBGL_PATH'],
+                                                                  error_handler=self.host.executive.ignore_error).strip()
+            if os.path.exists(os.path.join(llvmpipe_libgl_path, "libGL.so")):
+                # Force the Gallium llvmpipe software rasterizer
+                clean_env['LD_LIBRARY_PATH'] = llvmpipe_libgl_path
+                if os.environ.get('LD_LIBRARY_PATH'):
+                    clean_env['LD_LIBRARY_PATH'] += ':%s' % os.environ.get('LD_LIBRARY_PATH')
+            else:
+                _log.warning("Can't find Gallium llvmpipe driver. Try to run update-webkitgtk-libs")
         return clean_env
 
     # FIXME: We should find a way to share this implmentation with Gtk,
