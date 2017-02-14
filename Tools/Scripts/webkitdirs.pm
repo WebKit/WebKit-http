0eff21e70eb6c36354138b34d490e2acbce754cc
clopez@igalia.com <clopez@igalia.com>
2017-02-14 16:46:49 +0000

    build-webkit should print the cmake command executed https://bugs.webkit.org/show_bug.cgi?id=168318
    Reviewed by Michael Catanzaro.
    
    Run the cmake commands to configure or start the build through
    a wrapper that prints the command before executing it.
    
    * Scripts/webkitdirs.pm:
    (systemVerbose):
    (generateBuildSystemFromCMakeProject):
    (buildCMakeGeneratedProject):
    (cleanCMakeGeneratedProject):
    
    git-svn-id: http://svn.webkit.org/repository/webkit/trunk@212298 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Tools/Scripts/webkitdirs.pm
+++ Tools/Scripts/webkitdirs.pm
@@ -288,6 +288,11 @@ sub determineBaseProductDir
     }
 }
 
+sub systemVerbose {
+    print "+ @_\n";
+    return system(@_);
+}
+
 sub setBaseProductDir($)
 {
     ($baseProductDir) = @_;
@@ -2009,7 +2014,7 @@ sub generateBuildSystemFromCMakeProject
     # We call system("cmake @args") instead of system("cmake", @args) so that @args is
     # parsed for shell metacharacters.
     my $wrapper = join(" ", wrapperPrefixIfNeeded()) . " ";
-    my $returnCode = system($wrapper . "cmake @args");
+    my $returnCode = systemVerbose($wrapper . "cmake @args");
 
     chdir($originalWorkingDirectory);
     return $returnCode;
@@ -2043,7 +2048,7 @@ sub buildCMakeGeneratedProject($)
     # We call system("cmake @args") instead of system("cmake", @args) so that @args is
     # parsed for shell metacharacters. In particular, $makeArgs may contain such metacharacters.
     my $wrapper = join(" ", wrapperPrefixIfNeeded()) . " ";
-    return system($wrapper . "$command @args");
+    return systemVerbose($wrapper . "$command @args");
 }
 
 sub cleanCMakeGeneratedProject()
@@ -2051,7 +2056,7 @@ sub cleanCMakeGeneratedProject()
     my $config = configuration();
     my $buildPath = File::Spec->catdir(baseProductDir(), $config);
     if (-d $buildPath) {
-        return system("cmake", "--build", $buildPath, "--config", $config, "--target", "clean");
+        return systemVerbose("cmake", "--build", $buildPath, "--config", $config, "--target", "clean");
     }
     return 0;
 }
