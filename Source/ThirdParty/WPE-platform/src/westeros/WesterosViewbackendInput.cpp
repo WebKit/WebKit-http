384087e6f224d2313ed40ab6b233377154eeef2a
Alexandr Gavriliuc <agavriliuc@productengine.com>
2017-03-02 12:19:39 +0200

    Move WebKit API calls in WesterosViewbackend* to a main thread

--- Source/ThirdParty/WPE-platform/src/westeros/WesterosViewbackendInput.cpp
+++ Source/ThirdParty/WPE-platform/src/westeros/WesterosViewbackendInput.cpp
@@ -7,6 +7,7 @@
 #include <locale.h>
 #include <sys/mman.h>
 #include <unistd.h>
+#include <glib.h>
 #include <wpe/input.h>
 #include <wpe/view-backend.h>
 
@@ -138,24 +139,43 @@ void WesterosViewbackendInput::keyboardHandleRepeatInfo( void *userData, int32_t
 
 void WesterosViewbackendInput::handleKeyEvent(void* userData, uint32_t key, uint32_t state, uint32_t time)
 {
-    auto& backend_input = *static_cast<WesterosViewbackendInput*>(userData);
-    auto& handlerData = backend_input.m_handlerData;
-
-    auto& xkb = handlerData.xkb;
-    uint32_t keysym = xkb_state_key_get_one_sym(xkb.state, key);
-    uint32_t unicode = xkb_state_key_get_utf32(xkb.state, key);
-
-    if (xkb.composeState
-        && state == WL_KEYBOARD_KEY_STATE_PRESSED
-        && xkb_compose_state_feed(xkb.composeState, keysym) == XKB_COMPOSE_FEED_ACCEPTED
-        && xkb_compose_state_get_status(xkb.composeState) == XKB_COMPOSE_COMPOSED)
+    struct KeyEventData
     {
-        keysym = xkb_compose_state_get_one_sym(xkb.composeState);
-        unicode = xkb_keysym_to_utf32(keysym);
-    }
+        void* userData;
+        uint32_t key;
+        uint32_t state;
+        uint32_t time;
+    };
 
-    struct wpe_input_keyboard_event event{ time, keysym, unicode, !!state, xkb.modifiers };
-    wpe_view_backend_dispatch_keyboard_event(backend_input.m_viewbackend, &event);
+    KeyEventData *eventData = new KeyEventData { userData, key, state, time };
+
+    g_idle_add_full(G_PRIORITY_DEFAULT, [](gpointer data) -> gboolean
+    {
+        KeyEventData *e = (KeyEventData*)data;
+
+        auto& backend_input = *static_cast<WesterosViewbackendInput*>(e->userData);
+        auto& handlerData = backend_input.m_handlerData;
+
+        auto& xkb = handlerData.xkb;
+        uint32_t keysym = xkb_state_key_get_one_sym(xkb.state, e->key);
+        uint32_t unicode = xkb_state_key_get_utf32(xkb.state, e->key);
+
+        if (xkb.composeState
+            && e->state == WL_KEYBOARD_KEY_STATE_PRESSED
+            && xkb_compose_state_feed(xkb.composeState, keysym) == XKB_COMPOSE_FEED_ACCEPTED
+            && xkb_compose_state_get_status(xkb.composeState) == XKB_COMPOSE_COMPOSED)
+        {
+            keysym = xkb_compose_state_get_one_sym(xkb.composeState);
+            unicode = xkb_keysym_to_utf32(keysym);
+        }
+
+        struct wpe_input_keyboard_event event
+                { e->time, keysym, unicode, !!e->state, xkb.modifiers };
+        wpe_view_backend_dispatch_keyboard_event(backend_input.m_viewbackend, &event);
+
+        delete e;
+        return G_SOURCE_REMOVE;
+    }, eventData, nullptr);
 }
 
 gboolean WesterosViewbackendInput::repeatRateTimeout(void* userData)
@@ -187,46 +207,98 @@ void WesterosViewbackendInput::pointerHandleLeave( void *userData )
 
 void WesterosViewbackendInput::pointerHandleMotion( void *userData, uint32_t time, wl_fixed_t sx, wl_fixed_t sy )
 {
-    auto& backend_input = *static_cast<WesterosViewbackendInput*>(userData);
-    auto& handlerData = backend_input.m_handlerData;
+    struct MotionEventData
+    {
+        void* userData;
+        uint32_t time;
+        wl_fixed_t sx;
+        wl_fixed_t sy;
+    };
 
-    auto x = wl_fixed_to_int(sx);
-    auto y = wl_fixed_to_int(sy);
+    MotionEventData *eventData = new MotionEventData { userData, time, sx, sy };
 
-    auto& pointer = handlerData.pointer;
-    pointer.coords = { x, y };
+    g_idle_add_full(G_PRIORITY_DEFAULT, [](gpointer data) -> gboolean
+    {
+        MotionEventData *e = (MotionEventData*)data;
 
-    struct wpe_input_pointer_event event{ wpe_input_pointer_event_type_motion, time, x, y, 0, 0 };
-    wpe_view_backend_dispatch_pointer_event(backend_input.m_viewbackend, &event);
+        auto& backend_input = *static_cast<WesterosViewbackendInput*>(e->userData);
+        auto& handlerData = backend_input.m_handlerData;
+
+        auto x = wl_fixed_to_int(e->sx);
+        auto y = wl_fixed_to_int(e->sy);
+
+        auto& pointer = handlerData.pointer;
+        pointer.coords = { x, y };
+
+        struct wpe_input_pointer_event event
+                { wpe_input_pointer_event_type_motion, e->time, x, y, 0, 0 };
+        wpe_view_backend_dispatch_pointer_event(backend_input.m_viewbackend, &event);
+
+        delete e;
+        return G_SOURCE_REMOVE;
+    }, eventData, nullptr);
 }
 
 void WesterosViewbackendInput::pointerHandleButton( void *userData, uint32_t time, uint32_t button, uint32_t state )
 {
-    auto& backend_input = *static_cast<WesterosViewbackendInput*>(userData);
-    auto& handlerData = backend_input.m_handlerData;
+    struct PointerEventData
+    {
+        void* userData;
+        uint32_t time;
+        uint32_t button;
+        uint32_t state;
+    };
 
-    if (button >= BTN_MOUSE)
-            button = button - BTN_MOUSE + 1;
-        else
-            button = 0;
+    button = (button >= BTN_MOUSE) ? (button - BTN_MOUSE + 1) : 0;
+    PointerEventData *eventData = new PointerEventData { userData, time, button, state };
 
-    auto& pointer = handlerData.pointer;
-    auto& coords = pointer.coords;
+    g_idle_add_full(G_PRIORITY_DEFAULT, [](gpointer data) -> gboolean
+    {
+        PointerEventData *e = (PointerEventData*)data;
 
-    struct wpe_input_pointer_event event{ wpe_input_pointer_event_type_button, time, coords.first, coords.second, button, state };
-    wpe_view_backend_dispatch_pointer_event(backend_input.m_viewbackend, &event);
+        auto& backend_input = *static_cast<WesterosViewbackendInput*>(e->userData);
+        auto& handlerData = backend_input.m_handlerData;
+
+        auto& pointer = handlerData.pointer;
+        auto& coords = pointer.coords;
+
+        struct wpe_input_pointer_event event
+                { wpe_input_pointer_event_type_button, e->time, coords.first, coords.second, e->button, e->state };
+        wpe_view_backend_dispatch_pointer_event(backend_input.m_viewbackend, &event);
+
+        delete e;
+        return G_SOURCE_REMOVE;
+    }, eventData, nullptr);
 }
 
 void WesterosViewbackendInput::pointerHandleAxis( void *userData, uint32_t time, uint32_t axis, wl_fixed_t value )
 {
-    auto& backend_input = *static_cast<WesterosViewbackendInput*>(userData);
-    auto& handlerData = backend_input.m_handlerData;
+    struct AxisEventData
+    {
+        void* userData;
+        uint32_t time;
+        uint32_t axis;
+        wl_fixed_t value;
+    };
 
-    auto& pointer = handlerData.pointer;
-    auto& coords = pointer.coords;
+    AxisEventData *eventData = new AxisEventData { userData, time, axis, value };
 
-    struct wpe_input_axis_event event{ wpe_input_axis_event_type_motion, time, coords.first, coords.second, axis, -wl_fixed_to_int(value) };
-    wpe_view_backend_dispatch_axis_event(backend_input.m_viewbackend, &event);
+    g_idle_add_full(G_PRIORITY_DEFAULT, [](gpointer data) -> gboolean
+    {
+        AxisEventData *e = (AxisEventData*)data;
+
+        auto& backend_input = *static_cast<WesterosViewbackendInput*>(e->userData);
+        auto& handlerData = backend_input.m_handlerData;
+
+        auto& pointer = handlerData.pointer;
+        auto& coords = pointer.coords;
+
+        struct wpe_input_axis_event event{ wpe_input_axis_event_type_motion, e->time, coords.first, coords.second, e->axis, -wl_fixed_to_int(e->value) };
+        wpe_view_backend_dispatch_axis_event(backend_input.m_viewbackend, &event);
+
+        delete e;
+        return G_SOURCE_REMOVE;
+    }, eventData, nullptr);
 }
 
 WesterosViewbackendInput::WesterosViewbackendInput(struct wpe_view_backend* backend)
--- Source/ThirdParty/WPE-platform/src/westeros/WesterosViewbackendOutput.cpp
+++ Source/ThirdParty/WPE-platform/src/westeros/WesterosViewbackendOutput.cpp
@@ -8,6 +8,7 @@
 #include <locale.h>
 #include <sys/mman.h>
 #include <unistd.h>
+#include <glib.h>
 #include <wpe/view-backend.h>
 
 namespace Westeros {
@@ -26,15 +27,33 @@ void WesterosViewbackendOutput::handleGeometryCallback( void *userData, int32_t 
 
 void WesterosViewbackendOutput::handleModeCallback( void *userData, uint32_t flags, int32_t width, int32_t height, int32_t refreshRate )
 {
-    auto& backend_output = *static_cast<WesterosViewbackendOutput*>(userData);
-    if (flags == WesterosViewbackendModeCurrent) {
-        backend_output.m_width = width;
-        backend_output.m_height = height;
-        if(backend_output.m_viewbackend)
+    if (flags != WesterosViewbackendModeCurrent)
+        return;
+
+    struct ModeData
+    {
+        void* userData;
+        int32_t width;
+        int32_t height;
+    };
+
+    ModeData *modeData = new ModeData { userData, width, height };
+
+    g_idle_add_full(G_PRIORITY_DEFAULT, [](gpointer data) -> gboolean
+    {
+        ModeData *d = (ModeData*)data;
+
+        auto& backend_output = *static_cast<WesterosViewbackendOutput*>(d->userData);
+        backend_output.m_width = d->width;
+        backend_output.m_height = d->height;
+        if (backend_output.m_viewbackend)
         {
-            wpe_view_backend_dispatch_set_size(backend_output.m_viewbackend, backend_output.m_width, backend_output.m_height);
+            wpe_view_backend_dispatch_set_size(backend_output.m_viewbackend, d->width, d->height);
         }
-    }
+
+        delete d;
+        return G_SOURCE_REMOVE;
+    }, modeData, nullptr);
 }
 
 void WesterosViewbackendOutput::handleDoneCallback( void *UserData )
