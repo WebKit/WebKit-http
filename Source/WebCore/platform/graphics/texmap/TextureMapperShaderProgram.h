9f95b86e6e2a3365094d6a37a927753dd9857f21
Konstantin Tokarev <annulen@yandex.ru>
2017-03-04 14:51:31 +0300

    ASSERTION FAILED: The string being removed is atomic in the string table of an other thread! iterator != atomicStringTable.end() at Source/WTF/wtf/text/AtomicStringImpl.cpp(453) https://bugs.webkit.org/show_bug.cgi?id=161800
    Reviewed by Žan Doberšek.
    
    Speculative fix. These strings are created as static objects on a secondary thread, but all
    static objects are destroyed in exit handlers on the main thread, and AtomicStrings must
    always be destroyed on the same thread they are created.
    
    * platform/graphics/texmap/TextureMapperShaderProgram.h:
    
    git-svn-id: http://svn.webkit.org/repository/webkit/trunk@206061 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/platform/graphics/texmap/TextureMapperShaderProgram.h
+++ Source/WebCore/platform/graphics/texmap/TextureMapperShaderProgram.h
@@ -22,15 +22,23 @@
 #define TextureMapperShaderProgram_h
 
 #if USE(TEXTURE_MAPPER_GL)
+
 #include "GraphicsContext3D.h"
 #include "TransformationMatrix.h"
 #include <wtf/HashMap.h>
+#include <wtf/NeverDestroyed.h>
 #include <wtf/PassRefPtr.h>
 #include <wtf/RefPtr.h>
 #include <wtf/text/AtomicStringHash.h>
 
 namespace WebCore {
-#define TEXMAP_DECLARE_VARIABLE(Accessor, Name, Type) GC3Duint Accessor##Location() { static const AtomicString name(Name); return getLocation(name, Type); }
+
+#define TEXMAP_DECLARE_VARIABLE(Accessor, Name, Type) \
+    GC3Duint Accessor##Location() { \
+        static NeverDestroyed<const AtomicString> name(Name, AtomicString::ConstructFromLiteral); \
+        return getLocation(name.get(), Type); \
+    }
+
 #define TEXMAP_DECLARE_UNIFORM(Accessor) TEXMAP_DECLARE_VARIABLE(Accessor, "u_"#Accessor, UniformVariable)
 #define TEXMAP_DECLARE_ATTRIBUTE(Accessor) TEXMAP_DECLARE_VARIABLE(Accessor, "a_"#Accessor, AttribVariable)
 #define TEXMAP_DECLARE_SAMPLER(Accessor) TEXMAP_DECLARE_VARIABLE(Accessor, "s_"#Accessor, UniformVariable)
