a97f19a9ead6ca8ca91508e7724b747b23375595
Albert Dahan <a.dahan@metrological.com>
2017-03-21 16:38:04 +0100

    [EME][PR] Disable re-use of key for now

--- Source/WebCore/platform/graphics/gstreamer/eme/PlayreadySession.cpp
+++ Source/WebCore/platform/graphics/gstreamer/eme/PlayreadySession.cpp
@@ -149,13 +149,14 @@ RefPtr<Uint8Array> PlayreadySession::playreadyGenerateKeyRequest(Uint8Array* ini
                                   initData->byteLength()));
     GST_DEBUG("init data set on DRM context");
 
-    if (DRM_SUCCEEDED(Drm_Reader_Bind(m_poAppContext, g_rgpdstrRights, NO_OF(g_rgpdstrRights), _PolicyCallback, NULL, &m_oDecryptContext))) {
-        GST_DEBUG("Play rights already acquired!");
-        m_eKeyState = KEY_READY;
-        systemCode = dr;
-        errorCode = 0;
-        return nullptr;
-    }
+    // FIXME: Revert once re-use of key is fixed
+    //if (DRM_SUCCEEDED(Drm_Reader_Bind(m_poAppContext, g_rgpdstrRights, NO_OF(g_rgpdstrRights), _PolicyCallback, NULL, &m_oDecryptContext))) {
+    //    GST_DEBUG("Play rights already acquired!");
+    //    m_eKeyState = KEY_READY;
+    //    systemCode = dr;
+    //    errorCode = 0;
+    //    return nullptr;
+    //}
     GST_DEBUG("DRM reader bound");
 
     // Try to figure out the size of the license acquisition
