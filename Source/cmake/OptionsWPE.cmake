c138f655cf5ad62af2563c43609acb3b2771c3d7
Zan Dobersek <zdobersek@igalia.com>
2017-05-23 15:30:01 +0200

    OptionsWPE: compile with -ffunction-sections -fdata-sections, link with --gc-sections.

--- Source/cmake/OptionsWPE.cmake
+++ Source/cmake/OptionsWPE.cmake
@@ -209,3 +209,10 @@ if (NOT DEVELOPER_MODE)
     set(CMAKE_C_FLAGS_RELEASE "${CMAKE_C_FLAGS_RELEASE} -fvisibility=hidden")
     set(CMAKE_CXX_FLAGS_RELEASE "${CMAKE_CXX_FLAGS_RELEASE} -fvisibility=hidden -fvisibility-inlines-hidden")
 endif ()
+
+# Optimize binary size for release builds by removing dead sections on unix/gcc.
+if (CMAKE_COMPILER_IS_GNUCC AND UNIX AND NOT APPLE)
+    set(CMAKE_C_FLAGS_RELEASE "${CMAKE_C_FLAGS_RELEASE} -ffunction-sections -fdata-sections")
+    set(CMAKE_CXX_FLAGS_RELEASE "${CMAKE_CXX_FLAGS_RELEASE} -ffunction-sections -fdata-sections -fno-rtti")
+    set(CMAKE_SHARED_LINKER_FLAGS_RELEASE "${CMAKE_SHARED_LINKER_FLAGS_RELEASE} -Wl,--gc-sections")
+endif ()
