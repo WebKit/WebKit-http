fberlakovich/r2c-webkit

Add even more input validation in Connection::processMessage() https://bugs.webkit.org/show_bug.cgi?id=171682
Reviewed by Carlos Garcia Campos.

Verify that the size of the out-of-line message body matches the size of the AttachmentInfo
that is containing it.

* Platform/IPC/unix/ConnectionUnix.cpp:
(IPC::Connection::processMessage):

git-svn-id: https://svn.webkit.org/repository/webkit/trunk@217219 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Source/WebKit2/Platform/IPC/unix/ConnectionUnix.cpp
@@ -194,7 +194,7 @@ bool Connection::processMessage()
     if (messageInfo.isBodyOutOfLine()) {
         ASSERT(messageInfo.bodySize());
 
-        if (attachmentInfo[attachmentCount].isNull()) {
+        if (attachmentInfo[attachmentCount].isNull() || attachmentInfo[attachmentCount].size() != messageInfo.bodySize()) {
             ASSERT_NOT_REACHED();
             return false;
         }

WebKit/WebKit-http

git-svn-id: http://svn.webkit.org/repository/webkit/trunk@217219 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Source/WebKit2/Platform/IPC/unix/ConnectionUnix.cpp
@@ -261,7 +261,7 @@ bool Connection::processMessage()
     if (messageInfo.isMessageBodyIsOutOfLine()) {
         ASSERT(messageInfo.bodySize());
 
-        if (attachmentInfo[attachmentCount].isNull()) {
+        if (attachmentInfo[attachmentCount].isNull() || attachmentInfo[attachmentCount].getSize() != messageInfo.bodySize()) {
             ASSERT_NOT_REACHED();
             return false;
         }