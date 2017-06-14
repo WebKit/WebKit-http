fberlakovich/r2c-webkit

Add more input validation in Connection::processMessage() https://bugs.webkit.org/show_bug.cgi?id=171682
Reviewed by Michael Catanzaro.

Check limits of attachments and message size. Credit to Nathan Crandall for reporting this issue and submitting
an equivalent fix.

* Platform/IPC/unix/ConnectionUnix.cpp:
(IPC::Connection::processMessage):

git-svn-id: https://svn.webkit.org/repository/webkit/trunk@217126 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Source/WebKit2/Platform/IPC/unix/ConnectionUnix.cpp
@@ -132,6 +132,11 @@ bool Connection::processMessage()
     memcpy(&messageInfo, messageData, sizeof(messageInfo));
     messageData += sizeof(messageInfo);
 
+    if (messageInfo.attachmentCount() > attachmentMaxAmount || (!messageInfo.isBodyOutOfLine() && messageInfo.bodySize() > messageMaxSize)) {
+        ASSERT_NOT_REACHED();
+        return false;
+    }
+
     size_t messageLength = sizeof(MessageInfo) + messageInfo.attachmentCount() * sizeof(AttachmentInfo) + (messageInfo.isBodyOutOfLine() ? 0 : messageInfo.bodySize());
     if (m_readBuffer.size() < messageLength)
         return false;

WebKit/WebKit-http

Change-Id: I071bff4581163f46ed059475746d06de6bda77ea
git-svn-id: http://svn.webkit.org/repository/webkit/trunk@217126 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Source/WebKit2/Platform/IPC/unix/ConnectionUnix.cpp
@@ -199,6 +199,11 @@ bool Connection::processMessage()
     memcpy(&messageInfo, messageData, sizeof(messageInfo));
     messageData += sizeof(messageInfo);
 
+    if (messageInfo.attachmentCount() > attachmentMaxAmount || (!messageInfo.isMessageBodyIsOutOfLine() && messageInfo.bodySize() > messageMaxSize)) {
+        ASSERT_NOT_REACHED();
+        return false;
+    }
+
     size_t messageLength = sizeof(MessageInfo) + messageInfo.attachmentCount() * sizeof(AttachmentInfo) + (messageInfo.isMessageBodyIsOutOfLine() ? 0 : messageInfo.bodySize());
     if (m_readBuffer.size() < messageLength)
         return false;