71d0d30c449b6b4d083ebc83e5880188010518db
Glib Novodran <Glib_Novodran@comcast.com>
2017-07-19 13:13:57 +0300

    [GStreamer] Fix for invalid video size information sometimes returned by getVideoSizeAndFormatFromCaps (gst_video_info_from_caps)

--- Source/WebCore/platform/graphics/gstreamer/MediaPlayerPrivateGStreamerBase.cpp
+++ Source/WebCore/platform/graphics/gstreamer/MediaPlayerPrivateGStreamerBase.cpp
@@ -689,6 +689,13 @@ FloatSize MediaPlayerPrivateGStreamerBase::naturalSize() const
     if (!getVideoSizeAndFormatFromCaps(caps.get(), originalSize, format, pixelAspectRatioNumerator, pixelAspectRatioDenominator, stride))
         return FloatSize();
 
+    // Sanity check for the unlikely, but reproducible case when getVideoSizeAndFormatFromCaps returns incorrect values
+    if ((originalSize.width() == 0) || (originalSize.height() == 0)
+       || (pixelAspectRatioNumerator == 0) || (pixelAspectRatioNumerator == 0)) {
+        GST_DEBUG("getVideoSizeAndFormatFromCaps returned an invalid info, returning an empty size");
+        return FloatSize();
+    }
+
 #if USE(TEXTURE_MAPPER_GL)
     // When using accelerated compositing, if the video is tagged as rotated 90 or 270 degrees, swap width and height.
     if (m_player->client().mediaPlayerRenderingCanBeAccelerated(m_player)) {
