WebKit/WebKit-http

commit d051ab2eb655e97048c6d99d01fee5a3ec99ed67

Make NotificationPermissionRequest work with completion handlers https://bugs.webkit.org/show_bug.cgi?id=176753
Reviewed by Andy Estes.

API::UIClient::decidePolicyForNotificationPermissionRequest should take a completion handler
which it will eventually call with a bool representing whether notifications are allowed.
To do this, I make NotificationPermissionRequest just a wrapper for a lambda.
One subtle change in behavior is when invalidating all outstanding requests, we deny them instead
of just dropping them.  This results in a deny message sent where there wasn't one before,
resulting in better cleanup in NotificationPermissionRequestManager::didReceiveNotificationPermissionDecision.

* UIProcess/API/APIUIClient.h:
(API::UIClient::decidePolicyForNotificationPermissionRequest):
* UIProcess/API/C/WKPage.cpp:
(WKPageSetPageUIClient):
* UIProcess/Notifications/NotificationPermissionRequest.cpp: Removed.
* UIProcess/Notifications/NotificationPermissionRequest.h:
(WebKit::NotificationPermissionRequest::create):
(WebKit::NotificationPermissionRequest::allow):
(WebKit::NotificationPermissionRequest::deny):
(WebKit::NotificationPermissionRequest::NotificationPermissionRequest):
* UIProcess/Notifications/NotificationPermissionRequestManagerProxy.cpp:
(WebKit::NotificationPermissionRequestManagerProxy::invalidateRequests):
(WebKit::NotificationPermissionRequestManagerProxy::createRequest):
(WebKit::NotificationPermissionRequestManagerProxy::didReceiveNotificationPermissionDecision): Deleted.
* UIProcess/Notifications/NotificationPermissionRequestManagerProxy.h:
* UIProcess/WebPageProxy.cpp:
(WebKit::WebPageProxy::requestNotificationPermission):
* WebKit.xcodeproj/project.pbxproj:



git-svn-id: http://svn.webkit.org/repository/webkit/trunk@221894 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Source/WebKit/UIProcess/Notifications/NotificationPermissionRequestManagerProxy.cpp
@@ -41,28 +41,18 @@ NotificationPermissionRequestManagerProxy::NotificationPermissionRequestManagerP
 void NotificationPermissionRequestManagerProxy::invalidateRequests()
 {
     for (auto& request : m_pendingRequests.values())
-        request->invalidate();
+        request->deny();
 
     m_pendingRequests.clear();
 }
 
 Ref<NotificationPermissionRequest> NotificationPermissionRequestManagerProxy::createRequest(uint64_t notificationID)
 {
-    auto request = NotificationPermissionRequest::create(this, notificationID);
+    auto request = NotificationPermissionRequest::create([notificationID, page = makeRef(m_page)](bool allowed) {
+        page->process().send(Messages::WebPage::DidReceiveNotificationPermissionDecision(notificationID, allowed), page->pageID());
+    });
     m_pendingRequests.add(notificationID, request.ptr());
     return request;
-}
-
-void NotificationPermissionRequestManagerProxy::didReceiveNotificationPermissionDecision(uint64_t notificationID, bool allow)
-{
-    if (!m_page.isValid())
-        return;
-    
-    RefPtr<NotificationPermissionRequest> request = m_pendingRequests.take(notificationID);
-    if (!request)
-        return;
-    
-    m_page.process().send(Messages::WebPage::DidReceiveNotificationPermissionDecision(notificationID, allow), m_page.pageID());
 }
 
 } // namespace WebKit

Source/WebKit/CMakeLists.txt
@@ -422,7 +422,6 @@ set(WebKit2_SOURCES
 
     UIProcess/Network/NetworkProcessProxy.cpp
 
-    UIProcess/Notifications/NotificationPermissionRequest.cpp
     UIProcess/Notifications/NotificationPermissionRequestManagerProxy.cpp
     UIProcess/Notifications/WebNotification.cpp
     UIProcess/Notifications/WebNotificationManagerProxy.cpp

Source/WebKit/UIProcess/API/APIUIClient.h
@@ -129,7 +129,7 @@ public:
     virtual void decidePolicyForGeolocationPermissionRequest(WebKit::WebPageProxy&, WebKit::WebFrameProxy&, SecurityOrigin&, Function<void(bool)>&) { }
     virtual bool decidePolicyForUserMediaPermissionRequest(WebKit::WebPageProxy&, WebKit::WebFrameProxy&, SecurityOrigin&, SecurityOrigin&, WebKit::UserMediaPermissionRequestProxy&) { return false; }
     virtual bool checkUserMediaPermissionForOrigin(WebKit::WebPageProxy&, WebKit::WebFrameProxy&, SecurityOrigin&, SecurityOrigin&, WebKit::UserMediaPermissionCheckProxy&) { return false; }
-    virtual bool decidePolicyForNotificationPermissionRequest(WebKit::WebPageProxy*, SecurityOrigin*, WebKit::NotificationPermissionRequest*) { return false; }
+    virtual void decidePolicyForNotificationPermissionRequest(WebKit::WebPageProxy&, SecurityOrigin&, Function<void(bool)>&& completionHandler) { completionHandler(false); }
 
     // Printing.
     virtual float headerHeight(WebKit::WebPageProxy&, WebKit::WebFrameProxy&) { return 0; }

Source/WebKit/UIProcess/API/C/WKPage.cpp
@@ -1979,13 +1979,12 @@ void WKPageSetPageUIClient(WKPageRef pageRef, const WKPageUIClientBase* wkClient
             return true;
         }
         
-        bool decidePolicyForNotificationPermissionRequest(WebPageProxy* page, API::SecurityOrigin* origin, NotificationPermissionRequest* permissionRequest) final
+        void decidePolicyForNotificationPermissionRequest(WebPageProxy& page, API::SecurityOrigin& origin, Function<void(bool)>&& completionHandler) final
         {
             if (!m_client.decidePolicyForNotificationPermissionRequest)
-                return false;
+                return completionHandler(false);
 
-            m_client.decidePolicyForNotificationPermissionRequest(toAPI(page), toAPI(origin), toAPI(permissionRequest), m_client.base.clientInfo);
-            return true;
+            m_client.decidePolicyForNotificationPermissionRequest(toAPI(&page), toAPI(&origin), toAPI(NotificationPermissionRequest::create(WTFMove(completionHandler)).ptr()), m_client.base.clientInfo);
         }
 
         // Printing.

Source/WebKit/UIProcess/API/glib/WebKitUIClient.cpp
@@ -195,11 +195,10 @@ private:
         return true;
     }
 
-    bool decidePolicyForNotificationPermissionRequest(WebPageProxy*, API::SecurityOrigin*, NotificationPermissionRequest* permissionRequest) final
+    void decidePolicyForNotificationPermissionRequest(WebPageProxy&, API::SecurityOrigin&, Function<void(bool)>&& completionHandler) final
     {
-        GRefPtr<WebKitNotificationPermissionRequest> notificationPermissionRequest = adoptGRef(webkitNotificationPermissionRequestCreate(permissionRequest));
+        GRefPtr<WebKitNotificationPermissionRequest> notificationPermissionRequest = adoptGRef(webkitNotificationPermissionRequestCreate(NotificationPermissionRequest::create(WTFMove(completionHandler)).ptr()));
         webkitWebViewMakePermissionRequest(m_webView, WEBKIT_PERMISSION_REQUEST(notificationPermissionRequest.get()));
-        return true;
     }
 
 #if PLATFORM(GTK)

Source/WebKit/UIProcess/Notifications/NotificationPermissionRequest.cpp
@@ -1,67 +1,0 @@
-/*
- * Copyright (C) 2011 Apple Inc. All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
- * 2. Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in the
- *    documentation and/or other materials provided with the distribution.
- *
- * THIS SOFTWARE IS PROVIDED BY APPLE INC. AND ITS CONTRIBUTORS ``AS IS''
- * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
- * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
- * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL APPLE INC. OR ITS CONTRIBUTORS
- * BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
- * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
- * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
- * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
- * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
- * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
- * THE POSSIBILITY OF SUCH DAMAGE.
- */
-
-#include "config.h"
-#include "NotificationPermissionRequest.h"
-
-#include "NotificationPermissionRequestManagerProxy.h"
-
-namespace WebKit {
-    
-Ref<NotificationPermissionRequest> NotificationPermissionRequest::create(WebKit::NotificationPermissionRequestManagerProxy *manager, uint64_t notificationID)
-{
-    return adoptRef(*new NotificationPermissionRequest(manager, notificationID));
-}
-
-NotificationPermissionRequest::NotificationPermissionRequest(NotificationPermissionRequestManagerProxy* manager, uint64_t notificationID)
-    : m_manager(manager)
-    , m_notificationID(notificationID)
-{
-}
-
-void NotificationPermissionRequest::allow()
-{
-    if (!m_manager)
-        return;
-    
-    m_manager->didReceiveNotificationPermissionDecision(m_notificationID, true);
-    m_manager = 0;
-}
-
-void NotificationPermissionRequest::deny()
-{
-    if (!m_manager)
-        return;
-    
-    m_manager->didReceiveNotificationPermissionDecision(m_notificationID, false);
-    m_manager = 0;
-}
-
-void NotificationPermissionRequest::invalidate()
-{
-    m_manager = 0;
-}
-
-} // namespace WebKit

Source/WebKit/UIProcess/Notifications/NotificationPermissionRequest.h
@@ -23,10 +23,10 @@
  * THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-#ifndef NotificationPermissionRequest_h
-#define NotificationPermissionRequest_h
+#pragma once
 
 #include "APIObject.h"
+#include <wtf/Function.h>
 
 namespace WebKit {
 
@@ -34,20 +34,29 @@ class NotificationPermissionRequestManagerProxy;
 
 class NotificationPermissionRequest : public API::ObjectImpl<API::Object::Type::NotificationPermissionRequest> {
 public:
-    static Ref<NotificationPermissionRequest> create(NotificationPermissionRequestManagerProxy*, uint64_t notificationID);
+    static Ref<NotificationPermissionRequest> create(Function<void(bool)>&& completionHandler)
+    {
+        return adoptRef(*new NotificationPermissionRequest(WTFMove(completionHandler)));
+    }
+    
+    void allow()
+    {
+        if (auto completionHandler = std::exchange(m_completionHandler, nullptr))
+            completionHandler(true);
+    }
 
-    void allow();
-    void deny();
-
-    void invalidate();
+    void deny()
+    {
+        if (auto completionHandler = std::exchange(m_completionHandler, nullptr))
+            completionHandler(false);
+    }
 
 private:
-    NotificationPermissionRequest(NotificationPermissionRequestManagerProxy*, uint64_t notificationID);
-
-    NotificationPermissionRequestManagerProxy* m_manager;
-    uint64_t m_notificationID;    
+    NotificationPermissionRequest(Function<void(bool)>&& completionHandler)
+        : m_completionHandler(WTFMove(completionHandler))
+    { }
+    
+    Function<void(bool)> m_completionHandler;
 };
 
 } // namespace WebKit
-
-#endif // NotificationPermissionRequestProxy_h

Source/WebKit/UIProcess/Notifications/NotificationPermissionRequestManagerProxy.h
@@ -23,8 +23,7 @@
  * THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-#ifndef NotificationPermissionRequestManagerProxy_h
-#define NotificationPermissionRequestManagerProxy_h
+#pragma once
 
 #include "NotificationPermissionRequest.h"
 #include <wtf/HashMap.h>
@@ -37,20 +36,15 @@ class WebPageProxy;
 class NotificationPermissionRequestManagerProxy {
 public:
     explicit NotificationPermissionRequestManagerProxy(WebPageProxy&);
-    
+
     void invalidateRequests();
-    
+
     // Create a request to be presented to the user.
     Ref<NotificationPermissionRequest> createRequest(uint64_t notificationID);
-    
-    // Called by NotificationPermissionRequest when a decision is made by the user.
-    void didReceiveNotificationPermissionDecision(uint64_t notificationID, bool allow);
-    
+
 private:
     HashMap<uint64_t, RefPtr<NotificationPermissionRequest>> m_pendingRequests;
     WebPageProxy& m_page;
 };
 
 } // namespace WebKit
-
-#endif // NotificationPermissionRequestManagerProxy_h

commit 085aabd99798ae72683152246f66186b5bd3577e

Fix memory leak introduced in r221894. https://bugs.webkit.org/show_bug.cgi?id=176753
* UIProcess/Notifications/NotificationPermissionRequestManagerProxy.cpp:
(WebKit::NotificationPermissionRequestManagerProxy::createRequest):
The NotificationPermissionRequestManagerProxy keeps track of pending requests
so they can be denied when invalidating the page.  We add them to a HashMap and in
r221894 I removed the code that removes them from that map once the request is complete.
This code adds the completed task removal back.
The NotificationPermissionRequestManagerProxy and WebPageProxy have the same lifetime,
so protecting the WebPageProxy (m_page) makes sure there is no use-after-free when the
completion handler is called.



git-svn-id: http://svn.webkit.org/repository/webkit/trunk@221896 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Source/WebKit/UIProcess/Notifications/NotificationPermissionRequestManagerProxy.cpp
@@ -48,7 +48,8 @@ void NotificationPermissionRequestManagerProxy::invalidateRequests()
 
 Ref<NotificationPermissionRequest> NotificationPermissionRequestManagerProxy::createRequest(uint64_t notificationID)
 {
-    auto request = NotificationPermissionRequest::create([notificationID, page = makeRef(m_page)](bool allowed) {
+    auto request = NotificationPermissionRequest::create([this, notificationID, page = makeRef(m_page)](bool allowed) {
+        m_pendingRequests.take(notificationID);
         page->process().send(Messages::WebPage::DidReceiveNotificationPermissionDecision(notificationID, allowed), page->pageID());
     });
     m_pendingRequests.add(notificationID, request.ptr());