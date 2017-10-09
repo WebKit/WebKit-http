WebKit/WebKit-http

commit 581892363e0c80d1c43c8791f2483817fa2d2cb4

Web Inspector: Network Tab - Headers Detail View https://bugs.webkit.org/show_bug.cgi?id=177896 <rdar://problem/34071924>
Source/WebInspectorUI:

Reviewed by Devin Rousso.

* Localizations/en.lproj/localizedStrings.js:
* UserInterface/Main.html:
New resources and strings.

* UserInterface/Base/URLUtilities.js:
(parseURL):
(WI.h2Authority):
(WI.h2Path):
Utility methods to get the :authority and :path pseudo-headers from a URL.
This required adding user info (user:pass@) support to URL parsing.

* UserInterface/Views/NetworkTabContentView.js:
(WI.NetworkTabContentView):
* UserInterface/Views/NetworkTableContentView.js:
(WI.NetworkTableContentView.prototype.get navigationItems):
(WI.NetworkTableContentView.prototype.get filterNavigationItems):
Move the NetworkTab's filter controls to the left. Since these are not
dynamic just vend them from the TableContentView and place them in the
navigation bar.

* UserInterface/Models/Resource.js:
(WI.Resource.prototype.updateWithMetrics):
New event whenever metrics change. This is the first event that will allow
a client to react to a resource.protocol change.

* UserInterface/Views/NetworkResourceDetailView.css:
(.content-view.resource-details):
Base styles for the sub detail views.

* UserInterface/Views/NetworkResourceDetailView.js:
(WI.NetworkResourceDetailView):
(WI.NetworkResourceDetailView.prototype.headersContentViewGoToRequestData):
(WI.NetworkResourceDetailView.prototype.initialLayout):
(WI.NetworkResourceDetailView.prototype._showPreferredContentView):
(WI.NetworkResourceDetailView.prototype._showContentViewForNavigationItem):
Create a Header view and provide a way to switch to a particular view. This wil
be useful to jump from the Header's Request Data directly to the Preview's
Request ContentView.

* UserInterface/Views/ResourceDetailsSection.css:
(.resource-details > section):
(.resource-details > section > .title):
(.resource-details > section > .details):
(.resource-details > section > .details > p):

Source/WebInspectorUI/UserInterface/Views/ResourceHeadersContentView.js
@@ -1,0 +1,335 @@
+/*
+ * Copyright (C) 2017 Apple Inc. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY APPLE INC. AND ITS CONTRIBUTORS ``AS IS''
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL APPLE INC. OR ITS CONTRIBUTORS
+ * BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
+ * THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+WI.ResourceHeadersContentView = class ResourceHeadersContentView extends WI.ContentView
+{
+    constructor(resource, delegate)
+    {
+        super(null);
+
+        console.assert(resource instanceof WI.Resource);
+
+        this._resource = resource;
+        this._resource.addEventListener(WI.Resource.Event.MetricsDidChange, this._resourceMetricsDidChange, this);
+        this._resource.addEventListener(WI.Resource.Event.RequestHeadersDidChange, this._resourceRequestHeadersDidChange, this);
+        this._resource.addEventListener(WI.Resource.Event.ResponseReceived, this._resourceResponseReceived, this);
+
+        this._delegate = delegate || null;
+
+        this.element.classList.add("resource-details", "resource-headers");
+
+        this._needsSummaryRefresh = false;
+        this._needsRequestHeadersRefresh = false;
+        this._needsResponseHeadersRefresh = false;
+    }
+
+    // Protected
+
+    initialLayout()
+    {
+        super.initialLayout();
+
+        this._summarySection = new WI.ResourceDetailsSection(WI.UIString("Summary"), "summary");
+        this.element.appendChild(this._summarySection.element);
+        this._refreshSummarySection();
+
+        this._requestHeadersSection = new WI.ResourceDetailsSection(WI.UIString("Request"), "headers");
+        this.element.appendChild(this._requestHeadersSection.element);
+        this._refreshRequestHeadersSection();
+
+        // FIXME: <https://webkit.org/b/150005> Web Inspector: Redirect requests are not shown in either Network or Timeline tabs
+
+        this._responseHeadersSection = new WI.ResourceDetailsSection(WI.UIString("Response"), "headers");
+        this.element.appendChild(this._responseHeadersSection.element);
+        this._refreshResponseHeadersSection();
+
+        if (this._resource.urlComponents.queryString) {
+            this._queryStringSection = new WI.ResourceDetailsSection(WI.UIString("Query String"));
+            this.element.appendChild(this._queryStringSection.element);
+            this._refreshQueryStringSection();
+        }
+
+        if (this._resource.requestData) {
+            this._requestDataSection = new WI.ResourceDetailsSection(WI.UIString("Request Data"));
+            this.element.appendChild(this._requestDataSection.element);
+            this._refreshRequestDataSection();
+        }
+
+        this._needsSummaryRefresh = false;
+        this._needsRequestHeadersRefresh = false;
+        this._needsResponseHeadersRefresh = false;
+    }
+
+    layout()
+    {
+        super.layout();
+
+        if (this._needsSummaryRefresh) {
+            this._refreshSummarySection();
+            this._needsSummaryRefresh = false;
+        }
+
+        if (this._needsRequestHeadersRefresh) {
+            this._refreshRequestHeadersSection();
+            this._needsRequestHeadersRefresh = false;
+        }
+
+        if (this._needsResponseHeadersRefresh) {
+            this._refreshResponseHeadersSection();
+            this._needsResponseHeadersRefresh = false;
+        }
+    }
+
+    closed()
+    {
+        this._resource.removeEventListener(null, null, this);
+
+        super.closed();
+    }
+
+    // Private
+
+    _incompleteSectionWithMessage(section, message)
+    {
+        section.toggleIncomplete(true);
+
+        let p = section.detailsElement.appendChild(document.createElement("p"));
+        p.textContent = message;
+    }
+
+    _incompleteSectionWithLoadingIndicator(section)
+    {
+        section.toggleIncomplete(true);
+
+        let p = section.detailsElement.appendChild(document.createElement("p"));
+        let spinner = new WI.IndeterminateProgressSpinner;
+        p.appendChild(spinner.element);
+    }
+
+    _appendKeyValuePair(parentElement, key, value, className)
+    {
+        let p = parentElement.appendChild(document.createElement("p"));
+        p.className = "pair";
+        if (className)
+            p.classList.add(className);
+
+        // Don't include a colon if no value.
+        console.assert(typeof key === "string");
+        let displayKey = key + (!!value ? ": " : "");
+
+        let keyElement = p.appendChild(document.createElement("span"));
+        keyElement.className = "key";
+        keyElement.textContent = displayKey;
+
+        let valueElement = p.appendChild(document.createElement("span"));
+        valueElement.className = "value";
+        if (value instanceof Node)
+            valueElement.appendChild(value);
+        else
+            valueElement.textContent = value;
+    }
+
+    _responseSourceDisplayString(responseSource)
+    {
+        switch (responseSource) {
+        case WI.Resource.ResponseSource.Network:
+            return WI.UIString("Network");
+        case WI.Resource.ResponseSource.MemoryCache:
+            return WI.UIString("Memory Cache");
+        case WI.Resource.ResponseSource.DiskCache:
+            return WI.UIString("Disk Cache");
+        case WI.Resource.ResponseSource.Unknown:
+        default:
+            return null;
+        }
+    }
+
+    _refreshSummarySection()
+    {
+        let detailsElement = this._summarySection.detailsElement;
+        detailsElement.removeChildren();
+
+        this._summarySection.toggleError(this._resource.hadLoadingError());
+
+        this._appendKeyValuePair(detailsElement, WI.UIString("URL"), this._resource.url.insertWordBreakCharacters());
+
+        let status = emDash;
+        if (this._resource.hasResponse())
+            status = this._resource.statusCode + (this._resource.statusText ? " " + this._resource.statusText : "");
+        this._appendKeyValuePair(detailsElement, WI.UIString("Status"), status);
+
+        let source = this._responseSourceDisplayString(this._resource.responseSource) || emDash;
+        this._appendKeyValuePair(detailsElement, WI.UIString("Source"), source);
+    }
+
+    _refreshRequestHeadersSection()
+    {
+        let detailsElement = this._requestHeadersSection.detailsElement;
+        detailsElement.removeChildren();
+
+        // A revalidation request still sends a request even though we served from cache, so show the request.
+        if (this._resource.statusCode !== 304) {
+            if (this._resource.responseSource === WI.Resource.ResponseSource.MemoryCache) {
+                this._incompleteSectionWithMessage(this._requestHeadersSection, WI.UIString("No request, served from the memory cache."));
+                return;
+            }
+            if (this._resource.responseSource === WI.Resource.ResponseSource.DiskCache) {
+                this._incompleteSectionWithMessage(this._requestHeadersSection, WI.UIString("No request, served from the disk cache."));
+                return;
+            }
+        }
+
+        let protocol = this._resource.protocol || "";
+        let urlComponents = this._resource.urlComponents;
+        if (protocol.startsWith("http/1")) {
+            // HTTP/1.1 request line:
+            // https://www.w3.org/Protocols/rfc2616/rfc2616-sec5.html#sec5.1
+            let requestLine = `${this._resource.requestMethod} ${urlComponents.path} ${protocol.toUpperCase()}`
+            this._appendKeyValuePair(detailsElement, requestLine, null, "h1-status");
+        } else if (protocol === "h2") {
+            // HTTP/2 Request pseudo headers:
+            // https://tools.ietf.org/html/rfc7540#section-8.1.2.3
+            this._appendKeyValuePair(detailsElement, ":method", this._resource.requestMethod, "h2-pseudo-header");
+            this._appendKeyValuePair(detailsElement, ":scheme", urlComponents.scheme, "h2-pseudo-header");
+            this._appendKeyValuePair(detailsElement, ":authority", WI.h2Authority(urlComponents), "h2-pseudo-header");
+            this._appendKeyValuePair(detailsElement, ":path", WI.h2Path(urlComponents), "h2-pseudo-header");
+        }
+
+        let requestHeaders = this._resource.requestHeaders;
+        for (let key in requestHeaders)
+            this._appendKeyValuePair(detailsElement, key, requestHeaders[key], "header");
+
+        if (!detailsElement.firstChild)
+            this._incompleteSectionWithMessage(this._requestHeadersSection, WI.UIString("No request headers"));
+    }
+
+    _refreshResponseHeadersSection()
+    {
+        let detailsElement = this._responseHeadersSection.detailsElement;
+        detailsElement.removeChildren();
+
+        if (!this._resource.hasResponse()) {
+            this._incompleteSectionWithLoadingIndicator(this._responseHeadersSection);
+            return;
+        }
+
+        this._responseHeadersSection.toggleIncomplete(false);
+
+        let protocol = this._resource.protocol || "";
+        if (protocol.startsWith("http/1")) {
+            // HTTP/1.1 response status line:
+            // https://www.w3.org/Protocols/rfc2616/rfc2616-sec6.html#sec6.1
+            let responseLine = `${protocol.toUpperCase()} ${this._resource.statusCode} ${this._resource.statusText}`;
+            this._appendKeyValuePair(detailsElement, responseLine, null, "h1-status");
+        } else if (protocol === "h2") {
+            // HTTP/2 Response pseudo headers:
+            // https://tools.ietf.org/html/rfc7540#section-8.1.2.4
+            this._appendKeyValuePair(detailsElement, ":status", this._resource.statusCode, "h2-pseudo-header");
+        }
+
+        let responseHeaders = this._resource.responseHeaders;
+        for (let key in responseHeaders)
+            this._appendKeyValuePair(detailsElement, key, responseHeaders[key], "header");
+
+        if (!detailsElement.firstChild)
+            this._incompleteSectionWithMessage(this._responseHeadersSection, WI.UIString("No response headers"));
+    }
+
+    _refreshQueryStringSection()
+    {
+        if (!this._queryStringSection)
+            return;
+
+        let detailsElement = this._queryStringSection.detailsElement;
+        detailsElement.removeChildren();
+
+        let queryString = this._resource.urlComponents.queryString;
+        let queryStringPairs = parseQueryString(queryString, true);
+        for (let {name, value} of queryStringPairs)
+            this._appendKeyValuePair(detailsElement, name, value);
+    }
+
+    _refreshRequestDataSection()
+    {
+        if (!this._requestDataSection)
+            return;
+
+        let detailsElement = this._requestDataSection.detailsElement;
+        detailsElement.removeChildren();
+
+        let requestData = this._resource.requestData;
+        let requestDataContentType = this._resource.requestDataContentType || "";
+
+        if (requestDataContentType && requestDataContentType.match(/^application\/x-www-form-urlencoded\s*(;.*)?$/i)) {
+            // Simple form data that should be parsable like a query string.
+            this._appendKeyValuePair(detailsElement, WI.UIString("MIME Type"), requestDataContentType);
+            let queryStringPairs = parseQueryString(requestData, true)
+            for (let {name, value} of queryStringPairs)
+                this._appendKeyValuePair(detailsElement, name, value);
+            return;
+        }
+
+        let mimeTypeComponents = parseMIMEType(requestDataContentType);
+        let mimeType = mimeTypeComponents.type;
+        let boundary = mimeTypeComponents.boundary;
+        let encoding = mimeTypeComponents.encoding;
+
+        this._appendKeyValuePair(detailsElement, WI.UIString("MIME Type"), mimeType);
+        if (boundary)
+            this._appendKeyValuePair(detailsElement, WI.UIString("Boundary"), boundary);
+        if (encoding)
+            this._appendKeyValuePair(detailsElement, WI.UIString("Encoding"), encoding);
+
+        let goToButton = detailsElement.appendChild(WI.createGoToArrowButton());
+        goToButton.addEventListener("click", this._goToRequestDataClicked.bind(this));
+        this._appendKeyValuePair(detailsElement, WI.UIString("Request Data"), goToButton);
+    }
+
+    _resourceMetricsDidChange(event)
+    {
+        this._needsRequestHeadersRefresh = true;
+        this._needsResponseHeadersRefresh = true;
+        this.needsLayout();
+    }
+
+    _resourceRequestHeadersDidChange(event)
+    {
+        this._needsRequestHeadersRefresh = true;
+        this.needsLayout();
+    }
+
+    _resourceResponseReceived(event)
+    {
+        this._needsSummaryRefresh = true;
+        this._needsResponseHeadersRefresh = true;
+        this.needsLayout();
+    }
+
+    _goToRequestDataClicked(event)
+    {
+        if (this._delegate)
+            this._delegate.headersContentViewGoToRequestData(this);
+    }
+};

LayoutTests/inspector/unit-tests/url-utilities-expected.txt
@@ -12,6 +12,7 @@ PASS: URL constructor thinks this is invalid
 
 Test Valid: http://example.com
 PASS: scheme should be: 'http'
+PASS: userinfo should be: 'null'
 PASS: host should be: 'example.com'
 PASS: port should be: 'null'
 PASS: path should be: 'null'
@@ -21,6 +22,7 @@ PASS: lastPathComponent should be: 'null'
 
 Test Valid: http://example.com/
 PASS: scheme should be: 'http'
+PASS: userinfo should be: 'null'
 PASS: host should be: 'example.com'
 PASS: port should be: 'null'
 PASS: path should be: '/'
@@ -30,6 +32,7 @@ PASS: lastPathComponent should be: 'null'
 
 Test Valid: http://example.com:80/
 PASS: scheme should be: 'http'
+PASS: userinfo should be: 'null'
 PASS: host should be: 'example.com'
 PASS: port should be: '80'
 PASS: path should be: '/'
@@ -39,6 +42,7 @@ PASS: lastPathComponent should be: 'null'
 
 Test Valid: http://example.com/path/to/page.html
 PASS: scheme should be: 'http'
+PASS: userinfo should be: 'null'
 PASS: host should be: 'example.com'
 PASS: port should be: 'null'
 PASS: path should be: '/path/to/page.html'
@@ -48,6 +52,7 @@ PASS: lastPathComponent should be: 'page.html'
 
 Test Valid: http://example.com/path/to/page.html?
 PASS: scheme should be: 'http'
+PASS: userinfo should be: 'null'
 PASS: host should be: 'example.com'
 PASS: port should be: 'null'
 PASS: path should be: '/path/to/page.html'
@@ -57,6 +62,7 @@ PASS: lastPathComponent should be: 'page.html'
 
 Test Valid: http://example.com/path/to/page.html?a=1
 PASS: scheme should be: 'http'
+PASS: userinfo should be: 'null'
 PASS: host should be: 'example.com'
 PASS: port should be: 'null'
 PASS: path should be: '/path/to/page.html'
@@ -66,6 +72,7 @@ PASS: lastPathComponent should be: 'page.html'
 
 Test Valid: http://example.com/path/to/page.html?a=1&b=2
 PASS: scheme should be: 'http'
+PASS: userinfo should be: 'null'
 PASS: host should be: 'example.com'
 PASS: port should be: 'null'
 PASS: path should be: '/path/to/page.html'
@@ -75,6 +82,7 @@ PASS: lastPathComponent should be: 'page.html'
 
 Test Valid: http://example.com/path/to/page.html?a=1&b=2#test
 PASS: scheme should be: 'http'
+PASS: userinfo should be: 'null'
 PASS: host should be: 'example.com'
 PASS: port should be: 'null'
 PASS: path should be: '/path/to/page.html'
@@ -84,6 +92,7 @@ PASS: lastPathComponent should be: 'page.html'
 
 Test Valid: http://example.com:123/path/to/page.html?a=1&b=2#test
 PASS: scheme should be: 'http'
+PASS: userinfo should be: 'null'
 PASS: host should be: 'example.com'
 PASS: port should be: '123'
 PASS: path should be: '/path/to/page.html'
@@ -93,6 +102,7 @@ PASS: lastPathComponent should be: 'page.html'
 
 Test Valid: http://example.com/path/to/page.html#test
 PASS: scheme should be: 'http'
+PASS: userinfo should be: 'null'
 PASS: host should be: 'example.com'
 PASS: port should be: 'null'
 PASS: path should be: '/path/to/page.html'
@@ -102,6 +112,7 @@ PASS: lastPathComponent should be: 'page.html'
 
 Test Valid: http://example.com#alpha/beta
 PASS: scheme should be: 'http'
+PASS: userinfo should be: 'null'
 PASS: host should be: 'example.com'
 PASS: port should be: 'null'
 PASS: path should be: 'null'
@@ -111,6 +122,7 @@ PASS: lastPathComponent should be: 'null'
 
 Test Valid: app-specific://example.com
 PASS: scheme should be: 'app-specific'
+PASS: userinfo should be: 'null'
 PASS: host should be: 'example.com'
 PASS: port should be: 'null'
 PASS: path should be: 'null'
@@ -120,6 +132,7 @@ PASS: lastPathComponent should be: 'null'
 
 Test Valid: http://example
 PASS: scheme should be: 'http'
+PASS: userinfo should be: 'null'
 PASS: host should be: 'example'
 PASS: port should be: 'null'
 PASS: path should be: 'null'
@@ -129,6 +142,7 @@ PASS: lastPathComponent should be: 'null'
 
 Test Valid: http://my.example.com
 PASS: scheme should be: 'http'
+PASS: userinfo should be: 'null'
 PASS: host should be: 'my.example.com'
 PASS: port should be: 'null'
 PASS: path should be: 'null'
@@ -138,6 +152,7 @@ PASS: lastPathComponent should be: 'null'
 
 Test Valid: data:text/plain,test
 PASS: scheme should be: 'data'
+PASS: userinfo should be: 'null'
 PASS: host should be: 'null'
 PASS: port should be: 'null'
 PASS: path should be: 'null'
@@ -163,6 +178,7 @@ Test Valid: http:example.com/
 FAIL: scheme should be: 'http'
     Expected: "http"
     Actual: null
+PASS: userinfo should be: 'null'
 FAIL: host should be: 'example.com'
     Expected: "example.com"
     Actual: null
@@ -178,6 +194,7 @@ Test Valid: http:/example.com/
 FAIL: scheme should be: 'http'
     Expected: "http"
     Actual: null
+PASS: userinfo should be: 'null'
 FAIL: host should be: 'example.com'
     Expected: "example.com"
     Actual: null
@@ -189,10 +206,21 @@ PASS: queryString should be: 'null'
 PASS: fragment should be: 'null'
 PASS: lastPathComponent should be: 'null'
 
+Test Valid: http://user:pass@example.com/
+PASS: scheme should be: 'http'
+PASS: userinfo should be: 'user:pass'
+PASS: host should be: 'example.com'
+PASS: port should be: 'null'
+PASS: path should be: '/'
+PASS: queryString should be: 'null'
+PASS: fragment should be: 'null'
+PASS: lastPathComponent should be: 'null'
+
 Test Valid: http://user@pass:example.com/
 FAIL: scheme should be: 'http'
     Expected: "http"
     Actual: null
+PASS: userinfo should be: 'null'
 FAIL: host should be: 'example.com'
     Expected: "example.com"
     Actual: null
@@ -206,6 +234,7 @@ PASS: lastPathComponent should be: 'null'
 
 Test Valid: http://example.com?key=alpha/beta
 PASS: scheme should be: 'http'
+PASS: userinfo should be: 'null'
 FAIL: host should be: 'example.com'
     Expected: "example.com"
     Actual: "example.com?key=alpha"
@@ -305,4 +334,47 @@ PASS: mimeType should be: 'image/png'
 PASS: charset should be: 'US-ASCII'
 PASS: base64 should be: 'true'
 PASS: data should be: 'iVBORw0KGgoAAAANSUhEUgAAAAUAAAAFCAYAAACNbyblAAAAHElEQVQI12P4//8/w38GIAXDIBKE0DHxgljNBAAO9TXL0Y4OHwAAAABJRU5ErkJggg=='
+
+-- Running test case: WI.h2Authority
+PASS: HTTP/2 :authority of 'http://example.com' should be 'example.com'.
+PASS: HTTP/2 :authority of 'https://example.com' should be 'example.com'.
+PASS: HTTP/2 :authority of 'ftp://example.com' should be 'example.com'.
+PASS: HTTP/2 :authority of 'http://example.com/foo' should be 'example.com'.
+PASS: HTTP/2 :authority of 'https://example.com/foo' should be 'example.com'.
+PASS: HTTP/2 :authority of 'ftp://example.com/foo' should be 'example.com'.
+PASS: HTTP/2 :authority of 'http://example.com:123' should be 'example.com:123'.
+PASS: HTTP/2 :authority of 'https://example.com:123' should be 'example.com:123'.
+PASS: HTTP/2 :authority of 'ftp://example.com:123' should be 'example.com:123'.
+PASS: HTTP/2 :authority of 'ftp://user:pass@example.com/foo' should be 'user:pass@example.com'.
+PASS: HTTP/2 :authority of 'http://user:pass@example.com/foo' should be 'example.com'.
+PASS: HTTP/2 :authority of 'https://user:pass@example.com/foo' should be 'example.com'.
+PASS: HTTP/2 :authority of 'ftp://user:pass@example.com:123/foo' should be 'user:pass@example.com:123'.
+PASS: HTTP/2 :authority of 'http://user:pass@example.com:123/foo' should be 'example.com:123'.
+PASS: HTTP/2 :authority of 'https://user:pass@example.com:123/foo' should be 'example.com:123'.
+
+-- Running test case: WI.h2Path
+PASS: HTTP/2 :path of 'http://example.com' should be '/'.
+PASS: HTTP/2 :path of 'https://example.com' should be '/'.
+PASS: HTTP/2 :path of 'ftp://example.com' should be ''.
+PASS: HTTP/2 :path of 'http://example.com/foo' should be '/foo'.
+PASS: HTTP/2 :path of 'https://example.com/foo' should be '/foo'.
+PASS: HTTP/2 :path of 'ftp://example.com/foo' should be '/foo'.
+PASS: HTTP/2 :path of 'http://example.com/foo#hash' should be '/foo'.
+PASS: HTTP/2 :path of 'https://example.com/foo#hash' should be '/foo'.
+PASS: HTTP/2 :path of 'ftp://example.com/foo#hash' should be '/foo'.
+PASS: HTTP/2 :path of 'http://example.com/foo/bar.js' should be '/foo/bar.js'.
+PASS: HTTP/2 :path of 'https://example.com/foo/bar.js' should be '/foo/bar.js'.
+PASS: HTTP/2 :path of 'ftp://example.com/foo/bar.js' should be '/foo/bar.js'.
+PASS: HTTP/2 :path of 'http://example.com/foo/bar.js#hash' should be '/foo/bar.js'.
+PASS: HTTP/2 :path of 'https://example.com/foo/bar.js#hash' should be '/foo/bar.js'.
+PASS: HTTP/2 :path of 'ftp://example.com/foo/bar.js#hash' should be '/foo/bar.js'.
+PASS: HTTP/2 :path of 'http://example.com/?t=1' should be '/?t=1'.
+PASS: HTTP/2 :path of 'https://example.com/?t=1' should be '/?t=1'.
+PASS: HTTP/2 :path of 'ftp://example.com/?t=1' should be '/?t=1'.
+PASS: HTTP/2 :path of 'http://example.com/foo/bar.js?t=1' should be '/foo/bar.js?t=1'.
+PASS: HTTP/2 :path of 'https://example.com/foo/bar.js?t=1' should be '/foo/bar.js?t=1'.
+PASS: HTTP/2 :path of 'ftp://example.com/foo/bar.js?t=1' should be '/foo/bar.js?t=1'.
+PASS: HTTP/2 :path of 'http://example.com/foo/bar.js?t=1#hash' should be '/foo/bar.js?t=1'.
+PASS: HTTP/2 :path of 'https://example.com/foo/bar.js?t=1#hash' should be '/foo/bar.js?t=1'.
+PASS: HTTP/2 :path of 'ftp://example.com/foo/bar.js?t=1#hash' should be '/foo/bar.js?t=1'.
 

commit 93586c1002459a3ebd37e51f82c4e8d2f8c1b1f0

Unreviewed, minified WebInspector opens blank. <rdar://problem/34892307>
* UserInterface/Views/ResourceHeadersContentView.js:
(WI.ResourceHeadersContentView.prototype._refreshRequestHeadersSection):
Regression introduced in r223006. The minified build has a SyntaxError
causing WebInspector top open blank. Add the missing semicolon.

git-svn-id: http://svn.webkit.org/repository/webkit/trunk@223068 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Source/WebInspectorUI/UserInterface/Views/ResourceHeadersContentView.js
@@ -296,7 +296,7 @@ WI.ResourceHeadersContentView = class ResourceHeadersContentView extends WI.Cont
         if (protocol.startsWith("http/1")) {
             // HTTP/1.1 request line:
             // https://www.w3.org/Protocols/rfc2616/rfc2616-sec5.html#sec5.1
-            let requestLine = `${this._resource.requestMethod} ${urlComponents.path} ${protocol.toUpperCase()}`
+            let requestLine = `${this._resource.requestMethod} ${urlComponents.path} ${protocol.toUpperCase()}`;
             this._appendKeyValuePair(detailsElement, requestLine, null, "h1-status");
         } else if (protocol === "h2") {
             // HTTP/2 Request pseudo headers: