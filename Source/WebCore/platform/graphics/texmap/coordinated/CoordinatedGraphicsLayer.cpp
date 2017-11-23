fe9201adaab2af7ab0bcfd87556288c7d3d965e2
zandobersek@gmail.com <zandobersek@gmail.com@268f45cc-cd09-0410-ab3c-d52691b4dbfc>
2017-11-23 11:01:11 +0000

    [CoordGraphics] TiledBackingStore: use a reference to the client https://bugs.webkit.org/show_bug.cgi?id=179969
    Reviewed by Carlos Garcia Campos.
    
    TiledBackingStore should keep a C++ reference to the TiledBackingStoreClient
    object, and not a pointer. client() method is updated to reflect that, and
    uses of this method and the member variable are updated accordingly.
    
    No new tests -- no change in behavior.
    
    * platform/graphics/texmap/coordinated/CoordinatedGraphicsLayer.cpp:
    (WebCore::CoordinatedGraphicsLayer::createBackingStore):
    * platform/graphics/texmap/coordinated/Tile.cpp:
    (WebCore::Tile::~Tile):
    (WebCore::Tile::updateBackBuffer):
    (WebCore::Tile::paintToSurfaceContext):
    * platform/graphics/texmap/coordinated/TiledBackingStore.cpp:
    (WebCore::TiledBackingStore::TiledBackingStore):
    (WebCore::TiledBackingStore::updateTileBuffers):
    (WebCore::TiledBackingStore::createTiles):
    * platform/graphics/texmap/coordinated/TiledBackingStore.h:
    (WebCore::TiledBackingStore::client):
    
    
    git-svn-id: http://svn.webkit.org/repository/webkit/trunk@225112 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/platform/graphics/texmap/coordinated/CoordinatedGraphicsLayer.cpp
+++ Source/WebCore/platform/graphics/texmap/coordinated/CoordinatedGraphicsLayer.cpp
@@ -874,7 +874,7 @@ void CoordinatedGraphicsLayer::adjustContentsScale()
 
 void CoordinatedGraphicsLayer::createBackingStore()
 {
-    m_mainBackingStore = std::make_unique<TiledBackingStore>(this, effectiveContentsScale());
+    m_mainBackingStore = std::make_unique<TiledBackingStore>(*this, effectiveContentsScale());
     m_mainBackingStore->setSupportsAlpha(!contentsOpaque());
 }
 
