cfb0112b123fd70f592a46dc7e44a77d1a26a327
zandobersek@gmail.com <zandobersek@gmail.com@268f45cc-cd09-0410-ab3c-d52691b4dbfc>
2017-12-12 07:04:44 +0000

    [CoordGraphics] Move UpdateAtlas, AreaAllocator into the platform layer https://bugs.webkit.org/show_bug.cgi?id=180641
    Reviewed by Michael Catanzaro.
    
    Source/WebCore:
    
    Move the UpdateAtlas and AreaAllocator classes and their source files
    into the platform layer. This still means using the WebCore namespace,
    but apart from that these two classes have no dependency on anything in
    the WebKit layer, so they can be moved from there.
    
    No new tests -- no change in functionality.
    
    * platform/TextureMapper.cmake:
    * platform/graphics/texmap/coordinated/AreaAllocator.cpp: Renamed from Source/WebKit/WebProcess/WebPage/CoordinatedGraphics/AreaAllocator.cpp.
    (WebCore::AreaAllocator::AreaAllocator):
    (WebCore::AreaAllocator::~AreaAllocator):
    (WebCore::AreaAllocator::expand):
    (WebCore::AreaAllocator::expandBy):
    (WebCore::AreaAllocator::release):
    (WebCore::AreaAllocator::overhead const):
    (WebCore::AreaAllocator::roundAllocation const):
    (WebCore::GeneralAreaAllocator::GeneralAreaAllocator):
    (WebCore::GeneralAreaAllocator::~GeneralAreaAllocator):
    (WebCore::GeneralAreaAllocator::freeNode):
    (WebCore::GeneralAreaAllocator::expand):
    (WebCore::fitsWithin):
    (WebCore::GeneralAreaAllocator::allocate):
    (WebCore::GeneralAreaAllocator::allocateFromNode):
    (WebCore::GeneralAreaAllocator::splitNode):
    (WebCore::GeneralAreaAllocator::updateLargestFree):
    (WebCore::GeneralAreaAllocator::release):
    (WebCore::GeneralAreaAllocator::overhead const):
    * platform/graphics/texmap/coordinated/AreaAllocator.h: Renamed from Source/WebKit/WebProcess/WebPage/CoordinatedGraphics/AreaAllocator.h.
    (WebCore::nextPowerOfTwo):
    (WebCore::AreaAllocator::size const):
    (WebCore::AreaAllocator::minimumAllocation const):
    (WebCore::AreaAllocator::setMinimumAllocation):
    (WebCore::AreaAllocator::margin const):
    (WebCore::AreaAllocator::setMargin):
    * platform/graphics/texmap/coordinated/UpdateAtlas.cpp: Renamed from Source/WebKit/WebProcess/WebPage/CoordinatedGraphics/UpdateAtlas.cpp.
    (WebCore::UpdateAtlas::UpdateAtlas):
    (WebCore::UpdateAtlas::~UpdateAtlas):
    (WebCore::UpdateAtlas::buildLayoutIfNeeded):
    (WebCore::UpdateAtlas::didSwapBuffers):
    (WebCore::UpdateAtlas::getCoordinatedBuffer):
    * platform/graphics/texmap/coordinated/UpdateAtlas.h: Renamed from Source/WebKit/WebProcess/WebPage/CoordinatedGraphics/UpdateAtlas.h.
    (WebCore::UpdateAtlas::size const):
    (WebCore::UpdateAtlas::supportsAlpha const):
    (WebCore::UpdateAtlas::addTimeInactive):
    (WebCore::UpdateAtlas::isInactive const):
    (WebCore::UpdateAtlas::isInUse const):
    
    Source/WebKit:
    
    Move the UpdateAtlas and AreaAllocator classes into the platform layer,
    moving over build targets and adjust UpdateAtlas class references in
    CompositingCoordinator.
    
    * PlatformGTK.cmake:
    * PlatformWPE.cmake:
    * PlatformWin.cmake:
    * WebProcess/WebPage/CoordinatedGraphics/CompositingCoordinator.h:
    
    git-svn-id: http://svn.webkit.org/repository/webkit/trunk@225773 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebKit/PlatformWin.cmake
+++ Source/WebKit/PlatformWin.cmake
@@ -19,6 +19,29 @@ list(APPEND WebKit_SOURCES
 
     StorageProcess/win/StorageProcessMainWin.cpp
 
+    WebProcess/Cookies/curl/WebCookieManagerCurl.cpp
+
+    WebProcess/InjectedBundle/win/InjectedBundleWin.cpp
+
+    WebProcess/MediaCache/WebMediaKeyStorageManager.cpp
+
+    WebProcess/Plugins/Netscape/win/PluginProxyWin.cpp
+
+    WebProcess/WebCoreSupport/win/WebContextMenuClientWin.cpp
+    WebProcess/WebCoreSupport/win/WebFrameNetworkingContext.cpp
+    WebProcess/WebCoreSupport/win/WebPopupMenuWin.cpp
+
+    WebProcess/WebPage/AcceleratedDrawingArea.cpp
+    WebProcess/WebPage/AcceleratedSurface.cpp
+    WebProcess/WebPage/DrawingAreaImpl.cpp
+
+    WebProcess/WebPage/CoordinatedGraphics/CompositingCoordinator.cpp
+    WebProcess/WebPage/CoordinatedGraphics/CoordinatedLayerTreeHost.cpp
+    WebProcess/WebPage/CoordinatedGraphics/ThreadedCoordinatedLayerTreeHost.cpp
+
+    WebProcess/WebPage/win/WebInspectorUIWin.cpp
+    WebProcess/WebPage/win/WebPageWin.cpp
+
     WebProcess/win/WebProcessMainWin.cpp
 )
 
