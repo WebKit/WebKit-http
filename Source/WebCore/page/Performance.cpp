d0a31ac534449dbe09cc4cc0d6c0ff377c47981a
rniwa@webkit.org <rniwa@webkit.org@268f45cc-cd09-0410-ab3c-d52691b4dbfc>
2018-01-08 01:22:20 +0000

    Reduce the precision of "high" resolution time to 1ms https://bugs.webkit.org/show_bug.cgi?id=180910 <rdar://problem/36085943>
    Reviewed by Saam Barati.
    
    Source/WebCore:
    
    Reduced the high prevision time's resolution to 1ms, the same precision as Date.now().
    
    Also fixed the bug in fillRTCStats that we weren't reducing the time resolution in RTCStats dictionaries.
    
    * Modules/mediastream/libwebrtc/LibWebRTCMediaEndpoint.cpp:
    (WebCore::fillRTCStats):
    * page/Performance.cpp:
    (WebCore::Performance::reduceTimeResolution):
    
    LayoutTests:
    
    Add a flaky test expectation to imported/w3c/web-platform-tests/workers/worker-performance.worker.html
    due to a test bug. The test fix is tracked in https://github.com/w3c/web-platform-tests/pull/8711
    
    * TestExpectations:
    
    git-svn-id: http://svn.webkit.org/repository/webkit/trunk@226495 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebCore/page/Performance.cpp
+++ Source/WebCore/page/Performance.cpp
@@ -78,7 +78,7 @@ DOMHighResTimeStamp Performance::now() const
 
 Seconds Performance::reduceTimeResolution(Seconds seconds)
 {
-    double resolution = (100_us).seconds();
+    double resolution = (1000_us).seconds();
     double reduced = std::floor(seconds.seconds() / resolution) * resolution;
     return Seconds(reduced);
 }
