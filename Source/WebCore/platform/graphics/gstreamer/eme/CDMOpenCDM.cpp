f47a23b28457e114e99fd2c7fb63444608847429
Xabier Rodriguez Calvar <calvaris@igalia.com>
2018-01-22 13:52:59 +0100

    [EME] Remove Widevine support

--- Source/WebCore/platform/graphics/gstreamer/eme/CDMOpenCDM.cpp
+++ Source/WebCore/platform/graphics/gstreamer/eme/CDMOpenCDM.cpp
@@ -218,7 +218,7 @@ std::unique_ptr<CDMPrivate> CDMFactoryOpenCDM::createCDM(const String& keySystem
 
 bool CDMFactoryOpenCDM::supportsKeySystem(const String& keySystem)
 {
-    return GStreamerEMEUtilities::isPlayReadyKeySystem(keySystem) || GStreamerEMEUtilities::isWidevineKeySystem(keySystem);
+    return GStreamerEMEUtilities::isPlayReadyKeySystem(keySystem);
 }
 
 CDMInstanceOpenCDM::CDMInstanceOpenCDM(std::unique_ptr<media::OpenCdm> backend, const String& keySystem)
