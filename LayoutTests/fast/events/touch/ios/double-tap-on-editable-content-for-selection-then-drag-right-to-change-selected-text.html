WebKit/WebKit-http

commit 81ea0084e80a2062aa0a2ad0ba18601216ba6d8a

Don't activate Selection Assistant unless it is actually needed. https://bugs.webkit.org/show_bug.cgi?id=184944 <rdar://problem/39469671>
Reviewed by Tim Horton.
        
Source/WebKit:

Don't activate the selection unless we need to. Activating on init is overeager and allowing Javascript to 
activate selections without user input.

* UIProcess/ios/WKContentViewInteraction.mm:
(-[WKContentView useSelectionAssistantWithGranularity:]):

LayoutTests:

Turning on previously flakey tests that are fixed with this change.

* platform/ios-wk2/TestExpectations:



git-svn-id: http://svn.webkit.org/repository/webkit/trunk@231016 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Source/WebKit/UIProcess/ios/WKContentViewInteraction.mm
@@ -1932,9 +1932,6 @@ static void cancelPotentialTapIfNecessary(WKContentView* contentView)
         // Reset the gesture recognizers in case editibility has changed.
         [_textSelectionAssistant setGestureRecognizers];
     }
-
-    if (self.isFirstResponder && !self.suppressAssistantSelectionView)
-        [_textSelectionAssistant activateSelection];
 }
 
 - (void)clearSelection

LayoutTests/platform/ios-wk2/TestExpectations
@@ -1349,21 +1349,4 @@ webkit.org/b/183860 [ Release ] http/wpt/service-workers/third-party-registratio
 
 webkit.org/b/184245 http/tests/workers/service/service-worker-cache-api.https.html [ Pass Failure ]
 
-#FIXME: <rdar://problem/39469671> Tests that fail when run after fast/events/ios/double-tap-zoom.html with UIWKTextInteractionAssistant turned on
-fast/flexbox/flexbox-fail-to-select-same-line.html [ ImageOnlyFailure Pass ]
-fast/html/marquee-child-wrap.html [ ImageOnlyFailure Pass ]
-fast/misc/subtree-layouts.html [ ImageOnlyFailure Pass ]
-fast/multicol/simple-line-layout-widows-when-switching-over-to-normal-line-layout.html [ ImageOnlyFailure Pass ]
-fast/text/international/hebrew-selection.html [ ImageOnlyFailure Pass ]
-imported/blink/editing/selection/deleteFromDocument-crash.html [ ImageOnlyFailure Pass ]
-imported/blink/editing/style/justify-left-crash.html [ ImageOnlyFailure Pass ]
-imported/blink/fast/css/user-select-none.html [ ImageOnlyFailure Pass ]
-imported/blink/fast/text/international/repaint-glyph-bounds.html [ ImageOnlyFailure Pass ]
-imported/mozilla/svg/text/multiple-chunks-selection.svg [ ImageOnlyFailure Pass ]
-imported/mozilla/svg/text/selectSubString-2.svg [ ImageOnlyFailure Pass ]
-imported/mozilla/svg/text/selectSubString-3.svg [ ImageOnlyFailure Pass ]
-imported/mozilla/svg/text/selectSubString.svg [ ImageOnlyFailure Pass ]
-imported/mozilla/svg/text/textpath-selection.svg [ ImageOnlyFailure Pass ]
-svg/foreignObject/viewport-foreignobject-crash.html [ ImageOnlyFailure Pass ]
-
 webkit.org/b/184783 compositing/ios/overflow-scroll-touch-tiles.html [ Pass Failure ]

commit 1d4524c14e9394724df6f2d7d961d10b60388bd5

Activate selection when interacting with editable content https://bugs.webkit.org/show_bug.cgi?id=185017
Reviewed by Tim Horton.
        
Source/WebKit:

Fixes a regression from r231016 where selection now does not work when interacting with
editable content. When we go into editable content, we should turn on the assistant.
This fulfills the requirement of user interaction as well, so any javascript selections
after this point should be valid.

* UIProcess/ios/WKContentViewInteraction.mm:
(-[WKContentView _startAssistingKeyboard]):
(-[WKContentView _stopAssistingKeyboard]):

LayoutTests:

Fixed double-tap-on-editable-and-noneditable test, as it was not actually written correctly.
Should actually double tap on correct content and check for the correct output now.
Added two more tests to make sure that selections really are happening in editable content. 

* fast/events/touch/ios/double-tap-on-editable-and-noneditable.html:
* fast/events/touch/ios/double-tap-on-editable-content-for-selection-expected.txt: Added.
* fast/events/touch/ios/double-tap-on-editable-content-for-selection-then-drag-right-to-change-selected-text-expected.txt: Added.
* fast/events/touch/ios/double-tap-on-editable-content-for-selection-then-drag-right-to-change-selected-text.html: Added.
* fast/events/touch/ios/double-tap-on-editable-content-for-selection.html: Added.



git-svn-id: http://svn.webkit.org/repository/webkit/trunk@231054 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Source/WebKit/UIProcess/ios/WKContentViewInteraction.mm
@@ -3967,6 +3967,9 @@ static NSString *contentTypeFromFieldName(WebCore::AutofillFieldName fieldName)
 - (void)_startAssistingKeyboard
 {
     [self useSelectionAssistantWithGranularity:WKSelectionGranularityCharacter];
+    
+    if (self.isFirstResponder && !self.suppressAssistantSelectionView)
+        [_textSelectionAssistant activateSelection];
 
 #if !ENABLE(EXTRA_ZOOM_MODE)
     [self reloadInputViews];
@@ -3976,6 +3979,8 @@ static NSString *contentTypeFromFieldName(WebCore::AutofillFieldName fieldName)
 - (void)_stopAssistingKeyboard
 {
     [self useSelectionAssistantWithGranularity:_webView._selectionGranularity];
+    
+    [_textSelectionAssistant deactivateSelection];
 }
 
 - (const AssistedNodeInformation&)assistedNodeInformation

LayoutTests/fast/events/touch/ios/double-tap-on-editable-content-for-selection-expected.txt
@@ -1,0 +1,3 @@
+PASS: Has Caret Selection
+PASS: Correct Selection
+

LayoutTests/fast/events/touch/ios/double-tap-on-editable-content-for-selection-then-drag-right-to-change-selected-text-expected.txt
@@ -1,0 +1,8 @@
+PASS: Has Caret Selection
+PASS: Correct Selection
+PASS: Correct Selection
+PASS: Correct Selection
+PASS: Correct Selection
+PASS: Correct Selection
+PASS: Correct Selection
+

LayoutTests/fast/events/touch/ios/double-tap-on-editable-content-for-selection-then-drag-right-to-change-selected-text.html
@@ -1,0 +1,132 @@
+<!DOCTYPE html>
+
+<html>
+<head>
+    <script src="../../../../resources/basic-gestures.js"></script>
+    <script>
+        if (window.testRunner) {
+            testRunner.dumpAsText();
+            testRunner.waitUntilDone();
+        }
+    
+        async function runTest()
+        {
+            if (!testRunner.runUIScript)
+                return;
+
+            var output = '';
+            var editableTargetRect = document.getElementById('editable').getBoundingClientRect();
+            var firstTargetRect = document.getElementById('firstSelection').getBoundingClientRect();
+            var secondTargetRect = document.getElementById('secondSelection').getBoundingClientRect();
+            var thirdTargetRect = document.getElementById('thirdSelection').getBoundingClientRect();
+            var noneditableToEditableOffset = document.getElementById('editable').getBoundingClientRect().y - document.getElementById('noneditable').getBoundingClientRect().y;
+
+            var tapPointX = editableTargetRect.x + editableTargetRect.width / 2;
+            var tapPointY = editableTargetRect.y + editableTargetRect.height / 2;
+            var doubleTapPointX = firstTargetRect.x + firstTargetRect.width / 2;
+            var doubleTapPointY = firstTargetRect.y + firstTargetRect.height / 2 + noneditableToEditableOffset;
+            var dragY = firstTargetRect.y + firstTargetRect.height + noneditableToEditableOffset;
+            var dragX = firstTargetRect.x + firstTargetRect.width;
+            var dragX2 = secondTargetRect.x;
+            var dragX3 = thirdTargetRect.x;
+            var dragX4 = firstTargetRect.x - firstTargetRect.width;
+        
+            await tapAtPoint(tapPointX, tapPointY);
+            if (document.getSelection().type == "Caret")
+                output += 'PASS: Has Caret Selection';
+            else
+                output += 'FAIL: failed to activate caret as a result of a tap. Incorrect Selection: ' + document.getSelection().toString();
+            output += '<br>';
+
+            await didShowKeyboard();
+
+            await doubleTapAtPoint(doubleTapPointX, doubleTapPointY);
+            if (document.getSelection().toString() == "magna")
+                output += 'PASS: Correct Selection';
+            else
+                output += 'FAIL: failed to select a word as a result of a long press. Incorrect Selection: ' + document.getSelection().toString();
+            output += '<br>';
+
+            await touchAndDragFromPointToPoint(dragX, dragY, dragX2, dragY);
+            if (document.getSelection().toString() == "magna aliqua.")
+                output += 'PASS: Correct Selection';
+            else
+                output += 'FAIL: failed to select additional word after a drag. Incorrect Selection: ' + document.getSelection().toString();
+            output += '<br>';
+
+            await touchAndDragFromPointToPoint(dragX2, dragY, dragX3, dragY);
+            if (document.getSelection().toString() == "magna aliqua. Ut")
+                output += 'PASS: Correct Selection';
+            else
+                output += 'FAIL: failed to select additional word after a drag. Incorrect Selection: ' + document.getSelection().toString();
+            output += '<br>';
+
+            await touchAndDragFromPointToPoint(dragX3, dragY, dragX2, dragY);
+            if (document.getSelection().toString() == "magna aliqua.")
+                output += 'PASS: Correct Selection';
+            else
+                output += 'FAIL: failed to deselect word after a drag. Incorrect Selection: ' + document.getSelection().toString();
+            output += '<br>';
+
+            await touchAndDragFromPointToPoint(dragX2, dragY, dragX, dragY);
+            if (document.getSelection().toString() == "magna")
+                output += 'PASS: Correct Selection';
+            else
+                output += 'FAIL: failed to deselect word after a drag. Incorrect Selection: ' + document.getSelection().toString();
+            output += '<br>';
+
+            var result = await touchAndDragFromPointToPoint(dragX, dragY, dragX4, dragY);
+            if (document.getSelection().toString() == "m")
+                output += 'PASS: Correct Selection';
+            else
+                output += 'FAIL: failed to reduce selection to a single character by dragging left. Incorrect Selection: ' + document.getSelection().toString();
+            output += '<br>';
+            output += result;
+
+            var noneditableElement = document.getElementById('noneditable');
+            noneditableElement.parentNode.removeChild(noneditableElement);
+            var editableElement = document.getElementById('editable');
+            editableElement.parentNode.removeChild(editableElement);
+            document.getElementById('target').innerHTML = output;
+            testRunner.notifyDone();
+        }
+
+        window.addEventListener('load', runTest, false);
+    </script>
+        <style>
+        #noneditable {
+            height: 200px;
+            width: 300px;
+            background-color: silver;
+            font-family: monospace;
+            font-size: 18px;
+        }
+        #editable {
+            height: 200px;
+            width: 300px;
+            background-color: silver;
+            font-family: monospace;
+            font-size: 18px;
+        }
+        #target {
+            height: 50px;
+            width: 300px;
+            background-color: silver;
+            font-family: monospace;
+            font-size: 18px;
+        }
+    </style>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+<body>
+    <div id="noneditable">
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore <span id="firstSelection">magna</span> aliqua.<span id="secondSelection"></span> Ut<span id="thirdSelection"></span> enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+    </div>
+    <div id="editable" contenteditable>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+    </div> 
+    <div id="target">
+        This test requires UIScriptController to run.
+    </div>
+</body>
+</html>

LayoutTests/fast/events/touch/ios/double-tap-on-editable-content-for-selection.html
@@ -15,28 +15,29 @@
                 return;
 
             var output = '';
-            var noneditableTargetRect = document.getElementById('noneditableTapPoint').getBoundingClientRect();
-            
-            var noneditableTapPointX = noneditableTargetRect.x + noneditableTargetRect.width / 2;
-            var noneditableTapPointY = noneditableTargetRect.y + noneditableTargetRect.height / 2;
-            
-            await doubleTapAtPoint(noneditableTapPointX, noneditableTapPointY);
-            if (document.getSelection().type == "None")
-                output += 'PASS: No Selection';
+            var editableTargetRect = document.getElementById('editable').getBoundingClientRect();
+            var firstTargetRect = document.getElementById('firstSelection').getBoundingClientRect();
+            var noneditableToEditableOffset = document.getElementById('editable').getBoundingClientRect().y - document.getElementById('noneditable').getBoundingClientRect().y;
+
+            var tapPointX = editableTargetRect.x + editableTargetRect.width / 2;
+            var tapPointY = editableTargetRect.y + editableTargetRect.height / 2;
+            var doubleTapPointX = firstTargetRect.x + firstTargetRect.width / 2;
+            var doubleTapPointY = firstTargetRect.y + firstTargetRect.height / 2 + noneditableToEditableOffset;
+        
+            await tapAtPoint(tapPointX, tapPointY);
+            if (document.getSelection().type == "Caret")
+                output += 'PASS: Has Caret Selection';
             else
-                output += 'FAIL: Has selection even though we should not. Incorrect Selection: ' + document.getSelection().toString();
+                output += 'FAIL: failed to activate caret as a result of a tap. Incorrect Selection: ' + document.getSelection().toString();
             output += '<br>';
 
-            var editableTargetRect = document.getElementById('editable').getBoundingClientRect();
+            await didShowKeyboard();
 
-            var editableTapPointX = editableTargetRect.x + editableTargetRect.width / 2;
-            var editableTapPointY = editableTargetRect.y + editableTargetRect.height / 2;
-
-            await doubleTapAtPoint(editableTapPointX, editableTapPointY);
-            if (document.getSelection().type != "None")
-                output += 'PASS: Has Selection';
+            await doubleTapAtPoint(doubleTapPointX, doubleTapPointY);
+            if (document.getSelection().toString() == "magna")
+                output += 'PASS: Correct Selection';
             else
-                output += 'FAIL: No selection even though we should have one.';
+                output += 'FAIL: failed to select a word as a result of a long press. Incorrect Selection: ' + document.getSelection().toString();
             output += '<br>';
 
             var noneditableElement = document.getElementById('noneditable');
@@ -76,7 +77,7 @@
 </head>
 <body>
     <div id="noneditable">
-        <p>Lorem ipsum dolor sit amet, <span id="noneditableTapPoint">consectetur</span> adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore <span id="firstSelection">magna</span> aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
     </div>
     <div id="editable" contenteditable>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>