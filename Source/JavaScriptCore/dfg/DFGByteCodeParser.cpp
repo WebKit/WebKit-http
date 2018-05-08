6501be0f82559d90bde8e8743e1b30f0324f1193
utatane.tea@gmail.com <utatane.tea@gmail.com@268f45cc-cd09-0410-ab3c-d52691b4dbfc>
2018-05-08 01:13:03 +0000

    [DFG][MIPS] Simplify DFG code by increasing MIPS temporary registers https://bugs.webkit.org/show_bug.cgi?id=185371
    Reviewed by Mark Lam.
    
    Since MIPS GPRInfo claims it has only 7 registers, some of DFG code exhausts registers.
    As a result, we need to maintain separated code for MIPS. This increases DFG maintenance burden,
    but actually MIPS have much more registers.
    
    This patch adds $a0 - $a3 to temporary registers. This is OK since our temporary registers can be overlapped with
    argument registers (see ARM, X86 implementations). These registers are caller-save ones, so we do not need to
    have extra mechanism.
    
    Then, we remove several unnecessary MIPS code in our JIT infrastructure.
    
    * dfg/DFGByteCodeParser.cpp:
    (JSC::DFG::ByteCodeParser::handleIntrinsicCall):
    * dfg/DFGFixupPhase.cpp:
    (JSC::DFG::FixupPhase::fixupNode):
    * dfg/DFGSpeculativeJIT32_64.cpp:
    (JSC::DFG::SpeculativeJIT::compile):
    * jit/CCallHelpers.h:
    * jit/GPRInfo.h:
    (JSC::GPRInfo::toRegister):
    (JSC::GPRInfo::toIndex):
    * offlineasm/mips.rb:
    
    git-svn-id: http://svn.webkit.org/repository/webkit/trunk@231472 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/JavaScriptCore/dfg/DFGByteCodeParser.cpp
+++ Source/JavaScriptCore/dfg/DFGByteCodeParser.cpp
@@ -2359,7 +2359,7 @@ bool ByteCodeParser::handleIntrinsicCall(Node* callee, int resultOperand, Intrin
 
     case ArraySliceIntrinsic: {
 #if USE(JSVALUE32_64)
-        if (isX86() || isMIPS()) {
+        if (isX86()) {
             // There aren't enough registers for this to be done easily.
             return false;
         }
