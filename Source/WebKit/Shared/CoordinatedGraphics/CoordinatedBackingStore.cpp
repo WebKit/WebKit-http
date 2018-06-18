7c24fe6dc52011dc843a5b772477998eca0b2a4f
commit-queue@webkit.org <commit-queue@webkit.org@268f45cc-cd09-0410-ab3c-d52691b4dbfc>
2018-06-18 15:40:33 +0000

    [Threaded paintingEngine] Fix rendering glitches https://bugs.webkit.org/show_bug.cgi?id=186764
    Patch by Karl Leplat <karl.leplat_ext@softathome.com> on 2018-06-18
    Reviewed by Žan Doberšek.
    
    Source/WebCore:
    
    * platform/graphics/nicosia/NicosiaPaintingEngineBasic.cpp:
    (Nicosia::PaintingEngineBasic::paint):
    To keep backwards compatibility with no multi-threaded,
    we call Nicosia::Buffer functions beginPainting() and completePainting().
    
    Source/WebKit:
    
    * Shared/CoordinatedGraphics/CoordinatedBackingStore.cpp:
    (WebKit::CoordinatedBackingStoreTile::swapBuffers):
    We call Nicosia::Buffer function waitUntilPaintingComplete()
    in order to synchronize of using Nicosia:buffer between MainThread
    and ThreadedCompositor.
    
    git-svn-id: http://svn.webkit.org/repository/webkit/trunk@232931 268f45cc-cd09-0410-ab3c-d52691b4dbfc

--- Source/WebKit/Shared/CoordinatedGraphics/CoordinatedBackingStore.cpp
+++ Source/WebKit/Shared/CoordinatedGraphics/CoordinatedBackingStore.cpp
@@ -48,6 +48,7 @@ void CoordinatedBackingStoreTile::swapBuffers(TextureMapper& textureMapper)
     } else if (m_buffer->supportsAlpha() == m_texture->isOpaque())
         m_texture->reset(m_tileRect.size(), m_buffer->supportsAlpha());
 
+    m_buffer->waitUntilPaintingComplete();
     m_texture->updateContents(m_buffer->data(), m_sourceRect, m_bufferOffset, m_buffer->stride(), BitmapTexture::UpdateCanModifyOriginalImageData);
     m_buffer = nullptr;
 }
