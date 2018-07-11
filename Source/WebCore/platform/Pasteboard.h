fberlakovich/r2c-webkit

Add additional WEBCORE_EXPORTs https://bugs.webkit.org/show_bug.cgi?id=181414
Reviewed by Alex Christensen.

No new tests. No change in behavior.

* dom/Document.h:
* dom/ViewportArguments.h:
* page/DOMWindow.h:
* page/FrameView.h:
* page/PageOverlayController.h:
* platform/ContextMenuItem.h:
* platform/Pasteboard.h:
* platform/SharedBuffer.h:
* platform/UserAgent.h:
* platform/graphics/GLContext.h:
* platform/graphics/GraphicsContext.h:
* platform/graphics/GraphicsLayerTransform.h:
* platform/graphics/PlatformDisplay.h:
* platform/graphics/cairo/BackingStoreBackendCairoImpl.h:
* platform/graphics/cairo/PlatformContextCairo.h:
* platform/graphics/cairo/RefPtrCairo.h:
* platform/graphics/nicosia/NicosiaBuffer.h:
* platform/graphics/nicosia/NicosiaPaintingEngine.h:
* platform/graphics/texmap/TextureMapper.h:
* platform/graphics/texmap/TextureMapperAnimation.h:
* platform/graphics/texmap/TextureMapperBackingStore.h:
* platform/graphics/texmap/TextureMapperFPSCounter.h:
* platform/graphics/texmap/TextureMapperLayer.h:
(WebCore::TextureMapperLayer::TextureMapperLayer): Deleted.
(WebCore::TextureMapperLayer::setID): Deleted.
(WebCore::TextureMapperLayer::id): Deleted.
(WebCore::TextureMapperLayer:: const): Deleted.
(WebCore::TextureMapperLayer::setScrollClient): Deleted.
(WebCore::TextureMapperLayer::setIsScrollable): Deleted.
(WebCore::TextureMapperLayer::isScrollable const): Deleted.
(WebCore::TextureMapperLayer::textureMapper const): Deleted.
(WebCore::TextureMapperLayer::setTextureMapper): Deleted.
(WebCore::TextureMapperLayer::drawsContent const): Deleted.
(WebCore::TextureMapperLayer::contentsAreVisible const): Deleted.
(WebCore::TextureMapperLayer::size const): Deleted.
(WebCore::TextureMapperLayer::opacity const): Deleted.
(WebCore::TextureMapperLayer::transform const): Deleted.
(WebCore::TextureMapperLayer::hasFilters const): Deleted.
(WebCore::TextureMapperLayer::isShowingRepaintCounter const): Deleted.
(WebCore::TextureMapperLayer::fixedToViewport const): Delet

Source/WebCore/platform/Pasteboard.h
@@ -197,32 +197,32 @@ public:
 
     virtual bool isStatic() const { return false; }
 
-    virtual bool hasData();
-    virtual Vector<String> typesSafeForBindings(const String& origin);
-    virtual Vector<String> typesForLegacyUnsafeBindings();
-    virtual String readOrigin();
-    virtual String readString(const String& type);
-    virtual String readStringInCustomData(const String& type);
+    virtual WEBCORE_EXPORT bool hasData();
+    virtual WEBCORE_EXPORT Vector<String> typesSafeForBindings(const String& origin);
+    virtual WEBCORE_EXPORT Vector<String> typesForLegacyUnsafeBindings();
+    virtual WEBCORE_EXPORT String readOrigin();
+    virtual WEBCORE_EXPORT String readString(const String& type);
+    virtual WEBCORE_EXPORT String readStringInCustomData(const String& type);
 
-    virtual void writeString(const String& type, const String& data);
-    virtual void clear();
-    virtual void clear(const String& type);
+    virtual WEBCORE_EXPORT void writeString(const String& type, const String& data);
+    virtual WEBCORE_EXPORT void clear();
+    virtual WEBCORE_EXPORT void clear(const String& type);
 
-    virtual void read(PasteboardPlainText&);
-    virtual void read(PasteboardWebContentReader&);
-    virtual void read(PasteboardFileReader&);
+    virtual WEBCORE_EXPORT void read(PasteboardPlainText&);
+    virtual WEBCORE_EXPORT void read(PasteboardWebContentReader&);
+    virtual WEBCORE_EXPORT void read(PasteboardFileReader&);
 
-    virtual void write(const PasteboardURL&);
-    virtual void writeTrustworthyWebURLsPboardType(const PasteboardURL&);
-    virtual void write(const PasteboardImage&);
-    virtual void write(const PasteboardWebContent&);
+    virtual WEBCORE_EXPORT void write(const PasteboardURL&);
+    virtual WEBCORE_EXPORT void writeTrustworthyWebURLsPboardType(const PasteboardURL&);
+    virtual WEBCORE_EXPORT void write(const PasteboardImage&);
+    virtual WEBCORE_EXPORT void write(const PasteboardWebContent&);
 
-    virtual void writeCustomData(const PasteboardCustomData&);
+    virtual WEBCORE_EXPORT void writeCustomData(const PasteboardCustomData&);
 
-    virtual bool containsFiles();
-    virtual bool canSmartReplace();
+    virtual WEBCORE_EXPORT bool containsFiles();
+    virtual WEBCORE_EXPORT bool canSmartReplace();
 
-    virtual void writeMarkup(const String& markup);
+    virtual WEBCORE_EXPORT void writeMarkup(const String& markup);
     enum SmartReplaceOption { CanSmartReplace, CannotSmartReplace };
     virtual WEBCORE_EXPORT void writePlainText(const String&, SmartReplaceOption); // FIXME: Two separate functions would be clearer than one function with an argument.
 

WebKit/WebKit-http

git-svn-id: http://svn.webkit.org/repository/webkit/trunk@226657 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Source/WebCore/platform/Pasteboard.h
@@ -166,26 +166,26 @@ public:
 
     virtual bool isStatic() const { return false; }
 
-    virtual bool hasData();
-    virtual Vector<String> types();
-    virtual String readString(const String& type);
+    virtual WEBCORE_EXPORT bool hasData();
+    virtual WEBCORE_EXPORT Vector<String> types();
+    virtual WEBCORE_EXPORT String readString(const String& type);
 
-    virtual void writeString(const String& type, const String& data);
-    virtual void clear();
-    virtual void clear(const String& type);
+    virtual WEBCORE_EXPORT void writeString(const String& type, const String& data);
+    virtual WEBCORE_EXPORT void clear();
+    virtual WEBCORE_EXPORT void clear(const String& type);
 
-    virtual void read(PasteboardPlainText&);
-    virtual void read(PasteboardWebContentReader&);
+    virtual WEBCORE_EXPORT void read(PasteboardPlainText&);
+    virtual WEBCORE_EXPORT void read(PasteboardWebContentReader&);
 
-    virtual void write(const PasteboardURL&);
-    virtual void writeTrustworthyWebURLsPboardType(const PasteboardURL&);
-    virtual void write(const PasteboardImage&);
-    virtual void write(const PasteboardWebContent&);
+    virtual WEBCORE_EXPORT void write(const PasteboardURL&);
+    virtual WEBCORE_EXPORT void writeTrustworthyWebURLsPboardType(const PasteboardURL&);
+    virtual WEBCORE_EXPORT void write(const PasteboardImage&);
+    virtual WEBCORE_EXPORT void write(const PasteboardWebContent&);
 
-    virtual Vector<String> readFilenames();
-    virtual bool canSmartReplace();
+    virtual WEBCORE_EXPORT Vector<String> readFilenames();
+    virtual WEBCORE_EXPORT bool canSmartReplace();
 
-    virtual void writeMarkup(const String& markup);
+    virtual WEBCORE_EXPORT void writeMarkup(const String& markup);
     enum SmartReplaceOption { CanSmartReplace, CannotSmartReplace };
     virtual WEBCORE_EXPORT void writePlainText(const String&, SmartReplaceOption); // FIXME: Two separate functions would be clearer than one function with an argument.
 