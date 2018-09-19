WebKit/WebKit-http

commit c25368b5e4830c90fa1ca03bae156a0041752ea3

[JSC] alignas for RegisterState should respect alignof(RegisterState) too https://bugs.webkit.org/show_bug.cgi?id=188686
Reviewed by Saam Barati.

RegisterState would have larger alignment than `alignof(void*)`. We use the larger alignment value
for `alignof` for RegisterState.

* heap/RegisterState.h:

git-svn-id: http://svn.webkit.org/repository/webkit/trunk@234975 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Source/JavaScriptCore/heap/RegisterState.h
@@ -164,14 +164,13 @@ using RegisterState = jmp_buf;
 #if COMPILER(MSVC)
 #pragma warning(push)
 #pragma warning(disable: 4611)
+#endif
 #define ALLOCATE_AND_GET_REGISTER_STATE(registers) \
-    alignas(void*) RegisterState registers; \
+    alignas(std::max(alignof(void*), alignof(RegisterState))) RegisterState registers; \
     setjmp(registers)
+
+#if COMPILER(MSVC)
 #pragma warning(pop)
-#else
-#define ALLOCATE_AND_GET_REGISTER_STATE(registers) \
-    alignas(void*) RegisterState registers; \
-    setjmp(registers)
 #endif
 #endif // ALLOCATE_AND_GET_REGISTER_STATE
 

commit 5d887d0475b4e1d061e69f6cd8fc94ad842d07b2

Merge r235740 - [JSC] Build broken after r234975 on s390x, ppc64le, armv7hl https://bugs.webkit.org/show_bug.cgi?id=189078
Reviewed by Mark Lam.

Caused by the GCC bug - https://gcc.gnu.org/bugzilla/show_bug.cgi?id=70124.
Using the ternary operator instead of std::max() fixes it.

* heap/RegisterState.h:

git-svn-id: http://svn.webkit.org/repository/webkit/releases/WebKitGTK/webkit-2.22@236172 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Source/JavaScriptCore/heap/RegisterState.h
@@ -166,7 +166,7 @@ using RegisterState = jmp_buf;
 #pragma warning(disable: 4611)
 #endif
 #define ALLOCATE_AND_GET_REGISTER_STATE(registers) \
-    alignas(std::max(alignof(void*), alignof(RegisterState))) RegisterState registers; \
+    alignas(alignof(void*) > alignof(RegisterState) ? alignof(void*) : alignof(RegisterState)) RegisterState registers; \
     setjmp(registers)
 
 #if COMPILER(MSVC)