9105e9758fd174acb1cdcc9325ed88667e48144e
commit-queue@webkit.org <commit-queue@webkit.org@268f45cc-cd09-0410-ab3c-d52691b4dbfc>
2018-09-20 11:22:00 +0000

    [EME] Add WebM sanitization https://bugs.webkit.org/show_bug.cgi?id=189740
    Patch by Yacine Bandou <yacine.bandou@softathome.com> on 2018-09-20
    Reviewed by Xabier Rodriguez-Calvar.
    
    This patch adds support for sanitizing the WebM initialization data,
    ensures there are no bogus values.
    See https://www.w3.org/TR/encrypted-media/#dom-mediakeysession-generaterequest.
    
    Tests: imported/w3c/web-platform-tests/encrypted-media/clearkey-generate-request-disallowed-input.https.html
    
    * Modules/encryptedmedia/InitDataRegistry.cpp:
    (WebCore::sanitizeWebM): Added implementation, check if the initialization data doesn't empty and its size
    should be less than 64KB, return the buffer copy if it is ok, otherwise a nullptr.
    (WebCore::extractKeyIDsWebM): Added implementation.
    
    git-svn-id: http://svn.webkit.org/repository/webkit/trunk@236256 268f45cc-cd09-0410-ab3c-d52691b4dbfc
    Signed-off-by: Xabier Rodriguez Calvar <calvaris@igalia.com>

--- Source/WebCore/Modules/encryptedmedia/InitDataRegistry.cpp
+++ Source/WebCore/Modules/encryptedmedia/InitDataRegistry.cpp
@@ -44,7 +44,10 @@ namespace {
     constexpr uint8_t playreadyXMLMagicOffset = 10;
     // L("<WRMHEADER")
     const uint8_t playreadyXMLMagic[] = { 0x3c, 0x00, 0x57, 0x00, 0x52, 0x00, 0x4d, 0x00, 0x48, 0x00, 0x45, 0x00, 0x41, 0x00, 0x44, 0x00, 0x45, 0x00, 0x52, 0x00 };
-
+    // ContentEncKeyID has this EBML code [47][E2] in WebM,
+    // as per spec the size of the ContentEncKeyID is encoded on 16 bits.
+    // https://matroska.org/technical/specs/index.html#ContentEncKeyID/
+    const uint32_t kWebMMaxContentEncKeyIDSize = 64 * KB; // 2^16
 }
 
 static std::optional<Vector<Ref<SharedBuffer>>> extractKeyIDsKeyids(const SharedBuffer& buffer)
@@ -157,18 +160,25 @@ static RefPtr<SharedBuffer> sanitizeCenc(const SharedBuffer& buffer)
 
 static RefPtr<SharedBuffer> sanitizeWebM(const SharedBuffer& buffer)
 {
-    // 1. Format
-    // https://w3c.github.io/encrypted-media/format-registry/initdata/webm.html#format
-    notImplemented();
+    // Check if the buffer is a valid WebM initData.
+    // The WebM initData is the ContentEncKeyID, so should be less than kWebMMaxContentEncKeyIDSize.
+    if (buffer.isEmpty() || buffer.size() > kWebMMaxContentEncKeyIDSize)
+        return nullptr;
+
     return buffer.copy();
 }
 
-static std::optional<Vector<Ref<SharedBuffer>>> extractKeyIDsWebM(const SharedBuffer&)
+static std::optional<Vector<Ref<SharedBuffer>>> extractKeyIDsWebM(const SharedBuffer& buffer)
 {
+    Vector<Ref<SharedBuffer>> keyIDs;
+    RefPtr<SharedBuffer> sanitazedBuffer = sanitizeWebM(buffer);
+    if (!sanitazedBuffer)
+        return std::nullopt;
+
     // 1. Format
     // https://w3c.github.io/encrypted-media/format-registry/initdata/webm.html#format
-    notImplemented();
-    return std::nullopt;
+    keyIDs.append(sanitazedBuffer.releaseNonNull());
+    return keyIDs;
 }
 
 InitDataRegistry& InitDataRegistry::shared()
