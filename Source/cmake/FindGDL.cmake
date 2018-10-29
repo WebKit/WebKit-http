4c883f845ea5cc951a3589a76ba82b5ff0842dd4
Zan Dobersek <zdobersek@igalia.com>
2018-10-29 16:06:34 +0100

    Remove unnecessary CMake files.

--- Source/cmake/FindGDL.cmake
+++
@@ -1,8 +0,0 @@
-
-find_package(PkgConfig)
-pkg_check_modules(GDL gdl)
-
-include(FindPackageHandleStandardArgs)
-FIND_PACKAGE_HANDLE_STANDARD_ARGS(GDL DEFAULT_MSG GDL_LIBRARIES)
-
-mark_as_advanced(GDL_INCLUDE_DIRS GDL_LIBRARIES)
