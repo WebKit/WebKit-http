2c229b0004d2454663b0db746c487948e63800f2
Guillaume Emont <guijemont@igalia.com>
2019-01-15 20:04:54 +0100

    Revert "Revert "Only generate offline asm for the ARCHS (xcodebuild) or the current system (CMake)""
    This reverts commit 3eac000902af4c90174236cc53354ec58d35b57e.

--- Source/JavaScriptCore/offlineasm/generate_offset_extractor.rb
+++ Source/JavaScriptCore/offlineasm/generate_offset_extractor.rb
@@ -39,10 +39,8 @@ IncludeFile.processIncludeOptions()
 inputFlnm = ARGV.shift
 outputFlnm = ARGV.shift
 
-validBackends = ARGV.shift
-if validBackends
-    includeOnlyBackends(validBackends.split(","))
-end
+validBackends = canonicalizeBackendNames(ARGV.shift.split(/,/))
+includeOnlyBackends(validBackends)
 
 def emitMagicNumber
     OFFSET_MAGIC_NUMBERS.each {
