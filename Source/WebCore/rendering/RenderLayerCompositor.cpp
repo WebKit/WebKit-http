qt/qtwebkit

Allow 3D transforms without composited layers
Qt has no problems doing static 3D transforms without composited layers,
since the performance is not optimal we want to still trigger composited
layers when available, but allowing this without can fix the rendering
in the screenshot and printing API.

Task-number: QTBUG-43106
Change-Id: I30869d0f86704b84369273d7c2b875ee22badf22
Reviewed-by: Michael Brüning <michael.bruning@theqtcompany.com>

Source/WebCore/rendering/RenderLayerCompositor.cpp
@@ -325,7 +325,13 @@ void RenderLayerCompositor::cacheAcceleratedCompositingFlags()
 
 bool RenderLayerCompositor::canRender3DTransforms() const
 {
+#if !ENABLE(3D_RENDERING)
+    return false;
+#elif PLATFORM(QT)
+    return true;
+#else
     return hasAcceleratedCompositing() && (m_compositingTriggers & ChromeClient::ThreeDTransformTrigger);
+#endif
 }
 
 void RenderLayerCompositor::setCompositingLayersNeedRebuild(bool needRebuild)

WebKit/WebKit-http

Source/WebCore/rendering/RenderLayerCompositor.cpp
@@ -355,7 +355,13 @@ void RenderLayerCompositor::cacheAcceleratedCompositingFlags()
 
 bool RenderLayerCompositor::canRender3DTransforms() const
 {
+#if !ENABLE(3D_TRANSFORMS)
+    return false;
+#elif PLATFORM(QT)
+    return true;
+#else
     return hasAcceleratedCompositing() && (m_compositingTriggers & ChromeClient::ThreeDTransformTrigger);
+#endif
 }
 
 void RenderLayerCompositor::setCompositingLayersNeedRebuild(bool needRebuild)