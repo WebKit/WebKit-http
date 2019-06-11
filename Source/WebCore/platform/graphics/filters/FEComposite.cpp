1eb610aaf2be29f2aa01befea130f2ca04bb7471
Konstantin Tokarev <annulen@yandex.ru>
2019-06-11 23:07:24 +0300

    Revert "Merge r199243 - Timing attack on SVG feComposite filter circumvents same-origin policy"
    This reverts commit b95a8f4cf52a587f3ecb88a5a599a6f4377b342a.

--- Source/WebCore/platform/graphics/filters/FEComposite.cpp
+++ Source/WebCore/platform/graphics/filters/FEComposite.cpp
@@ -120,13 +120,6 @@ void FEComposite::correctFilterResultIfNeeded()
 
     forceValidPreMultipliedPixels();
 }
-    
-static unsigned char clampByte(int c)
-{
-    unsigned char buff[] = { static_cast<unsigned char>(c), 255, 0 };
-    unsigned uc = static_cast<unsigned>(c);
-    return buff[!!(uc & ~0xff) + !!(uc & ~(~0u >> 1))];
-}
 
 template <int b1, int b4>
 static inline void computeArithmeticPixels(unsigned char* source, unsigned char* destination, int pixelArrayLength,
@@ -148,7 +141,12 @@ static inline void computeArithmeticPixels(unsigned char* source, unsigned char*
         if (b4)
             result += scaledK4;
 
-        *destination = clampByte(result);
+        if (result <= 0)
+            *destination = 0;
+        else if (result >= 255)
+            *destination = 255;
+        else
+            *destination = result;
         ++source;
         ++destination;
     }
