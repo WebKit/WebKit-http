c563779429ef71cfe61fbd479d9d13a1ceacc0aa
Konstantin Tokarev <annulen@yandex.ru>
2019-06-11 23:07:23 +0300

    Revert "Merge r199278 - Fixed compilation of JPEGImageDecoder with libjpeg v9."
    This reverts commit 13f2ad2791913f72b040de0b8b8f05e0ee64d63f.

--- Source/WebCore/platform/image-decoders/jpeg/JPEGImageDecoder.h
+++ Source/WebCore/platform/image-decoders/jpeg/JPEGImageDecoder.h
@@ -30,9 +30,6 @@
 #include "ImageDecoder.h"
 #include <stdio.h> // Needed by jpeglib.h for FILE.
 
-// ICU defines TRUE and FALSE macros, breaking libjpeg v9 headers
-#undef TRUE
-#undef FALSE
 extern "C" {
 #include "jpeglib.h"
 }
