f05538256b13fce7b2e479b09810d9afca958a90
Konstantin Tokarev <annulen@yandex.ru>
2019-06-11 23:24:44 +0300

    Revert "Merge r197452 - REGRESSION(r197409): [GTK] Web process always crashes on WebPage construction after r197409"
    This reverts commit debcdf4e02b743b030c611277d90866825693ebf.

--- Source/WebKit2/WebProcess/WebPage/DrawingAreaImpl.cpp
+++ Source/WebKit2/WebProcess/WebPage/DrawingAreaImpl.cpp
@@ -68,6 +68,16 @@ DrawingAreaImpl::DrawingAreaImpl(WebPage& webPage, const WebPageCreationParamete
     , m_displayTimer(RunLoop::main(), this, &DrawingAreaImpl::displayTimerFired)
     , m_exitCompositingTimer(RunLoop::main(), this, &DrawingAreaImpl::exitAcceleratedCompositingMode)
 {
+
+#if USE(COORDINATED_GRAPHICS_THREADED)
+    webPage.corePage()->settings().setForceCompositingMode(true);
+#endif
+
+    if (webPage.corePage()->settings().acceleratedDrawingEnabled() || webPage.corePage()->settings().forceCompositingMode())
+        m_alwaysUseCompositing = true;
+
+    if (m_alwaysUseCompositing)
+        enterAcceleratedCompositingMode(0);
 }
 
 void DrawingAreaImpl::setNeedsDisplay()
@@ -247,10 +257,6 @@ void DrawingAreaImpl::updatePreferences(const WebPreferencesStore& store)
     settings.setAcceleratedCompositingForFixedPositionEnabled(true);
     settings.setFixedPositionCreatesStackingContext(true);
 #endif
-
-    m_alwaysUseCompositing = settings.acceleratedDrawingEnabled() && settings.forceCompositingMode();
-    if (m_alwaysUseCompositing && !m_layerTreeHost)
-        enterAcceleratedCompositingMode(nullptr);
 }
 
 void DrawingAreaImpl::layerHostDidFlushLayers()
