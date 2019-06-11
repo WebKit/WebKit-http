76bfea0457a5d0a0b686d9269baa3a04620f9da7
Konstantin Tokarev <annulen@yandex.ru>
2019-06-11 23:08:26 +0300

    Revert "Merge r198394 - Move IndexedDB regular test to web-platform-tests"
    This reverts commit fc5eb82bf63315bf202277364c2294b3bc7a6b9e.

--- LayoutTests/imported/w3c/indexeddb/support.js
+++ LayoutTests/imported/w3c/indexeddb/support.js
@@ -1,3 +1,12 @@
+if (window.testRunner) {
+    testRunner.dumpAsText();
+    testRunner.waitUntilDone();
+
+    // If the test file URL ends in "-private.htm(l)", enable private browsing.
+    if (window.location.href.endsWith("-private.htm") || window.location.href.endsWith("-private.html"))
+        testRunner.setPrivateBrowsingEnabled(true);
+}
+
 var databaseName = "database";
 var databaseVersion = 1;
 
