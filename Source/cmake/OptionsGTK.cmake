53972daea61564252cfb771c1d61b0ba67ad3aaf
Konstantin Tokarev <annulen@yandex.ru>
2019-06-11 23:09:11 +0300

    Revert "Merge r198058 - [GTK][Mac] Don't force ENABLE_INTROSPECTION=OFF on Mac"
    This reverts commit 2704bc845e3e37e040672ea7b110f3c53361dd5c.

--- Source/cmake/OptionsGTK.cmake
+++ Source/cmake/OptionsGTK.cmake
@@ -426,15 +426,10 @@ if (USE_LIBHYPHEN)
     endif ()
 endif ()
 
-# Override the cached variables, gtk-doc and gobject-introspection do not really work when cross-building.
-if (CMAKE_CROSSCOMPILING)
+# Override the cached variables, gtk-doc and gobject-introspection do not really work when cross-building or when building on Mac.
+if (CMAKE_CROSSCOMPILING OR APPLE)
     set(ENABLE_GTKDOC OFF)
     set(ENABLE_INTROSPECTION OFF)
-endif ()
-
-# Override the cached variable, gtk-doc does not really work when building on Mac.
-if (APPLE)
-    set(ENABLE_GTKDOC OFF)
 endif ()
 
 set(DERIVED_SOURCES_GOBJECT_DOM_BINDINGS_DIR ${DERIVED_SOURCES_DIR}/webkitdom)
