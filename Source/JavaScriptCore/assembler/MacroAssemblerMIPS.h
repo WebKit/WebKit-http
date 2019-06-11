00a80920a03b6da82f192103975284cc1209e788
Konstantin Tokarev <annulen@yandex.ru>
2019-06-11 23:06:24 +0300

    Revert "[mips] Implemented moveZeroToDouble."
    This reverts commit d70f8423f5bb5b61e6fc6d3b39d9d20a08e582a2.

--- Source/JavaScriptCore/assembler/MacroAssemblerMIPS.h
+++ Source/JavaScriptCore/assembler/MacroAssemblerMIPS.h
@@ -2499,11 +2499,6 @@ public:
             m_assembler.movd(dest, src);
     }
 
-    void moveZeroToDouble(FPRegisterID reg)
-    {
-        convertInt32ToDouble(MIPSRegisters::zero, reg);
-    }
-
     void swapDouble(FPRegisterID fr1, FPRegisterID fr2)
     {
         moveDouble(fr1, fpTempRegister);
