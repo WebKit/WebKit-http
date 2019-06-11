1a6da1f4703a405b534dc881ca08ce545a6ae7c3
Konstantin Tokarev <annulen@yandex.ru>
2019-06-11 23:08:23 +0300

    Revert "Merge r198451 - ASSERTION FAILED: m_isValid == valid() in WebCore::HTMLFormControlElement::isValidFormControlElement"
    This reverts commit 1ba5fb135896e7c1b9f380b401b0d484f5de9b66.

--- Source/WebCore/html/HTMLFormControlElement.cpp
+++ Source/WebCore/html/HTMLFormControlElement.cpp
@@ -274,12 +274,13 @@ static void removeInvalidElementToAncestorFromInsertionPoint(const HTMLFormContr
 
 Node::InsertionNotificationRequest HTMLFormControlElement::insertedInto(ContainerNode& insertionPoint)
 {
-    m_dataListAncestorState = Unknown;
-    setNeedsWillValidateCheck();
     if (willValidate() && !isValidFormControlElement())
         addInvalidElementToAncestorFromInsertionPoint(*this, &insertionPoint);
+
     if (document().hasDisabledFieldsetElement())
         setAncestorDisabled(computeIsDisabledByFieldsetAncestor());
+    m_dataListAncestorState = Unknown;
+    setNeedsWillValidateCheck();
     HTMLElement::insertedInto(insertionPoint);
     FormAssociatedElement::insertedInto(insertionPoint);
     return InsertionShouldCallFinishedInsertingSubtree;
