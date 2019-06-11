fbc26bc0bb4afa5716724ca026d15088a71d7d68
Konstantin Tokarev <annulen@yandex.ru>
2019-06-11 23:04:21 +0300

    Revert "Multiple HTTP tests fail with Apache 2.4.25"
    This reverts commit 86d17e566280cfa9bde6577c495c9c9e036c8fee.

--- LayoutTests/http/tests/security/contentSecurityPolicy/resources/echo-script-src.pl
+++ LayoutTests/http/tests/security/contentSecurityPolicy/resources/echo-script-src.pl
@@ -4,7 +4,6 @@ use CGI;
 
 my $cgi = new CGI;
 
-print "HTTP/1.1 200 OK\n";
 print "Content-Type: text/html; charset=UTF-8\n";
 my $experimental = $cgi->param('experimental') || "";
 if ($experimental eq 'true') {
