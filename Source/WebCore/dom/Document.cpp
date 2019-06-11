53286e2eccb97d00facb4ba0a93d626131ff6058
Konstantin Tokarev <annulen@yandex.ru>
2019-06-11 23:04:59 +0300

    Revert "Merge r202151 - Restrict security origin inheritance to empty, about:blank, and about:srcdoc URLs"
    This reverts commit d9a5ded1306430bc7fd4cabae6ebd5be07edeaec.

--- Source/WebCore/dom/Document.cpp
+++ Source/WebCore/dom/Document.cpp
@@ -343,6 +343,19 @@ static inline bool isValidNamePart(UChar32 c)
     return true;
 }
 
+static bool shouldInheritSecurityOriginFromOwner(const URL& url)
+{
+    // http://www.whatwg.org/specs/web-apps/current-work/#origin-0
+    //
+    // If a Document has the address "about:blank"
+    //     The origin of the Document is the origin it was assigned when its browsing context was created.
+    //
+    // Note: We generalize this to all "blank" URLs and invalid URLs because we
+    // treat all of these URLs as about:blank.
+    //
+    return url.isEmpty() || url.isBlankURL();
+}
+
 static Widget* widgetForElement(Element* focusedElement)
 {
     if (!focusedElement)
@@ -5118,7 +5131,7 @@ void Document::initSecurityContext()
         setBaseURLOverride(parentDocument->baseURL());
     }
 
-    if (!m_url.shouldInheritSecurityOriginFromOwner())
+    if (!shouldInheritSecurityOriginFromOwner(m_url))
         return;
 
     // If we do not obtain a meaningful origin from the URL, then we try to
@@ -5151,7 +5164,7 @@ void Document::initSecurityContext()
 
 void Document::initContentSecurityPolicy()
 {
-    if (!m_frame->tree().parent() || (!m_url.shouldInheritSecurityOriginFromOwner() && !isPluginDocument()))
+    if (!m_frame->tree().parent() || (!shouldInheritSecurityOriginFromOwner(m_url) && !isPluginDocument()))
         return;
 
     contentSecurityPolicy()->copyStateFrom(m_frame->tree().parent()->document()->contentSecurityPolicy());
