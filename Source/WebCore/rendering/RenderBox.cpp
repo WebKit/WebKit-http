746ceb9a0b270f8fe9dc3635d016ff45468ce7f4
Konstantin Tokarev <annulen@yandex.ru>
2019-06-11 23:05:44 +0300

    Revert "Merge r201407 - Setting overflow:hidden does not always repaint clipped content."
    This reverts commit a1f14c8b5aa194873fab5c0da5ae0705a62acf41.

--- Source/WebCore/rendering/RenderBox.cpp
+++ Source/WebCore/rendering/RenderBox.cpp
@@ -516,19 +516,17 @@ void RenderBox::updateFromStyle()
                 boxHasOverflowClip = false;
             }
         }
+        
         // Check for overflow clip.
         // It's sufficient to just check one direction, since it's illegal to have visible on only one overflow value.
         if (boxHasOverflowClip) {
-            if (!s_hadOverflowClip && hasRenderOverflow()) {
-                // Erase the overflow.
-                // Overflow changes have to result in immediate repaints of the entire layout overflow area because
-                // repaints issued by removal of descendants get clipped using the updated style when they shouldn't.
-                repaintRectangle(visualOverflowRect());
-                repaintRectangle(layoutOverflowRect());
-            }
+            if (!s_hadOverflowClip)
+                // Erase the overflow
+                repaint();
             setHasOverflowClip();
         }
     }
+
     setHasTransformRelatedProperty(styleToUse.hasTransformRelatedProperty());
     setHasReflection(styleToUse.boxReflect());
 }
