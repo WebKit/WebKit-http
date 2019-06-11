fd0557e823226ce08f8833f67244a4a3cf269f4a
Konstantin Tokarev <annulen@yandex.ru>
2019-06-11 23:09:52 +0300

    Revert "Merge r197417 - FTL should simplify StringReplace with an empty replacement string"
    This reverts commit 7f57e72ea3feae272e6bee900f62977aabb6a91a.

--- LayoutTests/js/regress/script-tests/string-replace-empty.js
+++
@@ -1,8 +0,0 @@
-(function() {
-    var result;
-    for (var i = 0; i < 400000; ++i) {
-        result = "foo".replace(/f/g, "");
-    }
-    if (result != "oo")
-        throw "Error: bad result: "+ result;
-})();
