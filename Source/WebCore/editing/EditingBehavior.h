8cffa6a8ffdfdaa1684935ef4c7cd049e8c6f11c
Konstantin Tokarev <annulen@yandex.ru>
2019-06-11 23:07:49 +0300

    Revert "Merge r198904 - SelectionController::positionForPlatform should ask EditingBehavior for platform specific behavior"
    This reverts commit b464eeadc03931c623ea00c66084f7e7fe6bdc17.

--- Source/WebCore/editing/EditingBehavior.h
+++ Source/WebCore/editing/EditingBehavior.h
@@ -91,9 +91,6 @@ public:
 
     bool shouldSelectBasedOnDictionaryLookup() const { return m_type == EditingMacBehavior; }
 
-    // Linux and Windows always extend selections from the extent endpoint.
-    bool shouldAlwaysExtendSelectionFromExtentEndpoint() const { return m_type != EditingMacBehavior && m_type != EditingIOSBehavior; }
-
 private:
     EditingBehaviorType m_type;
 };
