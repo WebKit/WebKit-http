movableink/webkit

more fixes to qt port

Source/WebCore/page/qt/EventHandlerQt.cpp
@@ -54,15 +54,15 @@ namespace WebCore {
 
 #if ENABLE(DRAG_SUPPORT)
 #if defined(Q_OS_OSX)
-const double EventHandler::TextDragDelay = 0.15;
+const Seconds EventHandler::TextDragDelay { 0.15_s };
 #else
-const double EventHandler::TextDragDelay = 0.0;
+const Seconds EventHandler::TextDragDelay  { 0.0_s };
 #endif
 #endif
 
 bool EventHandler::tabsToAllFormControls(KeyboardEvent* event) const
 {
-    return !isKeyboardOptionTab(event);
+    return !isKeyboardOptionTab(*event);
 }
 
 void EventHandler::focusDocumentView()
@@ -87,20 +87,13 @@ bool EventHandler::eventActivatedView(const PlatformMouseEvent&) const
     return false;
 }
 
-bool EventHandler::passWheelEventToWidget(const PlatformWheelEvent& event, Widget& widget)
+bool EventHandler::widgetDidHandleWheelEvent(const PlatformWheelEvent& event, Widget& widget)
 {
     if (!widget.isFrameView())
         return false;
 
     return downcast<FrameView>(widget).frame().eventHandler().handleWheelEvent(event);
 }
-
-#if ENABLE(DRAG_SUPPORT)
-PassRefPtr<DataTransfer> EventHandler::createDraggingDataTransfer() const
-{
-    return DataTransfer::createForDragAndDrop();
-}
-#endif
 
 bool EventHandler::passMousePressEventToSubframe(MouseEventWithHitTestResults& mev, Frame* subframe)
 {
@@ -120,12 +113,12 @@ bool EventHandler::passMouseReleaseEventToSubframe(MouseEventWithHitTestResults&
     return true;
 }
 
-unsigned EventHandler::accessKeyModifiers()
+OptionSet<PlatformEvent::Modifier> EventHandler::accessKeyModifiers()
 {
 #if OS(DARWIN)
-    return PlatformEvent::CtrlKey | PlatformEvent::AltKey;
+    return PlatformEvent::Modifier::CtrlKey | PlatformEvent::Modifier::AltKey;
 #else
-    return PlatformEvent::AltKey;
+    return PlatformEvent::Modifier::AltKey;
 #endif
 }
 

Source/WebCore/platform/graphics/qt/MediaPlayerPrivateQt.cpp
@@ -21,6 +21,7 @@
 #include "MediaPlayerPrivateQt.h"
 
 #include "Frame.h"
+#include "FrameLoader.h"
 #include "FrameView.h"
 #include "GraphicsContext.h"
 #include "GraphicsLayer.h"
@@ -80,20 +81,16 @@ MediaPlayer::SupportsType MediaPlayerPrivateQt::supportsType(const MediaEngineSu
     if (parameters.isMediaStream || parameters.isMediaSource)
         return MediaPlayer::IsNotSupported;
 
-    if (!parameters.type.startsWith("audio/") && !parameters.type.startsWith("video/"))
+    if (!parameters.type.raw().startsWith("audio/") && !parameters.type.raw().startsWith("video/"))
         return MediaPlayer::IsNotSupported;
 
-    // Parse and trim codecs.
-    QString codecStr = parameters.codecs;
-    QStringList codecList = codecStr.split(QLatin1Char(','), QString::SkipEmptyParts);
-    QStringList codecListTrimmed;
-    foreach (const QString& codecStrNotTrimmed, codecList) {
-        QString codecStrTrimmed = codecStrNotTrimmed.trimmed();
-        if (!codecStrTrimmed.isEmpty())
-            codecListTrimmed.append(codecStrTrimmed);
+    // Parse and trim codecs
+    QStringList codecList;
+    foreach (const String& codec, parameters.codecs) {
+        codecList.append(QString(codec));
     }
 
-    if (QMediaPlayer::hasSupport(parameters.type, codecListTrimmed) >= QMultimedia::ProbablySupported)
+    if (QMediaPlayer::hasSupport(parameters.type, codecList) >= QMultimedia::ProbablySupported)
         return MediaPlayer::IsSupported;
 
     return MediaPlayer::MayBeSupported;
@@ -648,14 +645,6 @@ void MediaPlayerPrivateQt::paintCurrentFrameInContext(GraphicsContext& context, 
 
 void MediaPlayerPrivateQt::paintToTextureMapper(TextureMapper& textureMapper, const FloatRect& targetRect, const TransformationMatrix& matrix, float opacity)
 {
-}
-
-PlatformMedia MediaPlayerPrivateQt::platformMedia() const
-{
-    PlatformMedia pm;
-    pm.type = PlatformMedia::QtMediaPlayerType;
-    pm.media.qtMediaPlayer = const_cast<MediaPlayerPrivateQt*>(this);
-    return pm;
 }
 
 } // namespace WebCore

WebKit/WebKit-http

Source/WebCore/page/qt/EventHandlerQt.cpp
@@ -55,15 +55,15 @@ namespace WebCore {
 
 #if ENABLE(DRAG_SUPPORT)
 #if defined(Q_OS_MACOS)
-const double EventHandler::TextDragDelay = 0.15;
+const double EventHandler::TextDragDelay { 0.15_s };
 #else
-const double EventHandler::TextDragDelay = 0.0;
+const Seconds EventHandler::TextDragDelay { 0.0_s };
 #endif
 #endif
 
 bool EventHandler::tabsToAllFormControls(KeyboardEvent* event) const
 {
-    return !isKeyboardOptionTab(event);
+    return !isKeyboardOptionTab(*event);
 }
 
 void EventHandler::focusDocumentView()
@@ -88,20 +88,13 @@ bool EventHandler::eventActivatedView(const PlatformMouseEvent&) const
     return false;
 }
 
-bool EventHandler::passWheelEventToWidget(const PlatformWheelEvent& event, Widget& widget)
+bool EventHandler::widgetDidHandleWheelEvent(const PlatformWheelEvent& event, Widget& widget)
 {
     if (!widget.isFrameView())
         return false;
 
     return downcast<FrameView>(widget).frame().eventHandler().handleWheelEvent(event);
 }
-
-#if ENABLE(DRAG_SUPPORT)
-PassRefPtr<DataTransfer> EventHandler::createDraggingDataTransfer() const
-{
-    return DataTransfer::createForDragAndDrop();
-}
-#endif
 
 bool EventHandler::passMousePressEventToSubframe(MouseEventWithHitTestResults& mev, Frame* subframe)
 {
@@ -121,7 +114,7 @@ bool EventHandler::passMouseReleaseEventToSubframe(MouseEventWithHitTestResults&
     return true;
 }
 
-unsigned EventHandler::accessKeyModifiers()
+OptionSet<PlatformEvent::Modifier> EventHandler::accessKeyModifiers()
 {
 #if OS(DARWIN)
     // On macOS, the ControlModifier value corresponds
@@ -129,11 +122,11 @@ unsigned EventHandler::accessKeyModifiers()
     // and the MetaModifier value corresponds to the Control keys.
     // See http://doc.qt.io/qt-5/qt.html#KeyboardModifier-enum
     if (UNLIKELY(QCoreApplication::testAttribute(Qt::AA_MacDontSwapCtrlAndMeta)))
-        return PlatformEvent::CtrlKey | PlatformEvent::AltKey;
+        return { PlatformEvent::CtrlKey, PlatformEvent::AltKey };
     else
-        return PlatformEvent::MetaKey | PlatformEvent::AltKey;
+        return { PlatformEvent::MetaKey, PlatformEvent::AltKey };
 #else
-    return PlatformEvent::AltKey;
+    return PlatformEvent::Modifier::AltKey;
 #endif
 }
 

Source/WebCore/platform/graphics/qt/MediaPlayerPrivateQt.cpp
@@ -21,6 +21,7 @@
 #include "MediaPlayerPrivateQt.h"
 
 #include "Frame.h"
+#include "FrameLoader.h"
 #include "FrameView.h"
 #include "GraphicsContext.h"
 #include "GraphicsLayer.h"
@@ -80,20 +81,16 @@ MediaPlayer::SupportsType MediaPlayerPrivateQt::supportsType(const MediaEngineSu
     if (parameters.isMediaStream || parameters.isMediaSource)
         return MediaPlayer::IsNotSupported;
 
-    if (!parameters.type.startsWithIgnoringASCIICase("audio/") && !parameters.type.startsWithIgnoringASCIICase("video/"))
+    if (!parameters.type.raw().startsWithIgnoringASCIICase("audio/") && !parameters.type.raw().startsWithIgnoringASCIICase("video/"))
         return MediaPlayer::IsNotSupported;
 
-    // Parse and trim codecs.
-    QString codecStr = parameters.codecs;
-    QStringList codecList = codecStr.split(QLatin1Char(','), QString::SkipEmptyParts);
-    QStringList codecListTrimmed;
-    foreach (const QString& codecStrNotTrimmed, codecList) {
-        QString codecStrTrimmed = codecStrNotTrimmed.trimmed();
-        if (!codecStrTrimmed.isEmpty())
-            codecListTrimmed.append(codecStrTrimmed);
+    // Parse and trim codecs
+    QStringList codecList;
+    foreach (const String& codec, parameters.codecs) {
+        codecList.append(QString(codec));
     }
 
-    if (QMediaPlayer::hasSupport(parameters.type, codecListTrimmed) >= QMultimedia::ProbablySupported)
+    if (QMediaPlayer::hasSupport(parameters.type, codecList) >= QMultimedia::ProbablySupported)
         return MediaPlayer::IsSupported;
 
     return MediaPlayer::MayBeSupported;
@@ -648,14 +645,6 @@ void MediaPlayerPrivateQt::paintCurrentFrameInContext(GraphicsContext& context, 
 
 void MediaPlayerPrivateQt::paintToTextureMapper(TextureMapper& textureMapper, const FloatRect& targetRect, const TransformationMatrix& matrix, float opacity)
 {
-}
-
-PlatformMedia MediaPlayerPrivateQt::platformMedia() const
-{
-    PlatformMedia pm;
-    pm.type = PlatformMedia::QtMediaPlayerType;
-    pm.media.qtMediaPlayer = const_cast<MediaPlayerPrivateQt*>(this);
-    return pm;
 }
 
 } // namespace WebCore