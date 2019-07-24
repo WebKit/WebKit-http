movableink/webkit

mac JSC_API_AVAILABLE follows gtk port

Source/JavaScriptCore/API/WebKitAvailability.h
@@ -68,7 +68,7 @@
 
 #endif /* !TARGET_OS_IPHONE && __MAC_OS_X_VERSION_MIN_REQUIRED < 101100 */
 
-#if defined(BUILDING_GTK__)
+#if defined(BUILDING_GTK__) || PLATFORM(QT)
 #undef JSC_API_AVAILABLE
 #define JSC_API_AVAILABLE(...)
 #endif

WebKit/WebKit-http

Change-Id: Ie6e0f296230dd7b0312beb9949d7bfb3088793ed

Source/JavaScriptCore/API/WebKitAvailability.h
@@ -68,7 +68,7 @@
 
 #endif /* !TARGET_OS_IPHONE && __MAC_OS_X_VERSION_MIN_REQUIRED < 101100 */
 
-#if defined(BUILDING_GTK__)
+#if defined(BUILDING_GTK__) || defined(BUILDING_QT__)
 #undef JSC_API_AVAILABLE
 #define JSC_API_AVAILABLE(...)
 #endif