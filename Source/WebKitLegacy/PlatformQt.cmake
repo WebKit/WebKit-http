23792dc35646c0ceab931c1bb522f427c8e67122
Herb Miller Jr <herbert.miller@industryweapon.com>
2019-07-26 09:30:57 -0400

    [cmake] Point WebKitLegacy to location of cmakeconfig.h so WebKitWidgetsPrefix.h can include it

--- Source/WebKitLegacy/PlatformQt.cmake
+++ Source/WebKitLegacy/PlatformQt.cmake
@@ -801,3 +801,7 @@ add_custom_command(
     COMMAND ${PERL_EXECUTABLE} ${WEBKITLEGACY_DIR}/scripts/generate-webkitversion.pl --config ${WEBKITLEGACY_DIR}/mac/Configurations/Version.xcconfig --outputDir ${DERIVED_SOURCES_WEBKITLEGACY_DIR}
     VERBATIM)
 list(APPEND WebKitLegacy_SOURCES ${DERIVED_SOURCES_WEBKITLEGACY_DIR}/WebKitVersion.h)
+
+set(common_generator_dependencies
+    ${CMAKE_BINARY_DIR}/cmakeconfig.h
+)
