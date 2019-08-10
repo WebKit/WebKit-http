WebKit/WebKit-http

commit 0e8af882c27682895cf60df965a967d2f34ce612

Web Inspector: Page: don't allow the domain to be disabled https://bugs.webkit.org/show_bug.cgi?id=200109
Reviewed by Brian Burg.

Source/JavaScriptCore:

The `PageAgent` is relied on by many of the other agents, so much so that it doesn't make
sense to support the ability to "disable" (as well as "enable") the agent.

When the first frontend connects, we should treat the `PageAgent` as active and available.

* inspector/protocol/Page.json:
Remove `enable`/`disable`.

Source/WebCore:

The `PageAgent` is relied on by many of the other agents, so much so that it doesn't make
sense to support the ability to "disable" (as well as "enable") the agent.

When the first frontend connects, we should treat the `PageAgent` as active and available.

* inspector/agents/InspectorPageAgent.h:
* inspector/agents/InspectorPageAgent.cpp:
(WebCore::InspectorPageAgent::didCreateFrontendAndBackend):
(WebCore::InspectorPageAgent::willDestroyFrontendAndBackend):
(WebCore::InspectorPageAgent::timestamp):
(WebCore::InspectorPageAgent::enable): Deleted.
(WebCore::InspectorPageAgent::disable): Deleted.

* inspector/agents/InspectorApplicationCacheAgent.cpp:
(WebCore::InspectorApplicationCacheAgent::updateApplicationCacheStatus):
(WebCore::InspectorApplicationCacheAgent::getFramesWithManifests):
(WebCore::InspectorApplicationCacheAgent::assertFrameWithDocumentLoader):
* inspector/agents/InspectorCSSAgent.cpp:
(WebCore::InspectorCSSAgent::createStyleSheet):
* inspector/agents/InspectorDOMAgent.cpp:
(WebCore::InspectorDOMAgent::highlightSelector):
(WebCore::InspectorDOMAgent::highlightFrame):
(WebCore::InspectorDOMAgent::buildObjectForNode):
* inspector/agents/InspectorTimelineAgent.cpp:
(WebCore::InspectorTimelineAgent::setFrameIdentifier):
* inspector/agents/page/PageNetworkAgent.cpp:
(WebCore::PageNetworkAgent::loaderIdentifier):
(WebCore::PageNetworkAgent::frameIdentifier):
(WebCore::PageNetworkAgent::scriptExecutionContext):
* inspector/agents/page/PageRuntimeAgent.cpp:

Source/WebCore/inspector/agents/InspectorTimelineAgent.cpp
@@ -740,11 +740,7 @@ void InspectorTimelineAgent::setFrameIdentifier(JSON::Object* record, Frame* fra
     if (!frame)
         return;
 
-    auto* pageAgent = m_instrumentingAgents.inspectorPageAgent();
-    if (!pageAgent)
-        return;
-
-    record->setString("frameId"_s, pageAgent->frameId(frame));
+    record->setString("frameId"_s, m_instrumentingAgents.inspectorPageAgent()->frameId(frame));
 }
 
 void InspectorTimelineAgent::didCompleteRecordEntry(const TimelineRecordEntry& entry)

Source/WebCore/testing/Internals.cpp
@@ -322,6 +322,7 @@ public:
     virtual ~InspectorStubFrontend();
 
 private:
+    void frontendLoaded() final;
     void attachWindow(DockSide) final { }
     void detachWindow() final { }
     void closeWindow() final;
@@ -337,26 +338,18 @@ private:
     void sendMessageToFrontend(const String& message) final;
     ConnectionType connectionType() const final { return ConnectionType::Local; }
 
-    Page* frontendPage() const
-    {
-        if (!m_frontendWindow || !m_frontendWindow->document())
-            return nullptr;
-
-        return m_frontendWindow->document()->page();
-    }
-
     RefPtr<DOMWindow> m_frontendWindow;
-    InspectorController& m_frontendController;
+    Vector<String> m_messages;
+    bool m_loaded { false };
 };
 
 InspectorStubFrontend::InspectorStubFrontend(Page& inspectedPage, RefPtr<DOMWindow>&& frontendWindow)
     : InspectorFrontendClientLocal(&inspectedPage.inspectorController(), frontendWindow->document()->page(), std::make_unique<InspectorFrontendClientLocal::Settings>())
     , m_frontendWindow(frontendWindow.copyRef())
-    , m_frontendController(frontendPage()->inspectorController())
 {
     ASSERT_ARG(frontendWindow, frontendWindow);
 
-    m_frontendController.setInspectorFrontendClient(this);
+    frontendPage()->inspectorController().setInspectorFrontendClient(this);
     inspectedPage.inspectorController().connectFrontend(*this);
 }
 
@@ -365,12 +358,21 @@ InspectorStubFrontend::~InspectorStubFrontend()
     closeWindow();
 }
 
+void InspectorStubFrontend::frontendLoaded()
+{
+    m_loaded = true;
+
+    for (auto& message : m_messages)
+        sendMessageToFrontend(message);
+    m_messages.clear();
+}
+
 void InspectorStubFrontend::closeWindow()
 {
     if (!m_frontendWindow)
         return;
 
-    m_frontendController.setInspectorFrontendClient(nullptr);
+    frontendPage()->inspectorController().setInspectorFrontendClient(nullptr);
     inspectedPage()->inspectorController().disconnectFrontend(*this);
 
     m_frontendWindow->close();
@@ -380,6 +382,11 @@ void InspectorStubFrontend::closeWindow()
 void InspectorStubFrontend::sendMessageToFrontend(const String& message)
 {
     ASSERT_ARG(message, !message.isEmpty());
+
+    if (!m_loaded) {
+        m_messages.append(message);
+        return;
+    }
 
     InspectorClient::doDispatchMessageOnFrontendPage(frontendPage(), message);
 }

Source/WebInspectorUI/UserInterface/Test/TestStub.js
@@ -29,6 +29,10 @@ InspectorFrontendAPI.dispatchMessageAsync = InspectorProtocol.dispatchMessageFro
 
 window.ProtocolTest = new ProtocolTestHarness();
 
+document.addEventListener("DOMContentLoaded", (event) => {
+    InspectorFrontendHost.loaded();
+});
+
 window.addEventListener("message", (event) => {
     try {
         eval(event.data);

LayoutTests/http/tests/inspector/page/loading-iframe-document-node.html
@@ -35,17 +35,14 @@ function test()
 
     function step1_bootstrap() {
         ProtocolTest.log("step1_bootstrap");
-        // Enable the frame events.
-        InspectorProtocol.sendCommand("Page.enable", {}, function() {
-            // Enable the Runtime.executionContextCreated event.
-            InspectorProtocol.sendCommand("Runtime.enable", {}, function() {
-                // Initialize the DOM agent.
-                InspectorProtocol.sendCommand("DOM.getDocument", {}, function() {
-                    ProtocolTest.log("Main document loaded");
-                    
-                    // Add the iframe to the DOM.
-                    InspectorProtocol.sendCommand("Runtime.evaluate", { "expression": "appendIframe()" });
-                });
+        // Enable the Runtime.executionContextCreated event.
+        InspectorProtocol.sendCommand("Runtime.enable", {}, function() {
+            // Initialize the DOM agent.
+            InspectorProtocol.sendCommand("DOM.getDocument", {}, function() {
+                ProtocolTest.log("Main document loaded");
+                
+                // Add the iframe to the DOM.
+                InspectorProtocol.sendCommand("Runtime.evaluate", { "expression": "appendIframe()" });
             });
         });
     }

LayoutTests/inspector/page/archive.html
@@ -4,7 +4,6 @@
 <script>
 function test()
 {
-    InspectorProtocol.sendCommand("Page.enable", {});
     InspectorProtocol.sendCommand("Page.archive", {}, function(event) {
         var data = event.result.data;
         if (!data)

LayoutTests/inspector/page/frameScheduledNavigation-async-delegates.html
@@ -16,7 +16,6 @@ function test()
     InspectorProtocol.eventHandler["Page.frameScheduledNavigation"] = onScheduled;
     InspectorProtocol.eventHandler["Page.frameStartedLoading"] = onStarted;
     InspectorProtocol.eventHandler["Page.frameClearedScheduledNavigation"] = onCleared;
-    InspectorProtocol.sendCommand("Page.enable", {});
 
     function onScheduled(msg)
     {

LayoutTests/inspector/page/frameScheduledNavigation.html
@@ -13,7 +13,6 @@ function test()
     InspectorProtocol.eventHandler["Page.frameScheduledNavigation"] = onScheduled;
     InspectorProtocol.eventHandler["Page.frameStartedLoading"] = onStarted;
     InspectorProtocol.eventHandler["Page.frameClearedScheduledNavigation"] = onCleared;
-    InspectorProtocol.sendCommand("Page.enable", {});
 
     function onScheduled(msg)
     {

LayoutTests/inspector/page/frameStartedLoading.html
@@ -14,7 +14,6 @@ function test()
 {
     InspectorProtocol.eventHandler["Page.frameStartedLoading"] = onStart;
     InspectorProtocol.eventHandler["Page.frameStoppedLoading"] = onStop;
-    InspectorProtocol.sendCommand("Page.enable", {});
 
     function onStart()
     {

commit 2e4795fcb01aa480bcbdb8f6c7bd16dd17d59e29

Web Inspector: REGRESSION(r248454): WK1 inspector frontend client doesn't queue messages to the frontend before it's loaded https://bugs.webkit.org/show_bug.cgi?id=200587
Reviewed by Joseph Pecoraro.

WK1 inspector sends messages to the frontend using `WebCore::InspectorClient::doDispatchMessageOnFrontendPage`,
which does not do any sort of queueing to wait until the frontend is loaded (`InspectorFrontendHost.loaded()`).

Now that we are sending messages immediately, we should always queue.

Source/WebCore:

Covered by existing tests (which were failing after r248454, and now won't fail).

* inspector/InspectorFrontendClientLocal.h:
* inspector/InspectorFrontendClientLocal.cpp:
(WebCore::InspectorFrontendClientLocal::setDockingUnavailable):
(WebCore::InspectorFrontendClientLocal::setAttachedWindow):
(WebCore::InspectorFrontendClientLocal::setDebuggingEnabled):
(WebCore::InspectorFrontendClientLocal::setTimelineProfilingEnabled):
(WebCore::InspectorFrontendClientLocal::startProfilingJavaScript):
(WebCore::InspectorFrontendClientLocal::stopProfilingJavaScript):
(WebCore::InspectorFrontendClientLocal::showConsole):
(WebCore::InspectorFrontendClientLocal::showResources):
(WebCore::InspectorFrontendClientLocal::showMainResourceForFrame):
(WebCore::InspectorFrontendClientLocal::dispatch): Added.
(WebCore::InspectorFrontendClientLocal::dispatchMessage): Added.
(WebCore::InspectorFrontendClientLocal::dispatchMessageAsync): Added.
(WebCore::InspectorFrontendClientLocal::evaluateOnLoad):
Provide additional ways for subclasses to call other `InspectorFrontendAPI` methods using
the "on load" queue.

* testing/Internals.cpp:
(WebCore::InspectorStubFrontend::sendMessageToFrontend):
(WebCore::InspectorStubFrontend::frontendLoaded): Deleted.
Leverage the base `InspectorFrontendClientLocal`'s functions for queueing messages.

* inspector/InspectorClient.h:
* inspector/InspectorClient.cpp: Removed.
(WebCore::InspectorClient::doDispatchMessageOnFrontendPage): Deleted.

Source/WebCore/WebCore.xcodeproj/project.pbxproj
@@ -9500,7 +9500,6 @@
 		7A0E771C10C00DB100A0276E /* JSInspectorFrontendHost.cpp */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.cpp.cpp; path = JSInspectorFrontendHost.cpp; sourceTree = "<group>"; };
 		7A0E771D10C00DB100A0276E /* JSInspectorFrontendHost.h */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.c.h; path = JSInspectorFrontendHost.h; sourceTree = "<group>"; };
 		7A1D7FCA18F85F0F00C385AD /* en */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.javascript; name = en; path = en.lproj/mediaControlsLocalizedStrings.js; sourceTree = SOURCE_ROOT; };
-		7A1F2B51126C61B20006A7E6 /* InspectorClient.cpp */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.cpp.cpp; path = InspectorClient.cpp; sourceTree = "<group>"; };
 		7A22731E20C9F9D900DB1DEF /* WebKitNSImageExtras.mm */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.cpp.objcpp; path = WebKitNSImageExtras.mm; sourceTree = "<group>"; };
 		7A22731F20C9F9DA00DB1DEF /* WebKitNSImageExtras.h */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.c.h; path = WebKitNSImageExtras.h; sourceTree = "<group>"; };
 		7A29BA67187B732200F29CEB /* TemporaryOpenGLSetting.h */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.c.h; path = TemporaryOpenGLSetting.h; sourceTree = "<group>"; };
@@ -17004,7 +17003,6 @@
 				9109E9C5222CABC800BB6264 /* InspectorAuditResourcesObject.h */,
 				6A22E8721F1042C400F546C3 /* InspectorCanvas.cpp */,
 				6A22E86F1F10418600F546C3 /* InspectorCanvas.h */,
-				7A1F2B51126C61B20006A7E6 /* InspectorClient.cpp */,
 				1C81B9580E97330800266E07 /* InspectorClient.h */,
 				1C81B9570E97330800266E07 /* InspectorController.cpp */,
 				1C81B9560E97330800266E07 /* InspectorController.h */,
@@ -25539,11 +25537,11 @@
 				C24A57BB21FEAFEA004C6DD1 /* WHLSLPrepare.h */,
 				522E1A172297D6D400E5D36A /* WHLSLPreserveVariableLifetimes.cpp */,
 				522E1A192297D6D400E5D36A /* WHLSLPreserveVariableLifetimes.h */,
-				526B3F0122FB7BDD0076D37D /* WHLSLPruneUnreachableStandardLibraryFunctions.cpp */,
-				526B3F0322FB7BDE0076D37D /* WHLSLPruneUnreachableStandardLibraryFunctions.h */,
 				C21BF73A21CD8D7000227979 /* WHLSLProgram.h */,
 				1CAA82F62242AE0500E84BBB /* WHLSLPropertyResolver.cpp */,
 				1CAA82F72242AE0500E84BBB /* WHLSLPropertyResolver.h */,
+				526B3F0122FB7BDD0076D37D /* WHLSLPruneUnreachableStandardLibraryFunctions.cpp */,
+				526B3F0322FB7BDE0076D37D /* WHLSLPruneUnreachableStandardLibraryFunctions.h */,
 				1CA0C2E021EEB5F500A11860 /* WHLSLRecursionChecker.cpp */,
 				1CA0C2DE21EEB5F400A11860 /* WHLSLRecursionChecker.h */,
 				C234A9AD21E92C19003C984D /* WHLSLRecursiveTypeChecker.cpp */,

Source/WebCore/inspector/InspectorClient.cpp
@@ -1,55 +1,0 @@
-/*
- * Copyright (C) 2010 Google Inc. All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are
- * met:
- *
- *     * Redistributions of source code must retain the above copyright
- * notice, this list of conditions and the following disclaimer.
- *     * Redistributions in binary form must reproduce the above
- * copyright notice, this list of conditions and the following disclaimer
- * in the documentation and/or other materials provided with the
- * distribution.
- *     * Neither the name of Google Inc. nor the names of its
- * contributors may be used to endorse or promote products derived from
- * this software without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
- * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
- * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
- * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
- * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
- * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
- * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
- * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
- * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
- * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
-
-#include "config.h"
-#include "InspectorClient.h"
-
-#include "Frame.h"
-#include "InspectorController.h"
-#include "Page.h"
-#include "ScriptController.h"
-#include "ScriptSourceCode.h"
-#include <JavaScriptCore/FrameTracers.h>
-
-namespace WebCore {
-using namespace JSC;
-using namespace Inspector;
-
-void InspectorClient::doDispatchMessageOnFrontendPage(Page* frontendPage, const String& message)
-{
-    if (!frontendPage)
-        return;
-
-    SuspendExceptionScope scope(&frontendPage->inspectorController().vm());
-    String dispatchToFrontend = makeString("InspectorFrontendAPI.dispatchMessageAsync(", message, ");");
-    frontendPage->mainFrame().script().evaluate(ScriptSourceCode(dispatchToFrontend));
-}
-
-} // namespace WebCore

Source/WebCore/inspector/InspectorClient.h
@@ -69,8 +69,6 @@ public:
 #if ENABLE(REMOTE_INSPECTOR)
     virtual bool allowRemoteInspectionToPageDirectly() const { return false; }
 #endif
-
-    WEBCORE_EXPORT static void doDispatchMessageOnFrontendPage(Page* frontendPage, const String& message);
 };
 
 } // namespace WebCore

Source/WebCore/inspector/InspectorFrontendClientLocal.cpp
@@ -45,11 +45,13 @@
 #include "InspectorPageAgent.h"
 #include "Page.h"
 #include "ScriptController.h"
+#include "ScriptSourceCode.h"
 #include "ScriptState.h"
 #include "Settings.h"
 #include "Timer.h"
 #include "UserGestureIndicator.h"
 #include "WindowFeatures.h"
+#include <JavaScriptCore/FrameTracers.h>
 #include <JavaScriptCore/InspectorBackendDispatchers.h>
 #include <wtf/Deque.h>
 #include <wtf/text/CString.h>
@@ -214,7 +216,7 @@ bool InspectorFrontendClientLocal::canAttachWindow()
 
 void InspectorFrontendClientLocal::setDockingUnavailable(bool unavailable)
 {
-    evaluateOnLoad(makeString("[\"setDockingUnavailable\", ", unavailable ? "true" : "false", ']'));
+    dispatch(makeString("[\"setDockingUnavailable\", ", unavailable ? "true" : "false", ']'));
 }
 
 void InspectorFrontendClientLocal::changeAttachedWindowHeight(unsigned height)
@@ -284,7 +286,7 @@ void InspectorFrontendClientLocal::setAttachedWindow(DockSide dockSide)
 
     m_dockSide = dockSide;
 
-    evaluateOnLoad(makeString("[\"setDockSide\", \"", side, "\"]"));
+    dispatch(makeString("[\"setDockSide\", \"", side, "\"]"));
 }
 
 void InspectorFrontendClientLocal::restoreAttachedWindowHeight()
@@ -308,7 +310,7 @@ bool InspectorFrontendClientLocal::isDebuggingEnabled()
 
 void InspectorFrontendClientLocal::setDebuggingEnabled(bool enabled)
 {
-    evaluateOnLoad(makeString("[\"setDebuggingEnabled\", ", enabled ? "true" : "false", ']'));
+    dispatch(makeString("[\"setDebuggingEnabled\", ", enabled ? "true" : "false", ']'));
 }
 
 bool InspectorFrontendClientLocal::isTimelineProfilingEnabled()
@@ -320,7 +322,7 @@ bool InspectorFrontendClientLocal::isTimelineProfilingEnabled()
 
 void InspectorFrontendClientLocal::setTimelineProfilingEnabled(bool enabled)
 {
-    evaluateOnLoad(makeString("[\"setTimelineProfilingEnabled\", ", enabled ? "true" : "false", ']'));
+    dispatch(makeString("[\"setTimelineProfilingEnabled\", ", enabled ? "true" : "false", ']'));
 }
 
 bool InspectorFrontendClientLocal::isProfilingJavaScript()
@@ -332,28 +334,28 @@ bool InspectorFrontendClientLocal::isProfilingJavaScript()
 
 void InspectorFrontendClientLocal::startProfilingJavaScript()
 {
-    evaluateOnLoad("[\"startProfilingJavaScript\"]");
+    dispatch("[\"startProfilingJavaScript\"]");
 }
 
 void InspectorFrontendClientLocal::stopProfilingJavaScript()
 {
-    evaluateOnLoad("[\"stopProfilingJavaScript\"]");
+    dispatch("[\"stopProfilingJavaScript\"]");
 }
 
 void InspectorFrontendClientLocal::showConsole()
 {
-    evaluateOnLoad("[\"showConsole\"]");
+    dispatch("[\"showConsole\"]");
 }
 
 void InspectorFrontendClientLocal::showResources()
 {
-    evaluateOnLoad("[\"showResources\"]");
+    dispatch("[\"showResources\"]");
 }
 
 void InspectorFrontendClientLocal::showMainResourceForFrame(Frame* frame)
 {
     String frameId = m_inspectedPageController->ensurePageAgent().frameId(frame);
-    evaluateOnLoad(makeString("[\"showMainResourceForFrame\", \"", frameId, "\"]"));
+    dispatch(makeString("[\"showMainResourceForFrame\", \"", frameId, "\"]"));
 }
 
 unsigned InspectorFrontendClientLocal::constrainedAttachedWindowHeight(unsigned preferredHeight, unsigned totalWindowHeight)
@@ -381,6 +383,29 @@ unsigned InspectorFrontendClientLocal::inspectionLevel() const
     return m_inspectedPageController->inspectionLevel() + 1;
 }
 
+void InspectorFrontendClientLocal::dispatch(const String& signature)
+{
+    ASSERT(!signature.isEmpty());
+    ASSERT(signature.startsWith('['));
+    ASSERT(signature.endsWith(']'));
+
+    evaluateOnLoad("InspectorFrontendAPI.dispatch(" + signature + ")");
+}
+
+void InspectorFrontendClientLocal::dispatchMessage(const String& messageObject)
+{
+    ASSERT(!messageObject.isEmpty());
+
+    evaluateOnLoad("InspectorFrontendAPI.dispatchMessage(" + messageObject + ")");
+}
+
+void InspectorFrontendClientLocal::dispatchMessageAsync(const String& messageObject)
+{
+    ASSERT(!messageObject.isEmpty());
+
+    evaluateOnLoad("InspectorFrontendAPI.dispatchMessageAsync(" + messageObject + ")");
+}
+
 bool InspectorFrontendClientLocal::evaluateAsBoolean(const String& expression)
 {
     auto& state = *mainWorldExecState(&m_frontendPage->mainFrame());
@@ -389,10 +414,13 @@ bool InspectorFrontendClientLocal::evaluateAsBoolean(const String& expression)
 
 void InspectorFrontendClientLocal::evaluateOnLoad(const String& expression)
 {
-    if (m_frontendLoaded)
-        m_frontendPage->mainFrame().script().executeScript("if (InspectorFrontendAPI) InspectorFrontendAPI.dispatch(" + expression + ")");
-    else
+    if (!m_frontendLoaded) {
         m_evaluateOnLoad.append(expression);
+        return;
+    }
+
+    JSC::SuspendExceptionScope scope(&m_frontendPage->inspectorController().vm());
+    m_frontendPage->mainFrame().script().evaluate(ScriptSourceCode(expression));
 }
 
 Page* InspectorFrontendClientLocal::inspectedPage() const

Source/WebCore/inspector/InspectorFrontendClientLocal.h
@@ -116,6 +116,11 @@ public:
 
     WEBCORE_EXPORT Page* inspectedPage() const;
     Page* frontendPage() const { return m_frontendPage; }
+
+    WEBCORE_EXPORT void dispatch(const String& signature);
+    WEBCORE_EXPORT void dispatchMessage(const String& messageObject);
+    WEBCORE_EXPORT void dispatchMessageAsync(const String& messageObject);
+
 protected:
     virtual void setAttachedWindowHeight(unsigned) = 0;
     virtual void setAttachedWindowWidth(unsigned) = 0;

Source/WebCore/inspector/agents/InspectorTimelineAgent.cpp
@@ -38,6 +38,7 @@
 #include "Frame.h"
 #include "InspectorCPUProfilerAgent.h"
 #include "InspectorClient.h"
+#include "InspectorController.h"
 #include "InspectorMemoryAgent.h"
 #include "InspectorPageAgent.h"
 #include "InstrumentingAgents.h"

Source/WebInspectorUI/UserInterface/Controllers/TimelineManager.js
@@ -551,11 +551,11 @@ WI.TimelineManager = class TimelineManager extends WI.Object
             return;
 
         console.assert(this._activeRecording);
-        console.assert(isNaN(WI.networkManager.mainFrame.domContentReadyEventTimestamp));
 
         let computedTimestamp = this._activeRecording.computeElapsedTime(timestamp);
 
-        WI.networkManager.mainFrame.markDOMContentReadyEvent(computedTimestamp);
+        if (WI.networkManager.mainFrame)
+            WI.networkManager.mainFrame.markDOMContentReadyEvent(computedTimestamp);
 
         let eventMarker = new WI.TimelineMarker(computedTimestamp, WI.TimelineMarker.Type.DOMContentEvent);
         this._activeRecording.addEventMarker(eventMarker);
@@ -567,11 +567,11 @@ WI.TimelineManager = class TimelineManager extends WI.Object
             return;
 
         console.assert(this._activeRecording);
-        console.assert(isNaN(WI.networkManager.mainFrame.loadEventTimestamp));
 
         let computedTimestamp = this._activeRecording.computeElapsedTime(timestamp);
 
-        WI.networkManager.mainFrame.markLoadEvent(computedTimestamp);
+        if (WI.networkManager.mainFrame)
+            WI.networkManager.mainFrame.markLoadEvent(computedTimestamp);
 
         let eventMarker = new WI.TimelineMarker(computedTimestamp, WI.TimelineMarker.Type.LoadEvent);
         this._activeRecording.addEventMarker(eventMarker);

Source/WebInspectorUI/UserInterface/Models/Frame.js
@@ -266,11 +266,15 @@ WI.Frame = class Frame extends WI.Object
 
     markDOMContentReadyEvent(timestamp)
     {
+        console.assert(isNaN(this._domContentReadyEventTimestamp));
+
         this._domContentReadyEventTimestamp = timestamp || NaN;
     }
 
     markLoadEvent(timestamp)
     {
+        console.assert(isNaN(this._loadEventTimestamp));
+
         this._loadEventTimestamp = timestamp || NaN;
     }
 

Source/WebKitLegacy/cf/WebCoreSupport/WebInspectorClientCF.cpp
@@ -96,7 +96,7 @@ static void deleteSetting(const String& key)
 
 void WebInspectorClient::sendMessageToFrontend(const String& message)
 {
-    doDispatchMessageOnFrontendPage(m_frontendPage, message);
+    m_frontendClient->dispatchMessageAsync(message);
 }
 
 bool WebInspectorClient::inspectorAttachDisabled()

Source/WebCore/testing/Internals.cpp
@@ -322,7 +322,6 @@ public:
     virtual ~InspectorStubFrontend();
 
 private:
-    void frontendLoaded() final;
     void attachWindow(DockSide) final { }
     void detachWindow() final { }
     void closeWindow() final;
@@ -339,8 +338,6 @@ private:
     ConnectionType connectionType() const final { return ConnectionType::Local; }
 
     RefPtr<DOMWindow> m_frontendWindow;
-    Vector<String> m_messages;
-    bool m_loaded { false };
 };
 
 InspectorStubFrontend::InspectorStubFrontend(Page& inspectedPage, RefPtr<DOMWindow>&& frontendWindow)
@@ -358,15 +355,6 @@ InspectorStubFrontend::~InspectorStubFrontend()
     closeWindow();
 }
 
-void InspectorStubFrontend::frontendLoaded()
-{
-    m_loaded = true;
-
-    for (auto& message : m_messages)
-        sendMessageToFrontend(message);
-    m_messages.clear();
-}
-
 void InspectorStubFrontend::closeWindow()
 {
     if (!m_frontendWindow)
@@ -381,14 +369,7 @@ void InspectorStubFrontend::closeWindow()
 
 void InspectorStubFrontend::sendMessageToFrontend(const String& message)
 {
-    ASSERT_ARG(message, !message.isEmpty());
-
-    if (!m_loaded) {
-        m_messages.append(message);
-        return;
-    }
-
-    InspectorClient::doDispatchMessageOnFrontendPage(frontendPage(), message);
+    dispatchMessageAsync(message);
 }
 
 static bool markerTypeFrom(const String& markerType, DocumentMarker::MarkerType& result)

Source/WebInspectorUI/UserInterface/Test/TestStub.js
@@ -25,6 +25,7 @@
  */
 
 InspectorFrontendAPI = {};
+InspectorFrontendAPI.dispatch = function () { };
 InspectorFrontendAPI.dispatchMessageAsync = InspectorProtocol.dispatchMessageFromBackend;
 
 window.ProtocolTest = new ProtocolTestHarness();