5885017b366f442631577c80da2f71a87ed987c8
Pawel Stanek <p.stanek@metrological.com>
2019-09-12 16:10:24 +0200

    [EME] Fix flushing condition.

--- Source/WebCore/platform/graphics/gstreamer/eme/WebKitCommonEncryptionDecryptorGStreamer.cpp
+++ Source/WebCore/platform/graphics/gstreamer/eme/WebKitCommonEncryptionDecryptorGStreamer.cpp
@@ -211,19 +211,13 @@ static GstFlowReturn webkitMediaCommonEncryptionDecryptTransformInPlace(GstBaseT
     WebKitMediaCommonEncryptionDecrypt* self = WEBKIT_MEDIA_CENC_DECRYPT(base);
     WebKitMediaCommonEncryptionDecryptPrivate* priv = WEBKIT_MEDIA_CENC_DECRYPT_GET_PRIVATE(self);
 
-    LockHolder locker(priv->m_mutex);
-
-    if (priv->m_isFlushing) {
-        GST_DEBUG_OBJECT(self, "flushing");
-        return GST_FLOW_FLUSHING;
-    }
-
     GstProtectionMeta* protectionMeta = reinterpret_cast<GstProtectionMeta*>(gst_buffer_get_protection_meta(buffer));
     if (!protectionMeta) {
         GST_TRACE_OBJECT(self, "buffer %p has no protection meta, assuming it's not encrypted", buffer);
         return GST_FLOW_OK;
     }
 
+    LockHolder locker(priv->m_mutex);
     const GValue* streamEncryptionEventsList = gst_structure_get_value(protectionMeta->info, "stream-encryption-events");
     if (streamEncryptionEventsList && GST_VALUE_HOLDS_LIST(streamEncryptionEventsList)) {
         unsigned streamEncryptionEventsListSize = gst_value_list_get_size(streamEncryptionEventsList);
@@ -264,13 +258,19 @@ static GstFlowReturn webkitMediaCommonEncryptionDecryptTransformInPlace(GstBaseT
             return GST_FLOW_NOT_SUPPORTED;
         }
         if (!priv->m_condition.waitFor(priv->m_mutex, WEBCORE_GSTREAMER_EME_LICENSE_KEY_RESPONSE_TIMEOUT, [priv] { return priv->m_isFlushing || priv->m_keyReceived; })) {
-            if (priv->m_isFlushing) {
-                GST_DEBUG_OBJECT(self, "flushing");
-                return GST_FLOW_FLUSHING;
+            if (!priv->m_keyReceived) {
+                GST_ERROR_OBJECT(self, "key not available");
+                return GST_FLOW_NOT_SUPPORTED;
             }
             GST_ERROR_OBJECT(self, "key not available");
             return GST_FLOW_NOT_SUPPORTED;
         }
+
+        if (priv->m_isFlushing) {
+            GST_DEBUG_OBJECT(self, "flushing");
+            return GST_FLOW_FLUSHING;
+        }
+
         GST_DEBUG_OBJECT(self, "key received, continuing");
     }
 
