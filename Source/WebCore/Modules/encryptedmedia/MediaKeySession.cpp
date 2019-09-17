vivienne-w/WPEWebKit

[EME][OCDM] Handle challenges generated not as a result of the API call.

Source/WebCore/Modules/encryptedmedia/MediaKeySession.cpp
@@ -609,6 +609,29 @@ void MediaKeySession::enqueueMessage(MediaKeyMessageType messageType, const Shar
     m_eventQueue.enqueueEvent(WTFMove(messageEvent));
 }
 
+void MediaKeySession::onMessage(CDMInstanceClient::MessageType type, Ref<SharedBuffer>&& message)
+{
+    m_taskQueue.enqueueTask([this, type, message = WTFMove(message)] () mutable {
+        MediaKeyMessageType messageType;
+        switch (type) {
+        case CDMInstance::MessageType::LicenseRequest:
+            messageType = MediaKeyMessageType::LicenseRequest;
+            break;
+        case CDMInstance::MessageType::LicenseRenewal:
+            messageType = MediaKeyMessageType::LicenseRenewal;
+            break;
+        case CDMInstance::MessageType::LicenseRelease:
+            messageType = MediaKeyMessageType::LicenseRelease;
+            break;
+        case CDMInstance::MessageType::IndividualizationRequest:
+            messageType = MediaKeyMessageType::IndividualizationRequest;
+            break;
+        }
+
+        enqueueMessage(messageType, WTFMove(message));
+    });
+}
+
 void MediaKeySession::updateKeyStatuses(CDMInstance::KeyStatusVector&& inputStatuses)
 {
     // https://w3c.github.io/encrypted-media/#update-key-statuses

Source/WebCore/platform/encryptedmedia/CDMInstance.h
@@ -48,8 +48,10 @@ public:
     virtual ~CDMInstanceClient() = default;
 
     using KeyStatus = CDMKeyStatus;
+    using MessageType = CDMMessageType;
     using KeyStatusVector = Vector<std::pair<Ref<SharedBuffer>, KeyStatus>>;
     virtual void updateKeyStatuses(KeyStatusVector&&) = 0;
+    virtual void onMessage(MessageType, Ref<SharedBuffer>&&) = 0;
 };
 
 class CDMInstance : public RefCounted<CDMInstance> {

Source/WebCore/platform/graphics/gstreamer/eme/CDMOpenCDM.cpp
@@ -366,19 +366,26 @@ CDMInstanceOpenCDM::Session::~Session()
 
 void CDMInstanceOpenCDM::Session::challengeGeneratedCallback(RefPtr<SharedBuffer>&& buffer)
 {
+    std::optional<WebCore::MediaKeyMessageType> requestType;
+    auto message = buffer ? parseResponseMessage(*buffer, requestType) : nullptr;
+
     // This can be called as a result of e.g. requestLicense() but update() or remove() as well.
+    // This called not as a response to API call is also possible.
     if (!m_challengeCallbacks.isEmpty()) {
         std::optional<WebCore::MediaKeyMessageType> requestType;
-        m_message = buffer ? parseResponseMessage(*buffer, requestType) : nullptr;
+        m_message = WTFMove(message);
         m_needsIndividualization = requestType == CDMInstance::MessageType::IndividualizationRequest;
 
         for (const auto& challengeCallback : m_challengeCallbacks)
             challengeCallback(this);
         m_challengeCallbacks.clear();
-    } else {
+    } else if (!m_sessionChangedCallbacks.isEmpty()) {
         for (auto& sessionChangedCallback : m_sessionChangedCallbacks)
-            sessionChangedCallback(this, true, buffer.copyRef(), m_keyStatuses);
+            sessionChangedCallback(this, true, message.copyRef(), m_keyStatuses);
         m_sessionChangedCallbacks.clear();
+    } else {
+        if (m_parent->client() && requestType.has_value())
+            m_parent->client()->onMessage(static_cast<CDMInstanceClient::MessageType>(requestType.value()), message.releaseNonNull());
     }
 }
 

WebKit/WebKit-http

Source/WebCore/Modules/encryptedmedia/MediaKeySession.cpp
@@ -619,6 +619,29 @@ void MediaKeySession::enqueueMessage(MediaKeyMessageType messageType, const Shar
     m_eventQueue.enqueueEvent(WTFMove(messageEvent));
 }
 
+void MediaKeySession::enqueueMessage(CDMInstanceClient::MessageType type, Ref<SharedBuffer>&& message)
+{
+    m_taskQueue.enqueueTask([this, type, message = WTFMove(message)] () mutable {
+        MediaKeyMessageType messageType;
+        switch (type) {
+        case CDMInstance::MessageType::LicenseRequest:
+            messageType = MediaKeyMessageType::LicenseRequest;
+            break;
+        case CDMInstance::MessageType::LicenseRenewal:
+            messageType = MediaKeyMessageType::LicenseRenewal;
+            break;
+        case CDMInstance::MessageType::LicenseRelease:
+            messageType = MediaKeyMessageType::LicenseRelease;
+            break;
+        case CDMInstance::MessageType::IndividualizationRequest:
+            messageType = MediaKeyMessageType::IndividualizationRequest;
+            break;
+        }
+
+        enqueueMessage(messageType, WTFMove(message));
+    });
+}
+
 void MediaKeySession::updateKeyStatuses(CDMInstanceClient::KeyStatusVector&& inputStatuses)
 {
     // https://w3c.github.io/encrypted-media/#update-key-statuses

Source/WebCore/platform/encryptedmedia/CDMInstance.h
@@ -48,8 +48,10 @@ public:
     virtual ~CDMInstanceClient() = default;
 
     using KeyStatus = CDMKeyStatus;
+    using MessageType = CDMMessageType;
     using KeyStatusVector = Vector<std::pair<Ref<SharedBuffer>, KeyStatus>>;
     virtual void updateKeyStatuses(KeyStatusVector&&) = 0;
+    virtual void enqueueMessage(MessageType, Ref<SharedBuffer>&&) = 0;
 };
 
 class CDMInstance : public RefCounted<CDMInstance> {

Source/WebCore/platform/graphics/gstreamer/eme/CDMOpenCDM.cpp
@@ -366,19 +366,26 @@ CDMInstanceOpenCDM::Session::~Session()
 
 void CDMInstanceOpenCDM::Session::challengeGeneratedCallback(RefPtr<SharedBuffer>&& buffer)
 {
+    std::optional<WebCore::MediaKeyMessageType> requestType;
+    auto message = buffer ? parseResponseMessage(*buffer, requestType) : nullptr;
+
     // This can be called as a result of e.g. requestLicense() but update() or remove() as well.
+    // This called not as a response to API call is also possible.
     if (!m_challengeCallbacks.isEmpty()) {
         std::optional<WebCore::MediaKeyMessageType> requestType;
-        m_message = buffer ? parseResponseMessage(*buffer, requestType) : nullptr;
+        m_message = WTFMove(message);
         m_needsIndividualization = requestType == CDMInstance::MessageType::IndividualizationRequest;
 
         for (const auto& challengeCallback : m_challengeCallbacks)
             challengeCallback(this);
         m_challengeCallbacks.clear();
-    } else {
+    } else if (!m_sessionChangedCallbacks.isEmpty()) {
         for (auto& sessionChangedCallback : m_sessionChangedCallbacks)
-            sessionChangedCallback(this, true, buffer.copyRef(), m_keyStatuses);
+            sessionChangedCallback(this, true, message.copyRef(), m_keyStatuses);
         m_sessionChangedCallbacks.clear();
+    } else {
+        if (m_parent->client() && requestType.has_value())
+            m_parent->client()->enqueueMessage(static_cast<CDMInstanceClient::MessageType>(requestType.value()), message.releaseNonNull());
     }
 }
 