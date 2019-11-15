WebKit/WebKit-http

commit d14abbaa573ae5b73d929b3880b1c045fc53a20a

Integrate scroll event into HTML5 event loop https://bugs.webkit.org/show_bug.cgi?id=203839 <rdar://problem/56890922>
Reviewed by Simon Fraser.

Source/WebCore:

Dispatch scroll events on each pending scroll event targets inside the update-the-rendering step
of the event loop as specified in the HTML5 and CSSOM View Module specifications:
https://html.spec.whatwg.org/multipage/webappapis.html#update-the-rendering
https://drafts.csswg.org/cssom-view/#run-the-scroll-steps

The new behavior also matches that of Chrome and Firefox.

Like resize event, the current visual viewport specification does not specify when scroll event
is dispatched on visualViewport interface: https://github.com/WICG/visual-viewport/issues/66
For now, we always dispatch upon it after dispatching on all pending scroll event targets.

Tests: fast/events/scroll-multiple-elements-in-rendering-update.html
       fast/events/scroll-subframe-in-rendering-update.html
       fast/visual-viewport/visual-viewport-scroll-after-resize-in-subframe.html

* dom/Document.cpp:
(WebCore::Document::addPendingScrollEventTarget): Added.
(WebCore::Document::setNeedsVisualViewportScrollEvent): Added.
(WebCore::Document::runScrollSteps): Added. This implements run the scroll steps in CSS OM View.
* dom/Document.h:
* dom/DocumentEventQueue.cpp:
(WebCore::DocumentEventQueue::enqueueOrDispatchScrollEvent): Deleted.
(WebCore::DocumentEventQueue::enqueueScrollEvent): Deleted.
* dom/DocumentEventQueue.h:
* page/EventHandler.cpp:
(WebCore::EventHandler::sendScrollEvent):
* page/Page.cpp:
(WebCore::Page::updateRendering): Invoke runScrollSteps on each document in the frame tree order.
* page/VisualViewport.cpp:
(WebCore::VisualViewport::update):
(WebCore::VisualViewport::enqueueScrollEvent): Deleted.
* page/VisualViewport.h:
* rendering/RenderLayer.cpp:
(WebCore::RenderLayer::scrollTo):
* rendering/RenderListBox.cpp:
(WebCore::RenderListBox::scrollTo):

LayoutTests:

Source/WebCore/dom/Document.cpp
@@ -4001,6 +4001,48 @@ void Document::runResizeSteps()
     }
 }
 
+void Document::addPendingScrollEventTarget(ContainerNode& target)
+{
+    if (m_pendingScrollEventTargets.contains(&target))
+        return;
+
+    if (m_pendingScrollEventTargets.isEmpty())
+        scheduleTimedRenderingUpdate();
+
+    m_pendingScrollEventTargets.append(makeWeakPtr(target));
+}
+
+void Document::setNeedsVisualViewportScrollEvent()
+{
+    if (!m_needsVisualViewportScrollEvent)
+        scheduleTimedRenderingUpdate();
+    m_needsVisualViewportScrollEvent = true;
+}
+
+// https://drafts.csswg.org/cssom-view/#run-the-scroll-steps
+void Document::runScrollSteps()
+{
+    // FIXME: The order of dispatching is not specified: https://github.com/WICG/visual-viewport/issues/66.
+    if (!m_pendingScrollEventTargets.isEmpty()) {
+        LOG_WITH_STREAM(Events, stream << "Document" << this << "sending scroll events to pending scroll event targets");
+        auto currentTargets = WTFMove(m_pendingScrollEventTargets);
+        for (auto target : currentTargets) {
+            auto protectedTarget = makeRefPtr(target.get());
+            ASSERT(protectedTarget);
+            if (!protectedTarget)
+                continue;
+            auto bubbles = protectedTarget->isDocumentNode() ? Event::CanBubble::Yes : Event::CanBubble::No;
+            protectedTarget->dispatchEvent(Event::create(eventNames().scrollEvent, bubbles, Event::IsCancelable::No));
+        }
+    }
+    if (m_needsVisualViewportScrollEvent) {
+        LOG_WITH_STREAM(Events, stream << "Document" << this << "sending scroll events to visualViewport");
+        m_needsVisualViewportResizeEvent = false;
+        if (auto* window = domWindow())
+            window->visualViewport().dispatchEvent(Event::create(eventNames().scrollEvent, Event::CanBubble::No, Event::IsCancelable::No));
+    }
+}
+
 void Document::addAudioProducer(MediaProducer& audioProducer)
 {
     m_audioProducers.add(audioProducer);

Source/WebCore/dom/Document.h
@@ -1364,6 +1364,10 @@ public:
     void setNeedsVisualViewportResize();
     void runResizeSteps();
 
+    void addPendingScrollEventTarget(ContainerNode&);
+    void setNeedsVisualViewportScrollEvent();
+    void runScrollSteps();
+
     WEBCORE_EXPORT void addAudioProducer(MediaProducer&);
     WEBCORE_EXPORT void removeAudioProducer(MediaProducer&);
     MediaProducer::MediaStateFlags mediaState() const { return m_mediaState; }
@@ -2019,6 +2023,7 @@ private:
     bool m_hasStyleWithViewportUnits { false };
     bool m_needsDOMWindowResizeEvent { false };
     bool m_needsVisualViewportResizeEvent { false };
+    bool m_needsVisualViewportScrollEvent { false };
     bool m_isTimerThrottlingEnabled { false };
     bool m_isSuspended { false };
 
@@ -2034,6 +2039,8 @@ private:
 #if ENABLE(TELEPHONE_NUMBER_DETECTION)
     bool m_isTelephoneNumberParsingAllowed { true };
 #endif
+
+    Vector<WeakPtr<ContainerNode>> m_pendingScrollEventTargets;
 
 #if ENABLE(MEDIA_STREAM)
     HashSet<HTMLMediaElement*> m_mediaStreamStateChangeElements;

LayoutTests/editing/selection/overflow-scroll-while-selecting-text.html
@@ -30,11 +30,13 @@ description("This test verifies that we don't crash while scrolling an overflow 
 
 const text = document.getElementById("text");
 text.scrollTo(0, 1000);
-addEventListener("load", () => text.scrollTo(0, 500));
-text.addEventListener("scroll", () => {
-    text.textContent = "";
-    testPassed("Successfully removed the text.");
-    finishJSTest();
+addEventListener("load", () => {
+    text.addEventListener("scroll", () => {
+        text.textContent = "";
+        testPassed("Successfully removed the text.");
+        finishJSTest();
+    });
+    text.scrollTo(0, 500);
 });
 </script>
 </html>

LayoutTests/fast/events/scroll-subframe-in-rendering-update-expected.txt
@@ -1,0 +1,16 @@
+This tests that scroll event is dispatched in the update-the-rendering step in the event loop across all relevant frames
+
+On success, you will see a series of "PASS" messages, followed by "TEST COMPLETE".
+
+
+iframeB.contentWindow.scrollTo(0, 1000); updateLayout(iframeB)
+iframeA.contentWindow.scrollTo(0, 1000); updateLayout(iframeA)
+iframeAA.contentWindow.scrollTo(0, 1000); updateLayout(iframeAA)
+PASS logs.length is 0
+After requestAnimationFrame
+PASS logs.length is 3
+PASS logs.join(", ") is "A, AA, B"
+PASS successfullyParsed is true
+
+TEST COMPLETE
+

LayoutTests/fast/events/scroll-subframe-in-rendering-update.html
@@ -1,0 +1,55 @@
+<!DOCTYPE html>
+<html>
+<body>
+<script src="../../resources/js-test.js"></script>
+<script>
+
+description('This tests that scroll event is dispatched in the update-the-rendering step in the event loop across all relevant frames');
+jsTestIsAsync = true;
+
+function createIframe(parentDocument)
+{
+    const iframe = document.createElement('iframe');
+    iframe.style.width = '100px';
+    iframe.style.height = '100px';
+    parentDocument.body.appendChild(iframe);
+    iframe.contentDocument.body.innerHTML = '<style> body { height: 2000px; } </style><span>hello, world</span>';
+    return iframe;
+}
+
+function updateLayout(iframe)
+{
+    iframe.contentDocument.querySelector("span").getBoundingClientRect();
+}
+
+const iframeA = createIframe(document);
+const iframeAA = createIframe(iframeA.contentDocument);
+const iframeB = createIframe(document);
+
+const logs = [];
+requestAnimationFrame(() => {
+    setTimeout(() => {
+        iframeAA.contentDocument.addEventListener('scroll', () => logs.push('AA'));
+        iframeA.contentDocument.addEventListener('scroll', () => logs.push('A'));
+        iframeB.contentDocument.addEventListener('scroll', () => logs.push('B'));
+
+        evalAndLog('iframeB.contentWindow.scrollTo(0, 1000); updateLayout(iframeB)');
+        evalAndLog('iframeA.contentWindow.scrollTo(0, 1000); updateLayout(iframeA)');
+        evalAndLog('iframeAA.contentWindow.scrollTo(0, 1000); updateLayout(iframeAA)');
+        shouldBe('logs.length', '0');
+        setTimeout(() => {
+            requestAnimationFrame(() => {
+                debug('After requestAnimationFrame');
+                shouldBe('logs.length', '3');
+                shouldBeEqualToString('logs.join(", ")', 'A, AA, B');
+                iframeA.remove();
+                iframeB.remove();
+                finishJSTest();
+            });
+        }, 0);
+    }, 0);
+});
+
+</script>
+</body>
+</html>

LayoutTests/fast/scrolling/programmatic-scroll-to-negative-offset.html
@@ -25,13 +25,13 @@
             scroller = document.getElementById('scroller');
 			scroller.scrollLeft = 50;
 
-			scroller.addEventListener('scroll', (event) => {
-				shouldBe("scroller.scrollLeft", "0");
-				shouldBe("scroller.scrollLeft", "0");
-				finishJSTest();
-			}, false);
-
 			setTimeout(() => {
+    			scroller.addEventListener('scroll', (event) => {
+    				shouldBe("scroller.scrollLeft", "0");
+    				shouldBe("scroller.scrollLeft", "0");
+    				finishJSTest();
+    			}, false);
+
 				scroller.scrollLeft = -200;
 				scroller.scrollTop = -100;
 			}, 0)

LayoutTests/fast/shadow-dom/trusted-event-scoped-flags.html
@@ -96,11 +96,13 @@ function testScrollEvent() {
     log(window, 'scroll');
     setTimeout(function () {
         window.scrollTo(0, 1000);
-        setTimeout(function () {
-            checkFlags('', {eventType: 'scroll', composed: false});
-            window.scrollTo(0, 0);
-            testResizeEvent();
-        }, 0);
+        requestAnimationFrame(() => {
+            setTimeout(function () {
+                checkFlags('', {eventType: 'scroll', composed: false});
+                window.scrollTo(0, 0);
+                testResizeEvent();
+            }, 0);
+        });
     }, 0);
 }
 

LayoutTests/fast/visual-viewport/visual-viewport-scroll-after-resize-in-subframe-expected.txt
@@ -1,0 +1,9 @@
+Tests that scroll event on visualViewport fires after resize event in a subframe. To manually test, pinch zoom.
+
+On success, you will see a series of "PASS" messages, followed by "TEST COMPLETE".
+
+
+PASS successfullyParsed is true
+
+TEST COMPLETE
+

commit 1779ac7a551b597ffcac5f7201d145159ab6c1b8

JS wrappers of scroll event targets can get prematurely collected by GC https://bugs.webkit.org/show_bug.cgi?id=204219
Reviewed by Keith Miller.

Source/WebCore:

This patch addresses the bug that the JS wrappers of the pending scroll event targets can be
collected by GC before scroll events are fired. This bug has always existed but it's worse after
r252205 because there is more of a time delay between an element is scrolled in RenderLayer sense
and when the corresponding scroll event is fired on the element.

Fixed the bug by using GCReachableRef to store the pending scroll event targets introduced in r252205
to keep the JS wrappers of those elements & document alive.

Test: fast/scrolling/scrolling-event-target-gc.html

* dom/Document.cpp:
(WebCore::Document::PendingScrollEventTargetList): Added. Wraps Vector<GCReachableRef<ContainerNode>>
to avoid including GCReachableRef.h in Document.h.
(WebCore::Document::commonTeardown): Clear m_pendingScrollEventTargetList to avoid document leaks.
(WebCore::Document::addPendingScrollEventTarget):
(WebCore::Document::runScrollSteps):
* dom/Document.h:

LayoutTests:

Added a regression test for adding elements as pending scroll event targets,
and removing them from the document before scroll events are fired.

* fast/scrolling/scrolling-event-target-gc-expected.txt: Added.
* fast/scrolling/scrolling-event-target-gc.html: Added.


git-svn-id: http://svn.webkit.org/repository/webkit/trunk@252504 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Source/WebCore/dom/Document.cpp
@@ -78,6 +78,7 @@
 #include "FrameLoaderClient.h"
 #include "FrameView.h"
 #include "FullscreenManager.h"
+#include "GCReachableRef.h"
 #include "GenericCachedHTMLCollection.h"
 #include "HTMLAllCollection.h"
 #include "HTMLAnchorElement.h"
@@ -361,6 +362,14 @@ struct FrameFlatteningLayoutDisallower {
 private:
     FrameView& m_frameView;
     bool m_disallowLayout { false };
+};
+
+// Defined here to avoid including GCReachableRef.h in Document.h
+struct Document::PendingScrollEventTargetList {
+    WTF_MAKE_FAST_ALLOCATED;
+
+public:
+    Vector<GCReachableRef<ContainerNode>> targets;
 };
 
 #if ENABLE(INTERSECTION_OBSERVER)
@@ -759,6 +768,8 @@ void Document::commonTeardown()
         accessSVGExtensions().pauseAnimations();
 
     clearScriptedAnimationController();
+
+    m_pendingScrollEventTargetList = nullptr;
 }
 
 Element* Document::elementForAccessKey(const String& key)
@@ -4003,13 +4014,17 @@ void Document::runResizeSteps()
 
 void Document::addPendingScrollEventTarget(ContainerNode& target)
 {
-    if (m_pendingScrollEventTargets.contains(&target))
+    if (!m_pendingScrollEventTargetList)
+        m_pendingScrollEventTargetList = makeUnique<PendingScrollEventTargetList>();
+
+    auto& targets = m_pendingScrollEventTargetList->targets;
+    if (targets.findMatching([&] (auto& entry) { return entry.ptr() == &target; }) != notFound)
         return;
 
-    if (m_pendingScrollEventTargets.isEmpty())
+    if (targets.isEmpty())
         scheduleTimedRenderingUpdate();
 
-    m_pendingScrollEventTargets.append(makeWeakPtr(target));
+    targets.append(target);
 }
 
 void Document::setNeedsVisualViewportScrollEvent()
@@ -4023,16 +4038,12 @@ void Document::setNeedsVisualViewportScrollEvent()
 void Document::runScrollSteps()
 {
     // FIXME: The order of dispatching is not specified: https://github.com/WICG/visual-viewport/issues/66.
-    if (!m_pendingScrollEventTargets.isEmpty()) {
+    if (m_pendingScrollEventTargetList && !m_pendingScrollEventTargetList->targets.isEmpty()) {
         LOG_WITH_STREAM(Events, stream << "Document" << this << "sending scroll events to pending scroll event targets");
-        auto currentTargets = WTFMove(m_pendingScrollEventTargets);
-        for (auto target : currentTargets) {
-            auto protectedTarget = makeRefPtr(target.get());
-            ASSERT(protectedTarget);
-            if (!protectedTarget)
-                continue;
-            auto bubbles = protectedTarget->isDocumentNode() ? Event::CanBubble::Yes : Event::CanBubble::No;
-            protectedTarget->dispatchEvent(Event::create(eventNames().scrollEvent, bubbles, Event::IsCancelable::No));
+        auto currentTargets = WTFMove(m_pendingScrollEventTargetList->targets);
+        for (auto& target : currentTargets) {
+            auto bubbles = target->isDocumentNode() ? Event::CanBubble::Yes : Event::CanBubble::No;
+            target->dispatchEvent(Event::create(eventNames().scrollEvent, bubbles, Event::IsCancelable::No));
         }
     }
     if (m_needsVisualViewportScrollEvent) {

Source/WebCore/dom/Document.h
@@ -2041,7 +2041,8 @@ private:
     bool m_isTelephoneNumberParsingAllowed { true };
 #endif
 
-    Vector<WeakPtr<ContainerNode>> m_pendingScrollEventTargets;
+    struct PendingScrollEventTargetList;
+    std::unique_ptr<PendingScrollEventTargetList> m_pendingScrollEventTargetList;
 
 #if ENABLE(MEDIA_STREAM)
     HashSet<HTMLMediaElement*> m_mediaStreamStateChangeElements;

LayoutTests/fast/scrolling/scrolling-event-target-gc-expected.txt
@@ -1,0 +1,5 @@
+This tests scheduling a scroll event on an element then removing the element.
+WebKit should not collect its JS wrappers.
+
+PASS
+

LayoutTests/fast/scrolling/scrolling-event-target-gc.html
@@ -1,0 +1,69 @@
+<!DOCTYPE html><!-- webkit-test-runner [ internal:AsyncOverflowScrollingEnabled=true ] -->
+<html>
+<body>
+<style>
+.container > div { width: 10px; height: 10px; overflow: scroll; }
+</style>
+<p>This tests scheduling a scroll event on an element then removing the element.<br>WebKit should not collect its JS wrappers.</p>
+<pre id="result"></pre>
+<div class="container"></div>
+<script src="../../resources/gc.js"></script>
+<script>
+
+if (window.testRunner) {
+    testRunner.waitUntilDone();
+    testRunner.dumpAsText();
+}
+
+const logs = [];
+let currentNumber = 0;
+function scheduleScrollEvents(count)
+{
+    let container = document.querySelector('.container');
+    for (let i = 0; i < count; i++) {
+        const div = document.createElement('div');
+        div.innerHTML = '<p>hello, world<br>this is a test</p>';
+        container.appendChild(div);
+        div.liveNumber = currentNumber;
+        div.addEventListener('scroll', log);
+        currentNumber++;
+    }
+
+    for (const div of container.children)
+        div.scrollTo(10, 10);
+
+    container.textContent = '';
+}
+
+function log()
+{
+    logs.push(this.liveNumber);
+}
+
+function checkLogs()
+{
+    let failed = false;
+    for (let i = 0; i < logs.length; ++i) {
+        if (logs[i] != i) {
+            result.textContent += `FAIL at ${i}: ${logs[i]}\n`;
+            failed = true;
+        }
+    }
+    if (!failed)
+        result.textContent = 'PASS';
+}
+
+for (let i = 0; i < 5; ++i) {
+    scheduleScrollEvents(20);
+    gc();
+}
+
+requestAnimationFrame(() => {
+    checkLogs();
+    if (window.testRunner)
+        testRunner.notifyDone();
+});
+
+</script>
+</body>
+</html>