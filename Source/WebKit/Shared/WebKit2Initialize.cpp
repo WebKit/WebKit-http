WebKit/WebKit-http

commit 14c083a2defb41fd1e10502cb0358551801bf4b9

Call AtomString::init in WebKit2Initialize. <https://webkit.org/b/206375> <rdar://problem/58646578>
Patch by Daniel Chen <thedanielchen@apple.com> on 2020-01-16
Reviewed by Timothy Hatcher.

* Shared/Cocoa/WebKit2InitializeCocoa.mm:
(WebKit::runInitializationCode):
* Shared/WebKit2Initialize.cpp:
(WebKit::InitializeWebKit2):

git-svn-id: http://svn.webkit.org/repository/webkit/trunk@254723 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Source/WebKit/Shared/WebKit2Initialize.cpp
@@ -39,6 +39,7 @@ namespace WebKit {
 
 void InitializeWebKit2()
 {
+    AtomString::init();
     JSC::initializeThreading();
     RunLoop::initializeMainRunLoop();
 

Source/WebKit/Shared/Cocoa/WebKit2InitializeCocoa.mm
@@ -44,6 +44,7 @@ static std::once_flag flag;
 
 static void runInitializationCode(void* = nullptr)
 {
+    AtomString::init();
 #if PLATFORM(IOS_FAMILY)
     InitWebCoreThreadSystemInterface();
 #endif

commit 235ff90a57a8b37e0bd850232b5c681c2655df31

[WinCairo][WK2] ASSERT(isUIThread()) is failing since r254723 https://bugs.webkit.org/show_bug.cgi?id=206394
Reviewed by Carlos Alberto Lopez Perez.

AtomString::init() should be called after
WTF::initializeThreading() is called on Windows because it has
ASSERT(isUIThread()) and Windows doesn't have a API to get main
thread handle like pthread_main_np.

* Shared/WebKit2Initialize.cpp:
(WebKit::InitializeWebKit2): Call AtomString::init() after JSC::initializeThreading().


git-svn-id: http://svn.webkit.org/repository/webkit/trunk@254746 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Source/WebKit/Shared/WebKit2Initialize.cpp
@@ -39,9 +39,9 @@ namespace WebKit {
 
 void InitializeWebKit2()
 {
-    AtomString::init();
     JSC::initializeThreading();
     RunLoop::initializeMainRunLoop();
+    AtomString::init();
 
     WTF::RefCountedBase::enableThreadingChecksGlobally();
 