WebKit/WebKit-http

commit 004b9e6b273a7d26edfd536981f352703042e717

[GTK] Stop using gtk foreign drawing API to style form controls https://bugs.webkit.org/show_bug.cgi?id=208129
Reviewed by Adrian Perez de Castro.

.:

Add a test case for datalist element.

* ManualTests/gtk/theme.html:

Source/WebCore:

It causes layout issues in some websites, it doesn't really work with all GTK themes and it won't be possible
with GTK4 because foreign drawing APIs have been removed. Instead, we can use the new custom style used by WPE
port, which is based on adwaita, but simplified to avoid the huge minimum control sizes, the usage of gradients
and transparencies, etc. We can still use the GTK API to get the selection colors, to keep some consistency with
the actual GTK theme, but that won't be possible with GTK4 either. This also means we won't be rendering
directly to the cairo context anymore (which was required by GTK foreign drawing), so we can use things like
threaded rendering in the GTK port now. This patch renames ScrollbarThemeWPE, RenderThemeWPE and ThemeWPE as
ScrollbarThemeAdwaita, RenderThemeAdwaita and ThemeAdwaita and adds ThemeGtk. GTK media controls CSS and
JavaScript files have been removed in favor of using the adwaita ones.

* Modules/mediacontrols/mediaControlsGtk.js: Removed.
* PlatformGTK.cmake:
* PlatformWPE.cmake:
* SourcesGTK.txt:
* SourcesWPE.txt:
* css/mediaControlsGtk.css: Removed.
* platform/adwaita/ScrollbarThemeAdwaita.cpp: Renamed from Source/WebCore/platform/wpe/ScrollbarThemeWPE.cpp.
(WebCore::ScrollbarThemeAdwaita::usesOverlayScrollbars const):
(WebCore::ScrollbarThemeAdwaita::scrollbarThickness):
(WebCore::ScrollbarThemeAdwaita::minimumThumbLength):
(WebCore::ScrollbarThemeAdwaita::hasButtons):
(WebCore::ScrollbarThemeAdwaita::hasThumb):
(WebCore::ScrollbarThemeAdwaita::backButtonRect):
(WebCore::ScrollbarThemeAdwaita::forwardButtonRect):
(WebCore::ScrollbarThemeAdwaita::trackRect):
(WebCore::ScrollbarThemeAdwaita::paint):
(WebCore::ScrollbarThemeAdwaita::handleMousePressEvent):

Source/WebCore/platform/adwaita/ThemeAdwaita.cpp
@@ -1,0 +1,389 @@
+/*
+ * Copyright (C) 2015, 2020 Igalia S.L.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY APPLE INC. AND ITS CONTRIBUTORS ``AS IS''
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL APPLE INC. OR ITS CONTRIBUTORS
+ * BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
+ * THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include "config.h"
+#include "ThemeAdwaita.h"
+
+#include "Color.h"
+#include "ControlStates.h"
+#include "FloatRoundedRect.h"
+#include "GraphicsContext.h"
+#include "LengthSize.h"
+#include <wtf/NeverDestroyed.h>
+
+namespace WebCore {
+
+static const unsigned focusLineWidth = 1;
+static const Color focusRingColor = makeRGBA(46, 52, 54, 150);
+static const unsigned arrowSize = 16;
+static const Color arrowColor = makeRGB(46, 52, 54);
+static const int buttonFocusOffset = -3;
+static const unsigned buttonPadding = 5;
+static const int buttonBorderSize = 1; // Keep in sync with menuListButtonBorderSize in RenderThemeAdwaita.
+static const Color buttonBorderColor = makeRGB(205, 199, 194);
+static const Color buttonBackgroundColor = makeRGB(244, 242, 241);
+static const Color buttonBackgroundPressedColor = makeRGB(214, 209, 205);
+static const Color buttonBackgroundHoveredColor = makeRGB(248, 248, 247);
+static const Color buttonBackgroundDisabledColor = makeRGB(246, 246, 244);
+static const Color toggleBackgroundColor = makeRGB(255, 255, 255);
+static const Color toggleBackgroundHoveredColor = makeRGB(242, 242, 242);
+static const Color toggleBackgroundDisabledColor = makeRGB(252, 252, 252);
+static const double toggleSize = 14.;
+static const int toggleFocusOffset = 2;
+static const Color toggleColor = makeRGB(46, 52, 54);
+static const Color toggleDisabledColor = makeRGB(160, 160, 160);
+static const Color spinButtonBorderColor = makeRGB(220, 223, 227);
+static const Color spinButtonBackgroundColor = makeRGB(252, 252, 252);
+static const Color spinButtonBackgroundHoveredColor = makeRGBA(46, 52, 54, 50);
+static const Color spinButtonBackgroundPressedColor = makeRGBA(46, 52, 54, 70);
+
+#if !PLATFORM(GTK)
+Theme& Theme::singleton()
+{
+    static NeverDestroyed<ThemeAdwaita> theme;
+    return theme;
+}
+#endif
+
+Color ThemeAdwaita::activeSelectionForegroundColor() const
+{
+    return makeRGB(255, 255, 255);
+}
+
+Color ThemeAdwaita::activeSelectionBackgroundColor() const
+{
+    return makeRGB(52, 132, 228);
+}
+
+Color ThemeAdwaita::inactiveSelectionForegroundColor() const
+{
+    return makeRGB(252, 252, 252);
+}
+
+Color ThemeAdwaita::inactiveSelectionBackgroundColor() const
+{
+    return activeSelectionBackgroundColor();
+}
+
+Color ThemeAdwaita::focusColor()
+{
+    return focusRingColor;
+}
+
+void ThemeAdwaita::paintFocus(GraphicsContext& graphicsContext, const FloatRect& rect, int offset)
+{
+    FloatRect focusRect = rect;
+    focusRect.inflate(offset);
+    Path path;
+    path.addRoundedRect(focusRect, { 2, 2 });
+    paintFocus(graphicsContext, path, focusRingColor);
+}
+
+void ThemeAdwaita::paintFocus(GraphicsContext& graphicsContext, const Path& path, const Color& color)
+{
+    GraphicsContextStateSaver stateSaver(graphicsContext);
+
+    graphicsContext.beginTransparencyLayer(color.alphaAsFloat());
+    graphicsContext.setStrokeThickness(focusLineWidth);
+    graphicsContext.setLineDash({ focusLineWidth, 2 * focusLineWidth }, 0);
+    graphicsContext.setLineCap(SquareCap);
+    graphicsContext.setLineJoin(MiterJoin);
+    graphicsContext.setStrokeColor(color.opaqueColor());
+    graphicsContext.strokePath(path);
+    graphicsContext.setFillRule(WindRule::NonZero);
+    graphicsContext.setCompositeOperation(CompositeOperator::Clear);
+    graphicsContext.fillPath(path);
+    graphicsContext.setCompositeOperation(CompositeOperator::SourceOver);
+    graphicsContext.endTransparencyLayer();
+}
+
+void ThemeAdwaita::paintFocus(GraphicsContext& graphicsContext, const Vector<FloatRect>& rects, const Color& color)
+{
+    FloatSize corner(2, 2);
+    Path path;
+    for (const auto& rect : rects)
+        path.addRoundedRect(rect, corner);
+    paintFocus(graphicsContext, path, color);
+}
+
+void ThemeAdwaita::paintArrow(GraphicsContext& graphicsContext, ArrowDirection direction)
+{
+    Path path;
+    switch (direction) {
+    case ArrowDirection::Down:
+        path.moveTo({ 3, 6 });
+        path.addLineTo({ 13, 6 });
+        path.addLineTo({ 8, 11 });
+        break;
+    case ArrowDirection::Up:
+        path.moveTo({ 3, 10 });
+        path.addLineTo({ 8, 5 });
+        path.addLineTo({ 13, 10});
+        break;
+    }
+    path.closeSubpath();
+
+    graphicsContext.setFillColor(arrowColor);
+    graphicsContext.fillPath(path);
+}
+
+LengthSize ThemeAdwaita::controlSize(ControlPart part, const FontCascade& fontCascade, const LengthSize& zoomedSize, float zoomFactor) const
+{
+    if (!zoomedSize.width.isIntrinsicOrAuto() && !zoomedSize.height.isIntrinsicOrAuto())
+        return Theme::controlSize(part, fontCascade, zoomedSize, zoomFactor);
+
+    switch (part) {
+    case CheckboxPart:
+    case RadioPart:
+        return LengthSize { Length(12, Fixed), Length(12, Fixed) };
+    case InnerSpinButtonPart: {
+        LengthSize spinButtonSize = zoomedSize;
+        if (spinButtonSize.width.isIntrinsicOrAuto())
+            spinButtonSize.width = Length(static_cast<int>(arrowSize), Fixed);
+        if (spinButtonSize.height.isIntrinsicOrAuto() || fontCascade.pixelSize() > static_cast<int>(arrowSize))
+            spinButtonSize.height = Length(fontCascade.pixelSize(), Fixed);
+        return spinButtonSize;
+    }
+    default:
+        break;
+    }
+
+    return Theme::controlSize(part, fontCascade, zoomedSize, zoomFactor);
+}
+
+void ThemeAdwaita::paint(ControlPart part, ControlStates& states, GraphicsContext& context, const FloatRect& zoomedRect, float zoomFactor, ScrollView*, float, float, bool, bool)
+{
+    switch (part) {
+    case CheckboxPart:
+        paintCheckbox(states, context, zoomedRect, zoomFactor);
+        break;
+    case RadioPart:
+        paintRadio(states, context, zoomedRect, zoomFactor);
+        break;
+    case PushButtonPart:
+    case DefaultButtonPart:
+    case ButtonPart:
+    case SquareButtonPart:
+        paintButton(states, context, zoomedRect, zoomFactor);
+        break;
+    case InnerSpinButtonPart:
+        paintSpinButton(states, context, zoomedRect, zoomFactor);
+        break;
+    default:
+        break;
+    }
+}
+
+void ThemeAdwaita::paintCheckbox(ControlStates& states, GraphicsContext& graphicsContext, const FloatRect& zoomedRect, float)
+{
+    GraphicsContextStateSaver stateSaver(graphicsContext);
+
+    FloatRect fieldRect = zoomedRect;
+    FloatSize corner(2, 2);
+    Path path;
+    path.addRoundedRect(fieldRect, corner);
+    fieldRect.inflate(-buttonBorderSize);
+    path.addRoundedRect(fieldRect, corner);
+    graphicsContext.setFillRule(WindRule::EvenOdd);
+    graphicsContext.setFillColor(buttonBorderColor);
+    graphicsContext.fillPath(path);
+    path.clear();
+
+    path.addRoundedRect(fieldRect, corner);
+    graphicsContext.setFillRule(WindRule::NonZero);
+    if (!(states.states() & ControlStates::EnabledState))
+        graphicsContext.setFillColor(toggleBackgroundDisabledColor);
+    else if (states.states() & ControlStates::HoverState)
+        graphicsContext.setFillColor(toggleBackgroundHoveredColor);
+    else
+        graphicsContext.setFillColor(toggleBackgroundColor);
+    graphicsContext.fillPath(path);
+    path.clear();
+
+    if (states.states() & (ControlStates::CheckedState | ControlStates::IndeterminateState)) {
+        GraphicsContextStateSaver checkedStateSaver(graphicsContext);
+        graphicsContext.translate(fieldRect.x(), fieldRect.y());
+        graphicsContext.scale(FloatSize::narrowPrecision(fieldRect.width() / toggleSize, fieldRect.height() / toggleSize));
+        if (states.states() & ControlStates::CheckedState) {
+            path.moveTo({ 2.43, 6.57 });
+            path.addLineTo({ 7.5, 11.63 });
+            path.addLineTo({ 14, 5 });
+            path.addLineTo({ 14, 1 });
+            path.addLineTo({ 7.5, 7.38 });
+            path.addLineTo({ 4.56, 4.44 });
+            path.closeSubpath();
+        } else
+            path.addRoundedRect(FloatRect(2, 5, 10, 4), corner);
+
+        if (!(states.states() & ControlStates::EnabledState))
+            graphicsContext.setFillColor(toggleDisabledColor);
+        else
+            graphicsContext.setFillColor(toggleColor);
+
+        graphicsContext.fillPath(path);
+        path.clear();
+    }
+
+    if (states.states() & ControlStates::FocusState)
+        paintFocus(graphicsContext, zoomedRect, toggleFocusOffset);
+}
+
+void ThemeAdwaita::paintRadio(ControlStates& states, GraphicsContext& graphicsContext, const FloatRect& zoomedRect, float)
+{
+    GraphicsContextStateSaver stateSaver(graphicsContext);
+    FloatRect fieldRect = zoomedRect;
+    Path path;
+    path.addEllipse(fieldRect);
+    fieldRect.inflate(-buttonBorderSize);
+    path.addEllipse(fieldRect);
+    graphicsContext.setFillRule(WindRule::EvenOdd);
+    graphicsContext.setFillColor(buttonBorderColor);
+    graphicsContext.fillPath(path);
+    path.clear();
+
+    path.addEllipse(fieldRect);
+    graphicsContext.setFillRule(WindRule::NonZero);
+    if (!(states.states() & ControlStates::EnabledState))
+        graphicsContext.setFillColor(toggleBackgroundDisabledColor);
+    else if (states.states() & ControlStates::HoverState)
+        graphicsContext.setFillColor(toggleBackgroundHoveredColor);
+    else
+        graphicsContext.setFillColor(toggleBackgroundColor);
+    graphicsContext.fillPath(path);
+    path.clear();
+
+    if (states.states() & ControlStates::CheckedState) {
+        fieldRect.inflate(-(fieldRect.width() - fieldRect.width() * 0.70));
+        path.addEllipse(fieldRect);
+        if (!(states.states() & ControlStates::EnabledState))
+            graphicsContext.setFillColor(toggleDisabledColor);
+        else
+            graphicsContext.setFillColor(toggleColor);
+        graphicsContext.fillPath(path);
+    }
+
+    if (states.states() & ControlStates::FocusState)
+        paintFocus(graphicsContext, zoomedRect, toggleFocusOffset);
+}
+
+void ThemeAdwaita::paintButton(ControlStates& states, GraphicsContext& graphicsContext, const FloatRect& zoomedRect, float)
+{
+    GraphicsContextStateSaver stateSaver(graphicsContext);
+
+    FloatRect fieldRect = zoomedRect;
+    FloatSize corner(5, 5);
+    Path path;
+    path.addRoundedRect(fieldRect, corner);
+    fieldRect.inflate(-buttonBorderSize);
+    path.addRoundedRect(fieldRect, corner);
+    graphicsContext.setFillRule(WindRule::EvenOdd);
+    graphicsContext.setFillColor(buttonBorderColor);
+    graphicsContext.fillPath(path);
+    path.clear();
+
+    path.addRoundedRect(fieldRect, corner);
+    graphicsContext.setFillRule(WindRule::NonZero);
+    if (!(states.states() & ControlStates::EnabledState))
+        graphicsContext.setFillColor(buttonBackgroundDisabledColor);
+    else if (states.states() & ControlStates::PressedState)
+        graphicsContext.setFillColor(buttonBackgroundPressedColor);
+    else if (states.states() & ControlStates::HoverState)
+        graphicsContext.setFillColor(buttonBackgroundHoveredColor);
+    else
+        graphicsContext.setFillColor(buttonBackgroundColor);
+    graphicsContext.fillPath(path);
+
+    if (states.states() & ControlStates::FocusState)
+        paintFocus(graphicsContext, zoomedRect, buttonFocusOffset);
+}
+
+void ThemeAdwaita::paintSpinButton(ControlStates& states, GraphicsContext& graphicsContext, const FloatRect& zoomedRect, float)
+{
+    GraphicsContextStateSaver stateSaver(graphicsContext);
+
+    FloatRect fieldRect = zoomedRect;
+    FloatSize corner(2, 2);
+    Path path;
+    path.addRoundedRect(fieldRect, corner);
+    fieldRect.inflate(-buttonBorderSize);
+    path.addRoundedRect(fieldRect, corner);
+    graphicsContext.setFillRule(WindRule::EvenOdd);
+    graphicsContext.setFillColor(spinButtonBorderColor);
+    graphicsContext.fillPath(path);
+    path.clear();
+
+    path.addRoundedRect(fieldRect, corner);
+    graphicsContext.setFillRule(WindRule::NonZero);
+    graphicsContext.setFillColor(spinButtonBackgroundColor);
+    graphicsContext.fillPath(path);
+    path.clear();
+
+    FloatRect buttonRect = fieldRect;
+    buttonRect.setHeight(fieldRect.height() / 2.0);
+    {
+        if (states.states() & ControlStates::SpinUpState) {
+            path.addRoundedRect(FloatRoundedRect(buttonRect, corner, corner, { }, { }));
+            if (states.states() & ControlStates::PressedState)
+                graphicsContext.setFillColor(spinButtonBackgroundPressedColor);
+            else if (states.states() & ControlStates::HoverState)
+                graphicsContext.setFillColor(spinButtonBackgroundHoveredColor);
+            graphicsContext.fillPath(path);
+            path.clear();
+        }
+
+        GraphicsContextStateSaver buttonStateSaver(graphicsContext);
+        if (buttonRect.height() > arrowSize)
+            graphicsContext.translate(buttonRect.x(), buttonRect.y() + (buttonRect.height() / 2.0) - (arrowSize / 2.));
+        else {
+            graphicsContext.translate(buttonRect.x(), buttonRect.y());
+            graphicsContext.scale(FloatSize::narrowPrecision(buttonRect.width() / arrowSize, buttonRect.height() / arrowSize));
+        }
+        paintArrow(graphicsContext, ArrowDirection::Up);
+    }
+
+    buttonRect.move(0, buttonRect.height());
+    {
+        if (!(states.states() & ControlStates::SpinUpState)) {
+            path.addRoundedRect(FloatRoundedRect(buttonRect, { }, { }, corner, corner));
+            if (states.states() & ControlStates::PressedState)
+                graphicsContext.setFillColor(spinButtonBackgroundPressedColor);
+            else if (states.states() & ControlStates::HoverState)
+                graphicsContext.setFillColor(spinButtonBackgroundHoveredColor);
+            graphicsContext.fillPath(path);
+            path.clear();
+        }
+
+        GraphicsContextStateSaver buttonStateSaver(graphicsContext);
+        if (buttonRect.height() > arrowSize)
+            graphicsContext.translate(buttonRect.x(), buttonRect.y() + (buttonRect.height() / 2.0) - (arrowSize / 2.));
+        else {
+            graphicsContext.translate(buttonRect.x(), buttonRect.y());
+            graphicsContext.scale(FloatSize::narrowPrecision(buttonRect.width() / arrowSize, buttonRect.height() / arrowSize));
+        }
+        paintArrow(graphicsContext, ArrowDirection::Down);
+    }
+}
+
+} // namespace WebCore

Source/WebCore/platform/adwaita/ThemeAdwaita.h
@@ -1,0 +1,60 @@
+/*
+ * Copyright (C) 2015, 2020 Igalia S.L.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY APPLE INC. AND ITS CONTRIBUTORS ``AS IS''
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL APPLE INC. OR ITS CONTRIBUTORS
+ * BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
+ * THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#pragma once
+
+#include "Color.h"
+#include "Theme.h"
+
+namespace WebCore {
+
+class Path;
+
+class ThemeAdwaita : public Theme {
+public:
+    static Color focusColor();
+    static void paintFocus(GraphicsContext&, const FloatRect&, int offset);
+    static void paintFocus(GraphicsContext&, const Path&, const Color&);
+    static void paintFocus(GraphicsContext&, const Vector<FloatRect>&, const Color&);
+    enum class ArrowDirection { Up, Down };
+    static void paintArrow(GraphicsContext&, ArrowDirection);
+
+    virtual Color activeSelectionForegroundColor() const;
+    virtual Color activeSelectionBackgroundColor() const;
+    virtual Color inactiveSelectionForegroundColor() const;
+    virtual Color inactiveSelectionBackgroundColor() const;
+    virtual void platformColorsDidChange() { };
+
+private:
+    LengthSize controlSize(ControlPart, const FontCascade&, const LengthSize&, float) const final;
+    void paint(ControlPart, ControlStates&, GraphicsContext&, const FloatRect&, float, ScrollView*, float, float, bool, bool) final;
+
+    void paintCheckbox(ControlStates&, GraphicsContext&, const FloatRect&, float);
+    void paintRadio(ControlStates&, GraphicsContext&, const FloatRect&, float);
+    void paintButton(ControlStates&, GraphicsContext&, const FloatRect&, float);
+    void paintSpinButton(ControlStates&, GraphicsContext&, const FloatRect&, float);
+};
+
+} // namespace WebCore

ManualTests/gtk/theme.html
@@ -147,6 +147,16 @@
       </html>">
   </iframe>
 
+  <h1>Datalist</h1>
+  <input list="ports" name="port">
+  <datalist id="ports">
+    <option value="WebKitGTK">
+    <option value="WPEWebKit">
+    <option value="PlayStation">
+    <option value="macOS">
+    <option value="iOS">
+  </datalist>
+
   <h1>Media controls</h1>
   <video controls></video>
   </body>

Source/WTF/wtf/PlatformUse.h
@@ -291,7 +291,7 @@
 #define USE_CFNETWORK_CONTENT_ENCODING_SNIFFING_OVERRIDE 1
 #endif
 
-#if PLATFORM(MAC) || PLATFORM(WPE)
+#if PLATFORM(MAC) || PLATFORM(WPE) || PLATFORM(GTK)
 /* FIXME: This really needs a descriptive name, this "new theme" was added in 2008. */
 #define USE_NEW_THEME 1
 #endif

commit 10886c8a0ecf254de669571aba6ee880f08cc6c0

REGRESSION(r257299): [GTK] Test imported/w3c/web-platform-tests/css/css-sizing/button-min-width.html is failing since r257299 https://bugs.webkit.org/show_bug.cgi?id=208296
Reviewed by Adrian Perez de Castro.

We are no longer honoring the style min width/height of buttons.

* platform/Theme.cpp:
(WebCore::Theme::minimumControlSize const): Add zoomedSize parameter.
* platform/Theme.h:
* platform/adwaita/ThemeAdwaita.cpp:
(WebCore::ThemeAdwaita::minimumControlSize const): Use the style min size unless it's intrinsic or auto.
* platform/adwaita/ThemeAdwaita.h:
* platform/mac/ThemeMac.h:
* platform/mac/ThemeMac.mm:
(WebCore::ThemeMac::minimumControlSize const): Add zoomedSize parameter.
* rendering/RenderTheme.cpp:
(WebCore::RenderTheme::adjustStyle): Pass style min size to Theme::minimumControlSize().

git-svn-id: http://svn.webkit.org/repository/webkit/trunk@257631 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Source/WebCore/platform/Theme.cpp
@@ -48,7 +48,7 @@ LengthSize Theme::controlSize(ControlPart, const FontCascade&, const LengthSize&
     return zoomedSize;
 }
 
-LengthSize Theme::minimumControlSize(ControlPart, const FontCascade&, float) const
+LengthSize Theme::minimumControlSize(ControlPart, const FontCascade&, const LengthSize&, float) const
 {
     return { { 0, Fixed }, { 0, Fixed } };
 }

Source/WebCore/platform/Theme.h
@@ -57,7 +57,7 @@ public:
     virtual LengthSize controlSize(ControlPart, const FontCascade&, const LengthSize& zoomedSize, float zoomFactor) const;
 
     // Returns the minimum size for a control in zoomed coordinates.
-    virtual LengthSize minimumControlSize(ControlPart, const FontCascade&, float zoomFactor) const;
+    virtual LengthSize minimumControlSize(ControlPart, const FontCascade&, const LengthSize& zoomedSize, float zoomFactor) const;
     
     // Allows the theme to modify the existing padding/border.
     virtual LengthBox controlPadding(ControlPart, const FontCascade&, const LengthBox& zoomedBox, float zoomFactor) const;

Source/WebCore/platform/adwaita/ThemeAdwaita.cpp
@@ -179,6 +179,19 @@ LengthSize ThemeAdwaita::controlSize(ControlPart part, const FontCascade& fontCa
     return Theme::controlSize(part, fontCascade, zoomedSize, zoomFactor);
 }
 
+LengthSize ThemeAdwaita::minimumControlSize(ControlPart, const FontCascade&, const LengthSize& zoomedSize, float) const
+{
+    if (!zoomedSize.width.isIntrinsicOrAuto() && !zoomedSize.height.isIntrinsicOrAuto())
+        return zoomedSize;
+
+    LengthSize minSize = zoomedSize;
+    if (minSize.width.isIntrinsicOrAuto())
+        minSize.width = Length(0, Fixed);
+    if (minSize.height.isIntrinsicOrAuto())
+        minSize.height = Length(0, Fixed);
+    return minSize;
+}
+
 LengthBox ThemeAdwaita::controlBorder(ControlPart part, const FontCascade& font, const LengthBox& zoomedBox, float zoomFactor) const
 {
     switch (part) {

Source/WebCore/platform/adwaita/ThemeAdwaita.h
@@ -49,6 +49,7 @@ public:
 
 private:
     LengthSize controlSize(ControlPart, const FontCascade&, const LengthSize&, float) const final;
+    LengthSize minimumControlSize(ControlPart, const FontCascade&, const LengthSize&, float) const final;
     LengthBox controlBorder(ControlPart, const FontCascade&, const LengthBox&, float) const final;
     void paint(ControlPart, ControlStates&, GraphicsContext&, const FloatRect&, float, ScrollView*, float, float, bool, bool) final;
 

Source/WebCore/platform/mac/ThemeMac.h
@@ -49,7 +49,7 @@ private:
     Optional<FontCascadeDescription> controlFont(ControlPart, const FontCascade&, float zoomFactor) const final;
 
     LengthSize controlSize(ControlPart, const FontCascade&, const LengthSize&, float zoomFactor) const final;
-    LengthSize minimumControlSize(ControlPart, const FontCascade&, float zoomFactor) const final;
+    LengthSize minimumControlSize(ControlPart, const FontCascade&, const LengthSize&, float zoomFactor) const final;
 
     LengthBox controlPadding(ControlPart, const FontCascade&, const LengthBox& zoomedBox, float zoomFactor) const final;
     LengthBox controlBorder(ControlPart, const FontCascade&, const LengthBox& zoomedBox, float zoomFactor) const final;

Source/WebCore/platform/mac/ThemeMac.mm
@@ -840,7 +840,7 @@ LengthSize ThemeMac::controlSize(ControlPart part, const FontCascade& font, cons
     }
 }
 
-LengthSize ThemeMac::minimumControlSize(ControlPart part, const FontCascade& font, float zoomFactor) const
+LengthSize ThemeMac::minimumControlSize(ControlPart part, const FontCascade& font, const LengthSize& zoomedSize, float zoomFactor) const
 {
     switch (part) {
     case SquareButtonPart:
@@ -856,7 +856,7 @@ LengthSize ThemeMac::minimumControlSize(ControlPart part, const FontCascade& fon
             { static_cast<int>(base.height() * zoomFactor), Fixed } };
     }
     default:
-        return Theme::minimumControlSize(part, font, zoomFactor);
+        return Theme::minimumControlSize(part, font, zoomedSize, zoomFactor);
     }
 }
 

Source/WebCore/rendering/RenderTheme.cpp
@@ -171,7 +171,7 @@ void RenderTheme::adjustStyle(RenderStyle& style, const Element* element, const 
             style.setHeight(WTFMove(controlSize.height));
 
         // Min-Width / Min-Height
-        LengthSize minControlSize = Theme::singleton().minimumControlSize(part, style.fontCascade(), style.effectiveZoom());
+        LengthSize minControlSize = Theme::singleton().minimumControlSize(part, style.fontCascade(), { style.minWidth(), style.minHeight() }, style.effectiveZoom());
         if (minControlSize.width != style.minWidth())
             style.setMinWidth(WTFMove(minControlSize.width));
         if (minControlSize.height != style.minHeight())