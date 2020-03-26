WebKit/WebKit-http

commit 82d417c0f253e1dbf3a32c6dcbefc92d4351d0fa

[iOS] ASSERTION FAILURE: !isMissingPostLayoutData in WebKit::EditorState::postLayoutData() https://bugs.webkit.org/show_bug.cgi?id=199960 <rdar://problem/53323966>
Reviewed by Simon Fraser.

Source/WebKit:

Refactor the computation of editor state so that we can request that a layout be performed
each time we compute the editor state as part of asking the UI process to interpret a key
event. The full (read: after layout) editor state is needed for UIKit to perform a deletion
because UIKit wants to know how many characters are before the selection. Otherwise, we hit
an assert due to the fact the last editor state sent (when the Web process asked the UI process
to interpret the key) is missing layout data.

The refactoring also moves the Cocoa-common code out of the platform-independent WebPage.cpp
file into WebPageCocoa.mm.

One side effect of the refactoring is that we no longer allow the platformEditorState() function
to override the isMissingPostLayoutData bit. Currently it can even though the calling code, the
platform independent code (PIE) in WebPage, may have attached layout data. Now the PIE code sets
this bit if it attached layout data and the platformEditorState() function only attaches more
layout data if that bit is set. platformEditorState() never unsets that bit (i.e. sets isMissingPostLayoutData
to true).

The patch also removes m_isEditorStateMissingPostLayoutData in WebPage.h. This instance variable
has been unused since <https://trac.webkit.org/changeset/221064/webkit>. Also we haven't been using
IncludePostLayoutDataHint::No since the last reference to it was removed in <https://trac.webkit.org/changeset/244494/webkit>.

I also renamed platformEditorState() to getPlatformEditorState() since it has an out argument.

Test: editing/deleting/ios/backspace-last-character.html

* Shared/EditorState.h:
* UIProcess/API/glib/WebKitEditorState.cpp:

Source/WebKit/WebProcess/WebPage/ios/WebPageIOS.mm
@@ -223,8 +223,21 @@ bool WebPage::isTransparentOrFullyClipped(const Element& element) const
     return renderer->hasNonEmptyVisibleRectRespectingParentFrames();
 }
 
-void WebPage::platformEditorState(Frame& frame, EditorState& result, IncludePostLayoutDataHint shouldIncludePostLayoutData) const
+bool WebPage::platformNeedsLayoutForEditorState(const Frame& frame) const
 {
+    // If we have a composition or are using a hardware keyboard then we need to send the full
+    // editor so that the UIProcess can update UI, including the position of the caret.
+    bool needsLayout = frame.editor().hasComposition();
+#if !PLATFORM(MACCATALYST)
+    needsLayout |= m_keyboardIsAttached;
+#endif
+    return needsLayout;
+}
+
+void WebPage::getPlatformEditorState(Frame& frame, EditorState& result) const
+{
+    getPlatformEditorStateCommon(frame, result);
+
     FrameView* view = frame.view();
     if (!view) {
         result.isMissingPostLayoutData = true;
@@ -255,14 +268,11 @@ void WebPage::platformEditorState(Frame& frame, EditorState& result, IncludePost
 #if !PLATFORM(MACCATALYST)
     requiresPostLayoutData |= m_keyboardIsAttached;
 #endif
-    if ((shouldIncludePostLayoutData == IncludePostLayoutDataHint::No || needsLayout) && !requiresPostLayoutData) {
-        result.isMissingPostLayoutData = true;
+    if (needsLayout || result.isMissingPostLayoutData)
         return;
-    }
 
     auto& postLayoutData = result.postLayoutData();
-    
-    const VisibleSelection& selection = frame.selection().selection();
+    const auto& selection = frame.selection().selection();
     postLayoutData.isStableStateUpdate = m_isInStableState;
     bool startNodeIsInsideFixedPosition = false;
     bool endNodeIsInsideFixedPosition = false;
@@ -479,7 +489,7 @@ bool WebPage::handleEditingKeyboardEvent(KeyboardEvent& event)
 
     // FIXME: Interpret the event immediately upon receiving it in UI process, without sending to WebProcess first.
     bool eventWasHandled = false;
-    bool sendResult = WebProcess::singleton().parentProcessConnection()->sendSync(Messages::WebPageProxy::InterpretKeyEvent(editorState(), platformEvent->type() == PlatformKeyboardEvent::Char),
+    bool sendResult = WebProcess::singleton().parentProcessConnection()->sendSync(Messages::WebPageProxy::InterpretKeyEvent(editorState(ShouldPerformLayout::Yes), platformEvent->type() == PlatformKeyboardEvent::Char),
         Messages::WebPageProxy::InterpretKeyEvent::Reply(eventWasHandled), m_identifier);
     return sendResult && eventWasHandled;
 }

LayoutTests/TestExpectations
@@ -21,6 +21,7 @@ editing/input/ios [ Skip ]
 editing/find [ Skip ]
 editing/pasteboard/gtk [ Skip ]
 editing/selection/ios [ Skip ]
+editing/deleting/ios [ Skip ]
 editing/undo-manager [ Skip ]
 tiled-drawing [ Skip ]
 fast/css/ios [ Skip ]

LayoutTests/editing/deleting/ios/backspace-last-character-expected.txt
@@ -1,0 +1,11 @@
+This tests using the software keyboard and pressing 'w' then backspace deletes the 'w' and does not cause an assertion failure in a debug build.
+
+On success, you will see a series of "PASS" messages, followed by "TEST COMPLETE".
+
+
+PASS document.getElementById("input").value is "w"
+PASS document.getElementById("input").value is ""
+PASS successfullyParsed is true
+
+TEST COMPLETE
+

LayoutTests/editing/deleting/ios/backspace-last-character.html
@@ -1,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+<head>
+<script src="../../../resources/js-test.js"></script>
+<script src="../../../resources/ui-helper.js"></script>
+</head>
+<body>
+<div id="test-container">
+    <textarea id="input"></textarea>
+    <textarea id="output" readonly></textarea>
+</div>
+<script>
+window.jsTestIsAsync = true;
+
+let input = document.getElementById("input");
+
+function appendANewline()
+{
+    let outputElement = document.getElementById("output");
+    outputElement.value = outputElement.value + "\n";
+}
+
+async function runTest()
+{
+    if (window.testRunner)
+        await UIHelper.setHardwareKeyboardAttached(false);
+
+    await UIHelper.callFunctionAndWaitForEvent(() => window.testRunner && UIHelper.activateElement(input), input, "focus");
+
+    await UIHelper.callFunctionAndWaitForEvent(() => window.testRunner && UIHelper.keyDown("w"), input, "input");
+    shouldBeEqualToString('document.getElementById("input").value', "w");
+
+    await UIHelper.callFunctionAndWaitForEvent(() => window.testRunner && UIHelper.keyDown("\b"), input, "input");
+    shouldBeEqualToString('document.getElementById("input").value', "");
+
+    document.body.removeChild(document.getElementById("test-container"));
+    finishJSTest();
+}
+
+description("This tests using the software keyboard and pressing 'w' then <kbd>backspace</kbd> deletes the 'w' and does not cause an assertion failure in a debug build.")
+input.addEventListener("keydown", appendANewline);
+
+runTest();
+</script>
+</body>
+</html>

LayoutTests/platform/ios/TestExpectations
@@ -8,6 +8,7 @@
 
 accessibility/ios-simulator [ Pass ]
 displaylists [ Pass ]
+editing/deleting/ios [ Pass ]
 http/tests/quicklook [ Pass ]
 media/ios [ Pass ]
 quicklook [ Pass ]

Source/WebKit/Shared/EditorState.h
@@ -74,7 +74,7 @@ struct EditorState {
     bool isInPasswordField { false };
     bool isInPlugin { false };
     bool hasComposition { false };
-    bool isMissingPostLayoutData { false };
+    bool isMissingPostLayoutData { true };
 
 #if PLATFORM(IOS_FAMILY)
     WebCore::IntRect firstMarkedRect;

Source/WebKit/UIProcess/API/glib/WebKitEditorState.cpp
@@ -109,6 +109,7 @@ WebKitEditorState* webkitEditorStateCreate(WebPageProxy& page)
 {
     WebKitEditorState* editorState = WEBKIT_EDITOR_STATE(g_object_new(WEBKIT_TYPE_EDITOR_STATE, nullptr));
     editorState->priv->page = &page;
+    editorState->priv->typingAttributes = WEBKIT_EDITOR_TYPING_ATTRIBUTE_NONE;
     webkitEditorStateChanged(editorState, page.editorState());
     return editorState;
 }

Source/WebKit/WebProcess/WebPage/Cocoa/WebPageCocoa.mm
@@ -36,12 +36,15 @@
 #import "WebRemoteObjectRegistry.h"
 #import <pal/spi/cocoa/LaunchServicesSPI.h>
 #import <WebCore/DictionaryLookup.h>
+#import <WebCore/Editing.h>
 #import <WebCore/Editor.h>
 #import <WebCore/EventHandler.h>
 #import <WebCore/EventNames.h>
 #import <WebCore/FocusController.h>
 #import <WebCore/FrameView.h>
 #import <WebCore/HTMLConverter.h>
+#import <WebCore/HTMLOListElement.h>
+#import <WebCore/HTMLUListElement.h>
 #import <WebCore/HitTestResult.h>
 #import <WebCore/NodeRenderStyle.h>
 #import <WebCore/PaymentCoordinator.h>
@@ -311,6 +314,7 @@ RetainPtr<CFDataRef> WebPage::pdfSnapshotAtSize(IntRect rect, IntSize bitmapSize
     return data;
 }
 
+
 void WebPage::getProcessDisplayName(CompletionHandler<void(String&&)>&& completionHandler)
 {
 #if PLATFORM(MAC)
@@ -318,6 +322,71 @@ void WebPage::getProcessDisplayName(CompletionHandler<void(String&&)>&& completi
 #else
     completionHandler({ });
 #endif
+}
+
+void WebPage::getPlatformEditorStateCommon(const Frame& frame, EditorState& result) const
+{
+    if (result.isMissingPostLayoutData)
+        return;
+
+    const auto& selection = frame.selection().selection();
+
+    if (!result.isContentEditable || selection.isNone())
+        return;
+
+    auto& postLayoutData = result.postLayoutData();
+    if (auto editingStyle = EditingStyle::styleAtSelectionStart(selection)) {
+        if (editingStyle->hasStyle(CSSPropertyFontWeight, "bold"_s))
+            postLayoutData.typingAttributes |= AttributeBold;
+
+        if (editingStyle->hasStyle(CSSPropertyFontStyle, "italic"_s) || editingStyle->hasStyle(CSSPropertyFontStyle, "oblique"_s))
+            postLayoutData.typingAttributes |= AttributeItalics;
+
+        if (editingStyle->hasStyle(CSSPropertyWebkitTextDecorationsInEffect, "underline"_s))
+            postLayoutData.typingAttributes |= AttributeUnderline;
+
+        if (auto* styleProperties = editingStyle->style()) {
+            bool isLeftToRight = styleProperties->propertyAsValueID(CSSPropertyDirection) == CSSValueLtr;
+            switch (styleProperties->propertyAsValueID(CSSPropertyTextAlign)) {
+            case CSSValueRight:
+            case CSSValueWebkitRight:
+                postLayoutData.textAlignment = RightAlignment;
+                break;
+            case CSSValueLeft:
+            case CSSValueWebkitLeft:
+                postLayoutData.textAlignment = LeftAlignment;
+                break;
+            case CSSValueCenter:
+            case CSSValueWebkitCenter:
+                postLayoutData.textAlignment = CenterAlignment;
+                break;
+            case CSSValueJustify:
+                postLayoutData.textAlignment = JustifiedAlignment;
+                break;
+            case CSSValueStart:
+                postLayoutData.textAlignment = isLeftToRight ? LeftAlignment : RightAlignment;
+                break;
+            case CSSValueEnd:
+                postLayoutData.textAlignment = isLeftToRight ? RightAlignment : LeftAlignment;
+                break;
+            default:
+                break;
+            }
+            if (auto textColor = styleProperties->propertyAsColor(CSSPropertyColor))
+                postLayoutData.textColor = *textColor;
+        }
+    }
+
+    if (auto* enclosingListElement = enclosingList(selection.start().containerNode())) {
+        if (is<HTMLUListElement>(*enclosingListElement))
+            postLayoutData.enclosingListType = UnorderedList;
+        else if (is<HTMLOListElement>(*enclosingListElement))
+            postLayoutData.enclosingListType = OrderedList;
+        else
+            ASSERT_NOT_REACHED();
+    }
+
+    postLayoutData.baseWritingDirection = frame.editor().baseWritingDirectionForSelectionStart();
 }
 
 } // namespace WebKit

commit 03d4abd492de4bc5fe8c2840effc474604b2be0c

REGRESSION (r258989): ASSERTION FAILED: !isMissingPostLayoutData in WebKit::EditorState::PostLayoutData https://bugs.webkit.org/show_bug.cgi?id=209570 <rdar://problem/60895050>
Reviewed by Darin Adler.

Send an editor state update before responding to a request for position information
to ensure that the UI process has up-to-date selection state. Otherwise, calling code
that uses this information to determine whether to query for the selection text will
cause an assertion failure.

This fixes the test failure TestWebKitAPI.ActionSheetTests.DataDetectorsLinkIsNotPresentedAsALink
caused by r258989. Following r258989 WebKit now accurately reports whether editor state
has or does not have post-layout details. Prior to this the default EditorState was marked
as having post-layout data even if it did not actually have such data.

* WebProcess/WebPage/ios/WebPageIOS.mm:
(WebKit::WebPage::getPositionInformation):
(WebKit::WebPage::requestPositionInformation):

git-svn-id: http://svn.webkit.org/repository/webkit/trunk@259071 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Source/WebKit/WebProcess/WebPage/ios/WebPageIOS.mm
@@ -2587,6 +2587,8 @@ void WebPage::getPositionInformation(const InteractionInformationRequest& reques
         return reply({ });
     }
 
+    sendEditorStateUpdate();
+
     m_pendingSynchronousPositionInformationReply = WTFMove(reply);
 
     auto information = positionInformation(request);
@@ -2961,6 +2963,7 @@ InteractionInformationAtPosition WebPage::positionInformation(const InteractionI
 
 void WebPage::requestPositionInformation(const InteractionInformationRequest& request)
 {
+    sendEditorStateUpdate();
     send(Messages::WebPageProxy::DidReceivePositionInformation(positionInformation(request)));
 }
 