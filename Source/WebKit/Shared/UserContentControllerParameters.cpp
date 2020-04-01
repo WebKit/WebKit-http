WebKit/WebKit-http

commit 7d0765eb1f48e03a8c1a355f761fce46a19b2a1e

Add SPI WKWebpagePreferences._userContentController https://bugs.webkit.org/show_bug.cgi?id=209795
Reviewed by Tim Hatcher.

Source/WebKit:

This will allow us to switch which WKUserContentController we are using at decidePolicyForNavigationAction time
like we do WKWebsiteDataStores.  This is only allowed with main frame navigations.

To do this I moved UserContentControllerParameters into their own struct.
I remove unused WebsitePoliciesData.websiteDataStoreParameters.
I pass an API::WebsitePolicies* further down the chain instead of switching to Optional<WebsitePoliciesData>,
which allows us to access the WebUserContentControllerProxy* from the former in WebPageProxy::creationParameters.
I removed an unused WebsitePolicies constructor.
I added a missing copied member variable in WebsitePolicies::copy.

* NetworkProcess/NetworkSession.cpp:
* Shared/UserContentControllerParameters.cpp: Added.
(WebKit::UserContentControllerParameters::encode const):
(WebKit::UserContentControllerParameters::decode):
* Shared/UserContentControllerParameters.h: Added.
* Shared/WebPageCreationParameters.cpp:
(WebKit::WebPageCreationParameters::encode const):
(WebKit::WebPageCreationParameters::decode):
* Shared/WebPageCreationParameters.h:
* Shared/WebsitePoliciesData.cpp:
(WebKit::WebsitePoliciesData::encode const):
(WebKit::WebsitePoliciesData::decode):
* Shared/WebsitePoliciesData.h:
* Sources.txt:
* UIProcess/API/APIWebsitePolicies.cpp:
(API::WebsitePolicies::copy const):
(API::WebsitePolicies::setUserContentController):
(API::WebsitePolicies::data):
(API::WebsitePolicies::WebsitePolicies): Deleted.
* UIProcess/API/APIWebsitePolicies.h:
* UIProcess/API/C/WKPage.cpp:
(WKPageUpdateWebsitePolicies):
* UIProcess/API/Cocoa/WKWebView.mm:
(-[WKWebView _updateWebpagePreferences:]):
* UIProcess/API/Cocoa/WKWebpagePreferences.mm:
(-[WKWebpagePreferences _userContentController]):
(-[WKWebpagePreferences _setUserContentController:]):
* UIProcess/API/Cocoa/WKWebpagePreferencesPrivate.h:

Source/WebKit/Shared/UserContentControllerParameters.cpp
@@ -1,0 +1,85 @@
+/*
+ * Copyright (C) 2020 Apple Inc. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY APPLE INC. AND ITS CONTRIBUTORS ``AS IS''
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL APPLE INC. OR ITS CONTRIBUTORS
+ * BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
+ * THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include "config.h"
+#include "UserContentControllerParameters.h"
+
+#include "Decoder.h"
+#include "Encoder.h"
+
+namespace WebKit {
+
+void UserContentControllerParameters::encode(IPC::Encoder& encoder) const
+{
+    encoder << userContentWorlds;
+    encoder << userScripts;
+    encoder << userStyleSheets;
+    encoder << messageHandlers;
+#if ENABLE(CONTENT_EXTENSIONS)
+    encoder << contentRuleLists;
+#endif
+}
+
+Optional<UserContentControllerParameters> UserContentControllerParameters::decode(IPC::Decoder& decoder)
+{
+    Optional<Vector<std::pair<ContentWorldIdentifier, String>>> userContentWorlds;
+    decoder >> userContentWorlds;
+    if (!userContentWorlds)
+        return WTF::nullopt;
+
+    Optional<Vector<WebUserScriptData>> userScripts;
+    decoder >> userScripts;
+    if (!userScripts)
+        return WTF::nullopt;
+    
+    Optional<Vector<WebUserStyleSheetData>> userStyleSheets;
+    decoder >> userStyleSheets;
+    if (!userStyleSheets)
+        return WTF::nullopt;
+    
+    Optional<Vector<WebScriptMessageHandlerData>> messageHandlers;
+    decoder >> messageHandlers;
+    if (!messageHandlers)
+        return WTF::nullopt;
+    
+#if ENABLE(CONTENT_EXTENSIONS)
+    Optional<Vector<std::pair<String, WebCompiledContentRuleListData>>> contentRuleLists;
+    decoder >> contentRuleLists;
+    if (!contentRuleLists)
+        return WTF::nullopt;
+#endif
+
+    return {{
+        WTFMove(*userContentWorlds),
+        WTFMove(*userScripts),
+        WTFMove(*userStyleSheets),
+        WTFMove(*messageHandlers),
+#if ENABLE(CONTENT_EXTENSIONS)
+        WTFMove(*contentRuleLists),
+#endif
+    }};
+}
+    
+} // namespace WebKit

Source/WebKit/Shared/UserContentControllerParameters.h
@@ -1,0 +1,52 @@
+/*
+ * Copyright (C) 2020 Apple Inc. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY APPLE INC. AND ITS CONTRIBUTORS ``AS IS''
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL APPLE INC. OR ITS CONTRIBUTORS
+ * BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
+ * THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#pragma once
+
+#include "WebCompiledContentRuleListData.h"
+#include "WebUserContentControllerDataTypes.h"
+
+namespace IPC {
+class Encoder;
+class Decoder;
+}
+
+namespace WebKit {
+
+struct UserContentControllerParameters {
+
+    Vector<std::pair<ContentWorldIdentifier, String>> userContentWorlds;
+    Vector<WebUserScriptData> userScripts;
+    Vector<WebUserStyleSheetData> userStyleSheets;
+    Vector<WebScriptMessageHandlerData> messageHandlers;
+#if ENABLE(CONTENT_EXTENSIONS)
+    Vector<std::pair<String, WebCompiledContentRuleListData>> contentRuleLists;
+#endif
+
+    void encode(IPC::Encoder&) const;
+    static Optional<UserContentControllerParameters> decode(IPC::Decoder&);
+};
+
+} // namespace WebKit

Source/WebKit/Shared/WebPageCreationParameters.cpp
@@ -129,13 +129,7 @@ void WebPageCreationParameters::encode(IPC::Encoder& encoder) const
     encoder << needsFontAttributes;
     encoder << iceCandidateFilteringEnabled;
     encoder << enumeratingAllNetworkInterfacesEnabled;
-    encoder << userContentWorlds;
-    encoder << userScripts;
-    encoder << userStyleSheets;
-    encoder << messageHandlers;
-#if ENABLE(CONTENT_EXTENSIONS)
-    encoder << contentRuleLists;
-#endif
+    encoder << userContentControllerParameters;
     encoder << backgroundColor;
     encoder << oldPageID;
     encoder << overriddenMediaType;
@@ -402,37 +396,11 @@ Optional<WebPageCreationParameters> WebPageCreationParameters::decode(IPC::Decod
     if (!decoder.decode(parameters.enumeratingAllNetworkInterfacesEnabled))
         return WTF::nullopt;
 
-    Optional<Vector<std::pair<ContentWorldIdentifier, String>>> userContentWorlds;
-    decoder >> userContentWorlds;
-    if (!userContentWorlds)
+    Optional<UserContentControllerParameters> userContentControllerParameters;
+    decoder >> userContentControllerParameters;
+    if (!userContentControllerParameters)
         return WTF::nullopt;
-    parameters.userContentWorlds = WTFMove(*userContentWorlds);
-
-    Optional<Vector<WebUserScriptData>> userScripts;
-    decoder >> userScripts;
-    if (!userScripts)
-        return WTF::nullopt;
-    parameters.userScripts = WTFMove(*userScripts);
-    
-    Optional<Vector<WebUserStyleSheetData>> userStyleSheets;
-    decoder >> userStyleSheets;
-    if (!userStyleSheets)
-        return WTF::nullopt;
-    parameters.userStyleSheets = WTFMove(*userStyleSheets);
-    
-    Optional<Vector<WebScriptMessageHandlerData>> messageHandlers;
-    decoder >> messageHandlers;
-    if (!messageHandlers)
-        return WTF::nullopt;
-    parameters.messageHandlers = WTFMove(*messageHandlers);
-    
-#if ENABLE(CONTENT_EXTENSIONS)
-    Optional<Vector<std::pair<String, WebCompiledContentRuleListData>>> contentRuleLists;
-    decoder >> contentRuleLists;
-    if (!contentRuleLists)
-        return WTF::nullopt;
-    parameters.contentRuleLists = WTFMove(*contentRuleLists);
-#endif
+    parameters.userContentControllerParameters = WTFMove(*userContentControllerParameters);
 
     Optional<Optional<WebCore::Color>> backgroundColor;
     decoder >> backgroundColor;

Source/WebKit/Shared/WebPageCreationParameters.h
@@ -28,12 +28,11 @@
 #include "DrawingAreaInfo.h"
 #include "LayerTreeContext.h"
 #include "SessionState.h"
-#include "WebCompiledContentRuleListData.h"
+#include "UserContentControllerParameters.h"
 #include "WebCoreArgumentCoders.h"
 #include "WebPageGroupData.h"
 #include "WebPageProxyIdentifier.h"
 #include "WebPreferencesStore.h"
-#include "WebUserContentControllerDataTypes.h"
 #include <WebCore/ActivityState.h>
 #include <WebCore/Color.h>
 #include <WebCore/FloatSize.h>
@@ -196,14 +195,7 @@ struct WebPageCreationParameters {
     bool iceCandidateFilteringEnabled { true };
     bool enumeratingAllNetworkInterfacesEnabled { false };
 
-    // UserContentController members
-    Vector<std::pair<ContentWorldIdentifier, String>> userContentWorlds;
-    Vector<WebUserScriptData> userScripts;
-    Vector<WebUserStyleSheetData> userStyleSheets;
-    Vector<WebScriptMessageHandlerData> messageHandlers;
-#if ENABLE(CONTENT_EXTENSIONS)
-    Vector<std::pair<String, WebCompiledContentRuleListData>> contentRuleLists;
-#endif
+    UserContentControllerParameters userContentControllerParameters;
 
     Optional<WebCore::Color> backgroundColor;
 

Source/WebKit/UIProcess/UserContent/WebUserContentControllerProxy.cpp
@@ -96,37 +96,39 @@ void WebUserContentControllerProxy::removeNetworkProcess(NetworkProcessProxy& pr
 }
 #endif
 
-void WebUserContentControllerProxy::addProcess(WebProcessProxy& webProcessProxy, WebPageCreationParameters& parameters)
+void WebUserContentControllerProxy::addProcess(WebProcessProxy& webProcessProxy)
 {
     ASSERT(!m_processes.hasNullReferences());
 
     if (m_processes.add(webProcessProxy).isNewEntry)
         webProcessProxy.addMessageReceiver(Messages::WebUserContentControllerProxy::messageReceiverName(), identifier(), *this);
+}
 
-    ASSERT(parameters.userContentWorlds.isEmpty());
+UserContentControllerParameters WebUserContentControllerProxy::parameters() const
+{
+    UserContentControllerParameters parameters;
+
     for (const auto& world : m_contentWorlds)
         parameters.userContentWorlds.append(world.key->worldData());
 
-    ASSERT(parameters.userScripts.isEmpty());
     for (auto userScript : m_userScripts->elementsOfType<API::UserScript>())
         parameters.userScripts.append({ userScript->identifier(), userScript->contentWorld().identifier(), userScript->userScript() });
 
-    ASSERT(parameters.userStyleSheets.isEmpty());
     for (auto userStyleSheet : m_userStyleSheets->elementsOfType<API::UserStyleSheet>())
         parameters.userStyleSheets.append({ userStyleSheet->identifier(), userStyleSheet->contentWorld().identifier(), userStyleSheet->userStyleSheet() });
 
-    ASSERT(parameters.messageHandlers.isEmpty());
     for (auto& handler : m_scriptMessageHandlers.values())
         parameters.messageHandlers.append({ handler->identifier(), handler->world().identifier(), handler->name() });
 
 #if ENABLE(CONTENT_EXTENSIONS)
-    ASSERT(parameters.contentRuleLists.isEmpty());
     parameters.contentRuleLists = contentRuleListData();
 #endif
+    
+    return parameters;
 }
 
 #if ENABLE(CONTENT_EXTENSIONS)
-Vector<std::pair<String, WebCompiledContentRuleListData>> WebUserContentControllerProxy::contentRuleListData()
+Vector<std::pair<String, WebCompiledContentRuleListData>> WebUserContentControllerProxy::contentRuleListData() const
 {
     Vector<std::pair<String, WebCompiledContentRuleListData>> data;
     data.reserveInitialCapacity(m_contentRuleLists.size());

commit 118e52fb5470ad6ef473b6ae5a18c6ded14650ff

Send correct UserContentControllerIdentifier after using SPI WKWebpagePreferences._userContentController https://bugs.webkit.org/show_bug.cgi?id=209833
Reviewed by Tim Hatcher.

Source/WebKit:

Covered by an API test.  I knew something was broken in r259307 and this was it.

* Shared/UserContentControllerParameters.cpp:
(WebKit::UserContentControllerParameters::encode const):
(WebKit::UserContentControllerParameters::decode):
* Shared/UserContentControllerParameters.h:
* Shared/WebPageCreationParameters.cpp:
(WebKit::WebPageCreationParameters::encode const):
(WebKit::WebPageCreationParameters::decode):
* Shared/WebPageCreationParameters.h:
* UIProcess/UserContent/WebUserContentControllerProxy.cpp:
(WebKit::WebUserContentControllerProxy::parameters const):
* UIProcess/WebPageProxy.cpp:
* WebProcess/WebPage/WebPage.cpp:

Tools:

* TestWebKitAPI/Tests/WebKitCocoa/WebsitePolicies.mm:



git-svn-id: http://svn.webkit.org/repository/webkit/trunk@259324 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Source/WebKit/Shared/UserContentControllerParameters.cpp
@@ -33,6 +33,7 @@ namespace WebKit {
 
 void UserContentControllerParameters::encode(IPC::Encoder& encoder) const
 {
+    encoder << identifier;
     encoder << userContentWorlds;
     encoder << userScripts;
     encoder << userStyleSheets;
@@ -44,6 +45,11 @@ void UserContentControllerParameters::encode(IPC::Encoder& encoder) const
 
 Optional<UserContentControllerParameters> UserContentControllerParameters::decode(IPC::Decoder& decoder)
 {
+    Optional<UserContentControllerIdentifier> identifier;
+    decoder >> identifier;
+    if (!identifier)
+        return WTF::nullopt;
+    
     Optional<Vector<std::pair<ContentWorldIdentifier, String>>> userContentWorlds;
     decoder >> userContentWorlds;
     if (!userContentWorlds)
@@ -72,6 +78,7 @@ Optional<UserContentControllerParameters> UserContentControllerParameters::decod
 #endif
 
     return {{
+        WTFMove(*identifier),
         WTFMove(*userContentWorlds),
         WTFMove(*userScripts),
         WTFMove(*userStyleSheets),

Source/WebKit/Shared/UserContentControllerParameters.h
@@ -25,6 +25,7 @@
 
 #pragma once
 
+#include "UserContentControllerIdentifier.h"
 #include "WebCompiledContentRuleListData.h"
 #include "WebUserContentControllerDataTypes.h"
 
@@ -37,6 +38,7 @@ namespace WebKit {
 
 struct UserContentControllerParameters {
 
+    UserContentControllerIdentifier identifier;
     Vector<std::pair<ContentWorldIdentifier, String>> userContentWorlds;
     Vector<WebUserScriptData> userScripts;
     Vector<WebUserStyleSheetData> userStyleSheets;

Source/WebKit/Shared/WebPageCreationParameters.cpp
@@ -54,7 +54,6 @@ void WebPageCreationParameters::encode(IPC::Encoder& encoder) const
     encoder << userAgent;
     encoder << itemStatesWereRestoredByAPIRequest;
     encoder << itemStates;
-    encoder << userContentControllerID;
     encoder << visitedLinkTableID;
     encoder << canRunBeforeUnloadConfirmPanel;
     encoder << canRunModal;
@@ -217,12 +216,6 @@ Optional<WebPageCreationParameters> WebPageCreationParameters::decode(IPC::Decod
     if (!itemStates)
         return WTF::nullopt;
     parameters.itemStates = WTFMove(*itemStates);
-
-    Optional<UserContentControllerIdentifier> userContentControllerIdentifier;
-    decoder >> userContentControllerIdentifier;
-    if (!userContentControllerIdentifier)
-        return WTF::nullopt;
-    parameters.userContentControllerID = *userContentControllerIdentifier;
 
     if (!decoder.decode(parameters.visitedLinkTableID))
         return WTF::nullopt;

Source/WebKit/Shared/WebPageCreationParameters.h
@@ -99,7 +99,6 @@ struct WebPageCreationParameters {
     bool itemStatesWereRestoredByAPIRequest { false };
     Vector<BackForwardListItemState> itemStates;
 
-    UserContentControllerIdentifier userContentControllerID;
     uint64_t visitedLinkTableID;
     bool canRunBeforeUnloadConfirmPanel;
     bool canRunModal;

Source/WebKit/UIProcess/UserContent/WebUserContentControllerProxy.cpp
@@ -108,6 +108,8 @@ UserContentControllerParameters WebUserContentControllerProxy::parameters() cons
 {
     UserContentControllerParameters parameters;
 
+    parameters.identifier = identifier();
+    
     for (const auto& world : m_contentWorlds)
         parameters.userContentWorlds.append(world.key->worldData());
 

Source/WebKit/UIProcess/WebPageProxy.cpp
@@ -7684,7 +7684,6 @@ WebPageCreationParameters WebPageProxy::creationParameters(WebProcessProxy& proc
     parameters.userAgent = userAgent();
     parameters.itemStatesWereRestoredByAPIRequest = m_sessionStateWasRestoredByAPIRequest;
     parameters.itemStates = m_backForwardList->itemStates();
-    parameters.userContentControllerID = m_userContentController->identifier();
     parameters.visitedLinkTableID = m_visitedLinkStore->identifier();
     parameters.canRunBeforeUnloadConfirmPanel = m_uiClient->canRunBeforeUnloadConfirmPanel();
     parameters.canRunModal = m_canRunModal;

Source/WebKit/WebProcess/WebPage/WebPage.cpp
@@ -422,7 +422,7 @@ WebPage::WebPage(PageIdentifier pageID, WebPageCreationParameters&& parameters)
     , m_uiClient(makeUnique<API::InjectedBundle::PageUIClient>())
     , m_findController(makeUniqueRef<FindController>(this))
     , m_inspectorTargetController(makeUnique<WebPageInspectorTargetController>(*this))
-    , m_userContentController(WebUserContentController::getOrCreate(parameters.userContentControllerID))
+    , m_userContentController(WebUserContentController::getOrCreate(parameters.userContentControllerParameters.identifier))
 #if ENABLE(GEOLOCATION)
     , m_geolocationPermissionRequestManager(makeUniqueRef<GeolocationPermissionRequestManager>(*this))
 #endif

Tools/TestWebKitAPI/Tests/WebKitCocoa/WebsitePolicies.mm
@@ -1778,5 +1778,6 @@ TEST(WebpagePreferences, WebsitePoliciesUserContentController)
     }
     EXPECT_TRUE(caughtException);
 
-    // FIXME: Make _addUserScriptImmediately work successfully after using WKWebpagePreferences._userContentController
+    [replacementUserContentController _addUserScriptImmediately:makeScript(@"alert('testAlert3');")];
+    EXPECT_WK_STREQ([uiDelegate waitForAlert], "testAlert3");
 }