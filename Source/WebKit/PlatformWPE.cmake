99f9b0541a4ab66aaed5a84b3b0fdf80a9830d47
Eugene Mutavchi <Ievgen_Mutavchi@comcast.com>
2020-04-23 20:02:32 +0000

    Fix breakpad integration

--- Source/WebKit/PlatformWPE.cmake
+++ Source/WebKit/PlatformWPE.cmake
@@ -303,20 +303,14 @@ list(APPEND WebKit_LIBRARIES
 WEBKIT_BUILD_INSPECTOR_GRESOURCES(${DERIVED_SOURCES_WEBINSPECTORUI_DIR})
 
 if (ENABLE_BREAKPAD)
-    list(APPEND WebProcess_INCLUDE_DIRECTORIES
+    list(APPEND WebKit_INCLUDE_DIRECTORIES
         ${BREAKPAD_INCLUDE_DIRS}
     )
-
-    list(APPEND WebProcess_LIBRARIES
+    list(APPEND WebKit_LIBRARIES
         ${BREAKPAD_LIBRARIES}
     )
-
-    list(APPEND NetworkProcess_INCLUDE_DIRECTORIES
-        ${BREAKPAD_INCLUDE_DIRS}
-    )
-
-    list(APPEND NetworkProcess_LIBRARIES
-        ${BREAKPAD_LIBRARIES}
+    list(APPEND WebKit_SOURCES
+        Shared/unix/BreakpadExceptionHandler.cpp
     )
 endif ()
 
