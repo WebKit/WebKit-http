f642c1316ce339be150e6238775075c57fb652a3
kenmays <maybird1776@yahoo.com>
2020-05-31 14:25:17 -0700

    commit f69d0e67d3e9fa88bb0c4f09e9c6f4272a197cf5

--- Source/WebCore/platform/PlatformMouseEvent.h
+++ Source/WebCore/platform/PlatformMouseEvent.h
@@ -23,18 +23,12 @@
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE. 
  */
 
-#ifndef PlatformMouseEvent_h
-#define PlatformMouseEvent_h
+#pragma once
 
 #include "IntPoint.h"
 #include "PlatformEvent.h"
 #include "PointerID.h"
 #include <wtf/WindowsExtras.h>
-
-#if PLATFORM(GTK)
-typedef struct _GdkEventButton GdkEventButton;
-typedef struct _GdkEventMotion GdkEventMotion;
-#endif
 
 #if PLATFORM(HAIKU)
 class BMessage;
@@ -55,15 +49,6 @@ const double ForceAtForceClick = 2;
     public:
         PlatformMouseEvent()
             : PlatformEvent(PlatformEvent::MouseMoved)
-            , m_button(NoButton)
-            , m_clickCount(0)
-            , m_modifierFlags(0)
-#if PLATFORM(MAC)
-            , m_eventNumber(0)
-            , m_menuTypeForEvent(0)
-#elif PLATFORM(WIN)
-            , m_didActivateWebView(false)
-#endif
         {
         }
 
@@ -74,19 +59,14 @@ const double ForceAtForceClick = 2;
             , m_globalPosition(globalPosition)
             , m_button(button)
             , m_clickCount(clickCount)
-            , m_modifierFlags(0)
             , m_force(force)
             , m_syntheticClickType(syntheticClickType)
             , m_pointerId(pointerId)
-#if PLATFORM(MAC)
-            , m_eventNumber(0)
-            , m_menuTypeForEvent(0)
-#elif PLATFORM(WIN)
-            , m_didActivateWebView(false)
-#endif
         {
         }
 
+        // This position is relative to the enclosing NSWindow in WebKit1, and is WKWebView-relative in WebKit2.
+        // Use ScrollView::windowToContents() to convert it to into the contents of a given view.
         const IntPoint& position() const { return m_position; }
         const IntPoint& globalPosition() const { return m_globalPosition; }
 #if ENABLE(POINTER_LOCK)
@@ -100,12 +80,6 @@ const double ForceAtForceClick = 2;
         double force() const { return m_force; }
         SyntheticClickType syntheticClickType() const { return m_syntheticClickType; }
         PointerID pointerId() const { return m_pointerId; }
-
-#if PLATFORM(GTK) 
-        explicit PlatformMouseEvent(GdkEventButton*);
-        explicit PlatformMouseEvent(GdkEventMotion*);
-        void setClickCount(int count) { m_clickCount = count; }
-#endif
 
 #if PLATFORM(MAC)
         int eventNumber() const { return m_eventNumber; }
@@ -122,25 +96,25 @@ const double ForceAtForceClick = 2;
         PlatformMouseEvent(const BMessage*);
 #endif
 
-    protected:
+protected:
         IntPoint m_position;
         IntPoint m_globalPosition;
 #if ENABLE(POINTER_LOCK)
         IntPoint m_movementDelta;
 #endif
-        MouseButton m_button;
+        MouseButton m_button { NoButton };
         unsigned short m_buttons { 0 };
-        int m_clickCount;
-        unsigned m_modifierFlags;
+        int m_clickCount { 0 };
+        unsigned m_modifierFlags { 0 };
         double m_force { 0 };
         SyntheticClickType m_syntheticClickType { NoTap };
         PointerID m_pointerId { mousePointerID };
 
 #if PLATFORM(MAC)
-        int m_eventNumber;
-        int m_menuTypeForEvent;
+        int m_eventNumber { 0 };
+        int m_menuTypeForEvent { 0 };
 #elif PLATFORM(WIN)
-        bool m_didActivateWebView;
+        bool m_didActivateWebView { false };
 #endif
     };
 
@@ -159,5 +133,3 @@ const double ForceAtForceClick = 2;
 #endif
 
 } // namespace WebCore
-
-#endif // PlatformMouseEvent_h
