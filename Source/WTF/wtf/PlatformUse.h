f445383e1786ca00ceb00eef48ca54ecc2736024
kenmays <maybird1776@yahoo.com>
2020-05-31 14:06:59 -0700

    commit c48a6fdcf7a78e6aaa265bf6c07e029d3220b1d9

--- Source/WTF/wtf/PlatformUse.h
+++ Source/WTF/wtf/PlatformUse.h
@@ -135,18 +135,24 @@
 #define USE_SYSTEM_MALLOC 1
 #endif
 
-#if !defined(USE_JSVALUE64) && !defined(USE_JSVALUE32_64)
-#if CPU(ADDRESS64) || CPU(ARM64)
+#if CPU(REGISTER64)
 #define USE_JSVALUE64 1
 #else
 #define USE_JSVALUE32_64 1
 #endif
-#endif /* !defined(USE_JSVALUE64) && !defined(USE_JSVALUE32_64) */
+
+#if USE(JSVALUE64)
+#define USE_BIGINT32 1
+#endif
 
 /* FIXME: This name should be more specific if it is only for use with CallFrame* */
 /* Use __builtin_frame_address(1) to get CallFrame* */
 #if COMPILER(GCC_COMPATIBLE) && (CPU(ARM64) || CPU(X86_64))
 #define USE_BUILTIN_FRAME_ADDRESS 1
+#endif
+
+#if PLATFORM(IOS_FAMILY) && CPU(ARM64) && HAVE(REMAP_JIT)
+#define USE_EXECUTE_ONLY_JIT_WRITE_FUNCTION 1
 #endif
 
 #if PLATFORM(IOS)
@@ -173,10 +179,6 @@
 #define USE_METAL 1
 #endif
 
-#if !defined(USE_EXPORT_MACROS) && (PLATFORM(COCOA) || OS(WINDOWS))
-#define USE_EXPORT_MACROS 1
-#endif
-
 #if PLATFORM(GTK) || PLATFORM(WPE) || PLATFORM(HAIKU)
 #define USE_UNIX_DOMAIN_SOCKETS 1
 #endif
@@ -185,31 +187,13 @@
 #define USE_IMLANG_FONT_LINK2 1
 #endif
 
-#if PLATFORM(COCOA)
-#define USE_COREMEDIA 1
-#endif
-
-#if PLATFORM(COCOA)
-#define USE_VIDEOTOOLBOX 1
-#endif
-
 #if PLATFORM(MAC) && __MAC_OS_X_VERSION_MAX_ALLOWED < 101500
 #define USE_AV_SAMPLE_BUFFER_DISPLAY_LAYER 1
-#endif
-
-#if !PLATFORM(WIN)
-#define USE_REQUEST_ANIMATION_FRAME_DISPLAY_MONITOR 1
-#endif
-
-#if PLATFORM(MAC)
-#define USE_COREAUDIO 1
 #endif
 
 #if !defined(USE_ZLIB)
 #define USE_ZLIB 1
 #endif
-
-#define USE_GRAMMAR_CHECKING 1
 
 #if PLATFORM(COCOA) || PLATFORM(GTK)
 #define USE_UNIFIED_TEXT_CHECKING 1
@@ -276,7 +260,6 @@
 #endif
 
 #if PLATFORM(COCOA)
-#define USE_MEDIAREMOTE 1
 #define USE_DICTATION_ALTERNATIVES 1
 #endif
 
@@ -289,8 +272,7 @@
 #endif
 
 /* The override isn't needed on iOS family, as the default behavior is to not sniff. */
-/* FIXME: This should probably be enabled on 10.13.2 and newer, not just 10.14 and newer. */
-#if PLATFORM(MAC) && __MAC_OS_X_VERSION_MIN_REQUIRED >= 101400
+#if PLATFORM(MAC)
 #define USE_CFNETWORK_CONTENT_ENCODING_SNIFFING_OVERRIDE 1
 #endif
 
@@ -326,5 +308,3 @@
 #if PLATFORM(COCOA) && !(PLATFORM(MAC) && __MAC_OS_X_VERSION_MIN_REQUIRED < 101600)
 #define USE_CTFONTTRANSFORMGLYPHSWITHLANGUAGE 1
 #endif
-
-#define USE_UTTYPE_SWIZZLER 0
