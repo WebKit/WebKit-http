WebKit/WebKit-http

commit 973f0081734983117567981c11f39303de525603

[Freetype] Support for the -webkit-font-smoothing CSS property https://bugs.webkit.org/show_bug.cgi?id=54763
Reviewed by Simon Fraser.

Source/WebCore:

Pass the smoothing preference defined on the CSS style, so its
taken into account when drawing the font. If the CSS doesn't
specify any preference then it behaves like before this patch
(it tries to automatically antialias the font, if supported).

This patch sets the antialiasing value at drawing time without
affecting the font, like the Mac/iOS ports do. This approach is
maybe less efficient than taking into account the antialiasing
value when the font platform data its created, but its a better
fit for the current cross-platform code.
I did some benchmarks (see the bug above if you are curious) and
I was unable to prove this method is less efficient than setting
the antialising value when the font platform data is created.

Test: platform/gtk/fonts/webkit-font-smoothing.html

* platform/graphics/cairo/CairoOperations.cpp:
(WebCore::Cairo::drawGlyphsToContext):
(WebCore::Cairo::drawGlyphsShadow):
(WebCore::Cairo::drawGlyphs):
* platform/graphics/cairo/CairoOperations.h:
* platform/graphics/cairo/CairoUtilities.cpp:
* platform/graphics/cairo/CairoUtilities.h:
* platform/graphics/cairo/FontCairo.cpp:
(WebCore::FontCascade::drawGlyphs):
* platform/graphics/cairo/GraphicsContextImplCairo.cpp:
(WebCore::GraphicsContextImplCairo::drawGlyphs):
* platform/graphics/nicosia/cairo/NicosiaCairoOperationRecorder.cpp:
(Nicosia::CairoOperationRecorder::drawGlyphs):

LayoutTests:

Add a regression test. This test is specific for platform gtk because
it depends on the custom fonts.conf file that its used for GTK layout
tests.

* platform/gtk/TestExpectations:
* platform/gtk/fast/css/font-smoothing-expected.png:
* platform/gtk/fonts/webkit-font-smoothing-expected.html: Added.
* platform/gtk/fonts/webkit-font-smoothing.html: Added.
* platform/wpe/TestExpectations:
* platform/wpe/fast/css/font-smoothing-expected.png: Added.

Source/WebCore/platform/graphics/cairo/CairoOperations.cpp
@@ -35,6 +35,7 @@
 
 #if USE(CAIRO)
 
+#include "CairoUniquePtr.h"
 #include "CairoUtilities.h"
 #include "DrawErrorUnderline.h"
 #include "FloatConversion.h"
@@ -306,13 +307,33 @@ static inline StrokeStyle focusRingStrokeStyle()
 #endif
 }
 
-static void drawGlyphsToContext(cairo_t* context, cairo_scaled_font_t* scaledFont, double syntheticBoldOffset, const Vector<cairo_glyph_t>& glyphs)
+static void drawGlyphsToContext(cairo_t* context, cairo_scaled_font_t* scaledFont, double syntheticBoldOffset, const Vector<cairo_glyph_t>& glyphs, FontSmoothingMode fontSmoothingMode)
 {
     cairo_matrix_t originalTransform;
     if (syntheticBoldOffset)
         cairo_get_matrix(context, &originalTransform);
 
     cairo_set_scaled_font(context, scaledFont);
+
+    // The scaled font defaults to FontSmoothingMode::AutoSmoothing. Only override antialiasing settings if its not auto.
+    if (fontSmoothingMode != FontSmoothingMode::AutoSmoothing) {
+        CairoUniquePtr<cairo_font_options_t> fontOptionsSmoothing(cairo_font_options_copy(getDefaultCairoFontOptions()));
+        switch (fontSmoothingMode) {
+        case FontSmoothingMode::Antialiased:
+            cairo_font_options_set_antialias(fontOptionsSmoothing.get(), CAIRO_ANTIALIAS_GRAY);
+            break;
+        case FontSmoothingMode::SubpixelAntialiased:
+            cairo_font_options_set_antialias(fontOptionsSmoothing.get(), CAIRO_ANTIALIAS_SUBPIXEL);
+            break;
+        case FontSmoothingMode::NoSmoothing:
+            cairo_font_options_set_antialias(fontOptionsSmoothing.get(), CAIRO_ANTIALIAS_NONE);
+            break;
+        default:
+            ASSERT_NOT_REACHED();
+        }
+        cairo_set_font_options(context, fontOptionsSmoothing.get());
+    }
+
     cairo_show_glyphs(context, glyphs.data(), glyphs.size());
 
     if (syntheticBoldOffset) {
@@ -323,7 +344,7 @@ static void drawGlyphsToContext(cairo_t* context, cairo_scaled_font_t* scaledFon
     }
 }
 
-static void drawGlyphsShadow(PlatformContextCairo& platformContext, const ShadowState& shadowState, TextDrawingModeFlags textDrawingMode, const FloatSize& shadowOffset, const Color& shadowColor, const FloatPoint& point, cairo_scaled_font_t* scaledFont, double syntheticBoldOffset, const Vector<cairo_glyph_t>& glyphs)
+static void drawGlyphsShadow(PlatformContextCairo& platformContext, const ShadowState& shadowState, TextDrawingModeFlags textDrawingMode, const FloatSize& shadowOffset, const Color& shadowColor, const FloatPoint& point, cairo_scaled_font_t* scaledFont, double syntheticBoldOffset, const Vector<cairo_glyph_t>& glyphs, FontSmoothingMode fontSmoothingMode)
 {
     ShadowBlur shadow({ shadowState.blur, shadowState.blur }, shadowState.offset, shadowState.color, shadowState.ignoreTransforms);
     if (!(textDrawingMode & TextModeFill) || shadow.type() == ShadowBlur::NoShadow)
@@ -336,7 +357,7 @@ static void drawGlyphsShadow(PlatformContextCairo& platformContext, const Shadow
 
         cairo_translate(context, shadowOffset.width(), shadowOffset.height());
         setSourceRGBAFromColor(context, shadowColor);
-        drawGlyphsToContext(context, scaledFont, syntheticBoldOffset, glyphs);
+        drawGlyphsToContext(context, scaledFont, syntheticBoldOffset, glyphs, fontSmoothingMode);
 
         cairo_restore(context);
         return;
@@ -347,9 +368,9 @@ static void drawGlyphsShadow(PlatformContextCairo& platformContext, const Shadow
     FloatRect fontExtentsRect(point.x() + extents.x_bearing, point.y() + extents.y_bearing, extents.width, extents.height);
 
     shadow.drawShadowLayer(State::getCTM(platformContext), State::getClipBounds(platformContext), fontExtentsRect,
-        [scaledFont, syntheticBoldOffset, &glyphs](GraphicsContext& shadowContext)
+        [scaledFont, syntheticBoldOffset, &glyphs, fontSmoothingMode](GraphicsContext& shadowContext)
         {
-            drawGlyphsToContext(shadowContext.platformContext()->cr(), scaledFont, syntheticBoldOffset, glyphs);
+            drawGlyphsToContext(shadowContext.platformContext()->cr(), scaledFont, syntheticBoldOffset, glyphs, fontSmoothingMode);
         },
         [&platformContext, &shadowState](ImageBuffer& layerImage, const FloatPoint& layerOrigin, const FloatSize& layerSize)
         {
@@ -836,16 +857,16 @@ void clearRect(PlatformContextCairo& platformContext, const FloatRect& rect)
     cairo_restore(cr);
 }
 
-void drawGlyphs(PlatformContextCairo& platformContext, const FillSource& fillSource, const StrokeSource& strokeSource, const ShadowState& shadowState, const FloatPoint& point, cairo_scaled_font_t* scaledFont, double syntheticBoldOffset, const Vector<cairo_glyph_t>& glyphs, float xOffset, TextDrawingModeFlags textDrawingMode, float strokeThickness, const FloatSize& shadowOffset, const Color& shadowColor)
+void drawGlyphs(PlatformContextCairo& platformContext, const FillSource& fillSource, const StrokeSource& strokeSource, const ShadowState& shadowState, const FloatPoint& point, cairo_scaled_font_t* scaledFont, double syntheticBoldOffset, const Vector<cairo_glyph_t>& glyphs, float xOffset, TextDrawingModeFlags textDrawingMode, float strokeThickness, const FloatSize& shadowOffset, const Color& shadowColor, FontSmoothingMode fontSmoothingMode)
 {
-    drawGlyphsShadow(platformContext, shadowState, textDrawingMode, shadowOffset, shadowColor, point, scaledFont, syntheticBoldOffset, glyphs);
+    drawGlyphsShadow(platformContext, shadowState, textDrawingMode, shadowOffset, shadowColor, point, scaledFont, syntheticBoldOffset, glyphs, fontSmoothingMode);
 
     cairo_t* cr = platformContext.cr();
     cairo_save(cr);
 
     if (textDrawingMode & TextModeFill) {
         prepareForFilling(cr, fillSource, AdjustPatternForGlobalAlpha);
-        drawGlyphsToContext(cr, scaledFont, syntheticBoldOffset, glyphs);
+        drawGlyphsToContext(cr, scaledFont, syntheticBoldOffset, glyphs, fontSmoothingMode);
     }
 
     // Prevent running into a long computation within cairo. If the stroke width is

LayoutTests/platform/gtk/TestExpectations
@@ -814,8 +814,6 @@ webkit.org/b/142268 editing/pasteboard/copy-paste-attachment.html [ Failure ]
 webkit.org/b/142269 fast/attachment/attachment-dom.html [ Failure ]
 webkit.org/b/142270 fast/canvas/canvas-ellipse-zero-lineto.html [ Failure ]
 
-webkit.org/b/142819 fast/text/baseline-inline-block.html [ ImageOnlyFailure ]
-
 # This feature is only enabled on Mac and iOS right now
 contentfiltering [ Skip ]
 http/tests/contentfiltering [ Skip ]
@@ -1205,6 +1203,9 @@ webkit.org/b/206001 imported/w3c/web-platform-tests/css/css-values/ch-unit-017.h
 
 webkit.org/b/206002 http/wpt/css/css-highlight-api/highlight-text-replace.html [ ImageOnlyFailure ]
 webkit.org/b/206002 http/wpt/css/css-highlight-api/highlight-text.html [ ImageOnlyFailure ]
+
+webkit.org/b/205186 compositing/contents-format/subpixel-antialiased-text.html [ ImageOnlyFailure ]
+webkit.org/b/205187 fast/text/font-antialiasing-save-restore.html [ ImageOnlyFailure ]
 
 #////////////////////////////////////////////////////////////////////////////////////////
 # End of Expected failures.

LayoutTests/platform/gtk/fonts/webkit-font-smoothing-expected.html
@@ -1,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <style>
+        p {
+            font-size: 16pt;
+            font-family: Liberation Sans;
+        }
+    </style>
+</head>
+<body>
+    <p>This test is not expected to work outside of WebKitTestRunner.</p>
+    <p>It depends on the custom fonts.conf file that its loaded by the test tooling.</p>
+    <p>Test results:</p>
+    <p style="font-family: NonAntiAliasedSans;">This should NOT be antialiased.</p>
+    <p style="font-family: SubpixelDisabledSans;">This should be antialiased.<p>
+</body>
+</html>

LayoutTests/platform/gtk/fonts/webkit-font-smoothing.html
@@ -1,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <style>
+        p {
+            font-size: 16pt;
+            font-family: Liberation Sans;
+        }
+    </style>
+</head>
+<body>
+    <p>This test is not expected to work outside of WebKitTestRunner.</p>
+    <p>It depends on the custom fonts.conf file that its loaded by the test tooling.</p>
+    <p>Test results:</p>
+    <p style="-webkit-font-smoothing: none">This should NOT be antialiased.</p>
+    <p style="-webkit-font-smoothing: antialiased">This should be antialiased.</p>
+</body>
+</html>

LayoutTests/platform/wpe/TestExpectations
@@ -505,6 +505,9 @@ webkit.org/b/200162 http/tests/websocket/tests/hybi/inspector/ [ Skip ]
 webkit.org/b/200165 imported/w3c/web-platform-tests/websockets/cookies/007.html [ Failure ]
 webkit.org/b/200165 imported/w3c/web-platform-tests/websockets/cookies/004.html [ Failure ]
 
+webkit.org/b/205186 compositing/contents-format/subpixel-antialiased-text.html [ ImageOnlyFailure ]
+webkit.org/b/205187 fast/text/font-antialiasing-save-restore.html [ ImageOnlyFailure ]
+
 #////////////////////////////////////////////////////////////////////////////////////////
 # 3. UNRESOLVED TESTS
 #////////////////////////////////////////////////////////////////////////////////////////

Source/WebCore/platform/graphics/cairo/CairoOperations.h
@@ -136,7 +136,7 @@ void strokeRect(PlatformContextCairo&, const FloatRect&, float, const StrokeSour
 void strokePath(PlatformContextCairo&, const Path&, const StrokeSource&, const ShadowState&);
 void clearRect(PlatformContextCairo&, const FloatRect&);
 
-void drawGlyphs(PlatformContextCairo&, const FillSource&, const StrokeSource&, const ShadowState&, const FloatPoint&, cairo_scaled_font_t*, double, const Vector<cairo_glyph_t>&, float, TextDrawingModeFlags, float, const FloatSize&, const Color&);
+void drawGlyphs(PlatformContextCairo&, const FillSource&, const StrokeSource&, const ShadowState&, const FloatPoint&, cairo_scaled_font_t*, double, const Vector<cairo_glyph_t>&, float, TextDrawingModeFlags, float, const FloatSize&, const Color&, FontSmoothingMode);
 
 void drawNativeImage(PlatformContextCairo&, cairo_surface_t*, const FloatRect&, const FloatRect&, const ImagePaintingOptions&, float, const ShadowState&);
 void drawPattern(PlatformContextCairo&, cairo_surface_t*, const IntSize&, const FloatRect&, const FloatRect&, const AffineTransform&, const FloatPoint&, const ImagePaintingOptions&);

commit 2707c95e23dd36bcdd54f10eb8d575c8f463890d

REGRESSION(r254506): [Freetype] Spektra variable font does not work properly https://bugs.webkit.org/show_bug.cgi?id=215214 <rdar://problem/66984524>
Reviewed by Adrian Perez de Castro.

This regressed in r254506, when a font smoothing mode is passed to drawGlyphsToContext() a new cairo font
options is set, using the default font options and changing the antialiasing. This means the font options from
the font (the ones containing the variation settings) set in the context by cairo_set_scaled_font() are
lost. We should copy the scaled font options instead, then set the antialiasing and apply them.

* platform/graphics/cairo/CairoOperations.cpp:
(WebCore::Cairo::drawGlyphsToContext): Use cairo_scaled_font_get_font_options() instead of getDefaultCairoFontOptions().

git-svn-id: http://svn.webkit.org/repository/webkit/trunk@265648 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Source/WebCore/platform/graphics/cairo/CairoOperations.cpp
@@ -286,7 +286,9 @@ static void drawGlyphsToContext(cairo_t* context, cairo_scaled_font_t* scaledFon
 
     // The scaled font defaults to FontSmoothingMode::AutoSmoothing. Only override antialiasing settings if its not auto.
     if (fontSmoothingMode != FontSmoothingMode::AutoSmoothing) {
-        CairoUniquePtr<cairo_font_options_t> fontOptionsSmoothing(cairo_font_options_copy(getDefaultCairoFontOptions()));
+        CairoUniquePtr<cairo_font_options_t> fontOptionsSmoothing(cairo_font_options_create());
+        cairo_scaled_font_get_font_options(scaledFont, fontOptionsSmoothing.get());
+
         switch (fontSmoothingMode) {
         case FontSmoothingMode::Antialiased:
             // Don't use CAIRO_ANTIALIAS_GRAY in Windows. It is mapped to ANTIALIASED_QUALITY which looks jaggy and faint.