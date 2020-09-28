WebKit/WebKit-http

commit c28a64dbf525283d8d028c531b2239cd016679f6

Selection API: Introduce LiveRangeSelectionEnabled, off by default https://bugs.webkit.org/show_bug.cgi?id=216656
Reviewed by Sam Weinig.

Source/WebCore:

For interoperability, the Selection API requires behavior that is quite different
from how our Selection object has behaves historically. Specifically, the range
returned is a live range that update as the selection updates and the selection,
in turn, is updated if the returned range is modified.

This significant change will have compatibility impact on websites and other
content that assumes the legacy  WebKit behavior, so we are developing it behind
a feature flag, LiveRangeSelectionEnabled.

This patch introduces the live range selection behind the flag, but leaves one
significant area unresolved, changing selection to keep track of the original
endpoints rather than only canonicalized endpoints, and a couple of loose ends,
lifetime of ride-along properties on the live range object, and updating test
expectations for the new behavior.

* dom/CharacterData.cpp:
(WebCore::CharacterData::setData): Fixed timing of call to Document::textRemoved
by letting setDataAndUpdate do it; otherwise it can be called after the selection
has been updated, and the range gets updated twice.
(WebCore::CharacterData::appendData): Pass UpdateLiveRanges::No to setDataAndUpdate,
to preserve the existing "don't update ranges" behavior for now at least.
(WebCore::CharacterData::insertData): Fixed timing of call to Document::textInserted
by letting setDataAndUpdate do it, for the same reason as above.
(WebCore::CharacterData::deleteData): Ditto, for textRemoved.
(WebCore::CharacterData::replaceData): Ditto, for textRemoved and textInserted.
(WebCore::CharacterData::setDataAndUpdate): Added UpdateLiveRanges argument and
calls to textRemoved and textInserted, after setting data, but before other updates.

* dom/CharacterData.h: Made setDataAndUpdate protected and added UpdateLiveRanges.

LayoutTests/editing/selection/move-to-line-boundary-clear-selection.html
@@ -17,7 +17,7 @@ if (window.testRunner)
     testRunner.execCommand("MoveToEndOfLine");
 shouldBe('getSelection().toString()', "''");
 getSelection().empty();
-getSelection().setPosition(span, 5);
+getSelection().setPosition(span, 1);
 getSelection().modify("extend", "backward", "line");
 if (window.testRunner)
     testRunner.execCommand("MoveToBeginningOfLine");

LayoutTests/editing/selection/toString-1.html
@@ -20,7 +20,7 @@ function runTest() {
         var htmlElement = bodyElement.parentNode;
         
         if (selection.setBaseAndExtent)
-            selection.setBaseAndExtent(bodyElement, 1, htmlElement, 3);
+            selection.setBaseAndExtent(bodyElement, 1, htmlElement, 2);
         else
             throw("Couldn't set a selection.");
         

LayoutTests/editing/selection/user-select-all-selection.html
@@ -49,7 +49,7 @@ function selectionShouldBeRelativeToUserSelectAllElement(str) {
 
 function placeCaretBeforeUserSelectAllElement(){
     var userSelectAllElement = document.getElementById("allArea");
-    execSetSelectionCommand(userSelectAllElement.previousSibling.firstChild, userSelectAllElement.previousSibling.textContent.length, userSelectAllElement.previousSibling, userSelectAllElement.previousSibling.textContent.length);
+    execSetSelectionCommand(userSelectAllElement.previousSibling.firstChild, userSelectAllElement.previousSibling.textContent.length, userSelectAllElement.previousSibling.firstChild, userSelectAllElement.previousSibling.textContent.length);
 }
 
 function mouseMoveFromTo(fromX, toX){

Source/WebCore/dom/CharacterData.cpp
@@ -63,7 +63,6 @@ void CharacterData::setData(const String& data)
     Ref<CharacterData> protectedThis(*this);
 
     setDataAndUpdate(nonNullData, 0, oldLength, nonNullData.length());
-    document().textRemoved(*this, 0, oldLength);
 }
 
 ExceptionOr<String> CharacterData::substringData(unsigned offset, unsigned count)
@@ -117,12 +116,7 @@ unsigned CharacterData::parserAppendData(const String& string, unsigned offset, 
 
 void CharacterData::appendData(const String& data)
 {
-    String newStr = m_data;
-    newStr.append(data);
-
-    setDataAndUpdate(newStr, m_data.length(), 0, data.length());
-
-    // FIXME: Should we call textInserted here?
+    setDataAndUpdate(m_data + data, m_data.length(), 0, data.length(), UpdateLiveRanges::No);
 }
 
 ExceptionOr<void> CharacterData::insertData(unsigned offset, const String& data)
@@ -130,12 +124,9 @@ ExceptionOr<void> CharacterData::insertData(unsigned offset, const String& data)
     if (offset > length())
         return Exception { IndexSizeError };
 
-    String newStr = m_data;
-    newStr.insert(data, offset);
-
-    setDataAndUpdate(newStr, offset, 0, data.length());
-
-    document().textInserted(*this, offset, data.length());
+    String newData = m_data;
+    newData.insert(data, offset);
+    setDataAndUpdate(newData, offset, 0, data.length());
 
     return { };
 }
@@ -147,12 +138,9 @@ ExceptionOr<void> CharacterData::deleteData(unsigned offset, unsigned count)
 
     count = std::min(count, length() - offset);
 
-    String newStr = m_data;
-    newStr.remove(offset, count);
-
-    setDataAndUpdate(newStr, offset, count, 0);
-
-    document().textRemoved(*this, offset, count);
+    String newData = m_data;
+    newData.remove(offset, count);
+    setDataAndUpdate(newData, offset, count, 0);
 
     return { };
 }
@@ -164,15 +152,10 @@ ExceptionOr<void> CharacterData::replaceData(unsigned offset, unsigned count, co
 
     count = std::min(count, length() - offset);
 
-    String newStr = m_data;
-    newStr.remove(offset, count);
-    newStr.insert(data, offset);
-
-    setDataAndUpdate(newStr, offset, count, data.length());
-
-    // update the markers for spell checking and grammar checking
-    document().textRemoved(*this, offset, count);
-    document().textInserted(*this, offset, data.length());
+    String newData = m_data;
+    newData.remove(offset, count);
+    newData.insert(data, offset);
+    setDataAndUpdate(newData, offset, count, data.length());
 
     return { };
 }
@@ -188,10 +171,15 @@ ExceptionOr<void> CharacterData::setNodeValue(const String& nodeValue)
     return { };
 }
 
-void CharacterData::setDataAndUpdate(const String& newData, unsigned offsetOfReplacedData, unsigned oldLength, unsigned newLength)
+void CharacterData::setDataAndUpdate(const String& newData, unsigned offsetOfReplacedData, unsigned oldLength, unsigned newLength, UpdateLiveRanges shouldUpdateLiveRanges)
 {
     String oldData = m_data;
     m_data = newData;
+
+    if (oldLength && shouldUpdateLiveRanges != UpdateLiveRanges::No)
+        document().textRemoved(*this, offsetOfReplacedData, oldLength);
+    if (newLength && shouldUpdateLiveRanges != UpdateLiveRanges::No)
+        document().textInserted(*this, offsetOfReplacedData, newLength);
 
     ASSERT(!renderer() || is<Text>(*this));
     if (is<Text>(*this) && parentNode())

Source/WebCore/dom/CharacterData.h
@@ -33,7 +33,6 @@ public:
     static ptrdiff_t dataMemoryOffset() { return OBJECT_OFFSETOF(CharacterData, m_data); }
 
     WEBCORE_EXPORT void setData(const String&);
-    virtual void setDataAndUpdate(const String&, unsigned offsetOfReplacedData, unsigned oldLength, unsigned newLength);
     unsigned length() const { return m_data.length(); }
     WEBCORE_EXPORT ExceptionOr<String> substringData(unsigned offset, unsigned count);
     WEBCORE_EXPORT void appendData(const String&);
@@ -59,6 +58,9 @@ protected:
         m_data = data;
     }
     void dispatchModifiedEvent(const String& oldValue);
+
+    enum class UpdateLiveRanges : bool { No, Yes };
+    virtual void setDataAndUpdate(const String&, unsigned offsetOfReplacedData, unsigned oldLength, unsigned newLength, UpdateLiveRanges = UpdateLiveRanges::Yes);
 
 private:
     String nodeValue() const final;

Source/WebCore/dom/Document.h
@@ -1591,6 +1591,9 @@ public:
     void canvasResized(CanvasBase&) final { };
     void canvasDestroyed(CanvasBase&) final;
 
+    bool contains(const Node& node) const { return this == &node.treeScope() && node.isConnected(); }
+    bool contains(const Node* node) const { return node && contains(*node); }
+
 protected:
     enum ConstructionFlags { Synthesized = 1, NonRenderedPlaceholder = 1 << 1 };
     WEBCORE_EXPORT Document(Frame*, const URL&, DocumentClassFlags = DefaultDocumentClass, unsigned constructionFlags = 0);

Source/WebCore/dom/Node.cpp
@@ -2,7 +2,7 @@
  * Copyright (C) 1999 Lars Knoll (knoll@kde.org)
  *           (C) 1999 Antti Koivisto (koivisto@kde.org)
  *           (C) 2001 Dirk Mueller (mueller@kde.org)
- * Copyright (C) 2004-2017 Apple Inc. All rights reserved.
+ * Copyright (C) 2004-2020 Apple Inc. All rights reserved.
  * Copyright (C) 2008 Nokia Corporation and/or its subsidiary(-ies)
  * Copyright (C) 2009 Torch Mobile Inc. All rights reserved. (http://www.torchmobile.com/)
  *
@@ -1020,12 +1020,12 @@ ExceptionOr<void> Node::checkSetPrefix(const AtomString& prefix)
 
 bool Node::isDescendantOf(const Node& other) const
 {
-    // Return true if other is an ancestor of this, otherwise false
+    // Return true if other is an ancestor of this.
+    if (other.isDocumentNode())
+        return &treeScope().rootNode() == &other && !isDocumentNode() && isConnected();
     if (!other.hasChildNodes() || isConnected() != other.isConnected())
         return false;
-    if (other.isDocumentNode())
-        return &document() == &other && !isDocumentNode() && isConnected();
-    for (const auto* ancestor = parentNode(); ancestor; ancestor = ancestor->parentNode()) {
+    for (auto ancestor = parentNode(); ancestor; ancestor = ancestor->parentNode()) {
         if (ancestor == &other)
             return true;
     }
@@ -1034,15 +1034,13 @@ bool Node::isDescendantOf(const Node& other) const
 
 bool Node::isDescendantOrShadowDescendantOf(const Node* other) const
 {
-    // FIXME: This element's shadow tree's host could be inside another shadow tree.
-    // This function doesn't handle that case correctly. Maybe share code with
-    // the containsIncludingShadowDOM function?
+    // FIXME: Correctly handle the case where the shadow host is itself in a shadow tree.
     return other && (isDescendantOf(*other) || other->contains(shadowHost()));
 }
 
-bool Node::contains(const Node* node) const
+bool Node::contains(const Node& node) const
 {
-    return this == node || (node && node->isDescendantOf(*this));
+    return this == &node || node.isDescendantOf(*this);
 }
 
 bool Node::containsIncludingShadowDOM(const Node* node) const

Source/WebCore/dom/Node.h
@@ -2,7 +2,7 @@
  * Copyright (C) 1999 Lars Knoll (knoll@kde.org)
  *           (C) 1999 Antti Koivisto (koivisto@kde.org)
  *           (C) 2001 Dirk Mueller (mueller@kde.org)
- * Copyright (C) 2004-2017 Apple Inc. All rights reserved.
+ * Copyright (C) 2004-2020 Apple Inc. All rights reserved.
  * Copyright (C) 2008, 2009 Torch Mobile Inc. All rights reserved. (http://www.torchmobile.com/)
  *
  * This library is free software; you can redistribute it and/or
@@ -371,9 +371,10 @@ public:
 
     WEBCORE_EXPORT bool isDescendantOf(const Node&) const;
     bool isDescendantOf(const Node* other) const { return other && isDescendantOf(*other); }
+    WEBCORE_EXPORT bool contains(const Node&) const;
+    bool contains(const Node* other) const { return other && contains(*other); }
 
     bool isDescendantOrShadowDescendantOf(const Node*) const;
-    WEBCORE_EXPORT bool contains(const Node*) const;
     WEBCORE_EXPORT bool containsIncludingShadowDOM(const Node*) const;
 
     // Whether or not a selection can be started in this object

commit 20b760c2eb0e71baacd2278f90d3eec1badf4a9d

Document.p.contains returns true for nodes in shadow tree https://bugs.webkit.org/show_bug.cgi?id=189164
Reviewed by Darin Adler.

Added a regression after r267220 had fixed this bug.

* fast/shadow-dom/document-contains-returns-false-for-node-inside-shadow-tree-expected.txt: Added.
* fast/shadow-dom/document-contains-returns-false-for-node-inside-shadow-tree.html: Added.


git-svn-id: http://svn.webkit.org/repository/webkit/trunk@267719 268f45cc-cd09-0410-ab3c-d52691b4dbfc

LayoutTests/fast/shadow-dom/document-contains-returns-false-for-node-inside-shadow-tree-expected.txt
@@ -1,0 +1,21 @@
+Tests for document.contains. It should return false on connected nodes within a shadow root
+
+On success, you will see a series of "PASS" messages, followed by "TEST COMPLETE".
+
+
+PASS shadowHost.isConnected is false
+PASS shadowRoot.isConnected is false
+PASS shadowRootChild.isConnected is false
+PASS document.contains(shadowHost) is false
+PASS document.contains(shadowRoot) is false
+PASS document.contains(shadowRootChild) is false
+PASS document.body.appendChild(shadowHost); shadowHost.isConnected is true
+PASS shadowRoot.isConnected is true
+PASS shadowRootChild.isConnected is true
+PASS document.contains(shadowHost) is true
+PASS document.contains(shadowRoot) is false
+PASS document.contains(shadowRootChild) is false
+PASS successfullyParsed is true
+
+TEST COMPLETE
+

LayoutTests/fast/shadow-dom/document-contains-returns-false-for-node-inside-shadow-tree.html
@@ -1,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<body>
+<script src="../../resources/js-test.js"></script>
+<script>
+
+description('Tests for document.contains. It should return false on connected nodes within a shadow root');
+
+const shadowHost = document.createElement("div");
+const shadowRoot = shadowHost.attachShadow({mode: 'closed'});
+const shadowRootChild = shadowRoot.appendChild(document.createElement('div'));
+
+shouldBeFalse('shadowHost.isConnected');
+shouldBeFalse('shadowRoot.isConnected');
+shouldBeFalse('shadowRootChild.isConnected');
+shouldBeFalse('document.contains(shadowHost)');
+shouldBeFalse('document.contains(shadowRoot)');
+shouldBeFalse('document.contains(shadowRootChild)');
+
+shouldBeTrue('document.body.appendChild(shadowHost); shadowHost.isConnected');
+shouldBeTrue('shadowRoot.isConnected');
+shouldBeTrue('shadowRootChild.isConnected');
+shouldBeTrue('document.contains(shadowHost)');
+shouldBeFalse('document.contains(shadowRoot)');
+shouldBeFalse('document.contains(shadowRootChild)');
+
+</script>
+</body>
+</html>