fberlakovich/r2c-webkit

[GStreamer] Add support for canvas captureStream, tests failing since they were added in r213598 https://bugs.webkit.org/show_bug.cgi?id=169811
Patch by Philippe Normand <pnormand@igalia.com> on 2020-07-20
Reviewed by Darin Adler.

Source/WebCore:

Hook captureStream support in GStreamer ports. The only missing bits were:

1. create a MediaSample from a BGRA buffer
2. convert a MediaSample to a RGBA buffer (used only for layout tests)
3. set video width and height tags not only on video capture tracks (tracks created from
   captureStream are not GStreamer capture device tracks)

* html/HTMLCanvasElement.cpp:
(WebCore::HTMLCanvasElement::toMediaSample):
* platform/graphics/avfoundation/objc/MediaSampleAVFObjC.h: Refactor createImageSample to
use unsigned for dimensions.
* platform/graphics/avfoundation/objc/MediaSampleAVFObjC.mm:
* platform/graphics/gstreamer/MediaSampleGStreamer.cpp:
(WebCore::MediaSampleGStreamer::createImageSample):
(WebCore::MediaSampleGStreamer::getRGBAImageData const):
* platform/graphics/gstreamer/MediaSampleGStreamer.h:
* platform/mediastream/gstreamer/GStreamerMediaStreamSource.cpp:
(mediaStreamTrackPrivateGetTags):

LayoutTests:

Unflag passing tests and rebaseline one platform-specific WPT test expectation which differs
from the default by only one line.

* platform/glib/imported/w3c/web-platform-tests/mediacapture-streams/MediaStream-MediaElement-srcObject.https-expected.txt: Added.
* platform/gtk/TestExpectations:
* platform/gtk/imported/w3c/web-platform-tests/mediacapture-streams/MediaStream-MediaElement-srcObject.https-expected.txt: Removed.
* platform/wpe/TestExpectations:

git-svn-id: https://svn.webkit.org/repository/webkit/trunk@264607 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Source/WebCore/platform/graphics/gstreamer/MediaSampleGStreamer.cpp
@@ -23,6 +23,8 @@
 
 #include "GStreamerCommon.h"
 
+#include <JavaScriptCore/JSCInlines.h>
+#include <JavaScriptCore/TypedArrayInlines.h>
 #include <algorithm>
 
 #if ENABLE(VIDEO) && USE(GSTREAMER)
@@ -93,6 +95,53 @@ Ref<MediaSampleGStreamer> MediaSampleGStreamer::createFakeSample(GstCaps*, Media
     gstreamerMediaSample->m_duration = duration;
     gstreamerMediaSample->m_flags = MediaSample::IsNonDisplaying;
     return adoptRef(*gstreamerMediaSample);
+}
+
+Ref<MediaSampleGStreamer> MediaSampleGStreamer::createImageSample(Vector<uint8_t>&& bgraData, unsigned width, unsigned height)
+{
+    size_t size = bgraData.sizeInBytes();
+    auto* data = bgraData.releaseBuffer().leakPtr();
+    auto buffer = adoptGRef(gst_buffer_new_wrapped_full(GST_MEMORY_FLAG_READONLY, data, size, 0, size, data, [](gpointer data) {
+        WTF::VectorMalloc::free(data);
+    }));
+    gst_buffer_add_video_meta(buffer.get(), GST_VIDEO_FRAME_FLAG_NONE, GST_VIDEO_FORMAT_BGRA, width, height);
+    auto caps = adoptGRef(gst_caps_new_simple("video/x-raw", "format", G_TYPE_STRING, "BGRA", "width", G_TYPE_INT, width, "height", G_TYPE_INT, height, "framerate", GST_TYPE_FRACTION, 1, 1, nullptr));
+    auto sample = adoptGRef(gst_sample_new(buffer.get(), caps.get(), nullptr, nullptr));
+    return create(WTFMove(sample), FloatSize(width, height), { });
+}
+
+RefPtr<JSC::Uint8ClampedArray> MediaSampleGStreamer::getRGBAImageData() const
+{
+    auto* caps = gst_sample_get_caps(m_sample.get());
+    GstVideoInfo inputInfo;
+    if (!gst_video_info_from_caps(&inputInfo, caps))
+        return nullptr;
+
+    // We could check the input format is RGBA before attempting a conversion, but it is very
+    // unlikely to pay off. The input format is likely to be BGRA (when the samples are created as a
+    // result of mediastream captureStream) or some YUV format if the sample is from a video capture
+    // device. This method is called only by internals during layout tests, it is thus not critical
+    // to optimize this code path.
+
+    auto outputCaps = adoptGRef(gst_caps_copy(caps));
+    gst_caps_set_simple(outputCaps.get(), "format", G_TYPE_STRING, "RGBA", nullptr);
+
+    GstVideoInfo outputInfo;
+    if (!gst_video_info_from_caps(&outputInfo, outputCaps.get()))
+        return nullptr;
+
+    int width = GST_VIDEO_INFO_WIDTH(&inputInfo);
+    int height = GST_VIDEO_INFO_HEIGHT(&inputInfo);
+    unsigned byteLength = GST_VIDEO_INFO_SIZE(&inputInfo);
+    auto bufferStorage = JSC::ArrayBuffer::create(width * height, 4);
+    auto outputBuffer = adoptGRef(gst_buffer_new_wrapped_full(GST_MEMORY_FLAG_NO_SHARE, bufferStorage->data(), byteLength, 0, byteLength, nullptr, [](gpointer) { }));
+    gst_buffer_add_video_meta(outputBuffer.get(), GST_VIDEO_FRAME_FLAG_NONE, GST_VIDEO_FORMAT_RGBA, width, height);
+    GstMappedFrame outputFrame(outputBuffer.get(), outputInfo, GST_MAP_WRITE);
+
+    GUniquePtr<GstVideoConverter> converter(gst_video_converter_new(&inputInfo, &outputInfo, nullptr));
+    GstMappedFrame inputFrame(gst_sample_get_buffer(m_sample.get()), inputInfo, GST_MAP_READ);
+    gst_video_converter_frame(converter.get(), inputFrame.get(), outputFrame.get());
+    return JSC::Uint8ClampedArray::tryCreate(WTFMove(bufferStorage), 0, byteLength);
 }
 
 void MediaSampleGStreamer::extendToTheBeginning()

WebKit/WebKit-http

git-svn-id: http://svn.webkit.org/repository/webkit/trunk@264607 268f45cc-cd09-0410-ab3c-d52691b4dbfc
Signed-off-by: Xabier Rodriguez Calvar <calvaris@igalia.com>

Source/WebCore/platform/graphics/gstreamer/MediaSampleGStreamer.cpp
@@ -23,6 +23,8 @@
 
 #include "GStreamerCommon.h"
 
+#include <JavaScriptCore/JSCInlines.h>
+#include <JavaScriptCore/TypedArrayInlines.h>
 #include <algorithm>
 
 #if ENABLE(VIDEO) && USE(GSTREAMER)
@@ -93,6 +95,53 @@ Ref<MediaSampleGStreamer> MediaSampleGStreamer::createFakeSample(GstCaps*, Media
     gstreamerMediaSample->m_duration = duration;
     gstreamerMediaSample->m_flags = MediaSample::IsNonDisplaying;
     return adoptRef(*gstreamerMediaSample);
+}
+
+Ref<MediaSampleGStreamer> MediaSampleGStreamer::createImageSample(Vector<uint8_t>&& bgraData, unsigned width, unsigned height)
+{
+    size_t size = bgraData.size();
+    auto* data = bgraData.releaseBuffer().leakPtr();
+    auto buffer = adoptGRef(gst_buffer_new_wrapped_full(GST_MEMORY_FLAG_READONLY, data, size, 0, size, data, [](gpointer data) {
+        WTF::VectorMalloc::free(data);
+    }));
+    gst_buffer_add_video_meta(buffer.get(), GST_VIDEO_FRAME_FLAG_NONE, GST_VIDEO_FORMAT_BGRA, width, height);
+    auto caps = adoptGRef(gst_caps_new_simple("video/x-raw", "format", G_TYPE_STRING, "BGRA", "width", G_TYPE_INT, width, "height", G_TYPE_INT, height, "framerate", GST_TYPE_FRACTION, 1, 1, nullptr));
+    auto sample = adoptGRef(gst_sample_new(buffer.get(), caps.get(), nullptr, nullptr));
+    return create(WTFMove(sample), FloatSize(width, height), { });
+}
+
+RefPtr<JSC::Uint8ClampedArray> MediaSampleGStreamer::getRGBAImageData() const
+{
+    auto* caps = gst_sample_get_caps(m_sample.get());
+    GstVideoInfo inputInfo;
+    if (!gst_video_info_from_caps(&inputInfo, caps))
+        return nullptr;
+
+    // We could check the input format is RGBA before attempting a conversion, but it is very
+    // unlikely to pay off. The input format is likely to be BGRA (when the samples are created as a
+    // result of mediastream captureStream) or some YUV format if the sample is from a video capture
+    // device. This method is called only by internals during layout tests, it is thus not critical
+    // to optimize this code path.
+
+    auto outputCaps = adoptGRef(gst_caps_copy(caps));
+    gst_caps_set_simple(outputCaps.get(), "format", G_TYPE_STRING, "RGBA", nullptr);
+
+    GstVideoInfo outputInfo;
+    if (!gst_video_info_from_caps(&outputInfo, outputCaps.get()))
+        return nullptr;
+
+    int width = GST_VIDEO_INFO_WIDTH(&inputInfo);
+    int height = GST_VIDEO_INFO_HEIGHT(&inputInfo);
+    unsigned byteLength = GST_VIDEO_INFO_SIZE(&inputInfo);
+    auto bufferStorage = JSC::ArrayBuffer::create(width * height, 4);
+    auto outputBuffer = adoptGRef(gst_buffer_new_wrapped_full(GST_MEMORY_FLAG_NO_SHARE, bufferStorage->data(), byteLength, 0, byteLength, nullptr, [](gpointer) { }));
+    gst_buffer_add_video_meta(outputBuffer.get(), GST_VIDEO_FRAME_FLAG_NONE, GST_VIDEO_FORMAT_RGBA, width, height);
+    GstMappedFrame outputFrame(outputBuffer.get(), outputInfo, GST_MAP_WRITE);
+
+    GUniquePtr<GstVideoConverter> converter(gst_video_converter_new(&inputInfo, &outputInfo, nullptr));
+    GstMappedFrame inputFrame(gst_sample_get_buffer(m_sample.get()), inputInfo, GST_MAP_READ);
+    gst_video_converter_frame(converter.get(), inputFrame.get(), outputFrame.get());
+    return JSC::Uint8ClampedArray::tryCreate(WTFMove(bufferStorage), 0, byteLength);
 }
 
 void MediaSampleGStreamer::extendToTheBeginning()