WebKit/WebKit-http

commit b7bf88383cc0eeada58e3d8c346e19a0ab1aab97

[iOS][FCR] Add new look for progress bars https://bugs.webkit.org/show_bug.cgi?id=218864 <rdar://problem/71334958>
Reviewed by Darin Adler and Wenson Hsieh.

Source/WebCore:

Tests: fast/forms/ios/form-control-refresh/progress/filled-appearance.html
       fast/forms/ios/form-control-refresh/progress/indeterminate-appearance.html
       fast/forms/ios/form-control-refresh/progress/rtl-appearance.html
       fast/forms/ios/form-control-refresh/progress/style-height.html
       fast/forms/ios/form-control-refresh/progress/style-width.html

* platform/graphics/FloatRoundedRect.h:
(WebCore::FloatRoundedRect::Radii::Radii):

Added a new initializer to be used when all radii have the same value
but the horizontal and vertical components of an individual radius are
not equal.

* rendering/RenderTheme.cpp:
(WebCore::RenderTheme::animationRepeatIntervalForProgressBar const):
(WebCore::RenderTheme::animationDurationForProgressBar const):
* rendering/RenderTheme.h: Updated methods to take const parameter.
* rendering/RenderThemeAdwaita.cpp:
(WebCore::RenderThemeAdwaita::animationRepeatIntervalForProgressBar const):
(WebCore::RenderThemeAdwaita::animationDurationForProgressBar const):
* rendering/RenderThemeAdwaita.h:
* rendering/RenderThemeIOS.h:
* rendering/RenderThemeIOS.mm:
(WebCore::RenderThemeIOS::paintProgressBar):
(WebCore::RenderThemeIOS::animationRepeatIntervalForProgressBar const):
(WebCore::RenderThemeIOS::paintProgressBarFCR):

Updated appearance of determinate progress bars to match UIKit.

Added an animated indeterminate progress bar, by implementing the
animationRepeatIntervalForProgressBar method in RenderThemeIOS. This
matches RenderThemeMac. On macOS, the animation start time and the
current time are forwarded to CoreUI for drawing. However, on iOS, we
use this information to paint the animated progress bar ourselves.

* rendering/RenderThemeMac.h:
* rendering/RenderThemeMac.mm: Renamed constant for accuracy.

Source/WebCore/rendering/RenderThemeIOS.mm
@@ -907,6 +907,11 @@ bool RenderThemeIOS::paintSliderThumbDecorations(const RenderObject& box, const 
 
 bool RenderThemeIOS::paintProgressBar(const RenderObject& renderer, const PaintInfo& paintInfo, const IntRect& rect)
 {
+#if ENABLE(IOS_FORM_CONTROL_REFRESH)
+    if (renderer.settings().iOSFormControlRefreshEnabled())
+        return paintProgressBarFCR(renderer, paintInfo, rect);
+#endif
+
     if (!is<RenderProgress>(renderer))
         return true;
 
@@ -2065,6 +2070,70 @@ bool RenderThemeIOS::paintRadio(const RenderObject& box, const PaintInfo& paintI
         context.setFillColor(controlBackgroundColor);
         context.fillEllipse(rect);
     }
+
+    return false;
+}
+
+// Animate the indeterminate progress bar at 30 fps. This value was chosen to
+// ensure a smooth animation, while trying to reduce the number of times the
+// progress bar is repainted.
+constexpr Seconds progressAnimationRepeatInterval = 33_ms;
+
+Seconds RenderThemeIOS::animationRepeatIntervalForProgressBar(const RenderProgress&) const
+{
+    return progressAnimationRepeatInterval;
+}
+
+bool RenderThemeIOS::paintProgressBarFCR(const RenderObject& renderer, const PaintInfo& paintInfo, const IntRect& rect)
+{
+    if (!is<RenderProgress>(renderer))
+        return true;
+    auto& renderProgress = downcast<RenderProgress>(renderer);
+
+    auto& context = paintInfo.context();
+    GraphicsContextStateSaver stateSaver(context);
+
+    constexpr auto barHeight = 4.0f;
+    FloatRoundedRect::Radii barCornerRadii(2.5f, 1.5f);
+
+    if (rect.height() < barHeight) {
+        // The rect is smaller than the standard progress bar. We clip to the
+        // element's rect to avoid leaking pixels outside the repaint rect.
+        context.clip(rect);
+    }
+
+    float barTop = rect.y() + (rect.height() - barHeight) / 2.0f;
+
+    FloatRect trackRect(rect.x(), barTop, rect.width(), barHeight);
+    FloatRoundedRect roundedTrackRect(trackRect, barCornerRadii);
+    context.fillRoundedRect(roundedTrackRect, controlBackgroundColor);
+
+    float barWidth;
+    float barLeft = rect.x();
+
+    if (renderProgress.isDeterminate()) {
+        barWidth = clampTo<float>(renderProgress.position(), 0.0f, 1.0f) * trackRect.width();
+
+        if (!renderProgress.style().isLeftToRightDirection())
+            barLeft = trackRect.maxX() - barWidth;
+    } else {
+        barWidth = 0.25f * trackRect.width();
+
+        Seconds elapsed = MonotonicTime::now() - renderProgress.animationStartTime();
+        float position = fmodf(elapsed.value(), 1.0f);
+        float offset = position * (trackRect.width() + barWidth);
+
+        bool reverseDirection = static_cast<int>(elapsed.value()) % 2;
+        if (reverseDirection)
+            barLeft = trackRect.maxX() - offset;
+        else
+            barLeft -= barWidth - offset;
+
+        context.clipRoundedRect(roundedTrackRect);
+    }
+
+    FloatRect barRect(barLeft, barTop, barWidth, barHeight);
+    context.fillRoundedRect(FloatRoundedRect(barRect, barCornerRadii), controlColor);
 
     return false;
 }

LayoutTests/fast/forms/ios/form-control-refresh/progress/filled-appearance-expected-mismatch.html
@@ -1,0 +1,9 @@
+<!DOCTYPE html> <!-- webkit-test-runner [ IOSFormControlRefreshEnabled=true ] -->
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+<body>
+<progress value="0.0"></progress>
+</body>
+</html>

LayoutTests/fast/forms/ios/form-control-refresh/progress/filled-appearance.html
@@ -1,0 +1,9 @@
+<!DOCTYPE html> <!-- webkit-test-runner [ IOSFormControlRefreshEnabled=true ] -->
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+<body>
+<progress value="0.6"></progress>
+</body>
+</html>

LayoutTests/fast/forms/ios/form-control-refresh/progress/indeterminate-appearance-expected-mismatch.html
@@ -1,0 +1,9 @@
+<!DOCTYPE html> <!-- webkit-test-runner [ IOSFormControlRefreshEnabled=true ] -->
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+<body>
+<progress value="0.0"></progress>
+</body>
+</html>

LayoutTests/fast/forms/ios/form-control-refresh/progress/indeterminate-appearance.html
@@ -1,0 +1,9 @@
+<!DOCTYPE html> <!-- webkit-test-runner [ IOSFormControlRefreshEnabled=true ] -->
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+<body>
+<progress></progress>
+</body>
+</html>

LayoutTests/fast/forms/ios/form-control-refresh/progress/rtl-appearance-expected-mismatch.html
@@ -1,0 +1,9 @@
+<!DOCTYPE html> <!-- webkit-test-runner [ IOSFormControlRefreshEnabled=true ] -->
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+<body>
+<progress value="0.5" dir="ltr"></progress>
+</body>
+</html>

LayoutTests/fast/forms/ios/form-control-refresh/progress/rtl-appearance.html
@@ -1,0 +1,9 @@
+<!DOCTYPE html> <!-- webkit-test-runner [ IOSFormControlRefreshEnabled=true ] -->
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+<body>
+<progress value="0.5" dir="rtl"></progress>
+</body>
+</html>

LayoutTests/fast/forms/ios/form-control-refresh/progress/style-height-expected.html
@@ -1,0 +1,18 @@
+<!DOCTYPE html> <!-- webkit-test-runner [ IOSFormControlRefreshEnabled=true ] -->
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<style>
+
+progress {
+    position: absolute;
+    top: 42px;
+    left: 0px;
+}
+
+</style>
+</head>
+<body>
+<progress value="0.5"></progress>
+</body>
+</html>

commit 926a6541ce9a7419a1252ffbf59ee1ea91d35607

REGRESSION: [iOS] imported/w3c/web-platform-tests/css/css-ui/appearance-revert-001.tentative.html is a flaky image failure https://bugs.webkit.org/show_bug.cgi?id=219410 <rdar://problem/71868276>
Reviewed by Wenson Hsieh.

The flaky failure started appearing after r270065, which introduced a
new look for progress bars when the iOSFormControlRefresh setting is
enabled. Animated indeterminate progress bars were added as part of the
new appearance.

However, since the setting is disabled by default, there should have
been no effect on existing tests. r270065 omitted an early return in
RenderThemeIOS::animationRepeatIntervalForProgressBar, which would
lead to the existing progress bar being repainted 30 times per second.
This repainting is the likely cause of the flaky image failure.

Furthermore, this test uses a determinate progress bar, so repainting
due to animation should never occur, regardless of whether or not the
iOSFormControlRefresh setting is enabled. To fix the incorrect behavior,
a change was made to RenderProgress::updateAnimationState.

* rendering/RenderProgress.cpp:
(WebCore::RenderProgress::updateAnimationState):

Only indeterminate progress bars have an animation.

* rendering/RenderThemeIOS.mm:
(WebCore::RenderThemeIOS::animationRepeatIntervalForProgressBar const):

Use the original value if the iOSFormControlRefresh setting is disabled.


git-svn-id: http://svn.webkit.org/repository/webkit/trunk@270346 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Source/WebCore/rendering/RenderProgress.cpp
@@ -88,7 +88,7 @@ void RenderProgress::updateAnimationState()
     m_animationDuration = theme().animationDurationForProgressBar(*this);
     m_animationRepeatInterval = theme().animationRepeatIntervalForProgressBar(*this);
 
-    bool animating = style().hasAppearance() && m_animationRepeatInterval > 0_s;
+    bool animating = style().hasAppearance() && m_animationRepeatInterval > 0_s && !isDeterminate();
     if (animating == m_animating)
         return;
 

Source/WebCore/rendering/RenderThemeIOS.mm
@@ -2069,8 +2069,11 @@ bool RenderThemeIOS::paintRadio(const RenderObject& box, const PaintInfo& paintI
 // progress bar is repainted.
 constexpr Seconds progressAnimationRepeatInterval = 33_ms;
 
-Seconds RenderThemeIOS::animationRepeatIntervalForProgressBar(const RenderProgress&) const
+Seconds RenderThemeIOS::animationRepeatIntervalForProgressBar(const RenderProgress& renderProgress) const
 {
+    if (!renderProgress.settings().iOSFormControlRefreshEnabled())
+        return RenderTheme::animationRepeatIntervalForProgressBar(renderProgress);
+
     return progressAnimationRepeatInterval;
 }
 