WebKit/WebKit-http

commit 14181b4d6740d9410cc49f90c099a0cb4b50b135

Adopt new NSURLSessionConfiguration SPI for connection cache configuration https://bugs.webkit.org/show_bug.cgi?id=222934
Reviewed by Geoffrey Garen.

Source/WebCore:

This uses the NSURLSessionConfiguration connection cache limit SPI introduced in Big Sur to
properly set the parameters we want for HTTP/1.1 connections. Previously we tried to do this
using _CFNetworkHTTPConnectionCacheSetLimit in NetworkProcessCocoa, but this didn't work
because that SPI only applies to NSURLConnection rather than NSURLSession.

In particular, this meant that the number of priority levels wasn't set correctly, which we
had to work around by constraining the number of priority levels when mapping WebKit
resource priorities to CFNetwork priorities (https://bugs.webkit.org/show_bug.cgi?id=203423).
This patch adopts the SPI and removes that workaround.

* platform/network/cf/ResourceRequestCFNet.h:
(WebCore::toResourceLoadPriority):
(WebCore::toPlatformRequestPriority):

Source/WebCore/PAL:

Declare NSURLSessionConfiguration connection cache limit SPI when building using the public SDK.

* pal/spi/cf/CFNetworkSPI.h:

Source/WebKit:

This uses the NSURLSessionConfiguration connection cache limit SPI introduced in Big Sur to
properly set the parameters we want for HTTP/1.1 connections. Previously we tried to do this
using _CFNetworkHTTPConnectionCacheSetLimit in NetworkProcessCocoa, but this didn't work
because that SPI only applies to NSURLConnection rather than NSURLSession.

In particular, this meant that the number of priority levels wasn't set correctly, which we
had to work around by constraining the number of priority levels when mapping WebKit
resource priorities to CFNetwork priorities (https://bugs.webkit.org/show_bug.cgi?id=203423).
This patch adopts the SPI and removes that workaround.

* NetworkProcess/cocoa/NetworkSessionCocoa.mm:
(WebKit::configurationForSessionID):

Source/WTF:

Add the HAVE_CFNETWORK_NSURLSESSION_CONNECTION_CACHE_LIMITS flag to control whether or not

Source/WebCore/platform/network/cf/ResourceRequestCFNet.h
@@ -44,9 +44,49 @@ void getResourceRequest(ResourceRequest&, CFURLRequestRef);
 CFURLRequestRef cfURLRequest(const ResourceRequest&);
 #endif
 
+#if HAVE(CFNETWORK_NSURLSESSION_CONNECTION_CACHE_LIMITS)
 inline ResourceLoadPriority toResourceLoadPriority(CFURLRequestPriority priority)
 {
-    // FIXME: switch VeryLow back to 0 priority when CFNetwork fixes <rdar://problem/56621205>
+    switch (priority) {
+    case 0:
+        return ResourceLoadPriority::VeryLow;
+    case 1:
+        return ResourceLoadPriority::Low;
+    case 2:
+        return ResourceLoadPriority::Medium;
+    case 3:
+        return ResourceLoadPriority::High;
+    case 4:
+        return ResourceLoadPriority::VeryHigh;
+    default:
+        ASSERT_NOT_REACHED();
+        return ResourceLoadPriority::Lowest;
+    }
+}
+
+inline CFURLRequestPriority toPlatformRequestPriority(ResourceLoadPriority priority)
+{
+    switch (priority) {
+    case ResourceLoadPriority::VeryLow:
+        return 0;
+    case ResourceLoadPriority::Low:
+        return 1;
+    case ResourceLoadPriority::Medium:
+        return 2;
+    case ResourceLoadPriority::High:
+        return 3;
+    case ResourceLoadPriority::VeryHigh:
+        return 4;
+    }
+
+    ASSERT_NOT_REACHED();
+    return 0;
+}
+
+#else
+
+inline ResourceLoadPriority toResourceLoadPriority(CFURLRequestPriority priority)
+{
     switch (priority) {
     case -1:
         return ResourceLoadPriority::VeryLow;
@@ -66,7 +106,6 @@ inline ResourceLoadPriority toResourceLoadPriority(CFURLRequestPriority priority
 
 inline CFURLRequestPriority toPlatformRequestPriority(ResourceLoadPriority priority)
 {
-    // FIXME: switch VeryLow back to 0 priority when CFNetwork fixes <rdar://problem/56621205>
     switch (priority) {
     case ResourceLoadPriority::VeryLow:
         return -1;
@@ -83,6 +122,8 @@ inline CFURLRequestPriority toPlatformRequestPriority(ResourceLoadPriority prior
     ASSERT_NOT_REACHED();
     return 0;
 }
+#endif
+
 
 inline RetainPtr<CFStringRef> httpHeaderValueUsingSuitableEncoding(HTTPHeaderMap::const_iterator::KeyValue header)
 {

Source/WTF/wtf/PlatformHave.h
@@ -416,6 +416,13 @@
 #define HAVE_CFNETWORK_NSURLSESSION_STRICTRUSTEVALUATE 1
 #endif
 
+#if (PLATFORM(MAC) && __MAC_OS_X_VERSION_MIN_REQUIRED >= 110000) \
+    || (PLATFORM(IOS) && __IPHONE_OS_VERSION_MIN_REQUIRED >= 140000) \
+    || (PLATFORM(WATCHOS) && __WATCH_OS_VERSION_MIN_REQUIRED >= 70000) \
+    || (PLATFORM(APPLETV) && __TV_OS_VERSION_MIN_REQUIRED >= 140000)
+#define HAVE_CFNETWORK_NSURLSESSION_CONNECTION_CACHE_LIMITS 1
+#endif
+
 #if (PLATFORM(MAC) && __MAC_OS_X_VERSION_MIN_REQUIRED >= 101500) || PLATFORM(IOS_FAMILY)
 #define HAVE_CFNETWORK_NEGOTIATED_SSL_PROTOCOL_CIPHER 1
 #endif

Source/WebCore/PAL/pal/spi/cf/CFNetworkSPI.h
@@ -242,6 +242,11 @@ typedef NS_ENUM(NSInteger, NSURLSessionCompanionProxyPreference) {
 #if HAVE(NETWORK_LOADER)
 @property BOOL _usesNWLoader;
 #endif
+#if HAVE(CFNETWORK_NSURLSESSION_CONNECTION_CACHE_LIMITS)
+@property (readwrite, assign) NSInteger _connectionCacheNumPriorityLevels;
+@property (readwrite, assign) NSInteger _connectionCacheNumFastLanes;
+@property (readwrite, assign) NSInteger _connectionCacheMinimumFastLanePriority;
+#endif
 @end
 
 @interface NSURLSessionTask ()

Source/WebKit/NetworkProcess/cocoa/NetworkSessionCocoa.mm
@@ -1078,6 +1078,15 @@ ALLOW_DEPRECATED_DECLARATIONS_BEGIN
     configuration._allowsSensitiveLogging = NO;
 ALLOW_DEPRECATED_DECLARATIONS_END
 #endif
+
+#if HAVE(CFNETWORK_NSURLSESSION_CONNECTION_CACHE_LIMITS)
+    if (WebCore::ResourceRequest::resourcePrioritiesEnabled()) {
+        configuration._connectionCacheNumPriorityLevels = WebCore::resourceLoadPriorityCount;
+        configuration._connectionCacheMinimumFastLanePriority = toPlatformRequestPriority(WebCore::ResourceLoadPriority::Medium);
+        configuration._connectionCacheNumFastLanes = 1;
+    }
+#endif
+
     return configuration;
 }
 

commit 33e6443d260bb9f1f57dde28b4584d2c76e7b15d

REGRESSION: [BigSur] ASSERT NOT REACHED in WebCore::ResourceLoadPriority WebCore::toResourceLoadPriority https://bugs.webkit.org/show_bug.cgi?id=222998
Unreviewed, fix the debug assert caused by r274161.

CFURLRequestGetRequestPriority can return -1 for requests with no priority set (e.g. some
requests from the media stack). We need to handle this case when transforming a
CFURLRequestPriority to a ResourceLoadPriority.

* platform/network/cf/ResourceRequestCFNet.h:
(WebCore::toResourceLoadPriority):

git-svn-id: http://svn.webkit.org/repository/webkit/trunk@274180 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Source/WebCore/platform/network/cf/ResourceRequestCFNet.h
@@ -48,6 +48,7 @@ CFURLRequestRef cfURLRequest(const ResourceRequest&);
 inline ResourceLoadPriority toResourceLoadPriority(CFURLRequestPriority priority)
 {
     switch (priority) {
+    case -1:
     case 0:
         return ResourceLoadPriority::VeryLow;
     case 1: