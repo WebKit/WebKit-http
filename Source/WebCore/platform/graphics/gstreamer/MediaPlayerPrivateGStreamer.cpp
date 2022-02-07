b61844acf9a48bc1d3638561099f58ee57a74c2d
Eugene Mutavchi <Ievgen_Mutavchi@comcast.com>
2022-02-07 18:16:26 +0000

    Disable sync on clock for westeros sink

--- Source/WebCore/platform/graphics/gstreamer/MediaPlayerPrivateGStreamer.cpp
+++ Source/WebCore/platform/graphics/gstreamer/MediaPlayerPrivateGStreamer.cpp
@@ -1035,8 +1035,10 @@ static void setSyncOnClock(GstElement *element, bool sync)
 
 void MediaPlayerPrivateGStreamer::syncOnClock(bool sync)
 {
+#if !USE(WESTEROS_SINK)
     setSyncOnClock(videoSink(), sync);
     setSyncOnClock(audioSink(), sync);
+#endif
 }
 
 void MediaPlayerPrivateGStreamer::notifyPlayerOfVideo()
