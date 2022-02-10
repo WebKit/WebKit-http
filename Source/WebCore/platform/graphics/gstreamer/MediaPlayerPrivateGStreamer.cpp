b7d40d433e4370ecf838b8658444867260c724a2
Xabier Rodriguez Calvar <calvaris@igalia.com>
2022-02-10 11:10:32 +0100

    Revert "[GStreamer] Control on-disk multimedia buffering via the WPE_SHELL_DISABLE_MEDIA_DISK_CACHE environment variable"
    This reverts commit 67551255c6836f142ad619c8c2cebfb3c0747cc3.

--- Source/WebCore/platform/graphics/gstreamer/MediaPlayerPrivateGStreamer.cpp
+++ Source/WebCore/platform/graphics/gstreamer/MediaPlayerPrivateGStreamer.cpp
@@ -2751,24 +2751,6 @@ MediaPlayer::SupportsType MediaPlayerPrivateGStreamer::supportsType(const MediaE
     return finalResult;
 }
 
-bool isMediaDiskCacheDisabled()
-{
-    static bool result = false;
-    static bool computed = false;
-
-    if (computed)
-        return result;
-
-    String s(std::getenv("WPE_SHELL_DISABLE_MEDIA_DISK_CACHE"));
-    if (!s.isEmpty()) {
-        String value = s.stripWhiteSpace().convertToLowercaseWithoutLocale();
-        result = (value=="1" || value=="t" || value=="true");
-    }
-
-    computed = true;
-    return result;
-}
-
 void MediaPlayerPrivateGStreamer::updateDownloadBufferingFlag()
 {
     if (!m_pipeline)
@@ -2792,10 +2774,7 @@ void MediaPlayerPrivateGStreamer::updateDownloadBufferingFlag()
         return;
     }
 
-    bool diskCacheDisabled = isMediaDiskCacheDisabled();
-    GST_DEBUG_OBJECT(pipeline(), "Media on-disk cache is %s", (diskCacheDisabled) ? "disabled":"enabled");
-
-    bool shouldDownload = !m_isLiveStream && m_preload == MediaPlayer::Preload::Auto && !diskCacheDisabled;
+    bool shouldDownload = !m_isLiveStream && m_preload == MediaPlayer::Preload::Auto;
     if (shouldDownload) {
         GST_INFO_OBJECT(pipeline(), "Enabling on-disk buffering");
         g_object_set(m_pipeline.get(), "flags", flags | flagDownload, nullptr);
